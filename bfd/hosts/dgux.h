e4a0e98e2b0e10938cf1ef7efff1e15af57830c8
Steve Chamberlain <steve@cygnus>
1991-04-03 22:19:10 +0000

    Initial revision

---
+++ bfd/hosts/dgux.h
@@ -0,0 +1,15 @@
+#include <sys/dg_c_generics.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <ctype.h>
+
+#include <stdio.h>
+
+#define NO_STDARG 1
+
+
+
