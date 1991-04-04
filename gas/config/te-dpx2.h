eb4edf5df2ec5960a3b7ad76f5be23ddd7278516
K. Richard Pixley <rich@cygnus>
1991-04-04 18:19:55 +0000

    new gas main line

---
+++ gas/config/te-dpx2.h
@@ -0,0 +1,10 @@
+/* Machine specific defines for the dpx2 machine */
+#define dpx2
+#define TC_M68K
+
+/* Preserve leading underscore in symbol names. */
+#define STRIP_UNDERSCORE 0
+
+/* The magic number is not the usual MC68MAGIC. */
+#define FILE_HEADER_MAGIC       MC68KBCSMAGIC
+
