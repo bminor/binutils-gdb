e6129e2bc2e5e31bdbfba725980a62febe44ac64
Steve Chamberlain <steve@cygnus>
1991-06-14 20:50:29 +0000

    Made it better

--- bfd/hosts/dgux.h
+++ bfd/hosts/dgux.h
@@ -1,7 +1,8 @@
 #include <errno.h>
+#include <sys/types.h>
 #include <sys/dg_c_generics.h>
 #include <unistd.h>
-#include <stdlib.h>
+
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -10,6 +11,16 @@
 
 #include <stdio.h>
 
+#ifndef DONTDECLARE_MALLOC
+extern PROTO(PTR,malloc,(unsigned));
+extern PROTO(PTR ,realloc, (PTR, unsigned));
+#endif
+extern PROTO(int, abort,(void));
+extern PROTO(int, free,(PTR));
+extern PROTO(void, bcopy,(char*,char*,int));
+extern PROTO(void, exit,(int));
+extern int strtol();
+extern PROTO(void, bzero,(char *, int));
 #define NO_STDARG 1
 
 
