e89f2fbe008a72300bab52dc4145fcf338ae3f18
Steve Chamberlain <steve@cygnus>
1991-10-01 03:43:11 +0000

    Initial revision

---
+++ bfd/cpu-a29k.c
@@ -0,0 +1,32 @@
+#include <sysdep.h>
+#include <bfd.h>
+#include "libbfd.h"
+
+
+
+static bfd_arch_info_struct_type arch_info_struct = 
+  {
+    32,	/* 32 bits in a word */
+    32,	/* 32 bits in an address */
+    8,	/* 8 bits in a byte */
+    bfd_arch_a29k,
+    0,	/* only 1 machine */
+    "a29k",
+    "a29k",
+    true, /* the one and only */
+    bfd_default_compatible, 
+    bfd_default_scan ,
+    0,
+    0,
+  };
+
+
+
+
+void DEFUN_VOID(bfd_a29k_arch)
+{
+  bfd_arch_linkin(&arch_info_struct);
+}
+
+
+
