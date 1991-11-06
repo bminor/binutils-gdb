ac7d9f2dd129a5df2e8d244c7c08f236171ed074
K. Richard Pixley <rich@cygnus>
1991-11-06 02:23:49 +0000

    adjust STRIP_UNDERSCORE, polish te-files

--- gas/config/te-dpx2.h
+++ gas/config/te-dpx2.h
@@ -2,9 +2,6 @@
 #define dpx2
 #define TC_M68K
 
-/* Preserve leading underscore in symbol names. */
-#define STRIP_UNDERSCORE 0
-
 /* The magic number is not the usual MC68MAGIC. */
 #define FILE_HEADER_MAGIC       MC68KBCSMAGIC
 
--- gas/config/te-sco386.h
+++ gas/config/te-sco386.h
@@ -3,6 +3,3 @@
 
 /* Return true if s (a non null string pointer), points to a local variable name. */
 #define LOCAL_LABEL(n)  ((n)[0] == '.' && (n)[1] == 'L')
-
-/* Compiler does not generate symbol names with a leading underscore. */
-#define STRIP_UNDERSCORE 0
