cf0cdb8751c96075b833e64af3aaea4d4fd012f5
Per Bothner <per@bothner.com>
1991-11-18 08:04:02 +0000

    Initial revision

---
+++ binutils/is-strip.c
@@ -0,0 +1,3 @@
+/* Linked with copy.o to flag that this program is 'strip' (not 'copy'). */
+
+int is_strip = 1;
---
+++ binutils/not-strip.c
@@ -0,0 +1,3 @@
+/* Linked with copy.o to flag that this program is 'copy' (not 'strip'). */
+
+int is_strip = 0;
