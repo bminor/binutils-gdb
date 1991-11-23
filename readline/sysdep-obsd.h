ec230564a6113c29c47a5b96c0e339cc2974b190
John Gilmore <gnu@cygnus>
1991-11-23 01:49:55 +0000

    Initial revision

---
+++ readline/sysdep-obsd.h
@@ -0,0 +1,11 @@
+/* System-dependent stuff, for Sony NEwS, Mach, and other systems 
+   in the "old BSD Unix" tradition.  */
+
+#ifdef __GNUC__
+#define alloca __builtin_alloca
+#else
+extern char *alloca ();
+#endif
+
+#include <sys/dir.h>
+typedef struct direct dirent;
