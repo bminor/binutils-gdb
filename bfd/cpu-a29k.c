431b4422004af53c4704cad0d554e694d3631cf4
Steve Chamberlain <sac@cygnus>
1991-12-04 18:30:12 +0000

    * aoutf1.h (sunos_<size>_object_p, sunos4_callback): moved the 	computation of arch and machine type so it's worked out before 	some_aout_object_p is called. 	* aoutx.h: (some_aout_object_p): don't set arch and mach to 	unknown. 	* archures.c: add extra field 'section_align_power' to hold 	default section alignment in object files. 	* cpu-a29k.c, cpu-h8300.c, cpu-i386.c, cpu-i960.c, cpu-m68k.c, 	cpu-m88k.c, cpu-mips.c, cpu-rs6000.c, cpu-sparc.c, cpu-vax.c: 	initialize the new field. 	* bout.c (b_out_callback): replace the text size, since aout 	fries them. 	* aoutx.h: Documentation in the new style

--- bfd/cpu-a29k.c
+++ bfd/cpu-a29k.c
@@ -11,6 +11,7 @@ static bfd_arch_info_type arch_info_struct =
     0,	/* only 1 machine */
     "a29k",
     "a29k",
+    4,
     true, /* the one and only */
     bfd_default_compatible, 
     bfd_default_scan ,
