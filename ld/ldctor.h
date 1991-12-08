bc71278ddd65a494ddd9a7eb9d47d132f49fdaab
Steve Chamberlain <sac@cygnus>
1991-12-08 01:21:25 +0000

    Initial revision

---
+++ ld/ldctor.h
@@ -0,0 +1,9 @@
+
+void EXFUN(find_constructors,(void));
+void EXFUN(ldlang_check_for_constructors,(
+				     struct
+				     lang_input_statement_struct *));
+
+
+void EXFUN(ldlang_add_constructor,(ldsym_type *name));
+
