9d8728716def41c6f9797d09c96f0e63d825f9fc
Fred Fish <fnf@specifix.com>
1991-12-20 19:08:12 +0000

    Renamed from tm-svr4.h to tm-sysv4.h

---
+++ gdb/tm-sysv4.h
@@ -0,0 +1,37 @@
+/* Macro definitions for GDB on all SVR4 target systems.
+   Copyright (C) 1991, Free Software Foundation, Inc.
+   Written by Fred Fish at Cygnus Support (fnf@cygint)
+
+This file is part of GDB.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* Support for SVR4 shared libraries. */
+
+#define CLEAR_SOLIB			clear_solib
+extern void clear_solib ();			/* solib.c */
+
+#define SOLIB_ADD(filename, from_tty, targ) solib_add (filename, from_tty, targ)
+extern void solib_add ();			/* solib.c */
+
+#define SOLIB_CREATE_INFERIOR_HOOK	solib_create_inferior_hook
+extern void solib_create_inferior_hook();	/* solib.c */
+
+/* If we can't set a breakpoint, and it's in a shared library, just
+   disable it.  */
+
+#define DISABLE_UNSETTABLE_BREAK(addr)	solib_address(addr)
+extern int solib_address ();			/* solib.c */
+
