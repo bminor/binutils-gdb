287f81eb64be5ef0e808b6022e45f15cfcb37d9c
John Gilmore <gnu@cygnus>
1992-01-31 08:49:34 +0000

    * xm-vax.h:  Include some system include files before our own definitions in defs.h.

--- gdb/xm-vax.h
+++ gdb/xm-vax.h
@@ -17,6 +17,11 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
+/* We have to include these files now, so that GDB will not make
+   competing definitions in defs.h.  */
+#include <machine/endian.h>
+#include <machine/limits.h>
+
 #define HOST_BYTE_ORDER LITTLE_ENDIAN
 
 /* Get rid of any system-imposed stack limit if possible.  */
