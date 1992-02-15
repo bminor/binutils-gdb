7e5c1057bb33fb6be25ada95ba7f58066d88de80
Per Bothner <per@bothner.com>
1992-02-15 23:13:00 +0000

    * Makefile.in:  Major changes.  Removed some the sed 	magic to converts scripts, since that is now handled 	by genscripts.sh and the *.sc-sh scipt generators. 	* config.h:  Remove a bunch of macros defining emulations 	and targets.  This becomes one less file to edit when 	adding emulations or targets. 	* ldemul.h (struct ld_emulation_xfer_struct):  Add 	emulation_name and target_name fields. 	* ldemul.c, ldemul.h:  Define some default functions used 	by most emulations (and remove from the *.em scripts). 	* ldemul.c (ldemul_choose_target):  Search the new 	ld_emulations array using a loop (instead of a hardwired 	nested if statement). 	Define the ld_emulation from the automatically-geenrated 	ldemul-list.h.  This means you no longer have to edit ldemul.c 	to add a new emulation. 	* ldmain.c:  Replace {GLD,LNK}960_EMULATION_NAME by 	their expansions, since the former no longer exist. 	* PORTING:  A very rough first draft of a porting guide.

--- ld/config.h
+++ ld/config.h
@@ -21,44 +21,16 @@
 /* Look in this environment name for the linker to pretend to be */
 #define EMULATION_ENVIRON "LDEMULATION"
 /* If in there look for the strings: */
-#define GLD_EMULATION_NAME "gld"
-#define VANILLA_EMULATION_NAME "vanilla"
-#define GLDM88KBCS_EMULATION_NAME "gldm88kbcs"
-#define GLD68K_EMULATION_NAME "gld68k"
-#define GLD960_EMULATION_NAME "gld960"
-#define GLD29K_EMULATION_NAME "gld29k"
-#define GLDNEWS_EMULATION_NAME "gldnews"
-#define LNK960_EMULATION_NAME "lnk960"
-#define H8300HMS_EMULATION_NAME "h8300hms"
-#define EBMON29K_EMULATION_NAME "ebmon29k"
-#define GLDI386AOUT_EMULATION_NAME "gldi386aout"
 /* Otherwise default to this emulation */
 
 #ifndef DEFAULT_EMULATION
-#ifdef GNU960
-#define DEFAULT_EMULATION GLD960_EMULATION_NAME
-#else
-#define DEFAULT_EMULATION GLD68K_EMULATION_NAME
-#endif
+#define DEFAULT_EMULATION "unknown"
 #endif /* DEFAULT_EMULATION */
 
 
 
 /* Look in this variable for a target format */
 #define TARGET_ENVIRON "GNUTARGET"
-/* If not there then choose this */
-#define GLD_TARGET "a.out-sunos-big"
-#define GLD68K_TARGET "a.out-sunos-big"
-#define LNK960_TARGET "coff-Intel-big"
-#define LNK960_TARGET_LITTLE "coff-Intel-little"
-#define GLD960_TARGET "b.out.big"
-#define VANILLA_TARGET "a.out-sunos-big"
-#define GLDM88KBCS_TARGET "m88kbcs"
-#define GLD29K_TARGET "coff-a29k-big"
-#define GLDI386AOUT_TARGET "a.out-i386"
-#define GLDNEWS_TARGET "a.out-newsos3"
-#define H8300HMS_TARGET "coff-h8300"
-#define EBMON29K_TARGET "coff-a29k-big"
 
 
 
