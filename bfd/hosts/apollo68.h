1e09a701a3d36eaf99d5eed956f669d95c3578eb
Sean Eric Fagan <sef@cygnus>
1992-08-18 19:49:58 +0000

    Need L_SET defined on the apollo, at least under BSD emulation mode.

--- bfd/hosts/apollo68.h
+++ bfd/hosts/apollo68.h
@@ -17,21 +17,9 @@
 #define	MAXPATHLEN	1024
 #endif	/* MAXPATHLEN */
 
-/*
- * Might not need these. Leave them out for now.
- *
-#ifdef SEEK_SET
-#	ifndef L_SET
-#		define L_SET SEEK_SET
-#	endif
-# endif
-
-#ifdef SEEK_CUR
-#	ifndef L_INCR
-#		define L_INCR SEEK_CUR
-#	endif
-# endif
- */
+#ifndef L_SET
+#	define L_SET 0
+#endif
 
 #ifndef DONTDECLARE_MALLOC
 extern PTR  EXFUN(malloc,(unsigned));
