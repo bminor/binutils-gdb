ede9d8a7eaa06a7176bc47fa96d037aa194e67df
John Gilmore <gnu@cygnus>
1992-10-16 11:35:02 +0000

    Split native dependent pieces out into new nm-file.

--- gdb/i386b-nat.c
+++ gdb/i386b-nat.c
@@ -1,5 +1,5 @@
-/* 386BSD host interface.
-   Copyright (C) 1988, 1989, 1991 Free Software Foundation, Inc.
+/* Native-dependent code for BSD Unix running on i386's, for GDB.
+   Copyright 1988, 1989, 1991, 1992 Free Software Foundation, Inc.
 
 This file is part of GDB.
 
@@ -18,10 +18,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #include "defs.h"
-
 #include <machine/reg.h>
 
-
 /* this table must line up with REGISTER_NAMES in tm-i386.h */
 /* symbols like 'tEAX' come from <machine/reg.h> */
 static int tregmap[] = 
