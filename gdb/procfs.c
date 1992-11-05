1549918a3f39110f41e78ab4ebbd022b4fe3da7d
Stu Grossman <grossman@cygnus>
1992-11-05 03:54:01 +0000

    * inflow.c (pass_signal, set_sigint_trap, clear_sigint_trap):  Add new routines to deal with sending SIGINTs to attached processes when the user interrupts the controlling GDB. * inftarg.c (child_wait), procfs.c (procfs_wait):  Add calls to the aforementioned routines when waiting for the attached process.

--- gdb/procfs.c
+++ gdb/procfs.c
@@ -1956,10 +1956,17 @@ procfs_wait (statloc)
     }
   else if (!(pi.prstatus.pr_flags & (PR_STOPPED | PR_ISTOP)))
     {
+      if (attach_flag)
+	set_sigint_trap();	/* Causes SIGINT to be passed on to the
+				   attached process. */
+
       if (ioctl (pi.fd, PIOCWSTOP, &pi.prstatus) < 0)
 	{
 	  checkerr++;
 	}
+
+      if (attach_flag)
+	clear_sigint_trap();
     }    
   if (checkerr)
     {
