71f555668692b85e088bce62a0d9931cc8ca842f
David D. Zuhn <zoo@cygnus>
1993-01-06 10:30:22 +0000

    handle machines other than DOS & Sun

--- gdb/29k-share/udi/udiphcfg.h
+++ gdb/29k-share/udi/udiphcfg.h
@@ -1,11 +1,19 @@
 /* This file just picks the correct udiphxxx.h depending on the host */
 /* The two hosts that are now defined are SUN and MSDOS */
 
-#ifdef sun
-#include "udiphsun.h"
-#endif
+/* The way this now works, all machines other than DOS are considered to
+   be Sun-like.  This is known to work for HP700's.  If this doesn't work
+   for some host, create an alternative udiphXXX.h file.  
+
+   This scheme for determining which file to include is *ugly* and should be
+   fixed at some later date.  -- zoo - 930106
+*/
 
 #ifdef MSDOS
+#define CONFIG_INCLUDED
 #include "udiphdos.h"
 #endif
 
+#ifndef CONFIG_INCLUDED
+#include "udiphsun.h"
+#endif
