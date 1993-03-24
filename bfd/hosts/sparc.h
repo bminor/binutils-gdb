550fb7afd36ae6b7f26fde657c1aa591f62a6780
Ken Raeburn <raeburn@cygnus>
1993-03-24 08:01:07 +0000

    turn on file-position optimizations again

--- bfd/hosts/sparc.h
+++ bfd/hosts/sparc.h
@@ -1,5 +1,5 @@
 #define STDC_HEADERS
-#undef FILE_OFFSET_IS_CHAR_INDEX
+#define FILE_OFFSET_IS_CHAR_INDEX
 #if defined(__STDC__) && __GNUC__ >= 2
 #define abort __hide_abort
 #define exit __hide_exit
