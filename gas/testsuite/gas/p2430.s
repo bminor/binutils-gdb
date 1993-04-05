96b4e5bb5e38f141d9e5da64788013737d619242
Ken Raeburn <raeburn@cygnus>
1993-04-05 17:26:51 +0000

    add some new tests, and new testing procs

--- gas/testsuite/gas/p2430.s
+++ gas/testsuite/gas/p2430.s
@@ -1,3 +1,13 @@
+# We get:
+#
+# m68k-coff-as x.s
+# x.s:5: FATAL:failed sanity check.
+#
+# from the following .s file:
+
 	.sect foo
 tag:
 	bra tag
+
+# Changing the section to .text makes it work, but we need to
+# use multiple sections other than .text to link the way we want.
