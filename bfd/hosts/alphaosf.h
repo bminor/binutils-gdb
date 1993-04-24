5f8f6d56f2c542ccdd85b0bd2c9a6a6661620cb2
Steve Chamberlain <sac@cygnus>
1993-04-24 02:10:09 +0000

    Support for the alpha

---
+++ bfd/hosts/alphaosf.h
@@ -0,0 +1,50 @@
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/file.h>
+#include <alloca.h>
+
+/* Make the basic types 64-bit quantities on the host */
+#define	HOST_64_BIT	long 
+#ifndef DONTDECLARE_MALLOC
+extern PTR  EXFUN(malloc,(unsigned));
+extern PTR  EXFUN(realloc, (PTR, unsigned));
+#endif
+
+
+
+/* EXACT TYPES */
+typedef char int8e_type;
+typedef unsigned char uint8e_type;
+typedef short int16e_type;
+typedef unsigned short uint16e_type;
+typedef int int32e_type;
+typedef unsigned int uint32e_type;
+
+
+
+typedef unsigned  long uint64e_type;
+
+
+/* CORRECT SIZE OR GREATER */
+typedef char int8_type;
+typedef unsigned char uint8_type;
+typedef short int16_type;
+typedef unsigned short uint16_type;
+typedef int int32_type;
+typedef unsigned int uint32_type;
+
+
+typedef unsigned  long uint64_type;
+typedef  long int64_type;
+
+
+#define BYTES_IN_PRINTF_INT 4
+
+#define uint64_typeLOW(x) (uint32_type)(((x) & 0xffffffff))
+#define uint64_typeHIGH(x) (uint32_type)(((x) >> 32) & 0xffffffff)
+#include "fopen-same.h"
