34b70237d2605bfa3ae03d230fe0b9eea8070e5f
Fred Fish <fnf@specifix.com>
1993-05-07 04:02:16 +0000

    * core.c (dis_asm_read_memory):  Cast second arg of 	target_read_memory to "char *". 	* breakpoint.c (watchpoint_check):  Change arg type from PTR to 	"char *", to match other functions called by catch_errors().

--- gdb/core.c
+++ gdb/core.c
@@ -170,7 +170,7 @@ dis_asm_read_memory (memaddr, myaddr, len, info)
      int len;
      disassemble_info *info;
 {
-  return target_read_memory (memaddr, myaddr, len);
+  return target_read_memory (memaddr, (char *) myaddr, len);
 }
 
 /* Like memory_error with slightly different parameters.  */
