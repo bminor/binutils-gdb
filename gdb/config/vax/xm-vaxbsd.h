cdaa790509954e264d9e47fe3a726ddec7983d72
Jim Kingdon <jkingdon@engr.sgi.com>
1993-05-19 02:01:05 +0000

    * config/vax/vaxbsd.mh: Add "NAT_FILE= nm-vax.h". 	* config/vax/xm-vaxbsd.h: Use <sys/param.h> not <machine/limits.h>

--- gdb/config/vax/xm-vaxbsd.h
+++ gdb/config/vax/xm-vaxbsd.h
@@ -1,9 +1,12 @@
-/* Definitions to make GDB run on a vax under 4.2bsd. */
+/* Definitions to make GDB run on a vax under BSD, 4.3 or 4.4. */
 
 /* We have to include these files now, so that GDB will not make
    competing definitions in defs.h.  */
 #include <machine/endian.h>
-#include <machine/limits.h>
+/* This should exist on either 4.3 or 4.4.  4.3 doesn't have limits.h
+   or machine/limits.h.  */
+#include <sys/param.h>
+
 #include "vax/xm-vax.h"
 
 /* In non-ANSI compiles, memcpy and memset are still void *, not char *.  */
