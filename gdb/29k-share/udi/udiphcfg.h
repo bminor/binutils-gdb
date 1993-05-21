976a7ee0b710e8ccddc04ec1fba8f52c2227e8b3
Sean Eric Fagan <sef@cygnus>
1993-05-21 17:51:50 +0000

    Changed the configuration scheme a bit, to be saner, cleaner, etc. Also, the so-called sun-dependent files were not very sun-dependent, so renamed it to "unix."

--- gdb/29k-share/udi/udiphcfg.h
+++ gdb/29k-share/udi/udiphcfg.h
@@ -1,19 +1,15 @@
 /* This file just picks the correct udiphxxx.h depending on the host */
-/* The two hosts that are now defined are SUN and MSDOS */
+/* The two hosts that are now defined are UNIX and MSDOS */
 
-/* The way this now works, all machines other than DOS are considered to
-   be Sun-like.  This is known to work for HP700's.  If this doesn't work
-   for some host, create an alternative udiphXXX.h file.  
+/*
+ * For the moment, we will default to BSD_IPC; this might change if/when
+ * another type of IPC (Mach? SysV?) is implemented.
+ */
 
-   This scheme for determining which file to include is *ugly* and should be
-   fixed at some later date.  -- zoo - 930106
-*/
-
-#ifdef MSDOS
-#define CONFIG_INCLUDED
+#ifdef DOS_IPC
 #include "udiphdos.h"
+#else
+/*#ifdef BSD_IPC */
+#include "udiphunix.h"
 #endif
 
-#ifndef CONFIG_INCLUDED
-#include "udiphsun.h"
-#endif
