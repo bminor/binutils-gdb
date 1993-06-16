31038b78a583d6be262faceb9cba0da09ee71675
Ken Raeburn <raeburn@cygnus>
1993-06-16 03:06:33 +0000

    * aout.sc-sh: Add SHLIB_PATH like STACKZERO.  Make STACKZERO dependent on RELOCATING, not RELOCATION. * hp3hpux.sh (SHLIB_PATH): Define it.

--- ld/hp3hpux.sh
+++ ld/hp3hpux.sh
@@ -5,3 +5,5 @@ TEXT_START_ADDR=0
 PAGE_SIZE=4096
 ARCH=m68k
 STACKZERO="___stack_zero = 0x2000;"
+# This is needed for HPUX 9.0; it is unnecessary but harmless for 8.0.
+SHLIB_PATH="___dld_shlib_path = 0;"
