c9d8fc9120db2c32dd5525c9e923771c607c9803
Jim Kingdon <jkingdon@engr.sgi.com>
1993-06-18 02:33:01 +0000

    * config/m88k/xm-delta88.h: Comment out unused defines which conflict 	with system headers. 	* printcmd.c (printf_command): Cast second arg to vprintf to PTR. 	Use VPRINTF macro if defined. 	* config/m88k/xm-delta88.h: Define VPRINTF.  Include <sys/siginfo.h>. 	Define TIOC{GETC,GLTC}_BROKEN. 	* m88k-nat.c: Uncomment include of <sys/ptrace.h>. 	* main.c: Rename initialize_{main,cmd_lists,history} to init_* to 	make things easier on munch (apparently this matters on 	the delta88 with svr3).

--- gdb/config/m88k/xm-delta88.h
+++ gdb/config/m88k/xm-delta88.h
@@ -24,14 +24,22 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #define USG 1
 #endif
 
+#define TIOCGETC_BROKEN 1
+#define TIOCGLTC_BROKEN 1
+
 #include <sys/param.h>
 #include <sys/time.h>
+
+/* Required by <sys/ptrace.h>.  */
+#include <sys/siginfo.h>
 
 #define HAVE_TERMIO
 
 /*#define USIZE 2048*/
-#define NBPG NBPC
-#define UPAGES USIZE
+/*#define NBPG NBPC*/
+/* Might be defined in <sys/param.h>.  I suspect this define was a relic
+   from before when BFD did core files.  */
+/* #define UPAGES USIZE */
 
 /* This is the amount to subtract from u.u_ar0
    to get the offset in the core file of the register values.  */
@@ -42,3 +50,14 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #define KERNEL_U_ADDR 0
 
+/* Like vprintf, but takes a a pointer to the args, laid out in order,
+   rather than a va_list.  */
+/* Does this really work, or is it just enough to get this to compile?  */
+#define VPRINTF(string, args) \
+  { \
+    __gnuc_va_list list; \
+    list.__va_arg = 0; \
+    list.__va_stk = (int *) args; \
+    list.__va_reg = (int *) args; \
+    vprintf (string, list); \
+  }
