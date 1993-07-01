95f2d25323e3233a675df759f23b261fa4ea08ea
Jim Kingdon <jkingdon@engr.sgi.com>
1993-07-01 01:10:22 +0000

    * config/vax/xm-vaxult2.h: Define FD_SET and FD_ZERO.

--- gdb/config/vax/xm-vaxult2.h
+++ gdb/config/vax/xm-vaxult2.h
@@ -5,3 +5,8 @@ extern char *strdup();
 
 #define MEM_FNS_DECLARED
 #define NO_PTRACE_H
+
+/* Old versions of ultrix have fd_set but not the FD_* macros.  */
+
+#define FD_SET(bit,fdsetp) ((fdsetp)->fds_bits[(n) / 32] |= (1 << ((n) % 32)))
+#define FD_ZERO(fdsetp) memset (fdsetp, 0, sizeof (*fdsetp))
