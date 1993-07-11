0a8f1742669223640fa8b4509418675cfb3ea5a4
Jim Kingdon <jkingdon@engr.sgi.com>
1993-07-11 23:29:31 +0000

    * configure.in (m68*-bull-sysv*): added support for Bull dpx2. 	* config/m68k/{t,x,n}m-dpx2.h, dpx2-nat.c: New files. 	* config/m68k/dpx2.m{h,t}: New files.

---
+++ gdb/config/m68k/tm-dpx2.h
@@ -0,0 +1,32 @@
+/* Parameters for targeting to a Bull DPX2.
+   Copyright (C) 1986, 1987, 1989, 1991 Free Software Foundation, Inc.
+
+This file is part of GDB.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* Define BPT_VECTOR if it is different than the default.
+   This is the vector number used by traps to indicate a breakpoint. */
+
+#define BPT_VECTOR 0xe
+
+#define HAVE_68881
+
+/* Need to get function ends by adding this to epilogue address from .bf
+   record, not using x_fsize field.  */
+#define FUNCTION_EPILOGUE_SIZE 4
+
+#include <sys/types.h>
+#include "tm-68k.h"
