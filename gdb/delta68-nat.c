598db9ee399cbc015ef75e97510be9a1a73a1e4e
Fred Fish <fnf@specifix.com>
1993-07-30 14:42:31 +0000

    * README:  Emphasize that C++ support works best with GNU C++ and 	stabs debugging format. 	* delta68-nat.c:  Add missing FSF copyright.

--- gdb/delta68-nat.c
+++ gdb/delta68-nat.c
@@ -1,3 +1,22 @@
+/* Functions specific to running gdb native on a Motorola Delta Series sysV68.
+   Copyright (C) 1993, Free Software Foundation, Inc.
+
+This file is part of GDB.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
 #include "defs.h"
 #include <sys/signal.h>	/* for MAXSIG in sys/user.h */
 #include <sys/types.h>	/* for ushort in sys/dir.h */
