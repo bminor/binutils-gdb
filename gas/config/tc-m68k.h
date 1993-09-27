e3fce8d283ead690698eb8fee44a6460d4e99ba9
Ken Raeburn <raeburn@cygnus>
1993-09-27 03:56:04 +0000

    define DIFF_EXPR_OK

--- gas/config/tc-m68k.h
+++ gas/config/tc-m68k.h
@@ -1,35 +1,33 @@
 /* This file is tc-m68k.h
 
    Copyright (C) 1987-1992 Free Software Foundation, Inc.
-   
+
    This file is part of GAS, the GNU Assembler.
-   
+
    GAS is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
    any later version.
-   
+
    GAS is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with GAS; see the file COPYING.  If not, write to
    the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-/*
- * This file is tp-generic.h and is intended to be a template for
- * target processor specific header files. 
- */
-
 #define TC_M68K 1
 
+#ifdef OBJ_AOUT
+#define TARGET_FORMAT "a.out-sunos-big"
+#endif
 
 #define COFF_MAGIC MC68MAGIC
 #define BFD_ARCH bfd_arch_m68k
 #define COFF_FLAGS F_AR32W
-#define TC_COUNT_RELOC(x) ((x)->fx_addsy||(x)->fx_subsy||(x)->fx_offset)
+#define TC_COUNT_RELOC(x) ((x)->fx_addsy||(x)->fx_subsy)
 
 #define TC_COFF_FIX2RTYPE(fixP) tc_coff_fix2rtype(fixP)
 #define TC_COFF_SIZEMACHDEP(frag) tc_coff_sizemachdep(frag)
@@ -40,17 +38,19 @@
 
 #define DEFAULT_MAGIC_NUMBER_FOR_OBJECT_FILE (2<<16|OMAGIC);	/* Magic byte for file header */
 #endif /* TE_SUN3 */
-    
-#define AOUT_MACHTYPE 0x2
-    
-#define tc_crawl_symbol_chain(a)	{;} /* not used */
-#define tc_headers_hook(a)		{;} /* not used */
-#define tc_aout_pre_write_hook(x)	{;} /* not used */
 
-#define LISTING_WORD_SIZE 2   /* A word is 2 bytes */
-#define LISTING_LHS_WIDTH 2   /* One word on the first line */
-#define LISTING_LHS_WIDTH_SECOND 2  /* One word on the second line */
-#define LISTING_LHS_CONT_LINES 4   /* And 4 lines max */
+#ifndef AOUT_MACHTYPE
+#define AOUT_MACHTYPE 0x2
+#endif
+
+#define tc_crawl_symbol_chain(a)	{;}	/* not used */
+#define tc_headers_hook(a)		{;}	/* not used */
+#define tc_aout_pre_write_hook(x)	{;}	/* not used */
+
+#define LISTING_WORD_SIZE 2	/* A word is 2 bytes */
+#define LISTING_LHS_WIDTH 2	/* One word on the first line */
+#define LISTING_LHS_WIDTH_SECOND 2	/* One word on the second line */
+#define LISTING_LHS_CONT_LINES 4/* And 4 lines max */
 #define LISTING_HEADER "68K GAS "
 
 /* Copied from write.c */
@@ -59,11 +59,18 @@
 					aim=this_type->rlx_forward+1; /* Force relaxation into word mode */ \
 				    }
 
-/*
- * Local Variables:
- * comment-column: 0
- * fill-column: 131
- * End:
- */
+#ifdef M68KCOFF
+#define DOT_LABEL_PREFIX
+#define REGISTER_PREFIX '%'
+#else
+#define OPTIONAL_REGISTER_PREFIX '%'
+#endif
+
+#ifdef BFD_ASSEMBLER
+#define tc_frob_symbol(sym,punt) \
+    if (S_GET_SEGMENT (sym) == reg_section) punt = 1
+#endif
+
+#define DIFF_EXPR_OK
 
 /* end of tc-m68k.h */
