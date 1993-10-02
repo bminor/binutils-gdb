17dbd909935e2c430a226c6f852c2f40ea3a2e8b
Jim Kingdon <jkingdon@engr.sgi.com>
1993-10-02 23:55:34 +0000

    new for mips corefiles

---
+++ bfd/hosts/news-mips.h
@@ -0,0 +1,12 @@
+/* Sony News running NewsOS 3.2.  */
+
+#include <machine/vmparam.h>
+
+#define HOST_PAGE_SIZE NBPG
+#define HOST_SEGMENT_SIZE NBPG
+#define HOST_MACHINE_ARCH bfd_arch_m68k
+#define HOST_TEXT_START_ADDR 0
+#define HOST_STACK_END_ADDR (KERNBASE - (UPAGES * NBPG))
+#define HOST_BIG_ENDIAN_P
+
+#include "hosts/std-host.h"
