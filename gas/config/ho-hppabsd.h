12696e9cb51a6042a0b3ac77b9324a5f47e0c8b1
Jeff Law <law@redhat.com>
1993-10-30 21:29:29 +0000

    * config/ho-hppabsd.h:  Delete IO* macros, they are defined in         stdio.h.  Delete declaration of free.  Include stdlib.h, unistd.h,         and string.h.

--- gas/config/ho-hppabsd.h
+++ gas/config/ho-hppabsd.h
@@ -17,10 +17,8 @@
    along with GAS; see the file COPYING.  If not, write to
    the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-/*
-	HP PA-RISC and BSD support was contributed by the Center for
-	Software Science at the University of Utah.
- */
+/* HP PA-RISC and BSD support was contributed by the Center for
+   Software Science at the University of Utah.  */
 
 #define M_HPPABSD 1
 
@@ -28,25 +26,14 @@
 #define NO_STDARG
 #endif /* not ansi */
 
-/* XXX  These should actually appear in stdio.h */
-#ifndef _IOFBF
-#define _IOFBF	0		/* setvbuf should set fully buffered */
-#endif
-
-#ifndef _IOLBF
-#define _IOLBF	1		/* setvbuf should set line buffered */
-#endif
-
-#ifndef _IONBF
-#define _IONBF	2		/* setvbuf should set unbuffered */
-#endif
-
 #define HAVE_STRERROR
 extern int errno;
 
-extern void free();
-
 #include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 
 /* SKV 12/08/92. Include correctly configured declaration of alloca. */
 
