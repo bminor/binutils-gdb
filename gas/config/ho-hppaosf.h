a50cf51f2ece049aa90aeea43153220bd9ee78d8
Jeff Law <law@redhat.com>
1993-11-02 23:07:00 +0000

    * config/ho-hppaosf.h: Delete _IO* macros.  They are defined in 	stdio.h.  Delete bogus declaration of free.  Get path to 	alloca-conf.h right.

--- gas/config/ho-hppaosf.h
+++ gas/config/ho-hppaosf.h
@@ -17,39 +17,22 @@
    along with GAS; see the file COPYING.  If not, write to
    the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-/*
-	HP PA-RISC and OSF/1 support was contributed by the Center for
-	Software Science at the University of Utah.
- */
+/* HP PA-RISC and OSF/1 support was contributed by the Center for
+   Software Science at the University of Utah.  */
 
 #define M_HPPAOSF 1
 
-#if __STDC__ != 1
+#ifdef __STDC__ != 1
 #define NO_STDARG
 #endif /* not ansi */
 
-/* XXX  These should actually appear in stdio.h */
-#ifndef _IOFBF
-#define _IOFBF	0		/* setvbuf should set fully buffered */
-#endif
-
-#ifndef _IOLBF
-#define _IOLBF	1		/* setvbuf should set line buffered */
-#endif
-
-#ifndef _IONBF
-#define _IONBF	2		/* setvbuf should set unbuffered */
-#endif
-
 #define HAVE_STRERROR
 extern int errno;
-
-extern int free();
 
 #include <ctype.h>
 
 /* SKV 12/08/92. Include correctly configured declaration of alloca. */
 
-#include "alloca-conf.h"
+#include "../libiberty/alloca-conf.h"
 
 /* end of ho-hppaosf.h */
