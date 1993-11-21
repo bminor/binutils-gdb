c60af619fbf8f3ce4cd6b52c3309182083d681d5
Ken Raeburn <raeburn@cygnus>
1993-11-21 21:29:46 +0000

    Modified to be based on lynx.h, like i386 and m68k versions.

--- bfd/hosts/sparclynx.h
+++ bfd/hosts/sparclynx.h
@@ -3,35 +3,8 @@
 #ifndef hosts_sparclynx_h
 #define hosts_sparclynx_h
 
-#include <fcntl.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include "hosts/lynx.h"
 
-#include <sys/conf.h>
-#include <sys/kernel.h>
-#include <sys/mem.h>
-#include <sys/signal.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <sys/itimer.h>
-#include <sys/file.h>
-#include <sys/proc.h>
-
-#define	HOST_PAGE_SIZE		NBPG
 #define	HOST_MACHINE_ARCH	bfd_arch_sparc
-#define	HOST_TEXT_START_ADDR	USRTEXT
-
-#define HOST_LYNX
-
-#include "fopen-same.h"
-
-#define FPRINTF_ALREADY_DECLARED
-
-/* Lynx has calloc, but no cfree, and gprof uses it. */
-
-#define cfree free
 
 #endif /* hosts_sparclynx_h */
