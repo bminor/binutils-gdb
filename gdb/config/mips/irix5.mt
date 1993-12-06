a2f1e2e5f8bf65d1751d90540a0667696e00a8fc
Ian Lance Taylor <ian@airs.com>
1993-12-06 17:12:23 +0000

    New files for Irix 5 support.

---
+++ gdb/config/mips/irix5.mt
@@ -0,0 +1,3 @@
+# Target: SGI mips running Irix 5
+TDEPFILES= mips-pinsn.o mips-tdep.o exec.o
+TM_FILE= tm-irix5.h
---
+++ gdb/config/mips/tm-irix5.h
@@ -0,0 +1,42 @@
+/* Target machine description for SGI Iris under Irix 5, for GDB.
+   Copyright 1990, 1991, 1992, 1993 Free Software Foundation, Inc.
+
+This file is part of GDB.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include "mips/tm-irix3.h"
+
+/* A function call in Irix 5 will sometimes skip the three initial
+   instructions which set the GP.  */
+#define AT_FUNCTION_START(pc,func_name,func_addr) ((pc) == (func_addr) + 12)
+
+/* When calling functions on Irix 5 (or any MIPS SVR4 ABI compliant
+   platform) $25 must hold the function address.  Dest_Reg is a macro
+   used in CALL_DUMMY in tm-mips.h.  */
+#undef Dest_Reg
+#define Dest_Reg 25
+
+/* Irix 5 saves a full 64 bits for each register.  We skip 2 * 4 to
+   get to the saved PC (the register mask and status register are both
+   32 bits) and then another 4 to get to the lower 32 bits.  We skip
+   the same 4 bytes, plus the 8 bytes for the PC to get to the
+   registers, and add another 4 to get to the lower 32 bits.  We skip
+   8 bytes per register.  */
+#undef SIGFRAME_PC_OFF
+#define SIGFRAME_PC_OFF		(SIGFRAME_BASE + 2 * 4 + 4)
+#undef SIGFRAME_REGSAVE_OFF
+#define SIGFRAME_REGSAVE_OFF	(SIGFRAME_BASE + 2 * 4 + 8 + 4)
+#define SIGFRAME_REG_SIZE	8
