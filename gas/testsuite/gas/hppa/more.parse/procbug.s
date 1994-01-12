96f147a09543c98a60cc71c24cfb4834aac73bcd
Jeff Law <law@redhat.com>
1994-01-12 21:42:55 +0000

    * gas/hppa/more.parse/procbug.s: Add test for another bug relating         to having a function's label follow the .PROC directive.

--- gas/testsuite/gas/hppa/more.parse/procbug.s
+++ gas/testsuite/gas/hppa/more.parse/procbug.s
@@ -6,3 +6,11 @@
 	.callinfo
 divu:	stws,ma		%r4,4(%r5)		; save registers on stack
 	.procend
+
+	.export divu2,entry
+	.proc
+	.callinfo
+	.entry
+divu2:	stws,ma		%r4,4(%r5)		; save registers on stack
+	.exit
+	.procend
