dac317dc3fc938bf1570c6fc074bf0fde96a4de1
Jim Kingdon <jkingdon@engr.sgi.com>
1994-01-15 15:27:49 +0000

    * c-typeprint.c (c_type_print_varspec_suffix, case TYPE_CODE_FUNC): 	Print our "()" first, then recurse for the target type.

--- gdb/c-typeprint.c
+++ gdb/c-typeprint.c
@@ -409,12 +409,12 @@ c_type_print_varspec_suffix (type, stream, show, passed_a_ptr, demangled_args)
       break;
 
     case TYPE_CODE_FUNC:
-      c_type_print_varspec_suffix (TYPE_TARGET_TYPE (type), stream, 0,
-				 passed_a_ptr, 0);
       if (passed_a_ptr)
 	fprintf_filtered (stream, ")");
       if (!demangled_args)
 	fprintf_filtered (stream, "()");
+      c_type_print_varspec_suffix (TYPE_TARGET_TYPE (type), stream, 0,
+				   passed_a_ptr, 0);
       break;
 
     case TYPE_CODE_UNDEF:
