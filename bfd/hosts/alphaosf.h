5802eb8f93f6ac0a7bd6a1a76e82ff1756e431ad
Ken Raeburn <raeburn@cygnus>
1994-01-19 22:35:48 +0000

    (sprintf_vma, fprintf_vma): New macros. (uint64_typeHIGH, uint64_typeLOW): Comment with HOST_64_BIT so they get copied to bfd.h.

--- bfd/hosts/alphaosf.h
+++ bfd/hosts/alphaosf.h
@@ -10,8 +10,11 @@
 #include <alloca.h>
 #include <stdlib.h>
 
-/* Make the basic types 64-bit quantities on the host */
-#define	HOST_64_BIT	long 
+/* Make the basic types 64-bit quantities on the host.
+   Also provide the support macros BFD needs.  */
+#define	HOST_64_BIT	long
+#define sprintf_vma(s,x) sprintf (s, "%016lx", x) /* HOST_64_BIT */
+#define fprintf_vma(f,x) fprintf (f, "%016lx", x) /* HOST_64_BIT */
 
 typedef unsigned  long uint64e_type;
 typedef unsigned  long uint64_type;
@@ -21,7 +24,7 @@ typedef  long int64_type;
 
 /* These must have type unsigned long because they are used as
    arguments in printf functions.  */
-#define uint64_typeLOW(x) ((unsigned long) (((x) & 0xffffffff)))
-#define uint64_typeHIGH(x) ((unsigned long) (((x) >> 32) & 0xffffffff))
+#define uint64_typeLOW(x) ((unsigned long) (((x) & 0xffffffff))) /* HOST_64_BIT */
+#define uint64_typeHIGH(x) ((unsigned long) (((x) >> 32) & 0xffffffff)) /* HOST_64_BIT */
 
 #include "fopen-same.h"
