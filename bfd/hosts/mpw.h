848743c1b688d0764c4293e635318b9d052040b5
Stan Shebs <shebs@codesourcery.com>
1994-02-15 20:48:53 +0000

    Tue Feb 15 12:44:30 1994  Stan Shebs  (shebs@andros.cygnus.com)
    	* mpw-make.in (CSEARCH): Add include/mpw to search path.
    	* hosts/mpw.h (stat): Remove incomplete definition, now in
    	include/mpw/sys.
    	(POSIX_UTIME): Define.

--- bfd/hosts/mpw.h
+++ bfd/hosts/mpw.h
@@ -1,4 +1,7 @@
+/* MPW host-specific definitions. */
+
 #ifndef	hosts_mpw_H
+#define hosts_mpw_H
 
 #ifndef MPW
 #define MPW
@@ -25,8 +28,9 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
 #include <fcntl.h>
+
+#include <sys/stat.h>
 
 #ifndef	O_ACCMODE
 #define O_ACCMODE (O_RDONLY | O_WRONLY | O_RDWR)
@@ -37,7 +41,6 @@
 #ifndef	SEEK_CUR
 #define SEEK_CUR 1
 #endif
-/*#include <string.h>*/
 
 #ifndef	BYTES_IN_PRINTF_INT
 #define BYTES_IN_PRINTF_INT 4
@@ -59,27 +62,9 @@
 
 #endif
 
-typedef short ino_t;
-typedef short dev_t;
-
-struct stat {
-	ino_t st_ino;
-	dev_t st_dev;
-	int st_mode;
-	int st_size;
-	int st_uid;
-	int st_gid;
-	int st_mtime;
-};
-
-#define S_ISREG(m) (0)
-#define S_ISDIR(m) (0)
-
 #define NO_FCNTL
 
-#ifdef __STDIO__
 FILE *mpw_fopen ();
-#endif
 int mpw_fstat ();
 int mpw_fseek ();
 void mpw_abort ();
@@ -102,9 +87,12 @@ void mpw_abort ();
 #endif
 #endif
 
+#ifndef TRUE_FALSE_ALREADY_DEFINED
 #define TRUE_FALSE_ALREADY_DEFINED
+#endif
+
+#define POSIX_UTIME
 
 #define LOSING_TOTALLY
 
-#define hosts_mpw_H
-#endif
+#endif /* hosts_mpw_H */
