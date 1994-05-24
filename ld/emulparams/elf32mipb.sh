f49f58c6a8ffdc40d3f11ffe9e5de782ef5e0017
Ian Lance Taylor <ian@airs.com>
1994-05-24 20:21:27 +0000

    * emulparams/elf32mipb.sh (OTHER_READONLY_SECTIONS): Don't give 	.reginfo an address. 	(OTHER_READWRITE_SECTIONS): Don't give .lit4 or .lit8 an address. 	(OTHER_SECTIONS): Define for .gptab.sdata and .gptab.sbss. 	* scripttempl/elf.sc: Use OTHER_SECTIONS at end of script.

--- ld/emulparams/elf32mipb.sh
+++ ld/emulparams/elf32mipb.sh
@@ -4,14 +4,18 @@ TEXT_START_ADDR=0x0400000
 DATA_ADDR=0x10000000
 MAXPAGESIZE=0x40000
 NONPAGED_TEXT_START_ADDR=0x0400000
-OTHER_READONLY_SECTIONS='.reginfo . : { *(.reginfo) }'
+OTHER_READONLY_SECTIONS='.reginfo : { *(.reginfo) }'
 OTHER_READWRITE_SECTIONS='
   _gp = . + 0x8000;
-  .lit8 . : { *(.lit8) }
-  .lit4 . : { *(.lit4) }
+  .lit8 : { *(.lit8) }
+  .lit4 : { *(.lit4) }
 '
 TEXT_START_SYMBOLS='_ftext = . ;'
 DATA_START_SYMBOLS='_fdata = . ;'
 OTHER_BSS_SYMBOLS='_fbss = .;'
 EXECUTABLE_SYMBOLS='_DYNAMIC_LINK = 0;'
+OTHER_SECTIONS='
+  .gptab.sdata : { *(.gptab.data) *(.gptab.sdata) }
+  .gptab.sbss : { *(.gptab.bss) *(.gptab.sbss) }
+'
 ARCH=mips
