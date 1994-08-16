969ceea035ef06d21bdb051bea89ed2fb1568d25
Ian Lance Taylor <ian@airs.com>
1994-08-16 04:15:50 +0000

    * i386linux.c (linux_link_create_dynamic_sections): Create section 	named .linux-dynamic, not .dynamic. 	(linux_add_one_symbol): Use .linux-dynamic, not .dynamic. 	(bfd_linux_size_dynamic_sections): Likewise. 	(linux_finish_dynamic_link): Likewise.

--- bfd/i386linux.c
+++ bfd/i386linux.c
@@ -295,9 +295,9 @@ linux_link_create_dynamic_sections (abfd, info)
   /* Note that we set the SEC_IN_MEMORY flag.  */
   flags = SEC_ALLOC | SEC_LOAD | SEC_HAS_CONTENTS | SEC_IN_MEMORY;
 
-  /* We choose to use the name ".dynamic" for the fixup table.  Why
-     not? */
-  s = bfd_make_section (abfd, ".dynamic");
+  /* We choose to use the name ".linux-dynamic" for the fixup table.
+     Why not? */
+  s = bfd_make_section (abfd, ".linux-dynamic");
   if (s == NULL
       || ! bfd_set_section_flags (abfd, s, flags)
       || ! bfd_set_section_alignment (abfd, s, 2))
@@ -387,7 +387,7 @@ linux_add_one_symbol (info, abfd, name, flags, section, value, string,
       /* Here we do our special thing to add the pointer to the
 	 dynamic section in the SHARABLE_CONFLICTS set vector.  */
       s = bfd_get_section_by_name (linux_hash_table (info)->dynobj,
-				   ".dynamic");
+				   ".linux-dynamic");
       BFD_ASSERT (s != NULL);
 
       if (! (_bfd_generic_link_add_one_symbol
@@ -494,11 +494,11 @@ linux_tally_symbols (h, data)
   return true;
 }
 
-/* This is called to set the size of the .dynamic section is.  It is
-   called by the Linux linker emulation before_allocation routine.  We
-   have finished reading all of the input files, and now we just scan
-   the hash tables to find out how many additional fixups are
-   required.  */
+/* This is called to set the size of the .linux-dynamic section is.
+   It is called by the Linux linker emulation before_allocation
+   routine.  We have finished reading all of the input files, and now
+   we just scan the hash tables to find out how many additional fixups
+   are required.  */
 
 boolean
 bfd_linux_size_dynamic_sections (output_bfd, info)
@@ -534,7 +534,8 @@ bfd_linux_size_dynamic_sections (output_bfd, info)
     }
 
   /* Allocate memory for our fixup table.  We will fill it in later.  */
-  s = bfd_get_section_by_name (linux_hash_table (info)->dynobj, ".dynamic");
+  s = bfd_get_section_by_name (linux_hash_table (info)->dynobj,
+			       ".linux-dynamic");
   if (s != NULL)
     {
       s->_raw_size = 8 + linux_hash_table (info)->fixup_count * 8;
@@ -570,7 +571,8 @@ linux_finish_dynamic_link (output_bfd, info)
   if (linux_hash_table (info)->dynobj == NULL)
     return true;
 
-  s = bfd_get_section_by_name (linux_hash_table (info)->dynobj, ".dynamic");
+  s = bfd_get_section_by_name (linux_hash_table (info)->dynobj,
+			       ".linux-dynamic");
   BFD_ASSERT (s != NULL);
   os = s->output_section;
   fixups_written = 0;
