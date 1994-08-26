c01fe9368436a721393117dbd93022b211c44c1f
J.T. Conklin <jtc@acorntoolworks.com>
1994-08-26 22:00:50 +0000

    Remove RCS ID's

--- gdb/config/sparc/xm-sparcnbsd.h
+++ gdb/config/sparc/xm-sparcnbsd.h
@@ -15,10 +15,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-	$Id$
-*/
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 /* Get generic NetBSD host definitions. */
 #include "xm-nbsd.h"
