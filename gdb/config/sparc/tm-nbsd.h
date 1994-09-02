4c35f877ee0f2d2cce94961a658ac83a7ba45ddc
J.T. Conklin <jtc@acorntoolworks.com>
1994-09-02 16:57:11 +0000

    * config/sparc/tm-nbsd.h: Add #defines to map NetBSD struct and   field names into what is expected by sparc-nat.c.

--- gdb/config/sparc/tm-nbsd.h
+++ gdb/config/sparc/tm-nbsd.h
@@ -23,4 +23,26 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include "sparc/tm-sparc.h"
 #include "tm-nbsd.h"
 
+/* make things match up with what is expected in sparc-nat.c */
+#define regs		trapframe
+#define fp_status	fpstate
+
+#define r_g1		tf_global[1]
+#define r_ps		tf_psr
+#define r_pc		tf_pc
+#define r_npc		tf_npc
+#define r_y		tf_y
+
+#define fpu		fpstate
+#define fpu_regs	fs_regs
+#define fpu_fsr		fs_fsr
+#define fpu_fr		fs_regs
+#define Fpu_fsr		fs_fsr
+#define FPU_FSR_TYPE	int
+
+#define PTRACE_GETREGS	 PT_GETREGS
+#define PTRACE_GETFPREGS PT_GETFPREGS
+#define PTRACE_SETREGS	 PT_SETREGS
+#define PTRACE_SETFPREGS PT_SETFPREGS
+
 #endif /* TM_NBSD_H */
