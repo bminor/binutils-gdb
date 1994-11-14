50e183a2baa78c992ce81790d41fc7a3f9d2b1bf
Rob Savoye <rob@cygnus>
1994-11-14 22:55:52 +0000

    Add copyright message.

--- gdb/op50-rom.c
+++ gdb/op50-rom.c
@@ -1,6 +1,23 @@
-/*
- * op50n-rom.c -- configure GDB for an Oki op59n eval board.
- */
+/* Remote target glue for the Oki op50n based eval board.
+
+   Copyright 1988, 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+
+This file is part of GDB.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
 #include "defs.h"
 #include "gdbcore.h"
 #include "target.h"
--- gdb/rom68k-rom.c
+++ gdb/rom68k-rom.c
@@ -1,3 +1,24 @@
+/* Remote target glue for the rom68k ROM monitor. This was running on a
+Motorola IDP board.
+
+   Copyright 1988, 1991, 1992, 1993, 1994 Free Software Foundation, Inc.
+
+This file is part of GDB.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
 #include "defs.h"
 #include "gdbcore.h"
 #include "target.h"
