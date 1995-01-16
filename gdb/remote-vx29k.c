83d9bb1416873498a6eb2da72e01fcdcb51b6370
Kung Hsu <kung@cygnus>
1995-01-16 23:56:41 +0000

    * Makefile.in: add new files remote-vx29k.c, config/a29k/tm-vx29k.h,         and config/a29k/vx29k.mt.         * configure.in: add new configuration a29k-*-vxworks.         * remote-vx29k.c: new file merged from WRS.         * remote-vx.c: merge changes from WRS.

---
+++ gdb/remote-vx29k.c
@@ -0,0 +1,169 @@
+/* Am29k-dependent portions of the RPC protocol
+   used with a VxWorks target 
+
+Contributed by Wind River Systems.
+
+This file is part of GDB.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+#include <stdio.h>
+#include "defs.h"
+
+#include "vx-share/regPacket.h"  
+#include "frame.h"
+#include "inferior.h"
+#include "wait.h"
+#include "target.h"
+#include "gdbcore.h"
+#include "command.h"
+#include "symtab.h"
+#include "symfile.h"		/* for struct complaint */
+
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+
+#ifdef _AIX                     /* IBM claims "void *malloc()" not char * */
+#define malloc bogon_malloc
+#endif
+
+#include <rpc/rpc.h>
+#include <sys/time.h>		/* UTek's <rpc/rpc.h> doesn't #incl this */
+#include <netdb.h>
+#include "vx-share/ptrace.h"
+#include "vx-share/xdr_ptrace.h"
+#include "vx-share/xdr_ld.h"
+#include "vx-share/xdr_rdb.h"
+#include "vx-share/dbgRpcLib.h"
+
+/* get rid of value.h if possible */
+#include <value.h>
+#include <symtab.h>
+
+/* Flag set if target has fpu */
+
+extern int target_has_fp;
+
+/* Generic register read/write routines in remote-vx.c.  */
+
+extern void net_read_registers ();
+extern void net_write_registers ();
+
+/* Read a register or registers from the VxWorks target.
+   REGNO is the register to read, or -1 for all; currently,
+   it is ignored.  FIXME look at regno to improve efficiency.  */
+
+void
+vx_read_register (regno)
+     int regno;
+{
+  char am29k_greg_packet[AM29K_GREG_PLEN];
+  char am29k_fpreg_packet[AM29K_FPREG_PLEN];
+
+  /* Get general-purpose registers.  When copying values into
+     registers [], don't assume that a location in registers []
+     is properly aligned for the target data type.  */
+
+  net_read_registers (am29k_greg_packet, AM29K_GREG_PLEN, PTRACE_GETREGS);
+
+  /* Now copy the register values into registers[].
+     Note that this code depends on the ordering of the REGNUMs
+     as defined in "tm-29k.h".  */
+
+  bcopy (&am29k_greg_packet[AM29K_R_GR96],
+	 &registers[REGISTER_BYTE (GR96_REGNUM)], 160 * AM29K_GREG_SIZE);
+  bcopy (&am29k_greg_packet[AM29K_R_VAB],
+	 &registers[REGISTER_BYTE (VAB_REGNUM)], 15 * AM29K_GREG_SIZE);
+  registers[REGISTER_BYTE (INTE_REGNUM)] = am29k_greg_packet[AM29K_R_INTE];
+  bcopy (&am29k_greg_packet[AM29K_R_RSP],
+	 &registers[REGISTER_BYTE (GR1_REGNUM)], 5 * AM29K_GREG_SIZE);
+
+  /* PAD For now, don't care about exop register */
+
+  bzero (&registers[REGISTER_BYTE (EXO_REGNUM)], 1 * AM29K_GREG_SIZE);
+
+  /* If the target has floating point registers, fetch them.
+     Otherwise, zero the floating point register values in
+     registers[] for good measure, even though we might not
+     need to.  */
+
+  if (target_has_fp)
+    {
+      net_read_registers (am29k_fpreg_packet, AM29K_FPREG_PLEN,
+                          PTRACE_GETFPREGS);
+      registers[REGISTER_BYTE (FPE_REGNUM)] = am29k_fpreg_packet[AM29K_R_FPE];
+      registers[REGISTER_BYTE (FPS_REGNUM)] = am29k_fpreg_packet[AM29K_R_FPS];
+
+      /* PAD For now, don't care about registers (?) AI0 to q */
+
+      bzero (&registers[REGISTER_BYTE (161)], 21 * AM29K_FPREG_SIZE);
+    }
+  else
+    { 
+      bzero (&registers[REGISTER_BYTE (FPE_REGNUM)], 1 * AM29K_FPREG_SIZE);
+      bzero (&registers[REGISTER_BYTE (FPS_REGNUM)], 1 * AM29K_FPREG_SIZE);
+
+      /* PAD For now, don't care about registers (?) AI0 to q */
+
+      bzero (&registers[REGISTER_BYTE (161)], 21 * AM29K_FPREG_SIZE);
+    }
+
+  /* Mark the register cache valid.  */
+
+  registers_fetched ();
+}
+
+/* Store a register or registers into the VxWorks target.
+   REGNO is the register to store, or -1 for all; currently,
+   it is ignored.  FIXME look at regno to improve efficiency.  */
+
+void
+vx_write_register (regno)
+     int regno;
+{
+  char am29k_greg_packet[AM29K_GREG_PLEN];
+  char am29k_fpreg_packet[AM29K_FPREG_PLEN];
+
+  /* Store general purpose registers.  When copying values from
+     registers [], don't assume that a location in registers []
+     is properly aligned for the target data type.  */
+
+  bcopy (&registers[REGISTER_BYTE (GR96_REGNUM)],
+	 &am29k_greg_packet[AM29K_R_GR96], 160 * AM29K_GREG_SIZE);
+  bcopy (&registers[REGISTER_BYTE (VAB_REGNUM)],
+	 &am29k_greg_packet[AM29K_R_VAB], 15 * AM29K_GREG_SIZE);
+  am29k_greg_packet[AM29K_R_INTE] = registers[REGISTER_BYTE (INTE_REGNUM)];
+  bcopy (&registers[REGISTER_BYTE (GR1_REGNUM)],
+	 &am29k_greg_packet[AM29K_R_RSP], 5 * AM29K_GREG_SIZE);
+
+  net_write_registers (am29k_greg_packet, AM29K_GREG_PLEN, PTRACE_SETREGS);
+
+  /* Store floating point registers if the target has them.  */
+
+  if (target_has_fp)
+    {
+      am29k_fpreg_packet[AM29K_R_FPE] = registers[REGISTER_BYTE (FPE_REGNUM)];
+      am29k_fpreg_packet[AM29K_R_FPS] = registers[REGISTER_BYTE (FPS_REGNUM)];
+
+      net_write_registers (am29k_fpreg_packet, AM29K_FPREG_PLEN,
+                           PTRACE_SETFPREGS);
+    }
+}
+
