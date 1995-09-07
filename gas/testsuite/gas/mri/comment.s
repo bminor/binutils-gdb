e840039ffc44e519efb492711b035d9934cd78ab
Ian Lance Taylor <ian@airs.com>
1995-09-07 01:45:07 +0000

    * gas/mri/comment.s, gas/mri/comment.d: New test. 	* gas/mri/mri.exp: Run it. 	* gas/mri/expr.s: Remove whitespace in operand field. 	* gas/mri/for.s: Add comments for further testing. 	* gas/macros/test2.s: Put in an upper case ELSE to test case 	insensitivity.

---
+++ gas/testsuite/gas/mri/comment.s
@@ -0,0 +1,10 @@
+	xref label
+	xdef	ROM,RAM
+	* this is a comment
+
+	dc.l	label	loop if we haven't reach end yet
+	
+ROM				EQU	$00000001	* word wide
+RAM				EQU	$00000002	word wide
+	dc.l	RAM
+	dc.l	0	,really,a,comment
