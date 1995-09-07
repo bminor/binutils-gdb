c632873c27144551ef119f4c9e165d296e9fd25d
J.T. Conklin <jtc@acorntoolworks.com>
1995-09-07 22:07:47 +0000

    (Try to) Update to new bfd autoconf scheme. * run.c: Don't include sysdep.h. * Makefile.in (INSTALL{,_PROGRAM,_DATA}): Use autoconf computed value. (CC, CFLAGS, AR, RANLIB): Likewise. (HDEFINES, TDEFINES): Define. (CC_FOR_BUILD): Delete. (host_makefile_frag): Delete. (Makefile): Don't depend on frags. * configure.in (sysdep.h): Don't create symlink. (host_makefile_frag, frags): Deleted. (CC, CFLAGS, AR, RANLIB, INSTALL): Compute values. * configure: Regenerated.

--- sim/sh/run.c
+++ sim/sh/run.c
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #include <stdio.h>
 #include <varargs.h>
 #include "bfd.h"
-#include "sysdep.h"
 #include "remote-sim.h"
 
 #ifndef SIGQUIT
