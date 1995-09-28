a11e10fe44a7c80be5560f1fc934f728251850a9
David Edelsohn <dje.gcc@gmail.com>
1995-09-28 08:08:49 +0000

    * config.bfd: Add arm-*-coff. 	* configure.in, configure: Add armcoff_{little,big}_vec. 	* targets.c (armcoff_{little,big}_vec): Declare. 	(bfd_target_vector): Add armcoff_{little,big}_vec. 	* coff-arm.c (armcoff_{little,big}_vec): Always define.

--- bfd/targets.c
+++ bfd/targets.c
@@ -459,6 +459,8 @@ extern const bfd_target aout_mips_big_vec;
 extern const bfd_target aout_mips_little_vec;
 extern const bfd_target aout0_big_vec;
 extern const bfd_target apollocoff_vec;
+extern const bfd_target armcoff_little_vec;
+extern const bfd_target armcoff_big_vec;
 extern const bfd_target armpe_little_vec;
 extern const bfd_target armpe_big_vec;
 extern const bfd_target armpei_little_vec;
@@ -669,6 +671,8 @@ const bfd_target * const bfd_target_vector[] = {
 	&i386os9k_vec,
 	&i386pe_vec,
 	&i386pei_vec,
+	&armcoff_little_vec,
+	&armcoff_big_vec,
 	&armpe_little_vec,
 	&armpe_big_vec,
 	&armpei_little_vec,
