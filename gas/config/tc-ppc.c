653b59c8a663508e460b20563c0c5e8b90ddc432
Michael Meissner <gnu@the-meissners.org>
1995-10-16 11:35:05 +0000

    Fix last fixes #2

--- gas/config/tc-ppc.c
+++ gas/config/tc-ppc.c
@@ -759,6 +759,8 @@ md_begin ()
 		 special case.  */
 	      if (strcmp (retval, "exists") != 0
 		  || (((ppc_cpu & PPC_OPCODE_601) == 0
+		       || ((ppc_cpu & ~PPC_OPCODE_POWER2)
+			   != (PPC_OPCODE_POWER | PPC_OPCODE_PPC))
 		       || strcmp (op->name, "mfdec") != 0)
 		      && (((ppc_cpu & ~PPC_OPCODE_POWER2)
 			  != (PPC_OPCODE_POWER | PPC_OPCODE_PPC))
