7be23c26d4464db8ae05bbe3a230f4bbaef1833b
Niklas Hallqvist <niklas@cygnus>
1995-10-28 00:36:29 +0000

    Define HOST_BIG_ENDIAN_P according to <machine/endian.h>.

--- bfd/hosts/nbsd.h
+++ bfd/hosts/nbsd.h
@@ -1,5 +1,6 @@
 /* Architecture independant NetBSD host support */
 
+#include <machine/endian.h>
 #include <machine/vmparam.h>
 #include <machine/param.h>
 #include <machine/reg.h>
@@ -7,3 +8,7 @@
 #define	HOST_PAGE_SIZE			NBPG
 #define	HOST_TEXT_START_ADDR		USRTEXT
 #define	HOST_STACK_END_ADDR		USRSTACK
+
+#if BYTE_ORDER == BIG_ENDIAN
+#define HOST_BIG_ENDIAN_P
+#endif
