904ae08389178d742da936508a8ef28af02a0793
David Edelsohn <dje.gcc@gmail.com>
1996-01-20 17:35:22 +0000

    Update copyright.

--- bfd/elf32-sparc.c
+++ bfd/elf32-sparc.c
@@ -1,5 +1,5 @@
 /* SPARC-specific support for 32-bit ELF
-   Copyright 1993, 1994, 1995 Free Software Foundation, Inc.
+   Copyright (C) 1993, 1994, 1995, 1996 Free Software Foundation, Inc.
 
 This file is part of BFD, the Binary File Descriptor library.
 
