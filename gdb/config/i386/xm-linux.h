44e9f3b3d38840ca7e72ddeed031391f0243b833
Fred Fish <fnf@specifix.com>
1996-01-31 20:35:51 +0000

    * config/i386/xm-linux.h (MMAP_BASE_ADDRESS, MMAP_INCREMENT): 	Define to what should be reasonable values.  However, apparently 	a bug in linux mmap prevents mapped symbol tables from working.

--- gdb/config/i386/xm-linux.h
+++ gdb/config/i386/xm-linux.h
@@ -33,4 +33,14 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 /* Need R_OK etc, but USG isn't defined.  */
 #include <unistd.h>
 
+/* If you expect to use the mmalloc package to obtain mapped symbol files,
+   for now you have to specify some parameters that determine how gdb places
+   the mappings in it's address space.  See the comments in map_to_address()
+   for details.  This is expected to only be a short term solution.  Yes it
+   is a kludge.
+   FIXME:  Make this more automatic. */
+
+#define MMAP_BASE_ADDRESS	0x20000000	/* First mapping here */
+#define MMAP_INCREMENT		0x01000000	/* Increment to next mapping */
+
 #endif	/* #ifndef XM_LINUX_H */
