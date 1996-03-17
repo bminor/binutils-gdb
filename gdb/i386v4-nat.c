d58ffc6cc6c304e25d46536469701a20cb67180f
Fred Fish <fnf@specifix.com>
1996-03-17 01:08:19 +0000

    * i386v4-nat.c (defs.h): Include before testing HAVE_SYS_PROCFS_H.

--- gdb/i386v4-nat.c
+++ gdb/i386v4-nat.c
@@ -17,9 +17,10 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
+#include "defs.h"
+
 #ifdef HAVE_SYS_PROCFS_H
 
-#include "defs.h"
 #include <sys/procfs.h>
 
 /*  The /proc interface divides the target machine's register set up into
