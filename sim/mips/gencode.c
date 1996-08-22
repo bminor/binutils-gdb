64d538ce806461aed599498bab023b4ae15a2ae2
Ian Lance Taylor <ian@airs.com>
1996-08-22 22:06:21 +0000

    * Makefile.in (gencode): Depend upon gencode.o, getopt.o, and 	getopt1.o, rather than on gencode.c.  Link objects together. 	Don't link against -liberty. 	(gencode.o, getopt.o, getopt1.o): New targets. 	* gencode.c: Include <ctype.h> and "ansidecl.h". 	(AND): Undefine after including "ansidecl.h". 	(ULONG_MAX): Define if not defined. 	(OP_*): Don't define macros; now defined in opcode/mips.h. 	(main): Call my_strtoul rather than strtoul. 	(my_strtoul): New static function.

--- sim/mips/gencode.c
+++ sim/mips/gencode.c
@@ -166,35 +166,18 @@
 #include <getopt.h>
 #include <limits.h>
 #include <errno.h>
+#include <ctype.h>
+#include "ansidecl.h"
 #include "opcode/mips.h"
 
-/* The following manifests do not appear in "include/opcode/mips.h" */
-#define OP_SH_LOCC       (8)     /* FP condition code */
-#define OP_SH_HICC       (18)    /* FP condition code */
-#define OP_MASK_CC       (0x07)
+/* FIXME: ansidecl.h defines AND.  */
+#undef AND
 
-#define OP_SH_COP1NORM   (25)    /* Normal COP1 encoding */
-#define OP_MASK_COP1NORM (0x01)  /* a single bit */
-#define OP_SH_COP1SPEC   (21)    /* Special COP1 encodings (within format field) */
-#define OP_MASK_COP1SPEC (0x0F)  /* Bits we are interested in */
-#define OP_MASK_COP1SCLR (0x04)  /* Bits to be explicitly cleared */
-#define OP_MASK_COP1CMP  (0x3)   /* Unfortunately another conditional field needed to decode the FP instructions */
-#define OP_SH_COP1CMP    (4)
+#ifndef ULONG_MAX
+#define	ULONG_MAX	((unsigned long)(~0L))		/* 0xFFFFFFFF */
+#endif
 
-#define OP_SH_FORMAT     (21)    /* FP short format field */
-#define OP_MASK_FORMAT   (0x07)
-
-#define OP_SH_TRUE       (16)
-#define OP_MASK_TRUE     (0x01)
-
-#define OP_SH_GE         (17)
-#define OP_MASK_GE       (0x01)
-
-#define OP_SH_UNSIGNED   (16)
-#define OP_MASK_UNSIGNED (0x01)
-
-#define OP_SH_HINT       (16)
-#define OP_MASK_HINT     (0x1F)
+static unsigned long my_strtoul ();
 
 #if 0
 #ifndef TRUE
@@ -2253,7 +2236,7 @@ main(argc,argv)
                    exit(1);
                  }
 
-                 num = strtoul(loptarg,&loptarg,10);
+                 num = my_strtoul(loptarg,&loptarg,10);
 
                  if ((num == ULONG_MAX) && (errno = ERANGE)) {
                    fprintf(stderr,"%s: Invalid number given to -mips option\n",progname);
@@ -2363,4 +2346,109 @@ main(argc,argv)
 }
 
 /*---------------------------------------------------------------------------*/
+
+/* We can't assume that the compiler for the build system has strtoul,
+   so we provide our own copy.  */
+
+/*
+ * Copyright (c) 1990 Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Convert a string to an unsigned long integer.
+ *
+ * Ignores `locale' stuff.  Assumes that the upper and lower case
+ * alphabets and digits are each contiguous.
+ */
+static unsigned long
+my_strtoul(nptr, endptr, base)
+	const char *nptr;
+	char **endptr;
+	register int base;
+{
+	register const char *s = nptr;
+	register unsigned long acc;
+	register int c;
+	register unsigned long cutoff;
+	register int neg = 0, any, cutlim;
+
+	/*
+	 * See strtol for comments as to the logic used.
+	 */
+	do {
+		c = *s++;
+	} while (isspace(c));
+	if (c == '-') {
+		neg = 1;
+		c = *s++;
+	} else if (c == '+')
+		c = *s++;
+	if ((base == 0 || base == 16) &&
+	    c == '0' && (*s == 'x' || *s == 'X')) {
+		c = s[1];
+		s += 2;
+		base = 16;
+	}
+	if (base == 0)
+		base = c == '0' ? 8 : 10;
+	cutoff = (unsigned long)ULONG_MAX / (unsigned long)base;
+	cutlim = (unsigned long)ULONG_MAX % (unsigned long)base;
+	for (acc = 0, any = 0;; c = *s++) {
+		if (isdigit(c))
+			c -= '0';
+		else if (isalpha(c))
+			c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+		else
+			break;
+		if (c >= base)
+			break;
+		if (any < 0 || acc > cutoff || acc == cutoff && c > cutlim)
+			any = -1;
+		else {
+			any = 1;
+			acc *= base;
+			acc += c;
+		}
+	}
+	if (any < 0) {
+		acc = ULONG_MAX;
+		errno = ERANGE;
+	} else if (neg)
+		acc = -acc;
+	if (endptr != 0)
+		*endptr = (char *) (any ? s - 1 : nptr);
+	return (acc);
+}
+
+/*---------------------------------------------------------------------------*/
+
 /*> EOF gencode.c <*/
