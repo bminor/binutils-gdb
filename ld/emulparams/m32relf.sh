a5c1ebecc5544012f7c5b73b12bf608dc03815f2
David Edelsohn <dje.gcc@gmail.com>
1996-10-17 17:19:52 +0000

    * emulparams/m32relf.sh (EMBEDDED): Define.

--- ld/emulparams/m32relf.sh
+++ ld/emulparams/m32relf.sh
@@ -4,6 +4,7 @@ OUTPUT_FORMAT="elf32-m32r"
 TEXT_START_ADDR=0x0
 ARCH=m32r
 MAXPAGESIZE=32
+EMBEDDED=yes
 
 # Hmmm, there's got to be a better way.  This sets the stack to the
 # top of the simulator memory (currently 1M).
