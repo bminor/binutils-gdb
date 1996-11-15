2583a05ba21aa5d54ca38ddd9b4b265c20cb36e1
Jeff Law <law@redhat.com>
1996-11-15 20:57:53 +0000

    * gas/mn10300/other.s: Put parens around register         argument in calls and jmp instructions.

--- gas/testsuite/gas/mn10300/other.s
+++ gas/testsuite/gas/mn10300/other.s
@@ -3,12 +3,12 @@
 	inc d1
 	inc a2
 	inc4 a3
-	jmp a2
+	jmp (a2)
 	jmp 256
 	jmp 131071
 	call 256,5,9
-	call 131071,0,0
-	calls a2
+	call 131071,9,32
+	calls (a2)
 	calls 256
 	calls 131071
 	ret 15,7
