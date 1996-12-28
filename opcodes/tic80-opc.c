6357e7f68e62ef85600908495de20eb2b28ccaa3
Fred Fish <fnf@specifix.com>
1996-12-28 05:36:52 +0000

    (Laying groundwork (that will be incrementally fleshed out) for TIc80 support) 	* configure.in (arch in $selarchs): Add bfd_tic80_arch entry. 	* configure: Regenerate with autoconf. 	* tic80-dis.c: Add file. 	* tic80-opc.c: Add file.

---
+++ opcodes/tic80-opc.c
@@ -0,0 +1,21 @@
+/* Opcode table for TI TMS320C80 (MVP).
+   Copyright 1996 Free Software Foundation, Inc.
+
+This file is part of GDB, GAS, and the GNU binutils.
+
+GDB, GAS, and the GNU binutils are free software; you can redistribute
+them and/or modify them under the terms of the GNU General Public
+License as published by the Free Software Foundation; either version
+1, or (at your option) any later version.
+
+GDB, GAS, and the GNU binutils are distributed in the hope that they
+will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file; see the file COPYING.  If not, write to the Free
+Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+#include "ansidecl.h"
+#include "opcode/tic80.h"
