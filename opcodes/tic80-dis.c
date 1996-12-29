a79d0193ecec8fb0032d2511233cd04ee805b326
Fred Fish <fnf@specifix.com>
1996-12-29 18:01:29 +0000

    * Makefile.in (ALL_MACHINES): Add tic80-dis.o and tic80-opc.o. 	* disassemble.c (ARCH_tic80): Define if ARCH_all is defined. 	(disassembler): Add bfd_arch_tic80 support to set disassemble  	to print_insn_tic80. 	* tic80-dis.c (print_insn_tic80): Add stub.

--- opcodes/tic80-dis.c
+++ opcodes/tic80-dis.c
@@ -14,3 +14,17 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+#include <stdio.h>
+
+#include "ansidecl.h"
+#include "opcode/tic80.h"
+#include "dis-asm.h"
+
+int 
+print_insn_tic80 (memaddr, info)
+     bfd_vma memaddr;
+     struct disassemble_info *info;
+{
+  abort ();
+}
