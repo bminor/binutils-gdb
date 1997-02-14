e3e72ac561e84749fb9602ce5cdf439459976b74
Ian Lance Taylor <ian@airs.com>
1997-02-14 01:36:24 +0000

    Thu Feb 13 20:31:37 1997  Philippe De Muyter  <phdm@info.ucl.ac.be>
    	* configure.in: Call BFD_NEED_DECLARATION on getenv.
    	* acconfig.h (NEED_DECLARATION_GETENV): New macro.
    	* sysdep.h (getenv): Declare if NEED_DECLARATION_GETENV.
    	* ldemul.c (ld_emul_default_target): Do not cast getenv return
    	value.
    	* ldmain.c (get_emulation): Likewise.
    	* configure, config.in: Rebuild.

--- ld/acconfig.h
+++ ld/acconfig.h
@@ -7,6 +7,9 @@
 
 /* Whether sbrk must be declared even if <unistd.h> is included.  */
 #undef NEED_DECLARATION_SBRK
+
+/* Whether getenv must be declared even if <stdlib.h> is included.  */
+#undef NEED_DECLARATION_GETENV
 @TOP@
 
 /* Do we need to use the b modifier when opening binary files?  */
