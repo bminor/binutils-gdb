414a1069b77d498337f060dd361c184fa2f15557
Ian Lance Taylor <ian@airs.com>
1997-02-14 23:05:36 +0000

    random copyright and formatting fixes

--- gas/config/tc-tic80.h
+++ gas/config/tc-tic80.h
@@ -1,5 +1,4 @@
 /* This file is tc-tic80.h
-
    Copyright (C) 1996, 1997, Free Software Foundation, Inc.
 
    This file is part of GAS, the GNU Assembler.
@@ -15,8 +14,9 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with GAS; see the file COPYING.  If not, write to
-   the Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   along with GAS; see the file COPYING.  If not, write to the Free
+   Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.  */
 
 #define TC_TIC80
 
