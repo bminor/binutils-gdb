70e6bc87372944b6b7a8f5c0f94f570af1e61129
Doug Evans <dje@google.com>
1998-01-06 13:49:12 +0000

    Update to new style, no | separator.

--- gas/testsuite/gas/txvu/basic-1.s
+++ gas/testsuite/gas/txvu/basic-1.s
@@ -1,5 +1,5 @@
 ; Simple testsuite, to get something going.
 
 foo:
-	abs.xyz vf10xyz,vf20xyz             | waitp
-	add.xyzw vf10xyzw,vf20xyzw,vf30xyzw | waitq
+	abs.xyz vf10xyz,vf20xyz               waitp
+	add.xyzw vf10xyzw,vf20xyzw,vf30xyzw   waitq
