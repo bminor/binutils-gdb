ecdf8528815ae69f8120ab9d95cb6d7fcbc6f526
Doug Evans <dje@google.com>
1998-01-23 03:15:26 +0000

    update, lower insn has lower address

--- gas/testsuite/gas/txvu/basic-1.d
+++ gas/testsuite/gas/txvu/basic-1.d
@@ -7,7 +7,7 @@
 Disassembly of section .text:
 
 0* <foo>:
- *0:	fd a1 ca 01 	abs.xyz vf10xyz,vf20xyz[ 	]*waitp
- *4:	bf 07 00 80 
- *8:	a8 a2 fe 01 	add.xyzw vf10xyzw,vf20xyzw,vf30xyzw[ 	]*waitq
- *c:	bf 03 00 80 
+ *0:	bf 07 00 80[ 	]*abs.xyz vf10xyz,vf20xyz[ 	]*waitp
+ *4:	fd a1 ca 01[ 	]*
+ *8:	bf 03 00 80[ 	]*add.xyzw vf10xyzw,vf20xyzw,vf30xyzw[ 	]*waitq
+ *c:	a8 a2 fe 01[ 	]*
