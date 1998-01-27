17d8f29c26234f32f4e284eb28e81003cc19a525
Doug Evans <dje@google.com>
1998-01-27 18:18:53 +0000

    use '/' in one case to test it

--- gas/testsuite/gas/txvu/basic-1.s
+++ gas/testsuite/gas/txvu/basic-1.s
@@ -1,6 +1,10 @@
-; Simple testsuite, to get something going.
+; Simple testsuite, to get something going, plus placeholder
+; for various random tests.
 
 	.vu
 foo:
 	abs.xyz vf10xyz,vf20xyz               waitp
-	add.xyzw vf10xyzw,vf20xyzw,vf30xyzw   waitq
+
+; Test / in dest spec.
+
+	add/xyzw vf10xyzw,vf20xyzw,vf30xyzw   waitq
