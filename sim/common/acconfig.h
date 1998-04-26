56df89bcf6fce376f7456b7f1ae8032d3d807776
Tom Tromey <tromey@redhat.com>
1998-04-26 21:39:16 +0000

    * configure: Regenerated to track ../common/aclocal.m4 changes. 	* config.in: Ditto. 	* acconfig.h (NEED_DECLARATION_PRINTF): Removed.

--- sim/common/acconfig.h
+++ sim/common/acconfig.h
@@ -13,6 +13,3 @@
 
 /* Define if your locale.h file contains LC_MESSAGES.  */
 #undef HAVE_LC_MESSAGES
-
-/* Whether printf must be declared even if <stdio.h> is included.  */
-#undef NEED_DECLARATION_PRINTF
