1cc17500c38fa1cff19c3b5021dd74296a872b8c
Frank Ch. Eigler <fche@redhat.com>
1998-05-18 17:48:38 +0000

    * Added tests for R5900 mult1 instructions.

--- gas/testsuite/gas/mips/r5900.d
+++ gas/testsuite/gas/mips/r5900.d
@@ -15,8 +15,8 @@ Disassembly of section \.text:
 0+0018 <stuff\+(0x|)18> mfdvbm	\$t6
 0+001c <stuff\+(0x|)1c> mfiab	\$t7
 0+0020 <stuff\+(0x|)20> mfiabm	\$s0
-0+0024 <stuff\+(0x|)24> mfpc	\$a0,\$1
-0+0028 <stuff\+(0x|)28> mfps	\$a0,\$1
+0+0024 <stuff\+(0x|)24> mfpc	\$a0,1
+0+0028 <stuff\+(0x|)28> mfps	\$a0,1
 0+002c <stuff\+(0x|)2c> mtbpc	\$t2
 0+0030 <stuff\+(0x|)30> mtdab	\$t3
 0+0034 <stuff\+(0x|)34> mtdabm	\$t4
@@ -24,8 +24,8 @@ Disassembly of section \.text:
 0+003c <stuff\+(0x|)3c> mtdvbm	\$t6
 0+0040 <stuff\+(0x|)40> mtiab	\$t7
 0+0044 <stuff\+(0x|)44> mtiabm	\$s0
-0+0048 <stuff\+(0x|)48> mtpc	\$a0,\$1
-0+004c <stuff\+(0x|)4c> mtps	\$a0,\$1
+0+0048 <stuff\+(0x|)48> mtpc	\$a0,1
+0+004c <stuff\+(0x|)4c> mtps	\$a0,1
 0+0050 <stuff\+(0x|)50> adda.s	\$f12,\$f14
 0+0054 <stuff\+(0x|)54> suba.s	\$f10,\$f12
 0+0058 <stuff\+(0x|)58> msuba.s	\$f10,\$f12
@@ -33,4 +33,6 @@ Disassembly of section \.text:
 0+0060 <stuff\+(0x|)60> madda.s	\$f10,\$f12
 0+0064 <stuff\+(0x|)64> max.s	\$f10,\$f12,\$f14
 0+0068 <stuff\+(0x|)68> min.s	\$f10,\$f12,\$f14
-0+006c <stuff\+(0x|)6c> nop
+0+006c <stuff\+(0x|)6c> mult1	\$v1,\$a0,\$zero
+0+0070 <stuff\+(0x|)70> mult1	\$a0,\$v1
+0+0074 <stuff\+(0x|)74> nop
--- gas/testsuite/gas/mips/r5900.s
+++ gas/testsuite/gas/mips/r5900.s
@@ -11,8 +11,8 @@ stuff:
 	mfdvbm	$14
 	mfiab	$15
 	mfiabm	$16
-	mfpc	$4,$1
-	mfps	$4,$1
+	mfpc	$4,1
+	mfps	$4,1
 	mtbpc	$10
 	mtdab	$11
 	mtdabm	$12
@@ -20,8 +20,8 @@ stuff:
 	mtdvbm	$14
 	mtiab	$15
 	mtiabm	$16
-	mtpc	$4,$1
-	mtps	$4,$1
+	mtpc	$4,1
+	mtps	$4,1
 
 
 	adda.s  $f12,$f14
@@ -31,5 +31,8 @@ stuff:
 	madda.s $f10,$f12
 	max.s   $f10,$f12,$f14
 	min.s   $f10,$f12,$f14
+
+	mult1	$3,$4,$0
+	mult1	$4,$3
 # Nop just to fill out ot a 16byte boundary
 	nop
