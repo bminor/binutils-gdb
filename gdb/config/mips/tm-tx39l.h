116b98b89ab999090be68eb5bf20958399757a21
Gavin Romig-Koch <gavin@redhat.com>
1998-05-20 19:31:04 +0000

    * mips/tm-tx39.h (MIPS_DEFAULT_FPU_TYPE) : Defined as MIPS_FPU_NONE. 	* mips/tm-tx39l.h : Same.

--- gdb/config/mips/tm-tx39l.h
+++ gdb/config/mips/tm-tx39l.h
@@ -18,6 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #define TARGET_BYTE_ORDER_SELECTABLE
 #define MIPS_EABI 1
+#define MIPS_DEFAULT_FPU_TYPE MIPS_FPU_NONE
 
 #include "mips/tm-mips.h"
 
