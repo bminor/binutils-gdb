695bc2ee12b2bfc0954e6856d37074f147e1c2a0
Doug Evans <dje@google.com>
1998-06-13 00:53:24 +0000

    * scripttempl/elf.sc (INPUT_FILES): Optional INPUT spec. 	* emulparams/m32relf.sh (INPUT_FILES): Define. 	(OTHER_RELOCATING_SECTIONS): Change top of stack to 8MB.

--- ld/emulparams/m32relf.sh
+++ ld/emulparams/m32relf.sh
@@ -1,11 +1,19 @@
 MACHINE=
 SCRIPT_NAME=elf
+TEMPLATE_NAME=elf32
 OUTPUT_FORMAT="elf32-m32r"
-TEXT_START_ADDR=0x0
+TEXT_START_ADDR=0x100
 ARCH=m32r
+MACHINE=
 MAXPAGESIZE=32
 EMBEDDED=yes
 
+# For the default linker script, include libgloss.
+# If the user specifies a different linker script, we want the
+# rest of the files (e.g. crt0.o) included by default, but not libgloss.
+# [Thus I was reluctant to handle this in gcc specs.]
+INPUT_FILES="INPUT(-lgloss)"
+
 # Hmmm, there's got to be a better way.  This sets the stack to the
-# top of the simulator memory (currently 1M).
-OTHER_RELOCATING_SECTIONS='.stack 0x100000 : { _stack = .; *(.stack) }'
+# top of simulator memory (8MB).
+OTHER_RELOCATING_SECTIONS='PROVIDE (_stack = 0x800000);'
