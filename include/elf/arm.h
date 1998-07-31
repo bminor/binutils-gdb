860fe0c07ee757781e2d18f1cc5361713a178fa3
Catherine Moore <clm@redhat.com>
1998-07-31 15:04:36 +0000

    Add R_ARM_THM_PC9 relocation.

--- include/elf/arm.h
+++ include/elf/arm.h
@@ -22,20 +22,40 @@
 
 #include "elf/reloc-macros.h"
 
+/* Processor specific flags for the ELF header e_flags field.  */
+#define EF_ARM_RELEXEC   0x01
+#define EF_ARM_HASENTRY  0x02
+
+/* ARM-specific values for sh_flags */
+#define SHF_ENTRYSECT      0x10000000   /* Section contains an entry point */
+#define SHF_COMDEF         0x80000000   /* Section may be multiply defined in the input to a link step */
+
+/* ARM-specific program header flags */
+#define PF_ARM_SB          0x10000000   /* Segment contains the location addressed by the static base */
+
 /* Relocation types.  */
 START_RELOC_NUMBERS (elf_arm_reloc_type)
-  RELOC_NUMBER (R_ARM_NONE, 0)		/* No reloc */
-  RELOC_NUMBER (R_ARM_PC24, 1)		/* PC relative 26 bit branch */
-  RELOC_NUMBER (R_ARM_ABS32, 2)		/* Direct 32 bit  */
-  RELOC_NUMBER (R_ARM_REL32, 3)		/* PC relative 32 bit */
-  RELOC_NUMBER (R_ARM_COPY, 20)		/* Copy symbol at runtime */
-  RELOC_NUMBER (R_ARM_GLOB_DAT, 21)	/* Create GOT entry */
-  RELOC_NUMBER (R_ARM_JUMP_SLOT, 22)	/* Create PLT entry */
-  RELOC_NUMBER (R_ARM_RELATIVE, 23)	/* Adjust by program base */
-  RELOC_NUMBER (R_ARM_GOTOFF, 24)	/* 32 bit offset to GOT */
-  RELOC_NUMBER (R_ARM_GOTPC, 25)	/* 32 bit PC relative offset to GOT */
-  RELOC_NUMBER (R_ARM_GOT32, 26)	/* 32 bit GOT entry */
-  RELOC_NUMBER (R_ARM_PLT32, 27)	/* 32 bit PLT address */
+  RELOC_NUMBER (R_ARM_NONE,         0)
+  RELOC_NUMBER (R_ARM_PC24,         1)
+  RELOC_NUMBER (R_ARM_ABS32,        2)
+  RELOC_NUMBER (R_ARM_REL32,        3)
+  RELOC_NUMBER (R_ARM_ABS8,         4)
+  RELOC_NUMBER (R_ARM_ABS16,        5)
+  RELOC_NUMBER (R_ARM_ABS12,        6)
+  RELOC_NUMBER (R_ARM_THM_ABS5,     7)
+  RELOC_NUMBER (R_ARM_THM_PC22,     8)
+  RELOC_NUMBER (R_ARM_SBREL32,      9)
+  RELOC_NUMBER (R_ARM_AMP_VCALL9,  10)
+  RELOC_NUMBER (R_ARM_THM_PC11,    11)       /* cygnus extension to abi: thumb unconditional branch */
+  RELOC_NUMBER (R_ARM_THM_PC9,     12)       /* cygnus extension to abi: thumb conditional branch */
+  FAKE_RELOC   (FIRST_INVALID_RELOC, 13)
+  FAKE_RELOC   (LAST_INVALID_RELOC,  249)
+  RELOC_NUMBER (R_ARM_RSBREL32,   250)
+  RELOC_NUMBER (R_ARM_THM_RPC22,  251)
+  RELOC_NUMBER (R_ARM_RREL32,     252)
+  RELOC_NUMBER (R_ARM_RABS32,     253)
+  RELOC_NUMBER (R_ARM_RPC24,      254)
+  RELOC_NUMBER (R_ARM_RBASE,      255)
 END_RELOC_NUMBERS
 
 #endif
