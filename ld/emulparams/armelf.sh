e9ebd69fe6c9f9a4b4e0075a04bbbf33758f48b4
Catherine Moore <clm@redhat.com>
1998-08-26 17:17:56 +0000

    remove armelf sanitization.

--- ld/emulparams/armelf.sh
+++ ld/emulparams/armelf.sh
@@ -1,10 +1,10 @@
-# start-sanitize-armelf
 MACHINE=
-SCRIPT_NAME=elf
+SCRIPT_NAME=elfarm
 OUTPUT_FORMAT="elf32-littlearm"
 BIG_OUTPUT_FORMAT="elf32-bigarm"
 LITTLE_OUTPUT_FORMAT="elf32-littlearm"
 TEXT_START_ADDR=0x0
+TEMPLATE_NAME=armelf
 ARCH=arm
 MACHINE=
 MAXPAGESIZE=256
@@ -14,4 +14,3 @@ EMBEDDED=yes
 # Hmmm, there's got to be a better way.  This sets the stack to the
 # top of the simulator memory (2^19 bytes).
 OTHER_RELOCATING_SECTIONS='.stack 0x80000 : { _stack = .; *(.stack) }'
-# end-sanitize-armelf
