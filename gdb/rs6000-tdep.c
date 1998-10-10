c6d28b22dad86b306a86c95b5a5bb2b8c03f6aef
Mark Alexander <marka@cygnus>
1998-10-10 01:17:07 +0000

    * rs6000-tdep.c: Don't include tm.h twice.

--- gdb/rs6000-tdep.c
+++ gdb/rs6000-tdep.c
@@ -19,7 +19,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include "defs.h"
-#include "tm.h"
 #include "frame.h"
 #include "inferior.h"
 #include "symtab.h"
