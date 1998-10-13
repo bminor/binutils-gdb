1dee2761ad89b43593f2a5fac932828189a56837
Jason Molenda <jmolenda@apple.com>
1998-10-13 00:35:48 +0000

    1998-10-12  Jason Molenda  (jsm@bugshack.cygnus.com)
            * configure.in: Check for sys/debugreg.h, asm/debugreg.h.
            * i386v-nat.c: Include asm/debugreg.h, sys/debugreg.h if it is not
            present.
    
    This is to work around a conflict where the Linux 2.1.x kernel and glibc
    2.0.x are not in sync; including <sys/debugreg.h> will result in an error.
    With luck, these losers will get their act together and we can trash
    this hack in the near future.

--- gdb/i386v-nat.c
+++ gdb/i386v-nat.c
@@ -1,5 +1,5 @@
 /* Intel 386 native support for SYSV systems (pre-SVR4).
-   Copyright (C) 1988, 1989, 1991, 1992, 1994, 1996 Free Software Foundation, Inc.
+   Copyright (C) 1988, 89, 91, 92, 94, 96, 1998 Free Software Foundation, Inc.
 
 This file is part of GDB.
 
@@ -43,8 +43,20 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #include <sys/ioctl.h>
 #include <fcntl.h>
 
+
+/* FIXME: The following used to be just "#include <sys/debugreg.h>", but
+ * the the Linux 2.1.x kernel and glibc 2.0.x are not in sync; including
+ * <sys/debugreg.h> will result in an error.  With luck, these losers
+ * will get their act together and we can trash this hack in the near future.
+ * --jsm 1998-10-21
+ */
+
 #ifdef TARGET_HAS_HARDWARE_WATCHPOINTS
-#include <sys/debugreg.h>
+# ifdef HAVE_ASM_DEBUGREG_H
+#  include <asm/debugreg.h>
+# else
+#  include <sys/debugreg.h>
+# endif
 #endif
 
 #include <sys/file.h>
