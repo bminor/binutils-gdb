61e6bcc839b7548f47d6a5565c70a59efd4d60ee
Felix Lee <flee@cygnus>
1998-10-17 17:40:35 +0000

    * core-sol2.c: #include <sys/types.h>, for sol2.7 weirdness.

--- gdb/core-sol2.c
+++ gdb/core-sol2.c
@@ -30,6 +30,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #undef fpregset_t
 
 #include <time.h>
+#include <sys/types.h>
 #include <sys/regset.h>
 #include <sys/procfs.h>
 #include <fcntl.h>
