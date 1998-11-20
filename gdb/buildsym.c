60a98b30b42d1e78e4d149eaccdca71c8fbbc55c
Andrew Cagney <cagney@redhat.com>
1998-11-20 00:00:24 +0000

    CARP: * buildsym.c (end_symtab): Cleanup PROCESS_LINENUMBER_HOOK.

--- gdb/buildsym.c
+++ gdb/buildsym.c
@@ -900,9 +900,10 @@ end_symtab (end_addr, objfile, section)
       blockvector = make_blockvector (objfile);
     }
 
-#ifdef PROCESS_LINENUMBER_HOOK
-  PROCESS_LINENUMBER_HOOK ();			/* Needed for xcoff. */
+#ifndef PROCESS_LINENUMBER_HOOK
+#define PROCESS_LINENUMBER_HOOK()
 #endif
+  PROCESS_LINENUMBER_HOOK ();			/* Needed for xcoff. */
 
   /* Now create the symtab objects proper, one for each subfile.  */
   /* (The main file is the last one on the chain.)  */
