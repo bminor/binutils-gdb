897ccbb0844f3707164c256aadef02c9273d51d5
Jason Molenda <jmolenda@apple.com>
1999-01-11 23:22:25 +0000

    1999-01-11  Jason Molenda  (jsm@bugshack.cygnus.com)
            * scm-tags.h: Update FSF's address on copyright notice.
            * ser-e7kpc.c: Same.
            * gnu-nat.h: Same.

--- gdb/gnu-nat.h
+++ gdb/gnu-nat.h
@@ -1,22 +1,21 @@
 /* Common things used by the various *gnu-nat.c files
-
    Copyright (C) 1995, 1996 Free Software Foundation, Inc.
 
-   Written by Miles Bader <miles@gnu.ai.mit.edu>
+Written by Miles Bader <miles@gnu.ai.mit.edu>
 
-   The GNU Hurd is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2, or (at
-   your option) any later version.
+The GNU Hurd is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2, or (at
+your option) any later version.
 
-   The GNU Hurd is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
+The GNU Hurd is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #ifndef __GNU_NAT_H__
 #define __GNU_NAT_H__
