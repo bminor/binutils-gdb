c6d4ea9dde813dff9eca629a95bc800c30220919
Doug Evans <dje@google.com>
1999-01-15 07:40:02 +0000

    regenerate

--- opcodes/i960c-dis.c
+++ opcodes/i960c-dis.c
@@ -3,7 +3,7 @@
 
 THIS FILE IS USED TO GENERATE i960c-dis.c.
 
-Copyright (C) 1996, 1997, 1998 Free Software Foundation, Inc.
+Copyright (C) 1996, 1997, 1998, 1999 Free Software Foundation, Inc.
 
 This file is part of the GNU Binutils and GDB, the GNU debugger.
 
