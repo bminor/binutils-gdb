a07304dfa340a5a1af6353420b5df3d8b39642b5
Frank Ch. Eigler <fche@redhat.com>
1999-01-26 11:34:10 +0000

    * Update copyright year.

--- sim/common/sim-memopt.c
+++ sim/common/sim-memopt.c
@@ -1,5 +1,5 @@
 /* Simulator memory option handling.
-   Copyright (C) 1996, 1997 Free Software Foundation, Inc.
+   Copyright (C) 1996-1999 Free Software Foundation, Inc.
    Contributed by Cygnus Support.
 
 This file is part of GDB, the GNU debugger.
