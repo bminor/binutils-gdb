5cee067a5894f13a3ce86508a700a51574901c11
Nick Clifton <nickc@redhat.com>
1999-01-29 10:17:51 +0000

    Add tests of readelf.

---
+++ binutils/testsuite/binutils-all/readelf.r
@@ -0,0 +1,4 @@
+
+Relocation section '.rel..text' at offset 0x2b8 contains 1 entries:
+  Offset    Info  Type            Symbol's Value  Symbol's Name          Addend
+  00000004  00.* R_.*           00000000  external_symbol           \+ 0
