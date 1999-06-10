4abec3504c6f1b1a39a5803bb25b4069299f80e0
Nick Clifton <nickc@redhat.com>
1999-06-10 01:20:33 +0000

    Updated to match latest output.

--- binutils/testsuite/binutils-all/readelf.h
+++ binutils/testsuite/binutils-all/readelf.h
@@ -1,5 +1,5 @@
 ELF Header:
-  Magic:   7f 45 4c 46 0[12] 0[12] 01 00 00 00 00 00 00 00 00 00 
+  Magic:   7f 45 4c 46 0[12] 0[12] 01 .. 00 00 00 00 00 00 00 00 
   Class:                             ELF[36][24]
   Data:                              2's complement,.* endian
   Version:                           1 \(current\)
@@ -8,7 +8,6 @@ ELF Header:
   Type:                              REL \(Relocatable file\)
   Machine:                           .*
   Version:                           0x1
-  Data:                              ELFDATA.* endian\)
   Entry point address:               0x0
   Start of program headers:          0 \(bytes into file\)
   Start of section headers:          .* \(bytes into file\)
