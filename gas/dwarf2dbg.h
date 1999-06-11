a7cd1c22994996423fd836cd774bc81535b0d5b2
Ian Lance Taylor <ian@airs.com>
1999-06-11 20:29:31 +0000

    * dwarf2dbg.h: Use PARAMS in function declarations.

--- gas/dwarf2dbg.h
+++ gas/dwarf2dbg.h
@@ -18,7 +18,7 @@ struct dwarf2_line_info
 /* Implements the .file FILENO "FILENAME" directive.  FILENO can be 0
    to indicate that no file number has been assigned.  All real file
    number must be >0.  */
-extern void dwarf2_directive_file (int dummy);
+extern void dwarf2_directive_file PARAMS ((int dummy));
 
 /* Implements the .loc FILENO LINENO [COLUMN] directive.  FILENO is
    the file number, LINENO the line number and the (optional) COLUMN
@@ -26,23 +26,24 @@ extern void dwarf2_directive_file (int dummy);
    corresponds to.  FILENO can be 0 to indicate that the filename
    specified by the textually most recent .file directive should be
    used.  */
-extern void dwarf2_directive_loc (int dummy);
+extern void dwarf2_directive_loc PARAMS ((int dummy));
 
 /* Returns the current source information.  If .file directives have
    been encountered, the info for the corresponding source file is
    returned.  Otherwise, the info for the assembly source file is
    returned.  */
-extern void dwarf2_where (struct dwarf2_line_info *l);
+extern void dwarf2_where PARAMS ((struct dwarf2_line_info *l));
 
 /* This function generates .debug_line info based on the address and
    source information passed in the arguments.  ADDR should be the
    frag-relative offset of the instruction the information is for and
    L is the source information that should be associated with that
    address. */
-extern void dwarf2_gen_line_info (bfd_vma addr, struct dwarf2_line_info *l);
+extern void dwarf2_gen_line_info PARAMS ((bfd_vma addr,
+					  struct dwarf2_line_info *l));
 
 /* Must be called after all other input is processed to finish up the
    .debug_line section.  */
-extern void dwarf2_finish (void);
+extern void dwarf2_finish PARAMS ((void));
 
 #endif /* AS_DWARF2DBG_H */
