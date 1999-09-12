946d15cab8383a8d04a6cd33a2b7165244f17a4e
Ian Lance Taylor <ian@airs.com>
1999-09-12 17:47:19 +0000

    1999-09-12  Donn Terry  <donn@interix.com>
    	* ar.c (print_contents): Change printing of member name for
    	POSIX.2 conformance.

--- binutils/ar.c
+++ binutils/ar.c
@@ -751,8 +751,7 @@ print_contents (abfd)
     fatal (_("internal stat error on %s"), bfd_get_filename (abfd));
 
   if (verbose)
-    /* xgettext:c-format */
-    printf (_("\n<member %s>\n\n"), bfd_get_filename (abfd));
+    printf ("\n<%s>\n\n", bfd_get_filename (abfd));
 
   bfd_seek (abfd, 0, SEEK_SET);
 
