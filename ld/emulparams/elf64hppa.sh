f68ca336122f3cc4cd16b02b4448c1d9b8a9781f
Jeff Law <law@redhat.com>
1999-09-17 17:02:03 +0000

    * emulparms/elf64hppa.sh (TEXT_START_SYMBOLS): Kill completely.         (DATA_START_SYMBOLS): Kill __hp_load_map definition

--- ld/emulparams/elf64hppa.sh
+++ ld/emulparams/elf64hppa.sh
@@ -24,28 +24,25 @@ OTHER_READONLY_SECTIONS='.PARISC.unwind : { *(.PARISC.unwind) } .stubs : { *(.st
 
 # The PA64 ELF port treats .plt sections differently than most.  We also have
 # to create a .opd section.  What most systems call the .got, we call the .dlt
-OTHER_READWRITE_SECTIONS='.opd : { *(.opd) } __gp = .; .plt : { *(.plt) } .dlt : { *(.dlt) }'
+OTHER_READWRITE_SECTIONS='.opd : { *(.opd) } PROVIDE (__gp = .); .plt : { *(.plt) } .dlt : { *(.dlt) }'
 
 # The PA64 ELF port has two additional bss sections. huge bss and thread bss.
 # Make sure they end up in the appropriate location.  We also have to set
 # __TLS_SIZE to the size of the thread bss section.
-OTHER_BSS_SECTIONS='.hbss : { *(.hbss) } .tbss : { *(.tbss) } __TLS_SIZE = SIZEOF(.tbss);'
+OTHER_BSS_SECTIONS='.hbss : { *(.hbss) } .tbss : { *(.tbss) }'
+#OTHER_BSS_END_SYMBOLS='PROVIDE (__TLS_SIZE = SIZEOF (.tbss));'
+OTHER_BSS_END_SYMBOLS='PROVIDE (__TLS_SIZE = 0);'
 
 # HPs use .dlt where systems use .got.  Sigh.
 OTHER_GOT_RELOC_SECTIONS='.rela.dlt : { *(.rela.dlt) }'
 
-# This does not have to be strictly the bottom of the text segment since
-# it is merely for performing relocations relative to a symbol that is not
-# going to be referenced in any other manner.
-#
-# If it was the true base of the text segment, then we could use it for
-# building unwinders instead of computing the base of the text segment
-# in the BFD backend.
-TEXT_START_SYMBOLS='__text_dummy = . ;'
-DATA_START_SYMBOLS='__hp_load_map = .; . += 16;'
+# We're not actually providing a symbol anymore (due to the inability to be
+# safe in regards to shared libraries). So we just allocate the hunk of space
+# unconditionally, but do not mess around with the symbol table.
+DATA_START_SYMBOLS='. += 16;'
 
 # The linker is required to define these two symbols.
-EXECUTABLE_SYMBOLS='__SYSTEM_ID = 0x214; _FPU_STATUS = 0x0;'
+EXECUTABLE_SYMBOLS='PROVIDE (__SYSTEM_ID = 0x214); PROVIDE (_FPU_STATUS = 0x0);'
 DATA_PLT=
 
 # .dynamic should be at the start of the .text segment.
@@ -53,7 +50,7 @@ TEXT_DYNAMIC=
 
 # The PA64 ELF port needs two additional initializer sections and also wants
 # a start/end symbol pair for the .init and .fini sections.
-INIT_START='KEEP (*(.HP.init)); __preinit_start = .; KEEP (*(.preinit)); __preinit_end = .;__init_start = .;'
-INIT_END='__init_end = .;'
-FINI_START='__fini_start = .;'
-FINI_END='__fini_end = .;'
+INIT_START='KEEP (*(.HP.init)); PROVIDE (__preinit_start = .); KEEP (*(.preinit)); PROVIDE (__preinit_end = .); PROVIDE (__init_start = .);'
+INIT_END='PROVIDE (__init_end = .);'
+FINI_START='PROVIDE (__fini_start = .);'
+FINI_END='PROVIDE (__fini_end = .);'
