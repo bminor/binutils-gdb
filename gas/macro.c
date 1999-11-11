c1eae11499c60c61b6aaa5fa5e316613dcb0b6ad
Nick Clifton <nickc@redhat.com>
1999-11-11 16:35:12 +0000

    Look for seperator after TO and FROM tokens when exploring nested structures

--- gas/macro.c
+++ gas/macro.c
@@ -245,9 +245,11 @@ buffer_and_nest (from, to, ptr, get_line)
 	{
 	  if (ptr->ptr[i] == '.')
 	      i++;
-	  if (strncasecmp (ptr->ptr + i, from, from_len) == 0)
+	  if (strncasecmp (ptr->ptr + i, from, from_len) == 0
+	      && ! isalnum (ptr->ptr[i + from_len]))
 	    depth++;
-	  if (strncasecmp (ptr->ptr + i, to, to_len) == 0)
+	  if (strncasecmp (ptr->ptr + i, to, to_len) == 0
+	      && ! isalnum (ptr->ptr[i + to_len]))
 	    {
 	      depth--;
 	      if (depth == 0)
