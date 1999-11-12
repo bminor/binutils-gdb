035349a40dd7d6fde2cb6176dcf229a09f9087b3
Nick Clifton <nickc@redhat.com>
1999-11-12 11:02:56 +0000

    do not look beyond the end of the buffer

--- gas/macro.c
+++ gas/macro.c
@@ -246,10 +246,10 @@ buffer_and_nest (from, to, ptr, get_line)
 	  if (ptr->ptr[i] == '.')
 	      i++;
 	  if (strncasecmp (ptr->ptr + i, from, from_len) == 0
-	      && ! isalnum (ptr->ptr[i + from_len]))
+	      && (ptr->len == (i + from_len || ! isalnum (ptr->ptr[i + from_len]))))
 	    depth++;
 	  if (strncasecmp (ptr->ptr + i, to, to_len) == 0
-	      && ! isalnum (ptr->ptr[i + to_len]))
+	      && (ptr->len == (i + to_len) || ! isalnum (ptr->ptr[i + to_len])))
 	    {
 	      depth--;
 	      if (depth == 0)
