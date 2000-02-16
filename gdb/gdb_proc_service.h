83d37ec863028d177ac35e57a113caa65ec80d59
Mark Kettenis <kettenis@gnu.org>
2000-02-16 13:45:24 +0000

    2000-02-09  Mark Kettenis  <kettenis@gnu.org>
    	* configure.in: Check for lwpid_t, psaddr_t, prgregset_t and
    	prfpregset_t in <sys/procfs.h>.
    	* config.in: Add HAVE_LWPID_T, HAVE_PSADDR_T, HAVE_PRGREGSET_T,
    	HAVE_PRFPREGSET_T.
    	* gdb_proc_service.h: Only provide typedefs for lwpid_t, psaddr_t,
    	prgregset_t and prfpregset_t if they are not already present.

--- gdb/gdb_proc_service.h
+++ gdb/gdb_proc_service.h
@@ -12,13 +12,22 @@ typedef enum {
    */
 }       ps_err_e;
 
+#ifndef HAVE_LWPID_T
 typedef unsigned int  lwpid_t;
+#endif
+
 typedef unsigned long paddr_t;
+
+#ifndef HAVE_PSADDR_T
 typedef unsigned long psaddr_t;
+#endif
 
-
+#ifndef HAVE_PRGREGSET_T
 typedef gregset_t  prgregset_t;		/* BOGUS BOGUS BOGUS */
-typedef fpregset_t prfpregset_t;	/* BOGUS BOGUS BOGUS */
+#endif
 
+#ifndef HAVE_PRFPREGSET_T
+typedef fpregset_t prfpregset_t;	/* BOGUS BOGUS BOGUS */
+#endif
 
 struct ps_prochandle;		/* user defined. */
