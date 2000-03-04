e54a9244cd82c100032fb4443fb6e02d211f359f
Andrew Cagney <cagney@redhat.com>
2000-03-04 01:15:32 +0000

    Oops, lost update to defs.h:null_cleanup().

--- gdb/defs.h
+++ gdb/defs.h
@@ -334,7 +334,7 @@ extern void restore_my_cleanups (struct cleanup **, struct cleanup *);
 
 extern void free_current_contents (char **);
 
-extern void null_cleanup (PTR);
+extern void null_cleanup (void *);
 
 extern int myread (int, char *, int);
 
