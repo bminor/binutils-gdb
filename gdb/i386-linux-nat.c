17f9defb0b0670c472e58af60cfa96ba4c3eaafb
Jim Blandy <jimb@codesourcery.com>
2000-03-16 23:53:35 +0000

    * i386-linux-nat.c (i386_linux_saved_pc_after_call): Lost in the merge; reinstated.

--- gdb/i386-linux-nat.c
+++ gdb/i386-linux-nat.c
@@ -1289,6 +1289,17 @@ i386_linux_sigtramp_saved_sp (struct frame_info *frame)
   return read_memory_integer (addr + LINUX_SIGCONTEXT_SP_OFFSET, 4);
 }
 
+/* Immediately after a function call, return the saved pc.  */
+
+CORE_ADDR
+i386_linux_saved_pc_after_call (struct frame_info *frame)
+{
+  if (frame->signal_handler_caller)
+    return i386_linux_sigtramp_saved_pc (frame);
+
+  return read_memory_integer (read_register (SP_REGNUM), 4);
+}
+
 
 /* Register that we are able to handle Linux ELF core file formats.  */
 
