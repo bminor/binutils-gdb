9285ab8042f103833e82d26868ab6cdd1e416004
Fernando Nasser <fnasser@redhat.com>
2000-04-10 15:07:37 +0000

    2000-04-10  Fernando Nasser  <fnasser@cygnus.com>
    	From 2000-04-10 Rodney Brown <rdb@redhat.com>:
    	* command.h (enum command_class): Remove trailing "," to placate
    	AIX xlc.

--- gdb/command.h
+++ gdb/command.h
@@ -33,7 +33,7 @@ enum command_class
   no_class = -1, class_run = 0, class_vars, class_stack,
   class_files, class_support, class_info, class_breakpoint, class_trace,
   class_alias, class_obscure, class_user, class_maintenance,
-  class_pseudo, class_tui, class_xdb,
+  class_pseudo, class_tui, class_xdb
 };
 
 /* Not a set/show command.  Note that some commands which begin with
