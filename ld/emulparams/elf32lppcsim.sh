bdbe5705753d6ea9e6d19405f47a2af91231399b
Geoffrey Keating <geoffk@geoffk.org>
2000-04-14 21:18:23 +0000

    * scripttempl/elfppc.sc: Remove. * emulparams/elf32ppc.sh: Use elf.sc. * emulparams/elf32lppc.sh: Use elf.sc. * emulparams/elf32ppclinux.sh: Use elf.sc. * emulparams/elf32ppcsim.sh: New file. * emulparams/elf32lppcsim.sh: New file. * Makefile.am: Update dependencies.  Add elf32ppcsim ad elf32lppcsim. (ALL_EMULATIONS): Add elf32ppcsim ad elf32lppcsim. * Makefile.in: Regenerate. * configure.tgt (powerpc-*): Add elf32ppcsim and elf32lppcsim.

--- ld/emulparams/elf32lppcsim.sh
+++ ld/emulparams/elf32lppcsim.sh
@@ -3,8 +3,8 @@ TEMPLATE_NAME=elf32
 # elf32ppc.sh elf32ppcsim.sh elf32lppc.sh elf32lppcsim.sh elf32ppclinux.sh
 GENERATE_SHLIB_SCRIPT=yes
 SCRIPT_NAME=elf
-OUTPUT_FORMAT="elf32-powerpc"
-TEXT_START_ADDR=0x01800000
+OUTPUT_FORMAT="elf32-powerpcle"
+TEXT_START_ADDR=0x10000000
 MAXPAGESIZE=0x10000
 ARCH=powerpc
 MACHINE=
