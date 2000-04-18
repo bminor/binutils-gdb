508866be8ac72620cbd38081a1fb5106092f9c59
H.J. Lu <hjl.tools@gmail.com>
2000-04-18 17:46:31 +0000

    2000-04-18  H.J. Lu  (hjl@gnu.org)
    	* config/tc-i386.c (offset_in_range): Use addressT instead of
    	bfd_vma for none bfd assemblers.

--- gas/config/tc-i386.c
+++ gas/config/tc-i386.c
@@ -465,19 +465,19 @@ offset_in_range (val, size)
      offsetT val;
      int size;
 {
-  bfd_vma mask;
+  addressT mask;
 
   switch (size)
     {
-    case 1: mask = ((bfd_vma) 1 <<  8) - 1; break;
-    case 2: mask = ((bfd_vma) 1 << 16) - 1; break;
-    case 4: mask = ((bfd_vma) 1 << 32) - 1; break;
+    case 1: mask = ((addressT) 1 <<  8) - 1; break;
+    case 2: mask = ((addressT) 1 << 16) - 1; break;
+    case 4: mask = ((addressT) 1 << 32) - 1; break;
     default: abort();
     }
 
   /* If BFD64, sign extend val.  */
-  if ((val & ~ (((bfd_vma) 1 << 32) - 1)) == 0)
-    val = (val ^ ((bfd_vma) 1 << 31)) - ((bfd_vma) 1 << 31);
+  if ((val & ~ (((addressT) 1 << 32) - 1)) == 0)
+    val = (val ^ ((addressT) 1 << 31)) - ((addressT) 1 << 31);
 
   if ((val & ~ mask) != 0 && (val & ~ mask) != ~ mask)
     {
