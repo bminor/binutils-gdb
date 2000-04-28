1bd54964ac860e975e1e76d2398f48d01beb5feb
Andrew Cagney <cagney@redhat.com>
2000-04-28 05:50:19 +0000

    From "Serge Nikulin" <nikulin@actsw.amat.com>: Don't trip on NULL function name in sigtramp.

--- gdb/m68k-tdep.c
+++ gdb/m68k-tdep.c
@@ -91,7 +91,10 @@ delta68_in_sigtramp (pc, name)
      CORE_ADDR pc;
      char *name;
 {
-  return strcmp (name, "_sigcode") == 0;
+  if (name != NULL)
+    return strcmp (name, "_sigcode") == 0;
+  else
+    return 0;
 }
 
 CORE_ADDR
