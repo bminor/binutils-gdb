32c092c3fab0ca778242005a793cc4641c3e1b4b
H.J. Lu <hjl.tools@gmail.com>
2000-05-22 07:26:58 +0000

    2000-05-22  H.J. Lu  <hjl@gnu.org>
    	* elflink.h (elf_link_output_extsym): Clear the visibility
    	field for symbols not defined locally.

--- bfd/elflink.h
+++ bfd/elflink.h
@@ -5089,6 +5089,11 @@ elf_link_output_extsym (h, data)
       sym.st_info = ELF_ST_INFO (bindtype, ELF_ST_TYPE (sym.st_info));
     }
 
+  /* If a symbol is not defined locally, we clear the visibility
+     field. */
+  if ((h->elf_link_hash_flags & ELF_LINK_HASH_DEF_REGULAR) == 0)
+    sym.st_other ^= ELF_ST_VISIBILITY(sym.st_other);
+
   /* If this symbol should be put in the .dynsym section, then put it
      there now.  We have already know the symbol index.  We also fill
      in the entry in the .hash section.  */
