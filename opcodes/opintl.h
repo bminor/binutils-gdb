bb08852125477c6202ce0e863b49950ef33611c1
Nick Clifton <nickc@redhat.com>
2000-05-30 21:04:24 +0000

    Undo part of previous delta, so that _() calls dgettext() not gettext().

--- opcodes/opintl.h
+++ opcodes/opintl.h
@@ -12,7 +12,7 @@
 
 #ifdef ENABLE_NLS
 # include <libintl.h>
-# define _(String) gettext (String)
+# define _(String) dgettext (PACKAGE, String)
 # ifdef gettext_noop
 #  define N_(String) gettext_noop (String)
 # else
