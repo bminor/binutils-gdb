83ea41ad9c2a9cc52f656bb88eee8b65e471f979
Nick Clifton <nickc@redhat.com>
2000-07-10 18:29:39 +0000

    Add set of bfd_mach_ cases for compatibility with older binutils

--- bfd/archures.c
+++ bfd/archures.c
@@ -807,6 +807,18 @@ bfd_default_scan (info, string)
   switch (number) 
     {
       /* FIXME: These are needed to parse IEEE objects.  */
+      /* The following seven case's are here only for compatibility with
+	 older binutils (at least IEEE objects from binutils 2.9.1 require
+	 them).  */
+    case bfd_mach_m68000:
+    case bfd_mach_m68010:
+    case bfd_mach_m68020:
+    case bfd_mach_m68030:
+    case bfd_mach_m68040:
+    case bfd_mach_m68060:
+    case bfd_mach_cpu32:
+      arch = bfd_arch_m68k;
+      break;
     case 68000: 
       arch = bfd_arch_m68k;
       number = bfd_mach_m68000;
