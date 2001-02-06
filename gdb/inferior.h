a4acd088c9415fce22f2acbc5a9ca3c0dc89e874
Christopher Faylor <me+cygwin@cgf.cx>
2001-02-06 04:03:23 +0000

    Change suggested by Dean Luick <luick@cray.com> * inferior.h (step_over_calls_kind): Remove trailing comma from last enum element. (step_over_calls): Declare as extern rather than global.

--- gdb/inferior.h
+++ gdb/inferior.h
@@ -348,8 +348,10 @@ enum step_over_calls_kind
   {
     STEP_OVER_NONE,
     STEP_OVER_ALL,
-    STEP_OVER_UNDEBUGGABLE,
-  } step_over_calls;
+    STEP_OVER_UNDEBUGGABLE
+  };
+
+extern enum step_over_calls_kind step_over_calls;
 
 /* If stepping, nonzero means step count is > 1
    so don't print frame next time inferior stops
