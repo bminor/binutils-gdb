0068722893c2b04ae34b633e59ab289f31c794a9
H.J. Lu <hjl.tools@gmail.com>
2001-03-30 01:20:41 +0000

    2001-03-29  H.J. Lu  <hjl@gnu.org>
    	* gas/i386/relax.s: New test for relaxation between sections.
    	* gas/i386/relax.d: New.
    
    	* gas/i386/i386.exp: Add tests for ELF.

---
+++ gas/testsuite/gas/i386/relax.d
@@ -0,0 +1,10 @@
+#name: i386 relax
+#objdump: -drw
+
+.*: +file format .*i386.*
+
+Disassembly of section .text:
+Disassembly of section .gnu.linkonce.t.blah:
+
+0+000 <.gnu.linkonce.t.blah>:
+   0:	eb 00 [ 	]*jmp    0x2
