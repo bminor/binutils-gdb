9e0c176c3ba84547dc6c5dbe3edd0796fdb8a44f
Daniel Berlin <dberlin@dberlin.org>
2001-04-16 14:36:16 +0000

    2001-04-16  Daniel Berlin  <dan@cgsoftware.com>
    	* demangle.c (_initialize_demangler): Use xcalloc on the
    	demangling_style_names, and make it a null terminated array of
    	names, to avoid a crash.

--- gdb/demangle.c
+++ gdb/demangle.c
@@ -186,7 +186,7 @@ _initialize_demangler (void)
        libiberty_demanglers[ndems].demangling_style != unknown_demangling; 
        ndems++)
     ;
-  demangling_style_names = xmalloc (ndems * sizeof (char *));
+  demangling_style_names = xcalloc (ndems + 1, sizeof (char *));
   for (i = 0;
        libiberty_demanglers[i].demangling_style != unknown_demangling; 
        i++)
