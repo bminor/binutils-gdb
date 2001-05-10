cbc5fe5056ce888fe1b801dbe54b6da6f1921778
Hans-Peter Nilsson <hp@axis.com>
2001-05-10 02:31:01 +0000

    * gas/cris/rd-dw2-10.d: Tweak for change in gas dwarf2 line number 	handling.

--- gas/testsuite/gas/cris/rd-dw2-10.d
+++ gas/testsuite/gas/cris/rd-dw2-10.d
@@ -7,5 +7,6 @@
  Line Number Statements:
   Extended opcode 2: set Address to 0x0
   Special opcode 10: advance Address by 0 to 0x0 and Line by 5 to 6
-  Advance PC by 4 to 4
+  Special opcode 19: advance Address by 2 to 0x2 and Line by 0 to 6
+  Advance PC by 2 to 4
   Extended opcode 1: End of Sequence
