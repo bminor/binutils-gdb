3ac4495ac8e68077d3094af8e2e3733a5d16b666
Michael Snyder <msnyder@vmware.com>
2001-05-11 17:48:31 +0000

    2001-02-28  Michael Snyder  <msnyder@mvstp600e.cygnus.com>
    	* symtab.c (print_msymbol_info): Print addresses by portable method
    	longest_local_hex_string_custom.  Allow for 64-bit addresses.

--- gdb/symtab.c
+++ gdb/symtab.c
@@ -2644,9 +2644,17 @@ print_symbol_info (namespace_enum kind, struct symtab *s, struct symbol *sym,
 static void
 print_msymbol_info (struct minimal_symbol *msymbol)
 {
-  printf_filtered ("	%08lx  %s\n",
-		   (unsigned long) SYMBOL_VALUE_ADDRESS (msymbol),
-		   SYMBOL_SOURCE_NAME (msymbol));
+  char *tmp;
+
+  if (TARGET_ADDR_BIT <= 32)
+    tmp = longest_local_hex_string_custom (SYMBOL_VALUE_ADDRESS (msymbol)
+					   & (CORE_ADDR) 0xffffffff,
+					   "08l");
+  else
+    tmp = longest_local_hex_string_custom (SYMBOL_VALUE_ADDRESS (msymbol),
+					   "016l");
+  printf_filtered ("%s  %s\n",
+		   tmp, SYMBOL_SOURCE_NAME (msymbol));
 }
 
 /* This is the guts of the commands "info functions", "info types", and
