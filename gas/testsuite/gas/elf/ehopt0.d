a75a744173ba876001708bcf4311eb53ecc3b601
nobody <>
2001-05-14 22:37:32 +0000

    This commit was manufactured by cvs2svn to create branch 'binutils- 2_11-branch'.
    Cherrypick from master 2001-05-14 22:37:31 UTC Richard Henderson <rth@redhat.com> '        * gas/elf/ehopt0.s: New.':
        bfd/peXXigen.c
        bfd/po/BLD-POTFILES.in
        bfd/po/SRC-POTFILES.in
        binutils/unwind-ia64.c
        binutils/unwind-ia64.h
        gas/testsuite/gas/elf/ehopt0.d
        gas/testsuite/gas/elf/ehopt0.s
        gas/testsuite/gas/i386/intelpic.d
        gas/testsuite/gas/i386/intelpic.s
        gas/testsuite/gas/i386/relax.d
        gas/testsuite/gas/i386/relax.s
        gprof/acconfig.h
        ld/emulparams/elf32ltsmip.sh
        ld/emulparams/elf64btsmip.sh
        ld/emulparams/elf64ltsmip.sh
        libiberty/lbasename.c

---
+++ gas/testsuite/gas/elf/ehopt0.d
@@ -0,0 +1,9 @@
+#objdump: -s -j .eh_frame
+#name: elf ehopt0 
+
+.*: +file format .*
+
+Contents of section .eh_frame:
+ 0+000 10000000 00000000 017a0001 781a0004 .*
+ 0+010 01000000 12000000 18000000 00000000 .*
+ 0+020 08000000 04080000 0044 .*
