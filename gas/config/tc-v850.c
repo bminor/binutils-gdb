507c8a1a573604258f12065a7c7fc9300ce32c6e
Alan Modra <amodra@gmail.com>
2001-05-22 08:41:10 +0000

    Update copyright date.

--- gas/config/tc-v850.c
+++ gas/config/tc-v850.c
@@ -1,5 +1,6 @@
 /* tc-v850.c -- Assembler code for the NEC V850
-   Copyright 1996, 1997, 1998, 1999, 2000 Free Software Foundation, Inc.
+   Copyright 1996, 1997, 1998, 1999, 2000, 2001
+   Free Software Foundation, Inc.
 
    This file is part of GAS, the GNU Assembler.
 
