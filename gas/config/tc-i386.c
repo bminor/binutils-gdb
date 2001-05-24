b58c9faf2fef1746403a4e4734654dd8a57fad82
Alan Modra <amodra@gmail.com>
2001-05-24 23:52:22 +0000

    From 2.11 branch 2001-03-30  Richard Henderson  <rth@redhat.com> 	* config/tc-i386.c (md_convert_frag): Don't die on local symbols 	that have been finalized.

--- gas/config/tc-i386.c
+++ gas/config/tc-i386.c
@@ -4049,7 +4049,12 @@ md_convert_frag (abfd, sec, fragP)
   target_address = S_GET_VALUE (fragP->fr_symbol) + fragP->fr_offset;
 #ifdef BFD_ASSEMBLER
   /* Not needed otherwise?  */
-  target_address += symbol_get_frag (fragP->fr_symbol)->fr_address;
+  {
+    /* Local symbols which have already been resolved have a NULL frag.  */
+    fragS *sym_frag = symbol_get_frag (fragP->fr_symbol);
+    if (sym_frag)
+      target_address += sym_frag->fr_address;
+  }
 #endif
 
   /* Address opcode resides at in file space.  */
