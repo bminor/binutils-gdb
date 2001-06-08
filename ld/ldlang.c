c00a5ef04b36062b05e5b1f4a6b678940f21aea8
Alan Modra <amodra@gmail.com>
2001-06-08 02:01:12 +0000

    * ldlang.c (record_bfd_errors): Remove.
    	Merge from mainline.
    	2001-05-31  H.J. Lu  <hjl@gnu.org>
    	* ldlang.c (open_input_bfds): Don't change the bfd error
    	handler whilst loading symbols.
    
    	2001-05-02  Nick Clifton  <nickc@redhat.com>
    	* emultempl/aix.em: Replace buystring with xstrdup.
    	* emultempl/beos.em: Replace buystring with xstrdup.

--- ld/ldlang.c
+++ ld/ldlang.c
@@ -127,7 +127,6 @@ static bfd_vma size_input_section
 	   fill_type, bfd_vma, boolean));
 static void lang_finish PARAMS ((void));
 static void ignore_bfd_errors PARAMS ((const char *, ...));
-static void record_bfd_errors PARAMS ((const char *, ...));
 static void lang_check PARAMS ((void));
 static void lang_common PARAMS ((void));
 static boolean lang_one_common PARAMS ((struct bfd_link_hash_entry *, PTR));
@@ -1900,7 +1899,6 @@ open_input_bfds (s, force)
 	  if (s->input_statement.real)
 	    {
 	      lang_statement_list_type add;
-	      bfd_error_handler_type pfn;
 
 	      s->input_statement.target = current_target;
 
@@ -1917,14 +1915,7 @@ open_input_bfds (s, force)
 
 	      lang_list_init (&add);
 
-	      /* We need to know if an error occurs whilst loading the
-		 symbols, since this means that a valid executable can
-		 not be produced.  */
-	      pfn = bfd_set_error_handler (record_bfd_errors);
-
 	      load_symbols (&s->input_statement, &add);
-
-	      bfd_set_error_handler (pfn);
 
 	      if (add.head != NULL)
 		{
@@ -3455,53 +3446,6 @@ lang_finish ()
 	}
     }
 }
-
-/* This is the routine to handle BFD error messages.  */
-
-#ifdef ANSI_PROTOTYPES
-
-static void
-record_bfd_errors (const char *s, ...)
-{
-  va_list p;
-
-  einfo ("%P: ");
-
-  va_start (p, s);
-
-  vfprintf (stderr, s, p);
-
-  va_end (p);
-
-  fprintf (stderr, "\n");
-
-  einfo ("%X");
-}
-
-#else /* ! defined (ANSI_PROTOTYPES) */
-
-static void
-record_bfd_errors (va_alist)
-     va_dcl
-{
-  va_list p;
-  const char *s;
-
-  einfo ("%P: ");
-
-  va_start (p);
-
-  s = va_arg (p, const char *);
-  vfprintf (stderr, s, p);
-
-  va_end (p);
-
-  fprintf (stderr, "\n");
-
-  einfo ("%X");
-}
-
-#endif /* ! defined (ANSI_PROTOTYPES) */
 
 /* This is a small function used when we want to ignore errors from
    BFD.  */
