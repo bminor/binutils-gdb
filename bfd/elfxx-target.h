28db233b6ca087b94ca667fc6f9217527b8040a5
Nick Clifton <nickc@redhat.com>
2001-08-20 14:28:46 +0000

    Never select the generic has table creator.  All elf backends need elf specific fields in the hash table.

--- bfd/elfxx-target.h
+++ bfd/elfxx-target.h
@@ -162,16 +162,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
   _bfd_elf_canonicalize_dynamic_reloc
 #endif
 
-#ifdef elf_backend_relocate_section
 #ifndef bfd_elfNN_bfd_link_hash_table_create
 #define bfd_elfNN_bfd_link_hash_table_create _bfd_elf_link_hash_table_create
 #endif
-#else /* ! defined (elf_backend_relocate_section) */
+#ifndef elf_backend_relocate_section
 /* If no backend relocate_section routine, use the generic linker.  */
-#ifndef bfd_elfNN_bfd_link_hash_table_create
-#define bfd_elfNN_bfd_link_hash_table_create \
-  _bfd_generic_link_hash_table_create
-#endif
 #ifndef bfd_elfNN_bfd_link_add_symbols
 #define bfd_elfNN_bfd_link_add_symbols	_bfd_generic_link_add_symbols
 #endif
