7283eb8605a152788f7748a5ae9f21eafc23c62e
Thiemo Seufer <ths@networkno.de>
2001-08-27 18:48:27 +0000

    * tc_mips.c (load_address): Reflect change to MAX_GPREL_OFFSET. 	(macro): Reflect change to MAX_GPREL_OFFSET.

--- gas/config/tc-mips.c
+++ gas/config/tc-mips.c
@@ -3330,7 +3330,7 @@ load_address (counter, reg, ep)
 	   lui		$reg,<sym>		(BFD_RELOC_HI16_S)
 	   addiu	$reg,$reg,<sym>		(BFD_RELOC_LO16)
 	 If we have an addend, we always use the latter form.  */
-      if ((valueT) ep->X_add_number >= MAX_GPREL_OFFSET
+      if ((valueT) ep->X_add_number > MAX_GPREL_OFFSET
 	  || nopic_need_relax (ep->X_add_symbol, 1))
 	p = NULL;
       else
@@ -4226,7 +4226,7 @@ macro (ip)
 	       addiu	$tempreg,$tempreg,<sym>	(BFD_RELOC_LO16)
 	     If we have a constant, we need two instructions anyhow,
 	     so we may as well always use the latter form.  */
-	  if ((valueT) offset_expr.X_add_number >= MAX_GPREL_OFFSET
+	  if ((valueT) offset_expr.X_add_number > MAX_GPREL_OFFSET
 	      || nopic_need_relax (offset_expr.X_add_symbol, 1))
 	    p = NULL;
 	  else
