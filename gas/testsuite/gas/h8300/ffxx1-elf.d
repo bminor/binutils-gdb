4961b73108c1d1783815b0e5fd03b9fc0e4fe04b
Jeff Law <law@redhat.com>
2001-09-05 02:05:07 +0000

    Tue Sep  4 20:06:09 2001  Jeffrey A Law  (law@cygnus.com)
            * gas/all/gas.exp (930509a): Expect failure on the H8/300 ELF port.
    
            * gas/h8300/ffxx1-elf.d: Update due to recent changes to H8/300
            ELF toolchain.
            * gas/h8300/h8300-elf.exp: Likewise.
    
            * gas/macros/macros.exp: Expect failure on all H8/300 ports.
    
            * gas/vtable/vtable.exp: Do not run tests on H8/300 ELF ports.

--- gas/testsuite/gas/h8300/ffxx1-elf.d
+++ gas/testsuite/gas/h8300/ffxx1-elf.d
@@ -6,9 +6,8 @@
 .*:     file format .*h8300.*
 
 Disassembly of section .text:
-0+0000 <reset> 04 00             orc	#0x0,ccr
-			0: R_H8_DIR16	.text[+]0x400
 	...
+			0: R_H8_DIR16	main
 0+0400 <main> f8 7f             mov.b	#0x7f,r0l
 0+0402 <main[+](0x|)2> 28 bb             mov.b	@0xbb:8,r0l
 0+0404 <main[+](0x|)4> 6a 88 ff b9       mov.b	r0l,@0xffb9:16
@@ -16,7 +15,9 @@ Disassembly of section .text:
 0+040a <loop> 6a 88 ff bb       mov.b	r0l,@0xffbb:16
 0+040e <delay> 79 01 00 00       mov.w	#0x0,r1
 0+0412 <deloop> 0b 01             adds	#0x1,er1
-0+0414 <deloop[+](0x|)2> 46 fc             bne	.-4 \(412\)
+0+0414 <deloop[+](0x|)2> 46 00             bne	.0 \(416\)
+			415: R_H8_PCREL8	deloop
 0+0416 <deloop[+](0x|)4> 12 88             rotl	r0l
-0+0418 <deloop[+](0x|)6> 40 f0             bra	.-16 \(40a\)
+0+0418 <deloop[+](0x|)6> 40 00             bra	.0 \(41a\)
+			419: R_H8_PCREL8	loop
 	...
