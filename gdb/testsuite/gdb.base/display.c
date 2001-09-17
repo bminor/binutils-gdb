739d0d99b33332323e03c11f9fc03a5c499199c6
Corinna Vinschen <corinna@vinschen.de>
2001-09-17 20:41:22 +0000

    * gdb.base/display.c (do_loops): Add float variable `f'.         Increment f in loop.         * gdb.base/display.exp: Increment timeout by 60 seconds.         Change float display test to use variable `f'.

--- gdb/testsuite/gdb.base/display.c
+++ gdb/testsuite/gdb.base/display.c
@@ -10,11 +10,11 @@ int do_loops()
     int i=0;
     int k=0;
     int j=0;
-    
+    float f=3.1415;
     for( i = 0; i < LOOP; i++ ) {
         for( j = 0; j < LOOP; j++ ) {
             for( k = 0; k < LOOP; k++ ) {
-                sum++;
+                sum++; f++;
             }
         }
     } 
