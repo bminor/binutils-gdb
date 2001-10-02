acb325ecee0165f154751927262c2b06ba6cca68
Nick Clifton <nickc@redhat.com>
2001-10-02 09:12:11 +0000

    Detect and warna bout more 64bit DWARF debug sections

--- binutils/readelf.c
+++ binutils/readelf.c
@@ -5490,6 +5490,13 @@ display_debug_lines (section, start, file)
 
       /* Check the length of the block.  */
       info.li_length = BYTE_GET (external->li_length);
+
+      if (info.li_length == 0xffffffff)
+	{
+	  warn (_("64-bit DWARF line info is not supported yet.\n"));
+	  break;
+	}
+
       if (info.li_length + sizeof (external->li_length) > section->sh_size)
 	{
 	  warn
@@ -5717,6 +5724,12 @@ display_debug_pubnames (section, start, file)
 
       data   = start + sizeof (* external);
       start += pubnames.pn_length + sizeof (external->pn_length);
+
+      if (pubnames.pn_length == 0xffffffff)
+	{
+	  warn (_("64-bit DWARF pubnames are not supported yet.\n"));
+	  break;
+	}
 
       if (pubnames.pn_version != 2)
 	{
@@ -6818,6 +6831,12 @@ display_debug_info (section, start, file)
       compunit.cu_abbrev_offset = BYTE_GET (external->cu_abbrev_offset);
       compunit.cu_pointer_size  = BYTE_GET (external->cu_pointer_size);
 
+      if (compunit.cu_length == 0xffffffff)
+	{
+	  warn (_("64-bit DWARF debug info is not supported yet.\n"));
+	  break;
+	}
+
       tags = start + sizeof (* external);
       cu_offset = start - section_begin;
       start += compunit.cu_length + sizeof (external->cu_length);
@@ -6947,7 +6966,7 @@ display_debug_aranges (section, start, file)
 
       if (arange.ar_length == 0xffffffff)
 	{
-	  warn (_("DWARF64 aranges not currently supported.\n"));
+	  warn (_("64-bit DWARF aranges are not supported yet.\n"));
 	  break;
 	}
 
@@ -7162,6 +7181,12 @@ display_debug_frames (section, start, file)
       if (length == 0)
 	return 1;
 
+      if (length == 0xffffffff)
+	{
+	  warn (_("64-bit DWARF format frames are not supported yet.\n"));
+	  break;
+	}
+
       block_end = saved_start + length + 4;
       cie_id = byte_get (start, 4); start += 4;
 
@@ -7270,7 +7295,7 @@ display_debug_frames (section, start, file)
 
 	  look_for = is_eh ? start - 4 - cie_id : section_start + cie_id;
 
-	  for (cie=chunks; cie ; cie = cie->next)
+	  for (cie = chunks; cie ; cie = cie->next)
 	    if (cie->chunk_start == look_for)
 	      break;
 
@@ -7712,6 +7737,7 @@ debug_displays[] =
   { ".debug_frame",       display_debug_frames, NULL },
   { ".eh_frame",          display_debug_frames, NULL },
   { ".debug_macinfo",     display_debug_not_supported, NULL },
+  { ".debug_pubtypes",    display_debug_not_supported, NULL },
   { ".debug_str",         display_debug_not_supported, NULL },
   { ".debug_static_func", display_debug_not_supported, NULL },
   { ".debug_static_vars", display_debug_not_supported, NULL },
