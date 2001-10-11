685bd8696659b13e21af82e8d720f650ca0db2b3
Nick Clifton <nickc@redhat.com>
2001-10-11 07:17:47 +0000

    Use ISSPACE instead of isspace

--- gas/input-file.c
+++ gas/input-file.c
@@ -26,9 +26,9 @@
 
 #include <stdio.h>
 #include <string.h>
-
 #include "as.h"
 #include "input-file.h"
+#include "safe-ctype.h"
 
 static int input_file_get PARAMS ((char *, int));
 
@@ -159,7 +159,7 @@ input_file_open (filename, pre)
       if (c == 'N')
         {
           fgets (buf, 80, f_in);
-          if (!strncmp (buf, "O_APP", 5) && isspace (buf[5]))
+          if (!strncmp (buf, "O_APP", 5) && ISSPACE (buf[5]))
             preprocess = 0;
           if (!strchr (buf, '\n'))
             ungetc ('#', f_in);	/* It was longer.  */
@@ -169,7 +169,7 @@ input_file_open (filename, pre)
       else if (c == 'A')
         {
           fgets (buf, 80, f_in);
-          if (!strncmp (buf, "PP", 2) && isspace (buf[2]))
+          if (!strncmp (buf, "PP", 2) && ISSPACE (buf[2]))
             preprocess = 1;
           if (!strchr (buf, '\n'))
             ungetc ('#', f_in);
