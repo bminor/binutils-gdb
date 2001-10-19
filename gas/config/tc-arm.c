da89cce1af92fe49e4c85eeb478aa8ab6d8d41ce
Nick Clifton <nickc@redhat.com>
2001-10-19 10:53:19 +0000

    Restore line_comment_chars after a SNAFU.

--- gas/config/tc-arm.c
+++ gas/config/tc-arm.c
@@ -128,7 +128,7 @@ const char comment_chars[] = "@";
 /* Also note that comments like this one will always work.  */
 const char line_comment_chars[] = "#";
 
-const char line_separator_chars[] = "|";
+const char line_separator_chars[] = ";";
 
 /* Chars that can be used to separate mant
    from exp in floating point numbers.  */
