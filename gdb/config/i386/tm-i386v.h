dec66fe5f0c12e8321a77f63b87d046b03919e78
Mark Kettenis <kettenis@gnu.org>
2001-10-29 23:54:56 +0000

    * config/i386/tm-i386v.h: Don't include "regcache.h". (EXTRACT_RETURN_VALUE, STORE_RETURN_VALUE, FRAME_CHAIN, FRAMELESS_FUNCTION_INVOCATION, FRAME_SAVED_PC, FRAME_NUM_ARGS): Remove macros.  The versions from "tm-i386.h" should work fine (and in most cases even better).

--- gdb/config/i386/tm-i386v.h
+++ gdb/config/i386/tm-i386v.h
@@ -24,7 +24,6 @@
 
 /* First pick up the generic *86 target file. */
 
-#include "regcache.h"
 #include "i386/tm-i386.h"
 
 /* Number of traps that happen between exec'ing the shell to run an
@@ -33,52 +32,5 @@
 
 #undef  START_INFERIOR_TRAPS_EXPECTED
 #define START_INFERIOR_TRAPS_EXPECTED 4
-
-/* Extract from an array REGBUF containing the (raw) register state
-   a function return value of type TYPE, and copy that, in virtual format,
-   into VALBUF.  */
-
-#undef  EXTRACT_RETURN_VALUE
-#define EXTRACT_RETURN_VALUE(TYPE,REGBUF,VALBUF) \
-  memcpy ((VALBUF), (REGBUF), TYPE_LENGTH (TYPE))
-
-/* Write into appropriate registers a function return value
-   of type TYPE, given in virtual format.  */
-
-#undef  STORE_RETURN_VALUE
-#define STORE_RETURN_VALUE(TYPE,VALBUF) \
-  write_register_bytes (0, VALBUF, TYPE_LENGTH (TYPE))
-
-
-/* Describe the pointer in each stack frame to the previous stack frame
-   (its caller).  */
-
-/* FRAME_CHAIN takes a frame's nominal address
-   and produces the frame's chain-pointer. */
-
-#undef  FRAME_CHAIN
-#define FRAME_CHAIN(thisframe) \
-  (!inside_entry_file ((thisframe)->pc) ? \
-   read_memory_integer ((thisframe)->frame, 4) :\
-   0)
-
-/* Define other aspects of the stack frame.  */
-
-/* A macro that tells us whether the function invocation represented
-   by FI does not have a frame on the stack associated with it.  If it
-   does not, FRAMELESS is set to 1, else 0.  */
-
-#undef  FRAMELESS_FUNCTION_INVOCATION
-#define FRAMELESS_FUNCTION_INVOCATION(FI) \
-  (frameless_look_for_prologue (FI))
-
-#undef  FRAME_SAVED_PC
-#define FRAME_SAVED_PC(FRAME) (read_memory_integer ((FRAME)->frame + 4, 4))
-
-/* Return number of args passed to a frame.
-   Can return -1, meaning no way to tell.  */
-
-#undef  FRAME_NUM_ARGS
-#define FRAME_NUM_ARGS(fi) (-1)
 
 #endif /* ifndef TM_I386V_H */
