4734a3efc850aff5745b175ca27724c2c6bb7fbe
Michael Chastain <mec@google.com>
2002-01-20 22:09:04 +0000

    2002-01-20  Michael Chastain  <mec@shout.net>
    	* top.c (print_gdb_version): Bump copyright year to 2002.

--- gdb/top.c
+++ gdb/top.c
@@ -1232,7 +1232,7 @@ print_gdb_version (struct ui_file *stream)
 
   /* Second line is a copyright notice. */
 
-  fprintf_filtered (stream, "Copyright 2001 Free Software Foundation, Inc.\n");
+  fprintf_filtered (stream, "Copyright 2002 Free Software Foundation, Inc.\n");
 
   /* Following the copyright is a brief statement that the program is
      free software, that users are free to copy and change it on
