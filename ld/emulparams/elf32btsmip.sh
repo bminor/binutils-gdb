493a6d47554d0576bf176f01aea9db415305d259
H.J. Lu <hjl.tools@gmail.com>
2002-01-21 17:19:09 +0000

    2002-01-21  H.J. Lu <hjl@gnu.org>
    	* emulparams/elf32btsmip.sh (SHLIB_TEXT_START_ADDR): Change to
    	0.
    	* emulparams/elf64btsmip.sh (SHLIB_TEXT_START_ADDR): Likewise.

--- ld/emulparams/elf32btsmip.sh
+++ ld/emulparams/elf32btsmip.sh
@@ -5,5 +5,5 @@
 OUTPUT_FORMAT="elf32-tradbigmips"
 BIG_OUTPUT_FORMAT="elf32-tradbigmips"
 LITTLE_OUTPUT_FORMAT="elf32-tradlittlemips"
-SHLIB_TEXT_START_ADDR=0x0
+SHLIB_TEXT_START_ADDR=0
 ENTRY=__start
