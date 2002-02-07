e108f470528369ba8b8d4a2899f0a740d034f33e
Daniel Jacobowitz <drow@false.org>
2002-02-07 22:28:31 +0000

    2002-02-07  Daniel Jacobowitz  <drow@mvista.com>
            * bfd-in.h: Update <stdbool.h> check to only see if <stdbool.h> has
            been included, not drag it in.
            * bfd-in2.h: Regenerate.

--- bfd/bfd-in.h
+++ bfd/bfd-in.h
@@ -89,12 +89,13 @@ typedef struct _bfd bfd;
 /* It gets worse if the host also defines a true/false enum... -sts */
 /* And even worse if your compiler has built-in boolean types... -law */
 /* And even worse if your compiler provides a stdbool.h that conflicts
-   with these definitions... gcc 2.95 and later do.  -drow */
+   with these definitions... gcc 2.95 and later do.  If so, it must
+   be included first.  -drow */
 #if defined (__GNUG__) && (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 6))
 #define TRUE_FALSE_ALREADY_DEFINED
 #else
-#if (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 95))
-#include <stdbool.h>
+#if defined (__bool_true_false_are_defined)
+/* We have <stdbool.h>.  */
 #define TRUE_FALSE_ALREADY_DEFINED
 #endif
 #endif
