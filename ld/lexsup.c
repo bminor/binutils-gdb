4e5bae56d47dbb1b1428a536ab61413afc544687
Daniel Jacobowitz <drow@false.org>
2002-02-10 19:12:13 +0000

    2002-02-10  Daniel Jacobowitz  <drow@mvista.com>
    	* ldmain.c: Add prototype for main ().
    	* lexsup.c: Guard declaration of strtoul with HAVE_STDLIB_H.
    	* emultempl/lnk960.em (lnk960_choose_target): Function should
    	take two arguments.

--- ld/lexsup.c
+++ ld/lexsup.c
@@ -1,6 +1,6 @@
 /* Parse options for the GNU linker.
    Copyright 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
-   2001
+   2001, 2002
    Free Software Foundation, Inc.
 
 This file is part of GLD, the Gnu Linker.
@@ -53,9 +53,12 @@ Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 #define	S_ISDIR(m) (((m) & S_IFMT) == S_IFDIR)
 #endif
 
+#ifndef HAVE_STDLIB_H
+/* If we have <stdlib.h>, assume it defines strtoul.  */
 /* Omit args to avoid the possibility of clashing with a system header
    that might disagree about consts.  */
 unsigned long strtoul ();
+#endif
 
 static int is_num PARAMS ((const char *, int, int, int));
 static void set_default_dirlist PARAMS ((char *dirlist_ptr));
