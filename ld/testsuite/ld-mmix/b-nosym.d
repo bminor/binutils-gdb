c61ac026cf2b9e5168feefea7c42d4a220c569d0
Hans-Peter Nilsson <hp@axis.com>
2002-02-17 22:58:40 +0000

    * ld-mmix/b-nosym.d, ld-mmix/sec-6.d: Tweak for change in symbol 	output. 	* ld-mmix/sec-7m.d, ld-mmix/sec-7a.s, ld-mmix/sec-7b.s, 	ld-mmix/sec-7c.s, ld-mmix/sec-7d.s, ld-mmix/sec-7e.s: New test.

--- ld/testsuite/ld-mmix/b-nosym.d
+++ ld/testsuite/ld-mmix/b-nosym.d
@@ -3,12 +3,13 @@
 #source: b-nosym.s
 #ld: --oformat binary
 #objcopy_linked_file:
-#objdump: -st
+#objdump: -st 2>/dev/null
+
+# Note that we have to redirect stderr when objdumping to get rid of the
+# "no symbols" message that would otherwise cause a spurious failure and
+# which we seemingly can't identify or prune in another way.
 
 .*:     file format mmo
-
-SYMBOL TABLE:
-0+4 g       \.text Main
 
 Contents of section \.text:
  0000 e3fd0001 e3fd0004                    .*
