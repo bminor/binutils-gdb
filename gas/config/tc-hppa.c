c7d004c5d30adf8a6d872d271759f8d12785fedc
Alan Modra <amodra@gmail.com>
2002-02-25 05:10:11 +0000

    * config/tc-hppa.c: Update copyright date.

--- gas/config/tc-hppa.c
+++ gas/config/tc-hppa.c
@@ -1,6 +1,6 @@
 /* tc-hppa.c -- Assemble for the PA
-   Copyright 1989, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001
-   Free Software Foundation, Inc.
+   Copyright 1989, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001,
+   2002 Free Software Foundation, Inc.
 
    This file is part of GAS, the GNU Assembler.
 
