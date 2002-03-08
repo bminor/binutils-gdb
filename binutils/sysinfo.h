41cae810df241a8b168c462eed8bfbe6d6c6aaec
Daniel Jacobowitz <drow@false.org>
2002-03-08 17:01:34 +0000

    Add generated files on 2.12 release branch.

---
+++ binutils/sysinfo.h
@@ -0,0 +1,21 @@
+#ifndef BISON_Y_TAB_H
+# define BISON_Y_TAB_H
+
+#ifndef YYSTYPE
+typedef union {
+ int i;
+ char *s;
+} yystype;
+# define YYSTYPE yystype
+#endif
+# define	COND	257
+# define	REPEAT	258
+# define	TYPE	259
+# define	NAME	260
+# define	NUMBER	261
+# define	UNIT	262
+
+
+extern YYSTYPE yylval;
+
+#endif /* not BISON_Y_TAB_H */
