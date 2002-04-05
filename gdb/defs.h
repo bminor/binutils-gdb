08b204d1d1d31ee81ef6868c42a88cd1056bd23a
Joel Brobecker <brobecker@gnat.com>
2002-04-05 16:39:36 +0000

    (xfullpath): Add declaration.

--- gdb/defs.h
+++ gdb/defs.h
@@ -372,6 +372,7 @@ extern CORE_ADDR host_pointer_to_address (void *ptr);
 extern void *address_to_host_pointer (CORE_ADDR addr);
 
 extern char *gdb_realpath (const char *);
+extern char *xfullpath (const char *);
 
 /* From demangle.c */
 
