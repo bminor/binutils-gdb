0e2bd21956791f9a638251d3364c8fc4e4bdeb85
Richard Earnshaw <richard.earnshaw@arm.com>
2002-05-08 15:59:52 +0000

    * remote-rdi.c (_initializie_remote_rdi): Use ANSI-style string concatenation for command help messages.

--- gdb/remote-rdi.c
+++ gdb/remote-rdi.c
@@ -1023,39 +1023,41 @@ _initialize_remote_rdi (void)
 
   c = add_cmd ("rdilogfile", class_maintenance,
 	       rdilogfile_command,
-	       "Set filename for ADP packet log.\n\
-This file is used to log Angel Debugger Protocol packets.\n\
-With a single argument, sets the logfile name to that value.\n\
-Without an argument, shows the current logfile name.\n\
-See also: rdilogenable\n",
-	   &maintenancelist);
+	       "Set filename for ADP packet log.\n"
+	       "This file is used to log Angel Debugger Protocol packets.\n"
+	       "With a single argument, sets the logfile name to that value.\n"
+	       "Without an argument, shows the current logfile name.\n"
+	       "See also: rdilogenable\n",
+	       &maintenancelist);
   set_cmd_completer (c, filename_completer);
 
   add_cmd ("rdilogenable", class_maintenance,
 	   rdilogenable_command,
-	   "Set enable logging of ADP packets.\n\
-This will log ADP packets exchanged between gdb and the\n\
-rdi target device.\n\
-An argument of 1,t,true,y,yes will enable.\n\
-An argument of 0,f,false,n,no will disabled.\n\
-Withough an argument, it will display current state.\n",
+	   "Set enable logging of ADP packets.\n"
+	   "This will log ADP packets exchanged between gdb and the\n"
+	   "rdi target device.\n"
+	   "An argument of 1, t, true, y or yes will enable.\n"
+	   "An argument of 0, f, false, n or no will disabled.\n"
+	   "Withough an argument, it will display current state.\n",
 	   &maintenancelist);
 
   add_show_from_set
-    (add_set_boolean_cmd ("rdiromatzero", no_class, &rom_at_zero,
-			  "Set target has ROM at addr 0.\n\
-A true value disables vector catching, false enables vector catching.\n\
-This is evaluated at the time the 'target rdi' command is executed\n",
-			  &setlist),
+    (add_set_boolean_cmd
+     ("rdiromatzero", no_class, &rom_at_zero,
+      "Set target has ROM at addr 0.\n"
+      "A true value disables vector catching, false enables vector catching.\n"
+      "This is evaluated at the time the 'target rdi' command is executed\n",
+      &setlist),
      &showlist);
 
   add_show_from_set
-    (add_set_boolean_cmd ("rdiheartbeat", no_class, &rdi_heartbeat,
-			  "Set enable for ADP heartbeat packets.\n\
-I don't know why you would want this. If you enable them,\n\
-it will confuse ARM and EPI JTAG interface boxes as well\n\
-as the Angel Monitor.\n",
-			  &setlist),
+    (add_set_boolean_cmd
+     ("rdiheartbeat", no_class, &rdi_heartbeat,
+      "Set enable for ADP heartbeat packets.\n"
+      "I don't know why you would want this. If you enable them,\n"
+      "it will confuse ARM and EPI JTAG interface boxes as well\n"
+      "as the Angel Monitor.\n",
+      &setlist),
      &showlist);
 }
 
