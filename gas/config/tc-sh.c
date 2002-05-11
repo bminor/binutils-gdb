9980a7698364829b9751634f7d531d4eacbebd27
Daniel Jacobowitz <drow@false.org>
2002-05-11 16:49:58 +0000

    2002-05-11  Daniel Jacobowitz  <drow@mvista.com>
            Merge from mainline:
            2002-05-11  Nick Clifton  <nickc@cambridge.redhat.com>
            * config/obj-coff.c: Fix compile time warnings when compiling
            without BFD_ASSEMBLER defined.
            Fix formatting.
    
            * config/tc-sh.c (md_pcrel_from): Define for use with sh-hms
            target.
            (md_pcrel_from_section): Use md_pcrel_from().

--- gas/config/tc-sh.c
+++ gas/config/tc-sh.c
@@ -2453,14 +2453,14 @@ md_undefined_symbol (name)
 
 void
 tc_crawl_symbol_chain (headers)
-     object_headers *headers;
+     object_headers *headers ATTRIBUTE_UNUSED;
 {
   printf (_("call to tc_crawl_symbol_chain \n"));
 }
 
 void
 tc_headers_hook (headers)
-     object_headers *headers;
+     object_headers *headers ATTRIBUTE_UNUSED;
 {
   printf (_("call to tc_headers_hook \n"));
 }
@@ -2903,7 +2903,7 @@ md_convert_frag (headers, seg, fragP)
 #ifdef BFD_ASSEMBLER
      bfd *headers ATTRIBUTE_UNUSED;
 #else
-     object_headers *headers;
+     object_headers *headers ATTRIBUTE_UNUSED;
 #endif
      segT seg;
      fragS *fragP;
@@ -3659,6 +3659,16 @@ md_number_to_chars (ptr, use, nbytes)
     number_to_chars_bigendian (ptr, use, nbytes);
 }
 
+/* This version is used in obj-coff.c when not using BFD_ASSEMBLER.
+   eg for the sh-hms target.  */
+
+long
+md_pcrel_from (fixP)
+     fixS *fixP;
+{
+  return fixP->fx_size + fixP->fx_where + fixP->fx_frag->fr_address + 2;
+}
+
 long
 md_pcrel_from_section (fixP, sec)
      fixS *fixP;
@@ -3677,7 +3687,7 @@ md_pcrel_from_section (fixP, sec)
       return fixP->fx_subsy ? fixP->fx_where + fixP->fx_frag->fr_address : 0;
     }
 
-  return fixP->fx_size + fixP->fx_where + fixP->fx_frag->fr_address + 2;
+  return md_pcrel_from (fixP);
 }
 
 #ifdef OBJ_COFF
