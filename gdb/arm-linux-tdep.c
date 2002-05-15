a6cdd8c5f6133c63df3f90cd56f0287433e4fe66
Richard Earnshaw <richard.earnshaw@arm.com>
2002-05-15 12:29:41 +0000

    * arm-linux-tdep.c (ARM_LINUX_JB_PC): Renamed from JB_PC. (ARM_LINUX_JB_ELEMENT_SIZE): Likewise.

--- gdb/arm-linux-tdep.c
+++ gdb/arm-linux-tdep.c
@@ -60,8 +60,8 @@ LONGEST arm_linux_call_dummy_words[] =
 };
 
 /* Description of the longjmp buffer.  */
-#define JB_ELEMENT_SIZE		INT_REGISTER_RAW_SIZE
-#define JB_PC			21
+#define ARM_LINUX_JB_ELEMENT_SIZE	INT_REGISTER_RAW_SIZE
+#define ARM_LINUX_JB_PC			21
 
 /* Extract from an array REGBUF containing the (raw) register state
    a function return value of type TYPE, and copy that, in virtual format,
@@ -524,8 +524,8 @@ arm_linux_init_abi (struct gdbarch_info info,
   tdep->arm_breakpoint = arm_linux_arm_le_breakpoint;
   tdep->arm_breakpoint_size = sizeof (arm_linux_arm_le_breakpoint);
 
-  tdep->jb_pc = JB_PC;
-  tdep->jb_elt_size = JB_ELEMENT_SIZE;
+  tdep->jb_pc = ARM_LINUX_JB_PC;
+  tdep->jb_elt_size = ARM_LINUX_JB_ELEMENT_SIZE;
 
   set_gdbarch_call_dummy_words (gdbarch, arm_linux_call_dummy_words);
   set_gdbarch_sizeof_call_dummy_words (gdbarch,
