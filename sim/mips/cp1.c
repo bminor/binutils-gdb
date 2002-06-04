d3eb724f8198916e9fdd3805811f467167149f62
Chris Demetriou <cgd@google.com>
2002-06-04 16:35:24 +0000

    2002-06-04  Chris Demetriou  <cgd@broadcom.com>
            * cp1.c: Add an FSF Copyright notice to this file.

--- sim/mips/cp1.c
+++ sim/mips/cp1.c
@@ -1,9 +1,32 @@
 /*> cp1.c <*/
+/* MIPS Simulator FPU (CoProcessor 1) support.
+   Copyright (C) 2002 Free Software Foundation, Inc.
+   Originally created by Cygnus Solutions, modified substially
+   by Broadcom Corporation (SiByte).
+
+This file is part of GDB, the GNU debugger.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* XXX: The following notice should be removed as soon as is practical:  */
 /* Floating Point Support for gdb MIPS simulators
 
    This file is part of the MIPS sim
 
 		THIS SOFTWARE IS NOT COPYRIGHTED
+   (by Cygnus.)
 
    Cygnus offers the following for use in the public domain.  Cygnus
    makes no warranty with regard to the software or it's performance
