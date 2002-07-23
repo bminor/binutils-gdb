63eb08093375052a60f37b2808db1ef8aea0f558
Alan Modra <amodra@gmail.com>
2002-07-23 05:12:26 +0000

    * budemang.c: Include config.h and string.h/strings.h.

--- binutils/budemang.c
+++ binutils/budemang.c
@@ -18,8 +18,15 @@
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
    02111-1307, USA.  */
 
+#include "config.h"
 #include <stdlib.h>
+#ifdef HAVE_STRING_H
 #include <string.h>
+#else
+#ifdef HAVE_STRINGS_H
+#include <strings.h>
+#endif
+#endif
 #include "bfd.h"
 #include "libiberty.h"
 #include "demangle.h"
