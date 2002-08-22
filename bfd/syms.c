0ab72ee2fdc5c3cd33575572d99c7199cfb5ff03
Nick Clifton <nickc@redhat.com>
2002-08-22 16:12:00 +0000

    Set bfd_error to no_symbols if the symbols could not be read.

--- bfd/syms.c
+++ bfd/syms.c
@@ -806,6 +806,7 @@ _bfd_generic_read_minisymbols (abfd, dynamic, minisymsp, sizep)
   return symcount;
 
  error_return:
+  bfd_set_error (bfd_error_no_symbols);
   if (syms != NULL)
     free (syms);
   return -1;
