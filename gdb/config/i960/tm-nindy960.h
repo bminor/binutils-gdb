4d210288d335827b47aac453c89f4b9bc9f2847f
Andrew Cagney <cagney@redhat.com>
2002-08-22 21:52:45 +0000

    Obsolete i960.

--- gdb/config/i960/tm-nindy960.h
+++ gdb/config/i960/tm-nindy960.h
@@ -1,106 +1,106 @@
-/* Parameters for Intel 960 running NINDY monitor, for GDB, the GNU debugger.
-   Copyright 1990, 1991, 1996, 1999, 2000 Free Software Foundation, Inc.
-   Contributed by Intel Corporation and Cygnus Support.
-
-   This file is part of GDB.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
-
-/*****************************************************************************
- * Definitions to target GDB to an i960 debugged over a serial line.
- ******************************************************************************/
-
-#include "i960/tm-i960.h"
-
-/* forward declarations */
-struct frame_info;
-
-/* Override the standard gdb prompt when compiled for this target.  */
-
-#define	DEFAULT_PROMPT	"(gdb960) "
-
-/* Additional command line options accepted by nindy gdb's, for handling
-   the remote-nindy.c interface.  These should really be target-specific
-   rather than architecture-specific.  */
-
-extern int nindy_old_protocol;	/* nonzero if old NINDY serial protocol */
-extern int nindy_initial_brk;	/* Send a BREAK to reset board first */
-extern char *nindy_ttyname;	/* Name of serial port to talk to nindy */
-
-#define	ADDITIONAL_OPTIONS \
-	{"O", no_argument, &nindy_old_protocol, 1},	\
-	{"brk", no_argument, &nindy_initial_brk, 1},	\
-	{"ser", required_argument, 0, 1004},	/* 1004 is magic cookie for ADDL_CASES */
-
-#define	ADDITIONAL_OPTION_CASES	\
-	case 1004:	/* -ser option:  remote nindy auto-start */	\
-	  nindy_ttyname = optarg;	\
-	  break;
-
-#define	ADDITIONAL_OPTION_HELP \
-	"\
-  -O                Use old protocol to talk to a Nindy target\n\
-  -brk              Send a break to a Nindy target to reset it.\n\
-  -ser SERIAL       Open remote Nindy session to SERIAL port.\n\
-"
-
-/* If specified on the command line, open tty for talking to nindy,
-   and download the executable file if one was specified.  */
-
-extern void nindy_open (char *name, int from_tty);
-#define	ADDITIONAL_OPTION_HANDLER					\
-	if (nindy_ttyname != NULL)					\
-          {								\
-            if (catch_command_errors (nindy_open, nindy_ttyname,	\
-				      !batch, RETURN_MASK_ALL))		\
-	      {								\
-                if (execarg != NULL)					\
-                  catch_command_errors (target_load, execarg, !batch,	\
-					RETURN_MASK_ALL);		\
-	      }								\
-	  }
-
-/* If configured for i960 target, we take control before main loop
-   and demand that we configure for a nindy target.  */
-
-#define	BEFORE_MAIN_LOOP_HOOK	\
-  nindy_before_main_loop();
-
-extern void
-  nindy_before_main_loop ();	/* In remote-nindy.c */
-
-/* FRAME_CHAIN_VALID returns zero if the given frame is the outermost one
-   and has no caller.
-
-   On the i960, each various target system type defines FRAME_CHAIN_VALID,
-   since it differs between NINDY and VxWorks, the two currently supported
-   targets types.  */
-
-extern int nindy_frame_chain_valid (CORE_ADDR, struct frame_info *);
-#define	FRAME_CHAIN_VALID(chain, thisframe) nindy_frame_chain_valid (chain, thisframe)
-
-extern int
-  nindy_frame_chain_valid ();	/* See nindy-tdep.c */
-
-/* Sequence of bytes for breakpoint instruction */
-
-#define BREAKPOINT {0x00, 0x3e, 0x00, 0x66}
-
-/* Amount ip must be decremented by after a breakpoint.
- * This is often the number of bytes in BREAKPOINT but not always.
- */
-
-#define DECR_PC_AFTER_BREAK 0
+// OBSOLETE /* Parameters for Intel 960 running NINDY monitor, for GDB, the GNU debugger.
+// OBSOLETE    Copyright 1990, 1991, 1996, 1999, 2000 Free Software Foundation, Inc.
+// OBSOLETE    Contributed by Intel Corporation and Cygnus Support.
+// OBSOLETE 
+// OBSOLETE    This file is part of GDB.
+// OBSOLETE 
+// OBSOLETE    This program is free software; you can redistribute it and/or modify
+// OBSOLETE    it under the terms of the GNU General Public License as published by
+// OBSOLETE    the Free Software Foundation; either version 2 of the License, or
+// OBSOLETE    (at your option) any later version.
+// OBSOLETE 
+// OBSOLETE    This program is distributed in the hope that it will be useful,
+// OBSOLETE    but WITHOUT ANY WARRANTY; without even the implied warranty of
+// OBSOLETE    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// OBSOLETE    GNU General Public License for more details.
+// OBSOLETE 
+// OBSOLETE    You should have received a copy of the GNU General Public License
+// OBSOLETE    along with this program; if not, write to the Free Software
+// OBSOLETE    Foundation, Inc., 59 Temple Place - Suite 330,
+// OBSOLETE    Boston, MA 02111-1307, USA.  */
+// OBSOLETE 
+// OBSOLETE /*****************************************************************************
+// OBSOLETE  * Definitions to target GDB to an i960 debugged over a serial line.
+// OBSOLETE  ******************************************************************************/
+// OBSOLETE 
+// OBSOLETE #include "i960/tm-i960.h"
+// OBSOLETE 
+// OBSOLETE /* forward declarations */
+// OBSOLETE struct frame_info;
+// OBSOLETE 
+// OBSOLETE /* Override the standard gdb prompt when compiled for this target.  */
+// OBSOLETE 
+// OBSOLETE #define	DEFAULT_PROMPT	"(gdb960) "
+// OBSOLETE 
+// OBSOLETE /* Additional command line options accepted by nindy gdb's, for handling
+// OBSOLETE    the remote-nindy.c interface.  These should really be target-specific
+// OBSOLETE    rather than architecture-specific.  */
+// OBSOLETE 
+// OBSOLETE extern int nindy_old_protocol;	/* nonzero if old NINDY serial protocol */
+// OBSOLETE extern int nindy_initial_brk;	/* Send a BREAK to reset board first */
+// OBSOLETE extern char *nindy_ttyname;	/* Name of serial port to talk to nindy */
+// OBSOLETE 
+// OBSOLETE #define	ADDITIONAL_OPTIONS \
+// OBSOLETE 	{"O", no_argument, &nindy_old_protocol, 1},	\
+// OBSOLETE 	{"brk", no_argument, &nindy_initial_brk, 1},	\
+// OBSOLETE 	{"ser", required_argument, 0, 1004},	/* 1004 is magic cookie for ADDL_CASES */
+// OBSOLETE 
+// OBSOLETE #define	ADDITIONAL_OPTION_CASES	\
+// OBSOLETE 	case 1004:	/* -ser option:  remote nindy auto-start */	\
+// OBSOLETE 	  nindy_ttyname = optarg;	\
+// OBSOLETE 	  break;
+// OBSOLETE 
+// OBSOLETE #define	ADDITIONAL_OPTION_HELP \
+// OBSOLETE 	"\
+// OBSOLETE   -O                Use old protocol to talk to a Nindy target\n\
+// OBSOLETE   -brk              Send a break to a Nindy target to reset it.\n\
+// OBSOLETE   -ser SERIAL       Open remote Nindy session to SERIAL port.\n\
+// OBSOLETE "
+// OBSOLETE 
+// OBSOLETE /* If specified on the command line, open tty for talking to nindy,
+// OBSOLETE    and download the executable file if one was specified.  */
+// OBSOLETE 
+// OBSOLETE extern void nindy_open (char *name, int from_tty);
+// OBSOLETE #define	ADDITIONAL_OPTION_HANDLER					\
+// OBSOLETE 	if (nindy_ttyname != NULL)					\
+// OBSOLETE           {								\
+// OBSOLETE             if (catch_command_errors (nindy_open, nindy_ttyname,	\
+// OBSOLETE 				      !batch, RETURN_MASK_ALL))		\
+// OBSOLETE 	      {								\
+// OBSOLETE                 if (execarg != NULL)					\
+// OBSOLETE                   catch_command_errors (target_load, execarg, !batch,	\
+// OBSOLETE 					RETURN_MASK_ALL);		\
+// OBSOLETE 	      }								\
+// OBSOLETE 	  }
+// OBSOLETE 
+// OBSOLETE /* If configured for i960 target, we take control before main loop
+// OBSOLETE    and demand that we configure for a nindy target.  */
+// OBSOLETE 
+// OBSOLETE #define	BEFORE_MAIN_LOOP_HOOK	\
+// OBSOLETE   nindy_before_main_loop();
+// OBSOLETE 
+// OBSOLETE extern void
+// OBSOLETE   nindy_before_main_loop ();	/* In remote-nindy.c */
+// OBSOLETE 
+// OBSOLETE /* FRAME_CHAIN_VALID returns zero if the given frame is the outermost one
+// OBSOLETE    and has no caller.
+// OBSOLETE 
+// OBSOLETE    On the i960, each various target system type defines FRAME_CHAIN_VALID,
+// OBSOLETE    since it differs between NINDY and VxWorks, the two currently supported
+// OBSOLETE    targets types.  */
+// OBSOLETE 
+// OBSOLETE extern int nindy_frame_chain_valid (CORE_ADDR, struct frame_info *);
+// OBSOLETE #define	FRAME_CHAIN_VALID(chain, thisframe) nindy_frame_chain_valid (chain, thisframe)
+// OBSOLETE 
+// OBSOLETE extern int
+// OBSOLETE   nindy_frame_chain_valid ();	/* See nindy-tdep.c */
+// OBSOLETE 
+// OBSOLETE /* Sequence of bytes for breakpoint instruction */
+// OBSOLETE 
+// OBSOLETE #define BREAKPOINT {0x00, 0x3e, 0x00, 0x66}
+// OBSOLETE 
+// OBSOLETE /* Amount ip must be decremented by after a breakpoint.
+// OBSOLETE  * This is often the number of bytes in BREAKPOINT but not always.
+// OBSOLETE  */
+// OBSOLETE 
+// OBSOLETE #define DECR_PC_AFTER_BREAK 0
--- gdb/remote-vx960.c
+++ gdb/remote-vx960.c
@@ -1,160 +1,160 @@
-/* i80960-dependent portions of the RPC protocol
-   used with a VxWorks target 
-
-   Contributed by Wind River Systems.
-
-   This file is part of GDB.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
-
-#include <stdio.h>
-#include "defs.h"
-
-#include "vx-share/regPacket.h"
-#include "frame.h"
-#include "inferior.h"
-#include "target.h"
-#include "gdbcore.h"
-#include "command.h"
-#include "symtab.h"
-#include "symfile.h"		/* for struct complaint */
-#include "regcache.h"
-
-#include "gdb_string.h"
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/socket.h>
-
-#ifdef _AIX			/* IBM claims "void *malloc()" not char * */
-#define malloc bogon_malloc
-#endif
-
-#include <rpc/rpc.h>
-#include <sys/time.h>		/* UTek's <rpc/rpc.h> doesn't #incl this */
-#include <netdb.h>
-#include "vx-share/ptrace.h"
-#include "vx-share/xdr_ptrace.h"
-#include "vx-share/xdr_ld.h"
-#include "vx-share/xdr_rdb.h"
-#include "vx-share/dbgRpcLib.h"
-
-/* get rid of value.h if possible */
-#include <value.h>
-#include <symtab.h>
-
-/* Flag set if target has fpu */
-
-extern int target_has_fp;
-
-/* 960 floating point format descriptor, from "i960-tdep.c."  */
-
-extern struct ext_format ext_format_i960;
-
-/* Generic register read/write routines in remote-vx.c.  */
-
-extern void net_read_registers ();
-extern void net_write_registers ();
-
-/* Read a register or registers from the VxWorks target.
-   REGNO is the register to read, or -1 for all; currently,
-   it is ignored.  FIXME look at regno to improve efficiency.  */
-
-void
-vx_read_register (int regno)
-{
-  char i960_greg_packet[I960_GREG_PLEN];
-  char i960_fpreg_packet[I960_FPREG_PLEN];
-
-  /* Get general-purpose registers.  When copying values into
-     registers [], don't assume that a location in registers []
-     is properly aligned for the target data type.  */
-
-  net_read_registers (i960_greg_packet, I960_GREG_PLEN, PTRACE_GETREGS);
-
-  bcopy (&i960_greg_packet[I960_R_R0],
-	 &registers[REGISTER_BYTE (R0_REGNUM)], 16 * I960_GREG_SIZE);
-  bcopy (&i960_greg_packet[I960_R_G0],
-	 &registers[REGISTER_BYTE (G0_REGNUM)], 16 * I960_GREG_SIZE);
-  bcopy (&i960_greg_packet[I960_R_PCW],
-	 &registers[REGISTER_BYTE (PCW_REGNUM)], sizeof (int));
-  bcopy (&i960_greg_packet[I960_R_ACW],
-	 &registers[REGISTER_BYTE (ACW_REGNUM)], sizeof (int));
-  bcopy (&i960_greg_packet[I960_R_TCW],
-	 &registers[REGISTER_BYTE (TCW_REGNUM)], sizeof (int));
-
-  /* If the target has floating point registers, fetch them.
-     Otherwise, zero the floating point register values in
-     registers[] for good measure, even though we might not
-     need to.  */
-
-  if (target_has_fp)
-    {
-      net_read_registers (i960_fpreg_packet, I960_FPREG_PLEN,
-			  PTRACE_GETFPREGS);
-      bcopy (&i960_fpreg_packet[I960_R_FP0],
-	     &registers[REGISTER_BYTE (FP0_REGNUM)],
-	     REGISTER_RAW_SIZE (FP0_REGNUM) * 4);
-    }
-  else
-    bzero (&registers[REGISTER_BYTE (FP0_REGNUM)],
-	   REGISTER_RAW_SIZE (FP0_REGNUM) * 4);
-
-  /* Mark the register cache valid.  */
-
-  registers_fetched ();
-}
-
-/* Store a register or registers into the VxWorks target.
-   REGNO is the register to store, or -1 for all; currently,
-   it is ignored.  FIXME look at regno to improve efficiency.  */
-
-void
-vx_write_register (int regno)
-{
-  char i960_greg_packet[I960_GREG_PLEN];
-  char i960_fpreg_packet[I960_FPREG_PLEN];
-
-  /* Store floating-point registers.  When copying values from
-     registers [], don't assume that a location in registers []
-     is properly aligned for the target data type.  */
-
-  bcopy (&registers[REGISTER_BYTE (R0_REGNUM)],
-	 &i960_greg_packet[I960_R_R0], 16 * I960_GREG_SIZE);
-  bcopy (&registers[REGISTER_BYTE (G0_REGNUM)],
-	 &i960_greg_packet[I960_R_G0], 16 * I960_GREG_SIZE);
-  bcopy (&registers[REGISTER_BYTE (PCW_REGNUM)],
-	 &i960_greg_packet[I960_R_PCW], sizeof (int));
-  bcopy (&registers[REGISTER_BYTE (ACW_REGNUM)],
-	 &i960_greg_packet[I960_R_ACW], sizeof (int));
-  bcopy (&registers[REGISTER_BYTE (TCW_REGNUM)],
-	 &i960_greg_packet[I960_R_TCW], sizeof (int));
-
-  net_write_registers (i960_greg_packet, I960_GREG_PLEN, PTRACE_SETREGS);
-
-  /* Store floating point registers if the target has them.  */
-
-  if (target_has_fp)
-    {
-      bcopy (&registers[REGISTER_BYTE (FP0_REGNUM)],
-	     &i960_fpreg_packet[I960_R_FP0],
-	     REGISTER_RAW_SIZE (FP0_REGNUM) * 4);
-
-      net_write_registers (i960_fpreg_packet, I960_FPREG_PLEN,
-			   PTRACE_SETFPREGS);
-    }
-}
+// OBSOLETE /* i80960-dependent portions of the RPC protocol
+// OBSOLETE    used with a VxWorks target 
+// OBSOLETE 
+// OBSOLETE    Contributed by Wind River Systems.
+// OBSOLETE 
+// OBSOLETE    This file is part of GDB.
+// OBSOLETE 
+// OBSOLETE    This program is free software; you can redistribute it and/or modify
+// OBSOLETE    it under the terms of the GNU General Public License as published by
+// OBSOLETE    the Free Software Foundation; either version 2 of the License, or
+// OBSOLETE    (at your option) any later version.
+// OBSOLETE 
+// OBSOLETE    This program is distributed in the hope that it will be useful,
+// OBSOLETE    but WITHOUT ANY WARRANTY; without even the implied warranty of
+// OBSOLETE    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// OBSOLETE    GNU General Public License for more details.
+// OBSOLETE 
+// OBSOLETE    You should have received a copy of the GNU General Public License
+// OBSOLETE    along with this program; if not, write to the Free Software
+// OBSOLETE    Foundation, Inc., 59 Temple Place - Suite 330,
+// OBSOLETE    Boston, MA 02111-1307, USA.  */
+// OBSOLETE 
+// OBSOLETE #include <stdio.h>
+// OBSOLETE #include "defs.h"
+// OBSOLETE 
+// OBSOLETE #include "vx-share/regPacket.h"
+// OBSOLETE #include "frame.h"
+// OBSOLETE #include "inferior.h"
+// OBSOLETE #include "target.h"
+// OBSOLETE #include "gdbcore.h"
+// OBSOLETE #include "command.h"
+// OBSOLETE #include "symtab.h"
+// OBSOLETE #include "symfile.h"		/* for struct complaint */
+// OBSOLETE #include "regcache.h"
+// OBSOLETE 
+// OBSOLETE #include "gdb_string.h"
+// OBSOLETE #include <errno.h>
+// OBSOLETE #include <fcntl.h>
+// OBSOLETE #include <sys/types.h>
+// OBSOLETE #include <sys/time.h>
+// OBSOLETE #include <sys/socket.h>
+// OBSOLETE 
+// OBSOLETE #ifdef _AIX			/* IBM claims "void *malloc()" not char * */
+// OBSOLETE #define malloc bogon_malloc
+// OBSOLETE #endif
+// OBSOLETE 
+// OBSOLETE #include <rpc/rpc.h>
+// OBSOLETE #include <sys/time.h>		/* UTek's <rpc/rpc.h> doesn't #incl this */
+// OBSOLETE #include <netdb.h>
+// OBSOLETE #include "vx-share/ptrace.h"
+// OBSOLETE #include "vx-share/xdr_ptrace.h"
+// OBSOLETE #include "vx-share/xdr_ld.h"
+// OBSOLETE #include "vx-share/xdr_rdb.h"
+// OBSOLETE #include "vx-share/dbgRpcLib.h"
+// OBSOLETE 
+// OBSOLETE /* get rid of value.h if possible */
+// OBSOLETE #include <value.h>
+// OBSOLETE #include <symtab.h>
+// OBSOLETE 
+// OBSOLETE /* Flag set if target has fpu */
+// OBSOLETE 
+// OBSOLETE extern int target_has_fp;
+// OBSOLETE 
+// OBSOLETE /* 960 floating point format descriptor, from "i960-tdep.c."  */
+// OBSOLETE 
+// OBSOLETE extern struct ext_format ext_format_i960;
+// OBSOLETE 
+// OBSOLETE /* Generic register read/write routines in remote-vx.c.  */
+// OBSOLETE 
+// OBSOLETE extern void net_read_registers ();
+// OBSOLETE extern void net_write_registers ();
+// OBSOLETE 
+// OBSOLETE /* Read a register or registers from the VxWorks target.
+// OBSOLETE    REGNO is the register to read, or -1 for all; currently,
+// OBSOLETE    it is ignored.  FIXME look at regno to improve efficiency.  */
+// OBSOLETE 
+// OBSOLETE void
+// OBSOLETE vx_read_register (int regno)
+// OBSOLETE {
+// OBSOLETE   char i960_greg_packet[I960_GREG_PLEN];
+// OBSOLETE   char i960_fpreg_packet[I960_FPREG_PLEN];
+// OBSOLETE 
+// OBSOLETE   /* Get general-purpose registers.  When copying values into
+// OBSOLETE      registers [], don't assume that a location in registers []
+// OBSOLETE      is properly aligned for the target data type.  */
+// OBSOLETE 
+// OBSOLETE   net_read_registers (i960_greg_packet, I960_GREG_PLEN, PTRACE_GETREGS);
+// OBSOLETE 
+// OBSOLETE   bcopy (&i960_greg_packet[I960_R_R0],
+// OBSOLETE 	 &registers[REGISTER_BYTE (R0_REGNUM)], 16 * I960_GREG_SIZE);
+// OBSOLETE   bcopy (&i960_greg_packet[I960_R_G0],
+// OBSOLETE 	 &registers[REGISTER_BYTE (G0_REGNUM)], 16 * I960_GREG_SIZE);
+// OBSOLETE   bcopy (&i960_greg_packet[I960_R_PCW],
+// OBSOLETE 	 &registers[REGISTER_BYTE (PCW_REGNUM)], sizeof (int));
+// OBSOLETE   bcopy (&i960_greg_packet[I960_R_ACW],
+// OBSOLETE 	 &registers[REGISTER_BYTE (ACW_REGNUM)], sizeof (int));
+// OBSOLETE   bcopy (&i960_greg_packet[I960_R_TCW],
+// OBSOLETE 	 &registers[REGISTER_BYTE (TCW_REGNUM)], sizeof (int));
+// OBSOLETE 
+// OBSOLETE   /* If the target has floating point registers, fetch them.
+// OBSOLETE      Otherwise, zero the floating point register values in
+// OBSOLETE      registers[] for good measure, even though we might not
+// OBSOLETE      need to.  */
+// OBSOLETE 
+// OBSOLETE   if (target_has_fp)
+// OBSOLETE     {
+// OBSOLETE       net_read_registers (i960_fpreg_packet, I960_FPREG_PLEN,
+// OBSOLETE 			  PTRACE_GETFPREGS);
+// OBSOLETE       bcopy (&i960_fpreg_packet[I960_R_FP0],
+// OBSOLETE 	     &registers[REGISTER_BYTE (FP0_REGNUM)],
+// OBSOLETE 	     REGISTER_RAW_SIZE (FP0_REGNUM) * 4);
+// OBSOLETE     }
+// OBSOLETE   else
+// OBSOLETE     bzero (&registers[REGISTER_BYTE (FP0_REGNUM)],
+// OBSOLETE 	   REGISTER_RAW_SIZE (FP0_REGNUM) * 4);
+// OBSOLETE 
+// OBSOLETE   /* Mark the register cache valid.  */
+// OBSOLETE 
+// OBSOLETE   registers_fetched ();
+// OBSOLETE }
+// OBSOLETE 
+// OBSOLETE /* Store a register or registers into the VxWorks target.
+// OBSOLETE    REGNO is the register to store, or -1 for all; currently,
+// OBSOLETE    it is ignored.  FIXME look at regno to improve efficiency.  */
+// OBSOLETE 
+// OBSOLETE void
+// OBSOLETE vx_write_register (int regno)
+// OBSOLETE {
+// OBSOLETE   char i960_greg_packet[I960_GREG_PLEN];
+// OBSOLETE   char i960_fpreg_packet[I960_FPREG_PLEN];
+// OBSOLETE 
+// OBSOLETE   /* Store floating-point registers.  When copying values from
+// OBSOLETE      registers [], don't assume that a location in registers []
+// OBSOLETE      is properly aligned for the target data type.  */
+// OBSOLETE 
+// OBSOLETE   bcopy (&registers[REGISTER_BYTE (R0_REGNUM)],
+// OBSOLETE 	 &i960_greg_packet[I960_R_R0], 16 * I960_GREG_SIZE);
+// OBSOLETE   bcopy (&registers[REGISTER_BYTE (G0_REGNUM)],
+// OBSOLETE 	 &i960_greg_packet[I960_R_G0], 16 * I960_GREG_SIZE);
+// OBSOLETE   bcopy (&registers[REGISTER_BYTE (PCW_REGNUM)],
+// OBSOLETE 	 &i960_greg_packet[I960_R_PCW], sizeof (int));
+// OBSOLETE   bcopy (&registers[REGISTER_BYTE (ACW_REGNUM)],
+// OBSOLETE 	 &i960_greg_packet[I960_R_ACW], sizeof (int));
+// OBSOLETE   bcopy (&registers[REGISTER_BYTE (TCW_REGNUM)],
+// OBSOLETE 	 &i960_greg_packet[I960_R_TCW], sizeof (int));
+// OBSOLETE 
+// OBSOLETE   net_write_registers (i960_greg_packet, I960_GREG_PLEN, PTRACE_SETREGS);
+// OBSOLETE 
+// OBSOLETE   /* Store floating point registers if the target has them.  */
+// OBSOLETE 
+// OBSOLETE   if (target_has_fp)
+// OBSOLETE     {
+// OBSOLETE       bcopy (&registers[REGISTER_BYTE (FP0_REGNUM)],
+// OBSOLETE 	     &i960_fpreg_packet[I960_R_FP0],
+// OBSOLETE 	     REGISTER_RAW_SIZE (FP0_REGNUM) * 4);
+// OBSOLETE 
+// OBSOLETE       net_write_registers (i960_fpreg_packet, I960_FPREG_PLEN,
+// OBSOLETE 			   PTRACE_SETFPREGS);
+// OBSOLETE     }
+// OBSOLETE }
