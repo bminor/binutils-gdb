d0403e0050dd2e8a717d830ed3298cd8f9c3676a
Andrew Cagney <cagney@redhat.com>
2002-08-25 15:18:41 +0000

    2002-08-25  Andrew Cagney  <ac131313@redhat.com>
    	* rs6000-tdep.c (rs6000_gdbarch_init): Set store_struct_return
    	instead of deprecated_store_return_value.  Fix fallout from
    	2002-08-23 Andrew Cagney <cagney@redhat.com>.

--- gdb/rs6000-tdep.c
+++ gdb/rs6000-tdep.c
@@ -2888,7 +2888,7 @@ rs6000_gdbarch_init (struct gdbarch_info info, struct gdbarch_list *arches)
   else
     set_gdbarch_push_arguments (gdbarch, rs6000_push_arguments);
 
-  set_gdbarch_deprecated_store_return_value (gdbarch, rs6000_store_return_value);
+  set_gdbarch_store_struct_return (gdbarch, rs6000_store_struct_return);
   set_gdbarch_deprecated_extract_struct_value_address (gdbarch, rs6000_extract_struct_value_address);
   set_gdbarch_pop_frame (gdbarch, rs6000_pop_frame);
 
