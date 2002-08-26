99e49e4e29dd86382be0b80f065377e0215132aa
Alan Modra <amodra@gmail.com>
2002-08-26 10:16:34 +0000

    * ld-mmix/b-nosym.d: Adjust for "no symbols" on stdout.

--- ld/testsuite/ld-mmix/b-nosym.d
+++ ld/testsuite/ld-mmix/b-nosym.d
@@ -3,13 +3,13 @@
 #source: b-nosym.s
 #ld: --oformat binary
 #objcopy_linked_file:
-#objdump: -st 2>/dev/null
-
-# Note that we have to redirect stderr when objdumping to get rid of the
-# "no symbols" message that would otherwise cause a spurious failure and
-# which we seemingly can't identify or prune in another way.
+#objdump: -st
 
 .*:     file format mmo
+
+SYMBOL TABLE:
+no symbols
+
 
 Contents of section \.text:
  0000 e3fd0001 e3fd0004                    .*
