42f986191ba958a9f996e411eb67888ec663ade7
H.J. Lu <hjl.tools@gmail.com>
2002-09-30 04:23:40 +0000

    No need to set OTHER_GOT_RELOC_SECTIONS in emulparams/elf32ppclinux.sh.

--- ld/emulparams/elf32ppclinux.sh
+++ ld/emulparams/elf32ppclinux.sh
@@ -6,6 +6,3 @@ test -z "${RELOCATING}" || OTHER_SECTIONS="/DISCARD/	: { *(.fixup) }"
 OTHER_READWRITE_SECTIONS="
   .got1         ${RELOCATING-0} : { *(.got1) }
   .got2         ${RELOCATING-0} : { *(.got2) }"
-OTHER_GOT_RELOC_SECTIONS="
-  .rela.got1         ${RELOCATING-0} : { *(.rela.got1) }
-  .rela.got2         ${RELOCATING-0} : { *(.rela.got2) }"
