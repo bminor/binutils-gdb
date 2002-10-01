97724570fe57a08d93c7c61269848e2326cf4efc
Alan Modra <amodra@gmail.com>
2002-10-01 06:15:33 +0000

    * config/tc-mips.h (TC_FORCE_RELOCATION_SUB_SAME): Define. 	(TC_FORCE_RELOCATION): Tidy arg.

--- gas/config/tc-mips.h
+++ gas/config/tc-mips.h
@@ -136,8 +136,12 @@ extern int mips_fix_adjustable PARAMS ((struct fix *));
 
 /* When generating embedded PIC code we must keep PC relative
    relocations.  */
-#define TC_FORCE_RELOCATION(fixp) mips_force_relocation (fixp)
+#define TC_FORCE_RELOCATION(FIX) mips_force_relocation (FIX)
 extern int mips_force_relocation PARAMS ((struct fix *));
+
+#define TC_FORCE_RELOCATION_SUB_SAME(FIX, SEG)	\
+  (mips_force_relocation (FIX)			\
+   || !SEG_NORMAL (SEG))
 
 /* Register mask variables.  These are set by the MIPS assembly code
    and used by ECOFF and possibly other object file formats.  */
