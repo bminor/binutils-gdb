7a68fea6e146cb82ef603df3057fa04adb49c1b3
Stephen Clarke <stephen.clarke@earthling.net>
2002-10-03 00:56:06 +0000

    * gas/sh/sh64/ptc32-noexp-1.d: Adjust for changes to *ABS* symbol generation. * gas/sh/sh64/ptc64-noexp-1.d: Likewise.

--- gas/testsuite/gas/sh/sh64/ptc32-noexp-1.d
+++ gas/testsuite/gas/sh/sh64/ptc32-noexp-1.d
@@ -8,5 +8,5 @@
 Disassembly of section \.text:
 
 [0]+ <start>:
-[ 	]+0:[ 	]+e8000610[ 	]+pta/l	4 <\*ABS\*\+0x4>,tr1
+[ 	]+0:[ 	]+e8000610[ 	]+pta/l	4 <start\+0x4>,tr1
 [ 	]+0:[ 	]+R_SH_PT_16	\*ABS\*\+0x100
