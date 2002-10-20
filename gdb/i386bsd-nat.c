98606147ed4c2e1b99d87a22a6f2117a35f7a3ff
Mark Kettenis <kettenis@gnu.org>
2002-10-20 12:14:39 +0000

    * configure.tgt (i[3456]86-*-openbsd*): Set gdb_target to obds. * config/i386/obsd.mt: New file. * i386bsd-nat.c (_initialize_i386bsd_nat): Introduce i386obsd_sc_pc_offset and i386obsd_sc_sp_offset; * i386obsd-nat.c (_initialize_i386obsd_nat): Set i386obsd_sigtramp_start and i386obsd_sigtramp_end instead of i386nbsd_sigtramp_start and i386nbsd_sigtramp_end. * i386obsd-tdep.c: New file.

--- gdb/i386bsd-nat.c
+++ gdb/i386bsd-nat.c
@@ -400,11 +400,16 @@ _initialize_i386bsd_nat (void)
   extern int i386fbsd4_sc_sp_offset;
 #define SC_PC_OFFSET i386fbsd4_sc_pc_offset
 #define SC_SP_OFFSET i386fbsd4_sc_sp_offset
-#elif defined (NetBSD) || defined (__NetBSD_Version__) || defined (OpenBSD)
+#elif defined (NetBSD) || defined (__NetBSD_Version__)
   extern int i386nbsd_sc_pc_offset;
   extern int i386nbsd_sc_sp_offset;
 #define SC_PC_OFFSET i386nbsd_sc_pc_offset
 #define SC_SP_OFFSET i386nbsd_sc_sp_offset
+#elif defined (OpenBSD)
+  extern int i386obsd_sc_pc_offset;
+  extern int i386obsd_sc_sp_offset;
+#define SC_PC_OFFSET i386obsd_sc_pc_offset
+#define SC_SP_OFFSET i386obsd_sc_sp_offset
 #else
   extern int i386bsd_sc_pc_offset;
   extern int i386bsd_sc_sp_offset;
