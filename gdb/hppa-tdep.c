4facf7e8905fed0e31cc7c1c21f86db158815c9f
Joel Brobecker <brobecker@gnat.com>
2002-11-07 02:19:56 +0000

    * hppa-tdep.c (_initialize_hppa_tdep): Move function body         to end of file, to be more consistent with the pratice followed         by other targets.

--- gdb/hppa-tdep.c
+++ gdb/hppa-tdep.c
@@ -4674,51 +4674,6 @@ hppa_skip_permanent_breakpoint (void)
   /* We can leave the tail's space the same, since there's no jump.  */
 }
 
-void
-_initialize_hppa_tdep (void)
-{
-  struct cmd_list_element *c;
-  void break_at_finish_command (char *arg, int from_tty);
-  void tbreak_at_finish_command (char *arg, int from_tty);
-  void break_at_finish_at_depth_command (char *arg, int from_tty);
-
-  tm_print_insn = print_insn_hppa;
-
-  add_cmd ("unwind", class_maintenance, unwind_command,
-	   "Print unwind table entry at given address.",
-	   &maintenanceprintlist);
-
-  deprecate_cmd (add_com ("xbreak", class_breakpoint, 
-			  break_at_finish_command,
-			  concat ("Set breakpoint at procedure exit. \n\
-Argument may be function name, or \"*\" and an address.\n\
-If function is specified, break at end of code for that function.\n\
-If an address is specified, break at the end of the function that contains \n\
-that exact address.\n",
-		   "With no arg, uses current execution address of selected stack frame.\n\
-This is useful for breaking on return to a stack frame.\n\
-\n\
-Multiple breakpoints at one place are permitted, and useful if conditional.\n\
-\n\
-Do \"help breakpoints\" for info on other commands dealing with breakpoints.", NULL)), NULL);
-  deprecate_cmd (add_com_alias ("xb", "xbreak", class_breakpoint, 1), NULL);
-  deprecate_cmd (add_com_alias ("xbr", "xbreak", class_breakpoint, 1), NULL);
-  deprecate_cmd (add_com_alias ("xbre", "xbreak", class_breakpoint, 1), NULL);
-  deprecate_cmd (add_com_alias ("xbrea", "xbreak", class_breakpoint, 1), NULL);
-
-  deprecate_cmd (c = add_com ("txbreak", class_breakpoint, 
-			      tbreak_at_finish_command,
-"Set temporary breakpoint at procedure exit.  Either there should\n\
-be no argument or the argument must be a depth.\n"), NULL);
-  set_cmd_completer (c, location_completer);
-  
-  if (xdb_commands)
-    deprecate_cmd (add_com ("bx", class_breakpoint, 
-			    break_at_finish_at_depth_command,
-"Set breakpoint at procedure exit.  Either there should\n\
-be no argument or the argument must be a depth.\n"), NULL);
-}
-
 /* Copy the function value from VALBUF into the proper location
    for a function return.
 
@@ -4768,3 +4723,49 @@ hppa_extract_return_value (struct type *type, char *regbuf, char *valbuf)
 		: (4 - TYPE_LENGTH (type)))),
 	    TYPE_LENGTH (type));
 }
+
+void
+_initialize_hppa_tdep (void)
+{
+  struct cmd_list_element *c;
+  void break_at_finish_command (char *arg, int from_tty);
+  void tbreak_at_finish_command (char *arg, int from_tty);
+  void break_at_finish_at_depth_command (char *arg, int from_tty);
+
+  tm_print_insn = print_insn_hppa;
+
+  add_cmd ("unwind", class_maintenance, unwind_command,
+	   "Print unwind table entry at given address.",
+	   &maintenanceprintlist);
+
+  deprecate_cmd (add_com ("xbreak", class_breakpoint, 
+			  break_at_finish_command,
+			  concat ("Set breakpoint at procedure exit. \n\
+Argument may be function name, or \"*\" and an address.\n\
+If function is specified, break at end of code for that function.\n\
+If an address is specified, break at the end of the function that contains \n\
+that exact address.\n",
+		   "With no arg, uses current execution address of selected stack frame.\n\
+This is useful for breaking on return to a stack frame.\n\
+\n\
+Multiple breakpoints at one place are permitted, and useful if conditional.\n\
+\n\
+Do \"help breakpoints\" for info on other commands dealing with breakpoints.", NULL)), NULL);
+  deprecate_cmd (add_com_alias ("xb", "xbreak", class_breakpoint, 1), NULL);
+  deprecate_cmd (add_com_alias ("xbr", "xbreak", class_breakpoint, 1), NULL);
+  deprecate_cmd (add_com_alias ("xbre", "xbreak", class_breakpoint, 1), NULL);
+  deprecate_cmd (add_com_alias ("xbrea", "xbreak", class_breakpoint, 1), NULL);
+
+  deprecate_cmd (c = add_com ("txbreak", class_breakpoint, 
+			      tbreak_at_finish_command,
+"Set temporary breakpoint at procedure exit.  Either there should\n\
+be no argument or the argument must be a depth.\n"), NULL);
+  set_cmd_completer (c, location_completer);
+  
+  if (xdb_commands)
+    deprecate_cmd (add_com ("bx", class_breakpoint, 
+			    break_at_finish_at_depth_command,
+"Set breakpoint at procedure exit.  Either there should\n\
+be no argument or the argument must be a depth.\n"), NULL);
+}
+
