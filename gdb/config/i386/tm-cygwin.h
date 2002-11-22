5633f842680a9fa40ec0fa6cd40ca2ab5d6bb7fb
Christopher Faylor <me+cygwin@cgf.cx>
2002-11-22 07:34:55 +0000

    * win32-nat.c (psapi_get_dll_name): Fix a compiler warning. (struct so_stuff): Add end_addr field. (register_loaded_dll): Calculate and store end address of loaded dll. (solib_address): New function. (child_solib_loaded_library_pathname): Pass carefully constructed section info to safe_symbol_file_add rather than ignoring it. (get_child_debug_event): Call re_enable_breakpoints_in_shlibs when a DLL is loaded. (do_initial_child_stuff): Call disable_breakpoints_in_shlibs. (child_create_inferior): Fix a compiler warning. * config/i386/tm-cygwin.h (PC_SOLIB): Define new macro. (solib_address): Declare new function.

--- gdb/config/i386/tm-cygwin.h
+++ gdb/config/i386/tm-cygwin.h
@@ -1,5 +1,5 @@
 /* Macro definitions for i386 running under the win32 API Unix.
-   Copyright 1995, 1996, 1997, 1998, 1999, 2000
+   Copyright 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002
    Free Software Foundation, Inc.
 
    This file is part of GDB.
@@ -30,6 +30,7 @@
 
 #define ATTACH_NO_WAIT
 #define SOLIB_ADD(filename, from_tty, targ, readsyms) child_solib_add(filename, from_tty, targ, readsyms)
+#define PC_SOLIB(addr) solib_address (addr)
 #define SOLIB_LOADED_LIBRARY_PATHNAME(pid) child_solib_loaded_library_pathname(pid)
 #define CLEAR_SOLIB child_clear_solibs
 #define ADD_SHARED_SYMBOL_FILES dll_symbol_command
@@ -37,6 +38,9 @@
 struct target_ops;
 char *cygwin_pid_to_str (ptid_t ptid);
 void child_solib_add (char *, int, struct target_ops *, int);
+char *solib_address (CORE_ADDR);
 char *child_solib_loaded_library_pathname(int);
 void child_clear_solibs (void);
 void dll_symbol_command (char *, int);
+
+#define LINKER_SYMBOLS_HAVE_WIN32_STDCALL_ARG_SIZES (1)
