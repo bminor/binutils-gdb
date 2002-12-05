d070dab9dbbef49db28f28ef82fe981847d929a4
Jim Wilson <wilson@tuliptree.org>
2002-12-05 18:33:27 +0000

    Fix IA-64 opc-d.x gas testsuite failure. binutils/ChangeLog 	* configure.in (OBJDUMP_DEFS): Define SKIP_ZEROES as 16 for IA-64. 	* configure: Regenerate. gas/testsuite/ChangeLog 	* gas/ia64/opc-x.d: Regenerate.

--- gas/testsuite/gas/ia64/opc-x.d
+++ gas/testsuite/gas/ia64/opc-x.d
@@ -7,32 +7,32 @@ Disassembly of section .text:
 
 0000000000000000 <_start>:
    0:	04 00 00 00 01 00 	\[MLX\]       nop\.m 0x0
-	...
-   e:	00 00 04 00       	            break\.x 0x0
-  12:	00 00 01 c0 ff ff 	\[MLX\]       nop\.m 0x0
-  18:	ff ff 7f e0 ff ff 	            break\.x 0x3fffffffffffffff
-  1e:	01 08 04 00 
-  22:	00 00 01 00 00 00 	\[MLX\]       nop\.m 0x0
-  28:	00 00 00 00 00 00 	            nop\.x 0x0
-  2e:	04 00 04 00 
-  32:	00 00 01 c0 ff ff 	\[MLX\]       nop\.m 0x0
-  38:	ff ff 7f e0 ff ff 	            nop\.x 0x3fffffffffffffff
-  3e:	05 08 04 00 
-  42:	00 00 01 00 00 00 	\[MLX\]       nop\.m 0x0
-  48:	00 00 00 80 00 00 	            movl r4=0x0
-  4e:	00 60 04 00 
-  52:	00 00 01 c0 ff ff 	\[MLX\]       nop\.m 0x0
-  58:	ff ff 7f 80 f0 f7 	            movl r4=0xffffffffffffffff
-  5e:	ff 6f 04 00 
-  62:	00 00 01 80 90 78 	\[MLX\]       nop\.m 0x0
-  68:	56 34 12 80 f0 76 	            movl r4=0x1234567890abcdef
-  6e:	6d 66 04 00 
-  72:	00 00 01 00 00 00 	\[MLX\]       nop\.m 0x0
-  78:	00 00 00 00 00 00 	            hint\.x 0x0
-  7e:	06 00 04 00 
-  82:	00 00 01 00 00 00 	\[MLX\]       nop\.m 0x0
-  88:	00 00 00 00 00 00 	            hint\.x 0x0
-  8e:	06 00 05 00 
-  92:	00 00 01 c0 ff ff 	\[MLX\]       nop\.m 0x0
-  98:	ff ff 7f e0 ff ff 	            hint\.x 0x3fffffffffffffff;;
-  9e:	07 08 00 00 
+   6:	00 00 00 00 00 00 	            break\.x 0x0
+   c:	00 00 00 00 
+  10:	04 00 00 00 01 c0 	\[MLX\]       nop\.m 0x0
+  16:	ff ff ff ff 7f e0 	            break\.x 0x3fffffffffffffff
+  1c:	ff ff 01 08 
+  20:	04 00 00 00 01 00 	\[MLX\]       nop\.m 0x0
+  26:	00 00 00 00 00 00 	            nop\.x 0x0
+  2c:	00 00 04 00 
+  30:	04 00 00 00 01 c0 	\[MLX\]       nop\.m 0x0
+  36:	ff ff ff ff 7f e0 	            nop\.x 0x3fffffffffffffff
+  3c:	ff ff 05 08 
+  40:	04 00 00 00 01 00 	\[MLX\]       nop\.m 0x0
+  46:	00 00 00 00 00 80 	            movl r4=0x0
+  4c:	00 00 00 60 
+  50:	04 00 00 00 01 c0 	\[MLX\]       nop\.m 0x0
+  56:	ff ff ff ff 7f 80 	            movl r4=0xffffffffffffffff
+  5c:	f0 f7 ff 6f 
+  60:	04 00 00 00 01 80 	\[MLX\]       nop\.m 0x0
+  66:	90 78 56 34 12 80 	            movl r4=0x1234567890abcdef
+  6c:	f0 76 6d 66 
+  70:	04 00 00 00 01 00 	\[MLX\]       nop\.m 0x0
+  76:	00 00 00 00 00 00 	            hint\.x 0x0
+  7c:	00 00 06 00 
+  80:	04 00 00 00 01 00 	\[MLX\]       nop\.m 0x0
+  86:	00 00 00 00 00 00 	            hint\.x 0x0
+  8c:	00 00 06 00 
+  90:	05 00 00 00 01 c0 	\[MLX\]       nop\.m 0x0
+  96:	ff ff ff ff 7f e0 	            hint\.x 0x3fffffffffffffff;;
+  9c:	ff ff 07 08 
