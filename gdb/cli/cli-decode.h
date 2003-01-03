8fe84d014901cb627303bb59822928b76d70d58c
Mark Kettenis <kettenis@gnu.org>
2003-01-03 14:10:08 +0000

    * cli/cli-decode.h: Don't include "gdb_regex.h"; provide a forward declaration for `struct re_pattern_buffer' instead. * Makefile.in (cli_decode_h): Remove $(gdb_regex_h).

--- gdb/cli/cli-decode.h
+++ gdb/cli/cli-decode.h
@@ -19,8 +19,9 @@
 #if !defined (CLI_DECODE_H)
 #define CLI_DECODE_H 1
 
-#include "gdb_regex.h"		/* Needed by apropos_cmd.  */
 #include "command.h"
+
+struct re_pattern_buffer;
 
 #if 0
 /* FIXME: cagney/2002-03-17: Once cmd_type() has been removed, ``enum
