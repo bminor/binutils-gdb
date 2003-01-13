b5e5c35c013aa9b106a2e94871d63fd31cb28aa8
Daniel Jacobowitz <drow@false.org>
2003-01-13 20:09:33 +0000

    Update copyright years.

--- gdb/gdbtypes.c
+++ gdb/gdbtypes.c
@@ -1,5 +1,5 @@
 /* Support routines for manipulating internal types for GDB.
-   Copyright 1992, 1993, 1994, 1995, 1996, 1998, 1999, 2000, 2001, 2002
+   Copyright 1992, 1993, 1994, 1995, 1996, 1998, 1999, 2000, 2001, 2002, 2003
    Free Software Foundation, Inc.
    Contributed by Cygnus Support, using pieces from other GDB modules.
 
