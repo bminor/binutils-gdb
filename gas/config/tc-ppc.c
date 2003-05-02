df12615d919cb6b436e8b190c0ec63658e2b4097
Alan Modra <amodra@gmail.com>
2003-05-02 02:29:22 +0000

    * config/tc-ppc.c (md_show_usage): Mention -a32, -a64, -l, -le, -b, 	-be and split strings to below 509 bytes in length.

--- gas/config/tc-ppc.c
+++ gas/config/tc-ppc.c
@@ -1064,36 +1064,40 @@ md_show_usage (stream)
 {
   fprintf (stream, _("\
 PowerPC options:\n\
+-a32			generate ELF32/XCOFF32\n\
+-a64			generate ELF64/XCOFF64\n\
 -u			ignored\n\
 -mpwrx, -mpwr2		generate code for POWER/2 (RIOS2)\n\
 -mpwr			generate code for POWER (RIOS1)\n\
 -m601			generate code for PowerPC 601\n\
 -mppc, -mppc32, -m603, -m604\n\
 			generate code for PowerPC 603/604\n\
--m403, -m405            generate code for PowerPC 403/405\n\
+-m403, -m405		generate code for PowerPC 403/405\n\
 -m7400, -m7410, -m7450, -m7455\n\
-			generate code For PowerPC 7400/7410/7450/7455\n\
+			generate code For PowerPC 7400/7410/7450/7455\n"));
+  fprintf (stream, _("\
 -mppc64, -m620		generate code for PowerPC 620/625/630\n\
 -mppc64bridge		generate code for PowerPC 64, including bridge insns\n\
 -mbooke64		generate code for 64-bit PowerPC BookE\n\
 -mbooke, mbooke32	generate code for 32-bit PowerPC BookE\n\
 -mpower4		generate code for Power4 architecture\n\
--maltivec		generate code for AltiVec\n\
 -mcom			generate code Power/PowerPC common instructions\n\
--many			generate code for any architecture (PWR/PWRX/PPC)\n\
+-many			generate code for any architecture (PWR/PWRX/PPC)\n"));
+  fprintf (stream, _("\
+-maltivec		generate code for AltiVec\n\
+-me500, -me500x2	generate code for Motorola e500 core complex\n\
+-mspe			generate code for Motorola SPE instructions\n\
 -mregnames		Allow symbolic names for registers\n\
 -mno-regnames		Do not allow symbolic names for registers\n"));
-  fprintf (stream, _("\
--me500, -me500x2	generate code for Motorola e500 core complex\n\
--mspe			generate code for Motorola SPE instructions\n"));
 #ifdef OBJ_ELF
   fprintf (stream, _("\
 -mrelocatable		support for GCC's -mrelocatble option\n\
 -mrelocatable-lib	support for GCC's -mrelocatble-lib option\n\
 -memb			set PPC_EMB bit in ELF flags\n\
--mlittle, -mlittle-endian\n\
+-mlittle, -mlittle-endian, -l, -le\n\
 			generate code for a little endian machine\n\
--mbig, -mbig-endian	generate code for a big endian machine\n\
+-mbig, -mbig-endian, -b, -be\n\
+			generate code for a big endian machine\n\
 -msolaris		generate code for Solaris\n\
 -mno-solaris		do not generate code for Solaris\n\
 -V			print assembler version number\n\
