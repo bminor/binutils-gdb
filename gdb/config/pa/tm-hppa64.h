30e221b4d32a44919e816c807d7dda1d552f8de0
Andrew Cagney <cagney@redhat.com>
2003-05-12 00:26:19 +0000

    Index: gdb/ChangeLog 2003-05-11  Andrew Cagney  <cagney@redhat.com>
    	* Makefile.in (mi-symbol-cmds.o): Rename mi-cmd-symbol.
    	* config/djgpp/fnchange.lst: Rename include/xtensa-isa-internal.h,
    	include/xtensa-isa.h, sim/testsuite/sim/arm/misaligned1.ms,
    	sim/testsuite/sim/arm/misaligned2.ms, and
    	sim/testsuite/sim/arm/misaligned3.ms.
    	* disasm.h (struct ui_file): Add opaque struct declaration.
    	* config/pa/tm-hppa64.h (struct type, struct frame_info): Ditto.
    	* frame.h (struct ui_file): Ditto.
    
    Index: gdb/mi/ChangeLog
    2003-05-11  Andrew Cagney  <cagney@redhat.com>
    
    	* mi-symbol-cmds.c: Rename "mi-cmd-symbol.c", avoid 8.3 problem.

--- gdb/config/pa/tm-hppa64.h
+++ gdb/config/pa/tm-hppa64.h
@@ -21,6 +21,9 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
+struct type;
+struct frame_info;
+
 /* PA 64-bit specific definitions.  Override those which are in
    tm-hppa.h */
 
