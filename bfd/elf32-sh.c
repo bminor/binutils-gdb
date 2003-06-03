2558b9a9f0a3b38756ba4044c052de5e0f2be6cc
Kaz Kojima <kkojima@rr.iij4u.or.jp>
2003-06-03 04:50:20 +0000

    * elf32-sh.c (sh_elf_size_dynamic_sections): Create .interp section 	and DT_DEBUG dynamic tag even for position independent executables. 	* elf64-sh64.c (sh64_elf64_size_dynamic_sections): Likewize.

--- bfd/elf32-sh.c
+++ bfd/elf32-sh.c
@@ -4338,7 +4338,7 @@ sh_elf_size_dynamic_sections (output_bfd, info)
   if (htab->root.dynamic_sections_created)
     {
       /* Set the contents of the .interp section to the interpreter.  */
-      if (! info->shared)
+      if (info->executable)
 	{
 	  s = bfd_get_section_by_name (dynobj, ".interp");
 	  BFD_ASSERT (s != NULL);
@@ -4500,7 +4500,7 @@ sh_elf_size_dynamic_sections (output_bfd, info)
 #define add_dynamic_entry(TAG, VAL) \
   bfd_elf32_add_dynamic_entry (info, (bfd_vma) (TAG), (bfd_vma) (VAL))
 
-      if (! info->shared)
+      if (info->executable)
 	{
 	  if (! add_dynamic_entry (DT_DEBUG, 0))
 	    return FALSE;
