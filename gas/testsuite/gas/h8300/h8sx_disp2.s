28d7dee9f8d1335bac2a2c51ea152d4d3db46823
Richard Sandiford <rdsandiford@googlemail.com>
2003-06-10 07:22:22 +0000

    * gas/h8300/h8sx_disp2.[sd]: New test. 	* gas/h8300/h8300.exp: Run it.

---
+++ gas/testsuite/gas/h8300/h8sx_disp2.s
@@ -0,0 +1,18 @@
+	.h8300sx
+	add.b	#2,@(1,er0)	; L_2
+	add.b	#2,@(2,er0)	; L_2
+	add.b	#2,@(3,er0)	; L_2
+	add.b	#2,@(4,er0)	; L_16
+
+	add.w	#2,@(1,er0)	; L_16
+	add.w	#2,@(2,er0)	; L_2
+	add.w	#2,@(4,er0)	; L_2
+	add.w	#2,@(6,er0)	; L_2
+	add.w	#2,@(8,er0)	; L_16
+
+	add.l	#2,@(1,er0)	; L_16
+	add.l	#2,@(2,er0)	; L_16
+	add.l	#2,@(4,er0)	; L_2
+	add.l	#2,@(8,er0)	; L_2
+	add.l	#2,@(12,er0)	; L_2
+	add.l	#2,@(16,er0)	; L_16
