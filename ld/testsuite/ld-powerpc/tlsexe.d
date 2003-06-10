81cacc150bd3f44d24302def2fdb6cd86e36ace2
Alan Modra <amodra@gmail.com>
2003-06-10 04:36:01 +0000

    * ld-elfvsb/elfvsb.exp: Run for powerpc64 too. 	* ld-powerpc/tls.t: Update. 	* ld-powerpc/tlsexe.d: Update. 	* ld-powerpc/tlsexe.r: Update. 	* ld-powerpc/tlsexetoc.d: Update. 	* ld-powerpc/tlsexetoc.r: Update. 	* ld-powerpc/tlsexetoc.t: Update. 	* ld-powerpc/tlsso.d: Update. 	* ld-powerpc/tlsso.g: Update. 	* ld-powerpc/tlsso.r: Update. 	* ld-powerpc/tlsso.t: Update. 	* ld-powerpc/tlstocso.d: Update. 	* ld-powerpc/tlstocso.g: Update. 	* ld-powerpc/tlstocso.r: Update. 	* ld-powerpc/tlstocso.t: Update.

--- ld/testsuite/ld-powerpc/tlsexe.d
+++ ld/testsuite/ld-powerpc/tlsexe.d
@@ -56,13 +56,21 @@ Disassembly of section \.text:
 .*	e9 4d 90 2a 	lwa	r10,-28632\(r13\)
 .*	3d 2d 00 00 	addis	r9,r13,0
 .*	a9 49 90 30 	lha	r10,-28624\(r9\)
-.*	e8 41 00 28 	ld	r2,40\(r1\)
-.*	3d 82 00 00 	addis	r12,r2,0
-.*	e9 6c 80 30 	ld	r11,-32720\(r12\)
-.*	e8 4c 80 38 	ld	r2,-32712\(r12\)
+.*	7d 89 02 a6 	mfctr	r12
+.*	78 0b 1f 24 	rldicr	r11,r0,3,60
+.*	34 40 80 00 	addic\.	r2,r0,-32768
+.*	7d 8b 60 50 	subf	r12,r11,r12
+.*	7c 42 fe 76 	sradi	r2,r2,63
+.*	78 0b 17 64 	rldicr	r11,r0,2,61
+.*	7c 42 58 38 	and	r2,r2,r11
+.*	7d 8b 60 50 	subf	r12,r11,r12
+.*	7d 8c 12 14 	add	r12,r12,r2
+.*	3d 8c 00 01 	addis	r12,r12,1
+.*	e9 6c 01 c4 	ld	r11,452\(r12\)
+.*	39 8c 01 c4 	addi	r12,r12,452
+.*	e8 4c 00 08 	ld	r2,8\(r12\)
 .*	7d 69 03 a6 	mtctr	r11
-.*	e9 6c 80 40 	ld	r11,-32704\(r12\)
+.*	e9 6c 00 10 	ld	r11,16\(r12\)
 .*	4e 80 04 20 	bctr
-.*	60 00 00 00 	nop
 .*	38 00 00 00 	li	r0,0
-.*	4b ff ff dc 	b	.*
+.*	4b ff ff bc 	b	.*
--- ld/testsuite/ld-powerpc/tlsexetoc.r
+++ ld/testsuite/ld-powerpc/tlsexetoc.r
@@ -5,37 +5,35 @@
 #readelf: -WSsrl
 #target: powerpc64*-*-*
 
-There are 23 section headers.*
+There are 21 section headers.*
 
 Section Headers:
  +\[Nr\] Name +Type +Address +Off +Size +ES Flg Lk Inf Al
  +\[ 0\] +NULL +0+ 0+ 0+ 0+ +0 +0 +0
  +\[ 1\] \.interp +PROGBITS +0+10000190 0+190 0+11 0+ +A +0 +0 +1
- +\[ 2\] \.hash +HASH +0+100001a8 0+1a8 0+3c 04 +A +3 +0 +8
- +\[ 3\] \.dynsym +DYNSYM +0+100001e8 0+1e8 0+f0 18 +A +4 +1 +8
- +\[ 4\] \.dynstr +STRTAB +0+100002d8 0+2d8 0+4d 0+ +A +0 +0 +1
- +\[ 5\] \.rela\.dyn +RELA +0+10000328 0+328 0+30 18 +A +3 +0 +8
- +\[ 6\] \.rela\.plt +RELA +0+10000358 0+358 0+18 18 +A +3 +12 +8
- +\[ 7\] \.text +PROGBITS +0+10000370 0+370 0+9c 0+ +AX +0 +0 +4
+ +\[ 2\] \.hash +HASH +0+100001a8 0+1a8 0+38 04 +A +3 +0 +8
+ +\[ 3\] \.dynsym +DYNSYM +0+100001e0 0+1e0 0+d8 18 +A +4 +1 +8
+ +\[ 4\] \.dynstr +STRTAB +0+100002b8 0+2b8 0+4c 0+ +A +0 +0 +1
+ +\[ 5\] \.rela\.dyn +RELA +0+10000308 0+308 0+30 18 +A +3 +0 +8
+ +\[ 6\] \.rela\.plt +RELA +0+10000338 0+338 0+18 18 +A +3 +10 +8
+ +\[ 7\] \.text +PROGBITS +0+10000350 0+350 0+bc 0+ +AX +0 +0 +4
  +\[ 8\] \.data +PROGBITS +0+10010410 0+410 0+ 0+ +WA +0 +0 +1
  +\[ 9\] \.branch_lt +PROGBITS +0+10010410 0+410 0+ 0+ +WA +0 +0 +8
  +\[10\] \.tdata +PROGBITS +0+10010410 0+410 0+38 0+ WAT +0 +0 +8
  +\[11\] \.tbss +NOBITS +0+10010448 0+448 0+38 0+ WAT +0 +0 +8
  +\[12\] \.dynamic +DYNAMIC +0+10010448 0+448 0+150 10 +WA +4 +0 +8
- +\[13\] \.ctors +PROGBITS +0+10010598 0+5f0 0+ 0+ +W +0 +0 +1
- +\[14\] \.dtors +PROGBITS +0+10010598 0+5f0 0+ 0+ +W +0 +0 +1
- +\[15\] \.got +PROGBITS +0+10010598 0+598 0+8 08 +WA +0 +0 +8
- +\[16\] \.toc +PROGBITS +0+100105a0 0+5a0 0+50 0+ +WA +0 +0 +1
- +\[17\] \.sbss +PROGBITS +0+100105f0 0+5f0 0+ 0+ +W +0 +0 +1
- +\[18\] \.plt +NOBITS +0+100105f0 0+5f0 0+30 18 +WA +0 +0 +8
- +\[19\] \.bss +NOBITS +0+10010620 0+5f0 0+ 0+ +WA +0 +0 +1
- +\[20\] \.shstrtab +STRTAB +0+ 0+5f0 0+9d 0+ +0 +0 +1
- +\[21\] \.symtab +SYMTAB +0+ 0+c50 0+480 18 +22 +1f +8
- +\[22\] \.strtab +STRTAB +0+ 0+10d0 0+92 0+ +0 +0 +1
+ +\[13\] \.got +PROGBITS +0+10010598 0+598 0+8 08 +WA +0 +0 +8
+ +\[14\] \.toc +PROGBITS +0+100105a0 0+5a0 0+50 0+ +WA +0 +0 +1
+ +\[15\] \.sbss +PROGBITS +0+100105f0 0+5f0 0+ 0+ +W +0 +0 +1
+ +\[16\] \.plt +NOBITS +0+100105f0 0+5f0 0+30 18 +WA +0 +0 +8
+ +\[17\] \.bss +NOBITS +0+10010620 0+5f0 0+ 0+ +WA +0 +0 +1
+ +\[18\] \.shstrtab +STRTAB +0+ 0+5f0 0+8f 0+ +0 +0 +1
+ +\[19\] \.symtab +SYMTAB +0+ 0+bc0 0+438 18 +20 +1d +8
+ +\[20\] \.strtab +STRTAB +0+ 0+ff8 0+8c 0+ +0 +0 +1
 #...
 
 Elf file type is EXEC \(Executable file\)
-Entry point 0x1000038c
+Entry point 0x1000036c
 There are 6 program headers.*
 
 Program Headers:
@@ -66,7 +64,7 @@ Relocation section '\.rela\.plt' at offset .* contains 1 entries:
  +Offset +Info +Type +Symbol's Value +Symbol's Name \+ Addend
 0+10010608 +0+300000015 R_PPC64_JMP_SLOT +0+ __tls_get_addr \+ 0
 
-Symbol table '\.dynsym' contains 10 entries:
+Symbol table '\.dynsym' contains 9 entries:
  +Num: +Value +Size Type +Bind +Vis +Ndx Name
  +0: 0+ +0 NOTYPE +LOCAL +DEFAULT +UND 
  +1: 0+10010448 +0 OBJECT +GLOBAL DEFAULT +ABS _DYNAMIC
@@ -74,58 +72,54 @@ Symbol table '\.dynsym' contains 10 entries:
  +3: 0+ +0 NOTYPE +GLOBAL DEFAULT +UND __tls_get_addr
  +4: 0+ +0 FUNC +GLOBAL DEFAULT +UND \.__tls_get_addr
  +5: 0+ +0 TLS +GLOBAL DEFAULT +UND ld
- +6: 0+10010620 +0 NOTYPE +GLOBAL DEFAULT +ABS __end
- +7: 0+100105f0 +0 NOTYPE +GLOBAL DEFAULT +ABS __bss_start
- +8: 0+100105f0 +0 NOTYPE +GLOBAL DEFAULT +ABS _edata
- +9: 0+10010620 +0 NOTYPE +GLOBAL DEFAULT +ABS _end
+ +6: 0+100105f0 +0 NOTYPE +GLOBAL DEFAULT +ABS __bss_start
+ +7: 0+100105f0 +0 NOTYPE +GLOBAL DEFAULT +ABS _edata
+ +8: 0+10010620 +0 NOTYPE +GLOBAL DEFAULT +ABS _end
 
-Symbol table '\.symtab' contains 48 entries:
+Symbol table '\.symtab' contains 45 entries:
  +Num: +Value +Size Type +Bind +Vis +Ndx Name
  +0: 0+ +0 NOTYPE +LOCAL +DEFAULT +UND 
  +1: 0+10000190 +0 SECTION LOCAL +DEFAULT +1 
  +2: 0+100001a8 +0 SECTION LOCAL +DEFAULT +2 
- +3: 0+100001e8 +0 SECTION LOCAL +DEFAULT +3 
- +4: 0+100002d8 +0 SECTION LOCAL +DEFAULT +4 
- +5: 0+10000328 +0 SECTION LOCAL +DEFAULT +5 
- +6: 0+10000358 +0 SECTION LOCAL +DEFAULT +6 
- +7: 0+10000370 +0 SECTION LOCAL +DEFAULT +7 
+ +3: 0+100001e0 +0 SECTION LOCAL +DEFAULT +3 
+ +4: 0+100002b8 +0 SECTION LOCAL +DEFAULT +4 
+ +5: 0+10000308 +0 SECTION LOCAL +DEFAULT +5 
+ +6: 0+10000338 +0 SECTION LOCAL +DEFAULT +6 
+ +7: 0+10000350 +0 SECTION LOCAL +DEFAULT +7 
  +8: 0+10010410 +0 SECTION LOCAL +DEFAULT +8 
  +9: 0+10010410 +0 SECTION LOCAL +DEFAULT +9 
  +10: 0+10010410 +0 SECTION LOCAL +DEFAULT +10 
  +11: 0+10010448 +0 SECTION LOCAL +DEFAULT +11 
  +12: 0+10010448 +0 SECTION LOCAL +DEFAULT +12 
  +13: 0+10010598 +0 SECTION LOCAL +DEFAULT +13 
- +14: 0+10010598 +0 SECTION LOCAL +DEFAULT +14 
- +15: 0+10010598 +0 SECTION LOCAL +DEFAULT +15 
- +16: 0+100105a0 +0 SECTION LOCAL +DEFAULT +16 
- +17: 0+100105f0 +0 SECTION LOCAL +DEFAULT +17 
- +18: 0+100105f0 +0 SECTION LOCAL +DEFAULT +18 
- +19: 0+10010620 +0 SECTION LOCAL +DEFAULT +19 
+ +14: 0+100105a0 +0 SECTION LOCAL +DEFAULT +14 
+ +15: 0+100105f0 +0 SECTION LOCAL +DEFAULT +15 
+ +16: 0+100105f0 +0 SECTION LOCAL +DEFAULT +16 
+ +17: 0+10010620 +0 SECTION LOCAL +DEFAULT +17 
+ +18: 0+ +0 SECTION LOCAL +DEFAULT +18 
+ +19: 0+ +0 SECTION LOCAL +DEFAULT +19 
  +20: 0+ +0 SECTION LOCAL +DEFAULT +20 
- +21: 0+ +0 SECTION LOCAL +DEFAULT +21 
- +22: 0+ +0 SECTION LOCAL +DEFAULT +22 
- +23: 0+ +0 TLS +LOCAL +DEFAULT +10 gd4
- +24: 0+8 +0 TLS +LOCAL +DEFAULT +10 ld4
- +25: 0+10 +0 TLS +LOCAL +DEFAULT +10 ld5
- +26: 0+18 +0 TLS +LOCAL +DEFAULT +10 ld6
- +27: 0+20 +0 TLS +LOCAL +DEFAULT +10 ie4
- +28: 0+28 +0 TLS +LOCAL +DEFAULT +10 le4
- +29: 0+30 +0 TLS +LOCAL +DEFAULT +10 le5
- +30: 0+100105e8 +0 NOTYPE +LOCAL +DEFAULT +16 \.Lie0
- +31: 0+10010448 +0 OBJECT +GLOBAL DEFAULT +ABS _DYNAMIC
- +32: 0+ +0 TLS +GLOBAL DEFAULT +UND gd
- +33: 0+60 +0 TLS +GLOBAL DEFAULT +11 le0
- +34: 0+ +0 NOTYPE +GLOBAL DEFAULT +UND __tls_get_addr
- +35: 0+ +0 FUNC +GLOBAL DEFAULT +UND \.__tls_get_addr
- +36: 0+40 +0 TLS +GLOBAL DEFAULT +11 ld0
- +37: 0+68 +0 TLS +GLOBAL DEFAULT +11 le1
- +38: 0+ +0 TLS +GLOBAL DEFAULT +UND ld
- +39: 0+1000038c +0 NOTYPE +GLOBAL DEFAULT +7 _start
- +40: 0+10010620 +0 NOTYPE +GLOBAL DEFAULT +ABS __end
- +41: 0+50 +0 TLS +GLOBAL DEFAULT +11 ld2
- +42: 0+48 +0 TLS +GLOBAL DEFAULT +11 ld1
- +43: 0+100105f0 +0 NOTYPE +GLOBAL DEFAULT +ABS __bss_start
- +44: 0+100105f0 +0 NOTYPE +GLOBAL DEFAULT +ABS _edata
- +45: 0+10010620 +0 NOTYPE +GLOBAL DEFAULT +ABS _end
- +46: 0+38 +0 TLS +GLOBAL DEFAULT +11 gd0
- +47: 0+58 +0 TLS +GLOBAL DEFAULT +11 ie0
+ +21: 0+ +0 TLS +LOCAL +DEFAULT +10 gd4
+ +22: 0+8 +0 TLS +LOCAL +DEFAULT +10 ld4
+ +23: 0+10 +0 TLS +LOCAL +DEFAULT +10 ld5
+ +24: 0+18 +0 TLS +LOCAL +DEFAULT +10 ld6
+ +25: 0+20 +0 TLS +LOCAL +DEFAULT +10 ie4
+ +26: 0+28 +0 TLS +LOCAL +DEFAULT +10 le4
+ +27: 0+30 +0 TLS +LOCAL +DEFAULT +10 le5
+ +28: 0+100105e8 +0 NOTYPE +LOCAL +DEFAULT +14 \.Lie0
+ +29: 0+10010448 +0 OBJECT +GLOBAL DEFAULT +ABS _DYNAMIC
+ +30: 0+ +0 TLS +GLOBAL DEFAULT +UND gd
+ +31: 0+60 +0 TLS +GLOBAL DEFAULT +11 le0
+ +32: 0+ +0 NOTYPE +GLOBAL DEFAULT +UND __tls_get_addr
+ +33: 0+ +0 FUNC +GLOBAL DEFAULT +UND \.__tls_get_addr
+ +34: 0+40 +0 TLS +GLOBAL DEFAULT +11 ld0
+ +35: 0+68 +0 TLS +GLOBAL DEFAULT +11 le1
+ +36: 0+ +0 TLS +GLOBAL DEFAULT +UND ld
+ +37: 0+1000036c +0 NOTYPE +GLOBAL DEFAULT +7 _start
+ +38: 0+50 +0 TLS +GLOBAL DEFAULT +11 ld2
+ +39: 0+48 +0 TLS +GLOBAL DEFAULT +11 ld1
+ +40: 0+100105f0 +0 NOTYPE +GLOBAL DEFAULT +ABS __bss_start
+ +41: 0+100105f0 +0 NOTYPE +GLOBAL DEFAULT +ABS _edata
+ +42: 0+10010620 +0 NOTYPE +GLOBAL DEFAULT +ABS _end
+ +43: 0+38 +0 TLS +GLOBAL DEFAULT +11 gd0
+ +44: 0+58 +0 TLS +GLOBAL DEFAULT +11 ie0
