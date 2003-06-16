6f4e5a4121640be19f146b24e5c0d1332d481e00
Andrew Cagney <cagney@redhat.com>
2003-06-16 13:19:36 +0000

    2003-06-16  Andrew Cagney  <cagney@redhat.com>
    	* regcache.c (do_cooked_read): Do not use register_valid_p.

--- gdb/regcache.c
+++ gdb/regcache.c
@@ -423,8 +423,7 @@ static int
 do_cooked_read (void *src, int regnum, void *buf)
 {
   struct regcache *regcache = src;
-  if (!regcache_valid_p (regcache, regnum)
-      && regcache->readonly_p)
+  if (!regcache->register_valid_p[regnum] && regcache->readonly_p)
     /* Don't even think about fetching a register from a read-only
        cache when the register isn't yet valid.  There isn't a target
        from which the register value can be fetched.  */
