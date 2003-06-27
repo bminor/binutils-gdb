c8a2fad7b41a8264964233608ae10c35918243fb
nobody <>
2003-06-27 17:05:59 +0000

    This commit was manufactured by cvs2svn to create branch 'carlton_dictionary-branch'.
    Cherrypick from master 2003-06-27 17:05:58 UTC Elena Zannoni <ezannoni@kwikemart.cygnus.com> '2003-06-27  Elena Zannoni  <ezannoni@redhat.com>':
        gdb/alpha-mdebug-tdep.c
        gdb/config/mips/mips.mt
        gdb/config/mips/mips64.mt
        gdb/config/powerpc/nm-ppc64-linux.h
        gdb/config/powerpc/ppc64-linux.mh
        gdb/dwarf2-frame.c
        gdb/dwarf2-frame.h
        gdb/linux-nat.c
        gdb/linux-nat.h
        gdb/remote-fileio.c
        gdb/remote-fileio.h
        gdb/stack.h
        gdb/testsuite/gdb.asm/alpha.inc
        gdb/testsuite/gdb.base/bang.exp
        gdb/testsuite/gdb.base/fileio.c
        gdb/testsuite/gdb.base/fileio.exp
        gdb/testsuite/gdb.base/gdb_history
        gdb/testsuite/gdb.base/shreloc.c
        gdb/testsuite/gdb.base/shreloc.exp
        gdb/testsuite/gdb.base/shreloc1.c
        gdb/testsuite/gdb.base/shreloc2.c
        gdb/testsuite/gdb.c++/pr-1210.cc
        gdb/testsuite/gdb.c++/pr-1210.exp
        gdb/trad-frame.c
        gdb/trad-frame.h
        include/gdb/fileio.h
        sim/h8300/sim-main.h
        sim/ppc/altivec.igen
        sim/ppc/altivec_expression.h
        sim/ppc/altivec_registers.h
        sim/ppc/e500.igen
        sim/ppc/e500_expression.h
        sim/ppc/e500_registers.h

---
+++ gdb/trad-frame.h
@@ -0,0 +1,70 @@
+/* Traditional frame unwind support, for GDB the GNU Debugger.
+
+   Copyright 2003 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#ifndef TRAD_FRAME_H
+#define TRAD_FRAME_H
+
+struct frame_info;
+
+/* A traditional saved regs table, indexed by REGNUM, encoding where
+   the value of REGNUM for the previous frame can be found in this
+   frame.
+
+   The table is initialized with an identity encoding (ADDR == 0,
+   REALNUM == REGNUM) indicating that the value of REGNUM in the
+   previous frame can be found in register REGNUM (== REALNUM) in this
+   frame.
+
+   The initial encoding can then be changed:
+
+   Modify ADDR (REALNUM >= 0, ADDR != 0) to indicate that the value of
+   register REGNUM in the previous frame can be found in memory at
+   ADDR in this frame.
+
+   Modify REALNUM (REALNUM >= 0, ADDR == 0) to indicate that the value
+   of register REGNUM in the previous frame is found in register
+   REALNUM in this frame.
+
+   Call trad_frame_register_value (REALNUM < 0) to indicate that the
+   value of register REGNUM in the previous frame is found in ADDR.  */
+
+struct trad_frame_saved_reg
+{
+  LONGEST addr; /* A CORE_ADDR fits in a longest.  */
+  int realnum;
+};
+
+/* Convenience function, encode REGNUM's location in the trad-frame.  */
+void trad_frame_register_value (struct trad_frame_saved_reg this_saved_regs[],
+				int regnum, LONGEST val);
+
+/* Return a freshly allocated (and initialized) trad_frame array.  */
+struct trad_frame_saved_reg *trad_frame_alloc_saved_regs (struct frame_info *next_frame);
+
+/* Given the trad_frame info, return the location of the specified
+   register.  */
+void trad_frame_prev_register (struct frame_info *next_frame,
+			       struct trad_frame_saved_reg this_saved_regs[],
+			       int regnum, int *optimizedp,
+			       enum lval_type *lvalp, CORE_ADDR *addrp,
+			       int *realnump, void *bufferp);
+
+#endif
