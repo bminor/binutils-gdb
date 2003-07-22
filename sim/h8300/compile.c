e22fef83d72c23935e2bca2bc71e87e7c911a229
Michael Snyder <msnyder@vmware.com>
2003-07-22 19:07:30 +0000

    2003-07-22  Michael Snyder  <msnyder@redhat.com>
            * compile.c (sim_resume): Revert 6-24 change, it does not
            work with gdb breakpoints.

--- sim/h8300/compile.c
+++ sim/h8300/compile.c
@@ -3577,12 +3577,14 @@ sim_resume (SIM_DESC sd, int step, int siggnal)
 	      sim_engine_set_run_state (sd, sim_exited, 
 					SIM_WEXITSTATUS (h8_get_reg (sd, 0)));
 	    }
+#if 0
 	  else if (SIM_WIFSTOPPED (h8_get_reg (sd, 0)))
 	    {
 	      /* Pass the stop signal up to gdb.  */
 	      sim_engine_set_run_state (sd, sim_stopped, 
 					SIM_WSTOPSIG (h8_get_reg (sd, 0)));
 	    }
+#endif
 	  else
 	    {
 	      /* Treat it as a sigtrap.  */
