c945b932a44c97019e76ca76b5444f3207ee13de
Mark Kettenis <kettenis@gnu.org>
2003-08-17 23:18:31 +0000

    * gdb.arch/i386-prologue.exp: Add testcase for PR backtrace/1338. * gdb.arch/i386-prologue.c (gdb1338): Add function.

--- gdb/testsuite/gdb.arch/i386-prologue.c
+++ gdb/testsuite/gdb.arch/i386-prologue.c
@@ -1,13 +1,15 @@
 void gdb1253 (void);
+void gdb1338 (void);
 
 int
 main (void)
 {
   gdb1253 ();
+  gdb1338 ();
   return 0;
 }
 
-/* Relevant part of the prologue from gdb/1253.  */
+/* Relevant part of the prologue from symtab/1253.  */
 
 asm(".text\n"
     "    .align 8\n"
@@ -18,4 +20,18 @@ asm(".text\n"
     "    pushl %edi\n"
     "    int   $0x03\n"
     "    leave\n"
+    "    ret\n");
+
+/* Relevant part of the prologue from backtrace/1338.  */
+
+asm(".text\n"
+    "    .align 8\n"
+    "gdb1338:\n"
+    "    pushl %edi\n"
+    "    pushl %esi\n"
+    "    pushl %ebx\n"
+    "    int   $0x03\n"
+    "    popl  %ebx\n"
+    "    popl  %esi\n"
+    "    popl  %edi\n"
     "    ret\n");
