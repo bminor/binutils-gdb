66903e97131aaa2dbb70f4dda9009be7dbedd1be
Christopher Faylor <me+cygwin@cgf.cx>
2003-09-13 02:26:52 +0000

    * win32-nat.c: Reorganize so that defines used by target headers are actually defined by the system headers. * config/i386/tm-cygwin.h: Check for CONTEXT_EXTENDED_REGISTERS rather than HAVE_CONTEXT_EXTENDED_REGISTERS, since the latter actually exists.

--- gdb/config/i386/tm-cygwin.h
+++ gdb/config/i386/tm-cygwin.h
@@ -20,7 +20,7 @@
    Boston, MA 02111-1307, USA.  */
 
 /* Use SSE registers if winnt.h contains information about them.  */
-#ifdef HAVE_CONTEXT_EXTENDED_REGISTERS
+#ifdef CONTEXT_EXTENDED_REGISTERS
 #define HAVE_SSE_REGS
 #else
 #undef HAVE_SSE_REGS
