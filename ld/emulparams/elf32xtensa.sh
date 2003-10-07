398b851510528409aed200e40b8d3e52ab2182fd
Bob Wilson <bob.wilson@acm.org>
2003-10-07 23:30:20 +0000

    * emulparams/elf32xtensa.sh (OTHER_READONLY_SECTIONS, 	OTHER_READWRITE_SECTIONS, OTHER_SECTIONS): Set section addresses to 	zero for relocatable links.

--- ld/emulparams/elf32xtensa.sh
+++ ld/emulparams/elf32xtensa.sh
@@ -15,18 +15,18 @@ MACHINE=
 GENERATE_SHLIB_SCRIPT=yes
 GENERATE_COMBRELOC_SCRIPT=yes
 NO_SMALL_DATA=yes
-OTHER_READONLY_SECTIONS='
-  .got.loc : { *(.got.loc) }
-  .xt_except_table : { KEEP (*(.xt_except_table)) }
-'
+OTHER_READONLY_SECTIONS="
+  .got.loc      ${RELOCATING-0} : { *(.got.loc) }
+  .xt_except_table ${RELOCATING-0} : { KEEP (*(.xt_except_table)) }
+"
 OTHER_READWRITE_SECTIONS="
-  .xt_except_desc :
+  .xt_except_desc ${RELOCATING-0} :
   {
     *(.xt_except_desc${RELOCATING+ .gnu.linkonce.h.*})
     ${RELOCATING+*(.xt_except_desc_end)}
   }
 "
 OTHER_SECTIONS="
-  .xt.lit : { *(.xt.lit${RELOCATING+ .xt.lit.* .gnu.linkonce.p.*}) }
-  .xt.insn : { *(.xt.insn${RELOCATING+ .gnu.linkonce.x.*}) }
+  .xt.lit         0 : { *(.xt.lit${RELOCATING+ .xt.lit.* .gnu.linkonce.p.*}) }
+  .xt.insn        0 : { *(.xt.insn${RELOCATING+ .gnu.linkonce.x.*}) }
 "
