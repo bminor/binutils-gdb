89b7466f7671017df5609bcbfcf2e6d836517535
Hans-Peter Nilsson <hp@axis.com>
2003-10-18 16:05:02 +0000

    * ld-mmix/greg-14.d, ld-mmix/greg-5.d, ld-mmix/pushja1b.d, 	ld-mmix/pushja1f.d, ld-mmix/pushja7b.d, ld-mmix/pushja7f.d: Pass 	--no-pushj-stubs to the assembler. 	* ld-mmix/pushjs2b.d, ld-mmix/greg-14s.d, ld-mmix/pushjs1.d, 	ld-mmix/greg-5s.d, ld-mmix/pushjs3b.d, ld-mmix/pushja1f-s.d, 	ld-mmix/pushjs1m.d, ld-mmix/pushja7b-s.d, ld-mmix/pushja7f-s.d, 	ld-mmix/pushja1b-s.d, ld-mmix/pushjs2.d, ld-mmix/pushjs3.d, 	ld-mmix/pushjs4b.d, ld-mmix/pushjs4.d, ld-mmix/pushjs1bm.d, 	ld-mmix/pushjs1b.d, ld-mmix/pushjs2m.d, ld-mmix/pushjs1r.d, 	ld-mmix/pushjs3m.d, ld-mmix/pushjs2bm.d, ld-mmix/pushjs4m.d, 	ld-mmix/pushjs3bm.d, ld-mmix/pushjs2r.d, ld-mmix/pushjs4bm.d, 	ld-mmix/pushjs3r.d, ld-mmix/pushjs4r.d: New tests.

--- ld/testsuite/ld-mmix/greg-5s.d
+++ ld/testsuite/ld-mmix/greg-5s.d
@@ -2,14 +2,13 @@
 #source: gregpsj1.s
 #source: start.s
 #source: a.s
-#as: -x --no-pushj-stubs
+#as: -x
 #ld: -m elf64mmix
 #objdump: -dt
 
 # Like greg-3, but a different expanding insn.
 
 .*:     file format elf64-mmix
-
 SYMBOL TABLE:
 0+ l    d  .text	0+ 
 2000000000000000 l    d  .data	0+ 
@@ -19,22 +18,14 @@ SYMBOL TABLE:
 0+ l    d  \*ABS\*	0+ 
 0+ l    d  \*ABS\*	0+ 
 0+ l    d  \*ABS\*	0+ 
-0+14 g       \.text	0+ _start
+0+4 g       \.text	0+ _start
 0+fe g       \*REG\*	0+ areg
 #...
-0+18 g       \.text	0+ a
-
+0+8 g       \.text	0+ a
 Disassembly of section \.text:
-
-0+ <_start-0x14>:
-   0:	e3ff0018 	setl \$255,0x18
-   4:	e6ff0000 	incml \$255,0x0
-   8:	e5ff0000 	incmh \$255,0x0
-   c:	e4ff0000 	inch \$255,0x0
-  10:	bffeff00 	pushgo \$254,\$255,0
-
-0+14 <_start>:
-  14:	e3fd0001 	setl \$253,0x1
-
-0+18 <a>:
-  18:	e3fd0004 	setl \$253,0x4
+0+ <_start-0x4>:
+   0:	f2fe0002 	pushj \$254,8 <a>
+0+4 <_start>:
+   4:	e3fd0001 	setl \$253,0x1
+0+8 <a>:
+   8:	e3fd0004 	setl \$253,0x4
