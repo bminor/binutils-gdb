3314fb38c10588b178ec7a02b6d03d40590962b4
Hans-Peter Nilsson <hp@axis.com>
2003-10-18 16:00:21 +0000

    * gas/mmix/op-0-1.d, gas/mmix/pushj-c.d, gas/mmix/weak1.d: Add 	--no-pushj-stubs option. 	* gas/mmix/reloclab-r.d, gas/mmix/reloclab.d: Add --no-stubs 	option. 	* gas/mmix/reloclab.l: Adjust for PUSHJ stub. 	* gas/mmix/pushj-cs.d, gas/mmix/reloclab-s.d, gas/mmix/relax2.s, 	gas/mmix/relax2.d, gas/mmix/op-0-1s.d, gas/mmix/reloclab-rs.d, 	gas/mmix/weak1-s.d: New tests.

--- gas/testsuite/gas/mmix/weak1-s.d
+++ gas/testsuite/gas/mmix/weak1-s.d
@@ -1,23 +1,19 @@
-#as: -x --no-pushj-stubs
+#as: -x
+#source: weak1.s
 #objdump: -str
 
-# Relaxation thought a weak symbol was within reach.
+# Like weak1, but with PUSHJ stubs.
 
 .*:     file format elf64-mmix
-
 SYMBOL TABLE:
 0+ l    d  \.text	0+ 
 0+ l    d  \.data	0+ 
 0+ l    d  \.bss	0+ 
 0+  w      \.text	0+ foo
 0+4 g       \.text	0+ main
-
 RELOCATION RECORDS FOR \[\.text\]:
 OFFSET           TYPE              VALUE 
-0+18 R_MMIX_64         foo
-0+4 R_MMIX_PUSHJ      foo
-
+0+8 R_MMIX_64         foo
+0+4 R_MMIX_PUSHJ_STUBBABLE  foo
 Contents of section \.text:
- 0000 f8010000 f20f0000 fd000000 fd000000  .*
- 0010 fd000000 fd000000 00000000 00000000  .*
-
+ 0000 f8010000 f20f0000 00000000 00000000  .*
