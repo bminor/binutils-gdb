853d6e5b6d55ee6b7d5d36f31d809f97d4ea99fc
Andrew Cagney <cagney@redhat.com>
2003-11-17 15:00:20 +0000

    2003-11-17  Andrew Cagney  <cagney@redhat.com>
    	* lib/gdb.exp (compiler_info): New global.
    	(test_compiler_info): New function.
    	(get_compiler_info): Set compiler_info.
    	* lib/compiler.c, lib/compiler.cc: Add copyright.  When GNUC, set
    	"compiler_info" to gcc-<major>-<minor>.

--- gdb/testsuite/lib/compiler.cc
+++ gdb/testsuite/lib/compiler.cc
@@ -1,3 +1,24 @@
+/* This test file is part of GDB, the GNU debugger.
+
+   Copyright 1995, 1999, 2003 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+ 
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+   Please email any bugs, comments, and/or additions to this file to:
+   bug-gdb@prep.ai.mit.edu  */
+
 /* Often the behavior of any particular test depends upon what compiler was
    used to compile the test.  As each test is compiled, this file is
    preprocessed by the same compiler used to compile that specific test
@@ -27,8 +48,10 @@ set supports_template_debugging 0
 
 #if defined (__GNUC__)
 set gcc_compiled __GNUC__
+set compiler_info [join {gcc __GNUC__ __GNUC_MINOR__ } -]
 #else
 set gcc_compiled 0
+set compiler_info ""
 #endif
 
 return 0
