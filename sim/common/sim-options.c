a69146da95175ae2ab5043397ddf22db85302ed9
Kazu Hirata <kazu@codesourcery.com>
2003-11-22 21:37:49 +0000

    * sim-options.c (standard_options): Fix the names of H8 	variants.

--- sim/common/sim-options.c
+++ sim/common/sim-options.c
@@ -154,13 +154,13 @@ static const OPTION standard_options[] =
 
 #ifdef SIM_H8300 /* FIXME: Should be movable to h8300 dir.  */
   { {"h8300h", no_argument, NULL, OPTION_H8300H},
-      'h', NULL, "Indicate the CPU is h8/300h",
+      'h', NULL, "Indicate the CPU is H8/300H",
       standard_option_handler },
   { {"h8300s", no_argument, NULL, OPTION_H8300S},
-      'S', NULL, "Indicate the CPU is h8/300s",
+      'S', NULL, "Indicate the CPU is H8S",
       standard_option_handler },
   { {"h8300sx", no_argument, NULL, OPTION_H8300SX},
-      'x', NULL, "Indicate the CPU is h8/300sx",
+      'x', NULL, "Indicate the CPU is H8SX",
       standard_option_handler },
 #endif
 
