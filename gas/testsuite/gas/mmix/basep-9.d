accf8041ebc2b800308379b3d312bfa095ff52b4
Alan Modra <amodra@gmail.com>
2003-12-13 23:32:25 +0000

    * gas/mmix/basep-8.d: Update for lcomm symbol type change. 	* gas/mmix/basep-9.d: Likewise.

--- gas/testsuite/gas/mmix/basep-9.d
+++ gas/testsuite/gas/mmix/basep-9.d
@@ -6,8 +6,8 @@ SYMBOL TABLE:
 0+ l    d  \.text	0+ 
 0+ l    d  \.data	0+ 
 0+ l    d  \.bss	0+ 
-0+ l       \.bss	0+4 comm_symbol3
-0+4 l       \.bss	0+4 comm_symbol4
+0+ l     O \.bss	0+4 comm_symbol3
+0+4 l     O \.bss	0+4 comm_symbol4
 0+ l    d  \.MMIX\.reg_contents	0+ 
 0+4       O \*COM\*	0+4 comm_symbol1
 
