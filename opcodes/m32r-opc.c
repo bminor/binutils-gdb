a711c44f29cfb1af925157ef24ec9254ccd0e68a
Nick Clifton <nickc@redhat.com>
2003-12-15 12:19:13 +0000

    Add PIPE_O attribute to "pop" instruction.

--- opcodes/m32r-opc.c
+++ opcodes/m32r-opc.c
@@ -1413,7 +1413,7 @@ static const CGEN_IBASE m32r_cgen_macro_insn_table[] =
 /* pop $dr */
   {
     -1, "pop", "pop", 16,
-    { 0|A(ALIAS), { (1<<MACH_BASE), PIPE_NONE } }
+    { 0|A(ALIAS), { (1<<MACH_BASE), PIPE_O } }
   },
 /* ldi $dr,$simm8 */
   {
