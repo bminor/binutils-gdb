bffe1ece0e72e008cdc9813e1f0a2517ef0ff2e7
Jeff Johnston <jjohnstn@redhat.com>
2003-12-17 22:21:02 +0000

    2003-12-17  Jeff Johnston  <jjohnstn@redhat.com>
            * symtab.c (decode_line_spec): Change call to decode_line_1.

--- gdb/symtab.c
+++ gdb/symtab.c
@@ -3868,7 +3868,7 @@ decode_line_spec (char *string, int funfirstline)
   
   sals = decode_line_1 (&string, funfirstline,
 			cursal.symtab, cursal.line,
-			(char ***) NULL);
+			(char ***) NULL, NULL);
 
   if (*string)
     error ("Junk at end of line specification: %s", string);
