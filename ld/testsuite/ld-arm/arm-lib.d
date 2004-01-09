c787b889467a41263df4f73cad327af7f3a46373
Daniel Jacobowitz <drow@false.org>
2004-01-09 16:53:10 +0000

    * ld-arm/arm-lib.d, ld-arm/arm-lib.r: Update for R_ARM_PLT32 	changes.

--- ld/testsuite/ld-arm/arm-lib.d
+++ ld/testsuite/ld-arm/arm-lib.d
@@ -4,12 +4,23 @@ architecture: arm, flags 0x00000150:
 HAS_SYMS, DYNAMIC, D_PAGED
 start address 0x.*
 
+Disassembly of section .plt:
+
+.* <.plt>:
+ .*:	e52de004 	str	lr, \[sp, #-4\]!
+ .*:	e59fe004 	ldr	lr, \[pc, #4\]	; .* <lib_func1-0x10>
+ .*:	e08fe00e 	add	lr, pc, lr
+ .*:	e5bef008 	ldr	pc, \[lr, #8\]!
+ .*:	.*
+ .*:	e28fc6.* 	add	ip, pc, #.*	; 0x.*
+ .*:	e28cca.* 	add	ip, ip, #.*	; 0x.*
+ .*:	e5bcf.* 	ldr	pc, \[ip, #.*\]!
 Disassembly of section .text:
 
 .* <lib_func1>:
  .*:	e1a0c00d 	mov	ip, sp
  .*:	e92dd800 	stmdb	sp!, {fp, ip, lr, pc}
- .*:	ebfffffe 	bl	.* <lib_func1\+0x8>
+ .*:	ebfffff9 	bl	.* <lib_func1-0xc>
  .*:	e89d6800 	ldmia	sp, {fp, sp, lr}
  .*:	e12fff1e 	bx	lr
 
--- ld/testsuite/ld-arm/arm-lib.r
+++ ld/testsuite/ld-arm/arm-lib.r
@@ -3,6 +3,6 @@ tmpdir/arm-lib.so:     file format elf32-littlearm
 
 DYNAMIC RELOCATION RECORDS
 OFFSET   TYPE              VALUE 
-.* R_ARM_PC24        app_func2
+.* R_ARM_JUMP_SLOT   app_func2
 
 
