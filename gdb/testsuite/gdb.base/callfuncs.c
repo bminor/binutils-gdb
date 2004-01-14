a476ccc9d74603a2dacfe0f6e7da5bd09e2da0b1
Michael Chastain <mec@google.com>
2004-01-14 14:32:54 +0000

    2004-01-14  Michael Chastain  <mec.gnu@mindspring.com>
    	* gdb.base/callfuncs.c: Remove explicit declaration of malloc.

--- gdb/testsuite/gdb.base/callfuncs.c
+++ gdb/testsuite/gdb.base/callfuncs.c
@@ -1,6 +1,6 @@
 /* This testcase is part of GDB, the GNU debugger.
 
-   Copyright 1993, 1994, 1995, 1998, 1999, 2000, 2001
+   Copyright 1993, 1994, 1995, 1998, 1999, 2000, 2001, 2004
    Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
@@ -379,7 +379,7 @@ int a, b;
 
 /* Gotta have a main to be able to generate a linked, runnable
    executable, and also provide a useful place to set a breakpoint. */
-extern void * malloc() ;
+
 int main ()
 {
 #ifdef usestubs
