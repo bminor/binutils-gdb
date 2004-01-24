ad27498312ec1446a651cb56a03a5fe54849f0b4
Nick Roberts <nickrob@snap.net.nz>
2004-01-24 04:22:51 +0000

    Update copyright year.

--- gdb/mi/mi-cmds.h
+++ gdb/mi/mi-cmds.h
@@ -1,6 +1,6 @@
 /* MI Command Set for GDB, the GNU debugger.
 
-   Copyright 2000, 2003 Free Software Foundation, Inc.
+   Copyright 2000, 2003, 2004 Free Software Foundation, Inc.
 
    Contributed by Cygnus Solutions (a Red Hat company).
 
