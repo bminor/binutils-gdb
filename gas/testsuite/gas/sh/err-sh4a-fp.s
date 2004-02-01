1b5d1a53bd6c3de1564d287956e215dc2b5f9b00
Kaz Kojima <kkojima@rr.iij4u.or.jp>
2004-02-01 22:59:43 +0000

    * gas/sh/sh4a-fp.d: Fix opcode name fssra to fsrra. 	* gas/sh/sh4a-fp.s: Likewise. 	* gas/sh/err-sh4a-fp.s: Likewise.

--- gas/testsuite/gas/sh/err-sh4a-fp.s
+++ gas/testsuite/gas/sh/err-sh4a-fp.s
@@ -5,10 +5,10 @@
 
 	fpchg	fpul		! { dg-error "excess operands" }
 
-	fssra	fr1, fr2	! { dg-error "excess operands" }
-	fssra			! { dg-error "invalid operands|missing operand" }
-	fssra	fpul		! { dg-error "invalid operands" }
-	fssra	dr0, dr2	! { dg-error "invalid operands" }
+	fsrra	fr1, fr2	! { dg-error "excess operands" }
+	fsrra			! { dg-error "invalid operands|missing operand" }
+	fsrra	fpul		! { dg-error "invalid operands" }
+	fsrra	dr0, dr2	! { dg-error "invalid operands" }
 
 	fsca	dr0, fpul	! { dg-error "invalid operands" }
 	fsca	fpul, fr0	! { dg-error "invalid operands" }
