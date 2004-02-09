f38069161116907b870668f979f81c8dc5bba0d9
Daniel Jacobowitz <drow@false.org>
2004-02-09 18:06:20 +0000

    * emulparams/armelf.sh, emulparams/armelf_linux.sh: Move 	.note.gnu.arm.ident to after allocated sections.  Mark its 	address as 0.

--- ld/emulparams/armelf_linux.sh
+++ ld/emulparams/armelf_linux.sh
@@ -11,9 +11,9 @@ GENERATE_SHLIB_SCRIPT=yes
 
 DATA_START_SYMBOLS='__data_start = . ;';
 OTHER_TEXT_SECTIONS='*(.glue_7t) *(.glue_7)'
-OTHER_READONLY_SECTIONS='.note.gnu.arm.ident : { KEEP (*(.note.gnu.arm.ident)) }'
 OTHER_BSS_SYMBOLS='__bss_start__ = .;'
 OTHER_BSS_END_SYMBOLS='_bss_end__ = . ; __bss_end__ = . ; __end__ = . ;'
+OTHER_SECTIONS='.note.gnu.arm.ident 0 : { KEEP (*(.note.gnu.arm.ident)) }'
 
 TEXT_START_ADDR=0x00008000
 
