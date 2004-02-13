4dde8e6152ff6e045f3c774911698703daf20184
Andreas Jaeger <aj@suse.de>
2004-02-13 16:12:47 +0000

    2004-02-13  Hannes Reinecke  <hare@suse.de>             Jakub Jelinek  <jakub@redhat.com>
    	* dwarf2dbg.c (get_filenum): Do not read beyond allocated memory.

--- gas/dwarf2dbg.c
+++ gas/dwarf2dbg.c
@@ -1,5 +1,5 @@
 /* dwarf2dbg.c - DWARF2 debug support
-   Copyright 1999, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
+   Copyright 1999, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
    Contributed by David Mosberger-Tang <davidm@hpl.hp.com>
 
    This file is part of GAS, the GNU Assembler.
@@ -372,7 +372,7 @@ get_filenum (const char *filename, unsigned int num)
     {
       --dir_len;
       for (dir = 1; dir < dirs_in_use; ++dir)
-	if (memcmp (filename, dirs[dir], dir_len) == 0
+	if (strncmp (filename, dirs[dir], dir_len) == 0
 	    && dirs[dir][dir_len] == '\0')
 	  break;
 
