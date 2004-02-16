fd33e6cbf81f2bc8f7332e06286634f59f1d57b3
Andrew Cagney <cagney@redhat.com>
2004-02-16 19:43:12 +0000

    2004-02-16  Andrew Cagney  <cagney@redhat.com>
    	* printcmd.c (display_command): Check that EXP isn't NULL.  Fix
    	suggested by Joshua Neuheisel

--- gdb/printcmd.c
+++ gdb/printcmd.c
@@ -1364,7 +1364,7 @@ display_command (char *exp, int from_tty)
 #if defined(TUI)
   /* NOTE: cagney/2003-02-13 The `tui_active' was previously
      `tui_version'.  */
-  if (tui_active && *exp == '$')
+  if (tui_active && exp != NULL && *exp == '$')
     display_it = (tui_set_layout_for_display_command (exp) == TUI_FAILURE);
 #endif
 
