f6fe98efdcadbe85913d89d0d67f6f9d81becb50
Daniel Jacobowitz <drow@false.org>
2004-02-21 02:13:35 +0000

    * dwarf2read.c (add_partial_symbol): Fix typo in adding enumerators 	to the partial symbol table.

--- gdb/dwarf2read.c
+++ gdb/dwarf2read.c
@@ -1562,8 +1562,8 @@ add_partial_symbol (struct partial_die_info *pdi,
       add_psymbol_to_list (actual_name, strlen (actual_name),
 			   VAR_DOMAIN, LOC_CONST,
 			   cu->language == language_cplus
-			   ? &objfile->static_psymbols
-			   : &objfile->global_psymbols,
+			   ? &objfile->global_psymbols
+			   : &objfile->static_psymbols,
 			   0, (CORE_ADDR) 0, cu->language, objfile);
       break;
     default:
