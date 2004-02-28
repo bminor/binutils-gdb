6e1d7d6c23e0e2e6250c8d0e2e650b00613fd1be
Andrew Cagney <cagney@redhat.com>
2004-02-28 16:56:12 +0000

    2004-02-28  Andrew Cagney  <cagney@redhat.com>
    	* breakpoint.c (_initialize_breakpoint): Use
    	"add_setshow_auto_boolean_cmd.  Better word-wrap help messages.
    	Add help to show command.

--- gdb/breakpoint.c
+++ gdb/breakpoint.c
@@ -8078,15 +8078,20 @@ pending breakpoint behavior",
 		  &breakpoint_show_cmdlist, "show breakpoint ",
 		  0/*allow-unknown*/, &showlist);
 
-  add_setshow_cmd ("pending", no_class, var_auto_boolean,
-		   (char *) &pending_break_support,
-		   "Set debugger's behavior regarding pending breakpoints.\n\
-If on, an unrecognized breakpoint location will cause gdb to create a pending\n\
-breakpoint.  If off, an unrecognized breakpoint location results in an error.\n\
-If auto, an unrecognized breakpoint location results in a user-query to see if\n\
-a pending breakpoint should be created.","\
-Show debugger's behavior regarding pending breakpoints.",
-		   NULL, NULL, &breakpoint_set_cmdlist, &breakpoint_show_cmdlist);
+  add_setshow_auto_boolean_cmd ("pending", no_class, &pending_break_support, "\
+Set debugger's behavior regarding pending breakpoints.\n\
+If on, an unrecognized breakpoint location will cause gdb to create a\n\
+pending breakpoint.  If off, an unrecognized breakpoint location results in\n\
+an error.  If auto, an unrecognized breakpoint location results in a\n\
+user-query to see if a pending breakpoint should be created.","\
+Show debugger's behavior regarding pending breakpoints.\n\
+If on, an unrecognized breakpoint location will cause gdb to create a\n\
+pending breakpoint.  If off, an unrecognized breakpoint location results in\n\
+an error.  If auto, an unrecognized breakpoint location results in a\n\
+user-query to see if a pending breakpoint should be created.",
+				NULL, NULL,
+				&breakpoint_set_cmdlist,
+				&breakpoint_show_cmdlist);
 
   pending_break_support = AUTO_BOOLEAN_AUTO;
 }
