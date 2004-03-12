26f07657db77ad7bbce56581809fc5ac76379353
Michal Ludvig <mludvig@suse.cz>
2004-03-12 13:37:38 +0000

    2004-03-12  Michal Ludvig  <mludvig@suse.cz>
    	* gas/i386/padlock.s, gas/i386/padlock.d: New tests for
    	VIA PadLock instructions.
    	* gas/i386/i386.exp: Run padlock tests.

---
+++ gas/testsuite/gas/i386/padlock.s
@@ -0,0 +1,16 @@
+# VIA Nehemiah PadLock instructions
+
+	.text
+foo:
+	xstorerng
+	rep xstorerng
+	xcryptecb
+	rep xcryptecb
+	xcryptcbc
+	rep xcryptcbc
+	xcryptcfb
+	rep xcryptcfb
+	xcryptofb
+	rep xcryptofb
+	xstore
+	rep xstore
