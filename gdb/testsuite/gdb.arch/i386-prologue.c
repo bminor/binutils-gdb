a8958849c4e86363003ed25c2bf1e7685fd7bf36
Mark Kettenis <kettenis@gnu.org>
2004-04-29 18:05:31 +0000

    * i386-prologue.exp: Add testcase for jump instruction as first instruction of the real code. * i386-prologue.c (jump_at_beginning): New function.

--- gdb/testsuite/gdb.arch/i386-prologue.c
+++ gdb/testsuite/gdb.arch/i386-prologue.c
@@ -1,11 +1,13 @@
 void gdb1253 (void);
 void gdb1338 (void);
+void jump_at_beginning (void);
 
 int
 main (void)
 {
   gdb1253 ();
   gdb1338 ();
+  jump_at_beginning ();
   return 0;
 }
 
@@ -34,4 +36,20 @@ asm(".text\n"
     "    popl  %ebx\n"
     "    popl  %esi\n"
     "    popl  %edi\n"
+    "    ret\n");
+
+/* The purpose of this function is to verify that, during prologue
+   skip, GDB does not follow a jump at the beginnning of the "real"
+   code.  */
+
+asm(".text\n"
+    "    .align 8\n"
+    "jump_at_beginning:\n"
+    "    pushl %ebp\n"
+    "    movl  %esp,%ebp\n"
+    "    jmp   .gdbjump\n"
+    "    nop\n"
+    ".gdbjump:\n"
+    "    movl  %ebp,%esp\n"
+    "    popl  %ebp\n"
     "    ret\n");
