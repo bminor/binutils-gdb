e492f9d624bd89cb1a3c3d4998a5a630d2553e11
Andrew Cagney <cagney@redhat.com>
2004-04-30 20:58:32 +0000

    2004-04-30  Andrew Cagney  <cagney@redhat.com>
    	* blockframe.c (find_pc_partial_function): Delete use of
    	DEPRECATED_PC_IN_SIGTRAMP, DEPRECATED_SIGTRAMP_START and
    	DEPRECATED_SIGTRAMP_END.

--- gdb/blockframe.c
+++ gdb/blockframe.c
@@ -352,23 +352,6 @@ find_pc_partial_function (CORE_ADDR pc, char **name, CORE_ADDR *address,
       && section == cache_pc_function_section)
     goto return_cached_value;
 
-  /* If sigtramp is in the u area, it counts as a function (especially
-     important for step_1).  */
-  /* NOTE: cagney/2004-03-16: Determining if the PC is in a signal
-     trampoline typically depends on the detailed analysis of dynamic
-     information obtained from the inferior yet this function is
-     expected to work using static information obtained from the
-     symbol table.  */
-  if (DEPRECATED_SIGTRAMP_START_P ()
-      && DEPRECATED_PC_IN_SIGTRAMP (mapped_pc, (char *) NULL))
-    {
-      cache_pc_function_low = DEPRECATED_SIGTRAMP_START (mapped_pc);
-      cache_pc_function_high = DEPRECATED_SIGTRAMP_END (mapped_pc);
-      cache_pc_function_name = "<sigtramp>";
-      cache_pc_function_section = section;
-      goto return_cached_value;
-    }
-
   msymbol = lookup_minimal_symbol_by_pc_section (mapped_pc, section);
   pst = find_pc_sect_psymtab (mapped_pc, section);
   if (pst)
