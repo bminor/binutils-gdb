2f2f4511bdcb69e610a446890e8110380b76937d
Kaz Kojima <kkojima@rr.iij4u.or.jp>
2004-05-12 05:58:09 +0000

    * ld-sh/tlsbin-2.d: Adjust for section reordering changes 	and removal of unneeded STT_SECTION symbols from .dynsym. 	* ld-sh/tlsbin-3.d: Likewise. 	* ld-sh/tlspic-2.d: Likewise.

--- ld/testsuite/ld-sh/tlsbin-2.d
+++ ld/testsuite/ld-sh/tlsbin-2.d
@@ -18,12 +18,11 @@ Section Headers:
   \[ 6\] \.rela\.plt +.*
   \[ 7\] \.plt +.*
   \[ 8\] \.text +PROGBITS +0+401000 .*
-  \[ 9\] \.data +.*
-  \[10\] \.tdata +PROGBITS +0+413000 [0-9a-f]+ 0+018 00 WAT  0   0  4
-  \[11\] \.tbss +NOBITS +0+413018 [0-9a-f]+ 0+010 00 WAT  0   0  1
-  \[12\] \.dynamic +DYNAMIC +0+413018 .*
-#...
-  \[[0-9a-f]+\] \.got +PROGBITS +0+4130b8 .*
+  \[ 9\] \.tdata +PROGBITS +0+413000 [0-9a-f]+ 0+018 00 WAT  0   0  4
+  \[10\] \.tbss +NOBITS +0+413018 [0-9a-f]+ 0+010 00 WAT  0   0  1
+  \[11\] \.dynamic +DYNAMIC +0+413018 .*
+  \[12\] \.data +.*
+  \[[0-9a-f]+\] \.got +PROGBITS +0+414000 .*
   \[[0-9a-f]+\] \.sbss +.*
   \[[0-9a-f]+\] \.bss +.*
 #...
@@ -60,14 +59,14 @@ Program Headers:
 
 Relocation section '\.rela\.dyn' at offset 0x[0-9a-f]+ contains 4 entries:
  Offset +Info +Type +Sym\.Value +Sym\. Name \+ Addend
-0+4130c8  00000197 R_SH_TLS_TPOFF32 +0+ +sG3 \+ 0
-0+4130cc  00000397 R_SH_TLS_TPOFF32 +0+ +sG2 \+ 0
-0+4130d0  00000497 R_SH_TLS_TPOFF32 +0+ +sG4 \+ 0
-0+4130d4  0000[0-9a-f]+97 R_SH_TLS_TPOFF32 +0+ +sG1 \+ 0
+0+414010  00000197 R_SH_TLS_TPOFF32 +0+ +sG3 \+ 0
+0+414014  00000397 R_SH_TLS_TPOFF32 +0+ +sG2 \+ 0
+0+414018  00000497 R_SH_TLS_TPOFF32 +0+ +sG4 \+ 0
+0+41401c  0000[0-9a-f]+97 R_SH_TLS_TPOFF32 +0+ +sG1 \+ 0
 
 Relocation section '\.rela\.plt' at offset 0x[0-9a-f]+ contains 1 entries:
  Offset +Info +Type +Sym\.Value +Sym\. Name \+ Addend
-0+4130c4  000005a4 R_SH_JMP_SLOT +[0-9a-f]+ +__tls_get_addr \+ [0-9a-f]+
+0+41400c  000005a4 R_SH_JMP_SLOT +[0-9a-f]+ +__tls_get_addr \+ [0-9a-f]+
 
 Symbol table '\.dynsym' contains [0-9]+ entries:
  +Num: +Value +Size Type +Bind +Vis +Ndx Name
@@ -102,30 +101,29 @@ Symbol table '\.symtab' contains [0-9]+ entries:
  +16: [0-9a-f]+ +0 SECTION LOCAL  DEFAULT +16 *
  +17: [0-9a-f]+ +0 SECTION LOCAL  DEFAULT +17 *
  +18: [0-9a-f]+ +0 SECTION LOCAL  DEFAULT +18 *
-#...
- +[0-9]+: 00000008 +0 TLS +LOCAL  DEFAULT +10 sl1
- +[0-9]+: 0000000c +0 TLS +LOCAL  DEFAULT +10 sl2
- +[0-9]+: 00000020 +0 TLS +LOCAL  DEFAULT +11 bl1
- +[0-9]+: 00000024 +0 TLS +LOCAL  DEFAULT +11 bl2
+ +[0-9]+: 00000008 +0 TLS +LOCAL  DEFAULT +9 sl1
+ +[0-9]+: 0000000c +0 TLS +LOCAL  DEFAULT +9 sl2
+ +[0-9]+: 00000020 +0 TLS +LOCAL  DEFAULT +10 bl1
+ +[0-9]+: 00000024 +0 TLS +LOCAL  DEFAULT +10 bl2
  +[0-9]+: 0+ +0 TLS +GLOBAL DEFAULT  UND sG3
  +[0-9]+: [0-9a-f]+ +0 OBJECT  GLOBAL DEFAULT  ABS _DYNAMIC
  +[0-9]+: 0+ +0 TLS +GLOBAL DEFAULT  UND sG2
  +[0-9]+: 0+ +0 TLS +GLOBAL DEFAULT  UND sG4
  +[0-9]+: [0-9a-f]+ +0 FUNC +GLOBAL DEFAULT  UND __tls_get_addr
- +[0-9]+: 0+ +0 TLS +GLOBAL DEFAULT +10 sg1
+ +[0-9]+: 0+ +0 TLS +GLOBAL DEFAULT +9 sg1
  +[0-9]+: 0+402000 +0 FUNC +GLOBAL DEFAULT +8 _start
 #...
  +[0-9]+: 0+401000 +0 FUNC +GLOBAL DEFAULT +8 fn2
 #...
- +[0-9]+: 00000004 +0 TLS +GLOBAL DEFAULT +10 sg2
+ +[0-9]+: 00000004 +0 TLS +GLOBAL DEFAULT +9 sg2
  +[0-9]+: 0+ +0 TLS +GLOBAL DEFAULT  UND sG1
- +[0-9]+: 00000010 +0 TLS +GLOBAL HIDDEN +10 sh1
- +[0-9]+: 004130d8 +0 NOTYPE  GLOBAL DEFAULT  ABS _edata
+ +[0-9]+: 00000010 +0 TLS +GLOBAL HIDDEN +9 sh1
+ +[0-9]+: [0-9a-f]+ +0 NOTYPE  GLOBAL DEFAULT  ABS _edata
  +[0-9]+: [0-9a-f]+ +0 OBJECT  GLOBAL DEFAULT  ABS _GLOBAL_OFFSET_TABLE_
  +[0-9]+: [0-9a-f]+ +0 NOTYPE  GLOBAL DEFAULT  ABS _end
- +[0-9]+: 00000014 +0 TLS +GLOBAL HIDDEN +10 sh2
- +[0-9]+: 0000001c +0 TLS +GLOBAL DEFAULT +11 bg2
- +[0-9]+: 00000018 +0 TLS +GLOBAL DEFAULT +11 bg1
+ +[0-9]+: 00000014 +0 TLS +GLOBAL HIDDEN +9 sh2
+ +[0-9]+: 0000001c +0 TLS +GLOBAL DEFAULT +10 bg2
+ +[0-9]+: 00000018 +0 TLS +GLOBAL DEFAULT +10 bg1
 #...
  +[0-9]+: .*
 #pass
