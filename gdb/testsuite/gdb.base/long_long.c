acd4ad442c0b4668d9d203df51602c55db10805e
Andrew Cagney <cagney@redhat.com>
2004-06-16 15:19:41 +0000

    2004-06-16  Andrew Cagney  <cagney@gnu.org>
    	* gdb.base/long_long.exp, gdb.base/long_long.c: Rewrite.

--- gdb/testsuite/gdb.base/long_long.c
+++ gdb/testsuite/gdb.base/long_long.c
@@ -27,55 +27,74 @@
  * cc +e +DA2.0 -g -o long_long long_long.c
  */
 
-#ifdef PROTOTYPES
-long long callee(long long i)
-#else
-long long callee( i )
-long long i;
-#endif
+enum { MAX_BYTES = 16 };
+
+void
+pack (unsigned char b[MAX_BYTES], int size, int nr)
 {
-   register long long result;
-
-   result  = 0x12345678;
-   result  = result << i;
-   result += 0x9abcdef0;
-
-   return result;
+  static long long val[] = { 0x123456789abcdefLL, 01234567123456701234567LL, 12345678901234567890ULL};
+  volatile static int e = 1;
+  int i;
+  for (i = 0; i < nr; i++)
+    {
+      int offset;
+      if (*(char *)&e)
+	/* Little endian.  */
+	offset = sizeof (long long) - size;
+      else
+	/* Big endian endian.  */
+	offset = 0;
+      memcpy (b + size * i, (char *) val + sizeof (long long) * i + offset, size);
+    }
 }
+
+unsigned char b[MAX_BYTES];
+unsigned char h[MAX_BYTES];
+unsigned char w[MAX_BYTES];
+unsigned char g[MAX_BYTES];
+
+unsigned char c[MAX_BYTES];
+unsigned char s[MAX_BYTES];
+unsigned char i[MAX_BYTES];
+unsigned char l[MAX_BYTES];
+unsigned char ll[MAX_BYTES];
 
 int known_types()
 {
-   long long bin = 0, oct = 0, dec = 0, hex = 0;
+  /* A union is used here as, hopefully it has well defined packing
+     rules.  */
+  struct {
+    long long bin, oct, dec, hex;    
+  } val;
+  memset (&val, 0, sizeof val);
 
-   /* Known values, filling the full 64 bits.
-    */
-   bin = 0x123456789abcdefLL; /* 64 bits = 16 hex digits */
-   oct = 01234567123456701234567LL; /*  = 21+ octal digits */
-   dec = 12345678901234567890ULL;    /*  = 19+ decimal digits */
+  /* Known values, filling the full 64 bits.  */
+  val.bin = 0x123456789abcdefLL; /* 64 bits = 16 hex digits */
+  val.oct = 01234567123456701234567LL; /*  = 21+ octal digits */
+  val.dec = 12345678901234567890ULL;    /*  = 19+ decimal digits */
 
-   /* Stop here and look!
-    */
-   hex = bin - dec | oct;
+  /* Stop here and look!  */
+  val.hex = val.bin - val.dec | val.oct;
 
-   return 0;
+  return 0;
 }
 
 int main() {
 
-   register long long x, y;
-   register long long i;
-   
-   x = (long long) 0xfedcba9876543210LL;
-   y = x++;
-   x +=y;
-   i = 11;
-   x = callee( i );
-   y += x;
+   /* Pack Byte, Half, Word and Giant arrays with byte-orderd values.
+      That way "(gdb) x" gives the same output on different
+      architectures.  */
+   pack (b, 1, 2);
+   pack (h, 2, 2);
+   pack (w, 4, 2);
+   pack (g, 8, 2);
+   pack (c, sizeof (char), 2);
+   pack (s, sizeof (short), 2);
+   pack (i, sizeof (int), 2);
+   pack (l, sizeof (long), 2);
+   pack (ll, sizeof (long long), 2);
 
    known_types();
    
    return 0;
 }
-
-
-
