62b51c0b7b7b92f33233ba08579d43388707edcd
Alan Modra <amodra@gmail.com>
2004-06-29 14:43:55 +0000

    * gas/m68k/mode5.s: Pad section. 	* gas/m68k/mode5.d: Update.

--- gas/testsuite/gas/m68k/mode5.d
+++ gas/testsuite/gas/m68k/mode5.d
@@ -10,3 +10,4 @@ Disassembly of section .text:
    0:	2213           	movel %a3@,%d1
    2:	2882           	movel %d2,%a4@
    4:	2295           	movel %a5@,%a1@
+	...
--- gas/testsuite/gas/m68k/mode5.s
+++ gas/testsuite/gas/m68k/mode5.s
@@ -4,3 +4,4 @@
 	move.l 0(%a3),%d1
 	move.l %d2,0(%a4)
 	move.l 0(%a5),0(%a1)
+	.p2align 4
