5bb2a876193d32bcc43554c792579525d7f45899
Alan Modra <amodra@gmail.com>
2004-07-02 06:46:17 +0000

    * ld-discard/static.d: Accept both original sym and section sym.

--- ld/testsuite/ld-discard/static.d
+++ ld/testsuite/ld-discard/static.d
@@ -1,3 +1,3 @@
 #source: static.s
 #ld: -T discard.ld
-#error: `\.data\.exit' referenced in section `\.text' of tmpdir/dump0.o: defined in discarded section `\.data\.exit' of tmpdir/dump0.o
+#error: `(\.data\.exit|data)' referenced in section `\.text' of tmpdir/dump0.o: defined in discarded section `\.data\.exit' of tmpdir/dump0.o
