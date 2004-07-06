e7bd9ea00e5f9144e69f3aeb477d4da420c281ed
Nick Clifton <nickc@redhat.com>
2004-07-06 16:42:04 +0000

    (output_file_create): Report the target format chosen when bfd_openw reports that it is invalid.

--- gas/output-file.c
+++ gas/output-file.c
@@ -48,7 +48,10 @@ output_file_create (char *name)
 
   else if (!(stdoutput = bfd_openw (name, TARGET_FORMAT)))
     {
-      as_perror (_("FATAL: can't create %s"), name);
+      if (bfd_get_error () == bfd_error_invalid_target)
+	as_perror (_("Selected target format '%s' unknown"), TARGET_FORMAT);
+      else
+	as_perror (_("FATAL: can't create %s"), name);
       exit (EXIT_FAILURE);
     }
 
