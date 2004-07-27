1bec8141aa331266a162701c3ba0e0ea9bf3b6d5
Nick Clifton <nickc@redhat.com>
2004-07-27 16:47:02 +0000

    ld-h8300/h8300.exp: Addition of new test case to check relaxation for H8S target. ld-h8300/relax-6.s: New test source file. ld-h8300/relax-6.d: New test expected disassembly file. ld-h8300/relax-6-coff.d: New test expected disassembly file for COFF format.

--- ld/testsuite/ld-h8300/gcsection.d
+++ ld/testsuite/ld-h8300/gcsection.d
@@ -6,22 +6,22 @@
 Disassembly of section .text:
 
 00000100 <_functionWeUse>:
- 100:	01 00 6d f6       mov.l	er6,@-er7
- 104:	0f f6             mov.l	er7,er6
- 106:	1b 97             subs	#4,er7
- 108:	6f e0 ff fe       mov.w	r0,@\(0xfffe:16,er6\)
- 10c:	6f 62 ff fe       mov.w	@\(0xfffe:16,er6\),r2
- 110:	0d 20             mov.w	r2,r0
- 112:	0b 97             adds	#4,er7
- 114:	01 00 6d 76       mov.l	@er7\+,er6
- 118:	54 70             rts	
+ 100:	mov.l	er6,@-er7
+ 104:	mov.l	er7,er6
+ 106:	subs	#4,er7
+ 108:	mov.w	r0,@\(0xfffe:16,er6\)
+ 10c:	mov.w	@\(0xfffe:16,er6\),r2
+ 110:	mov.w	r2,r0
+ 112:	adds	#4,er7
+ 114:	mov.l	@er7\+,er6
+ 118:	rts	
 
 0000011a <_start>:
- 11a:	01 00 6d f6       mov.l	er6,@-er7
- 11e:	0f f6             mov.l	er7,er6
- 120:	79 00 00 4b       mov.w	#0x4b,r0
- 124:	5e 00 01 00       jsr	@0x100:24
- 128:	0d 02             mov.w	r0,r2
- 12a:	0d 20             mov.w	r2,r0
- 12c:	01 00 6d 76       mov.l	@er7\+,er6
- 130:	54 70             rts	
+ 11a:	mov.l	er6,@-er7
+ 11e:	mov.l	er7,er6
+ 120:	mov.w	#0x4b,r0
+ 124:	jsr	@0x100:24
+ 128:	mov.w	r0,r2
+ 12a:	mov.w	r2,r0
+ 12c:	mov.l	@er7\+,er6
+ 130:	rts	
--- ld/testsuite/ld-h8300/relax-6-coff.d
+++ ld/testsuite/ld-h8300/relax-6-coff.d
@@ -1,11 +1,11 @@
-# name: H8300 Relaxation Test 2
-# ld: --relax -m h8300helf
+# name: H8300 Relaxation Test 6
+# source: relax-6.s
+# ld: --relax -m h8300s
 # objdump: -d --no-show-raw-insn
 
 .*:     file format .*-h8300
-
 Disassembly of section .text:
 
 00000100 <_start>:
- *100:	mov.b	@0x67:8,r0l
- *102:	mov.b	@0x4321:16,r0l
+ 100:	mov.b	r2l,@0xbd:8
+ 102:	rts	
