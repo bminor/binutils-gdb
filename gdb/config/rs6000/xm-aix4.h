e08e1886978388e115248bdd73012774edd9948d
Joel Brobecker <brobecker@gnat.com>
2004-08-11 22:40:45 +0000

    * config/rs6000/xm-aix4.h: Remove "config/xm-aix4.h" include,         this file is now empty.         * config/xm-aix4.h: Remove, this file is no longer included         anywhere.

--- gdb/config/rs6000/xm-aix4.h
+++ gdb/config/rs6000/xm-aix4.h
@@ -20,8 +20,6 @@
    Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
-#include "config/xm-aix4.h"
-
 /* AIX 4.x uses nonstandard "int *" as type of third argument to ptrace() */
 
 #define PTRACE_ARG3_TYPE int*
