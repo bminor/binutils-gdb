5069eab2fd44ef7ed28c6217dc4ebe5654da8063
Nick Clifton <nickc@redhat.com>
2004-08-24 11:37:54 +0000

    altmacro.[sd]: Split out part from here... altmac2.[sd]: ... to here. excl.s: New. gas.exp: Suppress both tests for a few targets known to break. Run the new  (split out) test only when the target doesn't use '!' as a comment character.

--- gas/testsuite/gas/all/altmac2.d
+++ gas/testsuite/gas/all/altmac2.d
@@ -1,11 +1,10 @@
 #as: --alternate
 #objdump: -s -j .data
-#name: alternate macro syntax
+#name: alternate macro syntax (escape)
 
 # Test the alternate macro syntax.
 
 .*: .*
 
 Contents of section .data:
- 0000 01020912 61626331 32332121 3c3e2721 .*
- 0010 3c3e27.*
+ 0000 3e3c21.*
