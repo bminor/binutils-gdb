b4f0ee661ed8324bced68bd383434176190729b0
Corinna Vinschen <corinna@vinschen.de>
2004-09-08 09:20:29 +0000

    * allinsn.exp: Add new tests. 	* bandor.s: New file. 	* bandornot.s: New file. 	* bclr.s: New file. 	* bld.s: New file. 	* bldnot.s: New file. 	* bset.s: New file. 	* bst.s: New file. 	* bxor.s: New file. 	* clip.s: New file. 	* div.s: New file. 	* fail.s: New file, make sure fail works. 	* fsca.s: New file. 	* fsrra.s: New file. 	* mov.s: New file. 	* mulr.s: New file. 	* pass.s: New file, make sure pass works. 	* pushpop.s: New file. 	* resbank.s: New file. 	* testutils.inc (bf8k, bt8k, assertmem): New macros.

--- sim/testsuite/sim/sh/clip.s
+++ sim/testsuite/sim/sh/clip.s
@@ -1,18 +1,17 @@
-# sh testcase for all mov <#imm> instructions
-# mach: sh
+# sh testcase for clips, clipu
+# mach:	 all
 # as(sh):	-defsym sim_cpu=0
+# as(shdsp):	-defsym sim_cpu=1 -dsp 
 
 	.include "testutils.inc"
 
 	start
 
-mov_i_reg:	# Test <imm8>
+clips_b:
 	set_grs_a5a5
-	mov	#-0x55, r1
-
-	assertreg 0xffffffab, r1
-
-	test_gr_a5a5 r0
+	clips.b	r1
+	test_gr0_a5a5
+	assertreg 0xffffff80 r1
 	test_gr_a5a5 r2
 	test_gr_a5a5 r3
 	test_gr_a5a5 r4
@@ -27,13 +26,30 @@ mov_i_reg:	# Test <imm8>
 	test_gr_a5a5 r13
 	test_gr_a5a5 r14
 
-movi20_reg:	# Test <imm20>
+clipu_b:
 	set_grs_a5a5
-	movi20	#-0x55555,r1
-
-	assertreg 0xfffaaaab, r1
-
-	test_gr_a5a5 r0
+	clipu.b	r1
+	test_gr0_a5a5
+	assertreg 0xff r1
+	test_gr_a5a5 r2
+	test_gr_a5a5 r3
+	test_gr_a5a5 r4
+	test_gr_a5a5 r5
+	test_gr_a5a5 r6
+	test_gr_a5a5 r7
+	test_gr_a5a5 r8
+	test_gr_a5a5 r9
+	test_gr_a5a5 r10
+	test_gr_a5a5 r11
+	test_gr_a5a5 r12
+	test_gr_a5a5 r13
+	test_gr_a5a5 r14
+	
+clips_w:
+	set_grs_a5a5
+	clips.w	r1
+	test_gr0_a5a5
+	assertreg 0xffff8000 r1
 	test_gr_a5a5 r2
 	test_gr_a5a5 r3
 	test_gr_a5a5 r4
@@ -48,13 +64,11 @@ movi20_reg:	# Test <imm20>
 	test_gr_a5a5 r13
 	test_gr_a5a5 r14
 
-movi20s_reg:	# Test <imm20> << 8
+clipu_w:
 	set_grs_a5a5
-	movi20s	#-0x5555500,r1
-
-	assertreg 0xfaaaab00, r1
-
-	test_gr_a5a5 r0
+	clipu.w	r1
+	test_gr0_a5a5
+	assertreg 0xffff r1
 	test_gr_a5a5 r2
 	test_gr_a5a5 r3
 	test_gr_a5a5 r4
@@ -68,9 +82,8 @@ movi20s_reg:	# Test <imm20> << 8
 	test_gr_a5a5 r12
 	test_gr_a5a5 r13
 	test_gr_a5a5 r14
-
+	
 	pass
 
 	exit 0
-
 
