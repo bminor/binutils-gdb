fdd2f0d80433c7a0d9a80454a70f09c2aebdad96
DJ Delorie <dj@redhat.com>
2004-09-13 21:02:33 +0000

    * sim/sh/allinsn.exp: Set global_as_options and global_ld_options appropriately for little endian builds. * sim/sh/movua.s: Support little endian.

--- sim/testsuite/sim/sh/movua.s
+++ sim/testsuite/sim/sh/movua.s
@@ -10,55 +10,107 @@ movua_1:
 	set_grs_a5a5
 	mov.l	srcp, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x03020100
+.else
 	assertreg0	0x00010203
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x04030201
+.else
 	assertreg0	0x01020304
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x05040302
+.else
 	assertreg0	0x02030405
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x06050403
+.else
 	assertreg0	0x03040506
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x07060504
+.else
 	assertreg0	0x04050607
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x08070605
+.else
 	assertreg0	0x05060708
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x09080706
+.else
 	assertreg0	0x06070809
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x0a090807
+.else
 	assertreg0	0x0708090a
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x0b0a0908
+.else
 	assertreg0	0x08090a0b
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x0c0b0a09
+.else
 	assertreg0	0x090a0b0c
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x0d0c0b0a
+.else
 	assertreg0	0x0a0b0c0d
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x0e0d0c0b
+.else
 	assertreg0	0x0b0c0d0e
+.endif
 
 	add	#1, r1
 	movua.l	@r1, r0
+.ifdef LITTLE
+	assertreg0	0x0f0e0d0c
+.else
 	assertreg0	0x0c0d0e0f
+.endif
 
 	assertreg	src+12, r1
 	test_gr_a5a5	r2
@@ -87,25 +139,41 @@ movua_4:
 	set_grs_a5a5
 	mov.l	srcp2, r1
 	movua.l	@r1+, r0
+.ifdef LITTLE
+	assertreg0	0x03020100
+.else
 	assertreg0	0x00010203
+.endif
 	assertreg	src+4, r1
 
 	mov.l	srcp2, r1
 	add	#1, r1
 	movua.l	@r1+, r0
+.ifdef LITTLE
+	assertreg0	0x04030201
+.else
 	assertreg0	0x01020304
+.endif
 	assertreg	src+5, r1
 
 	mov.l	srcp2, r1
 	add	#2, r1
 	movua.l	@r1+, r0
+.ifdef LITTLE
+	assertreg0	0x05040302
+.else
 	assertreg0	0x02030405
+.endif
 	assertreg	src+6, r1
 
 	mov.l	srcp2, r1
 	add	#3, r1
 	movua.l	@r1+, r0
+.ifdef LITTLE
+	assertreg0	0x06050403
+.else
 	assertreg0	0x03040506
+.endif
 	assertreg	src+7, r1
 
 	test_gr_a5a5	r2
