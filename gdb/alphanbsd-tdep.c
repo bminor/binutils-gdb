e8d74ed38e1accc1c90d7fdaa5f19952ff109ee2
Mark Kettenis <kettenis@gnu.org>
2004-09-28 20:02:20 +0000

    * alphanbsd-tdep.c (_initialize_alphanbsd_tdep): Register OS/ABI for OpenBSD ELF.

--- gdb/alphanbsd-tdep.c
+++ gdb/alphanbsd-tdep.c
@@ -226,6 +226,8 @@ _initialize_alphanbsd_tdep (void)
 {
   gdbarch_register_osabi (bfd_arch_alpha, 0, GDB_OSABI_NETBSD_ELF,
                           alphanbsd_init_abi);
+  gdbarch_register_osabi (bfd_arch_alpha, 0, GDB_OSABI_OPENBSD_ELF,
+                          alphanbsd_init_abi);
 
   deprecated_add_core_fns (&alphanbsd_core_fns);
   deprecated_add_core_fns (&alphanbsd_elfcore_fns);
