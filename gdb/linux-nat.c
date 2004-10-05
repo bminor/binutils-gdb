146c42e39d4f430b87bbbc68246bd84920d551ae
Jim Blandy <jimb@codesourcery.com>
2004-10-05 22:40:30 +0000

    * linux-nat.c (_initialize_linux_nat): Rename child_ops. (Previous change missed some uses.)

--- gdb/linux-nat.c
+++ gdb/linux-nat.c
@@ -3012,8 +3012,8 @@ _initialize_linux_nat (void)
   struct sigaction action;
   extern void thread_db_init (struct target_ops *);
 
-  child_ops.to_find_memory_regions = linux_nat_find_memory_regions;
-  child_ops.to_make_corefile_notes = linux_nat_make_corefile_notes;
+  deprecated_child_ops.to_find_memory_regions = linux_nat_find_memory_regions;
+  deprecated_child_ops.to_make_corefile_notes = linux_nat_make_corefile_notes;
 
   add_info ("proc", linux_nat_info_proc_cmd,
 	    "Show /proc process information about any running process.\n\
