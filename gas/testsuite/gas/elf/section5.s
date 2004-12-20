a904de94bd216aa104128278ed8c54f0b360dce7
Nick Clifton <nickc@redhat.com>
2004-12-20 15:35:53 +0000

    * gas/elf/section5.[ls]: Use % instead of @ in .section	directives.

--- gas/testsuite/gas/elf/section5.s
+++ gas/testsuite/gas/elf/section5.s
@@ -1,16 +1,16 @@
 .section	.test0
-.section	.test1, "", @progbits
+.section	.test1, "", %progbits
 .section	.test2
 .section	.test3, "aw"
-.section	.test4, "aw", @nobits
+.section	.test4, "aw", %nobits
 
-.section	.test1, "aw", @nobits
+.section	.test1, "aw", %nobits
 test1:	.long	test1
 
 .section	.test2, "w"
 test2:	.long	test2
 
-.section	.test3, "aw", @progbits
+.section	.test3, "aw", %progbits
 test3:	.long	test3
 
 .section	.test4, "aw"
@@ -19,6 +19,6 @@ test3:	.long	test3
 
 .section	.bss, "a"
 
-.section	.data, "aw", @nobits
+.section	.data, "aw", %nobits
 
-.section	.bss, "aw", @progbits
+.section	.bss, "aw", %progbits
