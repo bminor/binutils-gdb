5371425451c76a85621060cc61ca597a845bccb5
Daniel Jacobowitz <drow@false.org>
2005-01-31 21:58:43 +0000

    * ld-elf/group1.d: Accept OBJECT symbols.

--- ld/testsuite/ld-elf/group1.d
+++ ld/testsuite/ld-elf/group1.d
@@ -4,6 +4,6 @@
 #readelf: -s
 Symbol table '.symtab' contains .* entries:
 #...
-    .*: 0[0]*1000     0 NOTYPE  WEAK   DEFAULT    . foo
-    .*: 0[0]*0000     0 NOTYPE  GLOBAL DEFAULT  UND bar
+    .*: 0[0]*1000     0 (NOTYPE|OBJECT)  WEAK   DEFAULT    . foo
+    .*: 0[0]*0000     0 (NOTYPE|OBJECT)  GLOBAL DEFAULT  UND bar
 #...
