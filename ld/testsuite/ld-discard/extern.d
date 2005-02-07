769946806e26b79ea5ef8f8679eae58279b398eb
Hans-Peter Nilsson <hp@axis.com>
2005-02-07 03:09:10 +0000

    * ld-discard/extern.d, ld-discard/start.d, ld-discard/static.d: 	Adjust for being identified as warnings.

--- ld/testsuite/ld-discard/extern.d
+++ ld/testsuite/ld-discard/extern.d
@@ -1,3 +1,5 @@
 #source: extern.s
 #ld: -T discard.ld
-#error: `data' referenced in section `\.text' of tmpdir/dump0.o: defined in discarded section `\.data\.exit' of tmpdir/dump0.o
+#warning: `data' referenced in section `\.text' of tmpdir/dump0.o: defined in discarded section `\.data\.exit' of tmpdir/dump0.o
+#objdump: -p
+#pass
