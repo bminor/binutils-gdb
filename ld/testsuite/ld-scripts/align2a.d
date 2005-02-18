7921892a2fa91f709ca63767993582a2733641fe
Alan Modra <amodra@gmail.com>
2005-02-18 01:55:16 +0000

    * ld-scripts/align2a.d: Don't assume anything about section 	padding or alignment, or max page size.  Allow 64-bit addresses. 	* ld-scripts/align2b.d: Likewise.

--- ld/testsuite/ld-scripts/align2a.d
+++ ld/testsuite/ld-scripts/align2a.d
@@ -5,9 +5,8 @@
 
 Sections:
 Idx +Name +Size +VMA +LMA +File +off +Algn
- +0 +\.text +00000004 +00000000 +00000000 +00001000 +2\*\*2
+ +0 +\.text +[^ ]* +0+ +0+ .*
  +CONTENTS, +ALLOC, +LOAD, +READONLY, +CODE
- +1 +\.data +00000004 +00000010 +00000010 +00001010 +2\*\*2
+ +1 +\.data +[^ ]* +0+10 +0+10 .*
  +CONTENTS, +ALLOC, +LOAD, +DATA
- +2 +\.bss +00000000 +00000014 +00000014 +00001014 +2\*\*2
- +ALLOC
+#pass
--- ld/testsuite/ld-scripts/align2b.d
+++ ld/testsuite/ld-scripts/align2b.d
@@ -5,9 +5,8 @@
 
 Sections:
 Idx +Name +Size +VMA +LMA +File off +Algn
- +0 +\.text +00000004 +00000000 +00000000 +00001000 +2\*\*2
+ +0 +\.text +[^ ]* +0+ +0+ .*
  +CONTENTS, +ALLOC, +LOAD, +READONLY, +CODE
- +1 +\.data +00000004 +00000020 +00000020 +00001020 +2\*\*2
+ +1 +\.data +[^ ]* +0+20 +0+20 .*
  +CONTENTS, +ALLOC, +LOAD, +DATA
- +2 +\.bss +00000000 +00000024 +00000024 +00001024 +2\*\*2
- +ALLOC
+#pass
