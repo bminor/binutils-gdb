dc652011c764ca40b4e3c79062411fe9bd1fd8b7
Nick Clifton <nickc@redhat.com>
2005-03-01 16:14:29 +0000

    * ld-cris/warn3.d: Only run this version of the test for cris-aout targets. * ld-cris/warn4.d: Only run this version of the test for cris-elf targets.

--- ld/testsuite/ld-cris/warn4.d
+++ ld/testsuite/ld-cris/warn4.d
@@ -1,7 +1,7 @@
 #source: start1.s
 #source: globsym1ref.s
 #source: globsymw2.s
-#target: cris-*-*elf* cris-*-*aout*
+#target: cris-*-*elf*
 #as: --em=criself
 #ld: -mcriself
 #warning: warning: isatty is not implemented and will always fail$
