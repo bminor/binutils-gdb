4f495e616ddf03ac1c8cbb350cec2e292d9662b1
Nick Clifton <nickc@redhat.com>
2005-03-14 09:35:26 +0000

    Use ".word 0x0012 # Entry mask: r1 r2 >" instead of just "Entry mask: < r1 ... >"

--- opcodes/vax-dis.c
+++ opcodes/vax-dis.c
@@ -3,19 +3,19 @@
    Free Software Foundation, Inc.
    Contributed by Pauline Middelink <middelin@polyware.iaf.nl>
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include "sysdep.h"
 #include "opcode/vax.h"
@@ -39,7 +39,7 @@ static char *reg_names[] =
 static char *entry_mask_bit[] =
 {
   /* Registers 0 and 1 shall not be saved, since they're used to pass back
-     a function's result to it's caller...  */
+     a function's result to its caller...  */
   "~r0~", "~r1~",
   /* Registers 2 .. 11 are normal registers.  */
   "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11",
@@ -165,7 +165,7 @@ print_insn_vax (memaddr, info)
       int i = 0;
       int register_mask = buffer[1] << 8 | buffer[0];
 
-      (*info->fprintf_func) (info->stream, "Entry mask 0x%04x = <",
+      (*info->fprintf_func) (info->stream, ".word 0x%04x # Entry mask: <",
 			     register_mask);
 
       for (i = 15; i >= 0; i--)
