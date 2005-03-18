7451fd8967001ff0af98b084e231f39f68ddcd4f
H.J. Lu <hjl.tools@gmail.com>
2005-03-18 23:51:21 +0000

    2005-03-18  H.J. Lu  <hongjiu.lu@intel.com>
    	* elflink.c (elf_mark_used_section): Remove check for special
    	sections.

--- bfd/elflink.c
+++ bfd/elflink.c
@@ -9012,9 +9012,7 @@ elf_mark_used_section (struct elf_link_hash_entry *h,
       || h->root.type == bfd_link_hash_defweak)
     {
       asection *s = h->root.u.def.section;
-      if (s != NULL
-	  && s->output_section != NULL
-	  && !bfd_is_const_section (s->output_section))
+      if (s != NULL && s->output_section != NULL)
 	s->output_section->flags |= SEC_KEEP;
     }
 
