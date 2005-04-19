29f54b2417c42a61751149e95856ad305384e033
H.J. Lu <hjl.tools@gmail.com>
2005-04-19 18:11:11 +0000

    2005-04-19  H.J. Lu  <hongjiu.lu@intel.com>
    	* gas/i386/padlock.d: Updated.

--- gas/testsuite/gas/i386/padlock.d
+++ gas/testsuite/gas/i386/padlock.d
@@ -6,18 +6,18 @@
 Disassembly of section .text:
 
 00000000 <foo>:
-   0:[	 ]*0f a7 c0 [	 ]*xstorerng 
-   3:[	 ]*f3 0f a7 c0 [	 ]*repz xstorerng 
-   7:[	 ]*f3 0f a7 c8 [	 ]*repz xcryptecb 
-   b:[	 ]*f3 0f a7 c8 [	 ]*repz xcryptecb 
-   f:[	 ]*f3 0f a7 d0 [	 ]*repz xcryptcbc 
-  13:[	 ]*f3 0f a7 d0 [	 ]*repz xcryptcbc 
-  17:[	 ]*f3 0f a7 e0 [	 ]*repz xcryptcfb 
-  1b:[	 ]*f3 0f a7 e0 [	 ]*repz xcryptcfb 
-  1f:[	 ]*f3 0f a7 e8 [	 ]*repz xcryptofb 
-  23:[	 ]*f3 0f a7 e8 [	 ]*repz xcryptofb 
-  27:[	 ]*0f a7 c0 [	 ]*xstorerng 
-  2a:[	 ]*f3 0f a7 c0 [	 ]*repz xstorerng 
+   0:[	 ]*0f a7 c0 [	 ]*xstore-rng 
+   3:[	 ]*f3 0f a7 c0 [	 ]*repz xstore-rng 
+   7:[	 ]*f3 0f a7 c8 [	 ]*repz xcrypt-ecb 
+   b:[	 ]*f3 0f a7 c8 [	 ]*repz xcrypt-ecb 
+   f:[	 ]*f3 0f a7 d0 [	 ]*repz xcrypt-cbc 
+  13:[	 ]*f3 0f a7 d0 [	 ]*repz xcrypt-cbc 
+  17:[	 ]*f3 0f a7 e0 [	 ]*repz xcrypt-cfb 
+  1b:[	 ]*f3 0f a7 e0 [	 ]*repz xcrypt-cfb 
+  1f:[	 ]*f3 0f a7 e8 [	 ]*repz xcrypt-ofb 
+  23:[	 ]*f3 0f a7 e8 [	 ]*repz xcrypt-ofb 
+  27:[	 ]*0f a7 c0 [	 ]*xstore-rng 
+  2a:[	 ]*f3 0f a7 c0 [	 ]*repz xstore-rng 
   2e:[	 ]*f3 0f a6 c0 [	 ]*repz montmul 
   32:[	 ]*f3 0f a6 c0 [	 ]*repz montmul 
   36:[	 ]*f3 0f a6 c8 [	 ]*repz xsha1 
