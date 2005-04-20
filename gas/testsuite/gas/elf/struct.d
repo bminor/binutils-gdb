e7f9d279d966f95b0a279582657e6db51b582597
Jan Beulich <jbeulich@novell.com>
2005-04-20 12:28:00 +0000

    gas/testsuite/ 2005-04-20  Jan Beulich  <jbeulich@novell.com>
    	* gas/elf/struct.s: Adjust to not get into alignment issues.
    	* gas/elf/struct.d: Adjust for the above and the test's name.

--- gas/testsuite/gas/elf/struct.d
+++ gas/testsuite/gas/elf/struct.d
@@ -1,10 +1,10 @@
 #nm: --extern-only
-#name: struct
+#name: ELF struct
 
 # Test the .struct pseudo-op.
 
 0+0 D l1
 0+4 D l2
-0+1 A w1
-0+3 A w2
-0+5 A w3
+0+2 A w1
+0+4 A w2
+0+6 A w3
--- gas/testsuite/gas/elf/struct.s
+++ gas/testsuite/gas/elf/struct.s
@@ -1,7 +1,7 @@
 	.globl	w1, w2, w3, l1, l2
 	.data
 l1:	.long	0
-	.struct 1
+	.struct 2
 w1:	.short	0
 w2:	.short	0
 w3:	.short	0
