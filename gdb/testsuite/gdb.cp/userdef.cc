607fbc543c498bcbd959339a309266ed790b7b3d
Daniel Jacobowitz <drow@false.org>
2005-04-27 16:35:15 +0000

    * ChangeLog: Correct some 2003-01-13 dates. 	* gdb.base/sigbpt.exp, gdb.base/signull.exp, gdb.cp/bs15503.exp, 	gdb.cp/exception.exp, gdb.cp/userdef.cc, gdb.cp/userdef.exp, 	gdb.mi/mi-console.exp, gdb.mi/mi2-console.exp, 	gdb.trace/collection.exp, gdb.trace/passc-dyn.exp, 	gdb.trace/tfind.exp: Update copyright years.

--- gdb/testsuite/gdb.cp/userdef.cc
+++ gdb/testsuite/gdb.cp/userdef.cc
@@ -1,6 +1,6 @@
 /* This test script is part of GDB, the GNU debugger.
 
-   Copyright 1999, 2002, 2003, 2004,
+   Copyright 1999, 2002, 2003, 2004, 2005
    Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
