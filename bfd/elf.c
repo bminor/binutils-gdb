6f140a15a35d90cccc485451e2ecda586f23b4eb
H.J. Lu <hjl.tools@gmail.com>
2005-05-05 17:22:40 +0000

    2005-05-05  H.J. Lu  <hongjiu.lu@intel.com>
    	* elf.c (_bfd_elf_close_and_cleanup): Only call
    	_bfd_dwarf2_cleanup_debug_info on bfd_object.

--- bfd/elf.c
+++ bfd/elf.c
@@ -6683,9 +6683,8 @@ _bfd_elf_close_and_cleanup (bfd *abfd)
     {
       if (elf_shstrtab (abfd) != NULL)
 	_bfd_elf_strtab_free (elf_shstrtab (abfd));
+      _bfd_dwarf2_cleanup_debug_info (abfd);
     }
-
-  _bfd_dwarf2_cleanup_debug_info (abfd);
 
   return _bfd_generic_close_and_cleanup (abfd);
 }
