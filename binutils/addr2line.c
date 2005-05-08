b43b5d5f768ed7a7a150d78c9fda0c194a11497d
Nick Clifton <nickc@redhat.com>
2005-05-08 14:17:41 +0000

    Update FSF address

--- binutils/addr2line.c
+++ binutils/addr2line.c
@@ -17,7 +17,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   Foundation, 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
 
 /* Derived from objdump.c and nm.c by Ulrich.Lauther@mchp.siemens.de
 
--- binutils/windres.c
+++ binutils/windres.c
@@ -17,8 +17,8 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-   02111-1307, USA.  */
+   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA
+   02110-1301, USA.  */
 
 /* This program can read and write Windows resources in various
    formats.  In particular, it can act like the rc resource compiler
