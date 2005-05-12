380d9419c99660cdfb1501be9a14630ad5e398cf
Nick Clifton <nickc@redhat.com>
2005-05-12 07:36:59 +0000

    Update the address of the FSF organization

--- sim/arm/wrapper.c
+++ sim/arm/wrapper.c
@@ -16,8 +16,8 @@
 
    You should have received a copy of the GNU General Public
    License along with this program; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
 /* This file provides the interface between the simulator and
    run.c and gdb (when the simulator is linked with gdb).
