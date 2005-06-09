6e81d46e50b6c0ca5abaf6fdd80984b546c116ae
Alan Modra <amodra@gmail.com>
2005-06-09 02:06:58 +0000

    * ld-scripts/empty-aligned.d: Adjust.

--- ld/testsuite/ld-scripts/empty-aligned.d
+++ ld/testsuite/ld-scripts/empty-aligned.d
@@ -6,7 +6,9 @@
 Program Headers:
  +Type +Offset +VirtAddr +PhysAddr +FileSiz +MemSiz +Flg +Align
  +LOAD +0x[0-9a-f]+ 0x[0-9a-f]+ 0x[0-9a-f]+ 0x[0-9a-f]+ 0x[0-9a-f]+ [RWE ]+ +0x[0-9a-f]+
+ +LOAD +0x[0-9a-f]+ 0x[0-9a-f]+ 0x[0-9a-f]+ 0x[0-9a-f]+ 0x[0-9a-f]+ [RWE ]+ +0x[0-9a-f]+
 
  Section to Segment mapping:
  +Segment Sections\.\.\.
- +00.*\.text.*\.data.*
+ +00.*\.text.*\.text2.*
+ +01.*.data.*
