2c7ff8e102459aa6c45e35ba01d1c54a16038fb7
Mark Kettenis <kettenis@gnu.org>
2005-06-12 11:53:00 +0000

    * m88k-tdep.c (m88k_frame_prev_register): Change type of last argument to `gdb_byte *'.

--- gdb/m88k-tdep.c
+++ gdb/m88k-tdep.c
@@ -721,7 +721,7 @@ static void
 m88k_frame_prev_register (struct frame_info *next_frame, void **this_cache,
 			  int regnum, int *optimizedp,
 			  enum lval_type *lvalp, CORE_ADDR *addrp,
-			  int *realnump, void *valuep)
+			  int *realnump, gdb_byte *valuep)
 {
   struct m88k_frame_cache *cache = m88k_frame_cache (next_frame, this_cache);
 
