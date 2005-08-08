3c2e36dc6f7dc9c54ae4e7fa50b46cfe5d1ec98b
Richard Earnshaw <richard.earnshaw@arm.com>
2005-08-08 15:52:54 +0000

    * ld-arm/mixed-app.sym: Expact _stack to be in the ABS section. 	* ld-arm/mixed-lib.sym: Likewise. 	* tls-lib.d: Use a regexp for the address locations. 	* tls-lib.r: Likewise.

--- ld/testsuite/ld-arm/tls-lib.d
+++ ld/testsuite/ld-arm/tls-lib.d
@@ -6,10 +6,10 @@ start address 0x.*
 
 Disassembly of section .text:
 
-00000314 <foo>:
- 314:	e1a00000 	nop			\(mov r0,r0\)
- 318:	e1a00000 	nop			\(mov r0,r0\)
- 31c:	e1a0f00e 	mov	pc, lr
- 320:	00008098 	muleq	r0, r8, r0
- 324:	0000808c 	andeq	r8, r0, ip, lsl #1
- 328:	00000004 	andeq	r0, r0, r4
+.* <foo>:
+ .*:	e1a00000 	nop			\(mov r0,r0\)
+ .*:	e1a00000 	nop			\(mov r0,r0\)
+ .*:	e1a0f00e 	mov	pc, lr
+ .*:	00008098 	muleq	r0, r8, r0
+ .*:	0000808c 	andeq	r8, r0, ip, lsl #1
+ .*:	00000004 	andeq	r0, r0, r4
