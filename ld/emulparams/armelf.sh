3242b11e04acbbdd5a88276cc96c8e1ade35f24f
Paul Brook <paul@codesourcery.com>
2005-10-19 14:43:21 +0000

    2005-10-19  Paul Brook  <paul@codesourcery.com>
    	* emulparams/armelf.sh: Add .ARM.attributes to OTHER_SECTIONS.

--- ld/emulparams/armelf.sh
+++ ld/emulparams/armelf.sh
@@ -10,7 +10,8 @@ OTHER_TEXT_SECTIONS='*(.glue_7t) *(.glue_7)'
 OTHER_BSS_SYMBOLS='__bss_start__ = .;'
 OTHER_BSS_END_SYMBOLS='_bss_end__ = . ; __bss_end__ = . ;'
 OTHER_END_SYMBOLS='__end__ = . ;'
-OTHER_SECTIONS='.note.gnu.arm.ident 0 : { KEEP (*(.note.gnu.arm.ident)) }'
+OTHER_SECTIONS='.note.gnu.arm.ident 0 : { KEEP (*(.note.gnu.arm.ident)) }
+  .ARM.attributes 0 : { KEEP (*(.ARM.attributes)) }'
 OTHER_READONLY_SECTIONS="
   .ARM.extab ${RELOCATING-0} : { *(.ARM.extab${RELOCATING+* .gnu.linkonce.armextab.*}) }
   ${RELOCATING+ __exidx_start = .; }
