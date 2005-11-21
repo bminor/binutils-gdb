6745c7267e99deea1ae773f77825ceb2dbc89830
Dave Anglin <dave.anglin@nrc.ca>
2005-11-21 04:30:32 +0000

    Bug gas/1894 Bug gas/1895 	* gas/all/gas.exp (redef3): xfail on hppa*-*-hpux*. 	* gas/all/redef.d: Add -j "\$DATA\$".  Modify regexp to check for 	"$DATA$" as well as ".data". 	* gas/all/redef2.d: Likewise.

--- gas/testsuite/gas/all/redef2.d
+++ gas/testsuite/gas/all/redef2.d
@@ -1,4 +1,4 @@
-#objdump: -rsj .data
+#objdump: -rs -j .data -j "\$DATA\$"
 #name: .equ redefinitions (2)
 
 .*: .*
@@ -9,7 +9,7 @@ RELOCATION RECORDS FOR .*
 0+08.*xtrn
 0+10.*(sym|\.data(\+0x0+10)?)
 #...
-Contents of section \.data:
+Contents of section (\.data|\$DATA\$):
  0000 00000000 11111111 00000000 22222222[ 	]+................[ 	]*
  0010 [01]00000[01]0 .*
 #pass
