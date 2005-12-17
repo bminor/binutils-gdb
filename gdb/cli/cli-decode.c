10f9c2135edc8afa51cf68d16b7e7a4d552895cd
Eli Zaretskii <eliz@gnu.org>
2005-12-17 22:40:17 +0000

    * cli/cli-utils.h: 	* cli/cli-utils.c: 	* cli/cli-setshow.h: 	* cli/cli-setshow.c: 	* cli/cli-script.h: 	* cli/cli-script.c: 	* cli/cli-logging.c: 	* cli/cli-interp.c: 	* cli/cli-dump.h: 	* cli/cli-dump.c: 	* cli/cli-decode.h: 	* cli/cli-decode.c: 	* cli/cli-cmds.h: 	* cli/cli-cmds.c: Add (c) after Copyright.  Update FSF address.

--- gdb/cli/cli-decode.c
+++ gdb/cli/cli-decode.c
@@ -1,6 +1,6 @@
 /* Handle lists of commands, their decoding and documentation, for GDB.
 
-   Copyright 1986, 1989, 1990, 1991, 1998, 2000, 2001, 2002, 2004 Free
+   Copyright (c) 1986, 1989, 1990, 1991, 1998, 2000, 2001, 2002, 2004 Free
    Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
@@ -15,8 +15,8 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
 #include "defs.h"
 #include "symtab.h"
