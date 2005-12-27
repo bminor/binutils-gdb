70949bf12c3309d6c2d38628c6a249f6dd395fd9
Alan Modra <amodra@gmail.com>
2005-12-27 04:19:15 +0000

    missed copyright 2005 addition

--- bfd/corefile.c
+++ bfd/corefile.c
@@ -1,5 +1,5 @@
 /* Core file generic interface routines for BFD.
-   Copyright 1990, 1991, 1992, 1993, 1994, 2000, 2001, 2002, 2003
+   Copyright 1990, 1991, 1992, 1993, 1994, 2000, 2001, 2002, 2003, 2005
    Free Software Foundation, Inc.
    Written by Cygnus Support.
 
