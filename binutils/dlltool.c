671659efa0f1b20e0345a3b5989044956273d0e8
Danny Smith <dannysmith@users.sourceforge.net>
2006-03-09 21:50:53 +0000

    * dlltool.c: Update copyright year. 	* doc/binutils.texi: Likewise.

--- binutils/dlltool.c
+++ binutils/dlltool.c
@@ -1,6 +1,6 @@
 /* dlltool.c -- tool to generate stuff for PE style DLLs
    Copyright 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-   2005 Free Software Foundation, Inc.
+   2005, 2006 Free Software Foundation, Inc.
 
    This file is part of GNU Binutils.
 
