736976bbb98a16df628d8d7406d80a1c86cf755e
Paul Brook <paul@codesourcery.com>
2006-04-01 22:39:54 +0000

    2005-04-01  Paul Brook  <paul@codesourcery.com>
    	* config/tc-arm.c (arm_fix_adjustable): Return 0 for function symbols.

--- gas/config/tc-arm.c
+++ gas/config/tc-arm.c
@@ -12614,9 +12614,9 @@ arm_fix_adjustable (fixS * fixP)
 #endif
 
 #ifdef OBJ_ELF
-/* Relocations against Thumb function names must be left unadjusted,
-   so that the linker can use this information to correctly set the
-   bottom bit of their addresses.  The MIPS version of this function
+/* Relocations against function names must be left unadjusted,
+   so that the linker can use this information to generate interworking
+   stubs.  The MIPS version of this function
    also prevents relocations that are mips-16 specific, but I do not
    know why it does this.
 
@@ -12632,6 +12632,10 @@ arm_fix_adjustable (fixS * fixP)
 {
   if (fixP->fx_addsy == NULL)
     return 1;
+
+  /* Preserve relocations against symbols with function type.  */
+  if (symbol_get_bfdsym (fixP->fx_addsy)->flags & BSF_FUNCTION)
+    return 0;
 
   if (THUMB_IS_FUNC (fixP->fx_addsy)
       && fixP->fx_subsy == NULL)
