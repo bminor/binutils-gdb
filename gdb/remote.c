42a20fb6e5316447aaba18a210e7bb58106bd64c
Daniel Jacobowitz <drow@false.org>
2006-04-11 18:03:01 +0000

    Backport: 	2006-04-07  Daniel Jacobowitz  <dan@codesourcery.com> 	* remote.c (remote_wait): Convert warning to error before 	parsing corrupt packets.

--- gdb/remote.c
+++ gdb/remote.c
@@ -2892,7 +2892,7 @@ remote_wait (ptid_t ptid, struct target_waitstatus *status)
 		  {
 		    p1 = strchr (p, ':');
 		    if (p1 == NULL)
-		      warning (_("Malformed packet(a) (missing colon): %s\n\
+		      error (_("Malformed packet(a) (missing colon): %s\n\
 Packet: '%s'\n"),
 			       p, buf);
 		    if (strncmp (p, "thread", p1 - p) == 0)
