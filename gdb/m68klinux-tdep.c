cba5fab953d88c17c9bcc9abad2948dcea0222d0
Daniel Jacobowitz <drow@false.org>
2006-04-20 17:29:47 +0000

    * m68klinux-tdep.c (m68k_linux_sigtramp_frame_prev_register): 	Correct type of VALUEP.  Reported by Jean-Rene Peulve 	<jr.peulve@wanadoo.fr>.

--- gdb/m68klinux-tdep.c
+++ gdb/m68klinux-tdep.c
@@ -248,7 +248,7 @@ m68k_linux_sigtramp_frame_prev_register (struct frame_info *next_frame,
 					 int regnum, int *optimizedp,
 					 enum lval_type *lvalp,
 					 CORE_ADDR *addrp,
-					 int *realnump, void *valuep)
+					 int *realnump, gdb_byte *valuep)
 {
   /* Make sure we've initialized the cache.  */
   struct trad_frame_cache *cache =
