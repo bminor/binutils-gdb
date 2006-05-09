43cc0762b25b15d4e39fe23e68fd719f03108e98
H.J. Lu <hjl.tools@gmail.com>
2006-05-09 17:05:55 +0000

    2006-05-09  H.J. Lu  <hongjiu.lu@intel.com>
    	* gas/i386/x86-64-gidt.d: Adjusted.

--- gas/testsuite/gas/i386/x86-64-gidt.d
+++ gas/testsuite/gas/i386/x86-64-gidt.d
@@ -6,12 +6,12 @@
 Disassembly of section .text:
 
 0+000 <foo>:
-   0:	0f 01 08 [ 	]*sidt  \(%rax\)
-   3:	0f 01 18 [ 	]*sgdt  \(%rax\)
-   6:	0f 01 00 [ 	]*lidt  \(%rax\)
-   9:	0f 01 10 [ 	]*lgdt  \(%rax\)
-   c:	0f 01 08 [ 	]*sidt  \(%rax\)
-   f:	0f 01 18 [ 	]*sgdt  \(%rax\)
-  12:	0f 01 00 [ 	]*lidt  \(%rax\)
-  15:	0f 01 10 [ 	]*lgdt  \(%rax\)
+   0:	0f 01 08 [ 	]*sidt   \(%rax\)
+   3:	0f 01 18 [ 	]*lidt   \(%rax\)
+   6:	0f 01 00 [ 	]*sgdt   \(%rax\)
+   9:	0f 01 10 [ 	]*lgdt   \(%rax\)
+   c:	0f 01 08 [ 	]*sidt   \(%rax\)
+   f:	0f 01 18 [ 	]*lidt   \(%rax\)
+  12:	0f 01 00 [ 	]*sgdt   \(%rax\)
+  15:	0f 01 10 [ 	]*lgdt   \(%rax\)
 	...
