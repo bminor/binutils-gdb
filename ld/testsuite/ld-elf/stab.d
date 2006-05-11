b1f2595c5de02c4758e7a82863ad769122be982e
Alan Modra <amodra@gmail.com>
2006-05-11 12:35:44 +0000

    * ld-elf/stab.d: New.

---
+++ ld/testsuite/ld-elf/stab.d
@@ -0,0 +1,10 @@
+#source: start.s
+#as: -gstabs
+#readelf: -S --wide
+#ld:
+
+#...
+  \[[0-9 ][0-9]\] \.stab +PROGBITS +0+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ +[1-9]+ +0.*
+#...
+  \[[0-9 ][0-9]\] \.stabstr +STRTAB +0+ [0-9a-f]+ [0-9a-f]+ 00 +0 +0.*
+#...
