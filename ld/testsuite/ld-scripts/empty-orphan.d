c6f90868f62fd8872d2b2052092214d8d59a70d6
Alan Modra <amodra@gmail.com>
2006-05-19 06:10:03 +0000

    * ld-scripts/empty-orphan.t: Discard .reginfo. 	* ld-scripts/empty-orphan.d: Update.

--- ld/testsuite/ld-scripts/empty-orphan.d
+++ ld/testsuite/ld-scripts/empty-orphan.d
@@ -1,3 +1,10 @@
 #source: empty-orphan.s
 #ld: -T empty-orphan.t
-#error: no memory region specified for loadable section
+#readelf: -l --wide
+#...
+Program Headers:
+ +Type +Offset +VirtAddr +PhysAddr +FileSiz +MemSiz +Flg +Align
+ +LOAD +[x0-9a-f]+ 0x0+ 0x0+ 0x0+ 0x0+ .*
+ +LOAD +[x0-9a-f]+ 0x0+ 0x0+ 0x0+ 0x0+ .*
+ +LOAD +[x0-9a-f]+ [x0]+70000000 [x0]+70000000 [x0]+(2|4|8|10|20|40|80) .*
+#pass
