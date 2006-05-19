9e328bda631071fef5a161aaa7d754a5641ae29a
Alan Modra <amodra@gmail.com>
2006-05-19 11:50:31 +0000

    * ld-scripts/empty-orphan.d: Update again.

--- ld/testsuite/ld-scripts/empty-orphan.d
+++ ld/testsuite/ld-scripts/empty-orphan.d
@@ -2,9 +2,5 @@
 #ld: -T empty-orphan.t
 #readelf: -l --wide
 #...
-Program Headers:
- +Type +Offset +VirtAddr +PhysAddr +FileSiz +MemSiz +Flg +Align
- +LOAD +[x0-9a-f]+ 0x0+ 0x0+ 0x0+ 0x0+ .*
- +LOAD +[x0-9a-f]+ 0x0+ 0x0+ 0x0+ 0x0+ .*
  +LOAD +[x0-9a-f]+ [x0]+70000000 [x0]+70000000 [x0]+(2|4|8|10|20|40|80) .*
 #pass
