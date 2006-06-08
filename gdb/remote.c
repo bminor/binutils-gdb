1dd41f16d1f2ad0257298ebccc2fb0ce66da5532
Nathan Sidwell <nathan@codesourcery.com>
2006-06-08 19:04:08 +0000

    * gdb/remote.c (remote_open_1): Do preopen tasks before 	irreversably destroying state.

--- gdb/remote.c
+++ gdb/remote.c
@@ -2069,12 +2069,12 @@ remote_open_1 (char *name, int from_tty, struct target_ops *target,
   if (!async_p)
     wait_forever_enabled_p = 1;
 
-  reopen_exec_file ();
-  reread_symbols ();
-
   target_preopen (from_tty);
 
   unpush_target (target);
+
+  reopen_exec_file ();
+  reread_symbols ();
 
   remote_desc = remote_serial_open (name);
   if (!remote_desc)
