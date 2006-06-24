a87866e601c62a7d911022031db461952453cb38
Richard Sandiford <rdsandiford@googlemail.com>
2006-06-24 09:10:04 +0000

    binutils/testsuite/ 	* binutils-all/localize-hidden-1.d: Use objdump --syms instead 	of readelf.

--- binutils/testsuite/binutils-all/localize-hidden-1.d
+++ binutils/testsuite/binutils-all/localize-hidden-1.d
@@ -1,17 +1,19 @@
 #PROG: objcopy
-#readelf: --symbols
+#objdump: --syms
 #objcopy: --localize-hidden
 #name: --localize-hidden test 1
 #...
-.*: 0+1200 +0 +NOTYPE +LOCAL +HIDDEN +ABS +Lhidden
-.*: 0+1300 +0 +NOTYPE +LOCAL +INTERNAL +ABS +Linternal
-.*: 0+1400 +0 +NOTYPE +LOCAL +PROTECTED +ABS +Lprotected
-.*: 0+1100 +0 +NOTYPE +LOCAL +DEFAULT +ABS +Ldefault
-.*: 0+2200 +0 +NOTYPE +LOCAL +HIDDEN +ABS +Ghidden
-.*: 0+2300 +0 +NOTYPE +LOCAL +INTERNAL +ABS +Ginternal
-.*: 0+3200 +0 +NOTYPE +LOCAL +HIDDEN +ABS +Whidden
-.*: 0+3300 +0 +NOTYPE +LOCAL +INTERNAL +ABS +Winternal
-.*: 0+2100 +0 +NOTYPE +GLOBAL +DEFAULT +ABS +Gdefault
-.*: 0+2400 +0 +NOTYPE +GLOBAL +PROTECTED +ABS +Gprotected
-.*: 0+3100 +0 +NOTYPE +WEAK +DEFAULT +ABS +Wdefault
-.*: 0+3400 +0 +NOTYPE +WEAK +PROTECTED +ABS +Wprotected
+0+1200 l .*\*ABS\*	0+ \.hidden Lhidden
+0+1300 l .*\*ABS\*	0+ \.internal Linternal
+0+1400 l .*\*ABS\*	0+ \.protected Lprotected
+0+1100 l .*\*ABS\*	0+ Ldefault
+#...
+0+2200 l .*\*ABS\*	0+ \.hidden Ghidden
+0+2300 l .*\*ABS\*	0+ \.internal Ginternal
+0+3200 l .*\*ABS\*	0+ \.hidden Whidden
+0+3300 l .*\*ABS\*	0+ \.internal Winternal
+0+2100 g .*\*ABS\*	0+ Gdefault
+0+2400 g .*\*ABS\*	0+ \.protected Gprotected
+0+3100  w.*\*ABS\*	0+ Wdefault
+0+3400  w.*\*ABS\*	0+ \.protected Wprotected
+#pass
