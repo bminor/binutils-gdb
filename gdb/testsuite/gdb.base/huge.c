f6c957d29dcf36f30d86e6c76501f7d3be03ad9c
Nathan Sidwell <nathan@codesourcery.com>
2006-07-04 09:40:28 +0000

    * gdb.base/huge.c: Allow CRASH_GDB to be set from command 	line. 	* gdb.base/huge.exp: Loop over compilation to find a size that 	is acceptable. 	* gdb.base/remote.exp: Correct expected strings. 	* gdb.dwarf2/mac-fileno.S: Avoid # comments.

--- gdb/testsuite/gdb.base/huge.c
+++ gdb/testsuite/gdb.base/huge.c
@@ -7,9 +7,9 @@
 /* A value that will produce a target data object large enough to
    crash GDB.  0x200000 is big enough on GNU/Linux, other systems may
    need a larger number.  */
-
+#ifndef CRASH_GDB
 #define CRASH_GDB 0x200000
-
+#endif
 static int a[CRASH_GDB], b[CRASH_GDB];
 
 main()
