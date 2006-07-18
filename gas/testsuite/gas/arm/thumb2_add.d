509d475f5bba8e79b69030c495247ee26fb3dec0
nobody <>
2006-07-18 16:44:48 +0000

    This commit was manufactured by cvs2svn to create branch 'binutils-csl- 2_17-branch'.
    Cherrypick from master 2006-07-18 16:44:47 UTC Paul Brook <paul@codesourcery.com> '2006-07-18  Paul Brook  <paul@codesourcery.com>':
        gas/testsuite/gas/arm/thumb2_add.d
        gas/testsuite/gas/arm/thumb2_add.s

---
+++ gas/testsuite/gas/arm/thumb2_add.d
@@ -0,0 +1,18 @@
+# as: -march=armv6kt2
+# objdump: -dr --prefix-addresses --show-raw-insn
+
+.*: +file format .*arm.*
+
+Disassembly of section .text:
+0+000 <[^>]+> f60f 0000 	addw	r0, pc, #2048	; 0x800
+0+004 <[^>]+> f20f 0900 	addw	r9, pc, #0	; 0x0
+0+008 <[^>]+> f20f 4900 	addw	r9, pc, #1024	; 0x400
+0+00c <[^>]+> f509 6880 	add.w	r8, r9, #1024	; 0x400
+0+010 <[^>]+> f209 1801 	addw	r8, r9, #257	; 0x101
+0+014 <[^>]+> f201 1301 	addw	r3, r1, #257	; 0x101
+0+018 <[^>]+> f6af 0000 	subw	r0, pc, #2048	; 0x800
+0+01c <[^>]+> f2af 0900 	subw	r9, pc, #0	; 0x0
+0+020 <[^>]+> f2af 4900 	subw	r9, pc, #1024	; 0x400
+0+024 <[^>]+> f5a9 6880 	sub.w	r8, r9, #1024	; 0x400
+0+028 <[^>]+> f2a9 1801 	subw	r8, r9, #257	; 0x101
+0+02c <[^>]+> f2a1 1301 	subw	r3, r1, #257	; 0x101
---
+++ gas/testsuite/gas/arm/thumb2_add.s
@@ -0,0 +1,20 @@
+	.syntax unified
+	.text
+	.align	2
+	.global	thumb2_add
+	.thumb
+	.thumb_func
+	.type	thumb2_add, %function
+thumb2_add:
+	add r0, pc, #0x800
+	add r9, pc, #0
+	add r9, pc, #0x400
+	add r8, r9, #0x400
+	add r8, r9, #0x101
+	add r3, r1, #0x101
+	sub r0, pc, #0x800
+	sub r9, pc, #0
+	sub r9, pc, #0x400
+	sub r8, r9, #0x400
+	sub r8, r9, #0x101
+	sub r3, r1, #0x101
