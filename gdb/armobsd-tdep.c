e3ac4a1e11ab967067fda23a41cc77c5b3dc3382
Mark Kettenis <kettenis@gnu.org>
2006-07-18 21:10:28 +0000

    * armobsd-tdep.c: Include "trad-frame.h" and "tramp-frame.h". (armobsd_sigframe_init): New function. (armobsd_sigframe): New variable. (armobsd_init_abi): Prepend armobsd_sigframe unwinder. * Makefile.in (armobsd-tdep.o): Update dependencies.

--- gdb/armobsd-tdep.c
+++ gdb/armobsd-tdep.c
@@ -21,10 +21,54 @@
 
 #include "defs.h"
 #include "osabi.h"
+#include "trad-frame.h"
+#include "tramp-frame.h"
 
 #include "obsd-tdep.h"
 #include "arm-tdep.h"
 #include "solib-svr4.h"
+
+/* Signal trampolines.  */
+
+static void
+armobsd_sigframe_init (const struct tramp_frame *self,
+		       struct frame_info *next_frame,
+		       struct trad_frame_cache *cache,
+		       CORE_ADDR func)
+{
+  CORE_ADDR sp, sigcontext_addr, addr;
+  int regnum;
+
+  /* We find the appropriate instance of `struct sigcontext' at a
+     fixed offset in the signal frame.  */
+  sp = frame_unwind_register_signed (next_frame, ARM_SP_REGNUM);
+  sigcontext_addr = sp + 16;
+
+  /* PC.  */
+  trad_frame_set_reg_addr (cache, ARM_PC_REGNUM, sigcontext_addr + 76);
+
+  /* GPRs.  */
+  for (regnum = ARM_A1_REGNUM, addr = sigcontext_addr + 12;
+       regnum <= ARM_LR_REGNUM; regnum++, addr += 4)
+    trad_frame_set_reg_addr (cache, regnum, addr);
+
+  trad_frame_set_id (cache, frame_id_build (sp, func));
+}
+
+static const struct tramp_frame armobsd_sigframe =
+{
+  SIGTRAMP_FRAME,
+  4,
+  {
+    { 0xe28d0010, -1 },		/* add     r0, sp, #16 */
+    { 0xef000067, -1 },		/* swi     SYS_sigreturn */
+    { 0xef000001, -1 },		/* swi     SYS_exit */
+    { 0xeafffffc, -1 },		/* b       . - 8 */
+    { TRAMP_SENTINEL_INSN, -1 }
+  },
+  armobsd_sigframe_init
+};
+
 
 static void
 armobsd_init_abi (struct gdbarch_info info,
@@ -34,6 +78,8 @@ armobsd_init_abi (struct gdbarch_info info,
 
   if (tdep->fp_model == ARM_FLOAT_AUTO)
     tdep->fp_model = ARM_FLOAT_SOFT_VFP;
+
+  tramp_frame_prepend_unwinder (gdbarch, &armobsd_sigframe);
 
   /* OpenBSD/arm uses SVR4-style shared libraries.  */
   set_solib_svr4_fetch_link_map_offsets
