0152ebd7bc3baa16eff4729d88f25dd1051b3614
Vladimir Prus <vladimir@codesourcery.com>
2006-07-18 12:48:20 +0000

    * defs.h (print_transfer_performance): Improve comments

--- gdb/defs.h
+++ gdb/defs.h
@@ -565,7 +565,13 @@ extern void symbol_file_command (char *, int);
 /* Remote targets may wish to use this as their load function.  */
 extern void generic_load (char *name, int from_tty);
 
-/* Summarise a download */
+/* Report on STREAM the performance of memory transfer operation,
+   such as 'load'. 
+   DATA_COUNT is the number of bytes transferred.
+   WRITE_COUNT is the number of separate write operations, or 0,
+   if that information is not available.
+   START_TIME is the time at which an operation was started.
+   END_TIME is the time at which an operation ended.  */
 struct timeval;
 extern void print_transfer_performance (struct ui_file *stream,
 					unsigned long data_count,
