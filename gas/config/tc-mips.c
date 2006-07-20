d5f010e93bf3770eb8c443eb0b502c299860a1d8
Thiemo Seufer <ths@networkno.de>
2006-07-20 16:51:38 +0000

    * config/tc-mips.c (md_parse_option): Don't infer optimisation 	options from debug options.

--- gas/config/tc-mips.c
+++ gas/config/tc-mips.c
@@ -10992,11 +10992,6 @@ md_parse_option (int c, char *arg)
 	mips_debug = 2;
       else
 	mips_debug = atoi (arg);
-      /* When the MIPS assembler sees -g or -g2, it does not do
-         optimizations which limit full symbolic debugging.  We take
-         that to be equivalent to -O0.  */
-      if (mips_debug == 2)
-	mips_optimize = 1;
       break;
 
     case OPTION_MIPS1:
