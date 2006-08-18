7c9e8d98112842cba7fde29e05ab9860c9098b43
H.J. Lu <hjl.tools@gmail.com>
2006-08-18 14:38:00 +0000

    2006-08-18  H.J. Lu  <hongjiu.lu@intel.com>
    	PR ld/3052
    	* ld-elf/loadaddr.s: New file.
    	* ld-elf/loadaddr1.d: Likewise.
    	* ld-elf/loadaddr1.t: Likewise.
    	* ld-elf/loadaddr2.d: Likewise.
    	* ld-elf/loadaddr2.t: Likewise.

---
+++ ld/testsuite/ld-elf/loadaddr.s
@@ -0,0 +1,16 @@
+	.text
+	.globl main
+	.globl start
+	.globl _start
+	.globl __start
+main:
+start:
+_start:
+__start:
+	.byte 0
+	.section .bar,"ax","progbits"
+	.byte 0
+	.section .foo,"aw","progbits"
+	.byte 0
+	.data
+	.byte 0
