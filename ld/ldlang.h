49c13adb16c432e9d04770f7c6b8348e31bb1df1
H.J. Lu <hjl.tools@gmail.com>
2006-09-27 04:18:16 +0000

    bfd/
    2006-09-26  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR ld/3223
    	PR ld/3267
    	* elf.c (assign_file_positions_for_non_load_sections): Don't
    	warn zero size allocated sections.
    
    ld/
    
    2006-09-26  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR ld/3223
    	PR ld/3267
    	* ld.texinfo: Updated Output Section Discarding.
    
    	* ldlang.h (lang_output_section_statement_type): Add
    	section_relative_symbol.
    	* ldlang.c (strip_excluded_output_sections): Don't strip a
    	section with a symbol relative to it.
    	(lang_size_sections_1): Mark if an output section has a symbol
    	symbol relative to it.
    
    ld/testsuite/
    
    2006-09-26  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR ld/3223
    	PR ld/3267
    	* ld-scripts/empty-address-1.d: New file.
    	* ld-scripts/empty-address-1.s: Likewise.
    	* ld-scripts/empty-address-1.t: Likewise.
    	* ld-scripts/empty-address-2.s: Likewise.
    	* ld-scripts/empty-address-2a.d: Likewise.
    	* ld-scripts/empty-address-2a.t: Likewise.
    	* ld-scripts/empty-address-2b.d: Likewise.
    	* ld-scripts/empty-address-2b.t: Likewise.
    	* ld-scripts/empty-address-3.s: Likewise.
    	* ld-scripts/empty-address-3a.d: Likewise.
    	* ld-scripts/empty-address-3a.t: Likewise.
    	* ld-scripts/empty-address-3b.d: Likewise.
    	* ld-scripts/empty-address-3b.t: Likewise.
    	* ld-scripts/empty-address-3c.d: Likewise.
    	* ld-scripts/empty-address-3c.t: Likewise.
    	* ld-scripts/empty-address.exp: Likewise.

--- ld/ldlang.h
+++ ld/ldlang.h
@@ -153,7 +153,10 @@ typedef struct lang_output_section_statement_struct
   unsigned int processed_vma : 1;
   unsigned int processed_lma : 1;
   unsigned int all_input_readonly : 1;
+  /* If this section should be ignored.  */
   unsigned int ignored : 1; 
+  /* If there is a symbol relative to this section.  */
+  unsigned int section_relative_symbol : 1; 
 } lang_output_section_statement_type;
 
 typedef struct
