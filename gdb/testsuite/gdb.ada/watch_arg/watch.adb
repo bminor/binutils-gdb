098514179bf63599ca6bdbdf6975eba5056e9b49
Joel Brobecker <brobecker@gnat.com>
2006-10-06 17:46:56 +0000

    * gdb.ada/watch_arg/watch.adb: New file.         * gdb.ada/watch_arg.exp: New testcase.

---
+++ gdb/testsuite/gdb.ada/watch_arg/watch.adb
@@ -0,0 +1,31 @@
+--  Copyright 2006 Free Software Foundation, Inc.
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+--  MA 02110-1301, USA
+
+procedure Watch is
+
+   procedure Foo (X : access Integer) is
+   begin
+      X.all := 3;  -- BREAK1
+   end Foo;
+
+   X : aliased Integer := 1;
+
+begin
+   Foo (X'Access);
+   X := 2;  -- BREAK2
+end Watch;
+
