7e1fa4ad31e7789ed94d2339c7175047a107cfb0
Richard Sandiford <rdsandiford@googlemail.com>
2006-10-18 12:34:19 +0000

    ld/testsuite/ 	* ld-mips-elf/rel32-n32.d: Bump addresses by 0x30 to account for 	the fact that .reginfo is now placed before .text.  Change the 	section number of .text accordingly.

--- ld/testsuite/ld-mips-elf/rel32-n32.d
+++ ld/testsuite/ld-mips-elf/rel32-n32.d
@@ -1,7 +1,7 @@
 #name: MIPS rel32 n32
 #source: rel32.s
 #as: -KPIC -EB -n32
-#readelf: -x 5 -r
+#readelf: -x 6 -r
 #ld: -shared -melf32btsmipn32
 
 Relocation section '.rel.dyn' at offset .* contains 2 entries:
@@ -10,6 +10,6 @@ Relocation section '.rel.dyn' at offset .* contains 2 entries:
 [0-9a-f ]+R_MIPS_REL32     
 
 Hex dump of section '.text':
-  0x000002b0 00000000 00000000 00000000 00000000 ................
-  0x000002c0 000002c0 00000000 00000000 00000000 ................
   0x000002d0 00000000 00000000 00000000 00000000 ................
+  0x000002e0 000002e0 00000000 00000000 00000000 ................
+  0x000002f0 00000000 00000000 00000000 00000000 ................
