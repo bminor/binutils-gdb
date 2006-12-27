b534c6d307154911d8b55cc8722e575044ca0196
H.J. Lu <hjl.tools@gmail.com>
2006-12-27 18:34:08 +0000

    2006-12-27  H.J. Lu  <hongjiu.lu@intel.com>
    	* config/tc-i386.c: Document tc-i386.c, not i386.c.

--- gas/config/tc-i386.c
+++ gas/config/tc-i386.c
@@ -1,4 +1,4 @@
-/* i386.c -- Assemble code for the Intel 80386
+/* tc-i386.c -- Assemble code for the Intel 80386
    Copyright 1989, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
    2000, 2001, 2002, 2003, 2004, 2005, 2006
    Free Software Foundation, Inc.
