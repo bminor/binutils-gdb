e47251a9cb8ab12784843155e1f61cf0595b6047
H.J. Lu <hjl.tools@gmail.com>
2007-01-17 19:17:15 +0000

    2007-01-17  H.J. Lu  <hongjiu.lu@intel.com>
    	* ld-elf/header.d: Adjust for .text section with 16byte
    	alignment.

--- ld/testsuite/ld-elf/header.d
+++ ld/testsuite/ld-elf/header.d
@@ -5,9 +5,9 @@
 #...
 Program Header:
     LOAD off    0x0*0000000 vaddr 0x0*0010000 paddr 0x0*0010000 align 2..16
-         filesz 0x0*001002[48] memsz 0x0*001002[48] flags rwx
+         filesz 0x0*00100[23][048] memsz 0x0*00100[23][048] flags rwx
 #...
 Sections:
 Idx Name          Size      VMA       *LMA       *File off  Algn  Flags
-  0 .text         0*000ffac  0*001007[48]  0*001007[48]  0*000007[48]  2...  CONTENTS, ALLOC, LOAD, READONLY, CODE
-  1 .data         0*0000004  0*002002[04]  0*002002[04]  0*001002[04]  2...  CONTENTS, ALLOC, LOAD, DATA
+  0 .text         0*000ffac  0*00100[78][048]  0*00100[78][048]  0*00000[78][048]  2...  CONTENTS, ALLOC, LOAD, READONLY, CODE
+  1 .data         0*0000004  0*002002[04c]  0*002002[04c]  0*001002[04c]  2...  CONTENTS, ALLOC, LOAD, DATA
