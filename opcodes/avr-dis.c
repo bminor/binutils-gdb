c4f5c3d74c9a860cf1c762e2deb906212a71864c
H.J. Lu <hjl.tools@gmail.com>
2007-02-02 22:54:50 +0000

    2007-02-02  H.J. Lu  <hongjiu.lu@intel.com>
    	* avr-dis.c (avr_operand): Correct PR number in comment.

--- opcodes/avr-dis.c
+++ opcodes/avr-dis.c
@@ -140,7 +140,7 @@ avr_operand (unsigned int insn, unsigned int insn2, unsigned int pc, int constra
     case 'h':
       *sym = 1;
       *sym_addr = ((((insn & 1) | ((insn & 0x1f0) >> 3)) << 16) | insn2) * 2;
-      /* See PR binutils/2545.  Ideally we would like to display the hex
+      /* See PR binutils/2454.  Ideally we would like to display the hex
 	 value of the address only once, but this would mean recoding
 	 objdump_print_address() which would affect many targets.  */
       sprintf (buf, "%#lx", (unsigned long) *sym_addr);      
