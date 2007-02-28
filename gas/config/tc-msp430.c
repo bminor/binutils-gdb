870074ddaece1635f45566c00e2bae2a670b1149
Nick Clifton <nickc@redhat.com>
2007-02-28 10:02:37 +0000

    PR gas/2623 * config/tc-msp430.c (line_separator_char): Change to '{'.

--- gas/config/tc-msp430.c
+++ gas/config/tc-msp430.c
@@ -166,7 +166,7 @@ static struct hcodes_s msp430_hcodes[] =
 
 const char comment_chars[] = ";";
 const char line_comment_chars[] = "#";
-const char line_separator_chars[] = "|";
+const char line_separator_chars[] = "{";
 const char EXP_CHARS[] = "eE";
 const char FLT_CHARS[] = "dD";
 
