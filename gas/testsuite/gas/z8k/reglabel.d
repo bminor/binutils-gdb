46a1babec9afbac16e735fde40b823b29a13e7cc
Christian Groessler <chris@groessler.org>
2007-03-31 22:28:06 +0000

    * gas/z8k/calr.d: Fix for 64bit bfd. 	* gas/z8k/djnz.d: Likewise. 	* gas/z8k/inout.d: Likewise. 	* gas/z8k/jmp-cc.d: Likewise. 	* gas/z8k/jr-back.d: Likewise. 	* gas/z8k/jr-forw.d: Likewise. 	* gas/z8k/reglabel.d: Likewise. 	* gas/z8k/ctrl-names.d: Fix name.  Fix for 64bit bfd. 	* gas/z8k/ret-cc.d: Likewise.

--- gas/testsuite/gas/z8k/reglabel.d
+++ gas/testsuite/gas/z8k/reglabel.d
@@ -6,263 +6,263 @@
 
 Disassembly of section \.text:
 
-00000000 <sp_label>:
+0*00000000 <sp_label>:
    0:	7600 0000      	lda	r0,0x0
 
-00000004 <r0_label>:
+0*00000004 <r0_label>:
    4:	7600 0004      	lda	r0,0x4
 
-00000008 <r1_label>:
+0*00000008 <r1_label>:
    8:	7600 0008      	lda	r0,0x8
 
-0000000c <r2_label>:
+0*0000000c <r2_label>:
    c:	7600 000c      	lda	r0,0xc
 
-00000010 <r3_label>:
+0*00000010 <r3_label>:
   10:	7600 0010      	lda	r0,0x10
 
-00000014 <r4_label>:
+0*00000014 <r4_label>:
   14:	7600 0014      	lda	r0,0x14
 
-00000018 <r5_label>:
+0*00000018 <r5_label>:
   18:	7600 0018      	lda	r0,0x18
 
-0000001c <r6_label>:
+0*0000001c <r6_label>:
   1c:	7600 001c      	lda	r0,0x1c
 
-00000020 <r7_label>:
+0*00000020 <r7_label>:
   20:	7600 0020      	lda	r0,0x20
 
-00000024 <r8_label>:
+0*00000024 <r8_label>:
   24:	7600 0024      	lda	r0,0x24
 
-00000028 <r9_label>:
+0*00000028 <r9_label>:
   28:	7600 0028      	lda	r0,0x28
 
-0000002c <r10_label>:
+0*0000002c <r10_label>:
   2c:	7600 002c      	lda	r0,0x2c
 
-00000030 <r11_label>:
+0*00000030 <r11_label>:
   30:	7600 0030      	lda	r0,0x30
 
-00000034 <r12_label>:
+0*00000034 <r12_label>:
   34:	7600 0034      	lda	r0,0x34
 
-00000038 <r13_label>:
+0*00000038 <r13_label>:
   38:	7600 0038      	lda	r0,0x38
 
-0000003c <r14_label>:
+0*0000003c <r14_label>:
   3c:	7600 003c      	lda	r0,0x3c
 
-00000040 <r15_label>:
+0*00000040 <r15_label>:
   40:	7600 0040      	lda	r0,0x40
 
-00000044 <r16_label>:
+0*00000044 <r16_label>:
   44:	7600 0044      	lda	r0,0x44
 
-00000048 <rr0_label>:
+0*00000048 <rr0_label>:
   48:	7600 0048      	lda	r0,0x48
 
-0000004c <rr1_label>:
+0*0000004c <rr1_label>:
   4c:	7600 004c      	lda	r0,0x4c
 
-00000050 <rr2_label>:
+0*00000050 <rr2_label>:
   50:	7600 0050      	lda	r0,0x50
 
-00000054 <rr3_label>:
+0*00000054 <rr3_label>:
   54:	7600 0054      	lda	r0,0x54
 
-00000058 <rr4_label>:
+0*00000058 <rr4_label>:
   58:	7600 0058      	lda	r0,0x58
 
-0000005c <rr5_label>:
+0*0000005c <rr5_label>:
   5c:	7600 005c      	lda	r0,0x5c
 
-00000060 <rr6_label>:
+0*00000060 <rr6_label>:
   60:	7600 0060      	lda	r0,0x60
 
-00000064 <rr7_label>:
+0*00000064 <rr7_label>:
   64:	7600 0064      	lda	r0,0x64
 
-00000068 <rr8_label>:
+0*00000068 <rr8_label>:
   68:	7600 0068      	lda	r0,0x68
 
-0000006c <rr9_label>:
+0*0000006c <rr9_label>:
   6c:	7600 006c      	lda	r0,0x6c
 
-00000070 <rr10_label>:
+0*00000070 <rr10_label>:
   70:	7600 0070      	lda	r0,0x70
 
-00000074 <rr11_label>:
+0*00000074 <rr11_label>:
   74:	7600 0074      	lda	r0,0x74
 
-00000078 <rr12_label>:
+0*00000078 <rr12_label>:
   78:	7600 0078      	lda	r0,0x78
 
-0000007c <rr13_label>:
+0*0000007c <rr13_label>:
   7c:	7600 007c      	lda	r0,0x7c
 
-00000080 <rr14_label>:
+0*00000080 <rr14_label>:
   80:	7600 0080      	lda	r0,0x80
 
-00000084 <rr15_label>:
+0*00000084 <rr15_label>:
   84:	7600 0084      	lda	r0,0x84
 
-00000088 <rr16_label>:
+0*00000088 <rr16_label>:
   88:	7600 0088      	lda	r0,0x88
 
-0000008c <rq0_label>:
+0*0000008c <rq0_label>:
   8c:	7600 008c      	lda	r0,0x8c
 
-00000090 <rq1_label>:
+0*00000090 <rq1_label>:
   90:	7600 0090      	lda	r0,0x90
 
-00000094 <rq2_label>:
+0*00000094 <rq2_label>:
   94:	7600 0094      	lda	r0,0x94
 
-00000098 <rq3_label>:
+0*00000098 <rq3_label>:
   98:	7600 0098      	lda	r0,0x98
 
-0000009c <rq4_label>:
+0*0000009c <rq4_label>:
   9c:	7600 009c      	lda	r0,0x9c
 
-000000a0 <rq5_label>:
+0*000000a0 <rq5_label>:
   a0:	7600 00a0      	lda	r0,0xa0
 
-000000a4 <rq6_label>:
+0*000000a4 <rq6_label>:
   a4:	7600 00a4      	lda	r0,0xa4
 
-000000a8 <rq7_label>:
+0*000000a8 <rq7_label>:
   a8:	7600 00a8      	lda	r0,0xa8
 
-000000ac <rq8_label>:
+0*000000ac <rq8_label>:
   ac:	7600 00ac      	lda	r0,0xac
 
-000000b0 <rq9_label>:
+0*000000b0 <rq9_label>:
   b0:	7600 00b0      	lda	r0,0xb0
 
-000000b4 <rq10_label>:
+0*000000b4 <rq10_label>:
   b4:	7600 00b4      	lda	r0,0xb4
 
-000000b8 <rq11_label>:
+0*000000b8 <rq11_label>:
   b8:	7600 00b8      	lda	r0,0xb8
 
-000000bc <rq12_label>:
+0*000000bc <rq12_label>:
   bc:	7600 00bc      	lda	r0,0xbc
 
-000000c0 <rq13_label>:
+0*000000c0 <rq13_label>:
   c0:	7600 00c0      	lda	r0,0xc0
 
-000000c4 <rq14_label>:
+0*000000c4 <rq14_label>:
   c4:	7600 00c4      	lda	r0,0xc4
 
-000000c8 <rq15_label>:
+0*000000c8 <rq15_label>:
   c8:	7600 00c8      	lda	r0,0xc8
 
-000000cc <rq16_label>:
+0*000000cc <rq16_label>:
   cc:	7600 00cc      	lda	r0,0xcc
 
-000000d0 <rh0_label>:
+0*000000d0 <rh0_label>:
   d0:	7600 00d0      	lda	r0,0xd0
 
-000000d4 <rh1_label>:
+0*000000d4 <rh1_label>:
   d4:	7600 00d4      	lda	r0,0xd4
 
-000000d8 <rh2_label>:
+0*000000d8 <rh2_label>:
   d8:	7600 00d8      	lda	r0,0xd8
 
-000000dc <rh3_label>:
+0*000000dc <rh3_label>:
   dc:	7600 00dc      	lda	r0,0xdc
 
-000000e0 <rh4_label>:
+0*000000e0 <rh4_label>:
   e0:	7600 00e0      	lda	r0,0xe0
 
-000000e4 <rh5_label>:
+0*000000e4 <rh5_label>:
   e4:	7600 00e4      	lda	r0,0xe4
 
-000000e8 <rh6_label>:
+0*000000e8 <rh6_label>:
   e8:	7600 00e8      	lda	r0,0xe8
 
-000000ec <rh7_label>:
+0*000000ec <rh7_label>:
   ec:	7600 00ec      	lda	r0,0xec
 
-000000f0 <rh8_label>:
+0*000000f0 <rh8_label>:
   f0:	7600 00f0      	lda	r0,0xf0
 
-000000f4 <rh9_label>:
+0*000000f4 <rh9_label>:
   f4:	7600 00f4      	lda	r0,0xf4
 
-000000f8 <rh10_label>:
+0*000000f8 <rh10_label>:
   f8:	7600 00f8      	lda	r0,0xf8
 
-000000fc <rh11_label>:
+0*000000fc <rh11_label>:
   fc:	7600 00fc      	lda	r0,0xfc
 
-00000100 <rh12_label>:
+0*00000100 <rh12_label>:
  100:	7600 0100      	lda	r0,0x100
 
-00000104 <rh13_label>:
+0*00000104 <rh13_label>:
  104:	7600 0104      	lda	r0,0x104
 
-00000108 <rh14_label>:
+0*00000108 <rh14_label>:
  108:	7600 0108      	lda	r0,0x108
 
-0000010c <rh15_label>:
+0*0000010c <rh15_label>:
  10c:	7600 010c      	lda	r0,0x10c
 
-00000110 <rh16_label>:
+0*00000110 <rh16_label>:
  110:	7600 0110      	lda	r0,0x110
 
-00000114 <rl0_label>:
+0*00000114 <rl0_label>:
  114:	7600 0114      	lda	r0,0x114
 
-00000118 <rl1_label>:
+0*00000118 <rl1_label>:
  118:	7600 0118      	lda	r0,0x118
 
-0000011c <rl2_label>:
+0*0000011c <rl2_label>:
  11c:	7600 011c      	lda	r0,0x11c
 
-00000120 <rl3_label>:
+0*00000120 <rl3_label>:
  120:	7600 0120      	lda	r0,0x120
 
-00000124 <rl4_label>:
+0*00000124 <rl4_label>:
  124:	7600 0124      	lda	r0,0x124
 
-00000128 <rl5_label>:
+0*00000128 <rl5_label>:
  128:	7600 0128      	lda	r0,0x128
 
-0000012c <rl6_label>:
+0*0000012c <rl6_label>:
  12c:	7600 012c      	lda	r0,0x12c
 
-00000130 <rl7_label>:
+0*00000130 <rl7_label>:
  130:	7600 0130      	lda	r0,0x130
 
-00000134 <rl8_label>:
+0*00000134 <rl8_label>:
  134:	7600 0134      	lda	r0,0x134
 
-00000138 <rl9_label>:
+0*00000138 <rl9_label>:
  138:	7600 0138      	lda	r0,0x138
 
-0000013c <rl10_label>:
+0*0000013c <rl10_label>:
  13c:	7600 013c      	lda	r0,0x13c
 
-00000140 <rl11_label>:
+0*00000140 <rl11_label>:
  140:	7600 0140      	lda	r0,0x140
 
-00000144 <rl12_label>:
+0*00000144 <rl12_label>:
  144:	7600 0144      	lda	r0,0x144
 
-00000148 <rl13_label>:
+0*00000148 <rl13_label>:
  148:	7600 0148      	lda	r0,0x148
 
-0000014c <rl14_label>:
+0*0000014c <rl14_label>:
  14c:	7600 014c      	lda	r0,0x14c
 
-00000150 <rl15_label>:
+0*00000150 <rl15_label>:
  150:	7600 0150      	lda	r0,0x150
 
-00000154 <rl16_label>:
+0*00000154 <rl16_label>:
  154:	7600 0154      	lda	r0,0x154
 
-00000158 <r00_label>:
+0*00000158 <r00_label>:
  158:	7600 0158      	lda	r0,0x158
