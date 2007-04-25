495443d4ec9bf03e6a9e1f418593b321aed06816
Alan Modra <amodra@gmail.com>
2007-04-25 06:32:13 +0000

    * sysdep.h: Revert last change.

--- bfd/sysdep.h
+++ bfd/sysdep.h
@@ -22,6 +22,8 @@ Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA. 
 #ifndef BFD_SYSDEP_H
 #define BFD_SYSDEP_H
 
+#include "ansidecl.h"
+
 #include "config.h"
 
 #ifdef HAVE_STDDEF_H
