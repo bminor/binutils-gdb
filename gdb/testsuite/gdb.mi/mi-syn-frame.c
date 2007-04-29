d084b3317a56e70846746ea30faeeb61a47eea71
Daniel Jacobowitz <drow@false.org>
2007-04-29 16:20:50 +0000

    * gdb.mi/mi-syn-frame.c (main, foo, bar) 	(have_a_very_merry_interrupt): Remove calls to puts. 	* gdb.mi/mi-syn-frame.exp, gdb.mi/mi2-syn-frame.exp: Do not expect 	inferior output. 	* lib/mi-support.exp (mi_gdb_test): Check gdb,noinferiorio.  Correct 	test names.

--- gdb/testsuite/gdb.mi/mi-syn-frame.c
+++ gdb/testsuite/gdb.mi/mi-syn-frame.c
@@ -11,28 +11,21 @@ void have_a_very_merry_interrupt (void);
 
 main ()
 {
-  puts ("Starting up");
-
   foo ();   /* Put a breakpoint on foo() and call it to see a dummy frame */
 
 
   have_a_very_merry_interrupt ();
-
-  puts ("Shutting down");
 }
 
 void
 foo (void)
 {
-  puts ("hi in foo");
 }
 
 void 
 bar (void)
 {
   char *nuller = 0;
-
-  puts ("hi in bar");
 
   *nuller = 'a';      /* try to cause a segfault */
 }
@@ -58,7 +51,6 @@ subroutine (int in)
 void
 have_a_very_merry_interrupt (void)
 {
-  puts ("Waiting to get a signal");
   signal (SIGALRM, handler);
   alarm (1);
   sleep (2);  /* We'll receive that signal while sleeping */
