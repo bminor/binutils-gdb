f372da99abc824b501882845491115bdd6132565
Caroline Tice <cmtice@google.com>
2007-05-18 19:45:19 +0000

    Accidentally left this out of previous commit; part of adding ability to report when a variable's value is uninitialized.

--- gdb/value.h
+++ gdb/value.h
@@ -193,6 +193,12 @@ extern int value_contents_equal (struct value *val1, struct value *val2);
 extern int value_optimized_out (struct value *value);
 extern void set_value_optimized_out (struct value *value, int val);
 
+/* Set or return field indicating whether a variable is initialized or
+   not, based on debugging information supplied by the compiler. 
+   1 = initialized; 0 = uninitialized.  */
+extern int value_initialized (struct value *);
+extern void set_value_initialized (struct value *, int);
+
 /* While the following fields are per- VALUE .CONTENT .PIECE (i.e., a
    single value might have multiple LVALs), this hacked interface is
    limited to just the first PIECE.  Expect further change.  */
