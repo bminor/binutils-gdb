a961495892f6fcdb2386428c9bb63e19f364006f
Ulrich Weigand <uweigand@de.ibm.com>
2007-06-15 22:29:52 +0000

    * mips-tdep.c (deprecated_mips_set_processor_regs_hack): Read from 	current regcache instead of calling read_register.

--- gdb/mips-tdep.c
+++ gdb/mips-tdep.c
@@ -4472,10 +4472,10 @@ void
 deprecated_mips_set_processor_regs_hack (void)
 {
   struct gdbarch_tdep *tdep = gdbarch_tdep (current_gdbarch);
-  CORE_ADDR prid;
+  ULONGEST prid;
 
-  prid = read_register (MIPS_PRID_REGNUM);
-
+  regcache_cooked_read_unsigned (current_regcache,
+				 MIPS_PRID_REGNUM, &prid);
   if ((prid & ~0xf) == 0x700)
     tdep->mips_processor_reg_names = mips_r3041_reg_names;
 }
