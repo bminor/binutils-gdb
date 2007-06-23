4b3a42a7c8c8fa7f7137d55be38c2df69f706526
H.J. Lu <hjl.tools@gmail.com>
2007-06-23 16:15:53 +0000

    2007-06-23  H.J. Lu  <hongjiu.lu@intel.com>
    	* binutils-all/strip-3.d: Also strip .xtensa.info section.

--- binutils/testsuite/binutils-all/strip-3.d
+++ binutils/testsuite/binutils-all/strip-3.d
@@ -1,6 +1,6 @@
 #PROG: strip
 #source: empty.s
-#strip: -R .text -R .data -R .bss -R .ARM.attributes -R .reginfo -R .pdr
+#strip: -R .text -R .data -R .bss -R .ARM.attributes -R .reginfo -R .pdr -R .xtensa.info
 #readelf: -S --wide
 #name: strip empty file
 #target: *-*-linux*
