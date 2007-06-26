a46ed97fe4659094a3835a3909813b6ee1ff02c9
Nick Clifton <nickc@redhat.com>
2007-06-26 12:58:55 +0000

    * gas/arm/backslash-at.d: Fix for non-ELF arm targets.

--- gas/testsuite/gas/arm/backslash-at.d
+++ gas/testsuite/gas/arm/backslash-at.d
@@ -4,13 +4,14 @@
 .*:     file format .*arm.*
 
 Disassembly of section .text:
-00000000 <foo-0x2> 615c      	.short	0x615c
-00000002 <foo> e3a00000 	mov	r0, #0	; 0x0
-00000006 <foo\+0x4> e3a00000 	mov	r0, #0	; 0x0
-0000000a <foo\+0x8> e3a00000 	mov	r0, #0	; 0x0
-0000000e <foo\+0xc> e3a00001 	mov	r0, #1	; 0x1
-00000012 <foo\+0x10> e3a00001 	mov	r0, #1	; 0x1
-00000016 <foo\+0x14> e3a00001 	mov	r0, #1	; 0x1
-0000001a <foo\+0x18> e3a00002 	mov	r0, #2	; 0x2
-0000001e <foo\+0x1c> e3a00002 	mov	r0, #2	; 0x2
-00000022 <foo\+0x20> e3a00002 	mov	r0, #2	; 0x2
+0+000 <.*>.*615c.*
+0+002 <foo> e3a00000 	mov	r0, #0	; 0x0
+0+006 <foo\+0x4> e3a00000 	mov	r0, #0	; 0x0
+0+00a <foo\+0x8> e3a00000 	mov	r0, #0	; 0x0
+0+00e <foo\+0xc> e3a00001 	mov	r0, #1	; 0x1
+0+012 <foo\+0x10> e3a00001 	mov	r0, #1	; 0x1
+0+016 <foo\+0x14> e3a00001 	mov	r0, #1	; 0x1
+0+01a <foo\+0x18> e3a00002 	mov	r0, #2	; 0x2
+0+01e <foo\+0x1c> e3a00002 	mov	r0, #2	; 0x2
+0+022 <foo\+0x20> e3a00002 	mov	r0, #2	; 0x2
+#...
