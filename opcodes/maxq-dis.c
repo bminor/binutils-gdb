c908d778a723d69e9e6edc9bcc4176296a0c0b9b
Nick Clifton <nickc@redhat.com>
2007-07-19 16:23:47 +0000

    PR binutils/4801 * maxq-dis.c (get_reg_name): Fix the scan of the mem_access_syntax_table.

--- opcodes/maxq-dis.c
+++ opcodes/maxq-dis.c
@@ -182,7 +182,7 @@ get_reg_name (unsigned char reg_code, type1 arg_pos)
       if (reg_code == mem_acc->opcode)
 	{
 	  for (syntax = mem_access_syntax_table;
-	       mem_access_syntax_table != NULL || mem_access_syntax_table->name;
+	       syntax != NULL && syntax->name;
 	       ++syntax)
 	    if (!strcmp (mem_acc->name, syntax->name))
 	      {
