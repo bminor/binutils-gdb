e22f8b7c8c951ea8f15f9c7ef046243d05a0dbff
Joel Brobecker <brobecker@gnat.com>
2007-08-23 18:14:19 +0000

    Switch the license of all .exp files to GPLv3.         Switch the license of all .f and .f90 files to GPLv3.         Switch the license of all .s and .S files to GPLv3.

--- gdb/testsuite/gdb.fortran/subarray.f
+++ gdb/testsuite/gdb.fortran/subarray.f
@@ -2,17 +2,16 @@ c Copyright 2005 Free Software Foundation, Inc.
 
 c This program is free software; you can redistribute it and/or modify
 c it under the terms of the GNU General Public License as published by
-c the Free Software Foundation; either version 2 of the License, or
+c the Free Software Foundation; either version 3 of the License, or
 c (at your option) any later version.
-c 
+c
 c This program is distributed in the hope that it will be useful,
 c but WITHOUT ANY WARRANTY; without even the implied warranty of
 c MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 c GNU General Public License for more details.
-c 
+c
 c You should have received a copy of the GNU General Public License
-c along with this program; if not, write to the Free Software
-c Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+c along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 c Ihis file is the Fortran source file for subarray.exp.  It was written
 c by Wu Zhou. (woodzltc@cn.ibm.com)
--- gdb/testsuite/gdb.mi/array.f
+++ gdb/testsuite/gdb.mi/array.f
@@ -2,18 +2,16 @@ c Copyright 2006 Free Software Foundation, Inc.
 
 c This program is free software; you can redistribute it and/or modify
 c it under the terms of the GNU General Public License as published by
-c the Free Software Foundation; either version 2 of the License, or
+c the Free Software Foundation; either version 3 of the License, or
 c (at your option) any later version.
-c 
+c
 c This program is distributed in the hope that it will be useful,
 c but WITHOUT ANY WARRANTY; without even the implied warranty of
 c MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 c GNU General Public License for more details.
-c 
+c
 c You should have received a copy of the GNU General Public License
-c along with GNU Emacs; see the file COPYING.  If not, write to the
-c Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-c Boston, MA 02110-1301, USA.
+c along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
       INTEGER array(1:2,-1:1)
       DATA array/11,21,12,22,13,23/
