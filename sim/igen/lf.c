dfee3164ef603fbe4e2513452ec2e5bf26630a58
Joel Brobecker <brobecker@gnat.com>
2007-08-28 00:09:36 +0000

    * lf.c (lf_print__gnu_copyleft): Change license to GPL version 3.

--- sim/igen/lf.c
+++ sim/igen/lf.c
@@ -265,22 +265,20 @@ lf_print__gnu_copyleft (lf *file)
       nr += lf_printf (file, "\
 /* This file is part of GDB.\n\
 \n\
-   Copyright 2002 Free Software Foundation, Inc.\n\
+   Copyright 2002, 2007 Free Software Foundation, Inc.\n\
 \n\
    This program is free software; you can redistribute it and/or modify\n\
    it under the terms of the GNU General Public License as published by\n\
-   the Free Software Foundation; either version 2 of the License, or\n\
+   the Free Software Foundation; either version 3 of the License, or\n\
    (at your option) any later version.\n\
 \n\
    This program is distributed in the hope that it will be useful,\n\
    but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
    GNU General Public License for more details.\n\
- \n\
+\n\
    You should have received a copy of the GNU General Public License\n\
-   along with this program; if not, write to the Free Software\n\
-   Foundation, Inc., 59 Temple Place - Suite 330,\n\
-   Boston, MA 02111-1307, USA.\n\
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\
 \n\
    --\n\
 \n\
