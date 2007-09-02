0b93d57c33f9377f43325623b8a493cb5c9697be
Jan Kratochvil <jan.kratochvil@redhat.com>
2007-09-02 21:13:56 +0000

    * top.c (print_gdb_version): Fixed a string end-of-line compiler error.

--- gdb/top.c
+++ gdb/top.c
@@ -1157,7 +1157,7 @@ print_gdb_version (struct ui_file *stream)
   fprintf_filtered (stream, "\
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
 This is free software: you are free to change and redistribute it.\n\
-There is NO WARRANTY, to the extent permitted by law.  Type \"show copying\"
+There is NO WARRANTY, to the extent permitted by law.  Type \"show copying\"\n\
 and \"show warranty\" for details.\n");
 
   /* After the required info we print the configuration information. */
