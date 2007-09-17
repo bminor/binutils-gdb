916af0488c5c7f741cf92deb73451d3072b4e64e
H.J. Lu <hjl.tools@gmail.com>
2007-09-17 14:46:12 +0000

    gas/
    2007-09-17  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* config/tc-i386.c (intel_e04): Revert the last change.
    
    gas/testsuite/
    
    2007-09-17  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* gas/i386/x86-64-rip.s: Revert the last change.
    	* gas/i386/x86-64-rip-intel.d: Likewise.
    	* gas/i386/x86-64-rip.d: Likewise.

--- gas/testsuite/gas/i386/x86-64-rip-intel.d
+++ gas/testsuite/gas/i386/x86-64-rip-intel.d
@@ -7,13 +7,9 @@
 
 Disassembly of section .text:
 
-0+ <.text>:
-[ 	]*[a-f0-9]+:	8d 05 00 00 00 00    	lea    eax,\[rip\+0x0\][ 	]*#[ 	]*0x[a-f0-9]+[ 	]+[a-f0-9]+: R_X86_64_PC32	symbol-0x4
-[ 	]*[a-f0-9]+:	8d 05 11 11 11 11    	lea    eax,\[rip\+0x11111111\]        # 0x1111111d
-[ 	]*[a-f0-9]+:	8d 05 01 00 00 00    	lea    eax,\[rip\+0x1\]        # 0x13
-[ 	]*[a-f0-9]+:	8d 05 00 00 00 00    	lea    eax,\[rip\+0x0\]        # 0x18
-[ 	]*[a-f0-9]+:	48 8b 05 00 00 00 00 	mov    rax,QWORD PTR \[rip\+0x0\][ 	]*#[ 	]*0x[a-f0-9]+[ 	]+[a-f0-9]+: R_X86_64_PC32	__CTOR_END__-0xc
-[ 	]*[a-f0-9]+:	48 8b 05 00 00 00 00 	mov    rax,QWORD PTR \[rip\+0x0\][ 	]*#[ 	]*0x[a-f0-9]+[ 	]+[a-f0-9]+: R_X86_64_PC32	__CTOR_END__-0xc
-[ 	]*[a-f0-9]+:	48 8b 05 00 00 00 00 	mov    rax,QWORD PTR \[rip\+0x0\][ 	]*#[ 	]*0x[a-f0-9]+[ 	]+[a-f0-9]+: R_X86_64_PC32	__CTOR_END__-0xc
-[ 	]*[a-f0-9]+:	48 8b 05 00 00 00 00 	mov    rax,QWORD PTR \[rip\+0x0\][ 	]*#[ 	]*0x[a-f0-9]+[ 	]+[a-f0-9]+: R_X86_64_PC32	__CTOR_END__-0xc
+0+000 <.text>:
+[	 ]*0:[	 ]+8d 05 00 00 00 00[	 ]+lea[	 ]+eax,\[rip\+0x0\][ 	]*(#.*)?
+[	 ]*6:[	 ]+8d 05 11 11 11 11[	 ]+lea[	 ]+eax,\[rip\+0x11111111\][ 	]*(#.*)?
+[	 ]*c:[	 ]+8d 05 01 00 00 00[	 ]+lea[	 ]+eax,\[rip\+0x1\][ 	]*(#.*)?
+[	 ]*12:[	 ]+8d 05 00 00 00 00[	 ]+lea[	 ]+eax,\[rip\+0x0\][ 	]*(#.*)?
 #pass
