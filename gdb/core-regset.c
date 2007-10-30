8f4f3fbe31e52ff8d866167c822bc8ab0815c9ca
Ulrich Weigand <uweigand@de.ibm.com>
2007-10-30 21:50:46 +0000

    2007-10-30  Markus Deuling  <deuling@de.ibm.com>
    	* core-regset.c (fetch_core_registers): Use get_regcache_arch to get at
    	the current architecture by regcache.

--- gdb/core-regset.c
+++ gdb/core-regset.c
@@ -85,7 +85,7 @@ fetch_core_registers (struct regcache *regcache,
       else
 	{
 	  memcpy (&fpregset, core_reg_sect, sizeof (fpregset));
-	  if (gdbarch_fp0_regnum (current_gdbarch) >= 0)
+	  if (gdbarch_fp0_regnum (get_regcache_arch (regcache)) >= 0)
 	    supply_fpregset (regcache, (const gdb_fpregset_t *) fpregset_p);
 	}
       break;
