600d293e5fc14eb1feb984dd41c9f144b280fe31
Ulrich Weigand <uweigand@de.ibm.com>
2007-10-30 21:40:23 +0000

    2007-10-30  Markus Deuling  <deuling@de.ibm.com>
    	* alphabsd-nat.c (alphabsd_fetch_inferior_registers)
    	(alphabsd_store_inferior_registers): Use get_regcache_arch to get at
    	the current architecture by regcache.

--- gdb/alphabsd-nat.c
+++ gdb/alphabsd-nat.c
@@ -99,7 +99,7 @@ alphabsd_fetch_inferior_registers (struct regcache *regcache, int regno)
 	return;
     }
 
-  if (regno == -1 || regno >= gdbarch_fp0_regnum (current_gdbarch))
+  if (regno == -1 || regno >= gdbarch_fp0_regnum (get_regcache_arch (regcache)))
     {
       struct fpreg fpregs;
 
@@ -134,7 +134,7 @@ alphabsd_store_inferior_registers (struct regcache *regcache, int regno)
 	return;
     }
 
-  if (regno == -1 || regno >= gdbarch_fp0_regnum (current_gdbarch))
+  if (regno == -1 || regno >= gdbarch_fp0_regnum (get_regcache_arch (regcache)))
     {
       struct fpreg fpregs;
 
