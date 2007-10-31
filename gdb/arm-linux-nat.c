08790784703710bad064de53ce2805931204f7e7
Ulrich Weigand <uweigand@de.ibm.com>
2007-10-31 14:15:49 +0000

    2007-10-31  Markus Deuling  <deuling@de.ibm.com>
    	* arm-linux-nat.c (fetch_register, fetch_regs): Use get_regcache_arch
    	to get at the current architecture by regcache.

--- gdb/arm-linux-nat.c
+++ gdb/arm-linux-nat.c
@@ -255,7 +255,8 @@ fetch_register (struct regcache *regcache, int regno)
   if (ARM_PC_REGNUM == regno)
     { 
       regs[ARM_PC_REGNUM] = gdbarch_addr_bits_remove
-			      (current_gdbarch, regs[ARM_PC_REGNUM]);
+			      (get_regcache_arch (regcache),
+			       regs[ARM_PC_REGNUM]);
       regcache_raw_supply (regcache, ARM_PC_REGNUM,
 			   (char *) &regs[ARM_PC_REGNUM]);
     }
@@ -291,7 +292,7 @@ fetch_regs (struct regcache *regcache)
 			 (char *) &regs[ARM_PC_REGNUM]);
 
   regs[ARM_PC_REGNUM] = gdbarch_addr_bits_remove
-			  (current_gdbarch, regs[ARM_PC_REGNUM]);
+			  (get_regcache_arch (regcache), regs[ARM_PC_REGNUM]);
   regcache_raw_supply (regcache, ARM_PC_REGNUM,
 		       (char *) &regs[ARM_PC_REGNUM]);
 }
