e6760bcc5348499c79e82045e12adf17f27d9a40
H.J. Lu <hjl.tools@gmail.com>
2007-11-13 13:20:02 +0000

    2007-11-13  H.J. Lu  <hongjiu.lu@intel.com>
    	PR binutils/5233
    	* ld-elf/flags1.d: New.
    	* ld-elf/flags1.ld: Likewise.
    	* ld-elf/flags1.s: Likewise.

---
+++ ld/testsuite/ld-elf/flags1.d
@@ -0,0 +1,14 @@
+#name: --set-section-flags test 1 (sections)
+#ld: -Tflags1.ld
+#objcopy_linked_file: --set-section-flags .post_text_reserve=contents,alloc,load,readonly,code 
+#readelf: -l --wide
+
+#...
+Program Headers:
+  Type.*
+  LOAD +0x[0-9a-f]+ 0x0*0 0x0*0 0x0*0161 0x0*0161 RWE 0x[0-9a-f]+
+
+#...
+  Segment Sections...
+   00[ \t]+.text .post_text_reserve[ \t]*
+#pass
