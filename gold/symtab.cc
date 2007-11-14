601e4987b8dd2639159d387a00b4504744c4abfd
Ian Lance Taylor <iant@google.com>
2007-11-14 08:03:41 +0000

    Revert last patch.

--- gold/symtab.cc
+++ gold/symtab.cc
@@ -1610,8 +1610,7 @@ Symbol_table::sized_write_globals(const Target* target,
           && sym->object()->is_dynamic()
           && sym->shndx() == elfcpp::SHN_UNDEF
 	  && sym->binding() != elfcpp::STB_WEAK
-	  && !parameters->allow_shlib_undefined()
-	  && !target->is_always_defined(sym))
+	  && !parameters->allow_shlib_undefined())
 	{
 	  // A very ugly cast.
 	  Dynobj* dynobj = static_cast<Dynobj*>(sym->object());
