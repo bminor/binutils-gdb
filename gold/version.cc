53765ea3d793c1f2114aa4441e561fb81406c201
Craig Silverstein <csilvers@google.com>
2007-12-08 03:17:25 +0000

    Released gold-alpha 0.9.

--- gold/version.cc
+++ gold/version.cc
@@ -33,7 +33,7 @@ namespace gold
 // version number from configure.ac.  But it's easier to just change
 // this file for now.
 
-static const char* version_string = "0.8";
+static const char* version_string = "0.9";
 
 // Report version information.
 
