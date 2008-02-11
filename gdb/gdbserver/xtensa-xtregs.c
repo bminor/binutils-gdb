1525d5452d720e163509decda467b5de11a141e8
Maxim Grigoriev <maxim2405@gmail.com>
2008-02-11 22:00:31 +0000

    2008-02-11  Maxim Grigoriev  <maxim2405@gmail.com>
    	* gdbserver/Makefile.in (SFILES): Add linux-xtensa-low.c.
    	(clean): Add reg-xtensa.c.
    	(linux-xtensa-low.o, reg-xtensa.o, reg-xtensa.c): New dependencies.
    	* gdbserver/configure.srv (xtensa*-*-linux*) New target.
    	* gdbserver/linux-xtensa-low.c: New.
    	* gdbserver/xtensa-xtregs.c: New.

---
+++ gdb/gdbserver/xtensa-xtregs.c
@@ -0,0 +1,37 @@
+/* Table mapping between kernel xtregset and GDB register cache.
+   Copyright 2007, 2008 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+
+typedef struct {
+  int   gdb_regnum;
+  int   gdb_offset;
+  int   ptrace_cp_offset;
+  int   ptrace_offset;
+  int   size;
+  int   coproc;
+  int   dbnum;
+  char* name
+;} xtensa_regtable_t;
+
+#define XTENSA_ELF_XTREG_SIZE	0
+
+const xtensa_regtable_t  xtensa_regmap_table[] = {
+  /* gnum,gofs,cpofs,ofs,siz,cp, dbnum,  name */
+  { 0 }
+};
+
