9d37b37d379198334b8b3e0543b1298394062e9d
Craig Silverstein <csilvers@google.com>
2008-02-12 02:01:32 +0000

    Up version to 1.1, as the 1.1 binary was pushed to users today.

--- gold/version.cc
+++ gold/version.cc
@@ -33,7 +33,7 @@ namespace gold
 // version number from configure.ac.  But it's easier to just change
 // this file for now.
 
-static const char* version_string = "0.9";
+static const char* version_string = "1.1";
 
 // Report version information.
 
