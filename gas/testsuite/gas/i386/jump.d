58abc3ebf63249755146ba6a811ad909732641b1
H.J. Lu <hjl.tools@gmail.com>
2008-02-26 16:18:16 +0000

    2008-02-26  H.J. Lu  <hongjiu.lu@intel.com>
    	* gas/i386/jump.d: Updated for COFF.

--- gas/testsuite/gas/i386/jump.d
+++ gas/testsuite/gas/i386/jump.d
@@ -6,26 +6,26 @@
 Disassembly of section .text:
 
 0+ <.text>:
-[ 	]*[a-f0-9]+:	eb fe                	jmp    0x0
-[ 	]*[a-f0-9]+:	e9 fc ff ff ff       	jmp    0x3	3: R_386_PC32	xxx
-[ 	]*[a-f0-9]+:	ff 25 00 00 00 00    	jmp    \*0x0	9: R_386_32	xxx
+[ 	]*[a-f0-9]+:	eb fe                	jmp    (0x0|0 <.text>)
+[ 	]*[a-f0-9]+:	e9 ((fc|f9) ff ff ff|00 00 00 00)       	jmp    (0x)?(0|3|7)( <.text(\+0x7)?>)?	3: (R_386_PC)?(DISP)?32	xxx
+[ 	]*[a-f0-9]+:	ff 25 00 00 00 00    	jmp    \*0x0	9: (R_386_)?(dir)?32	xxx
 [ 	]*[a-f0-9]+:	ff e7                	jmp    \*%edi
 [ 	]*[a-f0-9]+:	ff 27                	jmp    \*\(%edi\)
-[ 	]*[a-f0-9]+:	ff 2c bd 00 00 00 00 	ljmp   \*0x0\(,%edi,4\)	14: R_386_32	xxx
-[ 	]*[a-f0-9]+:	66 ff 2c bd 00 00 00 00 	ljmpw  \*0x0\(,%edi,4\)	1c: R_386_32	xxx
-[ 	]*[a-f0-9]+:	ff 2d 00 00 00 00    	ljmp   \*0x0	22: R_386_32	xxx
-[ 	]*[a-f0-9]+:	66 ff 2d 00 00 00 00 	ljmpw  \*0x0	29: R_386_32	xxx
-[ 	]*[a-f0-9]+:	ea 00 00 00 00 34 12 	ljmp   \$0x1234,\$0x0	2e: R_386_32	xxx
-[ 	]*[a-f0-9]+:	e8 c7 ff ff ff       	call   0x0
-[ 	]*[a-f0-9]+:	e8 fc ff ff ff       	call   0x3a	3a: R_386_PC32	xxx
-[ 	]*[a-f0-9]+:	ff 15 00 00 00 00    	call   \*0x0	40: R_386_32	xxx
+[ 	]*[a-f0-9]+:	ff 2c bd 00 00 00 00 	ljmp   \*0x0\(,%edi,4\)	14: (R_386_)?(dir)?32	xxx
+[ 	]*[a-f0-9]+:	66 ff 2c bd 00 00 00 00 	ljmpw  \*0x0\(,%edi,4\)	1c: (R_386_)?(dir)?32	xxx
+[ 	]*[a-f0-9]+:	ff 2d 00 00 00 00    	ljmp   \*0x0	22: (R_386_)?(dir)?32	xxx
+[ 	]*[a-f0-9]+:	66 ff 2d 00 00 00 00 	ljmpw  \*0x0	29: (R_386_)?(dir)?32	xxx
+[ 	]*[a-f0-9]+:	ea 00 00 00 00 34 12 	ljmp   \$0x1234,\$0x0	2e: (R_386_)?(dir)?32	xxx
+[ 	]*[a-f0-9]+:	e8 c7 ff ff ff       	call   (0x0|0 <.text>)
+[ 	]*[a-f0-9]+:	e8 ((fc|d1) ff ff ff|00 00 00 00)       	call   (0x)?(0|3a|3e)( <.text(\+0x3e)?>)?	3a: (R_386_PC)?(DISP)?32	xxx
+[ 	]*[a-f0-9]+:	ff 15 00 00 00 00    	call   \*0x0	40: (R_386_)?(dir)?32	xxx
 [ 	]*[a-f0-9]+:	ff d7                	call   \*%edi
 [ 	]*[a-f0-9]+:	ff 17                	call   \*\(%edi\)
-[ 	]*[a-f0-9]+:	ff 1c bd 00 00 00 00 	lcall  \*0x0\(,%edi,4\)	4b: R_386_32	xxx
-[ 	]*[a-f0-9]+:	66 ff 1c bd 00 00 00 00 	lcallw \*0x0\(,%edi,4\)	53: R_386_32	xxx
-[ 	]*[a-f0-9]+:	ff 1d 00 00 00 00    	lcall  \*0x0	59: R_386_32	xxx
-[ 	]*[a-f0-9]+:	66 ff 1d 00 00 00 00 	lcallw \*0x0	60: R_386_32	xxx
-[ 	]*[a-f0-9]+:	9a 00 00 00 00 34 12 	lcall  \$0x1234,\$0x0	65: R_386_32	xxx
+[ 	]*[a-f0-9]+:	ff 1c bd 00 00 00 00 	lcall  \*0x0\(,%edi,4\)	4b: (R_386_)?(dir)?32	xxx
+[ 	]*[a-f0-9]+:	66 ff 1c bd 00 00 00 00 	lcallw \*0x0\(,%edi,4\)	53: (R_386_)?(dir)?32	xxx
+[ 	]*[a-f0-9]+:	ff 1d 00 00 00 00    	lcall  \*0x0	59: (R_386_)?(dir)?32	xxx
+[ 	]*[a-f0-9]+:	66 ff 1d 00 00 00 00 	lcallw \*0x0	60: (R_386_)?(dir)?32	xxx
+[ 	]*[a-f0-9]+:	9a 00 00 00 00 34 12 	lcall  \$0x1234,\$0x0	65: (R_386_)?(dir)?32	xxx
 [ 	]*[a-f0-9]+:	66 ff 13             	callw  \*\(%ebx\)
 [ 	]*[a-f0-9]+:	ff 13                	call   \*\(%ebx\)
 [ 	]*[a-f0-9]+:	ff 1b                	lcall  \*\(%ebx\)
