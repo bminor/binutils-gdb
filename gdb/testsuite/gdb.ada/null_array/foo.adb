d9c36a9152b92667378c667ad2a1e9fef66c4c73
Joel Brobecker <brobecker@gnat.com>
2008-05-23 18:15:23 +0000

    * gdb.ada/null_array/foo.adb: Add multi-dimensional array         of zero-size elements.         * gdb.ada/null_array.exp: Test printing this new array.

--- gdb/testsuite/gdb.ada/null_array/foo.adb
+++ gdb/testsuite/gdb.ada/null_array/foo.adb
@@ -18,7 +18,14 @@ with Pck; use Pck;
 procedure Foo is
    type Table is array (Integer range <>) of Integer;
 
+   type Matrix is array (1 .. 10, 1 .. 0) of Character;
+   type Wrapper is record
+      M : Matrix;
+   end record;
+
    My_Table : Table (Ident (10) .. Ident (1));
+   My_Matrix : Wrapper := (M => (others => (others => 'a')));
 begin
    Do_Nothing (My_Table'Address);  -- START
+   Do_Nothing (My_Matrix'Address);
 end Foo;
