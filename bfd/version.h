5ff3b161479c232a35041a9744c74e20a14ccf98
Alan Modra <amodra@gmail.com>
2008-06-06 00:00:10 +0000

    daily update

--- bfd/version.h
+++ bfd/version.h
@@ -1,4 +1,4 @@
-#define BFD_VERSION_DATE 20080605
+#define BFD_VERSION_DATE 20080606
 #define BFD_VERSION @bfd_version@
 #define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@
 #define REPORT_BUGS_TO @report_bugs_to@
