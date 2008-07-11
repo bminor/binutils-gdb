851b0ff7c6dbfc792aeb32a0953d4445d7e5f964
H.J. Lu <hjl.tools@gmail.com>
2008-07-11 17:19:00 +0000

    2008-07-11  H.J. Lu  <hongjiu.lu@intel.com>
    	* ld-m68k/got-12.s: Removed.
    	* ld-m68k/got-13.s: Likewise.
    	* ld-m68k/got-14.s: Likewise.
    	* ld-m68k/got-15.s: Likewise.
    	* ld-m68k/got-34.s: Likewise.
    	* ld-m68k/got-35.s: Likewise.
    	* ld-m68k/xgot-15.s: Likewise.
    
    	* ld-m68k/got-multigot-12-13-14-34-35-ok.d: Remove #source
    	and expected relocations.
    	* ld-m68k/got-multigot-14-ok.d: Likewise.
    	* ld-m68k/got-negative-12-13-14-34-ok.d: Likewise.
    	* ld-m68k/got-negative-14-ok.d: Likewise.
    	* ld-m68k/got-single-12-ok.d: Likewise.
    	* ld-m68k/got-xgot-12-13-14-15-34-35-ok.d: Likewise.
    	* ld-m68k/got-xgot-15-ok.d: Likewise.
    
    	* ld-m68k/got-multigot-15-er.d: Remove #source.
    	* ld-m68k/got-negative-12-13-14-35-er.d: Likewise.
    	* ld-m68k/got-negative-15-er.d: Likewise.
    	* ld-m68k/got-single-13-er.d: Likewise.
    
    	* ld-m68k/m68k.exp: Move GOT tests to ...
    	* ld-m68k/m68k-got.exp: This.  New.

--- ld/testsuite/ld-m68k/got-multigot-15-er.d
+++ ld/testsuite/ld-m68k/got-multigot-15-er.d
@@ -1,4 +1,3 @@
-#source: got-15.s
 #as: -mcpu=5206
 #ld: -shared --got=multigot
 #error: .*GOT overflow.*
--- ld/testsuite/ld-m68k/got-negative-15-er.d
+++ ld/testsuite/ld-m68k/got-negative-15-er.d
@@ -1,4 +1,3 @@
-#source: got-15.s
 #as: -mcpu=5206
 #ld: -shared --got=negative
 #error: .*GOT overflow.*
