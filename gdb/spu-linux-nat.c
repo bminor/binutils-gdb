d5b254911b8762c87f380aec389be5bf0d6ff4b0
Ulrich Weigand <uweigand@de.ibm.com>
2008-07-12 22:16:34 +0000

    * spu-linux-nat.c: Include "gdbthread.h". 	(spu_child_post_startup_inferior): Register main thread. 	(spu_child_post_attach): Likewise. 	* Makefile.in (spu-linux-nat.o): Update dependencies.

--- gdb/spu-linux-nat.c
+++ gdb/spu-linux-nat.c
@@ -27,6 +27,7 @@
 #include "regcache.h"
 #include "symfile.h"
 #include "gdb_wait.h"
+#include "gdbthread.h"
 
 #include <sys/ptrace.h>
 #include <asm/ptrace.h>
@@ -375,6 +376,8 @@ spu_child_post_startup_inferior (ptid_t ptid)
       ptrace (PT_SYSCALL, tid, (PTRACE_TYPE_ARG3) 0, 0);
       waitpid (tid, NULL, __WALL | __WNOTHREAD);
     }
+
+  add_thread_silent (ptid);
 }
 
 /* Override the post_attach routine to try load the SPE executable
@@ -393,6 +396,8 @@ spu_child_post_attach (int pid)
       ptrace (PT_SYSCALL, pid, (PTRACE_TYPE_ARG3) 0, 0);
       waitpid (pid, NULL, __WALL | __WNOTHREAD);
     }
+
+  add_thread_silent (inferior_ptid);
 
   /* If the user has not provided an executable file, try to extract
      the image from inside the target process.  */
