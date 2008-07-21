096b02cf4da48783a8945abbbea88d9880559a35
Craig Silverstein <csilvers@google.com>
2008-07-21 23:36:29 +0000

    2008-07-21  Craig Silverstein  <csilvers@google.com>
    	* symtab.cc (Symbol_table::warn_about_undefined_dynobj_symbol):
    	Include symbol version in error message.

--- gold/symtab.cc
+++ gold/symtab.cc
@@ -2376,9 +2376,17 @@ Symbol_table::warn_about_undefined_dynobj_symbol(
       // A very ugly cast.
       Dynobj* dynobj = static_cast<Dynobj*>(sym->object());
       if (!dynobj->has_unknown_needed_entries())
-	gold_error(_("%s: undefined reference to '%s'"),
-		   sym->object()->name().c_str(),
-                   sym->demangled_name().c_str());
+        {
+          if (sym->version())
+            gold_error(_("%s: undefined reference to '%s', version '%s'"),
+                       sym->object()->name().c_str(),
+                       sym->demangled_name().c_str(),
+                       sym->version());
+          else
+            gold_error(_("%s: undefined reference to '%s'"),
+                       sym->object()->name().c_str(),
+                       sym->demangled_name().c_str());
+        }
     }
 }
 
