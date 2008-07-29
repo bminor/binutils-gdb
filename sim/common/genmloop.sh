086c6838fab1ebd605b03d14e4fbd7a31a86062d
Nick Clifton <nickc@redhat.com>
2008-07-29 13:53:02 +0000

    * common/genmloop.sh: Add new parameter: -shell to specify the         command interpreter to use to run the input file.  This is         necessary because otherwise SHELL is taken from the user's         environment, and not from the makefile that invoked this script         and the user might not be running an sh-like shell.         * cris/Makefile.in: Pass -shell parameter to genmloop.sh.         * fr30/Makefile.in: Likewise.         * frv/Makefile.in: Likewise.         * i960/Makefile.in: Likewise.         * iq2000/Makefile.in: Likewise.         * m32r/Makefile.in: Likewise.
            * frv/mloop.in: Add missing start of line comment marker.

--- sim/common/genmloop.sh
+++ sim/common/genmloop.sh
@@ -118,6 +118,10 @@
 #
 #	Specify the suffix to append to output files.
 #
+# -shell <shell>
+#
+#	Specify the shell to use to execute <input-file>
+#
 # Only one of -scache/-pbb may be selected.
 # -simple is the default.
 #
@@ -160,6 +164,7 @@ do
 	-switch) shift ; switch=$1 ;;
 	-cpu) shift ; cpu=$1 ;;
 	-infile) shift ; infile=$1 ;;
+	-shell) shift ; SHELL=$1 ;;
 	*) echo "unknown option: $1" >&2 ; exit 1 ;;
 	esac
 	shift
