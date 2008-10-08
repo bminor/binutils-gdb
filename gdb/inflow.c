a364558244ae7258ab3cd795f687cb0906b81f9f
Hui Zhu <teawater@gmail.com>
2008-10-08 06:20:51 +0000

    2008-10-07  Hui Zhu  <teawater@gmail.com>
    	* inflow.c (kill_command): Remove query special for
    	process record.

--- gdb/inflow.c
+++ gdb/inflow.c
@@ -596,16 +596,8 @@ kill_command (char *arg, int from_tty)
 
   if (ptid_equal (inferior_ptid, null_ptid))
     error (_("The program is not being run."));
-  if (RECORD_IS_USED)
-    {
-      if (!query ("Stop the process record target and kill the program being debugged? "))
-        error (_("Not confirmed."));
-    }
-  else
-    {
-      if (!query ("Kill the program being debugged? "))
-        error (_("Not confirmed."));
-    }
+  if (!query ("Kill the program being debugged? "))
+    error (_("Not confirmed."));
   target_kill ();
 
   init_thread_list ();		/* Destroy thread info */
