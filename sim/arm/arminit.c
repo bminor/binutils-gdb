a85c0b494e07723a497e00242518c762b4169c5d
Joel Sherrill <joel.sherrill@oarcorp.com>
2008-11-26 14:24:12 +0000

    2008-11-24  Joel Sherrill <joel.sherrill@oarcorp.com>
        * arminit.c, iwmmxt.c: Include <string.h> to
        eliminate warning.

--- sim/arm/arminit.c
+++ sim/arm/arminit.c
@@ -15,6 +15,8 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA. */
 
+#include <string.h>
+
 #include "armdefs.h"
 #include "armemu.h"
 #include "dbg_rdi.h"
