137627db10bc0dedd3e2f58b9e4e054c816dd41c
Nick Clifton <nickc@redhat.com>
2008-12-04 11:00:18 +0000

    * ld-x86-64/ifunc.d: New test (of STT_IFUNC for x86_64).         * ld-x86-64/ifunc.s: Source file for new test.         * ld-x86-64/x86-64.exp: Run the new test.         * ld-i386/ifunc.d: Tidy up regexps for expected output.

--- ld/testsuite/ld-i386/ifunc.d
+++ ld/testsuite/ld-i386/ifunc.d
@@ -1,14 +1,14 @@
-#name: Generatiion of dynamic relocs for STT_IFUNC symbols
+#name: Generation of dynamic relocs for STT_IFUNC symbols
 #source: ifunc.s
 #as: --32
 #ld: -melf_i386 -lc --defsym _start=0 -L/usr/lib
 #readelf: --relocs --syms
 
 Relocation section '.rel.ifunc.dyn' at offset 0x[0-9a-f]+ contains 2 entries:
- Offset     Info    Type            Sym.Value  Sym. Name
-[0-9a-f]+  00000202 R_386_PC32        func\(\)     func
-[0-9a-f]+  00000102 R_386_PC32        long_fun\(\) long_func_name
+ +Offset +Info +Type +Sym.Value +Sym. Name
+[0-9a-f]+  0+0202 R_386_PC32        func\(\)     func
+[0-9a-f]+  0+0102 R_386_PC32        long_fun\(\) long_func_name
 #...
-    ..: 080482e0    29 IFUNC   GLOBAL DEFAULT   10 long_func_name
-    ..: 08048300    29 IFUNC   GLOBAL DEFAULT   10 func
+    ..: [0-9a-f]+    .. IFUNC   GLOBAL DEFAULT   .. long_func_name
+    ..: [0-9a-f]+    .. IFUNC   GLOBAL DEFAULT   .. func
 #pass
