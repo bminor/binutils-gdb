5df345fc2275dab5e66a8f485f6348b6db1c1ed8
H.J. Lu <hjl.tools@gmail.com>
2009-01-12 16:53:08 +0000

    Update gas/i386/sse2avx-opts.d, gas/i386/sse2avx-opts-intel.d, gas/i386/x86-64-sse2avx-opts.d and gas/i386/x86-64-sse2avx-opts-intel.d.

--- gas/testsuite/gas/i386/sse2avx-opts.d
+++ gas/testsuite/gas/i386/sse2avx-opts.d
@@ -9,6 +9,54 @@
 Disassembly of section .text:
 
 0+ <_start>:
+[ 	]*[a-f0-9]+:	00 d1                	addb   %dl,%cl
+[ 	]*[a-f0-9]+:	02 ca                	addb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 01 d1             	addw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 03 ca             	addw.s %dx,%cx
+[ 	]*[a-f0-9]+:	01 d1                	addl   %edx,%ecx
+[ 	]*[a-f0-9]+:	03 ca                	addl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	00 d1                	addb   %dl,%cl
+[ 	]*[a-f0-9]+:	02 ca                	addb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 01 d1             	addw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 03 ca             	addw.s %dx,%cx
+[ 	]*[a-f0-9]+:	01 d1                	addl   %edx,%ecx
+[ 	]*[a-f0-9]+:	03 ca                	addl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	10 d1                	adcb   %dl,%cl
+[ 	]*[a-f0-9]+:	12 ca                	adcb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 11 d1             	adcw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 13 ca             	adcw.s %dx,%cx
+[ 	]*[a-f0-9]+:	11 d1                	adcl   %edx,%ecx
+[ 	]*[a-f0-9]+:	13 ca                	adcl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	10 d1                	adcb   %dl,%cl
+[ 	]*[a-f0-9]+:	12 ca                	adcb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 11 d1             	adcw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 13 ca             	adcw.s %dx,%cx
+[ 	]*[a-f0-9]+:	11 d1                	adcl   %edx,%ecx
+[ 	]*[a-f0-9]+:	13 ca                	adcl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	20 d1                	andb   %dl,%cl
+[ 	]*[a-f0-9]+:	22 ca                	andb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 21 d1             	andw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 23 ca             	andw.s %dx,%cx
+[ 	]*[a-f0-9]+:	21 d1                	andl   %edx,%ecx
+[ 	]*[a-f0-9]+:	23 ca                	andl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	20 d1                	andb   %dl,%cl
+[ 	]*[a-f0-9]+:	22 ca                	andb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 21 d1             	andw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 23 ca             	andw.s %dx,%cx
+[ 	]*[a-f0-9]+:	21 d1                	andl   %edx,%ecx
+[ 	]*[a-f0-9]+:	23 ca                	andl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	38 d1                	cmpb   %dl,%cl
+[ 	]*[a-f0-9]+:	3a ca                	cmpb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 39 d1             	cmpw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 3b ca             	cmpw.s %dx,%cx
+[ 	]*[a-f0-9]+:	39 d1                	cmpl   %edx,%ecx
+[ 	]*[a-f0-9]+:	3b ca                	cmpl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	38 d1                	cmpb   %dl,%cl
+[ 	]*[a-f0-9]+:	3a ca                	cmpb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 39 d1             	cmpw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 3b ca             	cmpw.s %dx,%cx
+[ 	]*[a-f0-9]+:	39 d1                	cmpl   %edx,%ecx
+[ 	]*[a-f0-9]+:	3b ca                	cmpl.s %edx,%ecx
 [ 	]*[a-f0-9]+:	88 d1                	movb   %dl,%cl
 [ 	]*[a-f0-9]+:	8a ca                	movb.s %dl,%cl
 [ 	]*[a-f0-9]+:	66 89 d1             	movw   %dx,%cx
@@ -21,6 +69,54 @@ Disassembly of section .text:
 [ 	]*[a-f0-9]+:	66 8b ca             	movw.s %dx,%cx
 [ 	]*[a-f0-9]+:	89 d1                	movl   %edx,%ecx
 [ 	]*[a-f0-9]+:	8b ca                	movl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	08 d1                	orb    %dl,%cl
+[ 	]*[a-f0-9]+:	0a ca                	orb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 09 d1             	orw    %dx,%cx
+[ 	]*[a-f0-9]+:	66 0b ca             	orw.s %dx,%cx
+[ 	]*[a-f0-9]+:	09 d1                	orl    %edx,%ecx
+[ 	]*[a-f0-9]+:	0b ca                	orl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	08 d1                	orb    %dl,%cl
+[ 	]*[a-f0-9]+:	0a ca                	orb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 09 d1             	orw    %dx,%cx
+[ 	]*[a-f0-9]+:	66 0b ca             	orw.s %dx,%cx
+[ 	]*[a-f0-9]+:	09 d1                	orl    %edx,%ecx
+[ 	]*[a-f0-9]+:	0b ca                	orl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	18 d1                	sbbb   %dl,%cl
+[ 	]*[a-f0-9]+:	1a ca                	sbbb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 19 d1             	sbbw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 1b ca             	sbbw.s %dx,%cx
+[ 	]*[a-f0-9]+:	19 d1                	sbbl   %edx,%ecx
+[ 	]*[a-f0-9]+:	1b ca                	sbbl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	18 d1                	sbbb   %dl,%cl
+[ 	]*[a-f0-9]+:	1a ca                	sbbb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 19 d1             	sbbw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 1b ca             	sbbw.s %dx,%cx
+[ 	]*[a-f0-9]+:	19 d1                	sbbl   %edx,%ecx
+[ 	]*[a-f0-9]+:	1b ca                	sbbl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	28 d1                	subb   %dl,%cl
+[ 	]*[a-f0-9]+:	2a ca                	subb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 29 d1             	subw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 2b ca             	subw.s %dx,%cx
+[ 	]*[a-f0-9]+:	29 d1                	subl   %edx,%ecx
+[ 	]*[a-f0-9]+:	2b ca                	subl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	28 d1                	subb   %dl,%cl
+[ 	]*[a-f0-9]+:	2a ca                	subb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 29 d1             	subw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 2b ca             	subw.s %dx,%cx
+[ 	]*[a-f0-9]+:	29 d1                	subl   %edx,%ecx
+[ 	]*[a-f0-9]+:	2b ca                	subl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	30 d1                	xorb   %dl,%cl
+[ 	]*[a-f0-9]+:	32 ca                	xorb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 31 d1             	xorw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 33 ca             	xorw.s %dx,%cx
+[ 	]*[a-f0-9]+:	31 d1                	xorl   %edx,%ecx
+[ 	]*[a-f0-9]+:	33 ca                	xorl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	30 d1                	xorb   %dl,%cl
+[ 	]*[a-f0-9]+:	32 ca                	xorb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 31 d1             	xorw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 33 ca             	xorw.s %dx,%cx
+[ 	]*[a-f0-9]+:	31 d1                	xorl   %edx,%ecx
+[ 	]*[a-f0-9]+:	33 ca                	xorl.s %edx,%ecx
 [ 	]*[a-f0-9]+:	c5 fd 28 f4          	vmovapd %ymm4,%ymm6
 [ 	]*[a-f0-9]+:	c5 fd 29 e6          	vmovapd.s %ymm4,%ymm6
 [ 	]*[a-f0-9]+:	c5 fc 28 f4          	vmovaps %ymm4,%ymm6
@@ -71,12 +167,60 @@ Disassembly of section .text:
 [ 	]*[a-f0-9]+:	c5 ca 11 e2          	vmovss.s %xmm4,%xmm6,%xmm2
 [ 	]*[a-f0-9]+:	0f 6f e0             	movq   %mm0,%mm4
 [ 	]*[a-f0-9]+:	0f 7f c4             	movq.s %mm0,%mm4
+[ 	]*[a-f0-9]+:	00 d1                	addb   %dl,%cl
+[ 	]*[a-f0-9]+:	02 ca                	addb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 01 d1             	addw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 03 ca             	addw.s %dx,%cx
+[ 	]*[a-f0-9]+:	01 d1                	addl   %edx,%ecx
+[ 	]*[a-f0-9]+:	03 ca                	addl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	10 d1                	adcb   %dl,%cl
+[ 	]*[a-f0-9]+:	12 ca                	adcb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 11 d1             	adcw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 13 ca             	adcw.s %dx,%cx
+[ 	]*[a-f0-9]+:	11 d1                	adcl   %edx,%ecx
+[ 	]*[a-f0-9]+:	13 ca                	adcl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	20 d1                	andb   %dl,%cl
+[ 	]*[a-f0-9]+:	22 ca                	andb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 21 d1             	andw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 23 ca             	andw.s %dx,%cx
+[ 	]*[a-f0-9]+:	21 d1                	andl   %edx,%ecx
+[ 	]*[a-f0-9]+:	23 ca                	andl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	38 d1                	cmpb   %dl,%cl
+[ 	]*[a-f0-9]+:	3a ca                	cmpb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 39 d1             	cmpw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 3b ca             	cmpw.s %dx,%cx
+[ 	]*[a-f0-9]+:	39 d1                	cmpl   %edx,%ecx
+[ 	]*[a-f0-9]+:	3b ca                	cmpl.s %edx,%ecx
 [ 	]*[a-f0-9]+:	88 d1                	movb   %dl,%cl
 [ 	]*[a-f0-9]+:	8a ca                	movb.s %dl,%cl
 [ 	]*[a-f0-9]+:	66 89 d1             	movw   %dx,%cx
 [ 	]*[a-f0-9]+:	66 8b ca             	movw.s %dx,%cx
 [ 	]*[a-f0-9]+:	89 d1                	movl   %edx,%ecx
 [ 	]*[a-f0-9]+:	8b ca                	movl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	08 d1                	orb    %dl,%cl
+[ 	]*[a-f0-9]+:	0a ca                	orb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 09 d1             	orw    %dx,%cx
+[ 	]*[a-f0-9]+:	66 0b ca             	orw.s %dx,%cx
+[ 	]*[a-f0-9]+:	09 d1                	orl    %edx,%ecx
+[ 	]*[a-f0-9]+:	0b ca                	orl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	18 d1                	sbbb   %dl,%cl
+[ 	]*[a-f0-9]+:	1a ca                	sbbb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 19 d1             	sbbw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 1b ca             	sbbw.s %dx,%cx
+[ 	]*[a-f0-9]+:	19 d1                	sbbl   %edx,%ecx
+[ 	]*[a-f0-9]+:	1b ca                	sbbl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	28 d1                	subb   %dl,%cl
+[ 	]*[a-f0-9]+:	2a ca                	subb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 29 d1             	subw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 2b ca             	subw.s %dx,%cx
+[ 	]*[a-f0-9]+:	29 d1                	subl   %edx,%ecx
+[ 	]*[a-f0-9]+:	2b ca                	subl.s %edx,%ecx
+[ 	]*[a-f0-9]+:	30 d1                	xorb   %dl,%cl
+[ 	]*[a-f0-9]+:	32 ca                	xorb.s %dl,%cl
+[ 	]*[a-f0-9]+:	66 31 d1             	xorw   %dx,%cx
+[ 	]*[a-f0-9]+:	66 33 ca             	xorw.s %dx,%cx
+[ 	]*[a-f0-9]+:	31 d1                	xorl   %edx,%ecx
+[ 	]*[a-f0-9]+:	33 ca                	xorl.s %edx,%ecx
 [ 	]*[a-f0-9]+:	c5 fd 28 f4          	vmovapd %ymm4,%ymm6
 [ 	]*[a-f0-9]+:	c5 fd 29 e6          	vmovapd.s %ymm4,%ymm6
 [ 	]*[a-f0-9]+:	c5 fc 28 f4          	vmovaps %ymm4,%ymm6
