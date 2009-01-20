6f3b91a62110eaea42cd7df18d6f9293f5d60fa2
Alan Modra <amodra@gmail.com>
2009-01-20 07:22:30 +0000

    Regenerate for copyright date update.

--- opcodes/fr30-dis.c
+++ opcodes/fr30-dis.c
@@ -4,8 +4,8 @@
    THIS FILE IS MACHINE GENERATED WITH CGEN.
    - the resultant file is machine generated, cgen-dis.in isn't
 
-   Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2005, 2007
-   Free Software Foundation, Inc.
+   Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2005, 2007,
+   2008  Free Software Foundation, Inc.
 
    This file is part of libopcodes.
 
--- opcodes/iq2000-ibld.c
+++ opcodes/iq2000-ibld.c
@@ -3,8 +3,8 @@
    THIS FILE IS MACHINE GENERATED WITH CGEN: Cpu tools GENerator.
    - the resultant file is machine generated, cgen-ibld.in isn't
 
-   Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2005, 2006, 2007
-   Free Software Foundation, Inc.
+   Copyright 1996, 1997, 1998, 1999, 2000, 2001, 2005, 2006, 2007,
+   2008  Free Software Foundation, Inc.
 
    This file is part of libopcodes.
 
