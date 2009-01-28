75fe7426f972e2298027c5add4f3433597dc078d
Ian Lance Taylor <ian@airs.com>
2009-01-28 20:18:11 +0000

    * version.cc (version_string): Bump to 1.8.

--- gold/version.cc
+++ gold/version.cc
@@ -36,7 +36,7 @@ namespace gold
 // version number from configure.ac.  But it's easier to just change
 // this file for now.
 
-static const char* version_string = "1.8";
+static const char* version_string = "1.9";
 
 // Report version information.
 
