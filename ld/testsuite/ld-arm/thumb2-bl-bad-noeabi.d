aed1c03afd30b7e0619b5bf835c19e86257359f7
Christophe Lyon <christophe.lyon@st.com>
2009-02-25 16:05:15 +0000

    2009-02-25  Christophe Lyon  <christophe.lyon@st.com>
    	testsuite/
    	* ld-arm/thumb2-bl-as-thumb1-bad-noeabi.d: Update expected result,
    	as stubs are now generated and the end of the .text section.
    	* ld-arm/thumb2-bl-bad-noeabi.d: Likewise.

--- ld/testsuite/ld-arm/thumb2-bl-bad-noeabi.d
+++ ld/testsuite/ld-arm/thumb2-bl-bad-noeabi.d
@@ -2,17 +2,20 @@
 
 Disassembly of section .text:
 
-00001000 <__bar_veneer>:
-    1000:	b401      	push	{r0}
-    1002:	4802      	ldr	r0, \[pc, #8\]	\(100c <__bar_veneer\+0xc>\)
-    1004:	4684      	mov	ip, r0
-    1006:	bc01      	pop	{r0}
-    1008:	4760      	bx	ip
-    100a:	bf00      	nop
-    100c:	0100100d 	.word	0x0100100d
+00001000 <_start>:
+    1000:	f000 f802 	bl	1008 <__bar_veneer>
+    1004:	0000      	lsls	r0, r0, #0
+	...
 
-00001010 <_start>:
-    1010:	f7ff fff6 	bl	1000 <__bar_veneer>
+00001008 <__bar_veneer>:
+    1008:	b401      	push	{r0}
+    100a:	4802      	ldr	r0, \[pc, #8\]	\(1014 <__bar_veneer\+0xc>\)
+    100c:	4684      	mov	ip, r0
+    100e:	bc01      	pop	{r0}
+    1010:	4760      	bx	ip
+    1012:	bf00      	nop
+    1014:	0100100d 	.word	0x0100100d
+
 Disassembly of section .foo:
 
 0100100c <bar>:
