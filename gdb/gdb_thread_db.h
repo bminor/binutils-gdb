5b1ba0e54600876c027f4a35362f37f8fae41219
Nathan Sidwell <nathan@codesourcery.com>
2009-03-18 08:51:11 +0000

    * Makefile.in: Update license to GPLv3. 	* ada-exp.y: Update license to GPLv3. 	* ada-lex.l: Update license to GPLv3. 	* c-exp.y: Update license to GPLv3. 	* cp-name-parser.y: Update license to GPLv3. 	* darwin-nat-info.c: Update license to GPLv3. 	* f-exp.y: Update license to GPLv3. 	* gdb_thread_db.h: Update license to GPLv3. 	* hppanbsd-nat.c: Update license to GPLv3. 	* hppanbsd-tdep.c: Update license to GPLv3. 	* hppaobsd-tdep.c: Update license to GPLv3. 	* jv-exp.y: Update license to GPLv3. 	* m2-exp.y: Update license to GPLv3. 	* objc-exp.y: Update license to GPLv3. 	* p-exp.y: Update license to GPLv3. 	* reply_mig_hack.awk: Update license to GPLv3. 	* reverse.c: Update license to GPLv3. 	* xtensa-xtregs.c: Update license to GPLv3.

--- gdb/gdb_thread_db.h
+++ gdb/gdb_thread_db.h
@@ -7,7 +7,7 @@
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
+   published by the Free Software Foundation; either version 3 of the
    License, or (at your option) any later version.
 
    The GNU C Library is distributed in the hope that it will be useful,
@@ -15,10 +15,8 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #ifndef _THREAD_DB_H
 #define _THREAD_DB_H	1
--- gdb/reply_mig_hack.awk
+++ gdb/reply_mig_hack.awk
@@ -7,7 +7,7 @@
 #
 #   This program is free software; you can redistribute it and/or
 #   modify it under the terms of the GNU General Public License as
-#   published by the Free Software Foundation; either version 2, or (at
+#   published by the Free Software Foundation; either version 3, or (at
 #   your option) any later version.
 #
 #   This program is distributed in the hope that it will be useful, but
@@ -16,9 +16,7 @@
 #   General Public License for more details.
 #
 #   You should have received a copy of the GNU General Public License
-#   along with this program; if not, write to the Free Software
-#   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-#   Boston, MA 02110-1301, USA.
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # This awk script hacks the output of mig-generated reply server code
 # so that it allows replies with just the error-code in them (as this is
