c28c63d86bd759c7595e85ef13949502e95fd58a
Jon Beniston <jon@beniston.com>
2009-05-18 13:25:35 +0000

    gdb/ 2009-05-18  Jon Beniston <jon@beniston.com>
            * MAINTAINERS: Add lm32 target.
            * Makefile.in: Add lm32 dependencies.
            * NEWS: Indicate lm32 is a new target.
            * configure.tgt: Add lm32 targets.
            * lm32-tdep.c: New file.
    
    gdb/testsuite
    2009-05-18  Jon Beniston <jon@beniston.com>
    
            * gdb.asm/asm-source.exp: Add lm32 target.
    
    include/gdb/
    2009-05-18  Jon Beniston <jon@beniston.com>
    
            * sim-lm32.h: New file.
    
    sim/
    2009-05-18  Jon Beniston <jon@beniston.com>
    
            * MAINTAINERS: Add Jon Beniston as maintainer of lm32 sim.
            * configure.ac: Add lm32 target.
            * lm32: New directory.
    
    sim/common
    2009-05-18  Jon Beniston <jon@beniston.com>
    
            * gennltvals.sh: Add lm32 target.
            * nltvals.def: Add lm32 syscall definitions.
    
    sim/lm32/
    2009-05-18  Jon Beniston <jon@beniston.com>
    
            * Makefile.in: New file.
            * arch.c: New file.
            * arch.h: New file.
            * config.in: New file.
            * configure: New file.
            * configure.ac: New file.
            * cpu.c: New file.
            * cpu.h: New file.
            * cpuall.h: New file.
            * decode.c: New file.
            * decode.h: New file.
            * dv-lm32cpu.c: New file.
            * dv-lm32timer.c: New file.
            * dv-lm32uart.c: New file.
            * lm32.c: New file.
            * lm32-sim.h: New file.
            * mloop.in: New file.
            * model.c: New file.
            * sem.c: New file.
            * sem-switch.c: New file.
            * sim-if.c: New file.
            * sim-main.c: New file.
            * tconfig.in: New file.
            * traps.c: New file.
            * user.c: New file.

--- sim/common/gennltvals.sh
+++ sim/common/gennltvals.sh
@@ -73,3 +73,6 @@ dir=libgloss/v850/sys target=v850
 $shell ${srccom}/gentvals.sh $target sys ${srcroot}/$dir \
 	"syscall.h" 'SYS_[_A-Za-z0-9]*' "${cpp}"
 
+dir=libgloss target=lm32
+$shell ${srccom}/gentvals.sh $target sys ${srcroot}/$dir \
+        "syscall.h" 'SYS_[_A-Za-z0-9]*' "${cpp}"
