dd19d49e4bda8bf973f8e178f19e6d643298876f
Ulrich Weigand <uweigand@de.ibm.com>
2009-06-29 17:26:29 +0000

    * ada-lang.c (ada_index_type): Update comment.

--- gdb/ada-lang.c
+++ gdb/ada-lang.c
@@ -2408,7 +2408,10 @@ ada_array_element_type (struct type *type, int nindices)
 }
 
 /* The type of nth index in arrays of given type (n numbering from 1).
-   Does not examine memory.  */
+   Does not examine memory.  Throws an error if N is invalid or TYPE
+   is not an array type.  NAME is the name of the Ada attribute being
+   evaluated ('range, 'first, 'last, or 'length); it is used in building
+   the error message.  */
 
 static struct type *
 ada_index_type (struct type *type, int n, const char *name)
