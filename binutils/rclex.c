a87a00ea43681b9310995d15ce68d2e3906c6e93
Nick Clifton <nickc@redhat.com>
2009-07-01 15:52:27 +0000

    PR 6714         * rclex.c (handle_quotes): Do not complain about whitespace         separating quoted strings.         (rclex_string): Ignore whitespace when looking for a new opening         quote following a closing quote.

--- binutils/rclex.c
+++ binutils/rclex.c
@@ -390,9 +390,9 @@ handle_quotes (rc_uint_type *len)
 	}
       else
 	{
-	  rcparse_warning ("unexpected character after '\"'");
 	  ++t;
-	  assert (ISSPACE (*t));
+	  if (! ISSPACE (*t))
+	    rcparse_warning ("unexpected character after '\"'");
 	  while (ISSPACE (*t))
 	    {
 	      if ((*t) == '\n')
@@ -679,7 +679,7 @@ static void
 rclex_string (void)
 {
   int c;
-  
+
   while ((c = rclex_peekch ()) != -1)
     {
       if (c == '\n')
@@ -693,6 +693,18 @@ rclex_string (void)
         }
       else if (rclex_readch () == '"')
 	{
+	  /* PR 6714
+	     Skip any whitespace after the end of the double quotes.  */
+	  do
+	    {
+	      c = rclex_peekch ();
+	      if (ISSPACE (c))
+		rclex_readch ();
+	      else
+		c = -1;
+	    }
+	  while (c != -1);
+		
 	  if (rclex_peekch () == '"')
 	    rclex_readch ();
 	  else
