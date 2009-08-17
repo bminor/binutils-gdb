ff3536bc839654611f072ec3a77451eba1c47342
Ulrich Weigand <uweigand@de.ibm.com>
2009-08-17 20:09:38 +0000

    * symfile.c (reread_symbols): Call objfiles_changed *before* 	calling clear_symtab_users.

--- gdb/symfile.c
+++ gdb/symfile.c
@@ -2467,13 +2467,13 @@ reread_symbols (void)
 
   if (reread_one)
     {
+      /* Notify objfiles that we've modified objfile sections.  */
+      objfiles_changed ();
+
       clear_symtab_users ();
       /* At least one objfile has changed, so we can consider that
          the executable we're debugging has changed too.  */
       observer_notify_executable_changed ();
-
-      /* Notify objfiles that we've modified objfile sections.  */
-      objfiles_changed ();
     }
 }
 
