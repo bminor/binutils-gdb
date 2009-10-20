40625c9de32122a71ddc72826e6c5f43eae175cd
Pedro Alves <palves@redhat.com>
2009-10-20 11:23:28 +0000

    * darwin-nat.c (cancel_breakpoint): Pass the regcache's address 	space to breakpoint_inserted_here_p.

--- gdb/darwin-nat.c
+++ gdb/darwin-nat.c
@@ -995,7 +995,7 @@ cancel_breakpoint (ptid_t ptid)
   CORE_ADDR pc;
 
   pc = regcache_read_pc (regcache) - gdbarch_decr_pc_after_break (gdbarch);
-  if (breakpoint_inserted_here_p (pc))
+  if (breakpoint_inserted_here_p (get_regcache_aspace (regcache), pc))
     {
       inferior_debug (4, "cancel_breakpoint for thread %x\n",
 		      ptid_get_tid (ptid));
