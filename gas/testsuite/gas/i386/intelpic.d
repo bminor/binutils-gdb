14610ad10e3d4b84565d3590eaa9582f9793f743
Alan Modra <amodra@gmail.com>
2009-10-28 08:21:45 +0000

    PR gas/10856 	* expr.c (resolve_expression): Only add "left" value to O_symbol 	expression when the symbol is undefined and different from the 	original symbol.  Simplify negative logic.
    	* gas/i386/intelpic.d: Correct.

--- gas/testsuite/gas/i386/intelpic.d
+++ gas/testsuite/gas/i386/intelpic.d
@@ -12,6 +12,6 @@ Disassembly of section .text:
 0+1 <bar>:
 [ 	]*[a-f0-9]+:	8d 83 14 00 00 00    	lea    0x14\(%ebx\),%eax
 [ 	]*[a-f0-9]+:	8b 83 00 00 00 00    	mov    0x0\(%ebx\),%eax
-[ 	]*[a-f0-9]+:	ff 24 85 1a 00 00 00 	jmp    \*0x1a\(,%eax,4\)
+[ 	]*[a-f0-9]+:	ff 24 85 0d 00 00 00 	jmp    \*0xd\(,%eax,4\)
 [ 	]*[a-f0-9]+:	8d 83 14 00 00 00    	lea    0x14\(%ebx\),%eax
 #pass
