8da349a0742541a78ba236ed12463ce0b97aef5f
Ian Lance Taylor <ian@airs.com>
2009-11-03 15:42:51 +0000

    PR 10893 	* defstd.cc (in_section): Add entries for __rel_iplt_start, 	__rel_iplt_end, __rela_iplt_start, __rela_iplt_end, and __stack.

--- gold/defstd.cc
+++ gold/defstd.cc
@@ -107,7 +107,67 @@ const Define_symbol_in_section in_section[] =
     0,				// nonvis
     true,			// offset_is_from_end
     true			// only_if_ref
-  }
+  },
+  {
+    "__rel_iplt_start",		// name
+    ".rel.iplt",		// output_section
+    0,				// value
+    0,				// size
+    elfcpp::STT_NOTYPE,		// type
+    elfcpp::STB_GLOBAL,		// binding
+    elfcpp::STV_HIDDEN,		// visibility
+    0,				// nonvis
+    false,			// offset_is_from_end
+    true			// only_if_ref
+  },
+  {
+    "__rel_iplt_end",		// name
+    ".rel.iplt",		// output_section
+    0,				// value
+    0,				// size
+    elfcpp::STT_NOTYPE,		// type
+    elfcpp::STB_GLOBAL,		// binding
+    elfcpp::STV_HIDDEN,		// visibility
+    0,				// nonvis
+    true,			// offset_is_from_end
+    true			// only_if_ref
+  },
+  {
+    "__rela_iplt_start",	// name
+    ".rela.iplt",		// output_section
+    0,				// value
+    0,				// size
+    elfcpp::STT_NOTYPE,		// type
+    elfcpp::STB_GLOBAL,		// binding
+    elfcpp::STV_HIDDEN,		// visibility
+    0,				// nonvis
+    false,			// offset_is_from_end
+    true			// only_if_ref
+  },
+  {
+    "__rela_iplt_end",		// name
+    ".rela.iplt",		// output_section
+    0,				// value
+    0,				// size
+    elfcpp::STT_NOTYPE,		// type
+    elfcpp::STB_GLOBAL,		// binding
+    elfcpp::STV_HIDDEN,		// visibility
+    0,				// nonvis
+    true,			// offset_is_from_end
+    true			// only_if_ref
+  },
+  {
+    "__stack",			// name
+    ".stack",			// output_section
+    0,				// value
+    0,				// size
+    elfcpp::STT_NOTYPE,		// type
+    elfcpp::STB_GLOBAL,		// binding
+    elfcpp::STV_DEFAULT,	// visibility
+    0,				// nonvis
+    false,			// offset_is_from_end
+    true			// only_if_ref
+  },
 };
 
 const int in_section_count = sizeof in_section / sizeof in_section[0];
