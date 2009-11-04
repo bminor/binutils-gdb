1ae4d23b731a2a5fa5d688bc255d52e7f1eb9232
Ian Lance Taylor <ian@airs.com>
2009-11-04 01:24:41 +0000

    PR 10860 	* options.h (class General_options): Add --warn-common. 	* resolve.cc (Symbol_table::resolve): Handle --warn-common when 	merging two common symbols. 	(Symbol_table::should_override): Handle --warn-common when merging 	a common symbol with a defined symbol.  Use report_resolve_problem 	for multiple definitions. 	(Symbol_table::report_resolve_problem): New function. 	* symtab.h (class Symbol_table): Declare report_resolve_problem.

--- gold/options.h
+++ gold/options.h
@@ -918,6 +918,10 @@ class General_options
   DEFINE_special(version_script, options::TWO_DASHES, '\0',
                  N_("Read version script"), N_("FILE"));
 
+  DEFINE_bool(warn_common, options::TWO_DASHES, '\0', false,
+	      N_("Warn about duplicate common symbols"),
+	      N_("Do not warn about duplicate common symbols (default)"));
+
   DEFINE_bool(warn_constructors, options::TWO_DASHES, '\0', false,
 	      N_("Ignored"), N_("Ignored"));
 
