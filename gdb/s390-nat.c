1e061d1a6b958f46f91f19d4d953e86160d83294
Michael Snyder <msnyder@vmware.com>
2009-11-11 19:38:34 +0000

    2009-11-11  Michael Snyder  <msnyder@vmware.com>
    	* s390-nat.c: Update copyright.

--- gdb/s390-nat.c
+++ gdb/s390-nat.c
@@ -1,5 +1,5 @@
 /* S390 native-dependent code for GDB, the GNU debugger.
-   Copyright (C) 2001, 2003, 2004, 2005, 2006
+   Copyright (C) 2001, 2003, 2004, 2005, 2006, 2007, 2009
    Free Software Foundation, Inc
 
    Contributed by D.J. Barrow (djbarrow@de.ibm.com,barrow_dj@yahoo.com)
