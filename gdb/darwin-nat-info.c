f801e1e0ba77d02edaf429e823e2b4734c69a5b8
Michael Snyder <msnyder@vmware.com>
2009-11-11 20:07:39 +0000

    2009-11-11  Michael Snyder  <msnyder@vmware.com>
    	* darwin-nat-info.c: Update copyright.
    	* dbxread.c: Ditto.
    	* gdbarch.sh: Ditto.
    	* gdbarch.c: Ditto.
    	* gdbarch.h: Ditto.
    	* inline-frame.c: Ditto.
    	* inline-frame.h: Ditto.

--- gdb/darwin-nat-info.c
+++ gdb/darwin-nat-info.c
@@ -1,5 +1,5 @@
 /* Darwin support for GDB, the GNU debugger.
-   Copyright 1997, 1998, 1999, 2000, 2001, 2002, 2008
+   Copyright 1997, 1998, 1999, 2000, 2001, 2002, 2008, 2009
    Free Software Foundation, Inc.
 
    Contributed by Apple Computer, Inc.
--- gdb/dbxread.c
+++ gdb/dbxread.c
@@ -1,6 +1,6 @@
 /* Read dbx symbol tables and convert to internal format, for GDB.
    Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995,
-   1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2008.
+   1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2008, 2009.
    Free Software Foundation, Inc.
 
    This file is part of GDB.
