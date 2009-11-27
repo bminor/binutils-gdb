69a630b52974ca2d0469cd5c4f974c4030824fc7
H.J. Lu <hjl.tools@gmail.com>
2009-11-27 01:47:03 +0000

    2009-11-26  Per Øyvind Karlsen <peroyvind@mandriva.org>
    	* plugin.c (bfd_plugin_bfd_copy_link_hash_symbol_type): Define.

--- bfd/plugin.c
+++ bfd/plugin.c
@@ -68,6 +68,7 @@
 #define bfd_plugin_bfd_discard_group                  bfd_generic_discard_group
 #define bfd_plugin_section_already_linked             _bfd_generic_section_already_linked
 #define bfd_plugin_bfd_define_common_symbol           bfd_generic_define_common_symbol
+#define bfd_plugin_bfd_copy_link_hash_symbol_type     _bfd_generic_copy_link_hash_symbol_type
 
 static enum ld_plugin_status
 message (int level ATTRIBUTE_UNUSED,
