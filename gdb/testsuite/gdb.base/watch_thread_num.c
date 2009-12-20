d53a7b30c1996081dfffc6c33ecd18ac42a58610
Joel Brobecker <brobecker@gnat.com>
2009-12-20 11:55:25 +0000

    * Makefile.in gdb.ada/gnat_ada.gpr, gdb.base/gcore-buffer-overflow.c,         gdb.base/gcore-buffer-overflow.exp, gdb.base/source-error.gdb,         gdb.base/watch_thread_num.c, gdb.java/jprint.java,         gdb.mi/mi-async.exp, gdb.modula2/unbounded-array.exp,         gdb.modula2/unbounded1.c: Update copyright header.

--- gdb/testsuite/gdb.base/watch_thread_num.c
+++ gdb/testsuite/gdb.base/watch_thread_num.c
@@ -2,9 +2,12 @@
 
    Copyright 2002, 2003, 2004, 2007, 2008, 2009 Free Software Foundation, Inc.
 
+   Copyright 1992, 1993, 1994, 1995, 1999, 2002, 2003, 2007, 2008, 2009
+   Free Software Foundation, Inc.
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
+   the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -13,9 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
    This file is copied from schedlock.c.  */
 
