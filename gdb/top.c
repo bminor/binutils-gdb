4a70f6546bba7e665ecfb31d758cd20f9146d965
Joel Brobecker <brobecker@gnat.com>
2010-01-01 06:56:04 +0000

    * top.c (print_gdb_version): Update copyright year.

--- gdb/top.c
+++ gdb/top.c
@@ -1118,7 +1118,7 @@ print_gdb_version (struct ui_file *stream)
 
   /* Second line is a copyright notice. */
 
-  fprintf_filtered (stream, "Copyright (C) 2009 Free Software Foundation, Inc.\n");
+  fprintf_filtered (stream, "Copyright (C) 2010 Free Software Foundation, Inc.\n");
 
   /* Following the copyright is a brief statement that the program is
      free software, that users are free to copy and change it on
