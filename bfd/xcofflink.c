43ecc30f09f1c63d539bfcedb012b7ec658edce0
H.J. Lu <hjl.tools@gmail.com>
2010-01-01 18:06:10 +0000

    Move 2009 binutils ChangeLog to ChangeLog-2009.

--- bfd/xcofflink.c
+++ bfd/xcofflink.c
@@ -1,6 +1,6 @@
 /* POWER/PowerPC XCOFF linker support.
    Copyright 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-   2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+   2005, 2006, 2007, 2008, 2009, 2010 Free Software Foundation, Inc.
    Written by Ian Lance Taylor <ian@cygnus.com>, Cygnus Support.
 
    This file is part of BFD, the Binary File Descriptor library.
