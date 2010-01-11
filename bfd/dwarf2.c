94df17f2c8950c49bfdedced771bd55206d66b6b
Alan Modra <amodra@gmail.com>
2010-01-11 09:43:08 +0000

    update copyright date

--- bfd/dwarf2.c
+++ bfd/dwarf2.c
@@ -1,6 +1,6 @@
 /* DWARF 2 support.
    Copyright 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-   2004, 2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+   2004, 2005, 2006, 2007, 2008, 2009, 2010 Free Software Foundation, Inc.
 
    Adapted from gdb/dwarf2read.c by Gavin Koch of Cygnus Solutions
    (gavin@cygnus.com).
