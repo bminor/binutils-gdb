09be49c9dd141a814022c8302f9d1d57349a1eda
Dave Korn <dave.korn@artimi.com>
2010-01-28 19:00:03 +0000

    * gas/pe/section-align-1.d: Don't test section flags. 	* gas/pe/section-align-2.d: Likewise.

--- gas/testsuite/gas/pe/section-align-1.d
+++ gas/testsuite/gas/pe/section-align-1.d
@@ -8,22 +8,22 @@
 Sections:
 Idx Name          *Size      *VMA       *LMA       *File off  *Algn
   0 \.text         0*0000000  0*0000000  0*0000000  0*0000000  2\*\*[24]
-                  ALLOC, LOAD, READONLY, CODE
+.*
   1 \.data         0*0000000  0*0000000  0*0000000  0*0000000  2\*\*[24]
-                  ALLOC, LOAD, DATA
+.*
   2 \.bss          0*0000000  0*0000000  0*0000000  0*0000000  2\*\*[24]
-                  ALLOC
+.*
   3 \.none         0*0000000  0*0000000  0*0000000  0*0000000  2\*\*0
-                  ALLOC, LOAD, READONLY, DATA
+.*
   4 \.zero         0*0000000  0*0000000  0*0000000  0*0000000  2\*\*0
-                  ALLOC, LOAD, READONLY, DATA
+.*
   5 \.one          0*0000001  0*0000000  0*0000000  0*00001a4  2\*\*0
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
   6 \.two          0*0000002  0*0000000  0*0000000  0*00001a5  2\*\*0
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
   7 \.three        0*0000003  0*0000000  0*0000000  0*00001a7  2\*\*0
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
   8 \.four         0*0000004  0*0000000  0*0000000  0*00001aa  2\*\*0
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
   9 \.five         0*0000005  0*0000000  0*0000000  0*00001ae  2\*\*0
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
--- gas/testsuite/gas/pe/section-align-3.d
+++ gas/testsuite/gas/pe/section-align-3.d
@@ -8,32 +8,32 @@
 Sections:
 Idx Name          *Size      *VMA       *LMA       *File off  *Algn
   0 \.text         0*0000000  0*0000000  0*0000000  0*0000000  2\*\*[24]
-                  ALLOC, LOAD, READONLY, CODE
+.*
   1 \.data         0*0000000  0*0000000  0*0000000  0*0000000  2\*\*[24]
-                  ALLOC, LOAD, DATA
+.*
   2 \.bss          0*0000000  0*0000000  0*0000000  0*0000000  2\*\*[24]
-                  ALLOC
+.*
   3 \.none         0*0000000  0*0000000  0*0000000  0*0000000  2\*\*3
-                  ALLOC, LOAD, READONLY, DATA
+.*
   4 \.zero         0*0000000  0*0000000  0*0000000  0*0000000  2\*\*3
-                  ALLOC, LOAD, READONLY, DATA
+.*
   5 \.one          0*0000008  0*0000000  0*0000000  0*000026c  2\*\*3
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
   6 \.two          0*0000008  0*0000000  0*0000000  0*0000274  2\*\*3
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
   7 \.three        0*0000008  0*0000000  0*0000000  0*000027c  2\*\*3
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
   8 \.four         0*0000008  0*0000000  0*0000000  0*0000284  2\*\*3
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
   9 \.five         0*0000008  0*0000000  0*0000000  0*000028c  2\*\*3
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
  10 \.six          0*0000008  0*0000000  0*0000000  0*0000294  2\*\*3
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
  11 \.seven        0*0000008  0*0000000  0*0000000  0*000029c  2\*\*3
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
  12 \.eight        0*0000008  0*0000000  0*0000000  0*00002a4  2\*\*3
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
  13 \.nine         0*0000010  0*0000000  0*0000000  0*00002ac  2\*\*3
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
  14 \.ten          0*0000010  0*0000000  0*0000000  0*00002bc  2\*\*3
-                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+.*
