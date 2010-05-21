f346a30d48d166e2471126b8121a819ef39e3312
Pierre Muller <muller@sourceware.org>
2010-05-21 20:45:19 +0000

    * dwarf2read.c (new_symbol): Handle DW_AT_variable_parameter 	attribute.

--- gdb/dwarf2read.c
+++ gdb/dwarf2read.c
@@ -8664,6 +8664,15 @@ new_symbol (struct die_info *die, struct type *type, struct dwarf2_cu *cu)
 	    {
 	      dwarf2_const_value (attr, sym, cu);
 	    }
+	  attr = dwarf2_attr (die, DW_AT_variable_parameter, cu);
+	  if (attr && DW_UNSND (attr))
+	    {
+	      struct type *ref_type;
+
+	      ref_type = lookup_reference_type (SYMBOL_TYPE (sym));
+	      SYMBOL_TYPE (sym) = ref_type;
+	    }
+
 	  add_symbol_to_list (sym, cu->list_in_scope);
 	  break;
 	case DW_TAG_unspecified_parameters:
