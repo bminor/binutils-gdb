af76db58d70f013feb5e5854ea8bd23929275b7b
Jan Kratochvil <jan.kratochvil@redhat.com>
2010-06-25 15:32:49 +0000

    gdb/testsuite/ 	* gdb.dwarf2/dw2-ref-missing-frame-main.c: New. 	* gdb.dwarf2/dw2-ref-missing-frame.S: Replace compiler generated output 	by a hand made one. 	* gdb.dwarf2/dw2-ref-missing-frame.exp: Remove invalid comment.  Remove 	i?86 requirement.  Remove variables srcfile and binfile.  New variable 	sources, executable_nofb and executable_fb.  Call prepare_for_testing.

---
+++ gdb/testsuite/gdb.dwarf2/dw2-ref-missing-frame-main.c
@@ -0,0 +1,30 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2009, 2010 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+asm (".globl main_start");
+asm ("main_start:");
+
+int
+main (void)
+{
+  /* int main_var; */
+
+  return 0;
+}
+
+asm (".globl main_end");
+asm ("main_end:");
