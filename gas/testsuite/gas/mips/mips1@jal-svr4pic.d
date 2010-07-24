4d2ad3b06d0951856569e528340b4bb960bbdbca
Maciej W. Rozycki <macro@linux-mips.org>
2010-07-24 01:43:32 +0000

    * gas/mips/jal-svr4pic.d: Rename to... 	* gas/mips/mips1@jal-svr4pic.d: ... this. 	* gas/mips/r3000@jal-svr4pic.d: New test subarchitecture. 	* gas/mips/jal-svr4pic.d: New default subarchitecture patterns. 	* gas/mips/mips.exp: Run jal-svr4pic with run_dump_test_arches 	rather than run_dump_test.

--- gas/testsuite/gas/mips/mips1@jal-svr4pic.d
+++ gas/testsuite/gas/mips/mips1@jal-svr4pic.d
@@ -3,7 +3,7 @@
 #as: -32 -KPIC
 #source: jal-svr4pic.s
 
-# Test the jal macro with -KPIC.
+# Test the jal macro with -KPIC (MIPS1).
 
 .*: +file format .*mips.*
 
@@ -16,28 +16,36 @@ Disassembly of section \.text:
 [0-9a-f]+ <[^>]*> afbc0000 	sw	gp,0\(sp\)
 [0-9a-f]+ <[^>]*> 0320f809 	jalr	t9
 [0-9a-f]+ <[^>]*> 00000000 	nop
+[0-9a-f]+ <[^>]*> 8fbc0000 	lw	gp,0\(sp\)
 [0-9a-f]+ <[^>]*> 03202009 	jalr	a0,t9
+[0-9a-f]+ <[^>]*> 00000000 	nop
 [0-9a-f]+ <[^>]*> 8fbc0000 	lw	gp,0\(sp\)
-[0-9a-f]+ <[^>]*> 8fbc0000 	lw	gp,0\(sp\)
+[0-9a-f]+ <[^>]*> 00000000 	nop
 [0-9a-f]+ <[^>]*> 8f990000 	lw	t9,0\(gp\)
-[ 	]*24: R_MIPS_GOT16	.text
+[ 	]*2c: R_MIPS_GOT16	.text
+[0-9a-f]+ <[^>]*> 00000000 	nop
 [0-9a-f]+ <[^>]*> 27390000 	addiu	t9,t9,0
-[ 	]*28: R_MIPS_LO16	.text
+[ 	]*34: R_MIPS_LO16	.text
 [0-9a-f]+ <[^>]*> 0320f809 	jalr	t9
-[ 	]*2c: R_MIPS_JALR	text_label
+[ 	]*38: R_MIPS_JALR	text_label
 [0-9a-f]+ <[^>]*> 00000000 	nop
 [0-9a-f]+ <[^>]*> 8fbc0000 	lw	gp,0\(sp\)
+[0-9a-f]+ <[^>]*> 00000000 	nop
 [0-9a-f]+ <[^>]*> 8f990000 	lw	t9,0\(gp\)
-[ 	]*38: R_MIPS_CALL16	weak_text_label
+[ 	]*48: R_MIPS_CALL16	weak_text_label
+[0-9a-f]+ <[^>]*> 00000000 	nop
 [0-9a-f]+ <[^>]*> 0320f809 	jalr	t9
-[ 	]*3c: R_MIPS_JALR	weak_text_label
+[ 	]*50: R_MIPS_JALR	weak_text_label
 [0-9a-f]+ <[^>]*> 00000000 	nop
 [0-9a-f]+ <[^>]*> 8fbc0000 	lw	gp,0\(sp\)
+[0-9a-f]+ <[^>]*> 00000000 	nop
 [0-9a-f]+ <[^>]*> 8f990000 	lw	t9,0\(gp\)
-[ 	]*48: R_MIPS_CALL16	external_text_label
-[0-9a-f]+ <[^>]*> 0320f809 	jalr	t9
-[ 	]*4c: R_MIPS_JALR	external_text_label
+[ 	]*60: R_MIPS_CALL16	external_text_label
 [0-9a-f]+ <[^>]*> 00000000 	nop
-[0-9a-f]+ <[^>]*> 1000ffea 	b	0+0000 <text_label>
+[0-9a-f]+ <[^>]*> 0320f809 	jalr	t9
+[ 	]*68: R_MIPS_JALR	external_text_label
+[0-9a-f]+ <[^>]*> 00000000 	nop
 [0-9a-f]+ <[^>]*> 8fbc0000 	lw	gp,0\(sp\)
+[0-9a-f]+ <[^>]*> 1000ffe2 	b	0+0000 <text_label>
+[0-9a-f]+ <[^>]*> 00000000 	nop
 	\.\.\.
