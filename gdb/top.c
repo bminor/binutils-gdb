958c04dee315fefd3cbbfe385a5ebd25eda1d695
Jan Kratochvil <jan.kratochvil@redhat.com>
2010-07-27 19:13:11 +0000

    gdb/ 	* top.c (input_from_terminal_p): Return 0 on BATCH_FLAG. 	* utils.c (defaulted_query): Do not explicitly check for BATCH_FLAG. 	(fputs_maybe_filtered): Do not do filtering also on 	! INPUT_FROM_TERMINAL_P.

--- gdb/top.c
+++ gdb/top.c
@@ -1242,6 +1242,9 @@ input_from_terminal_p (void)
   if (interactive_mode != AUTO_BOOLEAN_AUTO)
     return interactive_mode == AUTO_BOOLEAN_TRUE;
 
+  if (batch_flag)
+    return 0;
+
   if (gdb_has_a_terminal () && instream == stdin)
     return 1;
 
