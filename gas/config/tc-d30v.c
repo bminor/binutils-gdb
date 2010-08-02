69da848e6ebe8d0b4196f2b630b112fa43dc9348
Alan Modra <amodra@gmail.com>
2010-08-02 13:53:29 +0000

    * config/tc-d30v.c (d30v_cons_align): Don't align constants 	in debug sections.

--- gas/config/tc-d30v.c
+++ gas/config/tc-d30v.c
@@ -1884,6 +1884,10 @@ d30v_cons_align (int size)
 {
   int log_size;
 
+  /* Don't specially align anything in debug sections.  */
+  if ((now_seg->flags & SEC_ALLOC) == 0)
+    return;
+
   log_size = 0;
   while ((size >>= 1) != 0)
     ++log_size;
