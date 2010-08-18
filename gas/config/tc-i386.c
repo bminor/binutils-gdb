9f2670f27b3d38d9faba9388a5fe6ccafaae9864
H.J. Lu <hjl.tools@gmail.com>
2010-08-18 19:36:41 +0000

    Check i.imm_operands VEXXDS.
    2010-08-18  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* config/tc-i386.c (build_modrm_byte): Check i.imm_operands
    	instead of VEXXDS.

--- gas/config/tc-i386.c
+++ gas/config/tc-i386.c
@@ -5264,15 +5264,15 @@ build_modrm_byte (void)
 			  && i.imm_operands == 1
 			  && (i.types[0].bitfield.imm8
 			      || i.types[i.operands - 1].bitfield.imm8)));
-	  if (i.tm.opcode_modifier.vexvvvv == VEXXDS)
+	  if (i.imm_operands == 2)
+	    source = 2;
+	  else
 	    {
 	      if (i.types[0].bitfield.imm8)
 		source = 1;
 	      else
 		source = 0;
 	    }
-	  else
-	    source = 2;
 	  break;
 	case 5:
 	  break;
