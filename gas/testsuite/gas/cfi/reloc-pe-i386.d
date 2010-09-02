f917d7a192c7134e09717e09f1316bf397b5d25b
Richard Henderson <rth@redhat.com>
2010-09-02 22:44:09 +0000

    * gas/cfi/reloc-pe-i386.d, gas/cfi/reloc-pe-i386.s: New test.

---
+++ gas/testsuite/gas/cfi/reloc-pe-i386.d
@@ -0,0 +1,9 @@
+#objdump: -r
+#name: reloc-pe-i386
+#...
+RELOCATION RECORDS FOR \[.debug_frame\]:
+OFFSET           TYPE              VALUE 
+000000000000001c secrel32          .debug_frame
+0000000000000020 .* .text
+
+
