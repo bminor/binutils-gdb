d3615e3a49afae80de3c47f213ac87114706f0ea
Hans-Peter Nilsson <hp@axis.com>
2010-09-10 01:00:28 +0000

    * ld-scripts/rgn-at5.d: Restrict to *-*-linux*.

--- ld/testsuite/ld-scripts/rgn-at5.d
+++ ld/testsuite/ld-scripts/rgn-at5.d
@@ -2,6 +2,7 @@
 # source: rgn-at5.s
 # ld: -T rgn-at5.t -z max-page-size=0x1000
 # objdump: -w -h
+# target: *-*-linux*
 # xfail: rx-*-*
 #   FAILS on the RX because the linker has to set LMA == VMA for the
 #   Renesas loader.
