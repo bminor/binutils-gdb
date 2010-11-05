b851162aada557f0e1e25246b0580a56261c3f1e
Dave Korn <dave.korn@artimi.com>
2010-11-05 04:58:24 +0000

    * config/obj-coff.c (weak_altname2name): Don't infer from the presence 	of a period that the symbol has been already uniquify-d. 	(weak_uniquify): Don't worry that the symbol might have been already 	uniquify-d.

--- gas/config/obj-coff.c
+++ gas/config/obj-coff.c
@@ -1095,15 +1095,8 @@ weak_name2altname (const char * name)
 static const char *
 weak_altname2name (const char * name)
 {
-  char * weak_name;
-  char * dot;
-
   gas_assert (weak_is_altname (name));
-
-  weak_name = xstrdup (name + 6);
-  if ((dot = strchr (weak_name, '.')))
-    *dot = 0;
-  return weak_name;
+  return xstrdup (name + 6);
 }
 
 /* Make a weak symbol name unique by
@@ -1120,9 +1113,6 @@ weak_uniquify (const char * name)
     unique = an_external_name;
 #endif
   gas_assert (weak_is_altname (name));
-
-  if (strchr (name + sizeof (weak_altprefix), '.'))
-    return name;
 
   ret = xmalloc (strlen (name) + strlen (unique) + 2);
   strcpy (ret, name);
