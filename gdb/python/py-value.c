fa595c2727bbe70440238f3a15236866d189e73f
Tom Tromey <tromey@redhat.com>
2010-12-16 17:40:27 +0000

    2010-12-16  Joel Borggrén-Franck  <joel.borggren.franck@gmail.com>
    	* python/py-value.c (value_object_type) <tp_flags>: Add
    	Py_TPFLAGS_BASETYPE.

--- gdb/python/py-value.c
+++ gdb/python/py-value.c
@@ -1320,7 +1320,7 @@ PyTypeObject value_object_type = {
   0,				  /*tp_getattro*/
   0,				  /*tp_setattro*/
   0,				  /*tp_as_buffer*/
-  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_CHECKTYPES,	/*tp_flags*/
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_CHECKTYPES | Py_TPFLAGS_BASETYPE, /*tp_flags*/
   "GDB value object",		  /* tp_doc */
   0,				  /* tp_traverse */
   0,				  /* tp_clear */
