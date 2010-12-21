bc2b29908f99c56de320c765c932776cef1aa7c6
Pierre Muller <muller@sourceware.org>
2010-12-21 08:41:16 +0000

    * peXXigen.c (_bfd_XXi_final_link_postscript): Use correct size 	for windows 64-bit TLS table size.

--- bfd/peXXigen.c
+++ bfd/peXXigen.c
@@ -2437,8 +2437,15 @@ _bfd_XXi_final_link_postscript (bfd * abfd, struct coff_final_link_info *pfinfo)
 	     abfd);
 	  result = FALSE;
 	}
-
+     /* According to PECOFF sepcifications by Microsoft version 8.2
+	the TLS data directory consists of 4 pointers, followed
+	by two 4-byte integer. This implies that the total size
+	is different for 32-bit and 64-bit executables.  */ 
+#if !defined(COFF_WITH_pep) && !defined(COFF_WITH_pex64)
       pe_data (abfd)->pe_opthdr.DataDirectory[PE_TLS_TABLE].Size = 0x18;
+#else
+      pe_data (abfd)->pe_opthdr.DataDirectory[PE_TLS_TABLE].Size = 0x28;
+#endif
     }
 
 /* If there is a .pdata section and we have linked pdata finally, we
