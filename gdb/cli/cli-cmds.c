f55af66d6ed6e6ad47757c5fff7ab5df1284ce33
Tom Tromey <tromey@redhat.com>
2011-01-17 16:20:56 +0000

    * cli/cli-cmds.c (apropos_command): Fix formatting.  Don't call 	re_compile_fastmap.

--- gdb/cli/cli-cmds.c
+++ gdb/cli/cli-cmds.c
@@ -1253,28 +1253,20 @@ show_user (char *args, int from_tty)
 void 
 apropos_command (char *searchstr, int from_tty)
 {
-  extern struct cmd_list_element *cmdlist; /* This is the main command
-					      list.  */
   regex_t pattern;
-  char *pattern_fastmap;
   char errorbuffer[512];
 
-  pattern_fastmap = xcalloc (256, sizeof (char));
   if (searchstr == NULL)
-      error (_("REGEXP string is empty"));
+    error (_("REGEXP string is empty"));
 
-  if (regcomp(&pattern,searchstr,REG_ICASE) == 0)
-    {
-      pattern.fastmap=pattern_fastmap;
-      re_compile_fastmap(&pattern);
-      apropos_cmd (gdb_stdout,cmdlist,&pattern,"");
-    }
+  if (regcomp (&pattern, searchstr, REG_ICASE) == 0)
+    apropos_cmd (gdb_stdout, cmdlist, &pattern, "");
   else
     {
-      regerror(regcomp(&pattern,searchstr,REG_ICASE),NULL,errorbuffer,512);
-      error (_("Error in regular expression:%s"),errorbuffer);
+      regerror (regcomp (&pattern, searchstr, REG_ICASE), NULL,
+		errorbuffer, 512);
+      error (_("Error in regular expression: %s"), errorbuffer);
     }
-  xfree (pattern_fastmap);
 }
 
 /* Print a list of files and line numbers which a user may choose from
