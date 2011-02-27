2a589cef7917d96d8ed313c45909722a7dc9ec99
Michael Snyder <msnyder@vmware.com>
2011-02-27 21:33:10 +0000

    2011-02-27  Michael Snyder  <msnyder@vmware.com>
    	* i386-low.c (i386-length_and_rw_bits): Comment the fact that
    	a case statement falls through.

--- gdb/gdbserver/i386-low.c
+++ gdb/gdbserver/i386-low.c
@@ -243,6 +243,7 @@ Invalid hardware breakpoint type %d in i386_length_and_rw_bits.\n",
 	return (DR_LEN_2 | rw);
       case 4:
 	return (DR_LEN_4 | rw);
+	/* ELSE FALL THROUGH */
       case 8:
         if (TARGET_HAS_DR_LEN_8)
  	  return (DR_LEN_8 | rw);
