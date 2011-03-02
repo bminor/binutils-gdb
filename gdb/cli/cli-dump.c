53624a9319e4e154f39609188f1d8c26771bd75e
Michael Snyder <msnyder@vmware.com>
2011-03-02 01:09:52 +0000

    2011-03-01  Michael Snyder  <msnyder@vmware.com>
    	* cli/cli-dump.c (dump_bfd_file): Check error return and warn.

--- gdb/cli/cli-dump.c
+++ gdb/cli/cli-dump.c
@@ -202,7 +202,9 @@ dump_bfd_file (const char *filename, const char *mode,
 					  | SEC_ALLOC
 					  | SEC_LOAD));
   osection->entsize = 0;
-  bfd_set_section_contents (obfd, osection, buf, 0, len);
+  if (!bfd_set_section_contents (obfd, osection, buf, 0, len))
+    warning (_("writing dump file '%s' (%s)"), filename, 
+	     bfd_errmsg (bfd_get_error ()));
 }
 
 static void
