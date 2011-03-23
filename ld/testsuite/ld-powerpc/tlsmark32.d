ac4356123863c4777bf1785013227420708d2da8
Alan Modra <amodra@gmail.com>
2011-03-23 15:26:20 +0000

    bfd/ 	* elf32-ppc.c (ppc_elf_tls_optimize): Catch more cases where 	old-style __tls_get_addr calls without marker relocs don't match 	their arg setup insn one for one.  If such mismatches are found 	report the reloc and don't do any tls optimization. 	* elf64-ppc.c (ppc64_elf_tls_optimize): Likewise. ld/testsuite/ 	* ld-powerpc/tlsmark.s: Delete non-optimizable section. 	* ld-powerpc/tlsmark32.s: Likewise. 	* ld-powerpc/tlsmark.d: Adjust to suit. 	* ld-powerpc/tlsmark32.d: Likewise. 	* ld-powerpc/tlsopt1.d, * ld-powerpc/tlsopt1.s: New. 	* ld-powerpc/tlsopt2.d, * ld-powerpc/tlsopt2.s: New. 	* ld-powerpc/tlsopt3.d, * ld-powerpc/tlsopt3.s: New. 	* ld-powerpc/tlsopt4.d, * ld-powerpc/tlsopt4.s: New. 	* ld-powerpc/tlsopt1_32.d, * ld-powerpc/tlsopt1_32.s: New. 	* ld-powerpc/tlsopt2_32.d, * ld-powerpc/tlsopt2_32.s: New. 	* ld-powerpc/tlsopt3_32.d, * ld-powerpc/tlsopt3_32.s: New. 	* ld-powerpc/tlsopt4_32.d, * ld-powerpc/tlsopt4_32.s: New. 	* ld-powerpc/powerpc.exp: Run new tests.

--- ld/testsuite/ld-powerpc/tlsmark32.d
+++ ld/testsuite/ld-powerpc/tlsmark32.d
@@ -19,11 +19,7 @@ Disassembly of section \.text:
  18000ac:	4b ff ff ec 	b       1800098 <_start\+0x4>
  18000b0:	38 63 10 00 	addi    r3,r3,4096
  18000b4:	80 83 80 00 	lwz     r4,-32768\(r3\)
- 18000b8:	38 7f ff f4 	addi    r3,r31,-12
- 18000bc:	3f a0 01 80 	lis     r29,384
- 18000c0:	3b bd 00 c8 	addi    r29,r29,200
- 18000c4:	48 00 00 05 	bl      18000c8 <__tls_get_addr>
 
-0+18000c8 <__tls_get_addr>:
- 18000c8:	4e 80 00 20 	blr
+0+18000b8 <__tls_get_addr>:
+ 18000b8:	4e 80 00 20 	blr
 #pass
