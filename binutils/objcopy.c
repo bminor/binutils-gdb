1e99536ae5e3e54a10f35e8c915d3bd6c44140bd
H.J. Lu <hjl.tools@gmail.com>
2011-04-04 01:35:59 +0000

    Make the unknown archive element readable.
    2011-04-03  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR binutils/12632
    	* objcopy.c (copy_unknown_object): Make the archive element
    	readable.

--- binutils/objcopy.c
+++ binutils/objcopy.c
@@ -1399,7 +1399,9 @@ copy_unknown_object (bfd *ibfd, bfd *obfd)
       ncopied += tocopy;
     }
 
-  chmod (bfd_get_filename (obfd), buf.st_mode);
+  /* We should at least to be able to read it back when copying an
+     unknown object in an archive.  */
+  chmod (bfd_get_filename (obfd), buf.st_mode | S_IRUSR);
   free (cbuf);
   return TRUE;
 }
