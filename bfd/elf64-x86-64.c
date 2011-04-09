9f973f284255fa36ee23ac15700037e6f19a1fce
H.J. Lu <hjl.tools@gmail.com>
2011-04-09 16:48:39 +0000

    Update copyright year.
    2011-04-09  H.J. Lu  <hongjiu.lu@intel.com>
    
    	* elf32-i386.c: Update copyright year.
    	* elf64-x86-64.c: Likewise.

--- bfd/elf64-x86-64.c
+++ bfd/elf64-x86-64.c
@@ -1,6 +1,7 @@
 /* X86-64 specific support for ELF
    Copyright 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-   2010  Free Software Foundation, Inc.
+   2010, 2011
+   Free Software Foundation, Inc.
    Contributed by Jan Hubicka <jh@suse.cz>.
 
    This file is part of BFD, the Binary File Descriptor library.
