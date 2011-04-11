0389ec1ea01d00241e4408e926e21dbcdcb8bfff
Nick Clifton <nickc@redhat.com>
2011-04-11 15:12:29 +0000

    * binutils-all/arm/simple.s: Fix assembly problems for COFF based 	ARM toolchaisn by removing .type and .size directives.

--- binutils/testsuite/binutils-all/arm/simple.s
+++ binutils/testsuite/binutils-all/arm/simple.s
@@ -8,7 +8,7 @@ l:
     .text
     .align    2
     .global    f1
-    .type    f1, %function
+
 f1:
     str    fp, [sp, #-4]!
     add    fp, sp, #0
@@ -19,10 +19,10 @@ f1:
     bx    lr
     .align    2
     .word    l
-    .size    f1, .-f1
+
     .align    2
     .global    main
-    .type    main, %function
+
 main:
     stmfd    sp!, {fp, lr}
     add    fp, sp, #4
@@ -31,5 +31,5 @@ main:
     .word    1717986919
     .word    -1840700269
     .word    l
-    .size    main, .-main
+
     .ident    "GCC: (Sourcery G++ 2011.03) 4.5.1"
