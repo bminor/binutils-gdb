1d859532c2502eb48d132591d3be80f468e2d381
Joel Brobecker <brobecker@gnat.com>
2011-06-07 19:50:08 +0000

    Fix build error on mips-irix (missing match_priority)
    A new field was recently added to struct bfd_target, and irix-core.c
    needs an update...
    
    bfd/ChangeLog:
    
            * irix-core.c (irix_core_vec): Add match_priority field.

--- bfd/irix-core.c
+++ bfd/irix-core.c
@@ -293,6 +293,7 @@ const bfd_target irix_core_vec =
     0,			                                   /* symbol prefix */
     ' ',						   /* ar_pad_char */
     16,							   /* ar_max_namelen */
+    0,							   /* match_priority */
     NO_GET64, NO_GETS64, NO_PUT64,	/* 64 bit data */
     NO_GET, NO_GETS, NO_PUT,		/* 32 bit data */
     NO_GET, NO_GETS, NO_PUT,		/* 16 bit data */
