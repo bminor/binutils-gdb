0b5574da47a991d913045af4a8dadcaf23535a43
Doug Evans <dje@google.com>
2011-06-08 00:15:54 +0000

    * cc-with-index.sh: Fix typos in comment.

--- gdb/cc-with-index.sh
+++ gdb/cc-with-index.sh
@@ -16,8 +16,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # This program requires gdb and objcopy in addition to gcc.
-# The default values are gdb from the build tree and objdump from $PATH.
-# They may be overridden by setting environment variables GDB and OBJDUMP
+# The default values are gdb from the build tree and objcopy from $PATH.
+# They may be overridden by setting environment variables GDB and OBJCOPY
 # respectively.
 # We assume the current directory is either $obj/gdb or $obj/gdb/testsuite.
 #
