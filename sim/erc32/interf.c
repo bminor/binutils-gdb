ce6f492fff761960211dd64d9577be6085140d5f
Mike Frysinger <vapier@gentoo.org>
2011-06-09 15:33:03 +0000

    sim: erc32: ignore --sysroot that gdb passes down

--- sim/erc32/interf.c
+++ sim/erc32/interf.c
@@ -234,6 +234,9 @@ sim_open (kind, callback, abfd, argv)
 		if ((stat + 1) < argc) {
 		    freq = strtol(argv[++stat], (char **)NULL, 0);
 		}
+	    } else
+	    if (strncmp(argv[stat], "--sysroot=", sizeof("--sysroot=") - 1) == 0) {
+		/* Ignore until we start to support this.  */
 	    } else {
 		(*sim_callback->printf_filtered) (sim_callback,
 						  "unknown option %s\n",
