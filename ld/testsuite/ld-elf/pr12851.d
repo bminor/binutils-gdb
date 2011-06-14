50b18678190f583324f563128887a6c09a920d75
Alan Modra <amodra@gmail.com>
2011-06-14 02:46:57 +0000

    PR ld/12851 	* ld-elf/pr12851.d: Correct target selection and comment.

--- ld/testsuite/ld-elf/pr12851.d
+++ ld/testsuite/ld-elf/pr12851.d
@@ -3,11 +3,8 @@
 #ld: --gc-sections
 #readelf: -s --wide
 #notarget: arc-*-* d30v-*-* dlx-*-* i960-*-* or32-*-* pj*-*-*
-#notarget: alpha-*-* hppa64-*-* i370-*-* i860-*-* ia64-*-* mep-*-* mn10200-*-*
-#xfail: cr16-*-* crx-*-*
+#notarget: hppa64-*-* i370-*-* i860-*-* ia64-*-* mep-*-* mn10200-*-*
 # generic linker targets don't support --gc-sections, nor do a bunch of others
-# cr16 and crx use non-standard scripts with memory regions, which don't play
-# well with unique group sections under ld -r.
 
 #...
  +.* _.stapsdt.base
