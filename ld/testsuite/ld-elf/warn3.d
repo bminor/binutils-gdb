5daeae9b9787af3a9407ccd2aac73a9effceb1b0
Alan Modra <amodra@gmail.com>
2011-06-14 02:55:29 +0000

    * ld-elf/elf.exp: Don't attempt to build symbol3.a for hppa64-hpux. 	* ld-elf/warn3.d: Correct target selection and comment.

--- ld/testsuite/ld-elf/warn3.d
+++ ld/testsuite/ld-elf/warn3.d
@@ -2,10 +2,9 @@
 #ld: tmpdir/symbol3w.o tmpdir/symbol3.a 
 #warning: .*: warning: badsym warning$
 #readelf: -s
-#notarget: "sparc64-*-solaris2*" "sparcv9-*-solaris2*"
-#xfail: arc-*-* d30v-*-* dlx-*-* fr30-*-* frv-*-elf i860-*-* i960-*-*
-#xfail: iq*-*-* mn10200-*-* moxie-*-* msp*-*-* mt-*-* or32-*-* pj*-*-*
-# if not using elf32.em, you don't get fancy section handling
+#notarget: hppa64*-hpux*
+#xfail: arc-*-* d30v-*-* dlx-*-* i960-*-* or32-*-* pj*-*-*
+# generic linker targets don't support .gnu.warning sections.
 
 # Check that warnings are generated for the symbols in .gnu.warning
 # construct and that the symbol still appears as expected.
