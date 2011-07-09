9c16daf1fcf87c2d75dea6775586135685237b4d
Ian Lance Taylor <ian@airs.com>
2011-07-09 05:09:52 +0000

    * testsuite/odr_violation2.cc (Ordering::operator()): Make 	expression more complex.

--- gold/testsuite/odr_violation2.cc
+++ gold/testsuite/odr_violation2.cc
@@ -12,7 +12,7 @@ class Ordering {
 bool Ordering::operator()(int a, int b) {
   // Optimization makes this operator() a different size than the one
   // in odr_violation1.cc.
-  return a + 1 > b + 1;
+  return a + 12345 > b / 67;
 }
 
 void SortDescending(int array[], int size) {
