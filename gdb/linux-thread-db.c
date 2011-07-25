6cb9b55b982a63d490e9086772eb201f658d6eb4
Paul Pluzhnikov <ppluzhnikov@google.com>
2011-07-25 15:22:07 +0000

    2011-07-25  Paul Pluzhnikov  <ppluzhnikov@google.com>
    	* linux-thread-db.c (thread_from_lwp): Initialize th.th_unique.

--- gdb/linux-thread-db.c
+++ gdb/linux-thread-db.c
@@ -431,6 +431,9 @@ thread_from_lwp (ptid_t ptid)
   struct thread_db_info *info;
   struct thread_get_info_inout io = {0};
 
+  /* Just in case td_ta_map_lwp2thr doesn't initialize it completely.  */
+  th.th_unique = 0;
+
   /* This ptid comes from linux-nat.c, which should always fill in the
      LWP.  */
   gdb_assert (GET_LWP (ptid) != 0);
