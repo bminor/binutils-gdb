8c9461a4ded07f261a0563831500870f259fb59e
Nick Clifton <nickc@redhat.com>
2011-07-29 11:02:40 +0000

    * gas/elf/warn-2.s: Add other types of NOP insn.

--- gas/testsuite/gas/elf/warn-2.s
+++ gas/testsuite/gas/elf/warn-2.s
@@ -1,12 +1,23 @@
 ;# { dg-do assemble }
-;# { dg-skip-if "DWARF2 output not supported" { mcore-*-* mn10*-*-* moxie-*-* v850-*-* } }
-;# { dg-skip-if "no NOP opcode" { mmix-*-* openrisc-*-* or32-*-* } }
-;# { dg-options "--gdwarf2 --defsym ia64=0" }
-;# { dg-options "--gdwarf2 --defsym ia64=1" { target ia64-*-* i370-*-* } }
+;# { dg-options "--gdwarf2 --defsym nop_type=0" }
+;# { dg-options "--gdwarf2 --defsym nop_type=1" { target ia64-*-* } }
+;# { dg-options "--gdwarf2 --defsym nop_type=2" { target or32-*-* openrisc-*-* } }
+;# { dg-options "--gdwarf2 --defsym nop_type=3" { target i370-*-* } }
+
 	.offset 40
-.ifeq ia64 - 1
+	
+.ifeq nop_type - 1
 	nop 0
+.else
+.ifeq nop_type - 2
+	l.nop 0
+.else
+.ifeq nop_type - 3
+	nopr 1
 .else
 	nop
 .endif
-;# { dg-warning "Warning: dwarf line number information for .* ignored" "" { target *-*-* } 0 }
+.endif
+.endif
+
+;# { dg-warning "Warning: dwarf line number information for .* ignored" "" { xfail i370-*-* mcore-*-* mn10200-*-* moxie-*-* openrisc-*-* or32-*-* v850-*-* } 0 }
