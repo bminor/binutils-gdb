72b5104c843e1f9d7f1867339917e6e998772151
Tristan Gingold <gingold@adacore.com>
2011-08-17 10:17:39 +0000

    2011-08-17  Tristan Gingold  <gingold@adacore.com>
    	* mach-o.c (bfd_mach_o_write_section_32): Fix typo.

--- bfd/mach-o.c
+++ bfd/mach-o.c
@@ -957,7 +957,7 @@ bfd_mach_o_write_section_32 (bfd *abfd, bfd_mach_o_section *section)
   struct mach_o_section_32_external raw;
 
   memcpy (raw.sectname, section->sectname, 16);
-  memcpy (raw.segname + 16, section->segname, 16);
+  memcpy (raw.segname, section->segname, 16);
   bfd_h_put_32 (abfd, section->addr, raw.addr);
   bfd_h_put_32 (abfd, section->size, raw.size);
   bfd_h_put_32 (abfd, section->offset, raw.offset);
