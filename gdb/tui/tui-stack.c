c04b3e8fc5b1096c502afa485cf468054af8c626
Jan Kratochvil <jan.kratochvil@redhat.com>
2011-08-29 16:55:16 +0000

    gdb/ 	Fix TUI stepi on code without symbols. 	* tui/tui-stack.c (tui_show_frame_info): Remove error, set LOW for 	current PC instead.

--- gdb/tui/tui-stack.c
+++ gdb/tui/tui-stack.c
@@ -380,8 +380,11 @@ tui_show_frame_info (struct frame_info *fi)
 	    {
 	      if (find_pc_partial_function (get_frame_pc (fi), (char **) NULL,
 					    &low, (CORE_ADDR) 0) == 0)
-		error (_("No function contains program "
-			 "counter for selected frame."));
+		{
+		  /* There is no symbol available for current PC.  There is no
+		     safe way how to "disassemble backwards".  */
+		  low = get_frame_pc (fi);
+		}
 	      else
 		low = tui_get_low_disassembly_address (get_frame_arch (fi),
 						       low, get_frame_pc (fi));
