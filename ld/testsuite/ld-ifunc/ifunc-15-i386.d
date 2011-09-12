7b5b197e80f04c9b95fe7fafbd9907fc61a1e43d
H.J. Lu <hjl.tools@gmail.com>
2011-09-12 18:17:36 +0000

    Use .got.plt for IFUNC symbols if there are no GOT relocations.
    bfd/
    
    2011-09-12  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR ld/13178
    	* elf-ifunc.c (_bfd_elf_allocate_ifunc_dyn_relocs): Use .got.plt
    	if there are no GOT relocations.
    
    ld/testsuite/
    
    2011-09-12  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR ld/13178
    	* ld-ifunc/ifunc-13-i386.d: Updated.
    	* ld-ifunc/ifunc-13-x86-64.d: Likewise.
    	* ld-ifunc/ifunc-3a-x86.d: Likewise.
    	* ld-x86-64/pr13082-5a.d: Likewise.
    
    	* ld-ifunc/ifunc-15-i386.d: New.
    	* ld-ifunc/ifunc-15-i386.s: Likewise.
    	* ld-ifunc/ifunc-15-x86-64.d: Likewise.
    	* ld-ifunc/ifunc-15-x86-64.s: Likewise.

--- ld/testsuite/ld-ifunc/ifunc-15-i386.d
+++ ld/testsuite/ld-ifunc/ifunc-15-i386.d
@@ -1,5 +1,4 @@
-#source: ifunc-13a-i386.s
-#source: ifunc-13b-i386.s
+#source: ifunc-15-i386.s
 #ld: -shared -m elf_i386 -z nocombreloc
 #as: --32
 #readelf: -r --wide
@@ -7,11 +6,7 @@
 
 Relocation section '.rel.got' at offset 0x[0-9a-f]+ contains 1 entries:
 [ ]+Offset[ ]+Info[ ]+Type[ ]+.*
-[0-9a-f]+[ ]+[0-9a-f]+[ ]+R_386_RELATIVE[ ]+
-
-Relocation section '.rel.ifunc' at offset 0x[0-9a-f]+ contains 1 entries:
-[ ]+Offset[ ]+Info[ ]+Type[ ]+.*
-[0-9a-f]+[ ]+[0-9a-f]+[ ]+R_386_32[ ]+ifunc\(\)[ ]+ifunc
+[0-9a-f]+[ ]+[0-9a-f]+[ ]+R_386_GLOB_DAT[ ]+ifunc\(\)[ ]+ifunc
 
 Relocation section '.rel.plt' at offset 0x[0-9a-f]+ contains 1 entries:
 [ ]+Offset[ ]+Info[ ]+Type[ ]+.*
