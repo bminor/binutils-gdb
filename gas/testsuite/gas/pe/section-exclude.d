9af6978cc0abbcd7203b6dbe960ea4b5e207a766
Kai Tietz <kai.tietz@onevision.com>
2011-09-28 12:04:24 +0000

    Another typo ... :(

--- gas/testsuite/gas/pe/section-exclude.d
+++ gas/testsuite/gas/pe/section-exclude.d
@@ -14,5 +14,4 @@ Idx Name          *Size      *VMA       *LMA       *File off  *Algn
   2 \.bss          0*0000000  0*0000000  0*0000000  0*0000000  2\*\*[24]
 .*
   3 \.foo1         0*0000004  0*0000000  0*0000000  .*  2\*\*[24]
-.*CONTENTS, EXCLUDE
-.*
+.*CONTENTS, EXCLUDE.*
