a13852d93636405b7b3cf87f10521e52bcaafe48
Mike Frysinger <vapier@gentoo.org>
2011-11-07 16:28:52 +0000

    sim: dv-cfi: check for posix_fallocate
    Some systems (like OS X) do not have posix_fallocate.  Add a configure
    check for it before we try to use it.  This is less work than trying
    to support old systems.
    
    URL: http://sourceware.org/bugzilla/show_bug.cgi?id=13161
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- sim/common/dv-cfi.c
+++ sim/common/dv-cfi.c
@@ -727,7 +727,7 @@ attach_cfi_regs (struct hw *me, struct cfi *cfi)
   /* Figure out where our initial flash data is coming from.  */
   if (fd != -1 && fd_writable)
     {
-#ifdef HAVE_MMAP
+#if defined (HAVE_MMAP) && defined (HAVE_POSIX_FALLOCATE)
       posix_fallocate (fd, 0, cfi->dev_size);
 
       cfi->mmap = mmap (NULL, cfi->dev_size,
