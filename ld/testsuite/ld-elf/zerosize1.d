3035ca03e8ae3e927fdcda22427cec18dba208e2
Richard Henderson <rth@redhat.com>
2012-02-14 20:43:26 +0000

    PR ld/13621         * ld-elf/zerosize1.d, ld-elf/zerosize1.s: New test.

---
+++ ld/testsuite/ld-elf/zerosize1.d
@@ -0,0 +1,10 @@
+#source: start.s
+#source: zerosize1.s
+#ld:
+#readelf: -s
+
+# Check that xyzzy is not placed in the .text section.
+
+#...
+ +[0-9]+: +[0-9a-f]+ +0 +NOTYPE +GLOBAL +DEFAULT +ABS xyzzy
+#pass
