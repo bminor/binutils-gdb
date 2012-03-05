aa443d3b23b7561c24fb21c0c6765d585c115a5d
Hans-Peter Nilsson <hp@axis.com>
2012-03-05 17:21:42 +0000

    PR ld/13803 	* ld-arm/fix-arm1176-off.d, ld-arm/fix-arm1176-on.d: Regexpify 	address of func_to_branch_to.

--- ld/testsuite/ld-arm/fix-arm1176-off.d
+++ ld/testsuite/ld-arm/fix-arm1176-off.d
@@ -9,7 +9,7 @@ Disassembly of section .foo:
 
 [0-9a-f]+ <__func_to_branch_to_veneer>:
  [0-9a-f]+:	e51ff004 	ldr	pc, \[pc, #-4\]	; 200101c <__func_to_branch_to_veneer\+0x4>
- [0-9a-f]+:	00008000 	.word	0x00008000
+ [0-9a-f]+:	........ 	.word	0x........
 
 Disassembly of section .text:
 
