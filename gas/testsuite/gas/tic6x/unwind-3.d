50c374809bf4b2d75eca57edc34bf8990d6b9a99
Alan Modra <amodra@gmail.com>
2012-05-19 10:37:46 +0000

    * gas/tic6x/unwind-1.d: Update for readelf changes. 	* gas/tic6x/unwind-2.d: Likewise. 	* gas/tic6x/unwind-3.d: Likewise.

--- gas/testsuite/gas/tic6x/unwind-3.d
+++ gas/testsuite/gas/tic6x/unwind-3.d
@@ -5,7 +5,7 @@
 Unwind table index '.c6xabi.exidx.text.bar' .*
 
 0x0: 0x830e2807
-  Compact model 3
+  Compact model index: 3
   Stack increment 56
   Registers restored: B11, B13
   Return register: B3
@@ -13,6 +13,6 @@ Unwind table index '.c6xabi.exidx.text.bar' .*
 Unwind table index '.c6xabi.exidx' .*
 
 0x0: 0x80008021
-  Compact model 0
+  Compact model index: 0
   0x00      sp = sp \+ 8
   0x80 0x21 pop {A10, B3}
