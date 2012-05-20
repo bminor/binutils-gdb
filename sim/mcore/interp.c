3550b23619ef1078f00b3583ae04656410f5667f
Hans-Peter Nilsson <hp@axis.com>
2012-05-20 05:34:23 +0000

    PR 14072 	* interp.c: Include config.h before system header files.

--- sim/mcore/interp.c
+++ sim/mcore/interp.c
@@ -18,6 +18,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#include "config.h"
 #include <signal.h>
 #include "sysdep.h"
 #include <sys/times.h>
