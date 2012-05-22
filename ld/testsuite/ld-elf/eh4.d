dfc8a1a28522a59bb4d3fc39a843b32469c6f7d5
Roland McGrath <roland@gnu.org>
2012-05-22 18:42:37 +0000

    ld/testsuite/ 	* ld-elf/eh4.d: Revert last change. 	Loosen CFI-matching regexps so they match x86_64-*-nacl* variant too.

--- ld/testsuite/ld-elf/eh4.d
+++ ld/testsuite/ld-elf/eh4.d
@@ -3,7 +3,7 @@
 #as: --64
 #ld: -melf_x86_64 -shared -Ttext 0x400
 #readelf: -wf
-#notarget: x86_64-*-linux-gnux32 x86_64-*-nacl*
+#notarget: x86_64-*-linux-gnux32
 #target: x86_64-*-*
 
 Contents of the .eh_frame section:
@@ -29,17 +29,14 @@ Contents of the .eh_frame section:
   DW_CFA_set_loc: 00000417
   DW_CFA_def_cfa_offset: 80
 
-00000048 00000024 0000004c FDE cie=00000000 pc=00000240..00000260
+00000048 00000024 0000004c FDE cie=00000000 pc=[0-9a-f]+\.\.[0-9a-f]+
   DW_CFA_def_cfa_offset: 16
-  DW_CFA_advance_loc: 6 to 00000246
+  DW_CFA_advance_loc: [0-9a-f]+ to [0-9a-f]+
   DW_CFA_def_cfa_offset: 24
-  DW_CFA_advance_loc: 10 to 00000250
-  DW_CFA_def_cfa_expression \(DW_OP_breg7 \(rsp\): 8; DW_OP_breg16 \(rip\): 0; DW_OP_lit15; DW_OP_and; DW_OP_lit11; DW_OP_ge; DW_OP_lit3; DW_OP_shl; DW_OP_plus\)
+  DW_CFA_advance_loc: [0-9a-f]+ to [0-9a-f]+
+  DW_CFA_def_cfa_expression \(DW_OP_breg7 \(rsp\): 8; DW_OP_breg16 \(rip\): 0;.*
   DW_CFA_nop
-  DW_CFA_nop
-  DW_CFA_nop
-  DW_CFA_nop
+#...
 
-00000070 ZERO terminator
+[0-9a-f]+ ZERO terminator
 #pass
-
