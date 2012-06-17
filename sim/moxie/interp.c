a6c2b87eec205ad6c5958eeb1711a7eb1638a51f
Mike Frysinger <vapier@gentoo.org>
2012-06-17 23:33:42 +0000

    sim: moxie: fix build by including config.h first

--- sim/moxie/interp.c
+++ sim/moxie/interp.c
@@ -17,6 +17,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#include "config.h"
+#include <fcntl.h>
 #include <signal.h>
 #include <stdlib.h>
 #include "sysdep.h"
