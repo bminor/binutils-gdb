266879991149e529ed25bcbcd610582a2fbbdc6b
Mike Frysinger <vapier@gentoo.org>
2012-06-17 23:34:17 +0000

    sim: cr16: include config.h rather than sysdep.h from bfd

--- sim/cr16/interp.c
+++ sim/cr16/interp.c
@@ -17,8 +17,10 @@
    You should have received a copy of the GNU General Public License 
    along with this program. If not, see <http://www.gnu.org/licenses/>.  */
 
+#include "config.h"
 #include <signal.h>
-#include "sysdep.h"
+#include <stdlib.h>
+#include <string.h>
 #include "bfd.h"
 #include "gdb/callback.h"
 #include "gdb/remote-sim.h"
