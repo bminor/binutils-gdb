406b1477f51e5a4f953f2792fb14e5c3cf1dc1df
Keith Seitz <keiths@redhat.com>
2012-08-22 20:04:05 +0000

    * defs.h: Include build-gnulib/config.h 	* server.h: Include build-gnulib-gbserver/config.h. 	* gdbreplay.c: Likewise.

--- gdb/gdbserver/gdbreplay.c
+++ gdb/gdbserver/gdbreplay.c
@@ -19,6 +19,8 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "config.h"
+#include "build-gnulib-gdbserver/config.h"
+
 #include <stdio.h>
 #if HAVE_SYS_FILE_H
 #include <sys/file.h>
--- gdb/gdbserver/server.h
+++ gdb/gdbserver/server.h
@@ -21,6 +21,7 @@
 #define SERVER_H
 
 #include "config.h"
+#include "build-gnulib-gdbserver/config.h"
 
 #ifdef __MINGW32CE__
 #include "wincecompat.h"
