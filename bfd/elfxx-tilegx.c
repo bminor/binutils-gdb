5ec61378372b535bfadce78ffa5832623b2ac99c
Walter Lee <walt@tilera.com>
2012-09-17 18:01:18 +0000

    Remove unused got_base variable. 	* elf32-tilepro.c (tilepro_elf_relocate_section): Remove unused 	got_base variable. 	* elfxx-tilegx.c (tilegx_elf_relocate_section): Ditto.

--- bfd/elfxx-tilegx.c
+++ bfd/elfxx-tilegx.c
@@ -3094,7 +3094,6 @@ tilegx_elf_relocate_section (bfd *output_bfd, struct bfd_link_info *info,
   Elf_Internal_Shdr *symtab_hdr;
   struct elf_link_hash_entry **sym_hashes;
   bfd_vma *local_got_offsets;
-  bfd_vma got_base;
   asection *sreloc;
   Elf_Internal_Rela *rel;
   Elf_Internal_Rela *relend;
@@ -3105,11 +3104,6 @@ tilegx_elf_relocate_section (bfd *output_bfd, struct bfd_link_info *info,
   symtab_hdr = &elf_symtab_hdr (input_bfd);
   sym_hashes = elf_sym_hashes (input_bfd);
   local_got_offsets = elf_local_got_offsets (input_bfd);
-
-  if (elf_hash_table (info)->hgot == NULL)
-    got_base = 0;
-  else
-    got_base = elf_hash_table (info)->hgot->root.u.def.value;
 
   sreloc = elf_section_data (input_section)->sreloc;
 
