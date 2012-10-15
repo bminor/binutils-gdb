801e41852339ac13d7d302c5e58de5d7f55d640b
Tom Tromey <tromey@redhat.com>
2012-10-15 15:23:47 +0000

    PR python/14635: 	* python/py-symtab.c (del_objfile_sal): Set 'symtab' field 	to Py_None. testsuite 	* gdb.python/py-symtab.exp: Test sal and symtab destructors.

--- gdb/python/py-symtab.c
+++ gdb/python/py-symtab.c
@@ -470,7 +470,10 @@ del_objfile_sal (struct objfile *objfile, void *datum)
     {
       sal_object *next = obj->next;
 
-      obj->symtab = NULL;
+      Py_DECREF (obj->symtab);
+      obj->symtab = (symtab_object *) Py_None;
+      Py_INCREF (Py_None);
+
       obj->next = NULL;
       obj->prev = NULL;
       xfree (obj->sal);
