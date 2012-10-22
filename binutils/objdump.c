849fc04e86b5566ebf6bf3b2bfd8b54bd8badd7f
Tristan Gingold <gingold@adacore.com>
2012-10-22 12:01:39 +0000

    binutils/ 2012-10-21  H.J. Lu  <hongjiu.lu@intel.com>
    	* objdump.c (dump_dwarf): Handle bfd_mach_x64_32 and
    	bfd_mach_x64_32_intel_syntax.

--- binutils/objdump.c
+++ binutils/objdump.c
@@ -2398,6 +2398,8 @@ dump_dwarf (bfd *abfd)
 	{
 	case bfd_mach_x86_64:
 	case bfd_mach_x86_64_intel_syntax:
+	case bfd_mach_x64_32:
+	case bfd_mach_x64_32_intel_syntax:
 	  init_dwarf_regnames_x86_64 ();
 	  break;
 
