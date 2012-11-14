e5d9816410384b2588c9b1972de031228a285444
Yao Qi <yao@codesourcery.com>
2012-11-14 05:52:32 +0000

    gdb/testsuite/ 2012-11-14  Yao Qi  <yao@codesourcery.com>
    	* gdb.threads/manythreads.c [DEBUG]: Include "stdio.h".
    	(thread_function) [DEBUG]: Call 'printf'.
    	* gdb.threads/manythreads.exp: Pass '-DDEBUG' to compilation
    	command line if 'DEBUG=1' is passed to test.
    	Remove a pattern to match inferior's output.

--- gdb/testsuite/gdb.threads/manythreads.c
+++ gdb/testsuite/gdb.threads/manythreads.c
@@ -20,7 +20,9 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include <pthread.h>
+#ifdef DEBUG
 #include <stdio.h>
+#endif
 #include <limits.h>
 
 void *
@@ -28,7 +30,9 @@ thread_function (void *arg)
 {
   int x = * (int *) arg;
 
+#ifdef DEBUG
   printf ("Thread <%d> executing\n", x);
+#endif /* DEBUG */
 
   return NULL;
 }
