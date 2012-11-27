af2e2429d41337bcdc734bf2bbd96f493425a62a
Roland McGrath <roland@gnu.org>
2012-11-27 17:23:31 +0000

    ld/testsuite/ 	* ld-elf/flags1.d: Add *-*-nacl* to xfail list. 	* ld-elf/orphan-region.d: Likewise.

--- ld/testsuite/ld-elf/flags1.d
+++ ld/testsuite/ld-elf/flags1.d
@@ -1,11 +1,12 @@
 #name: --set-section-flags test 1 (sections)
 #ld: -Tflags1.ld
-#objcopy_linked_file: --set-section-flags .post_text_reserve=contents,alloc,load,readonly,code 
+#objcopy_linked_file: --set-section-flags .post_text_reserve=contents,alloc,load,readonly,code
 #readelf: -l --wide
 #xfail: "avr-*-*" "dlx-*-*" "h8300-*-*" "i960-*-*" "ip2k-*-*" "m32r-*-*"
-#xfail: "moxie-*-*" "mt-*-*" "msp430-*-*"
+#xfail: "moxie-*-*" "mt-*-*" "msp430-*-*" "*-*-nacl*"
 #xfail: "*-*-hpux*" "hppa*64*-*-*"
-# Fails on the AVR, DLX, H8300, I960, IP2K, M32R, MOXIE, MT, and MSP430
+# Fails on the AVR, DLX, H8300, I960, IP2K, M32R, MOXIE, MT, and MSP430,
+#  and all NaCl targets,
 #  because the two sections are not merged into one segment.
 #  (There is no good reason why they have to be).
 # Fails on HPUX systems because the .type pseudo-op behaves differently.
