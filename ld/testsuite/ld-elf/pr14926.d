861b2eaac29166353c1977151de5ace0c7e7ab47
H.J. Lu <hjl.tools@gmail.com>
2012-12-07 22:30:19 +0000

    Use "readelf -S --wide" on ld-elf/pr14926.d
    	* ld-elf/pr14926.d: Use "readelf -S --wide".

--- ld/testsuite/ld-elf/pr14926.d
+++ ld/testsuite/ld-elf/pr14926.d
@@ -1,13 +1,6 @@
 #ld: -Ttext=0x60
-#readelf: -S -l --wide
+#readelf: -S --wide
 
 #...
   \[[ 0-9]+\] \.text[ \t]+PROGBITS[ \t]+0*60[ \t]+.*
-#...
-Program Headers:
-  Type.*
-  LOAD[ \t]+0x[0-9a-f]+ 0x[0-9a-f]+ 0x[0-9a-f]+ 0x[0-9a-f]+ 0x[0-9a-f]+ R E 0x[0-9a-f]+
-
- Section to Segment mapping:
-  Segment Sections...
-   00     .text *
+#pass
