51b318dec8f66b2544b24c359ff3d69425d0d6ff
Joel Brobecker <brobecker@gnat.com>
2012-12-19 07:18:22 +0000

    [sim] Update old contact info in GPL license notices
    sim/ChangeLog:
    
            Update old contact info in GPL license notices.

--- sim/arm/armos.c
+++ sim/arm/armos.c
@@ -12,8 +12,7 @@
     GNU General Public License for more details.
  
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA. */
+    along with this program; if not, see <http://www.gnu.org/licenses/>. */
 
 /* This file contains a model of Demon, ARM Ltd's Debug Monitor,
    including all the SWI's required to support the C library. The code in
--- sim/arm/armvirt.c
+++ sim/arm/armvirt.c
@@ -12,8 +12,7 @@
     GNU General Public License for more details.
  
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA. */
+    along with this program; if not, see <http://www.gnu.org/licenses/>. */
 
 /* This file contains a complete ARMulator memory model, modelling a
 "virtual memory" system. A much simpler model can be found in armfast.c,
