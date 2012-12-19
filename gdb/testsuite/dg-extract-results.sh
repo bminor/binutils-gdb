7785b8804f92599e62d52fb8cf61c7b5553ae2a3
Joel Brobecker <brobecker@gnat.com>
2012-12-19 08:19:59 +0000

    update copyright notices in various GDB files.
    Update some files in GDB that were accidently left with a GPL v2
    copyright header.
    
    Update some files where the copyright notice still provides the
    old contact info, using the approach for providing the FSF's contact
    info.
    
    gdb/ChangeLog:
    
            * acinclude.m4: Update contact info in copyright notice.
    
    gdb/doc/ChangeLog:
    
            * refcard.tex: Update copyright notice to GPL v3 or later.
            Update contact info.
    
    gdb/testsuite/ChangeLog:
    
            * dg-extract-results.sh: Update contact info in copyright notice.
            * gdb.arch/mips-octeon-bbit.exp: Update copyright notice to
            GPL v3 or later.  Update contact info.
            * gdb.fortran/logical.f90, gdb.threads/watchpoint-fork-child.c,
            gdb.threads/watchpoint-fork-mt.c,
            gdb.threads/watchpoint-fork-parent.c,
            gdb.threads/watchpoint-fork-st.c,
            gdb.threads/watchpoint-fork.h: Likewise.

--- gdb/testsuite/dg-extract-results.sh
+++ gdb/testsuite/dg-extract-results.sh
@@ -22,9 +22,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with GCC; see the file COPYING.  If not, write to
-# the Free Software Foundation, 51 Franklin Street, Fifth Floor,
-# Boston, MA 02110-1301, USA.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 PROGNAME=dg-extract-results.sh
 
