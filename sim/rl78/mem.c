69276d02644f403e5344c04fc72f867aee3b291a
Nick Clifton <nickc@redhat.com>
2013-01-08 15:05:00 +0000

    * mem.c (MDBL): Correct value. 	(MDBH): Correct value.

--- sim/rl78/mem.c
+++ sim/rl78/mem.c
@@ -78,8 +78,8 @@ mem_rom_size (int rom_bytes)
 #define MDUC	0xf00e8
 #define MDAL	0xffff0
 #define MDAH	0xffff2
-#define MDBL	0xffff4
-#define MDBH	0xffff6
+#define MDBL	0xffff6
+#define MDBH	0xffff4
 #define MDCL	0xf00e0
 #define MDCH	0xf00e2
 static long long mduc_clock = 0;
