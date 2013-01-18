46f352282bbb75ac5aa5d0b1d15eaf206c5bcf51
Tom Tromey <tromey@redhat.com>
2013-01-18 18:32:35 +0000

    PR c++/14999: 	* dwarf2loc.c (dwarf2_compile_expr_to_ax) <DW_OP_fbreg>: 	Call require_rvalue. gdb/testsuite 	* gdb.dwarf2/trace-crash.s: New file. 	* gdb.dwarf2/trace-crash.exp: New file.

--- gdb/dwarf2loc.c
+++ gdb/dwarf2loc.c
@@ -2878,6 +2878,7 @@ dwarf2_compile_expr_to_ax (struct agent_expr *expr, struct axs_value *loc,
 	    op_ptr = safe_read_sleb128 (op_ptr, op_end, &offset);
 	    dwarf2_compile_expr_to_ax (expr, loc, arch, addr_size, datastart,
 				       datastart + datalen, per_cu);
+	    require_rvalue (expr, loc);
 
 	    if (offset != 0)
 	      {
