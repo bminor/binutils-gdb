20e52bd2b8041df10920960f5882636d420ebb13
H.J. Lu <hjl.tools@gmail.com>
2013-02-07 02:15:30 +0000

    Don't resolve size relocation against non-zero TLS symbol
    bfd/
    
    	* elf32-i386.c (elf_i386_allocate_dynrelocs): Don't clear pc_count
    	for non-zero TLS symbol.
    	(elf_i386_relocate_section): Don't resolve size relocation against
    	non-zero TLS symbol.
    	* elf64-x86-64.c (elf_x86_64_allocate_dynrelocs): Don't clear
    	pc_count for non-zero TLS symbol.
    	(elf_x86_64_relocate_section): Don't resolve size relocation
    	against non-zero TLS symbol.
    
    ld/testsuite/
    
    	* ld-size/size-10.rd: Updated.
    	* ld-size/size-8.rd: Likewise.
    	* ld-size/size32-2-i386.d: Likewise.
    	* ld-size/size32-2-x32.d: Likewise.
    	* ld-size/size32-2-x86-64.d: Likewise.
    	* ld-size/size64-2-x32.d: Likewise.
    	* ld-size/size64-2-x86-64.d: Likewise.
    
    	* ld-size/size.exp (run_time_tests): Pass --hash-styl=gnu to
    	linker for size-8 test.

--- ld/testsuite/ld-size/size32-2-i386.d
+++ ld/testsuite/ld-size/size32-2-i386.d
@@ -8,11 +8,13 @@
 
 DYNAMIC RELOCATION RECORDS
 OFFSET   TYPE              VALUE 
-0+1234 R_386_SIZE32      zzz
-0+1238 R_386_SIZE32      zzz
-0+123c R_386_SIZE32      zzz
+0+123c R_386_SIZE32      xxx
+0+1240 R_386_SIZE32      yyy
+0+1244 R_386_SIZE32      zzz
+0+1248 R_386_SIZE32      zzz
+0+124c R_386_SIZE32      zzz
 
 
 Contents of section .data:
- 122c 28000000 28000000 00000000 e2ffffff  ................
- 123c 1e000000                             ....            
+ 123c 00000000 00000000 00000000 e2ffffff  ................
+ 124c 1e000000                             ....            
