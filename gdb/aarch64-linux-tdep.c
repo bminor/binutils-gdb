b5dbc8d42991fb1ac3cf5d9b7817102712701d30
Yufeng Zhang <yufeng.zhang@arm.com>
2013-02-08 12:46:28 +0000

    gdb/
    2013-02-08  Yufeng Zhang  <yufeng.zhang@arm.com>
    
    	* aarch64-linux-tdep.c (AARCH64_LINUX_SIZEOF_GREGSET): Change the
    	number of the registers from 36 to 34.

--- gdb/aarch64-linux-tdep.c
+++ gdb/aarch64-linux-tdep.c
@@ -36,9 +36,9 @@
 #include "regset.h"
 
 /* The general-purpose regset consists of 31 X registers, plus SP, PC,
-   PSTATE and two extra pseudo 64-bit registers, as defined in the
-   AArch64 port of the Linux kernel.  */
-#define AARCH64_LINUX_SIZEOF_GREGSET  (36 * X_REGISTER_SIZE)
+   and PSTATE registers, as defined in the AArch64 port of the Linux
+   kernel.  */
+#define AARCH64_LINUX_SIZEOF_GREGSET  (34 * X_REGISTER_SIZE)
 
 /* The fp regset consists of 32 V registers, plus FPCR and FPSR which
    are 4 bytes wide each, and the whole structure is padded to 128 bit
