fafd911d9f34471ac85b29643bd47448b9ea238f
Nick Clifton <nickc@redhat.com>
2013-03-04 14:22:25 +0000

    * elfcomm.c (error): Flush stdout before emitting the error 	message. 	(warn): Likewise.

--- binutils/elfcomm.c
+++ binutils/elfcomm.c
@@ -35,6 +35,9 @@ error (const char *message, ...)
 {
   va_list args;
 
+  /* Try to keep error messages in sync with the program's normal output.  */
+  fflush (stdout);
+
   va_start (args, message);
   fprintf (stderr, _("%s: Error: "), program_name);
   vfprintf (stderr, message, args);
@@ -46,6 +49,9 @@ warn (const char *message, ...)
 {
   va_list args;
 
+  /* Try to keep warning messages in sync with the program's normal output.  */
+  fflush (stdout);
+  
   va_start (args, message);
   fprintf (stderr, _("%s: Warning: "), program_name);
   vfprintf (stderr, message, args);
