0d0fb1ba5f0512ca8b670d3bdcf2083db355d3d7
Nick Clifton <nickc@redhat.com>
2013-03-15 16:25:00 +0000

    * addr2line.c (slurp_symtab): If canonicalization reveals that 	there were no ordinary symbols, try loading the dynamic symbols 	instead.

--- binutils/addr2line.c
+++ binutils/addr2line.c
@@ -130,6 +130,17 @@ slurp_symtab (bfd *abfd)
     symcount = bfd_canonicalize_symtab (abfd, syms);
   if (symcount < 0)
     bfd_fatal (bfd_get_filename (abfd));
+
+  /* If there are no symbols left after canonicalization and
+     we have not tried the dynamic symbols then give them a go.  */
+  if (symcount == 0
+      && ! dynamic
+      && (storage = bfd_get_dynamic_symtab_upper_bound (abfd)) > 0)
+    {
+      free (syms);
+      syms = xmalloc (storage);
+      symcount = bfd_canonicalize_dynamic_symtab (abfd, syms);
+    }
 }
 
 /* These global variables are used to pass information between
