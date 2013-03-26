1b493192488ccb9886ffe709864e5f5c0588b004
Pedro Alves <palves@redhat.com>
2013-03-26 20:19:31 +0000

    Make "set/show remoteaddresssize" a zuinteger command instead of uinteger.
    It makes no sense to talk about an "unlimited" address size in this
    context.
    
     (gdb) show remoteaddresssize
     The maximum size of the address (in bits) in a memory packet is 0.
     (gdb) set remoteaddresssize 0
     (gdb) show remoteaddresssize
     The maximum size of the address (in bits) in a memory packet is unlimited.
    
    "set remoteaddresssize 0" mapping to UINT_MAX means you can't
    force gdb through this path twice in the same GDB run:
    
      static CORE_ADDR
      remote_address_masked (CORE_ADDR addr)
      {
        unsigned int address_size = remote_address_size;
    
        /* If "remoteaddresssize" was not set, default to target address size.  */
        if (!address_size)
          address_size = gdbarch_addr_bit (target_gdbarch ());
    
    gdb/
    2013-03-26  Pedro Alves  <palves@redhat.com>
    
    	* remote.c (_initialize_remote): Make "set remoteaddresssize"
    	a zuinteger command instead of uinteger.

--- gdb/remote.c
+++ gdb/remote.c
@@ -11773,13 +11773,13 @@ Specify a negative limit for unlimited."),
 					   breakpoints is %s.  */
 			    &remote_set_cmdlist, &remote_show_cmdlist);
 
-  add_setshow_uinteger_cmd ("remoteaddresssize", class_obscure,
-			    &remote_address_size, _("\
+  add_setshow_zuinteger_cmd ("remoteaddresssize", class_obscure,
+			     &remote_address_size, _("\
 Set the maximum size of the address (in bits) in a memory packet."), _("\
 Show the maximum size of the address (in bits) in a memory packet."), NULL,
-			    NULL,
-			    NULL, /* FIXME: i18n: */
-			    &setlist, &showlist);
+			     NULL,
+			     NULL, /* FIXME: i18n: */
+			     &setlist, &showlist);
 
   add_packet_config_cmd (&remote_protocol_packets[PACKET_X],
 			 "X", "binary-download", 1);
