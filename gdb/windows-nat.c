c62fa0e2670b0de9b24c292e0f6dfe539d8ae555
Joel Brobecker <brobecker@gnat.com>
2013-03-26 23:40:08 +0000

    windows-nat.c: Add empty line after local block variable definitions.
    gdb/ChangeLog:
    
            * windows-nat.c (handle_output_debug_string): Add empty line
            after local block variable definition.

--- gdb/windows-nat.c
+++ gdb/windows-nat.c
@@ -981,11 +981,13 @@ handle_output_debug_string (struct target_waitstatus *ourstatus)
       char *p;
       int sig = strtol (s + sizeof (_CYGWIN_SIGNAL_STRING) - 1, &p, 0);
       int gotasig = gdb_signal_from_host (sig);
+
       ourstatus->value.sig = gotasig;
       if (gotasig)
 	{
 	  LPCVOID x;
 	  SIZE_T n;
+
 	  ourstatus->kind = TARGET_WAITKIND_STOPPED;
 	  retval = strtoul (p, &p, 0);
 	  if (!retval)
