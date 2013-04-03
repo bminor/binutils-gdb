203f0950a35c87f17016045f1cd5a0460b445532
Alan Modra <amodra@gmail.com>
2013-04-03 11:44:41 +0000

    PR ld/15227 	* ld-plugin/lto.exp (PR ld/12942 (3)): Remove file name and 	line number from regexp. 	(PR ld/15146 (2)): Similarly. 	* ld-plugin/pr12942a.cc (main): Use __builtin_abort.

--- ld/testsuite/ld-plugin/pr12942a.cc
+++ ld/testsuite/ld-plugin/pr12942a.cc
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include "pr12942a.h"
 
-extern "C" void abort ();
-
 test_t b(void);
 
 int
 main(void)
 {
   if (test != b ())
-    abort ();
+    __builtin_abort ();
 
   printf ("OK\n");
   return 0;
