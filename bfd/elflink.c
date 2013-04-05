c69ba956e3ea3f41672cd3bf4d9d27f91d6849c0
Nick Clifton <nickc@redhat.com>
2013-04-05 09:08:28 +0000

    * elflink.c (elf_link_add_object_symbols): Revert accidental commit.

--- bfd/elflink.c
+++ bfd/elflink.c
@@ -4437,7 +4437,6 @@ error_free_dyn:
 		 --no-add-needed is used and the reference was not
 		 a weak one.  */
 	      if (old_bfd != NULL
-		  && h->ref_regular_nonweak
 		  && (elf_dyn_lib_class (abfd) & DYN_NO_NEEDED) != 0)
 		{
 		  (*_bfd_error_handler)
