79bb5c2356e7651d0aad0a0c3818f1727bc3cb74
Nick Clifton <nickc@redhat.com>
2013-04-09 15:18:40 +0000

    * bfd.c (typedef bfd, Error reporting, Miscellaneous): Add INODEs.
            * bfd.texinfo (typedef bfd, Error reporting, Miscellaneous):
            Add menu items.

--- bfd/bfd.c
+++ bfd/bfd.c
@@ -1,7 +1,5 @@
 /* Generic BFD library interface and support routines.
-   Copyright 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
-   2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011
-   Free Software Foundation, Inc.
+   Copyright 1990-2013 Free Software Foundation, Inc.
    Written by Cygnus Support.
 
    This file is part of BFD, the Binary File Descriptor library.
@@ -22,6 +20,9 @@
    MA 02110-1301, USA.  */
 
 /*
+INODE
+typedef bfd, Error reporting, BFD front end, BFD front end
+
 SECTION
 	<<typedef bfd>>
 
@@ -340,6 +341,9 @@ CODE_FRAGMENT
    where it is needed.  The typedef's used are defined in bfd.h */
 
 /*
+INODE
+Error reporting, Miscellaneous, typedef bfd, BFD front end
+
 SECTION
 	Error reporting
 
@@ -883,6 +887,9 @@ bfd_get_assert_handler (void)
 }
 
 /*
+INODE
+Miscellaneous, Memory Usage, Error reporting, BFD front end
+
 SECTION
 	Miscellaneous
 
