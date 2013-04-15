591f19e89b4b671f1124cdc21605bba1d3d6b8d0
Tom Tromey <tromey@redhat.com>
2013-04-15 17:27:44 +0000

    PR c++/12824: 	* NEWS: Update. 	* breakpoint.c (enum exception_event_kind) <EX_EVENT_RETHROW>: 	New constant. 	(classify_exception_breakpoint): New function. 	(print_it_exception_catchpoint, print_one_exception_catchpoint) 	(print_mention_exception_catchpoint) 	(print_recreate_exception_catchpoint, handle_gnu_v3_exceptions) 	(catch_exception_command_1): Handle "rethrow" catchpoint. 	(catch_rethrow_command): New function. 	(_initialize_breakpoint): Add "catch rethrow" command. gdb/doc 	* gdb.texinfo (Set Catchpoints): Reorganize exception 	catchpoints.  Document "catch rethrow". 	(Debugging C Plus Plus): Mention "catch rethrow". gdb/testsuite 	* gdb.cp/exception.exp: Add "catch rethrow" tests.

--- gdb/breakpoint.c
+++ gdb/breakpoint.c
@@ -86,6 +86,7 @@
 enum exception_event_kind
 {
   EX_EVENT_THROW,
+  EX_EVENT_RETHROW,
   EX_EVENT_CATCH
 };
 
@@ -11570,16 +11571,30 @@ catch_exec_command_1 (char *arg, int from_tty,
   install_breakpoint (0, &c->base, 1);
 }
 
+/* A helper function that returns a value indicating the kind of the
+   exception catchpoint B.  */
+
+static enum exception_event_kind
+classify_exception_breakpoint (struct breakpoint *b)
+{
+  if (strstr (b->addr_string, "catch") != NULL)
+    return EX_EVENT_CATCH;
+  else if (strstr (b->addr_string, "rethrow") != NULL)
+    return EX_EVENT_RETHROW;
+  else
+    return EX_EVENT_THROW;
+}
+
 static enum print_stop_action
 print_it_exception_catchpoint (bpstat bs)
 {
   struct ui_out *uiout = current_uiout;
   struct breakpoint *b = bs->breakpoint_at;
-  int bp_temp, bp_throw;
+  int bp_temp;
+  enum exception_event_kind kind = classify_exception_breakpoint (b);
 
   annotate_catchpoint (b->number);
 
-  bp_throw = strstr (b->addr_string, "throw") != NULL;
   if (b->loc->address != b->loc->requested_address)
     breakpoint_adjustment_warning (b->loc->requested_address,
 	                           b->loc->address,
@@ -11591,8 +11606,9 @@ print_it_exception_catchpoint (bpstat bs)
   if (!ui_out_is_mi_like_p (uiout))
     ui_out_field_int (uiout, "bkptno", b->number);
   ui_out_text (uiout,
-	       bp_throw ? " (exception thrown), "
-		        : " (exception caught), ");
+	       (kind == EX_EVENT_THROW ? " (exception thrown), "
+		: (kind == EX_EVENT_CATCH ? " (exception caught), "
+		   : " (exception rethrown), ")));
   if (ui_out_is_mi_like_p (uiout))
     {
       ui_out_field_string (uiout, "reason", 
@@ -11609,6 +11625,7 @@ print_one_exception_catchpoint (struct breakpoint *b,
 {
   struct value_print_options opts;
   struct ui_out *uiout = current_uiout;
+  enum exception_event_kind kind = classify_exception_breakpoint (b);
 
   get_user_print_options (&opts);
   if (opts.addressprint)
@@ -11623,17 +11640,26 @@ print_one_exception_catchpoint (struct breakpoint *b,
   annotate_field (5);
   if (b->loc)
     *last_loc = b->loc;
-  if (strstr (b->addr_string, "throw") != NULL)
+
+  switch (kind)
     {
+    case EX_EVENT_THROW:
       ui_out_field_string (uiout, "what", "exception throw");
       if (ui_out_is_mi_like_p (uiout))
 	ui_out_field_string (uiout, "catch-type", "throw");
-    }
-  else
-    {
+      break;
+
+    case EX_EVENT_RETHROW:
+      ui_out_field_string (uiout, "what", "exception rethrow");
+      if (ui_out_is_mi_like_p (uiout))
+	ui_out_field_string (uiout, "catch-type", "rethrow");
+      break;
+
+    case EX_EVENT_CATCH:
       ui_out_field_string (uiout, "what", "exception catch");
       if (ui_out_is_mi_like_p (uiout))
 	ui_out_field_string (uiout, "catch-type", "catch");
+      break;
     }
 }
 
@@ -11642,15 +11668,15 @@ print_mention_exception_catchpoint (struct breakpoint *b)
 {
   struct ui_out *uiout = current_uiout;
   int bp_temp;
-  int bp_throw;
+  enum exception_event_kind kind = classify_exception_breakpoint (b);
 
   bp_temp = b->disposition == disp_del;
-  bp_throw = strstr (b->addr_string, "throw") != NULL;
   ui_out_text (uiout, bp_temp ? _("Temporary catchpoint ")
 			      : _("Catchpoint "));
   ui_out_field_int (uiout, "bkptno", b->number);
-  ui_out_text (uiout, bp_throw ? _(" (throw)")
-			       : _(" (catch)"));
+  ui_out_text (uiout, (kind == EX_EVENT_THROW ? _(" (throw)")
+		       : (kind == EX_EVENT_CATCH ? _(" (catch)")
+			  : _(" (rethrow)"))));
 }
 
 /* Implement the "print_recreate" breakpoint_ops method for throw and
@@ -11661,12 +11687,22 @@ print_recreate_exception_catchpoint (struct breakpoint *b,
 				     struct ui_file *fp)
 {
   int bp_temp;
-  int bp_throw;
+  enum exception_event_kind kind = classify_exception_breakpoint (b);
 
   bp_temp = b->disposition == disp_del;
-  bp_throw = strstr (b->addr_string, "throw") != NULL;
   fprintf_unfiltered (fp, bp_temp ? "tcatch " : "catch ");
-  fprintf_unfiltered (fp, bp_throw ? "throw" : "catch");
+  switch (kind)
+    {
+    case EX_EVENT_THROW:
+      fprintf_unfiltered (fp, "throw");
+      break;
+    case EX_EVENT_CATCH:
+      fprintf_unfiltered (fp, "catch");
+      break;
+    case EX_EVENT_RETHROW:
+      fprintf_unfiltered (fp, "rethrow");
+      break;
+    }
   print_recreate_thread (b, fp);
 }
 
@@ -11680,8 +11716,13 @@ handle_gnu_v3_exceptions (int tempflag, char *cond_string,
  
   if (ex_event == EX_EVENT_CATCH)
     trigger_func_name = "__cxa_begin_catch";
+  else if (ex_event == EX_EVENT_RETHROW)
+    trigger_func_name = "__cxa_rethrow";
   else
-    trigger_func_name = "__cxa_throw";
+    {
+      gdb_assert (ex_event == EX_EVENT_THROW);
+      trigger_func_name = "__cxa_throw";
+    }
 
   create_breakpoint (get_current_arch (),
 		     trigger_func_name, cond_string, -1, NULL,
@@ -11715,7 +11756,8 @@ catch_exception_command_1 (enum exception_event_kind ex_event, char *arg,
     error (_("Junk at end of arguments."));
 
   if (ex_event != EX_EVENT_THROW
-      && ex_event != EX_EVENT_CATCH)
+      && ex_event != EX_EVENT_CATCH
+      && ex_event != EX_EVENT_RETHROW)
     error (_("Unsupported or unknown exception event; cannot catch it"));
 
   if (handle_gnu_v3_exceptions (tempflag, cond_string, ex_event, from_tty))
@@ -11742,6 +11784,17 @@ catch_throw_command (char *arg, int from_tty, struct cmd_list_element *command)
   int tempflag = get_cmd_context (command) == CATCH_TEMPORARY;
 
   catch_exception_command_1 (EX_EVENT_THROW, arg, tempflag, from_tty);
+}
+
+/* Implementation of "catch rethrow" command.  */
+
+static void
+catch_rethrow_command (char *arg, int from_tty,
+		       struct cmd_list_element *command)
+{
+  int tempflag = get_cmd_context (command) == CATCH_TEMPORARY;
+
+  catch_exception_command_1 (EX_EVENT_RETHROW, arg, tempflag, from_tty);
 }
 
 void
@@ -16352,6 +16405,12 @@ Catch an exception, when caught."),
   add_catch_command ("throw", _("\
 Catch an exception, when thrown."),
 		     catch_throw_command,
+                     NULL,
+		     CATCH_PERMANENT,
+		     CATCH_TEMPORARY);
+  add_catch_command ("rethrow", _("\
+Catch an exception, when rethrown."),
+		     catch_rethrow_command,
                      NULL,
 		     CATCH_PERMANENT,
 		     CATCH_TEMPORARY);
