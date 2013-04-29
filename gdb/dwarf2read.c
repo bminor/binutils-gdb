9056882e81e43101c1ecc9314608d1c6bf2da77b
Joel Brobecker <brobecker@gnat.com>
2013-04-29 05:05:44 +0000

    Add ARI marker to get_DW_AT_signature_type.
    gdb/ChangeLog:
    
            * dwarf2read.c (get_DW_AT_signature_type): Add ARI marker.

--- gdb/dwarf2read.c
+++ gdb/dwarf2read.c
@@ -17924,8 +17924,8 @@ get_signatured_type (struct die_info *die, ULONGEST signature,
    reading in and processing the type unit if necessary.  */
 
 static struct type *
-get_DW_AT_signature_type (struct die_info *die, struct attribute *attr,
-			  struct dwarf2_cu *cu)
+get_DW_AT_signature_type /* ARI: editCase */
+  (struct die_info *die, struct attribute *attr, struct dwarf2_cu *cu)
 {
   /* Yes, DW_AT_signature can use a non-ref_sig8 reference.  */
   if (is_ref_attr (attr))
