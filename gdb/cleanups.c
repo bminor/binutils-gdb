5821aace701864d3aced34c974db27de52b0160c
Jan Kratochvil <jan.kratochvil@redhat.com>
2013-05-14 20:39:12 +0000

    gdb/ 	* cleanups.c (restore_my_cleanups): Replace gdb_assert by 	internal_warning.

--- gdb/cleanups.c
+++ gdb/cleanups.c
@@ -261,7 +261,10 @@ save_final_cleanups (void)
 static void
 restore_my_cleanups (struct cleanup **pmy_chain, struct cleanup *chain)
 {
-  gdb_assert (*pmy_chain == SENTINEL_CLEANUP);
+  if (*pmy_chain != SENTINEL_CLEANUP)
+    internal_warning (__FILE__, __LINE__,
+		      _("restore_my_cleanups has found a stale cleanup"));
+
   *pmy_chain = chain;
 }
 
