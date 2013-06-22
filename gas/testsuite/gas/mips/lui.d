16e5e222b6eae6f110ea72bf627585c095a453a8
Richard Sandiford <rdsandiford@googlemail.com>
2013-06-22 16:57:42 +0000

    gas/testsuite/ 	* gas/mips/mips.exp: Remove "LOSE" comments. 	(run_dump_test_arch): Remove format selector support. 	(run_dump_test_arches): Remove associated upvars. 	(elf, ecoff, aout, no_mips16, no_micromips): Remove variables. 	Remove all conditions based on them, on the assumption that $elf 	is true and the others are false.  Rename "elf-jal" to "jal". 	(tmips): Set to "t" for *bsd targets. 	* gas/mips/elf-jal.d: Rename to... 	* gas/mips/jal.d: ...this, replacing the old file. 	* gas/mips/micromips@elf-jal.d: Rename to... 	* gas/mips/micromips@jal.d: ...this. 	* gas/mips/at-1.d, gas/mips/ld.d, gas/mips/l_d.d, gas/mips/lui.d, 	gas/mips/mips1@l_d.d, gas/mips/mips1@ld-forward.d, gas/mips/mips1@ld.d, 	gas/mips/mips1@s_d.d, gas/mips/s_d.d, gas/mips/sd.d: Remove ECOFF 	relocation names.  Do not allow any offset on the symbol.

--- gas/testsuite/gas/mips/lui.d
+++ gas/testsuite/gas/mips/lui.d
@@ -13,13 +13,13 @@ Disassembly of section \.text:
 [0-9a-f]+ <[^>]*> 3c02000c 	lui	v0,0xc
 [0-9a-f]+ <[^>]*> 3c02000c 	lui	v0,0xc
 [0-9a-f]+ <[^>]*> 3c02000c 	lui	v0,0xc
-[ 	]*[0-9a-f]+: (R_MIPS_LO16|REFLO)	\.text
+[ 	]*[0-9a-f]+: R_MIPS_LO16	\.text
 [0-9a-f]+ <[^>]*> 3c020000 	lui	v0,0x0
-[ 	]*[0-9a-f]+: (R_MIPS_LO16|REFLO)	ext
+[ 	]*[0-9a-f]+: R_MIPS_LO16	ext
 [0-9a-f]+ <[^>]*> 3c020020 	lui	v0,0x20
-[ 	]*[0-9a-f]+: (R_MIPS_LO16|REFLO)	\.text
+[ 	]*[0-9a-f]+: R_MIPS_LO16	\.text
 [0-9a-f]+ <[^>]*> 3c020028 	lui	v0,0x28
-[ 	]*[0-9a-f]+: (R_MIPS_LO16|REFLO)	\.text
+[ 	]*[0-9a-f]+: R_MIPS_LO16	\.text
 [0-9a-f]+ <[^>]*> 3c020000 	lui	v0,0x0
 [0-9a-f]+ <[^>]*> 3c02ffff 	lui	v0,0xffff
 	\.\.\.
