6e2048d3e3a50472e73d74d97ae031741464b756
Tom Tromey <tromey@redhat.com>
2013-06-25 16:04:54 +0000

    * common/create-version.sh (date): Use "$", not "$$" in sed 	expression.

--- gdb/common/create-version.sh
+++ gdb/common/create-version.sh
@@ -28,7 +28,7 @@ target_alias="$3"
 output="$4"
 
 rm -f version.c-tmp $output version.tmp
-date=`sed -n -e 's/^.* BFD_VERSION_DATE \(.*\)$$/\1/p' $srcdir/../bfd/version.h`
+date=`sed -n -e 's/^.* BFD_VERSION_DATE \(.*\)$/\1/p' $srcdir/../bfd/version.h`
 sed -e "s/DATE/$date/" < $srcdir/common/version.in > version.tmp
 echo '#include "version.h"' >> version.c-tmp
 echo 'const char version[] = "'"`sed q version.tmp`"'";' >> version.c-tmp
