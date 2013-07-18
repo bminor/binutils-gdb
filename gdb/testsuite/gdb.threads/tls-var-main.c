623cc2854011a4ffa83755d7ce9a9e078fa7ec80
Will Newton <willnewton@sourceware.org>
2013-07-18 15:49:22 +0000

    gdb/testsuite/gdb.threads: Ensure TLS tests link against pthreads.
    On Ubuntu by default the compiler passes --as-needed to ld which
    means no DT_NEEDED entry is added for libpthread when building
    the TLS tests. This causes the test to fail as libpthread is
    required to look up TLS variables. Add calls to pthread_testcancel
    to make sure libpthread gets linked.
    
    gdb/testsuite/ChangeLog:
    
    2013-07-18  Will Newton  <will.newton@linaro.org>
    
    	* gdb.threads/tls-nodebug.c: Call pthread_testcancel
    	to ensure the test is linked against pthreads.
    	* gdb.threads/tls-var-main.c: Likewise.
    	* gdb.threads/tls-shared.c: Likewise.

--- gdb/testsuite/gdb.threads/tls-var-main.c
+++ gdb/testsuite/gdb.threads/tls-var-main.c
@@ -15,8 +15,12 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#include <pthread.h>
+
 int
 main (void)
 {
+  /* Ensure we link against pthreads even with --as-needed.  */
+  pthread_testcancel();
   return 0;
 }
