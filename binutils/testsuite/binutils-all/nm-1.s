3ce958b2ab9f0d1f6c4f12bdc42ea7e8cb21dd6a
Nick Clifton <nickc@redhat.com>
2013-08-22 11:29:43 +0000

    * binutils-all/nm.exp: --size-sort test: Add more ELF-like 	targets. 	* binutils-all/nm-1.s: Use .byte instead of .long. 	Provide a terminating symbol.

--- binutils/testsuite/binutils-all/nm-1.s
+++ binutils/testsuite/binutils-all/nm-1.s
@@ -1,13 +1,15 @@
 	.globl text_symbol1
 	.globl text_symbol2
 	.globl text_symbol3
+	.globl end_symbol
 	.text
 text_symbol1:
-	.long 0
-	.long 0
-	.long 0
+	.byte 0,0,0,0
+	.byte 0,0,0,0
+	.byte 0,0,0,0
 text_symbol2:
-	.long 0
-	.long 0
+	.byte 0,0,0,0
+	.byte 0,0,0,0
 text_symbol3:
-	.long 0
+	.byte 0,0,0,0
+end_symbol:
