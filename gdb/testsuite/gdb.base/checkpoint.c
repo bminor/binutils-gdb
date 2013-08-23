59b28c5dd267407c63968cb18d9f3c5aa78fa2ba
Tom Tromey <tromey@redhat.com>
2013-08-23 12:18:27 -0600

    update checkpoint test
    This fixes the "checkpoint" test to use the standard output directory.
    This makes the test be parallel-safe.
    
    2013-11-04  Tom Tromey  <tromey@redhat.com>
    
    	* gdb.base/checkpoint.c (main): Use PI_TXT and COPY1_TXT
    	defines.
    	* gdb.base/checkpoint.exp: Define PI_TXT and COPY1_TXT during
    	compilation.  Use prepare_for_testing, standard_output_file.

--- gdb/testsuite/gdb.base/checkpoint.c
+++ gdb/testsuite/gdb.base/checkpoint.c
@@ -28,8 +28,8 @@ main()
   long i;
   int c = 0;
 
-  in  = fopen ("pi.txt", "r");
-  out = fopen ("copy1.txt", "w");
+  in  = fopen (PI_TXT, "r");
+  out = fopen (COPY1_TXT, "w");
 
   if (!in || !out)
     {
@@ -54,6 +54,6 @@ main()
   fclose (in);
   fclose (out);
   printf ("Deleting copy.\n");	/* breakpoint 3 */
-  unlink ("copy1.txt");
+  unlink (COPY1_TXT);
   exit (0);			/* breakpoint 4 */
 }
