43df09d9373b3e235948e67093ad93f417e3b02b
Andrew Burgess <aburgess@broadcom.com>
2013-09-06 10:12:20 +0000

    OBVIOUS: Remove legacy comment referring to deprecated_init_ui_hook.
    gdb/ChangeLog
    
    	* tui/tui-io.c (tui_initialize_io): Remove legacy comment referring
    	to deprecated_init_ui_hook.

--- gdb/tui/tui-io.c
+++ gdb/tui/tui-io.c
@@ -609,8 +609,7 @@ tui_initialize_io (void)
   tui_stderr = tui_fileopen (stderr);
   tui_out = tui_out_new (tui_stdout);
 
-  /* Create the default UI.  It is not created because we installed a
-     deprecated_init_ui_hook.  */
+  /* Create the default UI.  */
   tui_old_uiout = cli_out_new (gdb_stdout);
 
 #ifdef TUI_USE_PIPE_FOR_READLINE
