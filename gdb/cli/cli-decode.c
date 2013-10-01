193a8eac2a1b511186559bb4184be389e5c5b81b
Doug Evans <dje@google.com>
2013-10-01 23:52:45 +0000

    * cli/cli-decode.c: Remove unnecessary inclusion of tui/tui.h.

--- gdb/cli/cli-decode.c
+++ gdb/cli/cli-decode.c
@@ -22,14 +22,8 @@
 #include "gdb_string.h"
 #include "completer.h"
 #include "ui-out.h"
-
 #include "cli/cli-cmds.h"
 #include "cli/cli-decode.h"
-
-#ifdef TUI
-#include "tui/tui.h"		/* For tui_active et al.  */
-#endif
-
 #include "gdb_assert.h"
 
 /* Prototypes for local functions.  */
