7b0648448ff241a59a3c9c337ff163986da05619
Hans-Peter Nilsson <hp@axis.com>
2013-10-15 20:40:22 +0000

    * create-version.sh: Align parameters to match those of 	../../gdb/common/create-version.sh. 	* Make-common.in (srcsim): New variable. 	(version.c): Adjust call to create-version.sh as per above.

--- sim/common/create-version.sh
+++ sim/common/create-version.sh
@@ -17,16 +17,21 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Create version.c from version.in.
+# Create version.c from $1/../gdb/version.in.
+# Please keep the same API as $1/../gdb/common/create-version.sh so
+# they can be called the same way, from e.g. $srcroot/src-release.
 # Usage:
-#    create-version.sh PATH-TO-COMMON-SIM-SRCDIR OUTPUT-FILE-NAME
+#    create-version.sh PATH-TO-SIM-SRCDIR HOST_ALIAS \
+#        TARGET_ALIAS OUTPUT-FILE-NAME
 
 srcdir="$1"
-output="$2"
+host_alias="$2"
+target_alias="$3"
+output="$4"
 
 rm -f version.c-tmp $output version.tmp
-date=`sed -n -e 's/^.* BFD_VERSION_DATE \(.*\)$/\1/p' $srcdir/../../bfd/version.h`
-sed -e "s/DATE/$date/" < $srcdir/../../gdb/version.in > version.tmp
+date=`sed -n -e 's/^.* BFD_VERSION_DATE \(.*\)$/\1/p' $srcdir/../bfd/version.h`
+sed -e "s/DATE/$date/" < $srcdir/../gdb/version.in > version.tmp
 echo '#include "version.h"' >> version.c-tmp
 echo 'const char version[] = "'"`sed q version.tmp`"'";' >> version.c-tmp
 mv version.c-tmp $output
