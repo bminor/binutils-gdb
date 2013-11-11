b5be8ce022f894831b133b3b424238d8058eb29e
Joel Brobecker <brobecker@adacore.com>
2013-11-11 09:19:32 +0400

    New function cli-utils.c:extract_arg_const
    This function provides the exact same functionality as extract_arg,
    except that it takes a "const char**" instead of a "char **".
    It allows us also to re-implement extract_arg almost as a simple
    wrapper around the new function.
    
    gdb/ChangeLog:
    
            Pedro Alves  <palves@redhat.com>
            Joel Brobecker  <brobecker@adacore.com>
    
            * cli/cli-utils.h (extract_arg_const): Add declaration.
            * cli/cli-utils.c (extract_arg_const): New function.
            (extract_arg): Reimplement using extract_arg_const.

--- gdb/cli/cli-utils.h
+++ gdb/cli/cli-utils.h
@@ -118,6 +118,13 @@ extern char *remove_trailing_whitespace (const char *start, char *s);
 
 extern char *extract_arg (char **arg);
 
+/* A const-correct version of "extract_arg".
+
+   Since the returned value is xmalloc'd, it eventually needs to be
+   xfree'ed, which prevents us from making it const as well.  */
+
+extern char *extract_arg_const (const char **arg);
+
 /* A helper function that looks for an argument at the start of a
    string.  The argument must also either be at the end of the string,
    or be followed by whitespace.  Returns 1 if it finds the argument,
