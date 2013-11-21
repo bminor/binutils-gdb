56244278a64d224e1494e2616ed4995d9ce6ecb0
Andrew Pinski <apinski@cavium.com>
2013-11-20 16:56:20 -0800

    ld/ChangeLog:
    	* Makefile.am (ALL_64_EMULATION_SOURCES): Add eaarch64linux32.c
    	and eaarch64linux32b.c
    	(eaarch64linux32.c): New target.
    	(eaarch64linux32b.c): Likewise.
    	* Makefile.in: Regenerate.
    	* configure.tgt (aarch64_be-*-linux*): Add
    	aarch64linux32 and aarch64linux32b to targ_extra_libpath.
    	(aarch64-*-linux*): Likewise.
    	* emulparams/aarch64elf32.sh (SEPARATE_GOTPLT): Change to 12 (3 words).
    	* emulparams/aarch64linux32.sh: New file.
    	* emulparams/aarch64linux32b.sh: New file.

--- ld/emulparams/aarch64linux32.sh
+++ ld/emulparams/aarch64linux32.sh
@@ -13,15 +13,16 @@ TEMPLATE_NAME=elf32
 EXTRA_EM_FILE=aarch64elf
 
 GENERATE_SHLIB_SCRIPT=yes
-MAXPAGESIZE="CONSTANT (MAXPAGESIZE)"
+GENERATE_PIE_SCRIPT=yes
 
-ENTRY=_start
-EMBEDDED=yes
+MAXPAGESIZE="CONSTANT (MAXPAGESIZE)"
+COMMONPAGESIZE="CONSTANT (COMMONPAGESIZE)"
 SEPARATE_GOTPLT=12
 IREL_IN_PLT=
-TEXT_START_ADDR=0x00400000
 
-DATA_START_SYMBOLS='__data_start = . ;';
+TEXT_START_ADDR=0x400000
+
+DATA_START_SYMBOLS='PROVIDE (__data_start = .);';
 
 # AArch64 does not support .s* sections.
 NO_SMALL_DATA=yes
@@ -32,6 +33,15 @@ OTHER_END_SYMBOLS='__end__ = . ;'
 
 OTHER_SECTIONS='.note.gnu.arm.ident 0 : { KEEP (*(.note.gnu.arm.ident)) }'
 ATTRS_SECTIONS='.ARM.attributes 0 : { KEEP (*(.ARM.attributes)) KEEP (*(.gnu.attributes)) }'
+# Ensure each PLT entry is aligned to a cache line.
+PLT=".plt          ${RELOCATING-0} : ALIGN(16) { *(.plt)${IREL_IN_PLT+ *(.iplt)} }"
 
-# This sets the stack to the top of the simulator memory (2^19 bytes).
-STACK_ADDR=0x80000
+# Linux modifies the default library search path to first include
+# a 32-bit specific directory.
+case "$target" in
+  aarch64*-linux*)
+    case "$EMULATION_NAME" in
+      aarch64linux*) LIBPATH_SUFFIX=32 ;;
+    esac
+    ;;
+esac
