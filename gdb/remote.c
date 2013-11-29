8b4f3082d8f1f7605c6f89f12bc3a93da06845c5
Pedro Alves <palves@redhat.com>
2013-11-29 14:50:26 +0000

    Plug target side conditions and commands leaks.
    The memory management of bp_location->target_info.conditions|tcommands
    is currently a little fragile.  If the target reports support for
    target conditions or commands, and then target side breakpoint support
    is disabled, or some error is thrown before remote_add_target_side_XXX
    is called, we'll leak these lists.  This patch makes us free these
    lists when the locations are deleted, and also, just before recreating
    the commands|conditions lists.
    
    Tested on x86_64 Fedora 17, native and gdbserver.
    
    gdb/
    2013-11-29  Pedro Alves  <palves@redhat.com>
    
    	* breakpoint.c (build_target_condition_list): Release previous
    	conditions.
    	(build_target_command_list): Release previous commands.
    	(bp_location_dtor): Release target conditions and commands.
    	* remote.c (remote_add_target_side_condition): Don't release
    	conditions.
    	(remote_add_target_side_commands): Don't release commands.

--- gdb/remote.c
+++ gdb/remote.c
@@ -8151,8 +8151,6 @@ remote_add_target_side_condition (struct gdbarch *gdbarch,
 	buf = pack_hex_byte (buf, aexpr->buf[i]);
       *buf = '\0';
     }
-
-  VEC_free (agent_expr_p, bp_tgt->conditions);
   return 0;
 }
 
@@ -8183,8 +8181,6 @@ remote_add_target_side_commands (struct gdbarch *gdbarch,
 	buf = pack_hex_byte (buf, aexpr->buf[i]);
       *buf = '\0';
     }
-
-  VEC_free (agent_expr_p, bp_tgt->tcommands);
 }
 
 /* Insert a breakpoint.  On targets that have software breakpoint
