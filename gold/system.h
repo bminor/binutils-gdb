8356f2d009e20e5ca567f5f9456a40f75e9560dc
Cary Coutant <ccoutant@google.com>
2013-12-18 15:53:05 -0800

    Add configure checks for <unordered_set> and <unordered_map>.
    gold/
    	* configure.ac: Check for <unordered_set> and <unordered_map>.
    	* config.in: Regenerate.
    	* configure: Regenerate.
    	* system.h: Use <unordered_set> and <unordered_map> if available.

--- gold/system.h
+++ gold/system.h
@@ -57,8 +57,21 @@
 
 // Figure out how to get a hash set and a hash map.
 
-#if defined(HAVE_TR1_UNORDERED_SET) && defined(HAVE_TR1_UNORDERED_MAP) \
-    && defined(HAVE_TR1_UNORDERED_MAP_REHASH)
+#if defined(HAVE_UNORDERED_SET) && defined(HAVE_UNORDERED_MAP)
+
+#include <unordered_set>
+#include <unordered_map>
+
+// We need a template typedef here.
+
+#define Unordered_set std::unordered_set
+#define Unordered_map std::unordered_map
+#define Unordered_multimap std::unordered_multimap
+
+#define reserve_unordered_map(map, n) ((map)->rehash(n))
+
+#elif defined(HAVE_TR1_UNORDERED_SET) && defined(HAVE_TR1_UNORDERED_MAP) \
+      && defined(HAVE_TR1_UNORDERED_MAP_REHASH)
 
 #include <tr1/unordered_set>
 #include <tr1/unordered_map>
