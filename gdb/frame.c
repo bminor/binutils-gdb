8372a7cb96e9d193cb1f85f3fd2c01e38c33a750
Markus Metzger <markus.t.metzger@intel.com>
2013-12-18 14:13:54 +0100

    frame: add frame_id_build_unavailable_stack_special
    Add a function to build a frame_id for a frame with unavailable stack
    and with a special identifier address.
    
    2014-01-16  Markus Metzger  <markus.t.metzger@intel.com>
    
    	* frame.h (frame_id_build_unavailable_stack_special): New.
    	* frame.c (frame_id_build_unavailable_stack_special): New.

--- gdb/frame.c
+++ gdb/frame.c
@@ -528,6 +528,22 @@ frame_id_build_unavailable_stack (CORE_ADDR code_addr)
   return id;
 }
 
+/* See frame.h.  */
+
+struct frame_id
+frame_id_build_unavailable_stack_special (CORE_ADDR code_addr,
+					  CORE_ADDR special_addr)
+{
+  struct frame_id id = null_frame_id;
+
+  id.stack_status = FID_STACK_UNAVAILABLE;
+  id.code_addr = code_addr;
+  id.code_addr_p = 1;
+  id.special_addr = special_addr;
+  id.special_addr_p = 1;
+  return id;
+}
+
 struct frame_id
 frame_id_build (CORE_ADDR stack_addr, CORE_ADDR code_addr)
 {
