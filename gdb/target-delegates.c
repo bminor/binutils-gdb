170049d41c3cb8becb46e8e71a30f67f5d47d5ad
Tom Tromey <tromey@redhat.com>
2013-12-18 20:13:40 -0700

    convert to_call_history
    2014-02-19  Tom Tromey  <tromey@redhat.com>
    
    	* target-delegates.c: Rebuild.
    	* target.c (target_call_history): Unconditionally delegate.
    	* target.h (struct target_ops) <to_call_history>: Use
    	TARGET_DEFAULT_NORETURN.

--- gdb/target-delegates.c
+++ gdb/target-delegates.c
@@ -1261,6 +1261,19 @@ tdefault_supports_btrace (struct target_ops *self)
 }
 
 static void
+delegate_call_history (struct target_ops *self, int arg1, int arg2)
+{
+  self = self->beneath;
+  self->to_call_history (self, arg1, arg2);
+}
+
+static void
+tdefault_call_history (struct target_ops *self, int arg1, int arg2)
+{
+  tcomplain ();
+}
+
+static void
 delegate_call_history_from (struct target_ops *self, ULONGEST arg1, int arg2, int arg3)
 {
   self = self->beneath;
@@ -1514,6 +1527,8 @@ install_delegators (struct target_ops *ops)
     ops->to_can_use_agent = delegate_can_use_agent;
   if (ops->to_supports_btrace == NULL)
     ops->to_supports_btrace = delegate_supports_btrace;
+  if (ops->to_call_history == NULL)
+    ops->to_call_history = delegate_call_history;
   if (ops->to_call_history_from == NULL)
     ops->to_call_history_from = delegate_call_history_from;
   if (ops->to_call_history_range == NULL)
@@ -1631,6 +1646,7 @@ install_dummy_methods (struct target_ops *ops)
   ops->to_use_agent = tdefault_use_agent;
   ops->to_can_use_agent = tdefault_can_use_agent;
   ops->to_supports_btrace = tdefault_supports_btrace;
+  ops->to_call_history = tdefault_call_history;
   ops->to_call_history_from = tdefault_call_history_from;
   ops->to_call_history_range = tdefault_call_history_range;
   ops->to_augmented_libraries_svr4_read = tdefault_augmented_libraries_svr4_read;
--- gdb/target.c
+++ gdb/target.c
@@ -4063,16 +4063,7 @@ target_insn_history_range (ULONGEST begin, ULONGEST end, int flags)
 void
 target_call_history (int size, int flags)
 {
-  struct target_ops *t;
-
-  for (t = current_target.beneath; t != NULL; t = t->beneath)
-    if (t->to_call_history != NULL)
-      {
-	t->to_call_history (t, size, flags);
-	return;
-      }
-
-  tcomplain ();
+  current_target.to_call_history (&current_target, size, flags);
 }
 
 /* See target.h.  */
