4199e3b8669d0a36448687850374fdc2ad7240b6
Alan Modra <amodra@gmail.com>
2014-01-15 21:50:55 +1030

    non-PIC references to __ehdr_start in pie and shared
    Rather than hacking every backend to not discard dynamic relocations
    against an undefined hidden __ehdr_start, make it appear to be defined
    early.  We want __ehdr_start hidden before size_dynamic_sections so
    that it isn't put in .dynsym, but we do need the dynamic relocations
    for a PIE or shared library with a non-PIC reference.  Defining it
    early is wrong if we don't actually define the symbol later to its
    proper value.  (In some cases we want to leave the symbol undefined,
    for example, when the ELF header isn't loaded, and we don't have this
    infomation available in before_allocation.)
    
    ld/
    	* emultempl/elf32.em (gld${EMULATION_NAME}_before_allocation): Define
    	__ehdr_start before size_dynamic_sections and restore afterwards.
    ld/testsuite/
    	* ld-elf/ehdr_start-shared.d: New.
    	* ld-elf/ehdr_start-userdef.d: xfail frv.
    	* ld-elf/ehdr_start-weak.d: Likewise.
    	* ld-elf/ehdr_start.d: Likewise.

--- ld/testsuite/ld-elf/ehdr_start-shared.d
+++ ld/testsuite/ld-elf/ehdr_start-shared.d
@@ -1,8 +1,8 @@
 #source: ehdr_start.s
-#ld: -e _start
+#ld: -e _start -shared
 #nm: -n
 #target: *-*-linux* *-*-gnu* *-*-nacl*
-#xfail: frv-*-*
+#xfail: cris*-*-* frv-*-*
 
 #...
 [0-9a-f]*000 [Adrt] __ehdr_start
