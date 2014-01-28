342df9df2426b93b1b218848224a26f387c37ecd
Nick Clifton <nickc@redhat.com>
2014-01-28 13:33:29 +0000

    A recent change to the BFD library has meant that the osabi field in ELF headers will be copied from input files.  This in turn has broken the strip-10 test in the binutils testsuite, so this patch updates the expected output.
    	PR binutils/16318
    	* binutils-all/strip-10.d: Allow "System V" in the osabi field.

--- binutils/testsuite/binutils-all/strip-10.d
+++ binutils/testsuite/binutils-all/strip-10.d
@@ -5,6 +5,6 @@
 #name: strip on STB_GNU_UNIQUE
 
 #...
-  OS/ABI:[ \t]+UNIX - GNU
+  OS/ABI:[ \t]+UNIX - (GNU|System V)
 #...
  +[0-9]+: +[0-9a-f]+ +[0-9]+ +OBJECT +(UNIQUE|<OS specific>: 10) +DEFAULT +[1-9] foo
