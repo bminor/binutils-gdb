718ee4dc9b09491707420ae403ad1ce8dfdb61b2
Ulrich Weigand <ulrich.weigand@de.ibm.com>
2014-01-30 19:12:35 +0100

    Add support for AT_HWCAP2 auxv entry
    Recent ppc64 Linux kernels provide a new auxv entry AT_HWCAP2,
    which is currently not recognized by GDB, causing every use of
    "info auxv" to show an error.
    
    This commit adds the AT_HWCAP2 define to include/elf/common.h
    and handles it in GDB.
    
    include/elf/ChangeLog:
    
    	* common.h (AT_HWCAP2): Define.
    
    gdb/ChangeLog:
    
    	* auxv.c (fprint_target_auxv): Handle AT_HWCAP2.

--- include/elf/common.h
+++ include/elf/common.h
@@ -959,6 +959,7 @@
 #define AT_BASE_PLATFORM 24		/* String identifying real platform,
 					   may differ from AT_PLATFORM.  */
 #define AT_RANDOM	25		/* Address of 16 random bytes.  */
+#define AT_HWCAP2	26		/* Extension of AT_HWCAP.  */
 #define AT_EXECFN	31		/* Filename of executable.  */
 /* Pointer to the global system page used for system calls and other
    nice things.  */
