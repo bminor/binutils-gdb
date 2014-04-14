8000c58e455259951daa4370631aa616b367d1a7
Siva Chandra <sivachandra@chromium.org>
2014-04-14 06:50:24 -0700

    Remove unused and incorrect macro TYPE_FN_FIELDS.
    	* gdbtypes.h: Remove definition of the macro TYPE_FN_FIELDS.

--- gdb/gdbtypes.h
+++ gdb/gdbtypes.h
@@ -1218,7 +1218,6 @@ extern void allocate_gnat_aux_type (struct type *);
 #define TYPE_VPTR_BASETYPE(thistype) TYPE_MAIN_TYPE(thistype)->vptr_basetype
 #define TYPE_DOMAIN_TYPE(thistype) TYPE_MAIN_TYPE(thistype)->vptr_basetype
 #define TYPE_VPTR_FIELDNO(thistype) TYPE_MAIN_TYPE(thistype)->vptr_fieldno
-#define TYPE_FN_FIELDS(thistype) TYPE_CPLUS_SPECIFIC(thistype)->fn_fields
 #define TYPE_NFN_FIELDS(thistype) TYPE_CPLUS_SPECIFIC(thistype)->nfn_fields
 #define TYPE_SPECIFIC_FIELD(thistype) \
   TYPE_MAIN_TYPE(thistype)->type_specific_field
