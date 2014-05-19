7f3c034326ce5d487e897826a12c3a4b9d457b49
Joel Brobecker <brobecker@adacore.com>
2014-05-19 11:46:19 -0700

    Remove "set/show remotebaud" (deprecated) commands.
    This patch removes support for the "set/show remotebaud" command,
    which were deprecated in GDB 7.7, and should be now be removed
    ahead of cutting the GDB 7.8 branch.
    
    gdb/ChangeLog:
    
            * serial.c (_initialize_serial): Remove support for
            the "set remotebaud" and "show remotebaud" commands.
            * NEWS: Add entry documenting the removal of that command.
    
    gdb/testsuite/ChangeLog:
    
            * config/monitor.exp (gdb_target_monitor): Replace use of
            "set remotebaud" by "set serial baud".

--- gdb/serial.c
+++ gdb/serial.c
@@ -671,34 +671,6 @@ using remote targets."),
 			    serial_baud_show_cmd,
 			    &serial_set_cmdlist, &serial_show_cmdlist);
 
-  /* The commands "set/show serial baud" used to have a different name.
-     Add aliases to those names to facilitate the transition, and mark
-     them as deprecated, in order to make users aware of the fact that
-     the command names have been changed.  */
-    {
-      const char *cmd_name;
-      struct cmd_list_element *cmd;
-
-      /* FIXME: There is a limitation in the deprecation mechanism,
-	 and the warning ends up not being displayed for prefixed
-	 aliases.  So use a real command instead of an alias.  */
-      add_setshow_zinteger_cmd ("remotebaud", class_alias, &baud_rate, _("\
-Set baud rate for remote serial I/O."), _("\
-Show baud rate for remote serial I/O."), _("\
-This value is used to set the speed of the serial port when debugging\n\
-using remote targets."),
-				NULL,
-				serial_baud_show_cmd,
-				&setlist, &showlist);
-      cmd_name = "remotebaud";
-      cmd = lookup_cmd (&cmd_name, setlist, "", -1, 1);
-      deprecate_cmd (cmd, "set serial baud");
-      cmd_name
-	= "remotebaud"; /* needed because lookup_cmd updates the pointer */
-      cmd = lookup_cmd (&cmd_name, showlist, "", -1, 1);
-      deprecate_cmd (cmd, "show serial baud");
-    }
-
   add_setshow_filename_cmd ("remotelogfile", no_class, &serial_logfile, _("\
 Set filename for remote session recording."), _("\
 Show filename for remote session recording."), _("\
