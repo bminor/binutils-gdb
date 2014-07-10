1a76d598884a052dacd8feb49f1999e1a0d537f1
Doug Evans <dje@google.com>
2014-07-10 11:15:32 -0700

    * gdbthread.h (any_running): Declare. 	* thread.c (any_running): New function.

--- gdb/gdbthread.h
+++ gdb/gdbthread.h
@@ -363,6 +363,9 @@ extern int is_exited (ptid_t ptid);
 /* In the frontend's perpective, is this thread stopped?  */
 extern int is_stopped (ptid_t ptid);
 
+/* In the frontend's perpective is there any thread running?  */
+extern int any_running (void);
+
 /* Marks thread PTID as executing, or not.  If ptid_get_pid (PTID) is -1,
    marks all threads.
 
