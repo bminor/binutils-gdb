ea7cc5bfc8ed2c139aeb50afe5652f8c4bb4d81a
Andreas Schwab <schwab@linux-m68k.org>
2014-07-14 20:59:50 +0200

    Don't complain about dbCC to long branch conversion
    * config/tc-m68k.c (md_convert_frag_1): Don't complain with
    --pcrel about TAB (DBCCLBR, LONG) conversion.

--- gas/config/tc-m68k.c
+++ gas/config/tc-m68k.c
@@ -5166,10 +5166,6 @@ md_convert_frag_1 (fragS *fragP)
       /* Only DBcc instructions can come here.
 	 Change dbcc into dbcc/bral.
 	 JF: these used to be fr_opcode[2-7], but that's wrong.  */
-      if (flag_keep_pcrel)
-    	as_bad_where (fragP->fr_file, fragP->fr_line,
-		  _("Conversion of DBcc to absolute jump"));
-
       *buffer_address++ = 0x00;	/* Branch offset = 4.  */
       *buffer_address++ = 0x04;
       *buffer_address++ = 0x60;	/* Put in bra pc+6.  */
