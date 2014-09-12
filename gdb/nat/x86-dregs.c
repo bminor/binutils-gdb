296b1496f756d26b2e3235133f89012fff940902
Gary Benson <gbenson@redhat.com>
2014-09-12 10:11:42 +0100

    Remove GDBSERVER uses from x86-dregs.c
    This commit makes nat/x86-dregs.c include common-defs.h rather than
    defs.h or server.h.  An extra header required including in order to
    support this change.
    
    gdb/ChangeLog:
    
    	* nat/x86-dregs.c: Include common-defs.h and break-common.h.
    	Don't include defs.h or server.h.

--- gdb/nat/x86-dregs.c
+++ gdb/nat/x86-dregs.c
@@ -17,13 +17,9 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-#ifdef GDBSERVER
-#include "server.h"
-#else
-#include "defs.h"
-#include "inferior.h"
-#endif
+#include "common-defs.h"
 #include "x86-dregs.h"
+#include "break-common.h"
 
 /* Support for hardware watchpoints and breakpoints using the x86
    debug registers.
