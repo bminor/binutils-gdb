14f195c9a0960335c0ff548e86e051072036cb5f
Ilya Tocar <ilya.tocar@intel.com>
2014-11-17 15:41:32 +0300

    Add AVX512VBMI instructions
    gas/
    
    	* config/tc-i386.c (cpu_arch): Add .avx512vbmi.
    	* doc/c-i386.texi: Document it.
    
    opcodes/
    
    	* i386-dis-evex.c (evex_table): Add vpermi2b, vpermt2b, vpermb,
    	vpmultishiftqb.
    	* i386-dis.c (PREFIX enum): Add PREFIX_EVEX_0F3883, EVEX_W_0F3883_P_2.
    	* i386-gen.c (cpu_flag_init): Add CPU_AVX512VBMI_FLAGS.
    	(cpu_flags): Add CpuAVX512VBMI.
    	* i386-opc.h (enum): Add CpuAVX512VBMI.
    	(i386_cpu_flags): Add cpuavx512vbmi.
    	* i386-opc.tbl: Add vpmadd52luq, vpmultishiftqb, vpermb, vpermi2b,
    	vpermt2b.
    	* i386-init.h: Regenerated.
    	* i386-tbl.h: Likewise.
    
    /gas/testsuite/
    
    	* gas/i386/i386.exp: Run new tests.
    	* gas/i386/avx512vbmi-intel.d: New file.
    	* gas/i386/avx512vbmi.d: Likewise.
    	* gas/i386/avx512vbmi.s: Likewise.
    	* gas/i386/avx512vbmi_vl-intel.d: Likewise.
    	* gas/i386/avx512vbmi_vl.d: Likewise.
    	* gas/i386/avx512vbmi_vl.s: Likewise.
    	* gas/i386/x86-64-avx512vbmi-intel.d: Likewise.
    	* gas/i386/x86-64-avx512vbmi.d: Likewise.
    	* gas/i386/x86-64-avx512vbmi.s: Likewise.
    	* gas/i386/x86-64-avx512vbmi_vl-intel.d: Likewise.
    	* gas/i386/x86-64-avx512vbmi_vl.d: Likewise.
    	* gas/i386/x86-64-avx512vbmi_vl.s: Likewise.

--- opcodes/i386-opc.h
+++ opcodes/i386-opc.h
@@ -190,6 +190,8 @@ enum
   CpuPCOMMIT,
   /* Intel AVX-512 IFMA Instructions support required.  */
   CpuAVX512IFMA,
+  /* Intel AVX-512 VBMI Instructions support required.  */
+  CpuAVX512VBMI,
   /* 64bit support required  */
   Cpu64,
   /* Not supported in the 64bit mode  */
@@ -292,6 +294,7 @@ typedef union i386_cpu_flags
       unsigned int cpuclwb:1;
       unsigned int cpupcommit:1;
       unsigned int cpuavx512ifma:1;
+      unsigned int cpuavx512vbmi:1;
       unsigned int cpu64:1;
       unsigned int cpuno64:1;
 #ifdef CpuUnused
