eb2ea29c6d2e200b4ff485f89c9ebc156bd5b391
Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
2014-11-18 11:19:05 +0000

    [AArch64] Add xgene1.
    The name xgene1 superceeds xgene-1.  We retain support for the
    original xgene-1 for compatibility but drop it from documentation.
    
    Conflicts:
    	gas/config/tc-aarch64.c

--- gas/config/tc-aarch64.c
+++ gas/config/tc-aarch64.c
@@ -7184,7 +7184,11 @@ static const struct aarch64_cpu_option_table aarch64_cpus[] = {
 				 AARCH64_FEATURE_CRC), "Cortex-A53"},
   {"cortex-a57", AARCH64_FEATURE(AARCH64_ARCH_V8,
 				 AARCH64_FEATURE_CRC), "Cortex-A57"},
+  /* The 'xgene-1' name is an older name for 'xgene1', which was used
+     in earlier releases and is superseded by 'xgene1' in all
+     tools.  */
   {"xgene-1", AARCH64_ARCH_V8, "APM X-Gene 1"},
+  {"xgene1", AARCH64_ARCH_V8, "APM X-Gene 1"},
   {"generic", AARCH64_ARCH_V8, NULL},
 
   /* These two are example CPUs supported in GCC, once we have real
