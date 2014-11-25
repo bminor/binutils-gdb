d92b6eece424f0ad35d96fdd85bf207295e8c4c3
Max Filippov <jcmvbkbc@gmail.com>
2014-11-25 21:33:21 +0300

    Fix trampolines search code for conditional branches
    For conditional branches that need more than one trampoline to reach its
    target assembler couldn't always find suitable trampoline because
    post-loop condition check was placed inside the loop, resulting in
    premature loop termination. Move check outside the loop.
    
    This fixes the following build errors seen when assembling huge files
    produced by gcc:
        Error: jump target out of range; no usable trampoline found
        Error: operand 1 of 'j' has out of range value '307307'
    
    2014-11-25  Max Filippov  <jcmvbkbc@gmail.com>
    
    gas/
    	* config/tc-xtensa.c (search_trampolines): Move post-loop
    	condition check outside the search loop.
    
    gas/testsuite/
    	* gas/xtensa/trampoline.d: Add expected output for branches.
    	* gas/xtensa/trampoline.s: Add test case for branches.

--- gas/config/tc-xtensa.c
+++ gas/config/tc-xtensa.c
@@ -9514,11 +9514,11 @@ search_trampolines (TInsn *tinsn, fragS *fragP, bfd_boolean unreachable_only)
 	      if (next_addr == 0 || addr - next_addr > J_RANGE)
 		break;
 	    }
-	  if (abs (addr - this_addr) < J_RANGE)
-	    return tf;
-
-	  return NULL;
 	}
+      if (abs (addr - this_addr) < J_RANGE)
+	return tf;
+
+      return NULL;
     }
   for ( ; tf; tf = tf->next)
     {
