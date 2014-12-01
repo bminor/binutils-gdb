ef77750ef779ac887423f84bcc11a8de4dd0fe79
H.J. Lu <hjl.tools@gmail.com>
2014-12-01 09:11:57 -0800

    Properly check for an out of range row index
    	* dwarf.c (process_cu_tu_index): Properly check for an out of
    	range row index.

--- binutils/dwarf.c
+++ binutils/dwarf.c
@@ -6825,7 +6825,7 @@ process_cu_tu_index (struct dwarf_section *section, int do_display)
 	  if (row != 0)
 	    {
 	      /* PR 17531: file: a05f6ab3.  */
-	      if (row >= nused)
+	      if (row > nused)
 		{
 		  warn (_("Row index (%u) is larger than number of used entries (%u)\n"),
 			row, nused);
