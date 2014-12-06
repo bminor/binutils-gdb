c18392d87d22191584d76f7b4a850eb9508a8f04
Eric Botcazou <ebotcazou@gcc.gnu.org>
2014-12-06 16:45:22 +0100

    Add Visium support to ld
    ld/
    	* configure.tgt: Add Visium support.
    	* Makefile.am (ALL_EMULATION_SOURCES): Add eelf32visium.c.
    	(eelf32visium.c): New rule.
    	* Makefile.in: Regenerate.
    	* emulparams/elf32visium.sh: New file.
    	* scripttempl/visium.sc: Likewise.
    ld/testsuite/
    	* lib/ld-lib.exp (check_shared_lib_support): Return 0 for Visium.
    	* ld-visium/: New directory.

--- ld/testsuite/ld-elf/eh-frame-hdr.d
+++ ld/testsuite/ld-elf/eh-frame-hdr.d
@@ -2,7 +2,7 @@
 #ld: -e _start --eh-frame-hdr
 #objdump: -hw
 #target: cfi
-#xfail: avr*-*-* or1k-*-*
+#xfail: avr*-*-* or1k-*-* visium-*-*
 # avr doesn't support shared libraries.
 #...
   [0-9] .eh_frame_hdr 0*[12][048c] .*
