6e8ec71244d3bb6a1223d07965066b7b9836ab7d
Matthew Fortune <matthew.fortune@imgtec.com>
2014-12-18 20:08:38 +0000

    Fix undefined weak symbol reloc tests
    ld/testsuite/
    
    	* ld-mips-elf/mips-elf.exp: Update undefweak-overflow tests.
    	* ld-mips-elf/undefweak-overflow-n32.d: Remove.
    	* ld-mips-elf/undefweak-overflow-n64.d: Likewise.
    	* ld-mips-elf/undefweak-overflow.s: Set mips64r6, noreorder and
    	add a label to mark the micromips region.
    	* ld-mips-elf/undefweak-overflow.d: Update expected output.

--- ld/testsuite/ld-mips-elf/undefweak-overflow.s
+++ ld/testsuite/ld-mips-elf/undefweak-overflow.s
@@ -1,25 +1,32 @@
 # relocs against undefined weak symbols should not be treated as
 # overflowing
 
-
+	.module mips64r6
 	.globl	start
 	.weak	foo
 start:
+	.set noreorder
 	.set mips64r6
 	beqzc	$2, foo
+	nop 
 	bnezc	$2, foo
 	lwpc	$2, foo
 	ldpc	$2, foo
 	bc	foo
-	lui	$4, %pcrel_hi(foo)
-	addiu	$4, $4, %pcrel_lo(foo)
+	auipc	$4, %pcrel_hi(foo)
+	addiu	$4, $4, %pcrel_lo(foo+4)
+
+	b	foo
+	nop
+	bal	foo
+	lui	$4, %gp_rel(foo)
 
 	.set mips32r2
-	b      foo
-	bal    foo
-	lui    $4, %gp_rel(foo)
-
 	.set micromips
-	beqz16 $4, foo
-	b16    foo
-	b      foo
+micro:
+	beqz16	$4, foo
+	nop
+	b16	foo
+	nop
+	b	foo
+	nop
