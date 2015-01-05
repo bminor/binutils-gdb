5fea8873ff34baf85dc2fdbbc4eba937f027b3b4
Will Newton <will.newton@linaro.org>
2015-01-05 16:40:40 +0000

    bfd/version.h: Bump VERSION

--- bfd/version.h
+++ bfd/version.h
@@ -1,4 +1,4 @@
 #define BFD_VERSION_DATE 20150105
 #define BFD_VERSION @bfd_version@
-#define BFD_VERSION_STRING  @bfd_version_package@ "Linaro 2015.01-2 " @bfd_version_string@
+#define BFD_VERSION_STRING  @bfd_version_package@ "Linaro 2015.01-3-git " @bfd_version_string@
 #define REPORT_BUGS_TO @report_bugs_to@
