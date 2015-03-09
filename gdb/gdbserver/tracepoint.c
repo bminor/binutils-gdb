33ebda9d6841e18d9ef18b94c3dcad7d0498ab3d
Pedro Alves <palves@redhat.com>
2015-03-09 11:58:10 +0000

    gdbserver/tracepoint.c: make exported IPA global int instead of enum
    Fixes another C++ -fpermissive error:
    
      src/gdb/gdbserver/tracepoint.c:4535:21: error: invalid conversion from ‘int’ to ‘eval_result_type’ [-fpermissive]
        expr_eval_result = ipa_expr_eval_result;
    
    gdb/gdbserver/ChangeLog:
    2015-08-06  Pedro Alves  <palves@redhat.com>
    
    	* tracepoint.c (expr_eval_result): Now an int.

--- gdb/gdbserver/tracepoint.c
+++ gdb/gdbserver/tracepoint.c
@@ -848,7 +848,10 @@ IP_AGENT_EXPORT_VAR int trace_buffer_is_full;
 
 /* The first error that occurred during expression evaluation.  */
 
-IP_AGENT_EXPORT_VAR enum eval_result_type expr_eval_result = expr_eval_no_error;
+/* Stored as an int to avoid the IPA ABI being dependent on whatever
+   the compiler decides to use for the enum's underlying type.  Holds
+   enum eval_result_type values.  */
+IP_AGENT_EXPORT_VAR int expr_eval_result = expr_eval_no_error;
 
 EXTERN_C_POP
 
