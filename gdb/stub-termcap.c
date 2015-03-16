d053f6be557fa3bedd4ccbd969103dbb51a37439
Yuanhui Zhang <asmwarrior@gmail.com>
2015-03-16 11:28:24 +0000

    stub termcap, add extern "C"
    Fixes linking an --enable-build-with-cxx build on mingw:
    
     ../readline/terminal.c:278: undefined reference to `tgetnum'
     ../readline/terminal.c:297: undefined reference to `tgetnum'
     ../readline/libreadline.a(terminal.o): In function `get_term_capabilities':
     ../readline/terminal.c:427: undefined reference to `tgetstr'
     ../readline/libreadline.a(terminal.o): In function `_rl_init_terminal_io':
     [etc.]
    
    gdb/ChangeLog:
    2015-03-16  Yuanhui Zhang  <asmwarrior@gmail.com>
    	    Pedro Alves  <palves@redhat.com>
    
    	* gdb_curses.h (tgetnum): Mark with EXTERN_C.
    	* stub-termcap.c (tgetent, tgetnum, tgetflag, tgetstr, tputs)
    	(tgoto): Wrap with extern "C".

--- gdb/stub-termcap.c
+++ gdb/stub-termcap.c
@@ -24,6 +24,10 @@
 
 #include <stdlib.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* -Wmissing-prototypes */
 extern int tgetent (char *buffer, char *termtype);
 extern int tgetnum (char *name);
@@ -31,6 +35,10 @@ extern int tgetflag (char *name);
 extern char* tgetstr (char *name, char **area);
 extern int tputs (char *string, int nlines, int (*outfun) (int));
 extern char *tgoto (const char *cap, int col, int row);
+
+#ifdef __cplusplus
+}
+#endif
 
 /* Each of the files below is a minimal implementation of the standard
    termcap function with the same name, suitable for use in a Windows
