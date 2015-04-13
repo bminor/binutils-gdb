bea3f671c5734eb96a02055fbaa0c938ad535b66
Mike Frysinger <vapier@gentoo.org>
2015-04-13 02:07:23 -0400

    sim: m68hc11: convert to sim-cpu
    Make cpu allocation fully dynamic so we can leverage the common
    sim-cpu and its APIs.

--- sim/m68hc11/sim-main.h
+++ sim/m68hc11/sim-main.h
@@ -28,6 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 typedef address_word sim_cia;
 
+typedef struct _sim_cpu SIM_CPU;
+
 #include "sim-signal.h"
 #include "sim-base.h"
 
@@ -576,13 +578,13 @@ extern void m68hc11cpu_set_port (struct hw *me, sim_cpu *cpu,
 #define CIA_SET(CPU,VAL)  (cpu_set_pc ((CPU), (VAL)))
 
 #if (WITH_SMP)
-#define STATE_CPU(sd,n) (&(sd)->cpu[n])
+#define STATE_CPU(sd,n) ((sd)->cpu[n])
 #else
-#define STATE_CPU(sd,n) (&(sd)->cpu[0])
+#define STATE_CPU(sd,n) ((sd)->cpu[0])
 #endif
 
 struct sim_state {
-  sim_cpu        cpu[MAX_NR_PROCESSORS];
+  sim_cpu        *cpu[MAX_NR_PROCESSORS];
   device         *devices;
   sim_state_base base;
 };
