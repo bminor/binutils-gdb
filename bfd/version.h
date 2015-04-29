66da126bb5d88c97a7e01832896c375dd94ab8f4
Maxim Kuvyrkov <maxim.kuvyrkov@linaro.org>
2015-04-29 18:41:50 +0200

    Change format of version string

--- bfd/version.h
+++ bfd/version.h
@@ -1,4 +1,8 @@
 #define BFD_VERSION_DATE 20150105
 #define BFD_VERSION @bfd_version@
-#define BFD_VERSION_STRING  @bfd_version_package@ "Linaro 2015.01-3-git " @bfd_version_string@
+/* Various parts of GCC parse Binutils version string in different fragile ways,
+   and we need to be very careful to not break them.  So far it seems that
+   appending Linaro-specific version at the end is fine so long as it doesn't
+   contain any dots ".".  */
+#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@ " Linaro 2015.01-3-git"
 #define REPORT_BUGS_TO @report_bugs_to@
