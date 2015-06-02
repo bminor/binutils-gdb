afa403d8d467466edfd77baea0bba7ae45a4447f
Jiong Wang <jiong.wang@arm.com>
2015-06-02 13:59:17 +0100

    [AArch64] Fix typo in testcase
    ld/testsuite/
      * ld-aarch64/emit-relocs-313.s: Use gotpage_lo15.

--- ld/testsuite/ld-aarch64/emit-relocs-313.s
+++ ld/testsuite/ld-aarch64/emit-relocs-313.s
@@ -1,5 +1,5 @@
 	.text
 	adrp  x2, _GLOBAL_OFFSET_TABLE_
-	ldr	x0, [x2, #:gotpage_lo14:globala]
-	ldr	x0, [x2, #:gotpage_lo14:globalb]
-	ldr	x0, [x2, #:gotpage_lo14:globalc]
+	ldr	x0, [x2, #:gotpage_lo15:globala]
+	ldr	x0, [x2, #:gotpage_lo15:globalb]
+	ldr	x0, [x2, #:gotpage_lo15:globalc]
