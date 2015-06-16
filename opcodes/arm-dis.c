b8ff9e984b48784362e42ad992efae46b97bd99b
Szabolcs Nagy <szabolcs.nagy@arm.com>
2015-06-16 13:35:33 +0100

    Fixes a compile time warnng about left shifting a negative value.
    	* arm-dis.c (print_insn_coprocessor): Avoid negative shift.

--- opcodes/arm-dis.c
+++ opcodes/arm-dis.c
@@ -3351,7 +3351,7 @@ print_insn_coprocessor (bfd_vma pc,
 
 		    /* Is ``imm'' a negative number?  */
 		    if (imm & 0x40)
-		      imm |= (-1 << 7);
+		      imm -= 0x80;
 
 		    func (stream, "%d", imm);
 		  }
