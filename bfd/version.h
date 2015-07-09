5a45d7f3983418cd083f76978e9a12866c5f2b53
GDB Administrator <gdbadmin@sourceware.org>
2015-07-09 00:00:23 +0000

    Automatic date update in version.in

--- bfd/version.h
+++ bfd/version.h
@@ -1,4 +1,4 @@
-#define BFD_VERSION_DATE 20150708
+#define BFD_VERSION_DATE 20150709
 #define BFD_VERSION @bfd_version@
 #define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@
 #define REPORT_BUGS_TO @report_bugs_to@
