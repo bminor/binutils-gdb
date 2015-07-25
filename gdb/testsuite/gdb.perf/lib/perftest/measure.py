ca3084f54b62ce06c6e70a6e1daafeb5e3317c12
Doug Evans <xdje42@gmail.com>
2015-07-24 17:39:56 -0700

    Revert 4fd4095a5ffe3d4e50e0dac5f8ad37b8478afa9d, log individual measurements.
    I think I lost a patch along the way, because I remember needing
    something like this, but the reverted patch isn't the right way to
    do this.  Removing ...
    
    gdb/testsuite/ChangeLog:
    
    	* gdb.perf/lib/perftest/measure.py (MeasurementCpuTime::stop): Print
    	result.
    	(MeasurementWallTime::stop): Ditto.
    	(MeasurementVmSizeTime::stop): Ditto.

--- gdb/testsuite/gdb.perf/lib/perftest/measure.py
+++ gdb/testsuite/gdb.perf/lib/perftest/measure.py
@@ -103,8 +103,6 @@ class MeasurementCpuTime(Measurement):
         else:
             cpu_time = time.clock() - self.start_time
         self.result.record (id, cpu_time)
-        # Log this result for debugging purposes.
-        print ("elapsed cpu time %s" % (cpu_time))
 
 class MeasurementWallTime(Measurement):
     """Measurement on Wall time."""
@@ -119,8 +117,6 @@ class MeasurementWallTime(Measurement):
     def stop(self, id):
         wall_time = time.time() - self.start_time
         self.result.record (id, wall_time)
-        # Log this result for debugging purposes.
-        print ("elapsed wall time %s" % (wall_time))
 
 class MeasurementVmSize(Measurement):
     """Measurement on memory usage represented by VmSize."""
@@ -148,5 +144,3 @@ class MeasurementVmSize(Measurement):
     def stop(self, id):
         memory_used = self._compute_process_memory_usage("VmSize:")
         self.result.record (id, memory_used)
-        # Log this result for debugging purposes.
-        print ("vm used %s" % (memory_used))
