64140f86ab732f9ed87beccab07c32befaf9ca52
H.J. Lu <hjl.tools@gmail.com>
2015-08-11 03:50:17 -0700

    Fix a typo in _bfd_elf_copy_private_bfd_data
    	* elf.c (_bfd_elf_copy_private_bfd_data): Fix a typo.

--- bfd/elf.c
+++ bfd/elf.c
@@ -1237,7 +1237,7 @@ _bfd_elf_copy_private_bfd_data (bfd *ibfd, bfd *obfd)
 		 into SHT_NOBITS sections, the output SHT_NOBITS type
 		 matches any input type.  */
 	      if ((oheader->sh_type == SHT_NOBITS
-		   || oheader->sh_type == oheader->sh_type)
+		   || iheader->sh_type == oheader->sh_type)
 		  && iheader->sh_flags == oheader->sh_flags
 		  && iheader->sh_addralign == oheader->sh_addralign
 		  && iheader->sh_entsize == oheader->sh_entsize
