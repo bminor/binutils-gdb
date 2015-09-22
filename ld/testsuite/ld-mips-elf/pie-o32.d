0a6b6047cfd01e229234b747c7694e545b9aefb3
H.J. Lu <hjl.tools@gmail.com>
2015-09-22 11:12:01 -0700

    Update MIPS PIE tests for DF_1_PIE change
    Since linker now sets the DF_1_PIE bit in the DT_FLAGS_1 tag for PIE,
    we need to update MIPS PIE tests for it.
    
    	* ld-mips-elf/pie-n32.d: Updated.
    	* ld-mips-elf/pie-n64.d: Likewise.
    	* ld-mips-elf/pie-o32.d: Likewise.

--- ld/testsuite/ld-mips-elf/pie-o32.d
+++ ld/testsuite/ld-mips-elf/pie-o32.d
@@ -3,11 +3,11 @@
 #ld: -melf32btsmip -pie
 #readelf: -d
 
-Dynamic section at offset 0x178 contains 16 entries:
+Dynamic section at offset 0x178 contains 17 entries:
   Tag * Type * Name/Value
- 0x00000004 \(HASH\) * 0x220
- 0x00000005 \(STRTAB\) * 0x2fc
- 0x00000006 \(SYMTAB\) * 0x25c
+ 0x00000004 \(HASH\) * 0x228
+ 0x00000005 \(STRTAB\) * 0x304
+ 0x00000006 \(SYMTAB\) * 0x264
  0x0000000a \(STRSZ\) * 72 \(bytes\)
  0x0000000b \(SYMENT\) * 16 \(bytes\)
  0x70000035 \(MIPS_RLD_MAP_REL\) * 0x101c0
@@ -20,4 +20,5 @@ Dynamic section at offset 0x178 contains 16 entries:
  0x70000011 \(MIPS_SYMTABNO\) * 10
  0x70000012 \(MIPS_UNREFEXTNO\) * 13
  0x70000013 \(MIPS_GOTSYM\) * 0xa
+ 0x6ffffffb \(FLAGS_1\) * Flags: PIE
  0x00000000 \(NULL\) * 0x0
