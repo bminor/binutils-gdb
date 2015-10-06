975a736c6e8507b26900cd81cb84cc228ba6b5ee
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2015-10-06 19:34:14 +0000

    bfd/version.h: Update VERSION for 2.25 2015.10 release

--- bfd/version.h
+++ bfd/version.h
@@ -1,8 +1,8 @@
-#define BFD_VERSION_DATE 20150105
+#define BFD_VERSION_DATE 20151001
 #define BFD_VERSION @bfd_version@
 /* Various parts of GCC parse Binutils version string in different fragile ways,
    and we need to be very careful to not break them.  So far it seems that
    appending Linaro-specific version at the end is fine so long as it doesn't
    contain any dots ".".  */
-#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@ " Linaro 2015.01-3-git"
+#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@ " Linaro 2015.10"
 #define REPORT_BUGS_TO @report_bugs_to@
