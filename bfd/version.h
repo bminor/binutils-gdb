e6da9b2de5b77ad8ee3dc9c3b7895dd57c3deb88
GDB Administrator <gdbadmin@sourceware.org>
2015-10-09 00:00:45 +0000

    Automatic date update in version.in

--- bfd/version.h
+++ bfd/version.h
@@ -1,4 +1,4 @@
-#define BFD_VERSION_DATE 20151008
+#define BFD_VERSION_DATE 20151009
 #define BFD_VERSION @bfd_version@
 /* Various parts of GCC parse Binutils version string in different fragile ways,
    and we need to be very careful to not break them.  So far it seems that
