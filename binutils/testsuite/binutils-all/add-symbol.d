8b89121d4444e5c23295c63835da538b11d4cb49
Alan Modra <amodra@gmail.com>
2015-10-22 12:44:24 +1030

    Run add-symbol test on mips
    	* binutils-all/add-symbol.d: Run test on mips.  Support either
    	order of NEW and NEWDATA symbols.

--- binutils/testsuite/binutils-all/add-symbol.d
+++ binutils/testsuite/binutils-all/add-symbol.d
@@ -3,14 +3,12 @@
 #source: symbols.s
 #objcopy: --add-symbol NEW=0x1234 --add-symbol NEW_DATA=.data:0x4321,local
 #objdump: --syms
-# MIPS targets swap the order of the symbols in the output.
-#not-target: mips-*-* tx39-*-*
 
 .*: +file format .*
 
 SYMBOL TABLE:
 #...
-0+04321 l[ 	]+.data[ 	]+0+00 NEW_DATA
+(0+04321 l[ 	]+.data[ 	]+0+00 NEW_DATA|0+01234 g[ 	]+\*ABS\*[ 	]+0+00 NEW)
 #...
-0+01234 g[ 	]+\*ABS\*[ 	]+0+00 NEW
+(0+01234 g[ 	]+\*ABS\*[ 	]+0+00 NEW|0+04321 l[ 	]+.data[ 	]+0+00 NEW_DATA)
 #pass
