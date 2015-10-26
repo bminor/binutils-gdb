a6af384b19c63bab4a1803c09592c5b5bd8674c2
H.J. Lu <hjl.tools@gmail.com>
2015-10-26 06:10:23 -0700

    Properly convert address load of __start_XXX/__stop_XXX
    Since __start_XXX and __stop_XXX symbols aren't defined when address
    load is being converted, we need to check if there is an XXX output
    section to get their section and value.  This patch adds a new function,
    bfd_link_get_defined_symbol, to search for the XXX output section to
    check if __start_XXX and __stop_XXX symbols are defined.
    
    bfd/
    
    	PR ld/19171
    	* elf32-i386.c (elf_i386_convert_load): Call
    	bfd_link_get_defined_symbol to check if a symbol is defined.
    	* elf64-x86-64.c (elf_x86_64_convert_load): Call
    	bfd_link_get_defined_symbol to get defined symbol section and
    	value.
    	* linker.c (bfd_link_get_defined_symbol): New function.
    	* bfd-in2.h: Regenerated.
    
    ld/testsuite/
    
    	PR ld/19171
    	* ld-i386/lea1.s: Add tests for address load of __start_XXX
    	and __stop_XXX.
    	* ld-i386/mov1.s: Likewise.
    	* ld-x86-64/lea1.s: Likewise.
    	* ld-x86-64/mov1.s: Likewise.
    	* ld-i386/lea1a.d: Updated.
    	* ld-i386/lea1b.d: Likewise.
    	* ld-i386/lea1c.d: Likewise.
    	* ld-i386/mov1a.d: Likewise.
    	* ld-i386/mov1b.d: Likewise.
    	* ld-x86-64/lea1a.d: Likewise.
    	* ld-x86-64/lea1b.d: Likewise.
    	* ld-x86-64/lea1c.d: Likewise.
    	* ld-x86-64/lea1d.d: Likewise.
    	* ld-x86-64/lea1e.d: Likewise.
    	* ld-x86-64/lea1f.d: Likewise.
    	* ld-x86-64/mov1a.d: Likewise.
    	* ld-x86-64/mov1b.d: Likewise.
    	* ld-x86-64/mov1c.d: Likewise.
    	* ld-x86-64/mov1d.d: Likewise.

--- ld/testsuite/ld-i386/lea1a.d
+++ ld/testsuite/ld-i386/lea1a.d
@@ -11,4 +11,6 @@ Disassembly of section .text:
 #...
 [ 	]*[a-f0-9]+:	8d 81 ([0-9a-f]{2} ){4} *	lea    -0x[a-f0-9]+\(%ecx\),%eax
 [ 	]*[a-f0-9]+:	8d 81 ([0-9a-f]{2} ){4} *	lea    0x[a-f0-9]+\(%ecx\),%eax
+[ 	]*[a-f0-9]+:	8d 81 ([0-9a-f]{2} ){4} *	lea    0x[a-f0-9]+\(%ecx\),%eax
+[ 	]*[a-f0-9]+:	8d 81 ([0-9a-f]{2} ){4} *	lea    0x[a-f0-9]+\(%ecx\),%eax
 #pass
