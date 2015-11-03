1c215b97f9a64d6ba9821e8a0cf0f44526f29d78
Simon Marchi <simon.marchi@polymtl.ca>
2015-11-03 13:33:16 -0500

    xtensa: Add missing statics
    This actually fixes the build in C:
    
    /home/simark/src/binutils-gdb/gdb/xtensa-linux-nat.c:100:1: error: no previous prototype for ‘supply_gregset_reg’ [-Werror=missing-prototypes]
     supply_gregset_reg (struct regcache *regcache,
     ^
    /home/simark/src/binutils-gdb/gdb/xtensa-linux-nat.c:257:1: error: no previous prototype for ‘xtensa_linux_fetch_inferior_registers’ [-Werror=missing-prototypes]
     xtensa_linux_fetch_inferior_registers (struct target_ops *ops,
     ^
    /home/simark/src/binutils-gdb/gdb/xtensa-linux-nat.c:272:1: error: no previous prototype for ‘xtensa_linux_store_inferior_registers’ [-Werror=missing-prototypes]
     xtensa_linux_store_inferior_registers (struct target_ops *ops,
     ^
    cc1: all warnings being treated as errors
    
    These functions are local to this file, so they should be static.
    
    gdb/ChangeLog:
    
    	* xtensa-linux-nat.c (supply_gregset_reg): Make static.
    	(xtensa_linux_fetch_inferior_registers): Likewise.
    	(xtensa_linux_store_inferior_registers): Likewise.

--- gdb/xtensa-linux-nat.c
+++ gdb/xtensa-linux-nat.c
@@ -96,7 +96,7 @@ fill_gregset (const struct regcache *regcache,
     }
 }
 
-void
+static void
 supply_gregset_reg (struct regcache *regcache,
 		    const gdb_gregset_t *gregsetp, int regnum)
 {
@@ -253,7 +253,7 @@ store_xtregs (struct regcache *regcache, int regnum)
     perror_with_name (_("Couldn't write extended registers"));
 }
 
-void
+static void
 xtensa_linux_fetch_inferior_registers (struct target_ops *ops,
 				       struct regcache *regcache, int regnum)
 {
@@ -268,7 +268,7 @@ xtensa_linux_fetch_inferior_registers (struct target_ops *ops,
     fetch_xtregs (regcache, regnum);
 }
 
-void
+static void
 xtensa_linux_store_inferior_registers (struct target_ops *ops,
 				       struct regcache *regcache, int regnum)
 {
