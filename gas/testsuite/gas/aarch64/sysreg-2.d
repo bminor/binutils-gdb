fe06690213df59e70d273ddfc10da053ec3db895
Matthew Wahab <matthew.wahab@arm.com>
2015-12-11 09:52:11 +0000

    [AArch64][Patch 2/5] Add Statistical Profiling Extension system registers.
    The Statistical Profile extension included in the ARMv8.2 architecture
    adds a number of system registers. This patch adds the registers to
    binutils, making them available when the architecture extension
    "+profile" is enabled.
    
    opcodes/
    2015-12-11  Matthew Wahab  <matthew.wahab@arm.com>
    
    	* aarch64-opc.c (aarch64_sys_reg): Add pbmlimitr_el1, pmbptr_el1,
    	pmbsr_el1, pmbidr_el1, pmscr_el1, pmsicr_el1, pmsirr_el1,
    	pmsfcr_el1, pmsevfr_el1, pmslatfr_el1, pmsidr_el1, pmscr_el2 and
    	pmscr_el2.
    	(aarch64_sys_reg_supported_p): Add architecture feature tests for
    	the new registers.
    
    gas/testsuite/
    2015-12-11  Matthew Wahab  <matthew.wahab@arm.com>
    
    	* gas/aarch64/sysreg-2.s: Add tests for the statistical profiling
    	system registers.
    	* gas/aarch64/sysreg-2.d: Enable the statistical profiling
    	extension and update the expected output.
    
    Change-Id: Ibf23ad34db7c33f0fcd30010b796748b38be6efb

--- gas/testsuite/gas/aarch64/sysreg-2.d
+++ gas/testsuite/gas/aarch64/sysreg-2.d
@@ -1,5 +1,5 @@
 #objdump: -dr
-#as: -march=armv8.2-a
+#as: -march=armv8.2-a+profile
 
 .*:     file .*
 
@@ -32,3 +32,29 @@ Disassembly of section .text:
   [0-9a-f]+:	d50b7c22 	dc	cvap, x2
   [0-9a-f]+:	d5087900 	at	s1e1rp, x0
   [0-9a-f]+:	d5087921 	at	s1e1wp, x1
+  [0-9a-f]+:	d5189a07 	msr	pmblimitr_el1, x7
+  [0-9a-f]+:	d5389a07 	mrs	x7, pmblimitr_el1
+  [0-9a-f]+:	d5189a27 	msr	pmbptr_el1, x7
+  [0-9a-f]+:	d5389a27 	mrs	x7, pmbptr_el1
+  [0-9a-f]+:	d5189a67 	msr	pmbsr_el1, x7
+  [0-9a-f]+:	d5389a67 	mrs	x7, pmbsr_el1
+  [0-9a-f]+:	d5189ae7 	msr	pmbidr_el1, x7
+  [0-9a-f]+:	d5389ae7 	mrs	x7, pmbidr_el1
+  [0-9a-f]+:	d5189907 	msr	pmscr_el1, x7
+  [0-9a-f]+:	d5389907 	mrs	x7, pmscr_el1
+  [0-9a-f]+:	d5189947 	msr	pmsicr_el1, x7
+  [0-9a-f]+:	d5389947 	mrs	x7, pmsicr_el1
+  [0-9a-f]+:	d5189967 	msr	pmsirr_el1, x7
+  [0-9a-f]+:	d5389967 	mrs	x7, pmsirr_el1
+  [0-9a-f]+:	d5189987 	msr	pmsfcr_el1, x7
+  [0-9a-f]+:	d5389987 	mrs	x7, pmsfcr_el1
+  [0-9a-f]+:	d51899a7 	msr	pmsevfr_el1, x7
+  [0-9a-f]+:	d53899a7 	mrs	x7, pmsevfr_el1
+  [0-9a-f]+:	d51899c7 	msr	pmslatfr_el1, x7
+  [0-9a-f]+:	d53899c7 	mrs	x7, pmslatfr_el1
+  [0-9a-f]+:	d51c9907 	msr	pmscr_el2, x7
+  [0-9a-f]+:	d53c9907 	mrs	x7, pmscr_el2
+  [0-9a-f]+:	d51d9907 	msr	pmscr_el12, x7
+  [0-9a-f]+:	d53d9907 	mrs	x7, pmscr_el12
+  [0-9a-f]+:	d5389ae7 	mrs	x7, pmbidr_el1
+  [0-9a-f]+:	d53899e7 	mrs	x7, pmsidr_el1
