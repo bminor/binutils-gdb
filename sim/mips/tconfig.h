8abe6c668e9336418277e64983587f121c8534e6
Mike Frysinger <vapier@gentoo.org>
2015-12-24 16:04:26 -0500

    sim: delete SIM_HAVE_SIMCACHE
    This was used by the old run interface, but we punted that awhile ago,
    so drop this define too.

--- sim/mips/tconfig.h
+++ sim/mips/tconfig.h
@@ -7,11 +7,5 @@
 #define SIM_HANDLES_LMA 0
 #endif
 
-/* Define this if the simulator uses an instruction cache.
-   See the h8/300 simulator for an example.
-   This enables the `-c size' option to set the size of the cache.
-   The target is required to provide sim_set_simcache_size.  */
-/* #define SIM_HAVE_SIMCACHE */
-
 /* MIPS uses an unusual format for floating point quiet NaNs.  */
 #define SIM_QUIET_NAN_NEGATED
