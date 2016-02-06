e909d859f5635d66e79fef467da70d6090bfae1b
Marcin Kościelnicki <koriakin@0x04.net>
2016-02-06 16:26:07 +0100

    gdb.trace: Use g packet order in tfile_fetch_registers.
    tfile_fetch_registers currently wrongly fetches registers using
    gdb order instead of g packet order.  On x86_64 with AVX, this causes
    problems with ymm*h and orig_rax registers: gdb has ymm*h first, while
    g packet has orig_rax first.
    
    gdb/ChangeLog:
    
    	* tracefile-tfile.c (tfile_fetch_registers): Use g packet order
    	instead of gdb order.
    
    gdb/doc/ChangeLog:
    
    	* gdb.texinfo (Trace File Format): Remove misleading information
    	about register block ordering.

--- gdb/tracefile-tfile.c
+++ gdb/tracefile-tfile.c
@@ -28,6 +28,7 @@
 #include "exec.h" /* exec_bfd */
 #include "completer.h"
 #include "filenames.h"
+#include "remote.h"
 
 #ifndef O_LARGEFILE
 #define O_LARGEFILE 0
@@ -796,7 +797,7 @@ tfile_fetch_registers (struct target_ops *ops,
 		       struct regcache *regcache, int regno)
 {
   struct gdbarch *gdbarch = get_regcache_arch (regcache);
-  int offset, regn, regsize;
+  int offset, regn, regsize, dummy;
 
   /* An uninitialized reg size says we're not going to be
      successful at getting register blocks.  */
@@ -809,11 +810,12 @@ tfile_fetch_registers (struct target_ops *ops,
 
       tfile_read (regs, trace_regblock_size);
 
-      /* Assume the block is laid out in GDB register number order,
-	 each register with the size that it has in GDB.  */
-      offset = 0;
       for (regn = 0; regn < gdbarch_num_regs (gdbarch); regn++)
 	{
+	  if (!remote_register_number_and_offset (get_regcache_arch (regcache),
+						  regn, &dummy, &offset))
+	    continue;
+
 	  regsize = register_size (gdbarch, regn);
 	  /* Make sure we stay within block bounds.  */
 	  if (offset + regsize > trace_regblock_size)
@@ -830,7 +832,6 @@ tfile_fetch_registers (struct target_ops *ops,
 		  regcache_raw_supply (regcache, regn, regs + offset);
 		}
 	    }
-	  offset += regsize;
 	}
     }
   else
