1e903e320352db86233e1dd2d10fd140f5d4a887
Pedro Alves <palves@redhat.com>
2016-03-01 17:25:56 +0000

    Fix gdb.threads/watchpoint-fork*.c compilation
    This testcase currently fails to compile on Fedora 23:
    
     .../src/gdb/testsuite/gdb.threads/watchpoint-fork-mt.c: In function 'start':
     .../src/gdb/testsuite/gdb.threads/watchpoint-fork-mt.c:70:11: warning: implicit declaration of function 'pthread_yield' [-Wimplicit-function-declaration]
    	i = pthread_yield ();
    	    ^
     .../src/gdb/testsuite/gdb.threads/watchpoint-fork-child.c: In function 'forkoff':
     .../src/gdb/testsuite/gdb.threads/watchpoint-fork-child.c:114:8: warning: implicit declaration of function 'pthread_yield' [-Wimplicit-function-declaratio
     n]
         i = pthread_yield ();
    	 ^
     /tmp/ccUkNIsI.o: In function `start':
     .../src/gdb/testsuite/gdb.threads/watchpoint-fork-mt.c:70: undefined reference to `pthread_yield'
     (...)
     collect2: error: ld returned 1 exit status
    
     UNSUPPORTED: gdb.threads/watchpoint-fork.exp: child: multithreaded: Couldn't compile watchpoint-fork-child.c: unrecognized error
     UNTESTED: gdb.threads/watchpoint-fork.exp: child: multithreaded: watchpoint-fork.exp
     testcase .../src/gdb/testsuite/gdb.threads/watchpoint-fork.exp completed i
    
    The glibc manual says, on _GNU_SOURCE:
    
     "You should define these macros by using ‘#define’ preprocessor
     directives at the top of your source code files. These directives must
     come before any #include of a system header file."
    
    I instead put it in the header all the .c files of the testcase must
    include anyway.
    
    gdb/testsuite/ChangeLog:
    2016-03-01  Pedro Alves  <palves@redhat.com>
    
    	* gdb.threads/watchpoint-fork-child.c: Include "watchpoint-fork.h"
    	before anything else.
    	* gdb.threads/watchpoint-fork-mt.c: Likewise.  Don't define
    	_GNU_SOURCE here.
    	* gdb.threads/watchpoint-fork-st.c: Include "watchpoint-fork.h"
    	before anything else.
    	* gdb.threads/watchpoint-fork.h: Define _GNU_SOURCE.

--- gdb/testsuite/gdb.threads/watchpoint-fork.h
+++ gdb/testsuite/gdb.threads/watchpoint-fork.h
@@ -17,6 +17,9 @@
    You should have received a copy of the GNU General Public License
    along with this program; if not, see <http://www.gnu.org/licenses/>.  */
 
+/* pthread_yield is a GNU extension.  */
+#define _GNU_SOURCE
+
 #ifdef THREAD
 #include <pthread.h>
 
