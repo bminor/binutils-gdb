2e75d7310ff0a512b58ebe074bab7daa36aa6554
H.J. Lu <hjl.tools@gmail.com>
2016-05-01 14:35:31 -0700

    Update x86-64 tests

--- ld/testsuite/ld-x86-64/discarded1.d
+++ ld/testsuite/ld-x86-64/discarded1.d
@@ -1,3 +1,16 @@
 #as: --64
 #ld: -melf_x86_64 -T discarded1.t
-#error: .*discarded output section: `.got.plt'
+#objdump: --sym -dw
+
+.*: +file format .*
+
+SYMBOL TABLE:
+#...
+0+8 g     O .data	0+4 x
+#...
+
+Disassembly of section .text:
+
+0+ <_start>:
+[ 	]*[a-f0-9]+:	48 c7 c0 08 00 00 00 	mov    \$0x8,%rax
+#pass
