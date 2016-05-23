daa4adae63f91377fe9b3e8d7421a0ceb4a51e26
Thomas Preud'homme <thomas.preudhomme@arm.com>
2016-05-23 09:38:32 +0100

    Support for dedicated output section for some ARM veneer types
    2016-05-23  Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
    bfd/
    	* bfd-in.h (bfd_elf32_arm_keep_private_stub_output_sections): Declare
    	bfd hook.
    	* bfd-in2.h: Regenerate.
    	* elf32-arm.c (arm_dedicated_stub_output_section_required): New
    	function.
    	(arm_dedicated_stub_output_section_required_alignment): Likewise.
    	(arm_dedicated_stub_output_section_name): Likewise.
    	(arm_dedicated_stub_input_section_ptr): Likewise.
    	(elf32_arm_create_or_find_stub_sec): Add stub type parameter and
    	function description comment. Add support for dedicated output stub
    	section to given stub types.
    	(elf32_arm_add_stub): Add a stub type parameter and pass it down to
    	elf32_arm_create_or_find_stub_sec.
    	(elf32_arm_create_stub): Pass stub type down to elf32_arm_add_stub.
    	(elf32_arm_size_stubs): Pass stub type when calling
    	elf32_arm_create_or_find_stub_sec for Cortex-A8 erratum veneers.
    	(bfd_elf32_arm_keep_private_stub_output_sections): New function.
    
    ld/
    	* emultempl/armelf.em (arm_elf_before_allocation): Call
    	bfd_elf32_arm_keep_private_stub_output_sections before generic
    	before_allocation function.

--- bfd/bfd-in.h
+++ bfd/bfd-in.h
@@ -903,6 +903,9 @@ extern bfd_boolean bfd_elf32_arm_get_bfd_for_interworking
 extern bfd_boolean bfd_elf32_arm_add_glue_sections_to_bfd
   (bfd *, struct bfd_link_info *);
 
+extern void bfd_elf32_arm_keep_private_stub_output_sections
+  (struct bfd_link_info *);
+
 /* ELF ARM mapping symbol support.  */
 #define BFD_ARM_SPECIAL_SYM_TYPE_MAP	(1 << 0)
 #define BFD_ARM_SPECIAL_SYM_TYPE_TAG	(1 << 1)
