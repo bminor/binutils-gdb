3b4412a0419d0faa42432df1e4faab880fe43b35
Maciej W. Rozycki <macro@imgtec.com>
2016-05-28 10:28:05 +0100

    MIPS/LD/testsuite: Rename `unaligned-syms' to `unaligned-data'
    	ld/
    	* testsuite/ld-mips-elf/unaligned-syms.s: Rename to...
    	* testsuite/ld-mips-elf/unaligned-data.s: ... this.
    	* testsuite/ld-mips-elf/unaligned-ldpc-0.d: Adjust accordingly.
    	* testsuite/ld-mips-elf/unaligned-ldpc-1.d: Likewise.
    	* testsuite/ld-mips-elf/unaligned-lwpc-0.d: Likewise.
    	* testsuite/ld-mips-elf/unaligned-lwpc-1.d: Likewise.

--- ld/testsuite/ld-mips-elf/unaligned-ldpc-1.d
+++ ld/testsuite/ld-mips-elf/unaligned-ldpc-1.d
@@ -1,6 +1,6 @@
 #name: MIPS LDPC from unaligned symbol 1
 #source: unaligned-ldpc-1.s
-#source: unaligned-syms.s
+#source: unaligned-data.s
 #as: -EB -32 -mips64r6
 #ld: -EB -Ttext 0x1c000000 -Tdata 0x1c080000 -e 0x1c000000
 #error: \A[^\n]*: In function `foo':\n
