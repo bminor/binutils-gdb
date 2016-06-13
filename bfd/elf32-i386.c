3cac3e2e8b3c2d70467ac14c07f78bf3088c2e42
H.J. Lu <hjl.tools@gmail.com>
2016-06-13 09:27:12 -0700

    Add the GOT base for GOT32 relocs against IFUNC
    Add the GOT base for R_386_GOT32/R_386_GOT32X relocations against IFUNC
    symbols if there is no base register and disallow them for PIC.
    
    bfd/
    
    	PR ld/20244
    	* elf32-i386.c (elf_i386_relocate_section): Add the .got.plt
    	section address for R_386_GOT32/R_386_GOT32X relocations against
    	IFUNC symbols if there is no base register and return error for
    	PIC.
    
    	* elf32-i386.c (elf_i386_relocate_section): Simplify IFUNC
    	GOT32 adjustment for static executables.
    
    ld/
    
    	PR ld/20244
    	* testsuite/ld-i386/i386.exp: Run pr20244-2a, pr20244-2b,
    	pr20244-2c and pr20244-2d.
    	* testsuite/ld-i386/no-plt.exp: Run pr20244-3a and pr20244-3b.
    	* testsuite/ld-i386/pr20244-2.s: New file.
    	* testsuite/ld-i386/pr20244-2a.d: Likewise.
    	* testsuite/ld-i386/pr20244-2b.d: Likewise.
    	* testsuite/ld-i386/pr20244-2c.d: Likewise.
    	* testsuite/ld-i386/pr20244-2d.d: Likewise.
    	* testsuite/ld-i386/pr20244-3a.c: Likewise.
    	* testsuite/ld-i386/pr20244-3b.S: Likewise.
    	* testsuite/ld-i386/pr20244-3c.S: Likewise.
    	* testsuite/ld-i386/pr20244-3d.S: Likewise.
    
    Add 2 i386 tests to call IFUNC functions via GOT
    
    ld/
    
    	* testsuite/ld-i386/i386.exp: Run ifunc-1a and ifunc-1b.
    	* testsuite/ld-i386/ifunc-1a.c: New file.
    	* testsuite/ld-i386/ifunc-1b.S: Likewise.
    	* testsuite/ld-i386/ifunc-1c.S: Likewise.
    	* testsuite/ld-i386/ifunc-1d.S: Likewise.

--- bfd/elf32-i386.c
+++ bfd/elf32-i386.c
@@ -3949,20 +3949,26 @@ elf_i386_relocate_section (bfd *output_bfd,
 		    }
 
 		  relocation = off;
-
-		  /* Adjust for static executables.  */
-		  if (htab->elf.splt == NULL)
-		    relocation += gotplt->output_offset;
 		}
 	      else
+		relocation = (base_got->output_section->vma
+			      + base_got->output_offset + off
+			      - gotplt->output_section->vma
+			      - gotplt->output_offset);
+
+	      if ((*(contents + rel->r_offset - 1) & 0xc7) == 0x5)
 		{
-		  relocation = (base_got->output_section->vma
-				+ base_got->output_offset + off
-				- gotplt->output_section->vma
-				- gotplt->output_offset);
+		  if (bfd_link_pic (info))
+		    goto disallow_got32;
+
+		  /* Add the GOT base if there is no base register.  */
+		  relocation += (gotplt->output_section->vma
+				 + gotplt->output_offset);
+		}
+	      else if (htab->elf.splt == NULL)
+		{
 		  /* Adjust for static executables.  */
-		  if (htab->elf.splt == NULL)
-		    relocation += gotplt->output_offset;
+		  relocation += gotplt->output_offset;
 		}
 
 	      goto do_relocation;
@@ -4131,6 +4137,7 @@ r_386_got32:
 		     is.  */
 		  const char *name;
 
+disallow_got32:
 		  if (h == NULL)
 		    name = bfd_elf_sym_name (input_bfd, symtab_hdr, sym,
 					     NULL);
@@ -4138,8 +4145,8 @@ r_386_got32:
 		    name = h->root.root.string;
 
 		  (*_bfd_error_handler)
-		    (_("%B: direct GOT relocation R_386_GOT32 against `%s' without base register can not be used when making a shared object"),
-		     input_bfd, name);
+		    (_("%B: direct GOT relocation %s against `%s' without base register can not be used when making a shared object"),
+		     input_bfd, howto->name, name);
 		  bfd_set_error (bfd_error_bad_value);
 		  return FALSE;
 		}
