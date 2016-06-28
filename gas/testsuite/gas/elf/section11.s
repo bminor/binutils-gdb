d48796b5054fa2a9165bf3b3f27cc1cdfe8933a1
Alan Modra <amodra@gmail.com>
2016-06-28 19:03:59 +0930

    Fix new testcase for hppa64
    Anything in first column is a label on hppa64.
    
    	PR gas/20247
    	* testsuite/gas/elf/section11.s: Don't start directives in first column.

--- gas/testsuite/gas/elf/section11.s
+++ gas/testsuite/gas/elf/section11.s
@@ -1,14 +1,11 @@
-.section .bss
-.balign 16
-.skip 1
+ .section .bss
+ .balign 16
+ .skip 1
 
+ .data
+ .balign 16
+ .skip 1
 
-.data
-.balign 16
-.skip 1
-
-
-.text
-.balign 16
-.skip 1
-
+ .text
+ .balign 16
+ .skip 1
