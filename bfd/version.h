8c3a2e88e074b9de45df364a23233fe80e1156e7
Ryan S. Arnold <ryan.arnold@linaro.org>
2016-07-01 17:10:27 -0500

    bfd/version.h: Increment Linaro version to 2016_02 to reflect commits in 2016.01

--- bfd/version.h
+++ bfd/version.h
@@ -4,5 +4,5 @@
    and we need to be very careful to not break them.  So far it seems that
    appending Linaro-specific version at the end is fine so long as it doesn't
    contain any dots ".".  */
-#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@ " Linaro 2015_10"
+#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@ " Linaro 2016_02"
 #define REPORT_BUGS_TO @report_bugs_to@
