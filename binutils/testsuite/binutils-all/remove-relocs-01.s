003c15c9fdfc1ef7038ed3306ae166e60663e7ca
Alan Modra <amodra@gmail.com>
2016-07-15 16:33:53 +0930

    Use address sized relocs in remove-relocs-01.s
    Fixes failures on alpha, ia64, mcore, metag, moxie, and pj due to lack
    of 16-bit relocs.
    
    	* testsuite/binutils-all/remove-relocs-01.s: Use .dc.a, not .word.

--- binutils/testsuite/binutils-all/remove-relocs-01.s
+++ binutils/testsuite/binutils-all/remove-relocs-01.s
@@ -1,19 +1,19 @@
         .section ".data.relocs.01", "aw"
-        .word   rel_01_01
-        .word   rel_01_02
-        .word   rel_01_03
+        .dc.a   rel_01_01
+        .dc.a   rel_01_02
+        .dc.a   rel_01_03
 
         .section ".data.relocs.02", "aw"
-        .word   rel_02_01
-        .word   rel_02_02
-        .word   rel_02_03
+        .dc.a   rel_02_01
+        .dc.a   rel_02_02
+        .dc.a   rel_02_03
 
         .section ".data.relocs.03", "aw"
-        .word   rel_03_01
-        .word   rel_03_02
-        .word   rel_03_03
+        .dc.a   rel_03_01
+        .dc.a   rel_03_02
+        .dc.a   rel_03_03
 
         .section ".data.01", "aw"
-        .word   0x1
-        .word   0x2
-        .word   0x3
+        .dc.a   0x1
+        .dc.a   0x2
+        .dc.a   0x3
