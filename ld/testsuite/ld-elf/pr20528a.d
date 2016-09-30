80169c8a23d9bd3f213028e3d44239b03d0aedf7
Alan Modra <amodra@gmail.com>
2016-09-30 16:09:42 +0930

    Fix pr20528 testsuite
    	PR ld/20528
    	* testsuite/ld-elf/pr20528a.d: xfail generic elf targets.  Allow
    	multiple .text sections for hppa-linux.
    	* testsuite/ld-elf/pr20528b.d: Likewise.

--- ld/testsuite/ld-elf/pr20528a.d
+++ ld/testsuite/ld-elf/pr20528a.d
@@ -2,8 +2,11 @@
 #source: pr20528b.s
 #ld: -r
 #readelf: -S --wide
+#xfail: d30v-*-* dlx-*-* fr30-*-* frv-*-elf ft32-*-* i860-*-* i960-*-*
+#xfail: iq*-*-* mn10200-*-* moxie-*-* msp*-*-* mt-*-* pj*-*-*
 
 #...
 [ 	]*\[.*\][ 	]+\.text.startup[ 	]+PROGBITS.*[ 	]+AX[   ]+.*
+#...
 [ 	]*\[.*\][ 	]+\.text.startup[ 	]+PROGBITS.*[ 	]+AXE[   ]+.*
 #pass
