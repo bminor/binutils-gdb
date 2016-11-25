8ce9efb079b6e7b3ab2e795db9477656375d7204
Simon Marchi <simon.marchi@ericsson.com>
2016-11-25 09:41:58 -0500

    Fix typos in comment
    gdb/ChangeLog:
    
    	* record-full.c (record_full_resume): Fix typos in comment.

--- gdb/record-full.c
+++ gdb/record-full.c
@@ -969,7 +969,7 @@ record_full_resume (struct target_ops *ops, ptid_t ptid, int step,
             }
           else
             {
-              /* This arch support soft sigle step.  */
+              /* This arch supports soft single step.  */
               if (thread_has_single_step_breakpoints_set (inferior_thread ()))
                 {
                   /* This is a soft single step.  */
