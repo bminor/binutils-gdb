b8760d2cb51517789110c7b2dbd8f61dce08291d
Maciej W. Rozycki <macro@imgtec.com>
2016-12-07 02:27:57 +0000

    MIPS/include: opcode/mips.h: Add a comment for ASE_DSPR3
    Complement commit 8f4f9071ad5f ("Add MIPS32 DSPr3 support.").
    
    	include/
    	* opcode/mips.h (ASE_DSPR3): Add a comment.

--- include/opcode/mips.h
+++ include/opcode/mips.h
@@ -1260,6 +1260,7 @@ static const unsigned int mips_isa_table[] = {
 #define ASE_MSA64		0x00001000
 /* eXtended Physical Address (XPA) Extension.  */
 #define ASE_XPA			0x00002000
+/* DSP R3 Module.  */
 #define ASE_DSPR3		0x00004000
 
 /* MIPS ISA defines, use instead of hardcoding ISA level.  */
