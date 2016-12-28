b733bcb7f58c42e0e0d94a3f266a4193030e5f3a
Cary Coutant <ccoutant@gmail.com>
2016-12-27 20:50:47 -0800

    Place copy relocations in .dynbss when target is read-only.
    gold/
    	PR ld/20995
    	* copy-relocs.cc (Copy_relocs::make_copy_reloc): Use .dynbss for
    	read-only data when linking with -z relro.
    	* copy-relocs.h (Copy_relocs::dynrelro_): New data member.
    	* testsuite/Makefile.am (copy_test_relro): New test case.
    	* testsuite/Makefile.in: Regenerate.
    	* testsuite/copy_test_relro.cc: New source file.
    	* testsuite/copy_test_relro_1.cc: New source file.

---
+++ gold/testsuite/copy_test_relro.cc
@@ -0,0 +1,45 @@
+// copy_test_relro.cc -- test copy relocs against read-only and relro symbols.
+
+// Copyright (C) 2016 Free Software Foundation, Inc.
+// Written by Cary Coutant <ccoutant@gmail.com>.
+
+// This file is part of gold.
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
+// MA 02110-1301, USA.
+
+#include <cassert>
+#include <stdint.h>
+
+extern char* _etext;
+extern char* __data_start;
+extern char* _edata;
+extern char* _end;
+
+extern int* const p;
+extern const int b[];
+int a = 123;
+
+int main()
+{
+  assert(*p == 123);
+  assert(b[0] == 100);
+  assert(b[1] == 200);
+  assert(b[2] == 300);
+  assert(b[3] == 400);
+  assert(reinterpret_cast<const void*>(&p) < reinterpret_cast<void*>(&__data_start));
+  assert(reinterpret_cast<const void*>(b) < reinterpret_cast<void*>(&__data_start));
+  return 0;
+}
