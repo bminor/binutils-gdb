9d736fbf01d20bc03804fa0cb49d99fdf6628fab
Jonah Graham <jonah@kichwacoders.com>
2017-03-22 11:19:44 +0000

    Import sys in gdb/python/lib/gdb/printer/bound_registers.py
    Pick up missing bits from the patch merged in.
    
    2017-03-22  Jonah Graham  <jonah@kichwacoders.com>
    
    	PR gdb/19637
    	* python/lib/gdb/printer/bound_registers.py: Import sys.

--- gdb/python/lib/gdb/printer/bound_registers.py
+++ gdb/python/lib/gdb/printer/bound_registers.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+
 import gdb.printing
 
 if sys.version_info[0] > 2:
