4a596fe20e93109300e841a3d0ee5f198146ab6d
Sergio Durigan Junior <sergiodj@redhat.com>
2017-04-05 12:28:30 -0400

    Fix PR 21352: Command tsave does not support -r argument
    This is an obvious fix for PR 21352.  The problem is that the argument
    parsing loop is not using an "else if" where it should, and therefore
    the '-r' option ends up unrecognized by GDB.
    
    gdb/ChangeLog:
    2017-04-05  Sergio Durigan Junior  <sergiodj@redhat.com>
    
    	PR gdb/21352
    	* tracefile.c (tsave_command): Fix argument parsing for '-r'
    	option.

--- gdb/tracefile.c
+++ gdb/tracefile.c
@@ -325,7 +325,7 @@ tsave_command (char *args, int from_tty)
     {
       if (strcmp (*argv, "-r") == 0)
 	target_does_save = 1;
-      if (strcmp (*argv, "-ctf") == 0)
+      else if (strcmp (*argv, "-ctf") == 0)
 	generate_ctf = 1;
       else if (**argv == '-')
 	error (_("unknown option `%s'"), *argv);
