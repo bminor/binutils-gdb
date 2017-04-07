b24b0d6c3be6b95d4b5e8da901e1ef315bd458b4
Tom Tromey <tom@tromey.com>
2017-04-06 21:07:00 -0600

    Fix up wchar_iterator comment
    This fixes up a comment in charset.h that has been obsolete for a
    while.
    
    gdb/ChangeLog
    2017-04-12  Tom Tromey  <tom@tromey.com>
    
    	* charset.h (wchar_iterator): Fix comment.

--- gdb/charset.h
+++ gdb/charset.h
@@ -93,10 +93,8 @@ class wchar_iterator
      buffer.  CHARSET is the name of the character set in which INPUT is
      encoded.  WIDTH is the number of bytes in a base character of
      CHARSET.
-   
-     This function either returns a new character set iterator, or calls
-     error.  The result can be freed using a cleanup; see
-     make_cleanup_wchar_iterator.  */
+
+     This constructor can throw on error.  */
   wchar_iterator (const gdb_byte *input, size_t bytes, const char *charset,
 		  size_t width);
 
