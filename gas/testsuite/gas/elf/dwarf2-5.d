71a616abae90970e221b78ad78e631c3af58ae5a
Alexandre Oliva <aoliva@redhat.com>
2017-04-27 03:59:58 -0300

    LVU: add gas testcases for view numbering

---
+++ gas/testsuite/gas/elf/dwarf2-5.d
@@ -0,0 +1,24 @@
+#as:
+#readelf: -x.rodata -wL
+#name: DWARF2 5
+
+Hex dump of section '\.rodata':
+  0x00000000 01010201 010203 *.*
+
+Decoded dump of debug contents of section \.debug_line:
+
+CU: dwarf2-5\.c:
+File name  *Line number  *Starting address  *View
+dwarf2-5\.c  *1  *0
+dwarf2-5\.c  *2  *0  *1
+dwarf2-5\.c  *3  *0x4
+dwarf2-5\.c  *4  *0x4  *1
+dwarf2-5\.c  *5  *0x4  *2
+dwarf2-5\.c  *6  *0x8
+dwarf2-5\.c  *7  *0xc
+dwarf2-5\.c  *8  *0xc  *1
+dwarf2-5\.c  *9  *0x10
+dwarf2-5\.c  *10  *0x10  *1
+dwarf2-5\.c  *11  *0x10  *2
+dwarf2-5\.c  *12  *0x10  *3
+dwarf2-5\.c  *12  *0x14
---
+++ gas/testsuite/gas/elf/dwarf2-6.d
@@ -0,0 +1,19 @@
+#as:
+#readelf: -wL
+#name: DWARF2 6
+
+Decoded dump of debug contents of section \.debug_line:
+
+CU: dwarf2-6\.c:
+File name  *Line number  *Starting address  *View
+dwarf2-6\.c  *1  *0
+dwarf2-6\.c  *1  *0  *1
+dwarf2-6\.c  *1  *0x1
+dwarf2-6\.c  *1  *0x1  *1
+dwarf2-6\.c  *1  *0x2
+dwarf2-6\.c  *1  *0x3  *1
+dwarf2-6\.c  *2  *0x3  *2
+dwarf2-6\.c  *3  *0x4
+dwarf2-6\.c  *3  *0x4  *1
+dwarf2-6\.c  *3  *0xf3
+dwarf2-6\.c  *3  *0x100
