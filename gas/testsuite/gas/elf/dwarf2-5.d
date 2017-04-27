5b5e6e3f9a074c9f8a76a1cbe2882b3ae4951bbc
Alexandre Oliva <aoliva@redhat.com>
2017-04-27 04:48:15 -0300

    LVU: add raw line number dumps to loc view tests

--- gas/testsuite/gas/elf/dwarf2-5.d
+++ gas/testsuite/gas/elf/dwarf2-5.d
@@ -1,9 +1,59 @@
 #as:
-#readelf: -x.rodata -wL
+#readelf: -x.rodata -wlL
 #name: DWARF2 5
 
 Hex dump of section '\.rodata':
   0x00000000 01010201 010203 *.*
+
+Raw dump of debug contents of section .debug_line:
+
+  Offset:                      0x0
+  Length:                      [0-9]*
+  DWARF Version:               2
+  Prologue Length:             [0-9]*
+  Minimum Instruction Length:  [0-9]*
+  Initial value of 'is_stmt':  1
+  Line Base:                   [-0-9]*
+  Line Range:                  [0-9]*
+  Opcode Base:                 [0-9]*
+
+ Opcodes:
+  Opcode 1 has 0 args
+  Opcode 2 has 1 args
+  Opcode 3 has 1 args
+  Opcode 4 has 1 args
+  Opcode 5 has 1 args
+  Opcode 6 has 0 args
+  Opcode 7 has 0 args
+  Opcode 8 has 0 args
+  Opcode 9 has 1 args
+  Opcode 10 has 0 args
+  Opcode 11 has 0 args
+  Opcode 12 has 1 args
+
+ The Directory Table is empty.
+
+ The File Name Table \(offset 0x1c\):
+  Entry	Dir	Time	Size	Name
+  1	0	0	0	dwarf2-5\.c
+
+ Line Number Statements:
+  \[0x.*\]  Extended opcode 2: set Address to 0x0
+  \[0x.*\]  Copy
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 0 to 0x0 and Line by 1 to 2 \(view 1\)
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 4 to 0x4 and Line by 1 to 3
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 0 to 0x4 and Line by 1 to 4 \(view 1\)
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 0 to 0x4 and Line by 1 to 5 \(view 2\)
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 4 to 0x8 and Line by 1 to 6
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 4 to 0xc and Line by 1 to 7
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 0 to 0xc and Line by 1 to 8 \(view 1\)
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 4 to 0x10 and Line by 1 to 9
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 0 to 0x10 and Line by 1 to 10 \(view 1\)
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 0 to 0x10 and Line by 1 to 11 \(view 2\)
+  \[0x.*\]  Special opcode [0-9]*: advance Address by 0 to 0x10 and Line by 1 to 12 \(view 3\)
+  \[0x.*\]  Advance PC by 4 to 0x14
+  \[0x.*\]  Extended opcode 1: End of Sequence
+
 
 Decoded dump of debug contents of section \.debug_line:
 
