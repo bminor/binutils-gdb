ea480a306d46efe3dd1839618137f0e73a80e9b3
Simon Marchi <simon.marchi@polymtl.ca>
2017-05-02 13:30:07 -0400

    Change field separator in gdbarch.sh
    The fields in the description of the gdbarch interface are separated
    using colons.  That becomes a problem if we want to use things like
    std::vector in it. This patch changes the field separator to use
    semicolons instead.
    
    I think there's very little chance we'll ever want to use a semicolon in
    one of the fields, but if you think another character would be more
    appropriate, let me know.
    
    gdb/ChangeLog:
    
    	* gdbarch.sh: Use semi-colon as field separator instead of colon.
    	* gdbarch.h: Re-generate.

--- gdb/gdbarch.h
+++ gdb/gdbarch.h
@@ -129,7 +129,7 @@ extern void set_gdbarch_bits_big_endian (struct gdbarch *gdbarch, int bits_big_e
 
 /* Number of bits in a char or unsigned char for the target machine.
    Just like CHAR_BIT in <limits.h> but describes the target machine.
-   v:TARGET_CHAR_BIT:int:char_bit::::8 * sizeof (char):8::0:
+   v;TARGET_CHAR_BIT;int;char_bit;;;;8 * sizeof (char);8;;0;
   
    Number of bits in a short or unsigned short for the target machine. */
 
