e645cf40b111daef4518a58547de577eb9379ccb
Andrew Goedhart <Andrewgoedhart@simplepowersolutions.co.za>
2017-05-09 12:14:48 +0100

    Fix resolution of R_ARM_THM_ALU_PREL_11_0 relocation against Thumb symbols.
    	PR ld/21458
    	* elf32-arm.c (elf32_arm_final_link_relocate): Set the bottom bit
    	of the value when resolving a R_ARM_THM_ALU_PREL_11_0 relocation
    	and the destination is a Thumb symbol.

--- bfd/elf32-arm.c
+++ bfd/elf32-arm.c
@@ -10511,6 +10511,10 @@ elf32_arm_final_link_relocate (reloc_howto_type *           howto,
 	if (value >= 0x1000)
 	  return bfd_reloc_overflow;
 
+	/* Destination is Thumb.  Force bit 0 to 1 to reflect this.  */
+	if (branch_type == ST_BRANCH_TO_THUMB)
+	  value |= 1;
+
 	insn = (insn & 0xfb0f8f00) | (value & 0xff)
 	     | ((value & 0x700) << 4)
 	     | ((value & 0x800) << 15);
