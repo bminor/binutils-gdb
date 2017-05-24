8c8f9122ce6e6c00f72914479af57a7b87244554
Yao Qi <yao.qi@linaro.org>
2017-05-24 22:15:23 +0100

    Restrict i387_convert_register_p
    gdb:
    
    2017-05-24  Yao Qi  <yao.qi@linaro.org>
    
    	* i387-tdep.c (i387_convert_register_p): Return false if type
    	code isn't TYPE_CODE_FLT.

--- gdb/i387-tdep.c
+++ gdb/i387-tdep.c
@@ -341,8 +341,9 @@ i387_convert_register_p (struct gdbarch *gdbarch, int regnum,
   if (i386_fp_regnum_p (gdbarch, regnum))
     {
       /* Floating point registers must be converted unless we are
-	 accessing them in their hardware type.  */
-      if (type == i387_ext_type (gdbarch))
+	 accessing them in their hardware type or TYPE is not float.  */
+      if (type == i387_ext_type (gdbarch)
+	  || TYPE_CODE (type) != TYPE_CODE_FLT)
 	return 0;
       else
 	return 1;
