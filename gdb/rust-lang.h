888e3ddb20f541220f18709fce832ad0163c3e71
Tom Tromey <tom@tromey.com>
2017-06-06 14:27:42 -0600

    Add missing "extern" in rust-lang.h
    I noticed that one function in rust-lang.h was not declared using
    "extern".  In the interested of uniformity, this patch adds it.
    
    Tested by rebuilding.
    
    2017-10-02  Tom Tromey  <tom@tromey.com>
    
    	* rust-lang.h (rust_slice_type): Add "extern".

--- gdb/rust-lang.h
+++ gdb/rust-lang.h
@@ -43,7 +43,7 @@ extern std::string rust_crate_for_block (const struct block *block);
    is the type of the elements of the slice.  USIZE_TYPE is the Rust
    "usize" type to use.  The new type is allocated whereever ELT_TYPE
    is allocated.  */
-struct type *rust_slice_type (const char *name, struct type *elt_type,
-			      struct type *usize_type);
+extern struct type *rust_slice_type (const char *name, struct type *elt_type,
+				     struct type *usize_type);
 
 #endif /* RUST_LANG_H */
