da614360f520b84a9c87506eb0c880f7a056468b
H.J. Lu <hjl.tools@gmail.com>
2017-06-13 12:03:40 -0700

    ld: Add tests for .startof.SECNAME/.sizeof.SECNAME
    	* testsuite/ld-elf/sizeof.d: Renamed to ...
    	* testsuite/ld-elf/sizeofa.d: This.  Updated.
    	* testsuite/ld-elf/startof.d: Renamed to ...
    	* testsuite/ld-elf/startofa.d: This.  Updated.
    	* testsuite/ld-elf/sizeofb.d: New file.
    	* testsuite/ld-elf/startofb.d: Likewise.

--- ld/testsuite/ld-elf/sizeofa.d
+++ ld/testsuite/ld-elf/sizeofa.d
@@ -1,3 +1,4 @@
+#source: sizeof.s
 #ld: -Ur
 #readelf: -sW
 
@@ -5,7 +6,7 @@ Symbol table '\.symtab' contains [0-9]+ entries:
  +Num: +Value +Size Type +Bind +Vis +Ndx Name
  +0: 0+ +0 +NOTYPE +LOCAL +DEFAULT +UND +
 #...
- +[0-9]+: +[a-f0-9]+ +0 +NOTYPE +GLOBAL +DEFAULT +UND +.startof.scnfoo
+ +[0-9]+: +[a-f0-9]+ +0 +NOTYPE +GLOBAL +DEFAULT +[0-9]+ +__stop_scnfoo
 #...
- +[0-9]+: +[a-f0-9]+ +0 +NOTYPE +GLOBAL +DEFAULT +[0-9]+ +__start_scnfoo
+ +[0-9]+: +[a-f0-9]+ +0 +NOTYPE +GLOBAL +DEFAULT +UND +.sizeof.scnfoo
 #pass
