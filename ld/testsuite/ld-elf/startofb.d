b6947a7f08011375120186688bf2efd866f7d148
H.J. Lu <hjl.tools@gmail.com>
2017-06-14 05:49:02 -0700

    Skip sizeof/startof tests on targets with leading char
    Symbol lookup in linker will always fail on targets with leading char
    in symbol name since __start_SECNAME and __stop_SECNAME in C may be
    ___start_SECNAME and ___stop_SECNAME in assembly.
    
    	* testsuite/ld-elf/sizeofa.d: Skip on targets with leading char
    	in symbol name.
    	* testsuite/ld-elf/sizeofb.d: Likewise.
    	* testsuite/ld-elf/startofa.d: Likewise.
    	* testsuite/ld-elf/startofb.d: Likewise.

--- ld/testsuite/ld-elf/startofb.d
+++ ld/testsuite/ld-elf/startofb.d
@@ -2,6 +2,10 @@
 #ld: -shared
 #readelf: -sW
 #target: *-*-linux* *-*-gnu*
+#notarget: bfin-*-* cr16-*-* cr16c-*-* cris*-*-* crx-*-* epiphany-*-*
+#notarget: h8300-*-* ip2k-*-* m10200-*-* m10300-*-* m32c-*-* metag-*-*
+#notarget: metag-*-* rl78-*-* rx-*-* sh*-*-* v850*-*-*
+# Targets with a leading char will faill this test.
 
 Symbol table '\.dynsym' contains [0-9]+ entries:
  +Num: +Value +Size Type +Bind +Vis +Ndx Name
