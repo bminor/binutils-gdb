4d36230d59903b92fbe2b53b31ed64a884860f0e
H.J. Lu <hjl.tools@gmail.com>
2017-08-01 05:53:27 -0700

    x86: Update segment register check in Intel syntax
    https://sourceware.org/ml/binutils/2009-04/msg00223.html
    
    introduced a new Intel syntax parser which accepts
    
    	mov	eax, fs:gs:[eax]
    
    It ignores anything between ':'s after fs and treats
    
    	mov	eax, DWORD PTR fs:foobar:16
    	mov	eax, DWORD PTR fs:foobar:barfoo:16
    	mov	eax, DWORD PTR fs:ds:16
    	mov	eax, DWORD PTR fs:ds:cs:16
    
    as
    
    	mov	eax, DWORD PTR fs:16
    
    This patch updates segment register check and only allows a single ':'.
    
    	PR gas/21874
    	* config/tc-i386-intel.c (i386_intel_operand): Update segment
    	register check.
    	* testsuite/gas/i386/intelok.s: Replace "fs:gs:[eax]" with
    	"fs:[eax]".
    	* testsuite/gas/i386/inval-seg.s: Add tests for invalid segment
    	register.
    	* testsuite/gas/i386/x86-64-inval-seg.s: Likewise.
    	* testsuite/gas/i386/inval-seg.l: Updated.
    	* testsuite/gas/i386/x86-64-inval-seg.l: Likewise.

--- gas/testsuite/gas/i386/x86-64-inval-seg.s
+++ gas/testsuite/gas/i386/x86-64-inval-seg.s
@@ -4,3 +4,9 @@
 	movl	%ds,(%rax)
 	movq	(%rax),%ds
 	movl	(%rax),%ds
+
+	.intel_syntax noprefix
+	mov	eax, DWORD PTR fs:foobar:16
+	mov	eax, DWORD PTR fs:foobar:barfoo:16
+	mov	eax, DWORD PTR fs:ds:16
+	mov	eax, DWORD PTR fs:ds:cs:16
