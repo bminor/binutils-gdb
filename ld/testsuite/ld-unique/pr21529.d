55ef6584ca8466e293e9f1b310c0bf44500c11b6
Alan Modra <amodra@gmail.com>
2017-08-05 23:28:21 +0930

    Don't fail in elf32_hppa_set_gp
    The base bfd_link_hash_table works fine here, the only thing to watch
    out for is to only set elf_gp if the output is ELF.
    
    bfd/
    	* elf32-hppa.c (elf32_hppa_set_gp): Don't require an
    	hppa_link_hash_table.
    ld/
    	* testsuite/ld-unique/pr21529.d: Don't xfail hppa.

--- ld/testsuite/ld-unique/pr21529.d
+++ ld/testsuite/ld-unique/pr21529.d
@@ -1,6 +1,6 @@
 #ld: --oformat binary -T pr21529.ld -e main
 #objdump: -s -b binary
-#notarget: aarch64*-*-* arm*-*-* avr-*-* hppa-*-* ia64-*-* m68hc1*-*-* nds32*-*-* score-*-*
+#notarget: aarch64*-*-* arm*-*-* avr-*-* ia64-*-* m68hc1*-*-* nds32*-*-* score-*-*
 # Skip targets which can't change output format to binary.
 
 #pass
