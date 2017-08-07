2760f24c4942853eac7b921e4b8843d57a602654
Romain Geissler <romain.geissler@gmail.com>
2017-08-08 07:25:39 +0930

    Add configure flag to enable gnu hash style by default.
    ld/
    	* configure.ac: Add --enable-default-hash-style option.
    	* ldmain.c (main): Set link_info.emit_hash to DEFAULT_EMIT_SYSV_HASH.
    	Set link_info.emit_gnu_hash to DEFAULT_EMIT_GNU_HASH.
    	* configure: Regenerate.
    	* config.in: Regenerate.
    
    gold/
    	* configure.ac: Add --enable-default-hash-style option.
    	* options.h (hash_style): Use DEFAULT_HASH_STYLE as default value.
    	* configure: Regenerate.
    	* config.in: Regenerate.

--- ld/ldmain.c
+++ ld/ldmain.c
@@ -273,7 +273,8 @@ main (int argc, char **argv)
   link_info.keep_memory = TRUE;
   link_info.combreloc = TRUE;
   link_info.strip_discarded = TRUE;
-  link_info.emit_hash = TRUE;
+  link_info.emit_hash = DEFAULT_EMIT_SYSV_HASH;
+  link_info.emit_gnu_hash = DEFAULT_EMIT_GNU_HASH;
   link_info.callbacks = &link_callbacks;
   link_info.input_bfds_tail = &link_info.input_bfds;
   /* SVR4 linkers seem to set DT_INIT and DT_FINI based on magic _init
