c3d7b541fa920e6ecb0f06ffe1e0e3f070fa295d
Simon Marchi <simon.marchi@ericsson.com>
2017-09-16 14:16:27 +0200

    Add unit test for xml_escape_text
    The following patch modifies xml_escape_text, so I took the opportunity
    to write a unit test for it.
    
    gdb/ChangeLog:
    
    	* Makefile.in (SUBDIR_UNITTESTS_SRCS): Add new source file.
    	(SUBDIR_UNITTESTS_OBS): Add new object file.
    	* unittests/xml-utils-selftests.c: New file.

---
+++ gdb/unittests/xml-utils-selftests.c
@@ -0,0 +1,46 @@
+/* Unit tests for the xml-utils.c file.
+
+   Copyright (C) 2017 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "defs.h"
+#include "xml-utils.h"
+#include "selftest.h"
+
+namespace selftests {
+namespace xml_utils {
+
+static void test_xml_escape_text ()
+{
+  const char *input = "<this isn't=\"xml\"> &";
+  const char *expected_output = "&lt;this isn&apos;t=&quot;xml&quot;&gt; &amp;";
+  char *actual_output = xml_escape_text (input);
+
+  SELF_CHECK (strcmp (actual_output, expected_output) == 0);
+
+  xfree (actual_output);
+}
+
+}
+}
+
+void
+_initialize_xml_utils ()
+{
+  selftests::register_test ("xml_escape_text",
+			    selftests::xml_utils::test_xml_escape_text);
+}
