5007d765ae09c10c7f3b18bb16841b9d2d59e181
Matthias Klose <doko@ubuntu.com>
2017-09-26 16:23:19 +0100

    Allow linking GDB with ncursesw
    Triggered by https://launchpad.net/bugs/1275210, to be able to cope
    with UTF-8 characters in gdbtui.
    
    Reference:
      https://sourceware.org/ml/gdb-patches/2017-09/msg00356.html
    
    gdb/ChangeLog:
    2017-09-26  Matthias Klose  <doko@ubuntu.com>
    
    	* configure.ac: Search ncursesw before ncurses.
    	Check ncursesw/ncurses.h before ncurses/ncurses.h.
    	* gdb_curses.h: Include <ncursesw/ncurses.h>
    	* config.in, configure: Regenerate.

--- gdb/gdb_curses.h
+++ gdb/gdb_curses.h
@@ -39,7 +39,9 @@
 #define NOMACROS
 #define NCURSES_NOMACROS
 
-#if defined (HAVE_NCURSES_NCURSES_H)
+#if defined (HAVE_NCURSESW_NCURSES_H)
+#include <ncursesw/ncurses.h>
+#elif defined (HAVE_NCURSES_NCURSES_H)
 #include <ncurses/ncurses.h>
 #elif defined (HAVE_NCURSES_H)
 #include <ncurses.h>
