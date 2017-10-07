2458d83bbc190f92f7718b30438dd7e5775e0d3c
H.J. Lu <hjl.tools@gmail.com>
2017-10-07 16:10:56 -0700

    ld-elf/merge.d: Don't expect to fail for sparc
    It passed with cross binutils to sparc-linux and sparc64-linux.
    
    	* testsuite/ld-elf/merge.d: Don't expect to fail for sparc.

--- ld/testsuite/ld-elf/merge.d
+++ ld/testsuite/ld-elf/merge.d
@@ -5,7 +5,7 @@
 #xfail: "dlx-*-*" "fr30-*-*" "frv-*-*" "hppa*64*-*-*" "h8300-*-*" "score-*-*"
 #xfail: "i370-*-*" "i860-*-*" "i960-*-*" "ip2k-*-*" "iq2000-*-*" "lm32-*-*"
 #xfail: "mcore-*-*" "mn102*-*-*" "ms1-*-*" "mep-*-*" "m68hc11-*-*" "nios2-*-*"
-#xfail: "or32-*-*" "pj-*-*" "sparc*-*-*" "tic6x-*-*" "vax-*-*" "xstormy16-*-*"
+#xfail: "or32-*-*" "pj-*-*" "tic6x-*-*" "vax-*-*" "xstormy16-*-*"
 #xfail: "xtensa*-*-*" "metag-*-*" "ft32-*-*" "pru-*-*"
 
 .*:     file format .*elf.*
