a60fff76279225bcb2eeaeb9f89b02bba8d3a126
Alan Modra <amodra@gmail.com>
2017-10-30 12:02:42 +1030

    frv-elf --gc-sections failures
    git commit 81742b83e9 exposed an frv-elf bug, with the object id not
    matching the hash table id.
    
    	* elf32-frv.c (ELF_TARGET_ID): Don't define for generic
    	elf target.

--- bfd/elf32-frv.c
+++ bfd/elf32-frv.c
@@ -6773,7 +6773,6 @@ elf32_frv_grok_psinfo (bfd *abfd, Elf_Internal_Note *note)
   return TRUE;
 }
 #define ELF_ARCH		bfd_arch_frv
-#define ELF_TARGET_ID		FRV_ELF_DATA
 #define ELF_MACHINE_CODE	EM_CYGNUS_FRV
 #define ELF_MAXPAGESIZE		0x1000
 
@@ -6812,6 +6811,8 @@ elf32_frv_grok_psinfo (bfd *abfd, Elf_Internal_Note *note)
 
 #include "elf32-target.h"
 
+#undef ELF_TARGET_ID
+#define ELF_TARGET_ID		FRV_ELF_DATA
 #undef ELF_MAXPAGESIZE
 #define ELF_MAXPAGESIZE		0x4000
 
