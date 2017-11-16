8c8cad3aa839747e368cc5db58331756a2b70e8e
H.J. Lu <hjl.tools@gmail.com>
2017-11-16 02:50:33 -0800

    i386: Replace .code64/.code32 with .byte
    Since .code64 directive isn't available for 32-bit BFD and ELF directive
    isn't available for non-ELF directive, we should avoid them.
    
    	* testsuite/gas/i386/noextreg.s: Replace .code64/.code32 and
    	64-bit instructions with .byte.  Remove ELF directive.

--- gas/testsuite/gas/i386/noextreg.s
+++ gas/testsuite/gas/i386/noextreg.s
@@ -2,24 +2,19 @@
 	.text
 ix86:
 	andn	eax, eax, [eax]
-	.code64
-	andn	rax, rax, [rax]
-	.code32
+	# andn rax, rax, [rax]
+	.byte	0xc4, 0xe2, 0xf8, 0xf2, 0x00
 
 	tzmsk	eax, [eax]
-	.code64
-	tzmsk	rax, [rax]
-	.code32
+	# tzmsk rax, [rax]
+	.byte	0x8f, 0xe9, 0xf8, 0x01, 0x20
 
 	llwpcb	eax
-	.code64
-	llwpcb	rax
-	.code32
+	# llwpcb rax
+	.byte	0x8f, 0xe9, 0xf8, 0x12, 0xc0
 
 	vfmaddps xmm0, xmm0, [eax], xmm0
-	.byte	0xc4, 0xe3, 0x79, 0x68, 0x00, 0x0f	# vfmaddps xmm0, xmm0, [eax], xmm0
+	# vfmaddps xmm0, xmm0, [eax], xmm0
+	.byte	0xc4, 0xe3, 0x79, 0x68, 0x00, 0x0f
 
 	ret
-
-	.type ix86, @function
-	.size ix86, . - ix86
