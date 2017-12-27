b67d92b06e6155a392a0c2d413f87d050880dbf4
Stafford Horne <shorne@gmail.com>
2017-12-27 23:36:30 +0900

    reggroups: Add test and docs for `info reg $reggroup` feature
    Until now this feature has existed but was not documented.  Adding docs
    and tests.
    
    gdb/ChangeLog:
    
    yyyy-mm-dd  Stafford Horne  <shorne@gmail.com>
    
    	* infcmd.c (_initialize_infcmd): Add help for info reg $reggroup
    	and info all-registers $reggroup feature.
    
    gdb/doc/ChangeLog:
    
    yyyy-mm-dd  Stafford Horne  <shorne@gmail.com>
    
    	* gdb.texinfo (Registers): Document info reg $reggroup feature.
    
    gdb/testsuite/ChangeLog:
    
    yyyy-mm-dd  Stafford Horne  <shorne@gmail.com>
    
    	* gdb.base/reggroups.c: New file.
    	* gdb.base/reggroups.exp: New file.

--- gdb/infcmd.c
+++ gdb/infcmd.c
@@ -3460,13 +3460,17 @@ interrupt all running threads in non-stop mode, use the -a option."));
 
   c = add_info ("registers", info_registers_command, _("\
 List of integer registers and their contents, for selected stack frame.\n\
-Register name as argument means describe only that register."));
+One or more register names as argument means describe the given registers.\n\
+One or more register group names as argument means describe the registers\n\
+in the named register groups."));
   add_info_alias ("r", "registers", 1);
   set_cmd_completer (c, reg_or_group_completer);
 
   c = add_info ("all-registers", info_all_registers_command, _("\
 List of all registers and their contents, for selected stack frame.\n\
-Register name as argument means describe only that register."));
+One or more register names as argument means describe the given registers.\n\
+One or more register group names as argument means describe the registers\n\
+in the named register groups."));
   set_cmd_completer (c, reg_or_group_completer);
 
   add_info ("program", info_program_command,
