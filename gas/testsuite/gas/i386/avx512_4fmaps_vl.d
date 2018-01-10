cbda583ada32e16bf8b6c6aff730cc9eab63b364
Jan Beulich <jbeulich@novell.com>
2018-01-10 14:53:43 +0100

    x86: fix Disp8 handling for scalar AVX512_4FMAPS insns
    Just like their packed counterparts the memory operand is always 16
    bytes wide, and the Disp8 scaling is the same for all of them. (As a
    side note: I'm also surprised by there being AVX512VL variants of
    these as well as the AVX512_4VNNIW ones - the SDM doesn't define any
    such.)
    
    Adjust the test cases also for the packed forms to actually live up to
    their promise of testing correct Disp8 encoding.

--- gas/testsuite/gas/i386/avx512_4fmaps_vl.d
+++ gas/testsuite/gas/i386/avx512_4fmaps_vl.d
@@ -12,67 +12,68 @@ Disassembly of section \.text:
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 0f 9a 09[ 	]*v4fmaddps \(%ecx\),%xmm4,%xmm1\{%k7\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 8f 9a 09[ 	]*v4fmaddps \(%ecx\),%xmm4,%xmm1\{%k7\}\{z\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8c f4 c0 1d fe ff[ 	]*v4fmaddps -0x1e240\(%esp,%esi,8\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a e0 0f 00 00[ 	]*v4fmaddps 0xfe0\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a 00 10 00 00[ 	]*v4fmaddps 0x1000\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a 00 f0 ff ff[ 	]*v4fmaddps -0x1000\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a e0 ef ff ff[ 	]*v4fmaddps -0x1020\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 4a 7f[ 	]*v4fmaddps 0x7f0\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a 00 08 00 00[ 	]*v4fmaddps 0x800\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 4a 80[ 	]*v4fmaddps -0x800\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a f0 f7 ff ff[ 	]*v4fmaddps -0x810\(%edx\),%xmm4,%xmm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 09[ 	]*v4fmaddps \(%ecx\),%ymm4,%ymm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 2f 9a 09[ 	]*v4fmaddps \(%ecx\),%ymm4,%ymm1\{%k7\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f af 9a 09[ 	]*v4fmaddps \(%ecx\),%ymm4,%ymm1\{%k7\}\{z\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8c f4 c0 1d fe ff[ 	]*v4fmaddps -0x1e240\(%esp,%esi,8\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a e0 0f 00 00[ 	]*v4fmaddps 0xfe0\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a 00 10 00 00[ 	]*v4fmaddps 0x1000\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a 00 f0 ff ff[ 	]*v4fmaddps -0x1000\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a e0 ef ff ff[ 	]*v4fmaddps -0x1020\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 4a 7f[ 	]*v4fmaddps 0x7f0\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a 00 08 00 00[ 	]*v4fmaddps 0x800\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 4a 80[ 	]*v4fmaddps -0x800\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a f0 f7 ff ff[ 	]*v4fmaddps -0x810\(%edx\),%ymm4,%ymm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 09[ 	]*v4fnmaddps \(%ecx\),%xmm4,%xmm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 0f aa 09[ 	]*v4fnmaddps \(%ecx\),%xmm4,%xmm1\{%k7\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 8f aa 09[ 	]*v4fnmaddps \(%ecx\),%xmm4,%xmm1\{%k7\}\{z\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8c f4 c0 1d fe ff[ 	]*v4fnmaddps -0x1e240\(%esp,%esi,8\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a e0 0f 00 00[ 	]*v4fnmaddps 0xfe0\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a 00 10 00 00[ 	]*v4fnmaddps 0x1000\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a 00 f0 ff ff[ 	]*v4fnmaddps -0x1000\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a e0 ef ff ff[ 	]*v4fnmaddps -0x1020\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 4a 7f[ 	]*v4fnmaddps 0x7f0\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a 00 08 00 00[ 	]*v4fnmaddps 0x800\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 4a 80[ 	]*v4fnmaddps -0x800\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a f0 f7 ff ff[ 	]*v4fnmaddps -0x810\(%edx\),%xmm4,%xmm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 09[ 	]*v4fnmaddps \(%ecx\),%ymm4,%ymm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 2f aa 09[ 	]*v4fnmaddps \(%ecx\),%ymm4,%ymm1\{%k7\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f af aa 09[ 	]*v4fnmaddps \(%ecx\),%ymm4,%ymm1\{%k7\}\{z\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8c f4 c0 1d fe ff[ 	]*v4fnmaddps -0x1e240\(%esp,%esi,8\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a e0 0f 00 00[ 	]*v4fnmaddps 0xfe0\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a 00 10 00 00[ 	]*v4fnmaddps 0x1000\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a 00 f0 ff ff[ 	]*v4fnmaddps -0x1000\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 4a 7f[ 	]*v4fnmaddps 0x7f0\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a 00 08 00 00[ 	]*v4fnmaddps 0x800\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 4a 80[ 	]*v4fnmaddps -0x800\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a f0 f7 ff ff[ 	]*v4fnmaddps -0x810\(%edx\),%ymm4,%ymm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 09[ 	]*v4fmaddps \(%ecx\),%xmm4,%xmm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 09[ 	]*v4fmaddps \(%ecx\),%xmm4,%xmm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 0f 9a 09[ 	]*v4fmaddps \(%ecx\),%xmm4,%xmm1\{%k7\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 8f 9a 09[ 	]*v4fmaddps \(%ecx\),%xmm4,%xmm1\{%k7\}\{z\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8c f4 c0 1d fe ff[ 	]*v4fmaddps -0x1e240\(%esp,%esi,8\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a e0 0f 00 00[ 	]*v4fmaddps 0xfe0\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a 00 10 00 00[ 	]*v4fmaddps 0x1000\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a 00 f0 ff ff[ 	]*v4fmaddps -0x1000\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a e0 ef ff ff[ 	]*v4fmaddps -0x1020\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 4a 7f[ 	]*v4fmaddps 0x7f0\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a 00 08 00 00[ 	]*v4fmaddps 0x800\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 4a 80[ 	]*v4fmaddps -0x800\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 9a 8a f0 f7 ff ff[ 	]*v4fmaddps -0x810\(%edx\),%xmm4,%xmm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 09[ 	]*v4fmaddps \(%ecx\),%ymm4,%ymm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 09[ 	]*v4fmaddps \(%ecx\),%ymm4,%ymm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 2f 9a 09[ 	]*v4fmaddps \(%ecx\),%ymm4,%ymm1\{%k7\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f af 9a 09[ 	]*v4fmaddps \(%ecx\),%ymm4,%ymm1\{%k7\}\{z\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8c f4 c0 1d fe ff[ 	]*v4fmaddps -0x1e240\(%esp,%esi,8\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a e0 0f 00 00[ 	]*v4fmaddps 0xfe0\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a 00 10 00 00[ 	]*v4fmaddps 0x1000\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a 00 f0 ff ff[ 	]*v4fmaddps -0x1000\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a e0 ef ff ff[ 	]*v4fmaddps -0x1020\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 4a 7f[ 	]*v4fmaddps 0x7f0\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a 00 08 00 00[ 	]*v4fmaddps 0x800\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 4a 80[ 	]*v4fmaddps -0x800\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 9a 8a f0 f7 ff ff[ 	]*v4fmaddps -0x810\(%edx\),%ymm4,%ymm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 09[ 	]*v4fnmaddps \(%ecx\),%xmm4,%xmm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 09[ 	]*v4fnmaddps \(%ecx\),%xmm4,%xmm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 0f aa 09[ 	]*v4fnmaddps \(%ecx\),%xmm4,%xmm1\{%k7\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 8f aa 09[ 	]*v4fnmaddps \(%ecx\),%xmm4,%xmm1\{%k7\}\{z\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8c f4 c0 1d fe ff[ 	]*v4fnmaddps -0x1e240\(%esp,%esi,8\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a e0 0f 00 00[ 	]*v4fnmaddps 0xfe0\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a 00 10 00 00[ 	]*v4fnmaddps 0x1000\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a 00 f0 ff ff[ 	]*v4fnmaddps -0x1000\(%edx\),%xmm4,%xmm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a e0 ef ff ff[ 	]*v4fnmaddps -0x1020\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 4a 7f[ 	]*v4fnmaddps 0x7f0\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a 00 08 00 00[ 	]*v4fnmaddps 0x800\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 4a 80[ 	]*v4fnmaddps -0x800\(%edx\),%xmm4,%xmm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 08 aa 8a f0 f7 ff ff[ 	]*v4fnmaddps -0x810\(%edx\),%xmm4,%xmm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 09[ 	]*v4fnmaddps \(%ecx\),%ymm4,%ymm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 09[ 	]*v4fnmaddps \(%ecx\),%ymm4,%ymm1
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 2f aa 09[ 	]*v4fnmaddps \(%ecx\),%ymm4,%ymm1\{%k7\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f af aa 09[ 	]*v4fnmaddps \(%ecx\),%ymm4,%ymm1\{%k7\}\{z\}
 [ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8c f4 c0 1d fe ff[ 	]*v4fnmaddps -0x1e240\(%esp,%esi,8\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a e0 0f 00 00[ 	]*v4fnmaddps 0xfe0\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a 00 10 00 00[ 	]*v4fnmaddps 0x1000\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a 00 f0 ff ff[ 	]*v4fnmaddps -0x1000\(%edx\),%ymm4,%ymm1
-[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a e0 ef ff ff[ 	]*v4fnmaddps -0x1020\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 4a 7f[ 	]*v4fnmaddps 0x7f0\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a 00 08 00 00[ 	]*v4fnmaddps 0x800\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 4a 80[ 	]*v4fnmaddps -0x800\(%edx\),%ymm4,%ymm1
+[ 	]*[a-f0-9]+:[ 	]*62 f2 5f 28 aa 8a f0 f7 ff ff[ 	]*v4fnmaddps -0x810\(%edx\),%ymm4,%ymm1
 #pass
