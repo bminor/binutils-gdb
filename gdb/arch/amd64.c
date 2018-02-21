8ec57239e91f22f427f8065eb742e6c8bfa223f1
John Baldwin <jhb@FreeBSD.org>
2018-02-21 11:03:41 -0800

    Add "common-defs.h" include to files in arch/ subdir not yet including it.
    This fixes a build breakage on FreeBSD hosts.
    
    gdb/ChangeLog:
    
    	* arch/aarch64.c: Include "common-defs.h".
    	* arch/amd64.c: Likewise.
    	* arch/i386.c: Likewise.

--- gdb/arch/amd64.c
+++ gdb/arch/amd64.c
@@ -15,6 +15,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#include "common-defs.h"
 #include "amd64.h"
 #include "x86-xstate.h"
 #include <stdlib.h>
--- gdb/arch/i386.c
+++ gdb/arch/i386.c
@@ -15,6 +15,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#include "common-defs.h"
 #include "i386.h"
 #include "tdesc.h"
 #include "x86-xstate.h"
