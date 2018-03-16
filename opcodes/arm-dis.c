54ef1671ba67210fb24e41d898ceea6939e9b1f5
Thomas Preud'homme <thomas.preudhomme@arm.com>
2018-03-16 14:18:53 +0000

    [ARM] Fix bxns mask
    Bit 7 of BXNS is a fixed bit which distinguish it from BLXNS. Yet it is
    not set in the disassembler entry mask. This commit fixes that.
    
    2018-03-16  Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
    	Backport from mainline
    	2018-02-19  Thomas Preud'homme  <thomas.preudhomme@arm.com>
    
    opcodes/
    	* arm-dis.c (thumb_opcodes): Fix BXNS mask.

--- opcodes/arm-dis.c
+++ opcodes/arm-dis.c
@@ -2530,7 +2530,7 @@ static const struct opcode16 thumb_opcodes[] =
 
   /* ARMv8-M Security Extensions instructions.  */
   {ARM_FEATURE_CORE_HIGH (ARM_EXT2_V8M), 0x4784, 0xff87, "blxns\t%3-6r"},
-  {ARM_FEATURE_CORE_HIGH (ARM_EXT2_V8M), 0x4704, 0xff07, "bxns\t%3-6r"},
+  {ARM_FEATURE_CORE_HIGH (ARM_EXT2_V8M), 0x4704, 0xff87, "bxns\t%3-6r"},
 
   /* ARM V8 instructions.  */
   {ARM_FEATURE_CORE_LOW (ARM_EXT_V8),  0xbf50, 0xffff, "sevl%c"},
