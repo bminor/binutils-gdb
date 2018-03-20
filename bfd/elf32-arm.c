fac7bd6475c4058e7c1e0df29dac50579e427b34
Christophe Lyon <christophe.lyon@st.com>
2018-03-20 10:55:53 +0100

    [ARM] FDPIC: Make _GLOBAL_OFFSET_TABLE_ a relative symbol
    2018-04-25  Christophe Lyon  <christophe.lyon@st.com>
    	Mickaël Guêné  <mickael.guene@st.com>
    
    	bfd/
    	* elf32-arm.c (elf32_arm_finish_dynamic_symbol): Handle
    	_GLOBAL_OFFSET_TABLE_ in FDPIC mode.

--- bfd/elf32-arm.c
+++ bfd/elf32-arm.c
@@ -17086,10 +17086,10 @@ elf32_arm_finish_dynamic_symbol (bfd * output_bfd,
     }
 
   /* Mark _DYNAMIC and _GLOBAL_OFFSET_TABLE_ as absolute.  On VxWorks,
-     the _GLOBAL_OFFSET_TABLE_ symbol is not absolute: it is relative
-     to the ".got" section.  */
+     and for FDPIC, the _GLOBAL_OFFSET_TABLE_ symbol is not absolute:
+     it is relative to the ".got" section.  */
   if (h == htab->root.hdynamic
-      || (!htab->vxworks_p && h == htab->root.hgot))
+      || (!htab->fdpic_p && !htab->vxworks_p && h == htab->root.hgot))
     sym->st_shndx = SHN_ABS;
 
   return TRUE;
