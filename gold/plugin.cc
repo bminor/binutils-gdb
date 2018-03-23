330b90b5ffbbc20c5de6ae6c7f60c40fab2e7a4f
Cary Coutant <ccoutant@gmail.com>
2018-03-23 09:03:34 -0700

    Fix case where IR file provides symbol visibility but replacement file does not.
    In PR 22868, two IR files provide conflicting visibility for a symbol.
    When a def with PROTECTED visibility is seen after a def with DEFAULT
    visibility, gold does not override the visibility. Later, if the
    replacement object define the symbol with DEFAULT visibility, the symbol
    remains DEFAULT. This was caused by a recent change to allow multiply-defined
    absolute symbols, combined with the fact that the plugin framework was using
    SHN_ABS as the section index for placeholder symbols. The solution is to
    use a real (but arbitrary) section index.
    
    gold/
    	PR gold/22868
    	* plugin.cc (Sized_pluginobj::do_add_symbols): Use a real section
    	index instead of SHN_ABS for defined symbols.
    	* testsuite/Makefile.am (plugin_pr22868): New test case.
    	* testsuite/Makefile.in: Regenerate
    	* testsuite/plugin_pr22868.sh: New test script.
    	* testsuite/plugin_pr22868_a.c: New source file.
    	* testsuite/plugin_pr22868_b.c: New source file.

--- gold/plugin.cc
+++ gold/plugin.cc
@@ -1144,7 +1144,8 @@ Sized_pluginobj<size, big_endian>::do_add_symbols(Symbol_table* symtab,
         {
         case LDPK_DEF:
         case LDPK_WEAKDEF:
-          shndx = elfcpp::SHN_ABS;
+          // We use an arbitrary section number for a defined symbol.
+          shndx = 1;
           break;
         case LDPK_COMMON:
           shndx = elfcpp::SHN_COMMON;
