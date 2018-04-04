aa15ca261023bb03b22a356787786aa5521ce2f8
H.J. Lu <hjl.tools@gmail.com>
2018-04-04 04:36:44 -0700

    i386: Clear vex instead of vex.evex
    "vex" has many fields to control how to decode an instruction.  Clear
    all fields in "vex" before decoding an instruction to avoid using values
    left from the previous instruction.
    
    gas/
    
    	PR binutils/23025
    	* testsuite/gas/i386/prefix.s: Add tests for vcvtpd2dq with
    	VEX and EVEX prefixes.
    	* testsuite/gas/i386/prefix.d: Updated.
    
    opcodes/
    
    	PR binutils/23025
    	* i386-dis.c (get_valid_dis386): Don't set vex.prefix nor vex.w
    	to 0.
    	(print_insn): Clear vex instead of vex.evex.
    
    (cherry picked from commit caf0678c84b5b55fbc4bcc853954745a4ad8b658)

--- opcodes/i386-dis.c
+++ opcodes/i386-dis.c
@@ -12815,7 +12815,6 @@ get_valid_dis386 (const struct dis386 *dp, disassemble_info *info)
       switch ((*codep & 0x3))
 	{
 	case 0:
-	  vex.prefix = 0;
 	  break;
 	case 1:
 	  vex.prefix = DATA_PREFIX_OPCODE;
@@ -12880,7 +12879,6 @@ get_valid_dis386 (const struct dis386 *dp, disassemble_info *info)
       switch ((*codep & 0x3))
 	{
 	case 0:
-	  vex.prefix = 0;
 	  break;
 	case 1:
 	  vex.prefix = DATA_PREFIX_OPCODE;
@@ -12918,12 +12916,10 @@ get_valid_dis386 (const struct dis386 *dp, disassemble_info *info)
       /* For the 2-byte VEX prefix in 32-bit mode, the highest bit in
 	 VEX.vvvv is 1.  */
       vex.register_specifier = (~(*codep >> 3)) & 0xf;
-      vex.w = 0;
       vex.length = (*codep & 0x4) ? 256 : 128;
       switch ((*codep & 0x3))
 	{
 	case 0:
-	  vex.prefix = 0;
 	  break;
 	case 1:
 	  vex.prefix = DATA_PREFIX_OPCODE;
@@ -12998,7 +12994,6 @@ get_valid_dis386 (const struct dis386 *dp, disassemble_info *info)
       switch ((*codep & 0x3))
 	{
 	case 0:
-	  vex.prefix = 0;
 	  break;
 	case 1:
 	  vex.prefix = DATA_PREFIX_OPCODE;
@@ -13356,7 +13351,7 @@ print_insn (bfd_vma pc, disassemble_info *info)
   need_vex = 0;
   need_vex_reg = 0;
   vex_w_done = 0;
-  vex.evex = 0;
+  memset (&vex, 0, sizeof (vex));
 
   if (dp->name == NULL && dp->op[0].bytemode == FLOATCODE)
     {
