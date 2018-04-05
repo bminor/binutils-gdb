2f8ceb38991ea2ee595afbdd5da51ba1a53cbec7
Maciej W. Rozycki <macro@mips.com>
2018-04-05 13:08:35 +0100

    binutils/testsuite: Support REL and MIPS64 reloc formats with `strip-13'
    Add source variants for the `strip-13' test that produce relocations in
    the REL and MIPS64 formats, fixing a failure for the `mips64el-openbsd'
    target.  This also corrects output for `i*86-*', `i960-*', `m6812-*' and
    `m68hc12-*', o32 `mips*-*', and `score*-*' targets, which however does
    not show up as a test result change due to lax error message matching
    causing `bad value' previously produced by `strip' as a result of input
    file rejection to be accepted as a test pass.
    
    For `m6811-*' aka `m68hc11-*' targets this causes a phantom regression,
    because they use 16-bit addressing and therefore `.dc.a' emits 16-bit
    quantities causing relocation data constructed in assembly not to be as
    expected.  Previously input was rejected by `strip' with a `bad value'
    message and now it is accepted, however due to the relocation data error
    the relocation number is not one of the unsupported ones and the tool
    completes successfully, which scores as a test failure.
    
    Disable the test case for `m6811-*' and `m68hc11-*' targets then, as it
    is a test case bug rather than a problem with the relevant backend.  A
    separate change to the test case is required to correct this problem, at
    which point the test case can be enabled for the affected targets.
    
    	binutils/
    	* testsuite/binutils-all/strip-13.s: Rename to...
    	* testsuite/binutils-all/strip-13rela.s: ... this.
    	* testsuite/binutils-all/strip-13rel.s: New test source.
    	* testsuite/binutils-all/strip-13mips64.s: New test source.
    	* testsuite/binutils-all/strip-13.d: Remove `arm-*', `d10v-*',
    	`dlx-*' and `xgate-*' from `not-target' list.  Add `m6811-*' and
    	`m68hc11-*' to `not-target' list.
    	* testsuite/binutils-all/objcopy.exp: Switch between sources for
    	`strip-13'.

--- binutils/testsuite/binutils-all/strip-13.d
+++ binutils/testsuite/binutils-all/strip-13.d
@@ -1,8 +1,9 @@
 #PROG: strip
 #strip: -g
 #error: .* bad value
-#not-target: arm-* d10v-* dlx-* h8300-* ip2k-* rx-* xgate-*
-# The D10V, DLX and XGATE targets only support REL relocations but this test uses RELA relocations.
-# The ARM target does support both types, but defaults to REL.
-# The H8300-*, IP2K and RX targets do not complain about unrecognised relocs, unless they are actually used
+#not-target: h8300-* ip2k-* m6811-* m68hc11-* rx-*
+# The H8300-*, IP2K and 68HC11 targets use 16-bit addressing, so `.dc.a'
+#  does not work for manual relocation data construction.
+# The RX targets do not complain about unrecognised relocs, unless they
+#  are actually used
 #  (which is what should really happen with the other targets...)
