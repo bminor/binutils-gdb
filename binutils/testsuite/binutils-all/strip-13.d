65bd20451fc7e5aea1f0283433f95fb2fe60771d
Maciej W. Rozycki <macro@mips.com>
2018-04-05 13:08:35 +0100

    binutils/testsuite: Tighten the error message check with `strip-13'
    Avoid false positives and actually verify both that an `unsupported
    relocation type 0x8f' message is produced and that no other message is,
    except for the final `bad value', in the `strip-13' test.  This ensures
    that it is a relocation processing error and not a different issue that
    has caused `strip' to terminate unsuccessfully, and that the number
    representing the unsupported relocation has not been clobbered.
    
    	binutils/
    	* testsuite/binutils-all/strip-13.d: Also expect `unsupported
    	relocation type 0x8f' error message.

--- binutils/testsuite/binutils-all/strip-13.d
+++ binutils/testsuite/binutils-all/strip-13.d
@@ -1,6 +1,7 @@
 #PROG: strip
 #strip: -g
-#error: .* bad value
+#error: \A[^\n]*: unsupported relocation type 0x8f\n
+#error:   [^\n]*: bad value\Z
 #not-target: h8300-* ip2k-* m6811-* m68hc11-* rx-*
 # The H8300-*, IP2K and 68HC11 targets use 16-bit addressing, so `.dc.a'
 #  does not work for manual relocation data construction.
