d4ae1932770d9ec76f3d0a429aab45f1cb15e964
Alan Modra <amodra@gmail.com>
2018-04-10 09:14:29 +0930

    Fix some strip test fails on nds32 and pru
    Fixes these fails:
    nds32le-linux  +FAIL: binutils-all/strip-14
    nds32le-linux  +FAIL: binutils-all/strip-15
    pru-elf  +FAIL: binutils-all/strip-14
    pru-elf  +FAIL: binutils-all/strip-15
    
    strip-13 fails on nds32 due to an assertion failure and out of bounds
    access to nds32_elf_howto_table.
    
    	* testsuite/binutils-all/objcopy.exp (strip-14, strip-15): Choose
    	reloc=11 for pru and reloc=50 for nds32.
    	* testsuite/binutils-all/strip-15.d: Accept 0xb reloc number.

--- binutils/testsuite/binutils-all/strip-15.d
+++ binutils/testsuite/binutils-all/strip-15.d
@@ -4,5 +4,5 @@
 
 Relocation section '\.rela?\.text' at offset .* contains 2 entries:
  *Offset * Info * Type * Sym\. *Value * Sym\. *Name(?: * \+ * Addend)?
-0+00 * 0+0(?:1|32|103) * R_[^ ]* *(?: * 55aa)?
+0+00 * 0+0(?:1|b|32|103) * R_[^ ]* *(?: * 55aa)?
 #pass
