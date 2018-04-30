c8a15b78e676b9101045b729a38128cbe5673701
Tom Tromey <tom@tromey.com>
2018-04-30 08:37:30 -0600

    Update help text in linux-fork.c
    This updates some help text in linux-fork.c to conform to GNU
    standards.
    
    2018-04-29  Tom Tromey  <tom@tromey.com>
    
    	* linux-fork.c (_initialize_linux_fork): Update help text.

--- gdb/linux-fork.c
+++ gdb/linux-fork.c
@@ -787,8 +787,8 @@ Fork a duplicate process (experimental)."));
      process.  */
 
   add_com ("restart", class_obscure, restart_command, _("\
-restart <n>: restore program context from a checkpoint.\n\
-Argument 'n' is checkpoint ID, as displayed by 'info checkpoints'."));
+restart N: restore program context from a checkpoint.\n\
+Argument N is checkpoint ID, as displayed by 'info checkpoints'."));
 
   /* Delete checkpoint command: kill the process and remove it from
      the fork list.  */
