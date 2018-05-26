5969f0dba74e17c694faf333b120f1e0b95783ed
Omair Javaid <omair.javaid@linaro.org>
2018-05-26 05:58:51 +0500

    Unset gdbarch significant_addr_bit by default
    This patch fixes a bug introduced by fix to AArch64 pointer tagging.
    
    In our fix for tagged pointer support our agreed approach was to sign
    extend user-space address after clearing tag bits. This is not same
    for all architectures and this patch allows sign extension for
    addresses on targets which specifically set significant_addr_bit.
    
    More information about patch that caused the issues and discussion
    around tagged pointer support can be found in links below:
    
    https://sourceware.org/ml/gdb-patches/2018-05/msg00000.html
    https://sourceware.org/ml/gdb-patches/2017-12/msg00159.html
    
    gdb/ChangeLog:
    
    2018-05-31  Omair Javaid  <omair.javaid@linaro.org>
    
    	PR gdb/23210
    	* gdbarch.sh (significant_addr_bit): Default to zero when
    	not set by target architecture.
    	* gdbarch.c: Re-generated.
    	* utils.c (address_significant): Update.

--- gdb/gdbarch.sh
+++ gdb/gdbarch.sh
@@ -622,7 +622,7 @@ m;CORE_ADDR;addr_bits_remove;CORE_ADDR addr;addr;;core_addr_identity;;0
 # For example, on AArch64, the top bits of an address known as the "tag"
 # are ignored by the kernel, the hardware, etc. and can be regarded as
 # additional data associated with the address.
-v;int;significant_addr_bit;;;;;gdbarch_addr_bit (gdbarch);
+v;int;significant_addr_bit;;;;;;0
 
 # FIXME/cagney/2001-01-18: This should be split in two.  A target method that
 # indicates if the target needs software single step.  An ISA method to
