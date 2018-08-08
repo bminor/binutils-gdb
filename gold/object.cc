f6f9ed015ea683ee2b00591aa31a4cacedb7a810
Tulio Magno Quites Machado Filho <tuliom@linux.ibm.com>
2018-08-07 22:39:23 -0700

    Fix "may be used uninitialized" warning.
    	* object.cc (Sized_relobj_file::map_to_kept_section): Initialize
    	kept_file.

--- gold/object.cc
+++ gold/object.cc
@@ -2951,7 +2951,7 @@ Sized_relobj_file<size, big_endian>::map_to_kept_section(
 	      // Since we're using this mapping for relocation processing,
 	      // we don't want to match sections unless they have the same
 	      // size.
-	      uint64_t kept_size;
+	      uint64_t kept_size = 0;
 	      if (kept_section->find_comdat_section(section_name, &kept_shndx,
 						    &kept_size))
 		{
@@ -2961,7 +2961,7 @@ Sized_relobj_file<size, big_endian>::map_to_kept_section(
 	    }
 	  else
 	    {
-	      uint64_t kept_size;
+	      uint64_t kept_size = 0;
 	      if (kept_section->find_single_comdat_section(&kept_shndx,
 							   &kept_size)
 		  && sh_size == kept_size)
