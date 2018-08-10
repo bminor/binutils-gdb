19449d7c67690c641b1ec9c13ff3531677a5afcc
H.J. Lu <hjl.tools@gmail.com>
2018-08-10 10:23:11 -0700

    x86: Replace evex-no-scale.s with evex-no-scale-[32|64].s
    .if is_64bit
    	vmovaps	-1024(%rip), %zmm0
    	vmovaps	64(,%rax), %zmm0
    	vmovaps	64(,%riz), %zmm0
    .endif
    
    doesn't with i686-elf cross binutils on 64-bit hosts:
    
    evex-no-scale.s: Assembler messages:
    evex-no-scale.s:10: Error: bad register name `%rip)'
    evex-no-scale.s:11: Error: bad register name `%rax)'
    evex-no-scale.s:12: Error: bad register name `%riz)'
    
    This patch replaces evex-no-scale.s with evex-no-scale-32.s and
    evex-no-scale-64.s.
    
    	* testsuite/gas/i386/evex-no-scale-32.d: Don't use
    	evex-no-scale.s.
    	* testsuite/gas/i386/evex-no-scale-64.d: Likewise.
    	* testsuite/gas/i386/evex-no-scale-32.s: New file.
    	* testsuite/gas/i386/evex-no-scale-64.s: Likewise.
    	* testsuite/gas/i386/evex-no-scale.s: Removed.

--- gas/testsuite/gas/i386/evex-no-scale-64.s
+++ gas/testsuite/gas/i386/evex-no-scale-64.s
@@ -1,19 +1,9 @@
 	.allow_index_reg
-	.section .probe, "", @progbits
-.Lprobe_64bit:
-	inc	%eax
-.equiv is_64bit, (. - .Lprobe_64bit) / 2
-
 	.text
 disp:
-.if is_64bit
 	vmovaps	-1024(%rip), %zmm0
 	vmovaps	64(,%rax), %zmm0
 	vmovaps	64(,%riz), %zmm0
-.endif
 	vmovaps	64(,%eax), %zmm0
 	vmovaps	64(,%eiz), %zmm0
 	vmovaps	64, %zmm0
-.if !is_64bit
-	addr16 vmovaps 64, %zmm0
-.endif
