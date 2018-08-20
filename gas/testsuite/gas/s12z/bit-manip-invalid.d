08a8fe2ffd9766877a74a52922b4391d37e85491
Alan Modra <amodra@gmail.com>
2018-08-20 18:16:34 +0930

    Fix s12z test regexps
    Fixes
    ERROR: tcl error sourcing .../gas/testsuite/gas/s12z/s12z.exp.
    ERROR: couldn't compile regular expression pattern: quantifier operand invalid
    
    run_dump_test expected output lines are regexps.
    
    	* testsuite/gas/s12z/bit-manip-invalid.d: Correct regexps.

--- gas/testsuite/gas/s12z/bit-manip-invalid.d
+++ gas/testsuite/gas/s12z/bit-manip-invalid.d
@@ -3,17 +3,17 @@
 #source:  bit-manip-invalid.s
 
 
-dump.o:     file format elf32-s12z
+.*:     file format elf32-s12z
 
 
-Disassembly of section .text:
+Disassembly of section \.text:
 
-00000000 <.text>:
+00000000 <\.text>:
    0:	01          	nop
-   1:	03 a5 10 04 	brset.w 4100, d4, *+6
+   1:	03 a5 10 04 	brset\.w 4100, d4, \*\+6
    5:	06 
    6:	01          	nop
    7:	01          	nop
-   8:	03 65 12    	brset d1, #4, *+18
+   8:	03 65 12    	brset d1, #4, \*\+18
    b:	01          	nop
    c:	01          	nop
