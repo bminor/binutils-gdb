6e1453f681aeec2ef6e3d4fb42f10df9da2a8cd2
H.J. Lu <hjl.tools@gmail.com>
2018-08-31 09:40:54 -0700

    gas/elf/section14.d: Change skip to xfail
    We will get an XPASS when h8300 port peculiarities are fixed, and will
    fix the testsuite too.
    
    	* testsuite/gas/elf/section14.d: Change skip to xfail.
    	* testsuite/lib/gas-defs.exp (run_dump_test): Add xfail support.

--- gas/testsuite/gas/elf/section14.d
+++ gas/testsuite/gas/elf/section14.d
@@ -3,7 +3,7 @@
 #readelf: -S --wide
 # The h8300 port issues a warning message for new sections created
 # without atrributes.
-#skip: h8300-*
+#xfail: h8300-*
 
 There are [0-9]+ section headers, starting at offset 0x[0-9a-f]+:
 
