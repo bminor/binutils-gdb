bminor/binutils-gdb

commit d82b3862f1218134f5301ed990c6db48fcb82b2f

compile: Remove non-const reference parameters
As mentioned here:

  https://sourceware.org/gdb/wiki/Internals%20GDB-C-Coding-Standards#Avoid_non-const_reference_parameters.2C_use_pointers_instead

we prefer to avoid non-const references.  This patch changes the
non-const references I could find in the compile/ directory, either by
making them rvalue-reference (&&) or changing them to pointers.

I'd say all the changes are pretty obvious, except the one in
compile_cplus_instance::enter_scope which might require more attention.

gdb/ChangeLog:

	* compile/compile-c.h (generate_c_for_variable_locations):
	Change reference to pointer.
	* compile/compile-c-support.c (compile_program) <compute>:
	Likewise.
	* compile/compile-c-symbols.c (generate_vla_size): Likewise.
	(generate_c_for_for_one_variable): Likewise
	(generate_c_for_variable_locations): Likewise
	* compile/compile-c-types.c (compile_c_instance::convert_type):
	Likewise
	* compile/compile-cplus-symbols.c (convert_one_symbol):
	std::move the scope passed to enter_scope.
	* compile/compile-cplus-types.c
	(compile_cplus_instance::enter_scope): Make parameter
	rvalue-reference.
	(compile_cplus_instance::new_scope): Change reference to
	pointer.
	(compile_cplus_instance::convert_type): Likewise
	(compile_cplus_convert_typedef): std::move the scope passed to
	enter_scope.
	(compile_cplus_convert_struct_or_union): Likewise.
	(compile_cplus_convert_enum): Likewise.
	(compile_cplus_convert_namespace): Likewise.
	* compile/compile-cplus.h (compile_cplus_instance)
	<enter_scope>: Make parameter rvalue-reference.
	* compile/compile-internal.h (compile_instance)
	<get_cached_type>: Likewise
	* compile/compile-loc2c.c (push): Likewise
	(pushf): Likewise
	(unary): Likewise
	(binary): Likewise
	(print_label): Likewise
	(pushf_register_address): Likewise
	(pushf_register): Likewise
	(do_compile_dwarf_expr_to_c): Likewise
	(compile_dwarf_expr_to_c): Likewise
	(compile_dwarf_bounds_to_c): Likewise

gdb/compile/compile-cplus-types.c
@@ -247,14 +247,14 @@ operator!= (const compile_scope &lhs, const compile_scope &rhs)
 /* See description in compile-cplus.h.  */
 
 void
-compile_cplus_instance::enter_scope (compile_scope &new_scope)
+compile_cplus_instance::enter_scope (compile_scope &&new_scope)
 {
   bool must_push = m_scopes.empty () || m_scopes.back () != new_scope;
 
   new_scope.m_pushed = must_push;
 
   /* Save the new scope.  */
-  m_scopes.push_back (new_scope);
+  m_scopes.push_back (std::move (new_scope));
 
   if (must_push)
     {
@@ -360,7 +360,7 @@ compile_cplus_instance::new_scope (const char *type_name, struct type *type)
 	     class, the previous call will give us that type's gcc_type.
 	     Upper layers are expecting to get the original type's
 	     gcc_type!  */
-	  get_cached_type (type, scope.m_nested_type);
+	  get_cached_type (type, &scope.m_nested_type);
 	  return scope;
 	}
     }
@@ -526,7 +526,7 @@ compile_cplus_convert_typedef (compile_cplus_instance *instance,
     = compile_cplus_instance::decl_name (TYPE_NAME (type));
 
   /* Make sure the scope for this type has been pushed.  */
-  instance->enter_scope (scope);
+  instance->enter_scope (std::move (scope));
 
   /* Convert the typedef's real type.  */
   gcc_type typedef_type = instance->convert_type (check_typedef (type));
@@ -822,7 +822,7 @@ compile_cplus_convert_struct_or_union (compile_cplus_instance *instance,
     }
 
   /* Push all scopes.  */
-  instance->enter_scope (scope);
+  instance->enter_scope (std::move (scope));
 
   /* First we create the resulting type and enter it into our hash
      table.  This lets recursive types work.  */
@@ -928,7 +928,7 @@ compile_cplus_convert_enum (compile_cplus_instance *instance, struct type *type,
     = compile_cplus_instance::decl_name (TYPE_NAME (type));
 
   /* Push all scopes.  */
-  instance->enter_scope (scope);
+  instance->enter_scope (std::move (scope));
 
   gcc_type int_type
     = instance->plugin ().get_int_type (TYPE_UNSIGNED (type),
@@ -1109,7 +1109,7 @@ compile_cplus_convert_namespace (compile_cplus_instance *instance,
     = compile_cplus_instance::decl_name (TYPE_NAME (type));
 
   /* Push scope.  */
-  instance->enter_scope (scope);
+  instance->enter_scope (std::move (scope));
 
   /* Convert this namespace.  */
   instance->plugin ().push_namespace (name.get ());
@@ -1211,7 +1211,7 @@ compile_cplus_instance::convert_type (struct type *type,
 {
   /* Check if TYPE has already been converted.  */
   gcc_type result;
-  if (get_cached_type (type, result))
+  if (get_cached_type (type, &result))
     return result;
 
   /* It is the first time this type has been seen -- convert it

gdb/compile/compile-c-support.c
@@ -572,7 +572,7 @@ public:
 	   register struct before the function body.  This requires a
 	   temporary stream.  */
 	gdb::unique_xmalloc_ptr<unsigned char> registers_used
-	  = generate_c_for_variable_locations (m_instance, var_stream, m_arch,
+	  = generate_c_for_variable_locations (m_instance, &var_stream, m_arch,
 					       expr_block, expr_pc);
 
 	buf.puts ("typedef unsigned int"

gdb/compile/compile-c-symbols.c
@@ -487,7 +487,7 @@ symbol_seen (htab_t hashtab, struct symbol *sym)
 
 static void
 generate_vla_size (compile_instance *compiler,
-		   string_file &stream,
+		   string_file *stream,
 		   struct gdbarch *gdbarch,
 		   unsigned char *registers_used,
 		   CORE_ADDR pc,
@@ -541,7 +541,7 @@ generate_vla_size (compile_instance *compiler,
 
 static void
 generate_c_for_for_one_variable (compile_instance *compiler,
-				 string_file &stream,
+				 string_file *stream,
 				 struct gdbarch *gdbarch,
 				 unsigned char *registers_used,
 				 CORE_ADDR pc,
@@ -556,10 +556,10 @@ generate_c_for_for_one_variable (compile_instance *compiler,
 	     occurs in the middle.  */
 	  string_file local_file;
 
-	  generate_vla_size (compiler, local_file, gdbarch, registers_used, pc,
+	  generate_vla_size (compiler, &local_file, gdbarch, registers_used, pc,
 			     SYMBOL_TYPE (sym), sym);
 
-	  stream.write (local_file.c_str (), local_file.size ());
+	  stream->write (local_file.c_str (), local_file.size ());
 	}
 
       if (SYMBOL_COMPUTED_OPS (sym) != NULL)
@@ -570,12 +570,12 @@ generate_c_for_for_one_variable (compile_instance *compiler,
 	     occurs in the middle.  */
 	  string_file local_file;
 
-	  SYMBOL_COMPUTED_OPS (sym)->generate_c_location (sym, local_file,
+	  SYMBOL_COMPUTED_OPS (sym)->generate_c_location (sym, &local_file,
 							  gdbarch,
 							  registers_used,
 							  pc,
 							  generated_name.get ());
-	  stream.write (local_file.c_str (), local_file.size ());
+	  stream->write (local_file.c_str (), local_file.size ());
 	}
       else
 	{
@@ -611,7 +611,7 @@ generate_c_for_for_one_variable (compile_instance *compiler,
 
 gdb::unique_xmalloc_ptr<unsigned char>
 generate_c_for_variable_locations (compile_instance *compiler,
-				   string_file &stream,
+				   string_file *stream,
 				   struct gdbarch *gdbarch,
 				   const struct block *block,
 				   CORE_ADDR pc)

gdb/compile/compile-c-types.c
@@ -353,7 +353,7 @@ compile_c_instance::convert_type (struct type *type)
   type = check_typedef (type);
 
   gcc_type result;
-  if (get_cached_type (type, result))
+  if (get_cached_type (type, &result))
     return result;
 
   result = convert_type_basic (this, type);

gdb/compile/compile-c.h
@@ -69,7 +69,7 @@ private:
 extern gdb::unique_xmalloc_ptr<unsigned char>
   generate_c_for_variable_locations
      (compile_instance *compiler,
-      string_file &stream,
+      string_file *stream,
       struct gdbarch *gdbarch,
       const struct block *block,
       CORE_ADDR pc);

gdb/compile/compile-cplus-symbols.c
@@ -184,13 +184,11 @@ convert_one_symbol (compile_cplus_instance *instance,
       /* Don't emit local variable decls for a raw expression.  */
       if (instance->scope () != COMPILE_I_RAW_SCOPE || symbol_name == nullptr)
 	{
-	  compile_scope scope;
-
 	  /* For non-local symbols, create/push a new scope so that the
 	     symbol is properly scoped to the plug-in.  */
 	  if (!is_local)
 	    {
-	      scope
+	      compile_scope scope
 		= instance->new_scope (SYMBOL_NATURAL_NAME (sym.symbol),
 				       SYMBOL_TYPE (sym.symbol));
 	      if (scope.nested_type () != GCC_TYPE_NONE)
@@ -200,7 +198,7 @@ convert_one_symbol (compile_cplus_instance *instance,
 		  return;
 		}
 
-	      instance->enter_scope (scope);
+	      instance->enter_scope (std::move (scope));
 	    }
 
 	  /* Get the `raw' name of the symbol.  */

gdb/compile/compile-cplus.h
@@ -159,7 +159,7 @@ public:
   compile_scope new_scope (const char *type_name, struct type *type);
 
   /* Enter the given NEW_SCOPE.  */
-  void enter_scope (compile_scope &scope);
+  void enter_scope (compile_scope &&scope);
 
   /* Leave the current scope.  */
   void leave_scope ();

gdb/compile/compile-internal.h
@@ -59,7 +59,7 @@ public:
 
   /* Query the type cache for TYPE, returning the compiler's
      type for it in RET.  */
-  bool get_cached_type (struct type *type, gcc_type &ret) const;
+  bool get_cached_type (struct type *type, gcc_type *ret) const;
 
   /* Insert GCC_TYPE into the type cache for TYPE.
 

commit 461464f22632163209937ba5128d1f9f32554ea3

Fix use-after-move in compile/compile-cplus-types.c
Patch

  d82b3862f12 ("compile: Remove non-const reference parameters")

introduced a regression in compile/compile-cplus-types.c.  The new_scope
variable in compile_cplus_instance::enter_scope is used after it was
std::moved.  This patch fixes it by referring to the back of the vector
where it was moved instead.

gdb/ChangeLog:

	* compile/compile-cplus-types.c
	(compile_cplus_instance::enter_scope): Don't use new_scope after
	std::move.

gdb/compile/compile-cplus-types.c
@@ -261,7 +261,7 @@ compile_cplus_instance::enter_scope (compile_scope &&new_scope)
       if (debug_compile_cplus_scopes)
 	{
 	  fprintf_unfiltered (gdb_stdlog, "entering new scope %s\n",
-			      host_address_to_string (&new_scope));
+			      host_address_to_string (&m_scopes.back ()));
 	}
 
       /* Push the global namespace. */
@@ -270,7 +270,7 @@ compile_cplus_instance::enter_scope (compile_scope &&new_scope)
       /* Push all other namespaces.  Note that we do not push the last
 	 scope_component -- that's the actual type we are converting.  */
       std::for_each
-	(new_scope.begin (), new_scope.end () - 1,
+	(m_scopes.back ().begin (), m_scopes.back ().end () - 1,
 	 [this] (const scope_component &comp)
 	 {
 	  gdb_assert (TYPE_CODE (SYMBOL_TYPE (comp.bsymbol.symbol))