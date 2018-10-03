fb9bbfd7f2d39a1d4850fcf0f63ef1f077f55e4b
H.J. Lu <hjl.tools@gmail.com>
2018-10-03 14:11:43 -0700

    ELF: Add testcases for PR ld/23658
    Add testcases to verify that all SHT_NOTE sections with the same section
    alignment are placed in a single PT_NOTE segment.
    
    	PR ld/23658
    	* testsuite/ld-elf/pr23658-1.d: New file.
    	* testsuite/ld-elf/pr23658-1a.s: Likewise.
    	* testsuite/ld-elf/pr23658-1b.s: Likewise.
    	* testsuite/ld-elf/pr23658-1c.s: Likewise.
    	* testsuite/ld-elf/pr23658-1d.s: Likewise.
    	* testsuite/ld-elf/pr23658-2.rd: Likewise.
    	* testsuite/ld-elf/pr23658-3.d: Likewise.
    	* testsuite/ld-elf/pr23658-3.s: Likewise.
    	* testsuite/ld-elf/pr23658-3.t: Likewise.
    	* testsuite/ld-elf/shared.exp: Run PR ld/23658 tests.

---
+++ ld/testsuite/ld-elf/pr23658-1.d
@@ -0,0 +1,16 @@
+#source: pr23658-1a.s
+#source: pr23658-1b.s
+#source: pr23658-1c.s
+#source: pr23658-1d.s
+#source: start.s
+#ld: --build-id
+#readelf: -l --wide
+# Since generic linker targets don't place SHT_NOTE sections as orphan,
+# SHT_NOTE sections aren't grouped nor sorted.
+#xfail: cr16-* crx-* d30v-* dlx-* fr30-* frv-* iq2000-*
+#xfail: m68hc12-* mn10200-* moxie-* mt-* msp430-* pj-*
+
+#...
+ +[0-9]+ +\.note\.4 \.note\.1 +
+ +[0-9]+ +\.note.gnu.build-id \.note\.2 .note\.3 +
+#pass
