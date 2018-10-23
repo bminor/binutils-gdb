8e3152af14a91df65af8e922a0e8e77c60ed99ba
Alan Modra <amodra@gmail.com>
2018-10-23 15:50:43 +1030

    alpha testsuite fixes
    Fixes a couple of ERROR results due to .set difference on alpha.
    
    	* testsuite/ld-elf/shared.exp: Don't build pr19073 test on alpha.
    	* testsuite/ld-elf/pr18720b.c: Don't use .set on alpha.

--- ld/testsuite/ld-elf/pr18720b.c
+++ ld/testsuite/ld-elf/pr18720b.c
@@ -7,7 +7,11 @@ foo (void)
 }
 
 asm (".symver foo,foo@FOO");
+#ifdef __alpha__
+asm ("foo_alias = foo");
+#else
 asm (".set foo_alias,foo");
+#endif
 asm (".global foo_alias");
 #if defined __powerpc64__ && defined _CALL_AIXDESC && !defined _CALL_LINUX
 asm (".symver .foo,.foo@FOO");
