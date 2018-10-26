7ed29001c4a965520f6a243ec7ad41c156f96a20
Pedro Franco de Carvalho <pedromfc@linux.ibm.com>
2018-10-26 09:37:53 -0300

    Add decfloat registers to float reggroup
    This patch changes default_register_reggroup_p to return true when the
    register type is decimal floating point and the reggroup is
    float_reggroup.
    
    gdb/ChangeLog:
    2018-10-26  Pedro Franco de Carvalho  <pedromfc@linux.ibm.com>
    
    	* reggroups.c (default_register_reggroup_p): Return true for
    	decfloat registers and float_reggroup.

--- gdb/reggroups.c
+++ gdb/reggroups.c
@@ -202,7 +202,9 @@ default_register_reggroup_p (struct gdbarch *gdbarch, int regnum,
   if (group == all_reggroup)
     return 1;
   vector_p = TYPE_VECTOR (register_type (gdbarch, regnum));
-  float_p = TYPE_CODE (register_type (gdbarch, regnum)) == TYPE_CODE_FLT;
+  float_p = (TYPE_CODE (register_type (gdbarch, regnum)) == TYPE_CODE_FLT
+	     || (TYPE_CODE (register_type (gdbarch, regnum))
+		 == TYPE_CODE_DECFLOAT));
   raw_p = regnum < gdbarch_num_regs (gdbarch);
   if (group == float_reggroup)
     return float_p;
