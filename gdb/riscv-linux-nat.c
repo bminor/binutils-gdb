a3d7226832e5750211ec300c5929dc0f035a3661
Andrew Burgess <andrew.burgess@embecosm.com>
2018-10-28 10:43:26 +0000

    gdb/riscv: Add back missing braces in riscv-linux-nat.c
    In this commit:
    
        commit ee67fd7f3f6ca78eede2862e309c0bcf266bbd7e
        Date:   Thu Oct 25 12:03:31 2018 +0100
    
            gdb/riscv: Use correct regnum in riscv_linux_nat_target::fetch_registers
    
    I incorrectly removed a set of braces in violation of the GDB coding
    standard.  This commit adds them back.
    
    gdb/ChangeLog:
    
    	* riscv-linux-nat.c (riscv_linux_nat_target::fetch_registers):
    	Add missing braces.  No functional change.

--- gdb/riscv-linux-nat.c
+++ gdb/riscv-linux-nat.c
@@ -201,8 +201,10 @@ riscv_linux_nat_target::fetch_registers (struct regcache *regcache, int regnum)
 
   if ((regnum == RISCV_CSR_MISA_REGNUM)
       || (regnum == -1))
-    /* TODO: Need to add a ptrace call for this.  */
-    regcache->raw_supply_zeroed (RISCV_CSR_MISA_REGNUM);
+    {
+      /* TODO: Need to add a ptrace call for this.  */
+      regcache->raw_supply_zeroed (RISCV_CSR_MISA_REGNUM);
+    }
 
   /* Access to other CSRs has potential security issues, don't support them for
      now.  */
