71f5e3f7b624a725ba550a2dd18a413c88ee6497
Nick Clifton <nickc@redhat.com>
2018-11-09 12:48:23 +0000

    Enhance the strings program so that it can display multibyte strings.
    	* strings.c (print_strings): Check for multibyte encodings.
    	* binutils-all/strings-1.bin: New file.  Test binary for string decoding.
    	* testsuite/binutils-all/strings.exp: New file.  Test the strings program.
    	* testsuite/config/default.exp (STRINGS): Define if not provided
    	by the environment.
    	(STRINGSFLAGS): Likewise.

--- binutils/strings.c
+++ binutils/strings.c
@@ -540,9 +540,48 @@ print_strings (const char *filename, FILE *stream, file_ptr address,
 	      free (buf);
 	      return;
 	    }
+
 	  if (! STRING_ISGRAPHIC (c))
-	    /* Found a non-graphic.  Try again starting with next char.  */
-	    goto tryline;
+	    {
+	      /* Found a non-graphic. Try again starting with next char.  */
+	      if (encoding_bytes > 1)
+		{
+		  /* In case of multibyte encodings rewind using magic buffer.  */
+		  if (magiccount == 0)
+		    {
+		      /* If no magic buffer exists: use memory of c.  */
+		      switch (encoding)
+			{
+			default:
+			  break;
+			case 'b':
+			  c = c & 0xff;
+			  magiccount += 1;
+			  break;
+			case 'l':
+			case 'L':
+			  c = c >> 8;
+			  magiccount += (encoding_bytes -1);
+			  break;
+			case 'B':
+			  c = (( c & 0xff0000) >> 16) | ( c & 0xff00)
+			    | (( c & 0xff) << 16);
+			  magiccount += 3;
+			  break;
+			}
+		      magic = (char *) &c;
+		    }
+		  else
+		    {
+		      /* If magic buffer exists: rewind.  */
+		      magic = magic - (encoding_bytes -1);
+		    }
+
+		  address = address - (encoding_bytes -1);
+		}
+
+	      goto tryline;
+	    }
 	  buf[i] = c;
 	}
 
