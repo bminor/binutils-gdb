bf12844a68f3f4f267846a19df6a280f53e7ec5f
Andrew Burgess <andrew.burgess@embecosm.com>
2019-01-10 18:18:39 +0000

    gdb/iq2000: Use default gdbarch methods where possible
    Make use of the default gdbarch methods for gdbarch_dummy_id,
    gdbarch_unwind_pc, and gdbarch_unwind_sp where possible.
    
    I have not tested this change but, by inspecting the code, I believe
    the default methods are equivalent to the code being deleted.
    
    gdb/ChangeLog:
    
    	* gdb/iq2000-tdep.c (iq2000_unwind_sp): Delete.
    	(iq2000_unwind_pc): Delete.
    	(iq2000_dummy_id): Delete.
    	(iq2000_gdbarch_init): Don't register deleted functions with
    	gdbarch.

--- gdb/iq2000-tdep.c
+++ gdb/iq2000-tdep.c
@@ -435,25 +435,6 @@ static const struct frame_unwind iq2000_frame_unwind = {
 };
 
 static CORE_ADDR
-iq2000_unwind_sp (struct gdbarch *gdbarch, struct frame_info *next_frame)
-{
-  return frame_unwind_register_unsigned (next_frame, E_SP_REGNUM);
-}   
-
-static CORE_ADDR
-iq2000_unwind_pc (struct gdbarch *gdbarch, struct frame_info *next_frame)
-{
-  return frame_unwind_register_unsigned (next_frame, E_PC_REGNUM);
-}
-
-static struct frame_id
-iq2000_dummy_id (struct gdbarch *gdbarch, struct frame_info *this_frame)
-{
-  CORE_ADDR sp = get_frame_register_unsigned (this_frame, E_SP_REGNUM);
-  return frame_id_build (sp, get_frame_pc (this_frame));
-}
-
-static CORE_ADDR
 iq2000_frame_base_address (struct frame_info *this_frame, void **this_cache)
 {
   struct iq2000_frame_cache *cache = iq2000_frame_cache (this_frame,
@@ -843,9 +824,6 @@ iq2000_gdbarch_init (struct gdbarch_info info, struct gdbarch_list *arches)
   set_gdbarch_inner_than           (gdbarch, core_addr_lessthan);
   set_gdbarch_register_type (gdbarch, iq2000_register_type);
   set_gdbarch_frame_align (gdbarch, iq2000_frame_align);
-  set_gdbarch_unwind_sp (gdbarch, iq2000_unwind_sp);
-  set_gdbarch_unwind_pc (gdbarch, iq2000_unwind_pc);
-  set_gdbarch_dummy_id (gdbarch, iq2000_dummy_id);
   frame_base_set_default (gdbarch, &iq2000_frame_base);
   set_gdbarch_push_dummy_call (gdbarch, iq2000_push_dummy_call);
 
