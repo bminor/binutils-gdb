a7c9855d033996252fc55349fd3e01670cdd7943
Tom Tromey <tom@tromey.com>
2019-01-21 15:18:50 -0700

    corelow.c does not need sys/file.h
    I did not see any reason that corelow.c should include <sys/file.h>.
    The provided explanatory comment seems to be wrong.  This patch
    removes the include.
    
    2019-01-22  Tom Tromey  <tom@tromey.com>
    
    	* corelow.c: Do not include sys/file.h.

--- gdb/corelow.c
+++ gdb/corelow.c
@@ -21,9 +21,6 @@
 #include "arch-utils.h"
 #include <signal.h>
 #include <fcntl.h>
-#ifdef HAVE_SYS_FILE_H
-#include <sys/file.h>		/* needed for F_OK and friends */
-#endif
 #include "frame.h"		/* required by inferior.h */
 #include "inferior.h"
 #include "infrun.h"
