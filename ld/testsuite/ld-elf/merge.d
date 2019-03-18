28a66f07f0695c740dc0e1b043cd36f90dbfb5ea
Alan Modra <amodra@gmail.com>
2019-03-18 11:11:39 +1030

    csky ld testsuite fixes
    	* testsuite/ld-elf/merge.d: Remove csky from xfails, add moxie.
    	* testsuite/ld-elf/pr21884.d: Remove csky from xfails.
    	* testsuite/ld-elf/shared.exp: Add csky to list not xfailing pr22374.
    	* testsuite/ld-unique/pr21529.d: Remove csky from xfails

--- ld/testsuite/ld-elf/merge.d
+++ ld/testsuite/ld-elf/merge.d
@@ -1,11 +1,11 @@
 #source: merge.s
 #ld: -T merge.ld
 #objdump: -s
-#xfail: bfin-*-* cr16-*-* cris*-*-* crx-*-* csky-*-* d10v-*-* d30v-*-* dlx-*-*
+#xfail: bfin-*-* cr16-*-* cris*-*-* crx-*-* d10v-*-* d30v-*-* dlx-*-*
 #xfail: fr30-*-* frv-*-* ft32-*-* h8300-*-* hppa*64*-*-* ip2k-*-* iq2000-*-*
-#xfail: lm32-*-* m68hc11-*-* mcore-*-* mep-*-* metag-*-* mn102*-*-* ms1-*-*
-#xfail: nds32*-*-* nios2-*-* or32-*-* pj-*-* pru-*-* s12z-*-* score-*-* tic6x-*-*
-#xfail: vax-*-* xgate-*-* xstormy16-*-* xtensa*-*-*
+#xfail: lm32-*-* m68hc11-*-* mcore-*-* mep-*-* metag-*-* mn102*-*-* moxie-*-*
+#xfail: ms1-*-* nds32*-*-* nios2-*-* or32-*-* pj-*-* pru-*-* s12z-*-* score-*-*
+#xfail: tic6x-*-* vax-*-* xgate-*-* xstormy16-*-* xtensa*-*-*
 
 .*:     file format .*elf.*
 
