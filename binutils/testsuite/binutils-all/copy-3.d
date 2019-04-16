a0fb96150814fef91598e0b528c071201fe18891
Alan Modra <amodra@gmail.com>
2019-04-16 18:08:11 +0930

    ns32k testsuite tidy
    Some of these tests were excluded for ns32k-netbsd, exclude for all
    ns32k instead.
    
    binutils/
    	* testsuite/binutils-all/copy-2.d: Don't run for ns32k-*-*.
    	* testsuite/binutils-all/copy-3.d: Likewise.
    gas/
    	* testsuite/gas/all/gas.exp: Remove ns32k xfails.
    	* testsuite/gas/all/weakref1u.d: Don't run for ns32k-*-*.
    ld/
    	* testsuite/ld-scripts/pr20302.d: Don't run for ns32k-*-*.
    	* testsuite/ld-scripts/section-match-1.d: Likewise.
    	* testsuite/ld-undefined/require-defined.exp: Likewise.

--- binutils/testsuite/binutils-all/copy-3.d
+++ binutils/testsuite/binutils-all/copy-3.d
@@ -3,7 +3,7 @@
 #objcopy: --set-section-flags .text=alloc,data
 #name: copy with setting section flags 3
 #source: bintest.s
-#notarget: *-*-*aout *-*-*coff *-*-cygwin* *-*-darwin *-*-mingw* *-*-go32 *-*-*pe hppa*-*-hpux* ns32k-*-netbsd powerpc-*-aix* rs6000-*-* rx-*-*
+#notarget: *-*-*aout *-*-*coff *-*-cygwin* *-*-darwin *-*-mingw* *-*-go32 *-*-*pe hppa*-*-hpux* ns32k-*-* powerpc-*-aix* rs6000-*-* rx-*-*
 # The .text # section in PE/COFF has a fixed set of flags and these
 # cannot be changed.  We skip it for them.
 
--- ld/testsuite/ld-scripts/section-match-1.d
+++ ld/testsuite/ld-scripts/section-match-1.d
@@ -1,7 +1,7 @@
 #source: section-match-1.s
 #ld: -T section-match-1.t
 #objdump: -s
-#notarget: *-*-osf* *-*-aix* *-*-pe *-*-*aout *-*-ecoff *-*-netbsd *-*-vms h8300-*-* tic30-*-*
+#notarget: *-*-osf* *-*-aix* *-*-pe *-*-*aout *-*-ecoff *-*-netbsd *-*-vms h8300-*-* ns32k-*-* tic30-*-*
 # This test uses arbitrary section names, which are not support by some
 # file formats.  Also these section names must be present in the
 # output, not translated into some other name, eg .text
