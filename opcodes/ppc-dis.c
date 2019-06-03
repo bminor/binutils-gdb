4f6d070adb32e61c3c61e621c6566fc2d5af2232
Alan Modra <amodra@gmail.com>
2019-06-03 11:46:04 +0930

    Don't waste space in prefix_opcd_indices
    	* ppc-dis.c (prefix_opcd_indices): Correct size.

--- opcodes/ppc-dis.c
+++ opcodes/ppc-dis.c
@@ -382,7 +382,7 @@ powerpc_init_dialect (struct disassemble_info *info)
 #define PPC_OPCD_SEGS (1 + PPC_OP (-1))
 static unsigned short powerpc_opcd_indices[PPC_OPCD_SEGS + 1];
 #define PREFIX_OPCD_SEGS (1 + PPC_PREFIX_SEG (-1))
-static unsigned short prefix_opcd_indices[PPC_OPCD_SEGS+1];
+static unsigned short prefix_opcd_indices[PREFIX_OPCD_SEGS + 1];
 #define VLE_OPCD_SEGS (1 + VLE_OP_TO_SEG (VLE_OP (-1, 0xffff)))
 static unsigned short vle_opcd_indices[VLE_OPCD_SEGS + 1];
 #define SPE2_OPCD_SEGS (1 + SPE2_XOP_TO_SEG (SPE2_XOP (-1)))
