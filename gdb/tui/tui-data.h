e6a1c5cbcb9cee5a08d814669419936ee151b19d
Tom Tromey <tom@tromey.com>
2019-06-19 22:24:30 -0600

    Don't declare tui_init_content_element
    I noticed that tui_init_content_element is declared but never defined.
    This removes the declaration.  Tested by rebuilding.  (I should have
    merged this with the previous patch but I had forgotten that I found
    two of these.)
    
    gdb/ChangeLog
    2019-06-19  Tom Tromey  <tom@tromey.com>
    
    	* tui/tui-data.h (tui_init_content_element): Don't declare.

--- gdb/tui/tui-data.h
+++ gdb/tui/tui-data.h
@@ -306,8 +306,6 @@ extern void tui_init_generic_part (struct tui_gen_win_info *);
 extern tui_win_content tui_alloc_content (int, enum tui_win_type);
 extern int tui_add_content_elements (struct tui_gen_win_info *, 
 				     int);
-extern void tui_init_content_element (struct tui_win_element *, 
-				      enum tui_win_type);
 extern void tui_free_window (struct tui_win_info *);
 extern void tui_free_win_content (struct tui_gen_win_info *);
 extern void tui_free_data_content (tui_win_content, int);
