a2bd7b82ee2847d0b9bc6994b7248306de3a618f
Tom Tromey <tom@tromey.com>
2019-07-06 08:31:57 -0600

    Don't declare tui_copy_win or tui_box_win
    tui_copy_win and tui_box_win are not implemented, so don't declare
    them.
    
    gdb/ChangeLog
    2019-07-16  Tom Tromey  <tom@tromey.com>
    
    	* tui/tui-wingeneral.h (tui_copy_win, tui_box_win): Don't
    	declare.

--- gdb/tui/tui-wingeneral.h
+++ gdb/tui/tui-wingeneral.h
@@ -31,8 +31,6 @@ extern void tui_unhighlight_win (struct tui_win_info *);
 extern void tui_make_all_visible (void);
 extern void tui_make_all_invisible (void);
 extern void tui_make_window (struct tui_gen_win_info *, enum tui_box);
-extern struct tui_win_info *tui_copy_win (struct tui_win_info *);
-extern void tui_box_win (struct tui_gen_win_info *, int);
 extern void tui_highlight_win (struct tui_win_info *);
 extern void tui_check_and_display_highlight_if_needed (struct tui_win_info *);
 extern void tui_refresh_all ();
