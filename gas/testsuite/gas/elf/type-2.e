bminor/binutils-gdb

commit f2d4ba38f5723a207c40a288036af2f38b70e837

gas/ELF: don't accumulate .type settings
Recently a patch was submitted for a Xen Project test harness binary to
override the compiler specified @object to @func (see [1]). In a reply I
suggested we shouldn't make ourselves dependent on currently unspecified
behavior of gas here: It accumulates all requests, and then
bfd/elf.c:swap_out_syms(), in an apparently ad hoc manner, prioritizes
certain flags over others.

Make the behavior predictable: Generally the last .type is what counts.
Exceptions are directives which set multiple bits (TLS, IFUNC, and
UNIQUE): Subsequent directives requesting just the more generic bit
(i.e. FUNC following IFUNC) won't clear the more specific one.  Warn
about incompatible changes, except from/to STT_NOTYPE.

Also add a new target hook, which hppa wants to use right away afaict.

In the course of adding the warning I ran into two ld testsuite
failures.  I can only assume that it was a copy-and-paste mistake that
lead to the same symbol having its type set twice.

[1] https://lists.xenproject.org/archives/html/xen-devel/2019-05/msg01980.html

gas/config/obj-elf.c
@@ -2069,7 +2069,38 @@ obj_elf_type (int ignore ATTRIBUTE_UNUSED)
   if (*input_line_pointer == '"')
     ++input_line_pointer;
 
-  elfsym->symbol.flags |= type;
+#ifdef md_elf_symbol_type_change
+  if (!md_elf_symbol_type_change (sym, elfsym, type))
+#endif
+    {
+      flagword mask = BSF_FUNCTION | BSF_OBJECT;
+
+      if (type != BSF_FUNCTION)
+	mask |= BSF_GNU_INDIRECT_FUNCTION;
+      if (type != BSF_OBJECT)
+	{
+	  mask |= BSF_GNU_UNIQUE | BSF_THREAD_LOCAL;
+
+	  if (S_IS_COMMON (sym))
+	    {
+	      as_bad (_("cannot change type of common symbol '%s'"),
+		      S_GET_NAME (sym));
+	      mask = type = 0;
+	    }
+	}
+
+      /* Don't warn when changing to STT_NOTYPE.  */
+      if (type)
+	{
+	  flagword new = (elfsym->symbol.flags & ~mask) | type;
+
+	  if (new != (elfsym->symbol.flags | type))
+	    as_warn (_("symbol '%s' already has its type set"), S_GET_NAME (sym));
+	  elfsym->symbol.flags = new;
+	}
+      else
+	elfsym->symbol.flags &= ~mask;
+    }
 
   demand_empty_rest_of_line ();
 }

gas/testsuite/gas/elf/type-2.e
@@ -1,0 +1,10 @@
+ +.+: 0+0 +0 +NOTYPE +LOCAL +DEFAULT +UND *
+ +.+: 0+0 +0 +OBJECT +LOCAL +DEFAULT +. test1
+ +.+: 0+1 +0 +FUNC +LOCAL +DEFAULT +. test2
+ +.+: 0+2 +0 +NOTYPE +LOCAL +DEFAULT +. test3
+ +.+: 0+3 +0 +NOTYPE +LOCAL +DEFAULT +. test4
+ +.+: 0+4 +0 +NOTYPE +LOCAL +DEFAULT +. test5
+ +.+: 0+5 +0 +NOTYPE +LOCAL +DEFAULT +. test6
+ +.+: 0+6 +0 +OBJECT +LOCAL +DEFAULT +. test7
+ +.+: 0+7 +0 +TLS +LOCAL +DEFAULT +. test8
+ +.+: 0+8 +0 +IFUNC +LOCAL +DEFAULT +. test9

gas/config/tc-hppa.h
@@ -177,6 +177,14 @@ int hppa_fix_adjustable (struct fix *);
        ), BSF_FUNCTION)							\
    : -1)
 
+/* Handle type change from .type pseudo: Zap STT_PARISC_MILLI when
+   switching to a non-function type.  */
+#define md_elf_symbol_type_change(sym, elf, type)			\
+  ((type) != BSF_FUNCTION						\
+   && (((elf)->internal_elf_sym.st_info = 				\
+	ELF_ST_INFO (ELF_ST_BIND ((elf)->internal_elf_sym.st_info),	\
+		     STT_NOTYPE)), 0))
+
 #define tc_frob_symbol(sym,punt) \
   { \
     if ((S_GET_SEGMENT (sym) == bfd_und_section_ptr \

gas/doc/as.texi
@@ -7215,6 +7215,10 @@ systems).
 
 @end table
 
+Changing between incompatible types other than from/to STT_NOTYPE will
+result in a diagnostic.  An intermediate change to STT_NOTYPE will silence
+this.
+
 Note: Some targets support extra types in addition to those listed above.
 
 @end ifset

gas/testsuite/gas/elf/elf.exp
@@ -229,6 +229,7 @@ if { [is_elf_format] } then {
     } else {
 	run_dump_test ifunc-1
 	run_elf_list_test "type" "" "" "-s" "| grep \"1 *\\\[FIONTCU\\\]\""
+	run_elf_list_test "type-2" "" "--warn" "-s" "| grep \"0 *\\\[FIONT\\\]\""
     }
 
     run_dump_test "section6"

gas/testsuite/gas/elf/type-2.l
@@ -1,0 +1,3 @@
+.*: Assembler messages:
+.*:4: Warning: .*
+.*:9: Warning: .*

gas/testsuite/gas/elf/type-2.s
@@ -1,0 +1,49 @@
+	.text
+
+	.type	test1,%function
+	.type	test1,%object
+test1:
+	.byte	0x0
+
+	.type	test2,%object
+	.type	test2,%function
+test2:
+	.byte	0x0
+
+	.type	test3,%object
+	.type	test3,%notype
+test3:
+	.byte	0x0
+
+	.type	test4,%function
+	.type	test4,%notype
+test4:
+	.byte	0x0
+
+	.type	test5,%tls_object
+	.type	test5,%notype
+test5:
+	.byte	0x0
+
+	.type	test6,%gnu_indirect_function
+	.type	test6,%notype
+test6:
+	.byte	0x0
+
+	.type	test7,%function
+	.type	test7,%notype
+	.type	test7,%object
+test7:
+	.byte	0x0
+
+	.type	test8,%object
+	.type	test8,%tls_object
+	.type	test8,%object
+test8:
+	.byte	0x0
+
+	.type	test9,%function
+	.type	test9,%gnu_indirect_function
+	.type	test9,%function
+test9:
+	.byte	0x0

ld/testsuite/ld-elf/group9.s
@@ -5,7 +5,7 @@ foo:
 	.byte 0
 	.section	.data.foo,"axG",%progbits,foo,comdat
 	.globl foo.data
-	.type	foo,%object
+	.type	foo.data,%object
 foo.data:
 	.byte 0
 	.section	.text.bar,"axG",%progbits,bar,comdat

commit d69cd47e7e9884f7b3a319936f70b8d93347e9e0

Re: gas/ELF: don't accumulate .type settings
git commit f2d4ba38f5 caused many failures for mips-sgi-irix targets,
and added a new test that failed for aarch64, nds32, and rl78.
The mips failures are due to BSF_OBJECT being set in many cases for
symbols by the mips .global/.globl directive.  This patch removes that
code and instead sets BSF_OBJECT in a target frob_symbol function,
also moving the mips hacks in elf_frob_symbol to the new function.

Note that common symbols are handled fine in elf.c:swap_out_syms
without needing to set BSF_OBJECT, so that old code can disappear.

	* config/obj-elf.c (elf_frob_symbol): Remove mips hacks.
	* config/tc-mips.h (tc_frob_symbol): Define.
	(mips_frob_symbol): Declare.
	* config/tc-mips.c (s_mips_globl): Don't set BSF_OBJECT for irix.
	(mips_frob_symbol): Fudge symbols for irix here.
	* testsuite/gas/elf/type-2.e: Allow random target symbols.

gas/config/obj-elf.c
@@ -2369,23 +2369,6 @@ elf_frob_symbol (symbolS *symp, int *puntp)
 	as_bad (_("symbol `%s' can not be both weak and common"),
 		S_GET_NAME (symp));
     }
-
-#ifdef TC_MIPS
-  /* The Irix 5 and 6 assemblers set the type of any common symbol and
-     any undefined non-function symbol to STT_OBJECT.  We try to be
-     compatible, since newer Irix 5 and 6 linkers care.  However, we
-     only set undefined symbols to be STT_OBJECT if we are on Irix,
-     because that is the only time gcc will generate the necessary
-     .global directives to mark functions.  */
-
-  if (S_IS_COMMON (symp))
-    symbol_get_bfdsym (symp)->flags |= BSF_OBJECT;
-
-  if (strstr (TARGET_OS, "irix") != NULL
-      && ! S_IS_DEFINED (symp)
-      && (symbol_get_bfdsym (symp)->flags & BSF_FUNCTION) == 0)
-    symbol_get_bfdsym (symp)->flags |= BSF_OBJECT;
-#endif
 }
 
 struct group_list

gas/config/tc-mips.c
@@ -16461,7 +16461,6 @@ s_mips_globl (int x ATTRIBUTE_UNUSED)
   char *name;
   int c;
   symbolS *symbolP;
-  flagword flag;
 
   do
     {
@@ -16471,14 +16470,6 @@ s_mips_globl (int x ATTRIBUTE_UNUSED)
 
       *input_line_pointer = c;
       SKIP_WHITESPACE_AFTER_NAME ();
-
-#ifdef TE_IRIX
-      /* On Irix 5, every global symbol that is not explicitly labelled as
-         being a function is apparently labelled as being an object.  */
-      flag = BSF_OBJECT;
-#else
-      flag = BSF_NO_FLAGS;
-#endif
 
       if (!is_end_of_line[(unsigned char) *input_line_pointer]
 	  && (*input_line_pointer != ','))
@@ -16493,10 +16484,8 @@ s_mips_globl (int x ATTRIBUTE_UNUSED)
 	  (void) restore_line_pointer (c);
 
 	  if (sec != NULL && (sec->flags & SEC_CODE) != 0)
-	    flag = BSF_FUNCTION;
+	    symbol_get_bfdsym (symbolP)->flags |= BSF_FUNCTION;
 	}
-
-      symbol_get_bfdsym (symbolP)->flags |= flag;
 
       c = *input_line_pointer;
       if (c == ',')
@@ -16511,6 +16500,23 @@ s_mips_globl (int x ATTRIBUTE_UNUSED)
 
   demand_empty_rest_of_line ();
 }
+
+#ifdef TE_IRIX
+/* The Irix 5 and 6 assemblers set the type of any common symbol and
+   any undefined non-function symbol to STT_OBJECT.  We try to be
+   compatible, since newer Irix 5 and 6 linkers care.  */
+
+void
+mips_frob_symbol (symbolS *symp ATTRIBUTE_UNUSED)
+{
+  /* This late in assembly we can set BSF_OBJECT indiscriminately
+     and let elf.c:swap_out_syms sort out the symbol type.  */
+  flagword *flags = &symbol_get_bfdsym (symp)->flags;
+  if ((*flags & (BSF_GLOBAL | BSF_WEAK)) != 0
+      || !S_IS_DEFINED (symp))
+    *flags |= BSF_OBJECT;
+}
+#endif
 
 static void
 s_option (int x ATTRIBUTE_UNUSED)

gas/config/tc-mips.h
@@ -126,6 +126,11 @@ extern void mips_frob_file (void);
 extern void mips_frob_file_after_relocs (void);
 #endif
 
+#ifdef TE_IRIX
+#define tc_frob_symbol(sym, punt) mips_frob_symbol (sym)
+extern void mips_frob_symbol (symbolS *);
+#endif
+
 #define tc_fix_adjustable(fixp) mips_fix_adjustable (fixp)
 extern int mips_fix_adjustable (struct fix *);
 

gas/testsuite/gas/elf/type-2.e
@@ -1,10 +1,20 @@
  +.+: 0+0 +0 +NOTYPE +LOCAL +DEFAULT +UND *
+#...
  +.+: 0+0 +0 +OBJECT +LOCAL +DEFAULT +. test1
+#...
  +.+: 0+1 +0 +FUNC +LOCAL +DEFAULT +. test2
+#...
  +.+: 0+2 +0 +NOTYPE +LOCAL +DEFAULT +. test3
+#...
  +.+: 0+3 +0 +NOTYPE +LOCAL +DEFAULT +. test4
+#...
  +.+: 0+4 +0 +NOTYPE +LOCAL +DEFAULT +. test5
+#...
  +.+: 0+5 +0 +NOTYPE +LOCAL +DEFAULT +. test6
+#...
  +.+: 0+6 +0 +OBJECT +LOCAL +DEFAULT +. test7
+#...
  +.+: 0+7 +0 +TLS +LOCAL +DEFAULT +. test8
+#...
  +.+: 0+8 +0 +IFUNC +LOCAL +DEFAULT +. test9
+#pass