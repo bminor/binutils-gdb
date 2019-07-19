231097b03afffd0c5a2b520cd999dbcbe8d64eda
Jose E. Marchesi <jose.marchesi@oracle.com>
2019-07-19 15:35:43 +0200

    cpu,opcodes,gas: use %r0 and %r6 instead of %a and %ctf in eBPF disassembler
    This patch changes the eBPF CPU description to prefer the register
    names %r0 and %r6 instead of %a and %ctx when disassembling.  This
    matches better with the current practice, vs. cBPF.
    
    It also updates the GAS tests in order to reflect this change.
    Tested in a x86_64 host.
    
    cpu/ChangeLog:
    
    2019-07-19  Jose E. Marchesi  <jose.marchesi@oracle.com>
    
    	* bpf.cpu (h-gpr): when disassembling, use %r0 and %r6 instead of
    	%a and %ctx.
    
    opcodes/ChangeLog:
    
    2019-07-19  Jose E. Marchesi  <jose.marchesi@oracle.com>
    
    	* bpf-desc.c: Regenerated.
    
    gas/ChangeLog:
    
    2019-07-19  Jose E. Marchesi  <jose.marchesi@oracle.com>
    
    	* testsuite/gas/bpf/alu.d: Use %r6 instead of %ctx.
    	* testsuite/gas/bpf/lddw-be.d: Likewise.
    	* testsuite/gas/bpf/lddw.d: Likewise.
    	* testsuite/gas/bpf/alu-be.d: Likewise.
    	* testsuite/gas/bpf/alu32.d: Likewise.

--- gas/testsuite/gas/bpf/lddw-be.d
+++ gas/testsuite/gas/bpf/lddw-be.d
@@ -14,5 +14,5 @@ Disassembly of section .text:
   18:	00 00 00 00 00 00 00 00 
   20:	18 50 00 00 55 66 77 88 	lddw %r5,0x1122334455667788
   28:	00 00 00 00 11 22 33 44 
-  30:	18 60 00 00 ff ff ff fe 	lddw %ctx,-2
+  30:	18 60 00 00 ff ff ff fe 	lddw %r6,-2
   38:	00 00 00 00 ff ff ff ff 
--- gas/testsuite/gas/bpf/lddw.d
+++ gas/testsuite/gas/bpf/lddw.d
@@ -13,5 +13,5 @@ Disassembly of section .text:
   18:	00 00 00 00 00 00 00 00 
   20:	18 05 00 00 88 77 66 55 	lddw %r5,0x1122334455667788
   28:	00 00 00 00 44 33 22 11 
-  30:	18 06 00 00 fe ff ff ff 	lddw %ctx,-2
+  30:	18 06 00 00 fe ff ff ff 	lddw %r6,-2
   38:	00 00 00 00 ff ff ff ff 
