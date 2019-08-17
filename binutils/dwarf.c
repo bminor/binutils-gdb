4bc59a5ab53c4cf43993e48d9a7eb5055d7b4e51
Alan Modra <amodra@gmail.com>
2019-08-17 18:01:05 +0930

    PR24911, Heap overflow issue in qsort_r, dwarf.c
    The actual args to this function are "pointers to pointers to
    debug_info".
    
    	PR 24911
    	* dwarf.c (comp_addr_base): Dereference args.
    
    (cherry picked from commit d367307b93e5589245d1f2c8bbcfe709404540a8)

--- binutils/dwarf.c
+++ binutils/dwarf.c
@@ -6470,8 +6470,8 @@ display_debug_aranges (struct dwarf_section *section,
 static int
 comp_addr_base (const void * v0, const void * v1)
 {
-  debug_info * info0 = (debug_info *) v0;
-  debug_info * info1 = (debug_info *) v1;
+  debug_info *info0 = *(debug_info **) v0;
+  debug_info *info1 = *(debug_info **) v1;
   return info0->addr_base - info1->addr_base;
 }
 
