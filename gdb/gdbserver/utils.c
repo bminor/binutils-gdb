52c64cf72d40aace2d2fedca32e5c0373cd9a484
Tom Tromey <tromey@adacore.com>
2019-08-29 10:45:06 -0400

    gdbserver does not need xstrdup
    gdbserver has its own implementation of xstrdup.  However, because
    gdbserver links against libiberty now, I think this is not needed.
    This patch removes it.
    
    gdb/gdbserver/ChangeLog
    2019-10-25  Tom Tromey  <tromey@adacore.com>
    
    	* utils.c (xstrdup): Remove.
    
    Change-Id: I2aa56d18d0f9af8e70a00dff431d2fda5705a5d5

--- gdb/gdbserver/utils.c
+++ gdb/gdbserver/utils.c
@@ -37,18 +37,6 @@ malloc_failure (long size)
   exit (1);
 }
 
-/* Copy a string into a memory buffer.
-   If malloc fails, this will print a message to stderr and exit.  */
-
-char *
-xstrdup (const char *s)
-{
-  char *ret = strdup (s);
-  if (ret == NULL)
-    malloc_failure (strlen (s) + 1);
-  return ret;
-}
-
 /* Print the system error message for errno, and also mention STRING
    as the file name for which the error was encountered.
    Then return to command level.  */
