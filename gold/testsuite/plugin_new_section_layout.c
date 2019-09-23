40ac0077b246e100bfeb918c821bd73562dd176b
Alan Modra <amodra@gmail.com>
2019-09-23 20:30:22 +0930

    implicit conversion from enum ld_plugin_level to enum ld_plugin_status
    This is a gcc10 warning fix.
    
    gold/
    	* testsuite/plugin_new_section_layout.c (new_input_hook): Correct
    	return status enum values.
    
    (cherry picked from commit 6831670dd3c0a64e332b316c95873c57ab19887a)

--- gold/testsuite/plugin_new_section_layout.c
+++ gold/testsuite/plugin_new_section_layout.c
@@ -144,12 +144,12 @@ new_input_hook(const struct ld_plugin_input_file *file)
     section.shndx = i;
 
     if (get_input_section_type(section, &type) != LDPS_OK)
-      return LDPL_FATAL;
+      return LDPS_ERR;
     if (type != SHT_PROGBITS)
       continue;
 
     if (get_input_section_name(section, &name))
-      return LDPL_FATAL;
+      return LDPS_ERR;
 
     /* As in plugin_section_order.c, order is foo() followed by bar()
        followed by baz() */
@@ -172,7 +172,7 @@ new_input_hook(const struct ld_plugin_input_file *file)
   }
 
   if (num_entries != 3)
-    return LDPL_FATAL;
+    return LDPS_ERR;
 
   update_section_order(section_list, num_entries);
   unique_segment_for_sections (".text.plugin_created_unique", 0, 0x1000,
