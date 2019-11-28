38b49e22b56e9ee93a5a5d4499c02caf2e41af25
Mihails Strasuns <mihails.strasuns@intel.com>
2019-11-28 09:59:23 +0100

    jit: minor improvement to debug logging
    gdb/ChangeLog:
    2019-11-28  Mihails Strasuns  <mihails.strasuns@intel.com>
    
    	* jit.c (jit_bfd_try_read_symtab): Fix printed function name in the
    	debug output.
    	* jit.c (jit_unregister_code): Add debug print to match
    	`jit_register_code`.
    
    Change-Id: Ie66064f3aaa1c74facfc025c8d87f3a057869779

--- gdb/jit.c
+++ gdb/jit.c
@@ -874,7 +874,7 @@ jit_bfd_try_read_symtab (struct jit_code_entry *code_entry,
 
   if (jit_debug)
     fprintf_unfiltered (gdb_stdlog,
-			"jit_register_code, symfile_addr = %s, "
+			"jit_bfd_try_read_symtab, symfile_addr = %s, "
 			"symfile_size = %s\n",
 			paddress (gdbarch, code_entry->symfile_addr),
 			pulongest (code_entry->symfile_size));
@@ -958,6 +958,9 @@ jit_register_code (struct gdbarch *gdbarch,
 static void
 jit_unregister_code (struct objfile *objfile)
 {
+  if (jit_debug)
+    fprintf_unfiltered (gdb_stdlog, "jit_unregister_code (%s)\n",
+			host_address_to_string (objfile));
   delete objfile;
 }
 
