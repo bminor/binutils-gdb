0bc3450e220a4fb29f931ada84b546ce8993e85e
Alan Modra <amodra@gmail.com>
2019-12-13 16:14:57 +1030

    Set no file contents PT_LOAD p_offset to first page
    	PR 25237
    	* elf.c (assign_file_positions_for_load_sections): Attempt to
    	keep meaningless p_offset for PT_LOAD segments without file
    	contents within file size.

--- bfd/elf.c
+++ bfd/elf.c
@@ -5752,7 +5752,15 @@ assign_file_positions_for_load_sections (bfd *abfd,
 	  || (p->p_type == PT_NOTE && bfd_get_format (abfd) == bfd_core))
 	{
 	  if (!m->includes_filehdr && !m->includes_phdrs)
-	    p->p_offset = off;
+	    {
+	      p->p_offset = off;
+	      if (no_contents)
+		/* Put meaningless p_offset for PT_LOAD segments
+		   without file contents somewhere within the first
+		   page, in an attempt to not point past EOF.  */
+		p->p_offset = off % (p->p_align > maxpagesize
+				     ? p->p_align : maxpagesize);
+	    }
 	  else
 	    {
 	      file_ptr adjust;
