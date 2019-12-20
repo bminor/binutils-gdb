e76832f125f6f005ddf3c75b7be675272568b01e
Alan Modra <amodra@gmail.com>
2019-12-20 12:44:33 +1030

    ubsan: xtensa: left shift cannot be represented in type 'int'
    	* xtensa-isa.c (xtensa_insnbuf_from_chars): Avoid signed overflow.

--- bfd/xtensa-isa.c
+++ bfd/xtensa-isa.c
@@ -223,7 +223,7 @@ xtensa_insnbuf_from_chars (xtensa_isa isa,
       int word_inx = byte_to_word_index (i);
       int bit_inx = byte_to_bit_index (i);
 
-      insn[word_inx] |= (*cp & 0xff) << bit_inx;
+      insn[word_inx] |= (unsigned) (*cp & 0xff) << bit_inx;
     }
 }
 
