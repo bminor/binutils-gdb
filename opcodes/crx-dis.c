100b122fc125bdf1fe768a3331a0cd413c3d1261
Alan Modra <amodra@gmail.com>
2019-12-26 16:04:20 +1030

    ubsan: crx: index 5 out of bounds for type 'operand_desc const[5]'
    	* crx-dis.c (get_number_of_operands): Don't access operands[]
    	out of bounds.

--- opcodes/crx-dis.c
+++ opcodes/crx-dis.c
@@ -105,7 +105,7 @@ get_number_of_operands (void)
 {
   int i;
 
-  for (i = 0; instruction->operands[i].op_type && i < MAX_OPERANDS; i++)
+  for (i = 0; i < MAX_OPERANDS && instruction->operands[i].op_type; i++)
     ;
 
   return i;
