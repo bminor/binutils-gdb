b3adc24a0713411ab38a21dc894dd40dbc5c8f4f
Alan Modra <amodra@gmail.com>
2020-01-01 18:27:01 +1030

    Update year range in copyright notice of binutils files

--- opcodes/crx-dis.c
+++ opcodes/crx-dis.c
@@ -1,5 +1,5 @@
 /* Disassembler code for CRX.
-   Copyright (C) 2004-2019 Free Software Foundation, Inc.
+   Copyright (C) 2004-2020 Free Software Foundation, Inc.
    Contributed by Tomer Levi, NSC, Israel.
    Written by Tomer Levi.
 
