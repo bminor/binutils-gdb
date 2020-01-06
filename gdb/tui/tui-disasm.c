a61b4f6997cefbf28df45d5e1fa40663f0679ba8
Shahab Vahedi <shahab@synopsys.com>
2020-01-06 13:54:18 +0100

    GDB: Remove trailing spaces in tui-disasm.c
    A few trailing spaces are removed.
    
    gdb/ChangeLog:
    2020-01-06  Shahab Vahedi  <shahab@synopsys.com>
    
    	* tui/tui-disasm.c: Remove trailing spaces.

--- gdb/tui/tui-disasm.c
+++ gdb/tui/tui-disasm.c
@@ -43,7 +43,7 @@
 
 #include "gdb_curses.h"
 
-struct tui_asm_line 
+struct tui_asm_line
 {
   CORE_ADDR addr;
   std::string addr_string;
@@ -150,7 +150,7 @@ tui_find_disassembly_address (struct gdbarch *gdbarch, CORE_ADDR pc, int from)
       CORE_ADDR last_addr;
       int pos;
       struct bound_minimal_symbol msymbol;
-              
+
       /* Find backward an address which is a symbol and for which
          disassembling from that address will fill completely the
          window.  */
@@ -176,7 +176,7 @@ tui_find_disassembly_address (struct gdbarch *gdbarch, CORE_ADDR pc, int from)
         do
           {
             CORE_ADDR next_addr;
-                 
+
             pos++;
             if (pos >= max_lines)
               pos = 0;
