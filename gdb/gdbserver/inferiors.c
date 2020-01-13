89e94ec9af58bfab38d469fe42c3eaf2aeb35e31
Simon Marchi <simon.marchi@efficios.com>
2020-01-13 14:03:18 -0500

    gdbserver: include gdbsupport/common-inferior.h in inferiors.c
    So that the definitions of get_inferior_cwd/set_inferior_cwd see their
    declarations.
    
          CXX    inferiors.o
        /home/smarchi/src/binutils-gdb/gdb/gdbserver/inferiors.c: In function ‘const char* get_inferior_cwd()’:
        /home/smarchi/src/binutils-gdb/gdb/gdbserver/inferiors.c:228:1: error: no previous declaration for ‘const char* get_inferior_cwd()’ [-Werror=missing-declarations]
         get_inferior_cwd ()
         ^~~~~~~~~~~~~~~~
        /home/smarchi/src/binutils-gdb/gdb/gdbserver/inferiors.c: In function ‘void set_inferior_cwd(const char*)’:
        /home/smarchi/src/binutils-gdb/gdb/gdbserver/inferiors.c:236:1: error: no previous declaration for ‘void set_inferior_cwd(const char*)’ [-Werror=missing-declarations]
         set_inferior_cwd (const char *cwd)
         ^~~~~~~~~~~~~~~~
    
    gdb/gdbserver/ChangeLog:
    
    	* inferiors.c: Include gdbsupport/common-inferior.h.
    
    Change-Id: Iae5ccb3e1dc37ce79f03f08465f603a0411e7af0

--- gdb/gdbserver/inferiors.c
+++ gdb/gdbserver/inferiors.c
@@ -19,6 +19,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "server.h"
+#include "gdbsupport/common-inferior.h"
 #include "gdbthread.h"
 #include "dll.h"
 
