4f18d7423cdbddc6b8d4a23f4a8c6ae226c576ec
Claudiu Zissulescu <claziss@gmail.com>
2020-01-13 11:16:47 +0200

    [ARC][committed] Update test matching pattern.
    xxxx-xx-xx  Claudiu Zissulescu <claziss@gmail.com>
    
            * testsuite/ld-arc/relax-local-pic.d: Improve matching patterns.

--- ld/testsuite/ld-arc/relax-local-pic.d
+++ ld/testsuite/ld-arc/relax-local-pic.d
@@ -8,7 +8,7 @@
 
 Disassembly of section \.text:
 
-00000100 <__start>:
- 100:	2700 7f84 0000 [0-9a-f]+ 	add	r4,pcl,0x[0-9a-f]+.*
-			104: R_ARC_PC32	a_in_other_thread
- 108:	1c00 [0-9a-f\s]+	st\s+.*
+[0-9a-f]+ <__start>:
+\s+[0-9a-f]+:	2700 7f84 0000 [0-9a-f]+\s+add\s+r4,pcl,.*
+			[0-9a-f]+: R_ARC_PC32	a_in_other_thread
+\s+[0-9a-f]+:	1c00 [0-9a-f\s]+	st\s+.*
