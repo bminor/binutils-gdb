c3036ed0633483325bf37e5cf70d44c7b66bfc61
Richard Sandiford <richard.sandiford@arm.com>
2020-01-31 08:03:56 +0000

    aarch64: Fix MOVPRFX markup for bf16 conversions
    bfcvt converts a .S input to a .H output, so any predicated movprfx
    needs to operate on .S rather than .H.  In common with SVE2 narrowing
    top operations, bfcvtnt doesn't accept movprfx.
    
    2020-01-31  Richard Sandiford  <richard.sandiford@arm.com>
    
    opcodes/
    	* aarch64-tbl.h (aarch64_opcode): Set C_MAX_ELEM for SVE bfcvt.
    	Remove C_SCAN_MOVPRFX for SVE bfcvtnt.
    
    gas/
    	* testsuite/gas/aarch64/sve-bfloat-movprfx.s: Use .h rather than
    	.s for the movprfx.
    	* testsuite/gas/aarch64/sve-bfloat-movprfx.d: Update accordingly.
    	* testsuite/gas/aarch64/sve-movprfx_28.d,
    	* testsuite/gas/aarch64/sve-movprfx_28.l,
    	* testsuite/gas/aarch64/sve-movprfx_28.s: New test.

--- gas/testsuite/gas/aarch64/sve-bfloat-movprfx.d
+++ gas/testsuite/gas/aarch64/sve-bfloat-movprfx.d
@@ -23,5 +23,5 @@ Disassembly of section \.text:
  *[0-9a-f]+:	64e34440 	bfmlalt	z0\.s, z2\.h, z3\.h\[0\]
  *[0-9a-f]+:	0420bc20 	movprfx	z0, z1
  *[0-9a-f]+:	658aa040 	bfcvt	z0\.h, p0/m, z2\.s
- *[0-9a-f]+:	04512020 	movprfx	z0\.h, p0/m, z1\.h
+ *[0-9a-f]+:	04912020 	movprfx	z0\.s, p0/m, z1\.s
  *[0-9a-f]+:	658aa040 	bfcvt	z0\.h, p0/m, z2\.s
