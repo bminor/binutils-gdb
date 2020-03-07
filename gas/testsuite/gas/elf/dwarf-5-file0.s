190e5fc8b3cb8da10d17bf23d81c74cfefdb4669
Alan Modra <amodra@gmail.com>
2020-03-07 20:09:57 +1030

    Re: Add support for a ".file 0" directive if supporting DWARF 5 or higher.
    Fixes a fail on hppa64-hp-hpux, where anything in the first column is
    a label.
    
    	* testsuite/gas/elf/dwarf-5-file0.s: Don't start directives in
    	first column.

--- gas/testsuite/gas/elf/dwarf-5-file0.s
+++ gas/testsuite/gas/elf/dwarf-5-file0.s
@@ -1,18 +1,17 @@
 	.text
 
-.file 0
-.line 1
+ .file 0
+ .line 1
 	.long 3
 
-.file 1 "asdf"
-.line 2
+ .file 1 "asdf"
+ .line 2
 	.long 5
 
-.file 0
-.line 4
+ .file 0
+ .line 4
 	.long 3
 
-.file 2 "def"
-.line 5
+ .file 2 "def"
+ .line 5
 	.long 3
-
