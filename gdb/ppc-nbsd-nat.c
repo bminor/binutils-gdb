52feded7781821d227db0be188e564698d29fe9e
Kamil Rytarowski <n54@gmx.com>
2020-03-14 12:51:15 +0100

    Define _KERNTYPES in ppc-nbsd-nat.c
    Fixes build on NetBSD. types.h does not define register_t by default.
    
    gdb/ChangeLog:
    
    	* ppc-nbsd-nat.c: Define _KERNTYPES to get the declaration of
    	register_t.

--- gdb/ppc-nbsd-nat.c
+++ gdb/ppc-nbsd-nat.c
@@ -19,6 +19,8 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+/* We define this to get types like register_t.  */
+#define _KERNTYPES
 #include "defs.h"
 
 #include <sys/types.h>
