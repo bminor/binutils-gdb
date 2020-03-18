aef397a1548908f82907854a6efc0cd4c0539904
Christophe Lyon <christophe.lyon@linaro.org>
2020-03-18 12:36:53 +0000

    Non-contiguous memory regions support: Fix testcases after previous commit
    2020-03-18  Christophe Lyon  <christophe.lyon@linaro.org>
    
    	* testsuite/ld-arm/non-contiguous-arm4.d: Fix expected output.
    	* testsuite/ld-powerpc/non-contiguous-powerpc.d: Likewise.
    
    Change-Id: I8870800177e1ff79cdd05af13613ec53ef95681f

--- ld/testsuite/ld-powerpc/non-contiguous-powerpc.d
+++ ld/testsuite/ld-powerpc/non-contiguous-powerpc.d
@@ -1,5 +1,5 @@
 #name: non-contiguous-powerpc
 #source: non-contiguous-powerpc.s
 #ld: --enable-non-contiguous-regions -T non-contiguous-powerpc.ld
-#error: \ARelaxation not supported with --enable-non-contiguous-regions.*
+#error: \A.*Relaxation not supported with --enable-non-contiguous-regions.*
 #skip: powerpc64*-*
