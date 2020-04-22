1d934d768573dc3e54f32d21f13e962718fcd47f
Alan Modra <amodra@gmail.com>
2020-04-22 21:09:31 +0930

    Correct mingw target select
    x86_64-w64-mingw32  +FAIL: ld-scripts/default-script1
    x86_64-w64-mingw32  +FAIL: ld-scripts/default-script2
    x86_64-w64-mingw32  +FAIL: ld-scripts/default-script3
    x86_64-w64-mingw32  +FAIL: ld-scripts/default-script4
    
    	* testsuite/ld-scripts/default-script1.d: Correct mingw skip.
    	* testsuite/ld-scripts/default-script2.d: Likewise.
    	* testsuite/ld-scripts/default-script3.d: Likewise.
    	* testsuite/ld-scripts/default-script4.d: Likewise.

--- ld/testsuite/ld-scripts/default-script2.d
+++ ld/testsuite/ld-scripts/default-script2.d
@@ -1,7 +1,7 @@
 # source: default-script.s
 # ld: -T default-script.t -defsym _START=0x800
 # nm: -n
-# skip: *-*-mingw64 x86_64-*-cygwin
+# skip: x86_64-*-mingw* x86_64-*-cygwin
 # Skipped on Mingw64 and Cygwin because the image base defaults to 0x100000000
 
 #...
