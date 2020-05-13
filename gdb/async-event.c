f7e23710fcb7133a3bbe7795ad0ddd2defca358a
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2020-05-13 14:53:37 +0200

    gdb: update the copyright year in async-event.[ch]
    The async-event.[ch] files were introduced recently as a result of
    splitting the event-loop.  I believe the copyright year update was
    just an oversight.  So, this patch fixes that.
    
    gdb/ChangeLog:
    2020-05-13  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* async-event.c: Update the copyright year.
    	* async-event.h: Update the copyright year.

--- gdb/async-event.c
+++ gdb/async-event.c
@@ -1,5 +1,5 @@
 /* Async events for the GDB event loop.
-   Copyright (C) 1999-2019 Free Software Foundation, Inc.
+   Copyright (C) 1999-2020 Free Software Foundation, Inc.
 
    This file is part of GDB.
 
