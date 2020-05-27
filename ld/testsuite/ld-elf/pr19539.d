2a37823bfe57d43f6c43a48a00237ba169599c3c
Hans-Peter Nilsson <hp@axis.com>
2020-05-27 23:14:45 +0200

    PR ld/22909 amendment; don't xfail ld-elf/pr19539.d for cris*-*-*.
    Noticed by my autotester covering cris-axis-linux-gnu:
    ...
    Running <x>/src/ld/testsuite/ld-elf/elf.exp ...
    XPASS: ld-elf/pr19539
    ...
    
    This was apparently as intended, so I'm just adjusting the xfail.
    
    	PR ld/22909
    	* testsuite/ld-elf/pr19539.d: Don't xfail for cris*-*-*.

--- ld/testsuite/ld-elf/pr19539.d
+++ ld/testsuite/ld-elf/pr19539.d
@@ -4,7 +4,7 @@
 #readelf : --dyn-syms --wide
 #warning: .*: creating DT_TEXTREL in a PIE
 #target: *-*-linux* *-*-gnu* *-*-solaris* arm*-*-uclinuxfdpiceabi
-#xfail: cris*-*-* ![check_pie_support] 
+#xfail: ![check_pie_support]
 
 Symbol table '\.dynsym' contains [0-9]+ entr(y|ies):
 #pass
