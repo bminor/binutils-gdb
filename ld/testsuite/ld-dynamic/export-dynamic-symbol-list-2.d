a0c0ad56704002e6438bdd077378de8d8cc1e1a9
Alan Modra <amodra@gmail.com>
2020-06-04 12:41:46 +0930

    ld-dynamic test fixes
    	* testsuite/ld-dynamic/export-dynamic-symbol-2.d: Match output for
    	mips-sgi-irix6.
    	* testsuite/ld-dynamic/export-dynamic-symbol-glob.d: Likewise.
    	* testsuite/ld-dynamic/export-dynamic-symbol-list-2.d: Likewise.
    	* testsuite/ld-dynamic/export-dynamic-symbol-list-glob.d: Likewise.
    	* testsuite/ld-dynamic/export-dynamic-symbol.exp: Exclude targets
    	with poor PIE support.

--- ld/testsuite/ld-dynamic/export-dynamic-symbol-list-2.d
+++ ld/testsuite/ld-dynamic/export-dynamic-symbol-list-2.d
@@ -2,8 +2,9 @@
 #source: export-dynamic-symbol.s
 #ld: -pie -u foo --export-dynamic-symbol-list foo-bar.list tmpdir/libpr25910.a
 #nm: -D
+# See comment in export-dynamic-symbol-2.d for why A is allowed here.
 
 #...
-[0-9a-f]+ T +bar
-[0-9a-f]+ T +foo
+[0-9a-f]+ [AT] +bar
+[0-9a-f]+ [AT] +foo
 #...
--- ld/testsuite/ld-dynamic/export-dynamic-symbol-list-glob.d
+++ ld/testsuite/ld-dynamic/export-dynamic-symbol-list-glob.d
@@ -2,7 +2,8 @@
 #source: export-dynamic-symbol.s
 #ld: -pie -u foo --export-dynamic-symbol-list fstar.list tmpdir/libpr25910.a
 #nm: -D
+# See comment in export-dynamic-symbol-2.d for why A is allowed here.
 
 #...
-[0-9a-f]+ T +foo
+[0-9a-f]+ [AT] +foo
 #...
