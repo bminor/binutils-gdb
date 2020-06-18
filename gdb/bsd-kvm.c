5d971d48b922afc1cfe3ba1798477473cfbd052e
Pedro Alves <palves@redhat.com>
2020-06-18 21:28:32 +0100

    Don't write to inferior_ptid in bsd-kvm.c
    gdb/ChangeLog:
    2020-06-18  Pedro Alves  <palves@redhat.com>
    
    	* bsd-kvm.c (bsd_kvm_target_open): Switch to thread after adding
    	it, instead of writing to inferior_ptid.

--- gdb/bsd-kvm.c
+++ gdb/bsd-kvm.c
@@ -136,8 +136,8 @@ bsd_kvm_target_open (const char *arg, int from_tty)
   core_kd = temp_kd;
   push_target (&bsd_kvm_ops);
 
-  add_thread_silent (&bsd_kvm_ops, bsd_kvm_ptid);
-  inferior_ptid = bsd_kvm_ptid;
+  thread_info *thr = add_thread_silent (&bsd_kvm_ops, bsd_kvm_ptid);
+  switch_to_thread (thr);
 
   target_fetch_registers (get_current_regcache (), -1);
 
