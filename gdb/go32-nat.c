1ee1a363454d88a87ad2ade7530b2a7fb670021e
Pedro Alves <palves@redhat.com>
2020-06-18 21:28:33 +0100

    Don't write to inferior_ptid in go32-nat.c
    gdb/ChangeLog:
    2020-06-18  Pedro Alves  <palves@redhat.com>
    
    	* go32-nat.c (go32_nat_target::create_inferior): Switch to thread
    	after creating it, instead of writing to inferior_ptid.  Don't
    	write to inferior_ptid.

--- gdb/go32-nat.c
+++ gdb/go32-nat.c
@@ -753,14 +753,14 @@ go32_nat_target::create_inferior (const char *exec_file,
   save_npx ();
 #endif
 
-  inferior_ptid = ptid_t (SOME_PID);
   inf = current_inferior ();
   inferior_appeared (inf, SOME_PID);
 
   if (!target_is_pushed (this))
     push_target (this);
 
-  add_thread_silent (inferior_ptid);
+  thread_info *thr = add_thread_silent (ptid_t (SOME_PID));
+  switch_to_thread (thr);
 
   clear_proceed_status (0);
   insert_breakpoints ();
