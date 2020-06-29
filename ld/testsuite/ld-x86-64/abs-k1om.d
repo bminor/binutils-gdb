290a25dc2da6f1069f53a063ba8167bd9baf7f9c
Alan Modra <amodra@gmail.com>
2020-06-29 10:04:11 +0930

    x86_64 k1om tests
    On x86_64-nacl we currently see
    FAIL: Absolute non-overflowing relocs
    FAIL: ld-x86-64/protected2-k1om
    FAIL: ld-x86-64/protected3-k1om
    
    This limits the tests as per the l1om variants.
    
    	* testsuite/ld-x86-64/abs-k1om.d: Run only on x86_64-*-linux*.
    	* testsuite/ld-x86-64/protected2-k1om.d: Likewise.
    	* testsuite/ld-x86-64/protected3-k1om.d: Likewise.

--- ld/testsuite/ld-x86-64/abs-k1om.d
+++ ld/testsuite/ld-x86-64/abs-k1om.d
@@ -4,6 +4,7 @@
 #as: --64 -march=k1om
 #ld: -m elf_k1om -z noseparate-code
 #objdump: -rs -j .text
+#target: x86_64-*-linux*
 
 .*:     file format .*
 
--- ld/testsuite/ld-x86-64/protected2-k1om.d
+++ ld/testsuite/ld-x86-64/protected2-k1om.d
@@ -2,6 +2,7 @@
 #as: --64 -march=k1om
 #ld: -shared -melf_k1om
 #objdump: -drw
+#target: x86_64-*-linux*
 
 .*: +file format .*
 
