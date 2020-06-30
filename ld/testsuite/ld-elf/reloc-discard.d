b9b9b0ef34c4ea7e75f5fc7cf3882e0485675e61
Alan Modra <amodra@gmail.com>
2020-06-30 13:47:32 +0930

    Don't xfail nds32 for ld-elf/reloc-discard test
    	* testsuite/ld-elf/reloc-discard.d: Don't xfail nds32.

--- ld/testsuite/ld-elf/reloc-discard.d
+++ ld/testsuite/ld-elf/reloc-discard.d
@@ -4,7 +4,7 @@
 #target: [check_shared_lib_support]
 #skip: powerpc64*-*-*
 #source: reloc-discard.s
-#xfail: nds32*-*-* tic6x-*-*
+#xfail: tic6x-*-*
 # Need to figure out how to pass `-fpic' for NDS32 or `-mpic -mpid=near'
 # for TI C6X targets to GAS for this test.
 # PowerPC64 warns when discarding dynamic relocs, which is generally
