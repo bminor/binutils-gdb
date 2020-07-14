b3239a5e9ee881c390ef688ba7948f652dd8ae71
H.J. Lu <hjl.tools@gmail.com>
2020-07-14 09:57:52 -0700

    x86-64: Zero-extend lower 32 bits displacement to 64 bits
    Since the addr32 (0x67) prefix zero-extends the lower 32 bits address to
    64 bits, change disassembler to zero-extend the lower 32 bits displacement
    to 64 bits when there is no base nor index registers.
    
    gas/
    
    	PR gas/26237
    	* testsuite/gas/i386/addr32.s: Add tests for 32-bit wrapped around
    	address.
    	* testsuite/gas/i386/x86-64-addr32.s: Likewise.
    	* testsuite/gas/i386/addr32.d: Updated.
    	* testsuite/gas/i386/x86-64-addr32-intel.d: Likewise.
    	* testsuite/gas/i386/x86-64-addr32.d: Likewise.
    	* testsuite/gas/i386/ilp32/x86-64-addr32-intel.d: Likewise.
    	* testsuite/gas/i386/ilp32/x86-64-addr32.d: Likewise.
    
    opcodes/
    
    	PR gas/26237
    	* i386-dis.c (OP_E_memory): Without base nor index registers,
    	32-bit displacement to 64 bits.
    
    (cherry picked from commit 8e58ef803cf752cbde547a5064540f05470ea714)

--- gas/testsuite/gas/i386/x86-64-addr32.d
+++ gas/testsuite/gas/i386/x86-64-addr32.d
@@ -17,12 +17,18 @@ Disassembly of section .text:
 [ 	]*[a-f0-9]+:	67 48 a1 98 08 60 00 	addr32 mov 0x600898,%rax
 [ 	]*[a-f0-9]+:	67 48 a1 98 08 80 00 	addr32 mov 0x800898,%rax
 [ 	]*[a-f0-9]+:	67 48 8b 1c 25 98 08 80 00 	mov[ ]+0x800898\(,%eiz,1\),%rbx
+[ 	]*[a-f0-9]+:	67 48 a1 ef cd ab 89 	addr32 mov 0x89abcdef,%rax
+[ 	]*[a-f0-9]+:	67 48 8b 1c 25 ef cd ab 89 	mov[ ]+0x89abcdef\(,%eiz,1\),%rbx
+[ 	]*[a-f0-9]+:	67 48 b8 ef cd ab 89 00 00 00 00 	addr32 movabs \$0x89abcdef,%rax
+[ 	]*[a-f0-9]+:	67 48 bb ef cd ab 89 00 00 00 00 	addr32 movabs \$0x89abcdef,%rbx
 [ 	]*[a-f0-9]+:	67 a2 98 08 60 00    	addr32 mov %al,0x600898
 [ 	]*[a-f0-9]+:	67 66 a3 98 08 60 00 	addr32 mov %ax,0x600898
 [ 	]*[a-f0-9]+:	67 a3 98 08 60 00    	addr32 mov %eax,0x600898
 [ 	]*[a-f0-9]+:	67 48 a3 98 08 60 00 	addr32 mov %rax,0x600898
 [ 	]*[a-f0-9]+:	67 48 a3 98 08 80 00 	addr32 mov %rax,0x800898
 [ 	]*[a-f0-9]+:	67 48 89 1c 25 98 08 80 00 	mov[ ]+%rbx,0x800898\(,%eiz,1\)
-[ 	]*[a-f0-9]+:	67 89 04 25 11 22 33 ff 	mov[ ]+%eax,-0xccddef\(,%eiz,1\)
-[ 	]*[a-f0-9]+:	67 89 04 65 11 22 33 ff 	mov[ ]+%eax,-0xccddef\(,%eiz,2\)
+[ 	]*[a-f0-9]+:	67 48 a3 ef cd ab 89 	addr32 mov %rax,0x89abcdef
+[ 	]*[a-f0-9]+:	67 48 89 1c 25 ef cd ab 89 	mov[ ]+%rbx,0x89abcdef\(,%eiz,1\)
+[ 	]*[a-f0-9]+:	67 89 04 25 11 22 33 ff 	mov[ ]+%eax,0xff332211\(,%eiz,1\)
+[ 	]*[a-f0-9]+:	67 89 04 65 11 22 33 ff 	mov[ ]+%eax,0xff332211\(,%eiz,2\)
 #pass
