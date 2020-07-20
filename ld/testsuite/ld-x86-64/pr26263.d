98b3697bcdedb92739d8feb9b881efdd70fd40d3
H.J. Lu <hjl.tools@gmail.com>
2020-07-20 04:53:58 -0700

    x86: Update PR gas/26263 linker tests
    Update and run PR gas/26263 linker tests for all x86 ELF targets to
    accept any program header layout.
    
    	PR gas/26263
    	* testsuite/ld-i386/pr26263.d: Updated.
    	* testsuite/ld-x86-64/pr26263.d: Likewise.
    	* testsuite/ld-x86-64/x86-64.exp: Run gas/26263 test for all ELF
    	targets.

--- ld/testsuite/ld-x86-64/pr26263.d
+++ ld/testsuite/ld-x86-64/pr26263.d
@@ -7,12 +7,12 @@
 
 Disassembly of section .text:
 
-0+1000 <printk>:
+0+[a-f0-9]+ <printk>:
  +[a-f0-9]+:	c3                   	ret    
 
 Disassembly of section .init.text:
 
-0+1001 <foo>:
- +[a-f0-9]+:	e8 fa ff ff ff       	call   1000 <printk>
- +[a-f0-9]+:	e8 f5 ff ff ff       	call   1000 <printk>
+0+[a-f0-9]+ <foo>:
+ +[a-f0-9]+:	e8 ([0-9a-f]{2} ){4}      	call   [a-f0-9]+ <printk>
+ +[a-f0-9]+:	e8 ([0-9a-f]{2} ){4}      	call   [a-f0-9]+ <printk>
 #pass
