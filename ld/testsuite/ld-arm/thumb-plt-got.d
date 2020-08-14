84bbb9672e940567fe4c6aefc8a74a365a1db813
Tamar Christina <tamar.christina@arm.com>
2020-08-14 07:51:03 +0100

    AArch64: Relax thumb-plt testcases regexpr.
    The regexpr in these two files are a bit strict in that they don't account for
    the slight changes in tags in certain arm targets which cause our address
    offsets to change.   This changes the tests to allow slight movement in
    locations while still strictly checking the rest.
    
    ld/ChangeLog:
    
    2020-08-03  Tamar Christina  <tamar.christina@arm.com>
    
    	* testsuite/ld-arm/thumb-plt-got.d: Relax regexpr.
    	* testsuite/ld-arm/thumb-plt.d: Likewise.
    
    (cherry picked from commit 95c210664d3eedad480fbade0eceb81af796259d)

--- ld/testsuite/ld-arm/thumb-plt-got.d
+++ ld/testsuite/ld-arm/thumb-plt-got.d
@@ -6,9 +6,9 @@
 
 Relocation section '.rel.plt' at offset 0x108 contains 1 entry:
  Offset     Info    Type            Sym.Value  Sym. Name
-000101c4  00000116 R_ARM_JUMP_SLOT   00000000   foo
+000101.+  00000116 R_ARM_JUMP_SLOT   00000000   foo
 
 Hex dump of section '.got':
  NOTE: This section has relocations against it, but these have NOT been applied to this dump.
-  0x000101b8 40010100 00000000 00000000 11010000 @...............
+  0x000101.+ 40010100 00000000 00000000 11010000 @...............
 
