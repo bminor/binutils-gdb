f5fc30d05c7e2aaba4fe892ab52fbf0930342dac
Peter Bergner <bergner@linux.ibm.com>
2020-08-18 12:42:31 -0500

    PowerPC: Rename xvcvbf16sp to xvcvbf16spn
    The xvcvbf16sp mnemonic has been renamed to xvcvbf16spn, to be consistent
    with the other non-signaling conversion instructions which all end with "n".
    
    opcodes/
    	* ppc-opc.c (powerpc_opcodes) <xvcvbf16sp>: Rename from this...
    	<xvcvbf16spn>: ...to this.
    
    gas/
    	* testsuite/gas/ppc/vsx4.s: Update test to use new mnemonic.
    	* testsuite/gas/ppc/vsx4.d: Likewise.

--- gas/testsuite/gas/ppc/vsx4.d
+++ gas/testsuite/gas/ppc/vsx4.d
@@ -7,6 +7,6 @@
 Disassembly of section \.text:
 
 0+0 <vsx4>:
-.*:	(f0 50 6f 6f|6f 6f 50 f0) 	xvcvbf16sp vs34,vs45
+.*:	(f0 50 6f 6f|6f 6f 50 f0) 	xvcvbf16spn vs34,vs45
 .*:	(f1 f1 27 6f|6f 27 f1 f1) 	xvcvspbf16 vs47,vs36
 #pass
