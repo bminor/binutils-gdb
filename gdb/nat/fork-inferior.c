f404573e4f8d22f0f1f87eee4f08fa132b15a9aa
Kamil Rytarowski <n54@gmx.com>
2020-09-02 19:24:05 +0200

    Avoid double free in startup_inferior
    Do not free the last execd pathname as it will be used in
    prepare_resume_reply(), after attaching a client side.
    
    gdb/ChangeLog:
    
    	* fork-inferior.c (startup_inferior): Avoid double free.

--- gdb/nat/fork-inferior.c
+++ gdb/nat/fork-inferior.c
@@ -526,7 +526,10 @@ startup_inferior (process_stratum_target *proc_target, pid_t pid, int ntraps,
 
 	  case TARGET_WAITKIND_EXECD:
 	    /* Handle EXEC signals as if they were SIGTRAP signals.  */
-	    xfree (ws.value.execd_pathname);
+	    /* Free the exec'ed pathname, but only if this isn't the
+	       waitstatus we are returning to the caller.  */
+	    if (pending_execs != 1)
+	      xfree (ws.value.execd_pathname);
 	    resume_signal = GDB_SIGNAL_TRAP;
 	    switch_to_thread (proc_target, event_ptid);
 	    break;
