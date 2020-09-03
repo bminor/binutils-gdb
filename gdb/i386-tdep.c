e56798df08154c6e5155d3bb6cb536a14de0ebb4
Alok Kumar Sharma <AlokKumar.Sharma@amd.com>
2020-09-03 21:51:01 +0530

    Support printing of 16 byte real/complex type for Flang compiler
    Currently GDB is not able to print correct value for real/complex type
    from binary generated from Flang compiler. This is due to GDB not able
    to recognise and determine correct format floatformats_ia64_quad and
    instead falling back to default_floatformat_for_type. This leads
    incorrect output.
    Now function i386_floatformat_for_type is fixed to correctly identify
    Flang generated 16 byte real/complex type.
    
    gdb/ChangeLog
    
    	* gdb/i386-tdep.c (i386_floatformat_for_type): Added conditions
    	to match 16 byte real/complex type generated by Flang compiler.

--- gdb/i386-tdep.c
+++ gdb/i386-tdep.c
@@ -8211,7 +8211,9 @@ i386_floatformat_for_type (struct gdbarch *gdbarch,
 	|| strcmp (name, "_Float128") == 0
 	|| strcmp (name, "complex _Float128") == 0
 	|| strcmp (name, "complex(kind=16)") == 0
-	|| strcmp (name, "real(kind=16)") == 0)
+	|| strcmp (name, "quad complex") == 0
+	|| strcmp (name, "real(kind=16)") == 0
+	|| strcmp (name, "real*16") == 0)
       return floatformats_ia64_quad;
 
   return default_floatformat_for_type (gdbarch, name, len);
