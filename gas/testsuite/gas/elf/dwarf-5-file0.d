d2a54558074287721ce9e47142f7fa92bda15531
Mark Wielaard <mark@klomp.org>
2020-09-07 14:03:20 +0100

    gas: Output directory and file names in .debug_line_str for DWARF5
    	* dwarf2dbg.c (add_line_strp): New function.
    	(out_dir_and_file_list): Take line_seg and sizeof_offset as
    	arguments, Use DW_FORM_line_strp for dir and file. Call
    	add_line_strp and set symbol offset for DWARF2_LINE_VERSION 5.
    	(out_debug_line): Call out_dir_and_file_list with line_seg and
    	sizeof_offset.
    	* gas/testsuite/gas/elf/dwarf-5-file0.d: Expect indirect line
    	strings.

--- gas/testsuite/gas/elf/dwarf-5-file0.d
+++ gas/testsuite/gas/elf/dwarf-5-file0.d
@@ -5,15 +5,15 @@
 #...
  The Directory Table \(offset 0x.*, lines 3, columns 1\):
   Entry	Name
-  0	master directory
-  1	secondary directory
-  2	/tmp
+  0	\(indirect line string, offset: 0x.*\): master directory
+  1	\(indirect line string, offset: 0x.*\): secondary directory
+  2	\(indirect line string, offset: 0x.*\): /tmp
 
  The File Name Table \(offset 0x.*, lines 3, columns 3\):
   Entry	Dir	MD5				Name
-  0	0 0x00000000000000000000000000000000	master source file
-  1	1 0x00000000000000000000000000000000	secondary source file
-  2	2 0x95828e8bc4f7404dbf7526fb7bd0f192	foo.c
+  0	0 0x00000000000000000000000000000000	\(indirect line string, offset: 0x.*\): master source file
+  1	1 0x00000000000000000000000000000000	\(indirect line string, offset: 0x.*\): secondary source file
+  2	2 0x95828e8bc4f7404dbf7526fb7bd0f192	\(indirect line string, offset: 0x.*\): foo.c
 #pass
 
 
