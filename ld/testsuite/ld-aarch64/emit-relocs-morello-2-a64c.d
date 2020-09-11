fba8f966caf264c31c3ba4aa232094e53d75ca8c
Siddhesh Poyarekar <siddesh.poyarekar@arm.com>
2020-09-11 09:18:10 +0530

    [Morello] Linker tests for capability data relocations
    ld/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* testsuite/ld-aarch64/emit-relocs-morello-2-a64c.d: New test.
    	* testsuite/ld-aarch64/emit-relocs-morello-2.d: Likewise.
    	* testsuite/ld-aarch64/emit-relocs-morello-2.s: Likewise.
    	* testsuite/ld-aarch64/emit-relocs-morello-3-a64c.d: Likewise.
    	* testsuite/ld-aarch64/emit-relocs-morello-3.d: Likewise.
    	* testsuite/ld-aarch64/emit-relocs-morello-3.s: Likewise.
    	* testsuite/ld-aarch64/emit-relocs-morello-a64c.d: Likewise.
    	* testsuite/ld-aarch64/emit-relocs-morello.d: Likewise.
    	* testsuite/ld-aarch64/emit-relocs-morello.s: Likewise.
    	* testsuite/ld-aarch64/aarch64-elf.exp: Add new tests.

---
+++ ld/testsuite/ld-aarch64/emit-relocs-morello-2-a64c.d
@@ -0,0 +1,45 @@
+#source: emit-relocs-morello-2.s
+#as: -march=morello+a64c
+#ld: -shared
+#objdump: -DR -j .text -j .data -j .got
+
+.*:     file format .*
+
+
+Disassembly of section .text:
+
+.* <_start>:
+.*:	90000082 	adrp	x2, [0-9a-f]+ <.*>
+.*:	826...42 	ldr	c2, \[c2, #[0-9]+\]
+.*:	c24...42 	ldr	c2, \[x2, #[0-9]+\]
+.*:	f94...42 	ldr	x2, \[x2, #[0-9]+\]
+
+Disassembly of section .got:
+
+.* <.got>:
+.*:	[0-9a-f]+ 	.*
+	...
+			.*: R_MORELLO_GLOB_DAT	cap
+
+Disassembly of section .data:
+
+.* <str>:
+.*:	6c6c6548 	.*
+.*:	6874206f 	.*
+.*:	20657265 	.*
+.*:	20676962 	.*
+.*:	20646162 	.*
+.*:	6c726f77 	.*
+.*:	.*
+			.*: R_AARCH64_RELATIVE	\*ABS\*\+.*
+
+.* <ptr>:
+.*:	[0-9a-f]+ 	.*
+	...
+
+.* <cap>:
+.*:	[0-9a-f]+ 	.*
+			.*: R_MORELLO_RELATIVE	\*ABS\*
+.*:	00000000 	.*
+.*:	00001b02 	.*
+.*:	00000000 	.*
