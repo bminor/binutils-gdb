7ff36d1a856fce85c7ad854270289815758a8816
Siddhesh Poyarekar <siddesh.poyarekar@arm.com>
2020-09-11 09:18:11 +0530

    [Morello] Pad section alignment to account for capability range format
    The capability format has limitations on the alignment and length of
    capability bounds and are subject to rounding.  Add alignment and
    padding at the boundaries of such long (typically >16M) sections so
    that any capabilities referencing these sections do not end up
    overlapping into neighbouring sections.
    
    There are two cases where this is in use.  The first and most
    important due to the current implementation is the range for PCC,
    which needs to span all executable sections and all PLT and GOT
    sections.  The other case is for linker and ldscript defined symbols
    that may be used in dynamic relocations.
    
    bfd/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* elfnn-aarch64.c (elf_aarch64_link_hash_table): New member.
    	(section_start_symbol, c64_valid_cap_range, exponent): Move
    	up.
    	(sec_change_queue): New structure.
    	(queue_section_padding, record_section_change,
    	elfNN_c64_resize_sections): New functions.
    	(bfd_elfNN_aarch64_init_maps): Add info argument.  Adjust
    	callers.
    	* elfxx-aarch64.h (bfd_elf64_aarch64_init_maps,
    	bfd_elf32_aarch64_init_maps): Add info argument.
    	(elf64_c64_resize_sections, elf32_c64_resize_sections): New
    	function declarations.
    
    ld/ChangeLog:
    
    2020-10-20  Siddhesh Poyarekar  <siddesh.poyarekar@arm.com>
    
    	* emultempl/aarch64elf.em (elf64_c64_pad_section): New
    	function.
    	(gld${EMULATION_NAME}_after_allocation): Resize C64 sections.
    	* ldlang.c (lang_add_newdot): New function.
    	* ldlang.h (lang_add_newdot): New function declaration.
    	* testsuite/ld-aarch64/aarch64-elf.exp: Add new test.
    	* testsuite/ld-aarch64/morello-sec-round.d: New file.
    	* testsuite/ld-aarch64/morello-sec-round.ld: New file.
    	* testsuite/ld-aarch64/morello-sec-round.s: New file.

---
+++ ld/testsuite/ld-aarch64/morello-sec-round.d
@@ -0,0 +1,11 @@
+#as: -march=morello+c64
+#ld: -static -pie -T morello-sec-round.ld
+#objdump: -x
+#...
+Sections:
+Idx Name          Size      VMA               LMA               File off  Algn
+#...
+  1 .text_sec     00020004  ...........20000  [0-9a-f]+  [0-9a-f]+  2\*\*17
+                  CONTENTS, ALLOC, LOAD, READONLY, CODE
+  2 \..*       [0-9a-f]+  ...........40080  [0-9a-f]+  [0-9a-f]+  .*
+#pass
