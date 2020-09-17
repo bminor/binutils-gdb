7474326cb9441644313cb749e491bec5e001d236
Pedro Alves <pedro@palves.net>
2020-09-17 23:33:43 +0100

    gdb.cp/call-c.exp C++ify
    Make the testcase work when built with a C++ compiler.
    
    gdb/testsuite/ChangeLog:
    
    	* gdb.cp/call-c-1.c (foo) [__cplusplus]: Add extern "C".

--- gdb/testsuite/gdb.cp/call-c-1.c
+++ gdb/testsuite/gdb.cp/call-c-1.c
@@ -15,4 +15,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#ifdef __cplusplus
+extern "C"
+#endif
 int foo(int x) { return x; }
