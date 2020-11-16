1bbda94fe0174714e296335e7cc744b5fb618525
Przemyslaw Wirkus <przemyslaw.wirkus@arm.com>
2020-11-16 20:26:29 +0000

    arm: Add support for Cortex-A78C
    This patch adds support for -mcpu=cortex-a78c command line option.
    For more information about this processor, see [0]:
    
    [0] https://developer.arm.com/ip-products/processors/cortex-a/cortex-a78c

--- bfd/cpu-arm.c
+++ bfd/cpu-arm.c
@@ -152,6 +152,7 @@ processors[] =
   { bfd_mach_arm_8,	  "cortex-a77"	    },
   { bfd_mach_arm_8,	  "cortex-a78"	    },
   { bfd_mach_arm_8,	  "cortex-a78ae"    },
+  { bfd_mach_arm_8,	  "cortex-a78c"     },
   { bfd_mach_arm_6SM,	  "cortex-m0"	    },
   { bfd_mach_arm_6SM,	  "cortex-m0plus"   },
   { bfd_mach_arm_6SM,	  "cortex-m1"	    },
