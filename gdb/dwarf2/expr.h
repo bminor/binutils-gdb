467ee74161474a02026e68e3ca4339355779975b
Zoran Zaric <Zoran.Zaric@amd.com>
2020-12-07 19:00:12 +0000

    Merge evaluate_for_locexpr_baton evaluator
    The evaluate_for_locexpr_baton is the last derived class from the
    dwarf_expr_context class. It's purpose is to support the passed in
    buffer functionality.
    
    Although, it is not really necessary to merge this class with it's
    base class, doing that simplifies new expression evaluator design.
    
    Considering that this functionality is going around the DWARF standard,
    it is also reasonable to expect that with a new evaluator design and
    extending the push object address functionality to accept any location
    description, there will be no need to support passed in buffers.
    
    Alternatively, it would also makes sense to abstract the interaction
    between the evaluator and a given resource in the near future. The
    passed in buffer would then be a specialization of that abstraction.
    
    gdb/ChangeLog:
    
    	* dwarf2/expr.c (dwarf_expr_context::read_mem): Merge with
    	evaluate_for_locexpr_baton implementation.
    	* dwarf2/loc.c (class evaluate_for_locexpr_baton): Remove
    	class.
    	(evaluate_for_locexpr_baton::read_mem): Move to
    	dwarf_expr_context.
    	(dwarf2_locexpr_baton_eval): Instantiate dwarf_expr_context
    	instead of evaluate_for_locexpr_baton class.
    
    Change-Id: I7925e07fa93245ea542af81a9f9f0bfbdac7f309

--- gdb/dwarf2/expr.h
+++ gdb/dwarf2/expr.h
@@ -195,16 +195,8 @@ struct dwarf_expr_context
   /* Object address used for the evaluation.  */
   CORE_ADDR obj_address;
 
-  /* Read LENGTH bytes at ADDR into BUF.  */
-  virtual void read_mem (gdb_byte *buf, CORE_ADDR addr, size_t length);
-
-  /* Return the `object address' for DW_OP_push_object_address.  */
-  virtual CORE_ADDR get_object_address ()
-  {
-    if (obj_address == 0)
-      error (_("Location address is not set."));
-    return obj_address;
-  }
+  /* The data that was passed in.  */
+  gdb::array_view<const gdb_byte> data_view;
 
 private:
 
@@ -239,6 +231,7 @@ private:
   void push_dwarf_reg_entry_value (enum call_site_parameter_kind kind,
 				   union call_site_parameter_u kind_u,
 				   int deref_size);
+  void read_mem (gdb_byte *buf, CORE_ADDR addr, size_t length);
 };
 
 /* Return the value of register number REG (a DWARF register number),
