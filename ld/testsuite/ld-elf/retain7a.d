99d8bab0c125618005b96cbca9bae94919ae93b7
Alan Modra <amodra@gmail.com>
2020-12-24 22:52:51 +1030

    SHF_GNU_RETAIN 7a and 7b tests
    	* testsuite/ld-elf/retain7a.d: Don't run on hppa-linux.
    	* testsuite/ld-elf/retain7b.d: Correct name.  Don't run on tic6x.
    	Allow OSABI FreeBSD.

--- ld/testsuite/ld-elf/retain7a.d
+++ ld/testsuite/ld-elf/retain7a.d
@@ -1,7 +1,8 @@
 #name: SHF_GNU_RETAIN 7a
 #source: retain7.s
 #ld: -e _start --gc-sections
-#notarget: ![supports_gnu_osabi] ![check_gc_sections_available]
+# hppa-linux chooses ELFOSABI_GNU regardless of GNU feature use
+#notarget: ![supports_gnu_osabi] ![check_gc_sections_available] hppa-*-linux*
 #readelf: -h
 
 #failif
