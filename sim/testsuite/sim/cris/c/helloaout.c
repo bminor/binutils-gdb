e6c1dbbfe8bed2b9396f74e3594701c8abe875ad
Mike Frysinger <vapier@gentoo.org>
2021-01-07 01:57:35 -0500

    sim: cris: disable test that crashes the linker
    PR ld/13900
    Linking this test crashes the linker, so disable it.  The crash
    was reported about 9 years ago but haven't made progress, so lets
    avoid the failures in test runs.

--- sim/testsuite/sim/cris/c/helloaout.c
+++ sim/testsuite/sim/cris/c/helloaout.c
@@ -4,7 +4,11 @@
    doesn't help.  Adjust as needed to get a.out which says "pass".  If
    necessary, move to the asm subdir.  By design, it doesn't work with
    CRIS v32.)
-#target: cris-*-elf
+
+NB: We'd rely on kfail, but that doesn't skip compilation, and that's where
+the crash in ld happens to break the testcase.
+#target: disabled-cris-*-elf
+#kfail: ld/13900 cris-*-elf
 #cc: ldflags=-Wl,-mcrisaout\ -sim\ -Ttext=0
 */
 #include "hello.c"
