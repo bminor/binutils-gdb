5f40035fb87b3a98a5834e4fed1e7e9c1f7807e5
Nelson Chu <nelson.chu@sifive.com>
2021-02-05 11:12:59 +0800

    RISC-V: PR27348, Remove the obsolete OP_*CUSTOM_IMM.
    include/
        PR 27348
        * opcode/riscv.h: Remove obsolete OP_*CUSTOM_IMM.

--- include/opcode/riscv.h
+++ include/opcode/riscv.h
@@ -223,8 +223,6 @@ static const char * const riscv_pred_succ[16] =
 #define OP_MASK_RL		0x1
 #define OP_SH_RL		25
 
-#define OP_MASK_CUSTOM_IMM	0x7fU
-#define OP_SH_CUSTOM_IMM	25
 #define OP_MASK_CSR		0xfffU
 #define OP_SH_CSR		20
 
