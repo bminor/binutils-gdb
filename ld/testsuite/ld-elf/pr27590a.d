fba7f7533c97d03d86f648a42375212c38980706
Alan Modra <amodra@gmail.com>
2021-03-19 17:39:39 +1030

    pr27590 testcase fixes
    	PR 27590
    	* testsuite/ld-elf/pr27590a.d: Match relocs using label, and
    	R_MIPS_NONE.
    	* testsuite/ld-elf/pr27590b.d: Likewise.

--- ld/testsuite/ld-elf/pr27590a.d
+++ ld/testsuite/ld-elf/pr27590a.d
@@ -5,6 +5,8 @@
 
 Relocation section '\.rel.*\.gnu\.debuglto_\.debug_macro' at offset 0x[0-9a-z]+ contains 2 entries:
 [ \t]+Offset[ \t]+Info[ \t]+Type[ \t]+Sym.*
-[0-9a-f]+[ \t]+[0-9a-f]+[ \t]+R_.*[ \t]+[0]+[ \t]+\.gnu\.debuglto_\.debug_macro.*
-[0-9a-f]+[ \t]+[0-9a-f]+[ \t]+R_.*[ \t]+[0]+[ \t]+\.gnu\.debuglto_\.debug_macro.*
+[0-9a-f]+[ \t]+[0-9a-f]+[ \t]+R_.*[ \t]+[0]+[ \t]+(\.gnu\.debuglto_\.debug_macro|\.Ldebug_macro2).*
+#?.*R_MIPS_NONE.*
+#?.*R_MIPS_NONE.*
+[0-9a-f]+[ \t]+[0-9a-f]+[ \t]+R_.*[ \t]+[0]+[ \t]+(\.gnu\.debuglto_\.debug_macro|\.Ldebug_macro2).*
 #pass
