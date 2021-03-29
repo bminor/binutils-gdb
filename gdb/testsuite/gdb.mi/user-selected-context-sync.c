aa33ea68330ae5f34013a47b8b7b019a0d1e38e7
Tankut Baris Aktemur <tankut.baris.aktemur@intel.com>
2021-03-29 16:00:41 +0200

    testsuite, mi: avoid a clang bug in 'user-selected-context-sync.exp'
    This test causes several timeouts for Clang, taking too long time to
    finish.  The reason is, for an infinite loop of the form
    
       while(1); /* suppose this is line 30.  */
    
    Clang generates code that looks like
    
       0x00000000004004d4 <+4>:     jmp    0x4004d9 <loop+9>
       0x00000000004004d9 <+9>:     jmp    0x4004d9 <loop+9>
    
    So, the real loop is the instruction at address 0x4004d9.  But a
    breakpoint that's defined at the loop line (assume line 30 in this
    case) is inserted at address 0x4004d4.
    
      (gdb) break 30
      Breakpoint 1 at 0x4004d4: file test.c, line 30.
    
    Therefore, continuing a thread that was spinning on the loop does not hit
    the breakpoint.  The bug is reported at
    
      https://bugs.llvm.org/show_bug.cgi?id=49614
    
    Tweak the infinite loop to spin on a variable to avoid this bug.  The
    test is unrelated to the bug.
    
    gdb/testsuite/ChangeLog:
    2021-03-29  Tankut Baris Aktemur  <tankut.baris.aktemur@intel.com>
    
    	* gdb.mi/user-selected-context-sync.exp: Spin on a variable in
    	the infinite loop to avoid a Clang bug.

--- gdb/testsuite/gdb.mi/user-selected-context-sync.c
+++ gdb/testsuite/gdb.mi/user-selected-context-sync.c
@@ -27,7 +27,10 @@ static pthread_barrier_t barrier;
 static void
 child_sub_function (void)
 {
-  while (1); /* thread loop line */
+  /* Deliberately spin on a variable instead of plain 'while (1)' to
+     avoid the Clang bug https://bugs.llvm.org/show_bug.cgi?id=49614.  */
+  int spin = 1;
+  while (spin); /* thread loop line */
 }
 
 static void *
@@ -57,7 +60,10 @@ main (void)
 
   pthread_barrier_wait (&barrier);
 
-  while (1); /* main break line */
+  /* Deliberately spin on a variable instead of plain 'while (1)' to
+     avoid the Clang bug https://bugs.llvm.org/show_bug.cgi?id=49614.  */
+  int spin = 1;
+  while (spin); /* main break line */
 
   return 0;
 }
