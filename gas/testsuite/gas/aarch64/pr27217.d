b12389f219facfb4aa29b97fdcbc2664a5b0732a
Alan Modra <amodra@gmail.com>
2021-04-07 18:12:38 +0930

    Fix pr27217 testcase failure
    aarch64_be-linux-gnu_ilp32  +FAIL: PR27212
    
    	PR 27217
    	* testsuite/gas/aarch64/pr27217.d: Correct name.  Accept ilp32 relocs.

--- gas/testsuite/gas/aarch64/pr27217.d
+++ gas/testsuite/gas/aarch64/pr27217.d
@@ -1,5 +1,5 @@
 # Check that expressions that generate relocations work when the symbol is a constant.
-#name: PR27212
+#name: PR27217
 #objdump: -rd
 
 .*:     file format .*
@@ -8,8 +8,8 @@ Disassembly of section \.text:
 
 0+000 <.*>:
 [ 	]+0:[ 	]+90000000[ 	]+adrp[ 	]+x0, 12345678[ 	]+<bar>
-[ 	]+0:[ 	]+R_AARCH64_ADR_PREL_PG_HI21[ 	]+bar
+[ 	]+0:[ 	]+R_AARCH64(|_P32)_ADR_PREL_PG_HI21[ 	]+bar
 [ 	]+4:[ 	]+91000000[ 	]+add[ 	]+x0, x0, #0x0
-[ 	]+4:[ 	]+R_AARCH64_ADD_ABS_LO12_NC[ 	]+bar
+[ 	]+4:[ 	]+R_AARCH64(|_P32)_ADD_ABS_LO12_NC[ 	]+bar
 [ 	]+8:[ 	]+d65f03c0[ 	]+ret
 #pass
