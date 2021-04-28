24aebc79b16a82faef4736f4abdc7e4d95d4cc67
Nick Clifton <nickc@redhat.com>
2021-04-28 11:49:09 +0100

    Stop the BFD library from treating annobin symbols as potential function symbols.
    bfd	* elf.c (_bfd_elf_maybe_function_sym): Do not accept annobin
    	symbols as potential function symbols.
    	* elfnn-aarch64.c (elfNN_aarch64_maybe_function_sym): Likewise.
    	* elf64-ppc.c (ppc64_elf_maybe_function_sym): Likewise.
    	* elf32-arm.c (elf32_arm_maybe_function_sym): Likewise.
    
    ld	* testsuite/ld-elf/anno-sym.s: New test source file.
    	* testsuite/ld-elf/anno-sym.d: New test driver.
    	* testsuite/ld-elf/anno-sym.l: New test error output.

--- bfd/elfnn-aarch64.c
+++ bfd/elfnn-aarch64.c
@@ -8012,34 +8012,42 @@ elfNN_aarch64_maybe_function_sym (const asymbol *sym, asection *sec,
 				  bfd_vma *code_off)
 {
   bfd_size_type size;
+  elf_symbol_type * elf_sym = (elf_symbol_type *) sym;
 
   if ((sym->flags & (BSF_SECTION_SYM | BSF_FILE | BSF_OBJECT
 		     | BSF_THREAD_LOCAL | BSF_RELC | BSF_SRELC)) != 0
       || sym->section != sec)
     return 0;
 
+  size = (sym->flags & BSF_SYNTHETIC) ? 0 : elf_sym->internal_elf_sym.st_size;
+  
   if (!(sym->flags & BSF_SYNTHETIC))
-    switch (ELF_ST_TYPE (((elf_symbol_type *) sym)->internal_elf_sym.st_info))
+    switch (ELF_ST_TYPE (elf_sym->internal_elf_sym.st_info))
       {
-	case STT_FUNC:
 	case STT_NOTYPE:
+	  /* Ignore symbols created by the annobin plugin for gcc and clang.
+	     These symbols are hidden, local, notype and have a size of 0.  */
+	  if (size == 0
+	      && sym->flags & BSF_LOCAL
+	      && ELF_ST_VISIBILITY (elf_sym->internal_elf_sym.st_other) == STV_HIDDEN)
+	    return 0;
+	  /* Fall through.  */
+	case STT_FUNC:
+	  /* FIXME: Allow STT_GNU_IFUNC as well ?  */
 	  break;
 	default:
 	  return 0;
       }
-
+  
   if ((sym->flags & BSF_LOCAL)
       && bfd_is_aarch64_special_symbol_name (sym->name,
 					     BFD_AARCH64_SPECIAL_SYM_TYPE_ANY))
     return 0;
 
   *code_off = sym->value;
-  size = 0;
-  if (!(sym->flags & BSF_SYNTHETIC))
-    size = ((elf_symbol_type *) sym)->internal_elf_sym.st_size;
-  if (size == 0)
-    size = 1;
-  return size;
+
+  /* Do not return 0 for the function's size.  */
+  return size ? size : 1;
 }
 
 static bool
