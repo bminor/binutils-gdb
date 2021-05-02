db543a7de3f3356133ef3f7f7fc9ae444fd55325
Alan Modra <amodra@gmail.com>
2021-05-03 07:50:03 +0930

    testsuite: Don't start directives in first column
    Fixes fails on hppa64-hp-hpux11.23
    
    	* testsuite/gas/elf/section25.s: Don't start directives in first
    	column.
    	* testsuite/gas/elf/section26.s: Likewise.

--- gas/testsuite/gas/elf/section25.s
+++ gas/testsuite/gas/elf/section25.s
@@ -1,8 +1,8 @@
-.section .noinit
-.word 0
+ .section .noinit
+ .word 0
 
-.section .noinit.foo
-.word 0
+ .section .noinit.foo
+ .word 0
 
-.section .gnu.linkonce.n.bar
-.word 0
+ .section .gnu.linkonce.n.bar
+ .word 0
