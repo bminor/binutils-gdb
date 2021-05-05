ae61ef2c5615a06f829468b57249a17762a44220
Shahab Vahedi <shahab@synopsys.com>
2021-05-05 23:07:38 +0200

    arc: Add 'set disassembler-options' support
    Implement ARC target support for passing options to the disassembler
    through the command interface. e.g.:
    
    gdb> set disassembler-options cpu=hs38_linux ...
    
    gdb/ChangeLog:
    
    	* NEWS: Document 'set disassembler-options' support for the ARC
    	target.
    	* arc-tdep.c (arc_gdbarch_init): Set
    	'gdbarch_valid_disassembler_options'.
    
    gdb/doc/ChangeLog:
    
    	* gdb.texinfo (Source and Machine Code): Document 'set
    	disassembler-options' support for the ARC target.
    
    gdb/testsuite/ChangeLog:
    
    	* gdb.arch/arc-disassembler-options.exp: New test.
    	* gdb.arch/arc-disassembler-options.s: New test source.

--- gdb/arc-tdep.c
+++ gdb/arc-tdep.c
@@ -2434,10 +2434,12 @@ arc_gdbarch_init (struct gdbarch_info info, struct gdbarch_list *arches)
 	      arc_disassembler_options = NULL;
 	      break;
 	    }
-	  set_gdbarch_disassembler_options (gdbarch,
-					    &arc_disassembler_options);
 	}
     }
+
+  set_gdbarch_disassembler_options (gdbarch, &arc_disassembler_options);
+  set_gdbarch_valid_disassembler_options (gdbarch,
+					  disassembler_options_arc ());
 
   tdesc_use_registers (gdbarch, tdesc, std::move (tdesc_data));
 
