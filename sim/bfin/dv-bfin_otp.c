d699be882b42f36677836c320edbf7db24021a30
Mike Frysinger <vapier@gentoo.org>
2021-05-23 22:15:01 -0400

    sim: bfin: fix the otp fix
    I misread the code and thought data0/... were bu64 when they were
    actually bu32.  Fix the call to assemble the 2 64-bit values instead
    of passing the 2 halves of the first 64-bit value.

--- sim/bfin/dv-bfin_otp.c
+++ sim/bfin/dv-bfin_otp.c
@@ -91,7 +91,8 @@ bfin_otp_write_page_val2 (struct bfin_otp *otp, bu16 page, bu64 lo, bu64 hi)
 static void
 bfin_otp_write_page (struct bfin_otp *otp, bu16 page)
 {
-  bfin_otp_write_page_val2 (otp, page, otp->data0, otp->data1);
+  bfin_otp_write_page_val2 (otp, page, (bu64)otp->data1 | otp->data0,
+			    (bu64)otp->data3 | otp->data2);
 }
 
 static unsigned
