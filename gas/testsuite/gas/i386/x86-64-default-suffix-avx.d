17a089ffda6045908a30c86066748d239a5616d0
Cui,Lili <lili.cui@intel.com>
2021-06-14 11:15:51 +0800

    [PATCH 2/2] Add tests for Intel AVX512_FP16 instructions
    Intel AVX512 FP16 instructions use maps 3, 5 and 6. Maps 5 and 6 use 3 bits
    in the EVEX.mmm field (0b101, 0b110). Map 5 is for instructions that were FP32
    in map 1 (0Fxx). Map 6 is for instructions that were FP32 in map 2 (0F38xx).
    There are some exceptions to this rule. Some things in map 1 (0Fxx) with imm8
    operands predated our current conventions; those instructions moved to map 3.
    FP32 things in map 3 (0F3Axx) found new opcodes in map3 for FP16 because map3
    is very sparsely populated. Most of the FP16 instructions share opcodes and
    prefix (EVEX.pp) bits with the related FP32 operations.
    
    Intel AVX512 FP16 instructions has new displacements scaling rules, please refer
    to the public software developer manual for detail information.
    
    gas/
    
    2021-08-05  Igor Tsimbalist  <igor.v.tsimbalist@intel.com>
                H.J. Lu  <hongjiu.lu@intel.com>
                Wei Xiao <wei3.xiao@intel.com>
                Lili Cui  <lili.cui@intel.com>
    
    	* testsuite/gas/i386/i386.exp: Run FP16 tests.
    	* testsuite/gas/i386/avx512_fp16-intel.d: New test.
    	* testsuite/gas/i386/avx512_fp16-inval-bcast.l: Ditto.
    	* testsuite/gas/i386/avx512_fp16-inval-bcast.s: Ditto.
    	* testsuite/gas/i386/avx512_fp16.d: Ditto.
    	* testsuite/gas/i386/avx512_fp16.s: Ditto.
    	* testsuite/gas/i386/avx512_fp16_pseudo_ops.d: Ditto.
    	* testsuite/gas/i386/avx512_fp16_pseudo_ops.s: Ditto.
    	* testsuite/gas/i386/avx512_fp16_vl-intel.d: Ditto.
    	* testsuite/gas/i386/avx512_fp16_vl.d: Ditto.
    	* testsuite/gas/i386/avx512_fp16_vl.s: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16-intel.d: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16-inval-bcast.l: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16-inval-bcast.s: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16.d: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16.s: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16_pseudo_ops.d: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16_pseudo_ops.s: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16_vl-intel.d: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16_vl.d: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16_vl.s: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16-inval-register.l: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16-inval-register.s: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16-bad.d: Ditto.
    	* testsuite/gas/i386/x86-64-avx512_fp16-bad.s: Ditto.
    	* testsuite/gas/i386/x86-64-default-suffix-avx.d: Add new testcase.
    	* testsuite/gas/i386/x86-64-default-suffix.d: Ditto.
    	* testsuite/gas/i386/x86-64-default-suffix.s: Ditto.
    	* testsuite/gas/i386/xmmword.l: Ditto.
    	* testsuite/gas/i386/xmmword.s: Ditto.

--- gas/testsuite/gas/i386/x86-64-default-suffix-avx.d
+++ gas/testsuite/gas/i386/x86-64-default-suffix-avx.d
@@ -16,4 +16,7 @@ Disassembly of section .text:
  +[a-f0-9]+:	62 61 7e 08 2a 38    	vcvtsi2ssl \(%rax\),%xmm0,%xmm31
  +[a-f0-9]+:	62 f1 7f 08 7b 00    	vcvtusi2sdl \(%rax\),%xmm0,%xmm0
  +[a-f0-9]+:	62 f1 7e 08 7b 00    	vcvtusi2ssl \(%rax\),%xmm0,%xmm0
+ +[a-f0-9]+:	62 f5 7e 08 2a 00    	vcvtsi2shl \(%rax\),%xmm0,%xmm0
+ +[a-f0-9]+:	62 65 7e 08 2a 38    	vcvtsi2shl \(%rax\),%xmm0,%xmm31
+ +[a-f0-9]+:	62 f5 7e 08 7b 00    	vcvtusi2shl \(%rax\),%xmm0,%xmm0
 #pass
--- gas/testsuite/gas/i386/x86-64-default-suffix.s
+++ gas/testsuite/gas/i386/x86-64-default-suffix.s
@@ -8,3 +8,6 @@ foo:
 	vcvtsi2ss (%rax), %xmm0, %xmm31
 	vcvtusi2sd (%rax), %xmm0, %xmm0
 	vcvtusi2ss (%rax), %xmm0, %xmm0
+	vcvtsi2sh (%rax), %xmm0, %xmm0
+	vcvtsi2sh (%rax), %xmm0, %xmm31
+	vcvtusi2sh (%rax), %xmm0, %xmm0
