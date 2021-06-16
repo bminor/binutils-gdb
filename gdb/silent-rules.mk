7daf500de25c0e93bc70d593a7979657a2d4ceb5
Simon Marchi <simon.marchi@polymtl.ca>
2021-06-16 11:55:53 -0400

    sim: make some rules silent by default in Make-common.in
    Use GDB's silent-rules.mk to make some rules silent by default.  These
    rules cover most of what is built in sim/.
    
    gdb/ChangeLog:
    
    	* silent-rules.mk (ECHO_CCLD, ECHO_AR, ECHO_RANLIB): New.
    
    sim/ChangeLog:
    
    	* common/Make-common.in (COMPILE, libsim.a, run$(EXEEXT),
    	gentmap.o, gentmap): Make rules silent.
    
    Change-Id: Idf9ba5beaee10c7c614859ace5fbdcd1de0287db

--- gdb/silent-rules.mk
+++ gdb/silent-rules.mk
@@ -4,6 +4,7 @@ ifeq ($(V),0)
 ECHO_CXX =    @echo "  CXX    $@";
 ECHO_CC  =    @echo "  CC     $@";
 ECHO_CXXLD =  @echo "  CXXLD  $@";
+ECHO_CCLD =   @echo "  CCLD  $@";
 ECHO_REGDAT = @echo "  REGDAT $@";
 ECHO_GEN =    @echo "  GEN    $@";
 ECHO_GEN_XML_BUILTIN = \
@@ -14,5 +15,7 @@ ECHO_INIT_C = @echo "  GEN    init.c"
 ECHO_SIGN =   @echo "  SIGN   gdb";
 ECHO_YACC =   @echo "  YACC   $@";
 ECHO_LEX  =   @echo "  LEX    $@";
+ECHO_AR =     @echo "  AR     $@";
+ECHO_RANLIB = @echo "  RANLIB $@";
 SILENCE = @
 endif
