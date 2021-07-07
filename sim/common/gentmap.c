88c8370b25c6a76cb7de19a3ef50a34abc090756
Mike Frysinger <vapier@gentoo.org>
2021-07-06 22:43:10 -0400

    sim: nltvals: pull target signal out into a dedicated source file
    Like we just did for pulling out the errno map, pull out the signal
    map into a dedicated common file.  All newlib ports are using the
    same signal map which makes it easy.

--- sim/common/gentmap.c
+++ sim/common/gentmap.c
@@ -16,13 +16,6 @@ static struct tdefs sys_tdefs[] = {
   { 0, 0 }
 };
 
-static struct tdefs signal_tdefs[] = {
-#define signal_defs
-#include "nltvals.def"
-#undef signal_defs
-  { 0, 0 }
-};
-
 static struct tdefs open_tdefs[] = {
 #define open_defs
 #include "nltvals.def"
@@ -46,11 +39,6 @@ gen_targ_vals_h (void)
     printf ("#define TARGET_%s %d\n", t->symbol, t->value);
   printf ("\n");
 
-  printf ("/* signal values */\n");
-  for (t = &signal_tdefs[0]; t->symbol; ++t)
-    printf ("#define TARGET_%s %d\n", t->symbol, t->value);
-  printf ("\n");
-
   printf ("/* open flag values */\n");
   for (t = &open_tdefs[0]; t->symbol; ++t)
     printf ("#define TARGET_%s 0x%x\n", t->symbol, t->value);
@@ -69,7 +57,6 @@ gen_targ_map_c (void)
 
   printf ("#include \"defs.h\"\n");
   printf ("#include <fcntl.h>\n");
-  printf ("#include <signal.h>\n");
   printf ("#include \"ansidecl.h\"\n");
   printf ("#include \"sim/callback.h\"\n");
   printf ("#include \"targ-vals.h\"\n");
@@ -82,17 +69,6 @@ gen_targ_map_c (void)
       printf ("#ifdef CB_%s\n", t->symbol);
       /* Skip the "SYS_" prefix for the name.  */
       printf ("  { \"%s\", CB_%s, TARGET_%s },\n", t->symbol + 4, t->symbol, t->symbol);
-      printf ("#endif\n");
-    }
-  printf ("  { 0, -1, -1 }\n");
-  printf ("};\n\n");
-
-  printf ("/* signals mapping table */\n");
-  printf ("CB_TARGET_DEFS_MAP cb_init_signal_map[] = {\n");
-  for (t = &signal_tdefs[0]; t->symbol; ++t)
-    {
-      printf ("#ifdef %s\n", t->symbol);
-      printf ("  { \"%s\", %s, TARGET_%s },\n", t->symbol, t->symbol, t->symbol);
       printf ("#endif\n");
     }
   printf ("  { 0, -1, -1 }\n");
