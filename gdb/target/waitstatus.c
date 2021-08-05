b765e92113f2a6d13f6cfe09b3a074a6e0dec19f
Simon Marchi <simon.marchi@polymtl.ca>
2021-08-05 11:56:43 -0400

    gdb: target_waitstatus_to_string: print extra info for FORKED, VFORKED, EXECD
    Print the extra information contained in target_waitstatus for these
    events.  For TARGET_WAITKIND_{FORKED,VFORKED}, the extra information is
    contained in related_pid, and is the ptid of the new process.  For
    TARGET_WAITKIND_EXECD, it,s the exec'd path name in execd_pathname.
    Print it using the same format used for TARGET_WAITKIND_STOPPED and
    others.
    
    Here are sample outputs for all three events:
    
        [infrun] print_target_wait_results: target_wait (-1.0.0 [process -1], status) =
        [infrun] print_target_wait_results:   726890.726890.0 [process 726890],
        [infrun] print_target_wait_results:   status->kind = vforked, related_pid = 726894.726894.0
    
        [infrun] print_target_wait_results: target_wait (-1.0.0 [process -1], status) =
        [infrun] print_target_wait_results:   727045.727045.0 [process 727045],
        [infrun] print_target_wait_results:   status->kind = forked, related_pid = 727049.727049.0
    
        [infrun] print_target_wait_results: target_wait (-1.0.0 [process -1], status) =
        [infrun] print_target_wait_results:   727119.727119.0 [process 727119],
        [infrun] print_target_wait_results:   status->kind = execd, execd_pathname = /usr/bin/ls
    
    Change-Id: I4416a74e3bf792a625a68bf26c51689e170f2184

--- gdb/target/waitstatus.c
+++ gdb/target/waitstatus.c
@@ -32,41 +32,60 @@ target_waitstatus_to_string (const struct target_waitstatus *ws)
     case TARGET_WAITKIND_EXITED:
       return string_printf ("%sexited, status = %d",
 			    kind_str, ws->value.integer);
+
     case TARGET_WAITKIND_STOPPED:
       return string_printf ("%sstopped, signal = %s",
 			    kind_str,
 			    gdb_signal_to_symbol_string (ws->value.sig));
+
     case TARGET_WAITKIND_SIGNALLED:
       return string_printf ("%ssignalled, signal = %s",
 			    kind_str,
 			    gdb_signal_to_symbol_string (ws->value.sig));
+
     case TARGET_WAITKIND_LOADED:
       return string_printf ("%sloaded", kind_str);
+
     case TARGET_WAITKIND_FORKED:
-      return string_printf ("%sforked", kind_str);
+      return string_printf ("%sforked, related_pid = %s", kind_str,
+			    ws->value.related_pid.to_string ().c_str ());
+
     case TARGET_WAITKIND_VFORKED:
-      return string_printf ("%svforked", kind_str);
+      return string_printf ("%svforked, related_pid = %s", kind_str,
+			    ws->value.related_pid.to_string ().c_str ());
+
     case TARGET_WAITKIND_EXECD:
-      return string_printf ("%sexecd", kind_str);
+      return string_printf ("%sexecd, execd_pathname = %s", kind_str,
+			    ws->value.execd_pathname);
+
     case TARGET_WAITKIND_VFORK_DONE:
       return string_printf ("%svfork-done", kind_str);
+
     case TARGET_WAITKIND_SYSCALL_ENTRY:
       return string_printf ("%sentered syscall", kind_str);
+
     case TARGET_WAITKIND_SYSCALL_RETURN:
       return string_printf ("%sexited syscall", kind_str);
+
     case TARGET_WAITKIND_SPURIOUS:
       return string_printf ("%sspurious", kind_str);
+
     case TARGET_WAITKIND_IGNORE:
       return string_printf ("%signore", kind_str);
+
     case TARGET_WAITKIND_NO_HISTORY:
       return string_printf ("%sno-history", kind_str);
+
     case TARGET_WAITKIND_NO_RESUMED:
       return string_printf ("%sno-resumed", kind_str);
+
     case TARGET_WAITKIND_THREAD_CREATED:
       return string_printf ("%sthread created", kind_str);
+
     case TARGET_WAITKIND_THREAD_EXITED:
       return string_printf ("%sthread exited, status = %d",
 			    kind_str, ws->value.integer);
+
     default:
       return string_printf ("%sunknown???", kind_str);
     }
