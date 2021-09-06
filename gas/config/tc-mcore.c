1bced5243e82e7a30f415a999c136997d7a79934
Nick Clifton <nickc@redhat.com>
2021-09-06 10:52:49 +0100

    Fix potential use on an uninitialised vairable in the MCore assembler.

--- gas/config/tc-mcore.c
+++ gas/config/tc-mcore.c
@@ -852,7 +852,7 @@ md_assemble (char * str)
   char * op_start;
   char * op_end;
   mcore_opcode_info * opcode;
-  char * output;
+  char * output = NULL;
   int nlen = 0;
   unsigned short inst;
   unsigned reg;
@@ -1596,8 +1596,11 @@ md_assemble (char * str)
   if (strcmp (op_end, opcode->name) && strcmp (op_end, ""))
     as_warn (_("ignoring operands: %s "), op_end);
 
-  output[0] = INST_BYTE0 (inst);
-  output[1] = INST_BYTE1 (inst);
+  if (output != NULL)
+    {
+      output[0] = INST_BYTE0 (inst);
+      output[1] = INST_BYTE1 (inst);
+    }
 
 #ifdef OBJ_ELF
   dwarf2_emit_insn (2);
