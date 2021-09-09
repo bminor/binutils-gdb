0c320e1bdcb45e0d46f69a1567a06be326529c34
Mike Frysinger <vapier@gentoo.org>
2021-09-08 21:49:00 -0400

    sim: ppc: enable -Wmissing-declarations & -Wmissing-prototypes
    This aligns with common code which already uses this flag.  We have
    to add another local prototype to fix the failure, and add another
    local decl for the SIM_DESC type.  Unwinding these will require a
    lot more work & conversions in the process, so going with the decl
    for now unblocks the warning unification.

--- sim/ppc/sim_callbacks.h
+++ sim/ppc/sim_callbacks.h
@@ -103,6 +103,13 @@ sim_io_read_stdin
 void sim_io_flush_stdoutput
 (void);
 
+/* TODO: Untangle this SIM_DESC forward decl someday. */
+typedef struct sim_state *SIM_DESC;
+void sim_io_error (SIM_DESC sd,
+		   const char *fmt,
+		   ...)
+  ATTRIBUTE_PRINTF (2, 3)
+  ATTRIBUTE_NORETURN;
 
 /* Simulator instance.  */
 extern psim *simulator;
