9de46719da07a439af59088d69cb4dd70c030612
Tom de Vries <tdevries@suse.de>
2021-10-25 18:15:15 +0200

    [gdb/testsuite] Fix duplicate in gdb.mi/mi-var-cp.exp
    With test-case gdb.mi/mi-var-cp.exp I run into this duplicate:
    ...
    PASS: gdb.mi/mi-var-cp.exp: run to mi-var-cp.cc:104 (set breakpoint)
    PASS: gdb.mi/mi-var-cp.exp: create varobj for s
    PASS: gdb.mi/mi-var-cp.exp: create varobj for s
    DUPLICATE: gdb.mi/mi-var-cp.exp: create varobj for s
    ...
    
    This is due to a duplicate test name here:
    ...
    $ cat -n gdb/testsuite/gdb.mi/mi-var-cp.cc
      ...
       100  int reference_to_struct ()
       101  {
       102    /*: BEGIN: reference_to_struct :*/
       103    S s = {7, 8};
       104    S& r = s;
       105    /*:
       106      mi_create_varobj S s "create varobj for s"
       107      mi_create_varobj R r "create varobj for s"
    ...
    
    Fix this by using "create varobj for r" instead.
    
    Tested on x86_64-linux.

--- gdb/testsuite/gdb.mi/mi-var-cp.cc
+++ gdb/testsuite/gdb.mi/mi-var-cp.cc
@@ -104,7 +104,7 @@ int reference_to_struct ()
   S& r = s;
   /*:
     mi_create_varobj S s "create varobj for s"
-    mi_create_varobj R r "create varobj for s"
+    mi_create_varobj R r "create varobj for r"
     mi_gdb_test "-var-show-attributes S" \
 	"\\^done,attr=\"noneditable\"" \
 	"check attributes of S"
