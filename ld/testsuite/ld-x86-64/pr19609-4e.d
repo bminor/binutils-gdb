14aa4ee440f85a0e4d86b7adb50c93bbf0b7fc5b
H.J. Lu <hjl.tools@gmail.com>
2021-12-13 12:09:39 -0800

    x86: Adjust linker tests for --disable-separate-code
    Adjust linker tests for linker configured with --disable-separate-code:
    
    1. Update expected outputs.
    2. Pass -z max-page-size=0x1000 -z separate-code" to linker.
    
    	* testsuite/ld-i386/report-reloc-1.l: Updated.
    	* testsuite/ld-x86-64/report-reloc-1.l: Likewise.
    	* testsuite/ld-x86-64/pe-x86-64.exp: Pass
    	"-z max-page-size=0x1000 -z separate-code" to linker.
    	* testsuite/ld-x86-64/pr19609-4e.d: Likewise.
    	* testsuite/ld-x86-64/pr19609-6a.d: Likewise.
    	* testsuite/ld-x86-64/pr19609-6b.d: Likewise.
    	* testsuite/ld-x86-64/pr19609-7b.d: Likewise.
    	* testsuite/ld-x86-64/pr19609-7d.d: Likewise.

--- ld/testsuite/ld-x86-64/pr19609-4e.d
+++ ld/testsuite/ld-x86-64/pr19609-4e.d
@@ -1,6 +1,6 @@
 #source: pr19609-4.s
 #as: --64 -mrelax-relocations=yes
-#ld: -melf_x86_64 -Ttext=0x70000000 -Tdata=0xa0000000 --no-relax
+#ld: -melf_x86_64 -Ttext=0x70000000 -Tdata=0xa0000000 --no-relax -z max-page-size=0x1000 -z separate-code
 #objdump: -dw
 
 .*: +file format .*
