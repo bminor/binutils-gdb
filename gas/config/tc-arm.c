b3e4d9326faf88d83745379f254d56b8a8324b63
Richard Sandiford <richard.sandiford@arm.com>
2021-12-16 09:32:00 +0000

    arm: Add support for Armv8.7-A and Armv8.8-A
    This patch adds AArch32 support for -march=armv8.[78]-a.
    The behaviour of the new options can be expressed using a
    combination of existing feature flags and tables.
    
    The cpu_arch_ver entries are technically redundant but
    it seemed less surprising to include them anyway.
    
    include/
    	* opcode/arm.h (ARM_ARCH_V8_7A, ARM_ARCH_V8_8A): New macros.
    
    gas/
    	* doc/c-arm.texi: Add armv8.7-a and armv8.8-a.
    	* config/tc-arm.c (armv87a_ext_table, armv88a_ext_table): New macros.
    	(arm_archs): Add armv8.7-a and armv8.8-a.
    	(cpu_arch_ver): Add ARM_ARCH_V8_7A and ARM_ARCH_V8_8A.
    	* NEWS: Mention the above.
    	* testsuite/gas/arm/attr-march-armv8_7-a.d: New test.
    	* testsuite/gas/arm/attr-march-armv8_8-a.d: Likewise.
    	* testsuite/gas/arm/bfloat16-armv8.7-a.d: Likewise.
    	* testsuite/gas/arm/bfloat16-armv8.8-a.d: Likewise.
    	* testsuite/gas/arm/i8mm-armv8.7-a.d: Likewise.
    	* testsuite/gas/arm/i8mm-armv8.8-a.d: Likewise.

--- gas/config/tc-arm.c
+++ gas/config/tc-arm.c
@@ -31954,6 +31954,9 @@ static const struct arm_ext_table armv86a_ext_table[] =
   { NULL, 0, ARM_ARCH_NONE, ARM_ARCH_NONE }
 };
 
+#define armv87a_ext_table armv86a_ext_table
+#define armv88a_ext_table armv87a_ext_table
+
 static const struct arm_ext_table armv9a_ext_table[] =
 {
   ARM_ADD ("simd", FPU_ARCH_DOTPROD_NEON_VFP_ARMV8),
@@ -32091,6 +32094,8 @@ static const struct arm_arch_option_table arm_archs[] =
   ARM_ARCH_OPT2 ("armv8.4-a",	  ARM_ARCH_V8_4A,	FPU_ARCH_VFP, armv84a),
   ARM_ARCH_OPT2 ("armv8.5-a",	  ARM_ARCH_V8_5A,	FPU_ARCH_VFP, armv85a),
   ARM_ARCH_OPT2 ("armv8.6-a",	  ARM_ARCH_V8_6A,	FPU_ARCH_VFP, armv86a),
+  ARM_ARCH_OPT2 ("armv8.7-a",	  ARM_ARCH_V8_7A,	FPU_ARCH_VFP, armv87a),
+  ARM_ARCH_OPT2 ("armv8.8-a",	  ARM_ARCH_V8_8A,	FPU_ARCH_VFP, armv88a),
   ARM_ARCH_OPT2 ("armv9-a",	  ARM_ARCH_V9A,	FPU_ARCH_VFP, armv9a),
   ARM_ARCH_OPT ("xscale",	  ARM_ARCH_XSCALE,	FPU_ARCH_VFP),
   ARM_ARCH_OPT ("iwmmxt",	  ARM_ARCH_IWMMXT,	FPU_ARCH_VFP),
@@ -32875,6 +32880,8 @@ static const cpu_arch_ver_table cpu_arch_ver[] =
     {TAG_CPU_ARCH_V8,	      ARM_ARCH_V8_5A},
     {TAG_CPU_ARCH_V8_1M_MAIN, ARM_ARCH_V8_1M_MAIN},
     {TAG_CPU_ARCH_V8,	    ARM_ARCH_V8_6A},
+    {TAG_CPU_ARCH_V8,	    ARM_ARCH_V8_7A},
+    {TAG_CPU_ARCH_V8,	    ARM_ARCH_V8_8A},
     {TAG_CPU_ARCH_V9,	    ARM_ARCH_V9A},
     {-1,		    ARM_ARCH_NONE}
 };
