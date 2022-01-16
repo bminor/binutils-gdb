afe40966990503d07548c0b16594a154f3520e97
Joel Brobecker <brobecker@adacore.com>
2022-01-16 14:46:40 +0400

    Manual copyright year update of various GDB files
    This commit updates the copyright year of various files which
    gdb/copyright.py is not able to handle automatically.
    
    gdb/ChangeLog:
    
            * gdbarch.sh: Update end year of copyright year range for
            copyright header of generated files.
    
    gdb/doc/ChangeLog:
    
            * gdb.texinfo: Change end year of the manual's copyright notice
            to 2022.
            * refcard.tex: Ditto.

--- gdb/gdbarch.sh
+++ gdb/gdbarch.sh
@@ -1264,7 +1264,7 @@ cat <<EOF
 
 /* Dynamic architecture support for GDB, the GNU debugger.
 
-   Copyright (C) 1998-2021 Free Software Foundation, Inc.
+   Copyright (C) 1998-2022 Free Software Foundation, Inc.
 
    This file is part of GDB.
 
