1eb855e322d688ae57687094f5c47d9ab5122fe5
Alex Coplan <alex.coplan@arm.com>
2022-01-17 16:52:07 +0000

    ld: Ignore TLS relocs against weak undef symbols
    The behaviour of weak undef thread-local variables is not well defined.
    TLS relocations against weak undef symbols are not handled properly by
    the linker, and in some cases cause the linker to crash (notably when
    linking glibc for purecap Morello). This patch simply ignores these and
    emits a warning to that effect. This is a compromise to enable progress
    for Morello.
    
    bfd/ChangeLog:
    
    2022-01-17  Alex Coplan  <alex.coplan@arm.com>
    
    	* elfnn-aarch64.c (elfNN_aarch64_relocate_section): Skip over TLS
    	relocations against weak undef symbols.
    	(elfNN_aarch64_check_relocs): Likewise, but also warn.
    
    ld/ChangeLog:
    
    2022-01-17  Alex Coplan  <alex.coplan@arm.com>
    
    	* testsuite/ld-aarch64/aarch64-elf.exp: Add morello-weak-tls test.
    	* testsuite/ld-aarch64/morello-weak-tls.d: New test.
    	* testsuite/ld-aarch64/morello-weak-tls.s: New test.
    	* testsuite/ld-aarch64/weak-tls.d: Update test wrt new behaviour.

--- ld/testsuite/ld-aarch64/weak-tls.d
+++ ld/testsuite/ld-aarch64/weak-tls.d
@@ -1,8 +1,6 @@
 #source: weak-tls.s
 #ld: -e0 --emit-relocs
-#objdump: -dr
-#...
-#error:.*: warning: Weak TLS is implementation defined and may not work as expected.*
-#error:.*: warning: Weak TLS is implementation defined and may not work as expected.*
-#error:.*: in function `get':.*
-#error:.*: relocation truncated to fit: R_AARCH64_TLSLD_ADD_DTPREL_LO12 against undefined symbol `dtl'.*
+#warning:.*: ignoring TLS relocation .* against undef weak symbol tls
+#warning:.*: ignoring TLS relocation .* against undef weak symbol tls
+#warning:.*: ignoring TLS relocation .* against undef weak symbol dtl
+#warning:.*: ignoring TLS relocation .* against undef weak symbol dtl
