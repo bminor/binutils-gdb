91ddba836c22cb95fef94a422d8dc573591eb4ae
Keith Seitz <keiths@redhat.com>
2022-01-26 08:56:18 -0800

    Reference array of structs instead of first member during memcpy
    aarch64-tdep.c defines the following macro:
    
    #define MEM_ALLOC(MEMS, LENGTH, RECORD_BUF) \
            do  \
              { \
                unsigned int mem_len = LENGTH; \
                if (mem_len) \
                  { \
                    MEMS =  XNEWVEC (struct aarch64_mem_r, mem_len);  \
                    memcpy(&MEMS->len, &RECORD_BUF[0], \
                           sizeof(struct aarch64_mem_r) * LENGTH); \
                  } \
              } \
              while (0)
    
    This is simlpy allocating a new array and copying it. However, for
    the destination address, it is actually copying into the first member
    of the first element of the array (`&MEMS->len"). This elicits a
    warning with GCC 12:
    
    ../../binutils-gdb/gdb/aarch64-tdep.c: In function ‘int aarch64_process_record(gdbarch*, regcache*, CORE_ADDR)’:
    ../../binutils-gdb/gdb/aarch64-tdep.c:3711:23: error: writing 16 bytes into a region of size 8 [-Werror=stringop-overflow=]
     3711 |                 memcpy(&MEMS->len, &RECORD_BUF[0], \
          |                       ^
    ../../binutils-gdb/gdb/aarch64-tdep.c:4394:3: note: in expansion of macro ‘MEM_ALLOC’
     4394 |   MEM_ALLOC (aarch64_insn_r->aarch64_mems, aarch64_insn_r->mem_rec_count,
          |   ^~~~~~~~~
    ../../binutils-gdb/gdb/aarch64-tdep.c:3721:12: note: destination object ‘aarch64_mem_r::len’ of size 8
     3721 |   uint64_t len;    /* Record length.  */
          |            ^~~
    
    The simple fix is to reference the array, `MEMS' as the destination of the copy.
    
    Tested by rebuilding.
    
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Tue Jan 25 08:28:32 2022 -0800
    #
    # On branch master
    # Your branch is ahead of 'origin/master' by 1 commit.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   aarch64-tdep.c
    #

--- gdb/aarch64-tdep.c
+++ gdb/aarch64-tdep.c
@@ -3708,7 +3708,7 @@ When on, AArch64 specific debugging is enabled."),
 	    if (mem_len) \
 	      { \
 		MEMS =  XNEWVEC (struct aarch64_mem_r, mem_len);  \
-		memcpy(&MEMS->len, &RECORD_BUF[0], \
+		memcpy(MEMS, &RECORD_BUF[0], \
 		       sizeof(struct aarch64_mem_r) * LENGTH); \
 	      } \
 	  } \
