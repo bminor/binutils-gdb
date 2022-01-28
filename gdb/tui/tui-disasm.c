bminor/binutils-gdb

commit 5d10a2041eb843fd321ce1d850cf3e0df7648bc7

gdb: add string_file::release method
A common pattern for string_file is to want to move out the internal
string buffer, because it is the result of the computation that we want
to return.  It is the reason why string_file::string returns a non-const
reference, as explained in the comment.  I think it would make sense to
have a dedicated method for that instead and make string_file::string
return a const reference.

This allows removing the explicit std::move in the typical case.  Note
that compile_program::compute was missing a move, meaning that the
resulting string was copied.  With the new version, it's not possible to
forget to move.

Change-Id: Ieaefa35b73daa7930b2f3a26988b6e3b4121bb79

gdb/tui/tui-disasm.c
@@ -130,14 +130,12 @@ tui_disassemble (struct gdbarch *gdbarch,
 	}
 
       /* Capture the disassembled instruction.  */
-      tal.insn = std::move (gdb_dis_out.string ());
-      gdb_dis_out.clear ();
+      tal.insn = gdb_dis_out.release ();
 
       /* And capture the address the instruction is at.  */
       tal.addr = orig_pc;
       print_address (gdbarch, orig_pc, &gdb_dis_out);
-      tal.addr_string = std::move (gdb_dis_out.string ());
-      gdb_dis_out.clear ();
+      tal.addr_string = std::move (gdb_dis_out.release ());
 
       if (addr_size != nullptr)
 	{

gdb/ada-lang.c
@@ -6915,7 +6915,7 @@ type_as_string (struct type *type)
 
   type_print (type, "", &tmp_stream, -1);
 
-  return std::move (tmp_stream.string ());
+  return tmp_stream.release ();
 }
 
 /* Given a type TYPE, look up the type of the component of type named NAME.

gdb/ada-valprint.c
@@ -307,7 +307,7 @@ ada_print_floating (const gdb_byte *valaddr, struct type *type,
 
   print_floating (valaddr, type, &tmp_stream);
 
-  std::string &s = tmp_stream.string ();
+  std::string s = tmp_stream.release ();
   size_t skip_count = 0;
 
   /* Modify for Ada rules.  */

gdb/ada-varobj.c
@@ -82,7 +82,7 @@ ada_varobj_scalar_image (struct type *type, LONGEST val)
   string_file buf;
 
   ada_print_scalar (type, val, &buf);
-  return std::move (buf.string ());
+  return buf.release ();
 }
 
 /* Assuming that the (PARENT_VALUE, PARENT_TYPE) pair designates
@@ -817,7 +817,7 @@ ada_varobj_get_value_image (struct value *value,
   string_file buffer;
 
   common_val_print (value, &buffer, 0, opts, current_language);
-  return std::move (buffer.string ());
+  return buffer.release ();
 }
 
 /* Assuming that the (VALUE, TYPE) pair designates an array varobj,

gdb/c-exp.y
@@ -1780,11 +1780,11 @@ oper:	OPERATOR NEW
 	|	OPERATOR OBJC_LBRAC ']'
 			{ $$ = operator_stoken ("[]"); }
 	|	OPERATOR conversion_type_id
-			{ string_file buf;
-
+			{
+			  string_file buf;
 			  c_print_type ($2, NULL, &buf, -1, 0,
 					&type_print_raw_options);
-			  std::string name = std::move (buf.string ());
+			  std::string name = buf.release ();
 
 			  /* This also needs canonicalization.  */
 			  gdb::unique_xmalloc_ptr<char> canon

gdb/cli/cli-setshow.c
@@ -659,7 +659,7 @@ get_setshow_command_value_string (const setting &var)
       gdb_assert_not_reached ("bad var_type");
     }
 
-  return std::move (stb.string ());
+  return stb.release ();
 }
 
 

gdb/compile/compile-c-support.c
@@ -635,7 +635,7 @@ public:
       PopUserExpressionPolicy::pop_user_expression (&buf);
 
     AddCodeFooterPolicy::add_code_footer (m_instance->scope (), &buf);
-    return buf.string ();
+    return buf.release ();
   }
 
 private:

gdb/guile/scm-type.c
@@ -109,7 +109,7 @@ tyscm_type_name (struct type *type)
       string_file stb;
 
       LA_PRINT_TYPE (type, "", &stb, -1, 0, &type_print_raw_options);
-      return std::move (stb.string ());
+      return stb.release ();
     }
   catch (const gdb_exception &except)
     {

commit 12735d34728d540ca39b8158da0ed1e257fa8f50

gdb/build: Fix Wpessimizing-move in clang build
When building with clang, I run into an error:

...
tui/tui-disasm.c:138:25: error: moving a temporary object prevents copy
elision [-Werror,-Wpessimizing-move]
      tal.addr_string = std::move (gdb_dis_out.release ());
                        ^
tui/tui-disasm.c:138:25: note: remove std::move call here
      tal.addr_string = std::move (gdb_dis_out.release ());
                        ^~~~~~~~~~~                      ~
...

The error above is caused by the recent commit 5d10a2041eb8 ("gdb: add
string_file::release method").

Fix this by removing std::move.

Build on x86_64-linux with clang 13.0.0.

gdb/tui/tui-disasm.c
@@ -135,7 +135,7 @@ tui_disassemble (struct gdbarch *gdbarch,
       /* And capture the address the instruction is at.  */
       tal.addr = orig_pc;
       print_address (gdbarch, orig_pc, &gdb_dis_out);
-      tal.addr_string = std::move (gdb_dis_out.release ());
+      tal.addr_string = gdb_dis_out.release ();
 
       if (addr_size != nullptr)
 	{