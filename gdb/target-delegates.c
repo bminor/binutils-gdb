fb079cb5c419e03d400a0a139c3ccc4eedc33bef
Tom Tromey <tromey@adacore.com>
2022-02-14 10:19:06 -0700

    Rewrite make-target-delegates in Python
    I think gdb is probably better off having fewer languages involved
    when generating code.  'sh' is unavoidable for build-time generation,
    but for other things, let's use Python.
    
    This rewrites make-target-delegates in Python.  I've stuck pretty
    closely to the original code in this rewrite, so it may look slightly
    weird from a Python perspective.
    
    The only output difference is that a copyright header is now
    generated, using the code introduced in the previous patch.
    
    make-target-delegates.py is simpler to invoke, as it knows the correct
    input file to scan and it creates the output file itself.

--- gdb/target-delegates.c
+++ gdb/target-delegates.c
@@ -1,8 +1,28 @@
-/* THIS FILE IS GENERATED -*- buffer-read-only: t -*- */
+/* *INDENT-OFF* */ /* THIS FILE IS GENERATED -*- buffer-read-only: t -*- */
 /* vi:set ro: */
 
-/* To regenerate this file, run:*/
-/*      make-target-delegates target.h > target-delegates.c */
+/* Boilerplate target methods for GDB
+
+   Copyright (C) 2013-2022 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* To regenerate this file, run:
+   ./make-target-delegates.py
+*/
 
 struct dummy_target : public target_ops
 {
