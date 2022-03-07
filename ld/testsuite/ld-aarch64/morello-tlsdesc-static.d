a020f289cebda749ca7974f18e19dadffa591393
Matthew Malcomson <matthew.malcomson@arm.com>
2022-03-07 18:10:51 +0000

    Assign correct size on Morello TLS relocations
    The previous code was not actually using the size of a symbol when the
    symbol was in the hash table.  This meant that our TLS relaxations
    created an instruction sequence with bounds of zero so that the GCC TLS
    instruction sequence eventually ended up giving a length-zero
    capability.
    
    Also handle extra size of pointers in TCB for c64.  For purecap we have
    16 byte pointers.  Hence the TCB is 32 bytes.  This was not yet handled
    in our relaxations.
    
    Here we determine whether to use a 32 or 16 byte TCB based on the flags
    of the current BFD (i.e. whether this is a purecap binary that we're
    creating).
    
    Testcases are updated to account for the fact that the length
    of the capability to the symbol itself is now sometimes non-zero and for
    the different offset required into the TLS block for modules loaded at
    startup time.

--- ld/testsuite/ld-aarch64/morello-tlsdesc-static.d
+++ ld/testsuite/ld-aarch64/morello-tlsdesc-static.d
@@ -14,8 +14,8 @@ Disassembly of section .text:
 .*:	c29bd042 	mrs	c2, ctpidr_el0
 .*:	d2a00001 	movz	x1, #0x0, lsl #16
 .*:	d2a00000 	movz	x0, #0x0, lsl #16
-.*:	f2800200 	movk	x0, #0x10
-.*:	f2800001 	movk	x1, #0x0
+.*:	f2800400 	movk	x0, #0x20
+.*:	f2800081 	movk	x1, #0x4
 .*:	c2a06040 	add	c0, c2, x0, uxtx
 .*:	c2c10000 	scbnds	c0, c0, x1
 
@@ -23,7 +23,7 @@ Disassembly of section .text:
 .*:	c29bd042 	mrs	c2, ctpidr_el0
 .*:	d2a00001 	movz	x1, #0x0, lsl #16
 .*:	d2a00000 	movz	x0, #0x0, lsl #16
-.*:	f2800280 	movk	x0, #0x14
+.*:	f2800480 	movk	x0, #0x24
 .*:	f2800281 	movk	x1, #0x14
 .*:	c2a06040 	add	c0, c2, x0, uxtx
 .*:	c2c10000 	scbnds	c0, c0, x1
