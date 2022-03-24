ee41183df40c738e3f7e5530e469357762ce2101
Alan Modra <amodra@gmail.com>
2022-03-25 09:02:22 +1030

    Re: gas/Dwarf: improve debug info generation from .irp and alike blocks
    am33_2.0-linux is a mn10300 target.
    
    	* testsuite/gas/elf/dwarf-5-irp.d: xfail am33.

--- gas/testsuite/gas/elf/dwarf-5-irp.d
+++ gas/testsuite/gas/elf/dwarf-5-irp.d
@@ -1,11 +1,11 @@
 #as: --gdwarf-5
 #name: line number entries for section changes inside .irp
 #readelf: -W -wlrR
-# The cr16 crx ft32 mn10* msp430 nds32* and rl78 targets do not evaluate the subtraction of symbols at assembly time.
+# The am33 cr16 crx ft32 mn10* msp430 nds32* and rl78 targets do not evaluate the subtraction of symbols at assembly time.
 # The bfin target does not allow .subsection with an equated symbol as operand.
 # The d30v target emits sufficiently different debug info, apparently also covering padding it inserts.
 # The riscv targets do not support the subtraction of symbols.
-#xfail: bfin-* cr16-* crx-* d30v-* ft32-* mn10*-* msp430-* nds32*-* riscv*-* rl78-*
+#xfail: am33*-* bfin-* cr16-* crx-* d30v-* ft32-* mn10*-* msp430-* nds32*-* riscv*-* rl78-*
 
 Raw dump of debug contents .*
 #...
