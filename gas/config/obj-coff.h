96d2cea340575b12dad5b50edeb3bd66319ab491
Alan Modra <amodra@gmail.com>
2022-04-16 08:45:42 +0930

    Delete unused COFF gas macro
    	* config/obj-coff.h (sy_obj): Don't define.
    	(OBJ_SYMFIELD_TYPE): Revise comments.

--- gas/config/obj-coff.h
+++ gas/config/obj-coff.h
@@ -129,10 +129,8 @@
 
 #define OUTPUT_FLAVOR bfd_target_coff_flavour
 
-/* Alter the field names, for now, until we've fixed up the other
-   references to use the new name.  */
+/* COFF symbol flags.  See SF_* macros.  */
 #define OBJ_SYMFIELD_TYPE	unsigned long
-#define sy_obj			sy_obj_flags
 
 /* We can't use the predefined section symbols in bfd/section.c, as
    COFF symbols have extra fields.  See bfd/libcoff.h:coff_symbol_type.  */
@@ -187,9 +185,8 @@
 #define SA_SET_SECT_NRELOC(s,v)	(SYM_AUXENT (s)->x_sect.x_nreloc = (v))
 #endif
 
-/* Internal use only definitions. SF_ stands for symbol flags.
-
-   These values can be assigned to sy_symbol.ost_flags field of a symbolS.  */
+/* Internal use only definitions.  SF_ stands for symbol flags.  These
+   values can be assigned to OBJ_SYMFIELD_TYPE obj field of a symbolS.  */
 
 #define SF_NORMAL_MASK	0x0000ffff	/* bits 12-15 are general purpose.  */
 
