edb6b77c755545afb969fcebfa900b07a4b93c94
Tom Tromey <tromey@adacore.com>
2022-06-08 09:26:08 -0600

    Allow 'interrupt -a' in all-stop mode
    PR gdb/17160 points out that "interrupt -a" errors in all-stop mode,
    but there's no good reason for this.  This patch removes the error.
    
    Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=17160

--- gdb/infcmd.c
+++ gdb/infcmd.c
@@ -2874,9 +2874,6 @@ interrupt_command (const char *args, int from_tty)
 	  && startswith (args, "-a"))
 	all_threads = 1;
 
-      if (!non_stop && all_threads)
-	error (_("-a is meaningless in all-stop mode."));
-
       interrupt_target_1 (all_threads);
     }
 }
