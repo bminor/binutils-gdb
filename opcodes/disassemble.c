bbcab3366bc5269c2896e82ebd1b0f068ec9a50b
Claudiu Zissulescu <claziss@gmail.com>
2022-07-18 11:43:07 +0300

    opcodes/arc: Implement style support in the disassembler
    Update the ARC disassembler to supply style information to the
    disassembler output. The output formatting remains unchanged.
    
    opcodes/ChangeLog:
    	* disassemble.c (disassemble_init_for_target): Set
    	created_styled_output for ARC based targets.
    	* arc-dis.c (find_format_from_table): Use fprintf_styled_ftype
    	instead of fprintf_ftype throughout.
    	(find_format): Likewise.
    	(print_flags): Likewise.
    	(print_insn_arc): Likewise.
    
    Signed-off-by: Claudiu Zissulescu <claziss@gmail.com>

--- opcodes/disassemble.c
+++ opcodes/disassemble.c
@@ -612,6 +612,11 @@ disassemble_init_for_target (struct disassemble_info * info)
       info->disassembler_needs_relocs = true;
       break;
 #endif
+#ifdef ARCH_arc
+    case bfd_arch_arc:
+      info->created_styled_output = true;
+      break;
+#endif
 #ifdef ARCH_arm
     case bfd_arch_arm:
       info->symbol_is_valid = arm_symbol_is_valid;
