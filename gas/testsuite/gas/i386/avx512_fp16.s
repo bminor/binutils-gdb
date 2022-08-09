3fbe5a01086ca3e68475b111e9a274b1df6fc4a7
Jan Beulich <jbeulich@suse.com>
2022-08-09 09:18:35 +0200

    x86: allow use of broadcast with X/Y/Z-suffixed AVX512-FP16 insns
    While the x/y/z suffix isn't necessary to use in this case, it is still
    odd that these forms don't support broadcast (unlike their AVX512F /
    AVX512DQ counterparts). The lack thereof can e.g. make macro-ized
    programming more difficult.

--- gas/testsuite/gas/i386/avx512_fp16.s
+++ gas/testsuite/gas/i386/avx512_fp16.s
@@ -541,6 +541,7 @@ _start:
 	vfpclassph	$123, (%ecx){1to32}, %k5	 #AVX512-FP16 BROADCAST_EN
 	vfpclassphz	$123, 8128(%ecx), %k5	 #AVX512-FP16 Disp8(7f)
 	vfpclassph	$123, -256(%edx){1to32}, %k5{%k7}	 #AVX512-FP16 BROADCAST_EN Disp8(80) MASK_ENABLING
+	vfpclassphz	$123, 2(%ecx){1to32}, %k5	 #AVX512-FP16 BROADCAST_EN
 	vfpclasssh	$123, %xmm6, %k5	 #AVX512-FP16
 	vfpclasssh	$123, %xmm6, %k5{%k7}	 #AVX512-FP16 MASK_ENABLING
 	vfpclasssh	$123, 0x10000000(%esp, %esi, 8), %k5{%k7}	 #AVX512-FP16 MASK_ENABLING
