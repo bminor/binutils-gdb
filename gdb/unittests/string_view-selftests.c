a5b6e43669b78729d2ef78d668e19bac2b11197d
Tsukasa OI <research_trasio@irq.a4lg.com>
2022-10-20 09:32:06 +0000

    gdb/unittests: PR28413, suppress warnings generated by Gnulib
    Gnulib generates a warning if the system version of certain functions
    are used (to redirect the developer to use Gnulib version).  It caused a
    compiler error when...
    
    -   Compiled with Clang
    -   -Werror is specified (by default)
    -   C++ standard used by Clang is before C++17 (by default as of 15.0.0)
        when this unit test is activated.
    
    This issue is raised as PR28413.
    
    However, previous proposal to fix this issue (a "fix" to Gnulib):
    <https://lists.gnu.org/archive/html/bug-gnulib/2021-10/msg00003.html>
    was rejected because it ruins the intent of Gnulib warnings.
    
    So, we need a Binutils/GDB-side solution.
    
    This commit tries to address this issue on the GDB side.  We have
    "include/diagnostics.h" to disable certain warnings only when necessary.
    
    This commit suppresses the Gnulib warnings by surrounding entire #include
    block with DIAGNOSTIC_IGNORE_USER_DEFINED_WARNINGS to disable Gnulib-
    generated warnings on all standard C++ header files.
    
    Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=28413
    Approved-By: Simon Marchi <simon.marchi@efficios.com>
    Change-Id: Ieeb5a31a6902808d4c7263a2868ae19a35e0ccaa

--- gdb/unittests/string_view-selftests.c
+++ gdb/unittests/string_view-selftests.c
@@ -23,6 +23,15 @@
 
 #define GNULIB_NAMESPACE gnulib
 
+#include "diagnostics.h"
+
+/* Since this file uses GNULIB_NAMESPACE, some code defined in headers ends up
+   using system functions rather than gnulib replacements.  This is not really
+   a problem for this test, but it generates some warnings with Clang, silence
+   them.  */
+DIAGNOSTIC_PUSH
+DIAGNOSTIC_IGNORE_USER_DEFINED_WARNINGS
+
 #include "defs.h"
 #include "gdbsupport/selftest.h"
 #include "gdbsupport/gdb_string_view.h"
@@ -33,6 +42,8 @@
 #include <sstream>
 #include <fstream>
 #include <iostream>
+
+DIAGNOSTIC_POP
 
 /* libstdc++'s testsuite uses VERIFY.  */
 #define VERIFY SELF_CHECK
