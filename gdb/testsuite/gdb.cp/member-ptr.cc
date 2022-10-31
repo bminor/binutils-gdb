bminor/binutils-gdb

commit 6b5a7bc768570edc15532759969cd9bff3f02c28

Handle member pointers directly in generic_value_print
TYPE_CODE_MEMBERPTR and TYPE_CODE_METHODPTR are only used for C++, so
it seems to me that the generic value-printing code ought to handle
these cases -- that way, printing these objects will work even when
the current language is not C++.  This patch implements this idea.

gdb/ChangeLog
2020-09-15  Tom Tromey  <tom@tromey.com>

	* rust-lang.c (rust_value_print_inner): Remove TYPE_CODE_MEMBERPTR
	and TYPE_CODE_METHODPTR cases.
	* c-valprint.c (c_value_print_memberptr): Move to valprint.c.
	(c_value_print_inner): Update.
	* valprint.c (generic_value_print_memberptr): New function, from
	c_value_print_memberptr.
	(generic_value_print): Use it.  Call cplus_print_method_ptr.

gdb/valprint.c
@@ -40,6 +40,8 @@
 #include "gdbarch.h"
 #include "cli/cli-style.h"
 #include "count-one-bits.h"
+#include "c-lang.h"
+#include "cp-abi.h"
 
 /* Maximum number of wchars returned from wchar_iterate.  */
 #define MAX_WCHARS 4
@@ -811,6 +813,27 @@ generic_value_print_complex (struct value *val, struct ui_file *stream,
   fprintf_filtered (stream, "%s", decorations->complex_suffix);
 }
 
+/* generic_value_print helper for TYPE_CODE_MEMBERPTR.  */
+
+static void
+generic_value_print_memberptr
+  (struct value *val, struct ui_file *stream,
+   int recurse,
+   const struct value_print_options *options,
+   const struct generic_val_print_decorations *decorations)
+{
+  if (!options->format)
+    {
+      /* Member pointers are essentially specific to C++, and so if we
+	 encounter one, we should print it according to C++ rules.  */
+      struct type *type = check_typedef (value_type (val));
+      const gdb_byte *valaddr = value_contents_for_printing (val);
+      cp_print_class_member (valaddr, type, stream, "&");
+    }
+  else
+    generic_value_print (val, stream, recurse, options, decorations);
+}
+
 /* See valprint.h.  */
 
 void
@@ -828,7 +851,8 @@ generic_value_print (struct value *val, struct ui_file *stream, int recurse,
       break;
 
     case TYPE_CODE_MEMBERPTR:
-      value_print_scalar_formatted (val, options, 0, stream);
+      generic_value_print_memberptr (val, stream, recurse, options,
+				     decorations);
       break;
 
     case TYPE_CODE_PTR:
@@ -914,9 +938,13 @@ generic_value_print (struct value *val, struct ui_file *stream, int recurse,
       generic_value_print_complex (val, stream, options, decorations);
       break;
 
+    case TYPE_CODE_METHODPTR:
+      cplus_print_method_ptr (value_contents_for_printing (val), type,
+			      stream);
+      break;
+
     case TYPE_CODE_UNION:
     case TYPE_CODE_STRUCT:
-    case TYPE_CODE_METHODPTR:
     default:
       error (_("Unhandled type code %d in symbol table."),
 	     type->code ());

gdb/c-valprint.c
@@ -416,23 +416,6 @@ c_value_print_int (struct value *val, struct ui_file *stream,
     }
 }
 
-/* c_value_print helper for TYPE_CODE_MEMBERPTR.  */
-
-static void
-c_value_print_memberptr (struct value *val, struct ui_file *stream,
-			 int recurse,
-			 const struct value_print_options *options)
-{
-  if (!options->format)
-    {
-      struct type *type = check_typedef (value_type (val));
-      const gdb_byte *valaddr = value_contents_for_printing (val);
-      cp_print_class_member (valaddr, type, stream, "&");
-    }
-  else
-    generic_value_print (val, stream, recurse, options, &c_decorations);
-}
-
 /* See c-lang.h.  */
 
 void
@@ -440,17 +423,12 @@ c_value_print_inner (struct value *val, struct ui_file *stream, int recurse,
 		     const struct value_print_options *options)
 {
   struct type *type = value_type (val);
-  const gdb_byte *valaddr = value_contents_for_printing (val);
 
   type = check_typedef (type);
   switch (type->code ())
     {
     case TYPE_CODE_ARRAY:
       c_value_print_array (val, stream, recurse, options);
-      break;
-
-    case TYPE_CODE_METHODPTR:
-      cplus_print_method_ptr (valaddr, type, stream);
       break;
 
     case TYPE_CODE_PTR:
@@ -466,10 +444,8 @@ c_value_print_inner (struct value *val, struct ui_file *stream, int recurse,
       c_value_print_int (val, stream, options);
       break;
 
+    case TYPE_CODE_METHODPTR:
     case TYPE_CODE_MEMBERPTR:
-      c_value_print_memberptr (val, stream, recurse, options);
-      break;
-
     case TYPE_CODE_REF:
     case TYPE_CODE_RVALUE_REF:
     case TYPE_CODE_ENUM:

gdb/rust-lang.c
@@ -535,11 +535,6 @@ rust_value_print_inner (struct value *val, struct ui_file *stream,
       }
       goto generic_print;
 
-    case TYPE_CODE_METHODPTR:
-    case TYPE_CODE_MEMBERPTR:
-      c_value_print_inner (val, stream, recurse, &opts);
-      break;
-
     case TYPE_CODE_INT:
       /* Recognize the unit type.  */
       if (type->is_unsigned () && TYPE_LENGTH (type) == 0

commit 86430497337968e6163aef370c6312e7b5ed6504

Fix regression in pointer-to-member printing
PR c++/29243 points out that "info func" on a certain C++ executable
will cause an infinite loop in gdb.

I tracked this down to a bug introduced by commit 6b5a7bc76 ("Handle
member pointers directly in generic_value_print").  Before this
commit, the C++ code to print a member pointer would wind up calling
value_print_scalar_formatted; but afterward it simply calls
generic_value_print and gets into a loop.

This patch restores the previous behavior and adds a regression test.

gdb/valprint.c
@@ -879,7 +879,7 @@ generic_value_print_memberptr
       cp_print_class_member (valaddr, type, stream, "&");
     }
   else
-    generic_value_print (val, stream, recurse, options, decorations);
+    value_print_scalar_formatted (val, options, 0, stream);
 }
 
 /* See valprint.h.  */

gdb/testsuite/gdb.cp/member-ptr.cc
@@ -142,6 +142,11 @@ int Diamond::vget_base ()
   return this->Left::x + 2000;
 }
 
+struct Container
+{
+  PMI member;
+};
+
 int
 func (int x)
 {
@@ -204,6 +209,9 @@ int main ()
 
   null_pmi = NULL;
   null_pmf = NULL;
+
+  Container contain;
+  contain.member = &A::j;
 
   pmi = NULL; /* Breakpoint 1 here.  */
 

gdb/testsuite/gdb.cp/member-ptr.exp
@@ -628,3 +628,7 @@ gdb_test "print null_pmi = 0" "$vhn = NULL"
 gdb_test "print null_pmf" "$vhn = NULL"
 gdb_test "print null_pmf = &A::foo" "$vhn = \\(int \\(A::\\*\\)\\(A \\*( const)?, int\\)\\) $hex <A::foo ?\\(int\\)>"
 gdb_test "print null_pmf = 0" "$vhn = NULL"
+
+# Print with a format, bypassing the direct call to the scalar
+# printer.  See PR c++/29243.
+gdb_test "print/x contain" " = {member = $hex}"