fe6e58267a20de5817aabf5b896ad09fe56f448f
Mike Frysinger <vapier@gentoo.org>
2022-11-01 18:49:48 +0545

    sim: frv: invert sim_cpu storage
    The cpu.h change is in generated cgen code, but that has been sent
    upstream too, so the next regen should include it automatically.

--- sim/frv/sim-if.c
+++ sim/frv/sim-if.c
@@ -65,7 +65,7 @@ sim_open (SIM_OPEN_KIND kind, host_callback *callback, bfd *abfd,
   current_target_byte_order = BFD_ENDIAN_BIG;
 
   /* The cpu data is kept in a separately allocated chunk of memory.  */
-  if (sim_cpu_alloc_all (sd, 1) != SIM_RC_OK)
+  if (sim_cpu_alloc_all_extra (sd, 1, sizeof (struct frv_sim_cpu)) != SIM_RC_OK)
     {
       free_state (sd);
       return 0;
