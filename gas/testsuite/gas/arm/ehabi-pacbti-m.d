e0ecefa782205124cf48892cdf0bd88d0738c03e
Alan Modra <amodra@gmail.com>
2022-11-16 07:59:27 +1030

    Re: [gas] arm: Add support for new unwinder directive ".pacspval".
    	* testsuite/gas/arm/ehabi-pacbti-m.d: Limit test to ELF.

--- gas/testsuite/gas/arm/ehabi-pacbti-m.d
+++ gas/testsuite/gas/arm/ehabi-pacbti-m.d
@@ -1,7 +1,8 @@
-# name: Unwind Stack Frame information for Armv8.1-M.Mainline PAC extension
-# source: ehabi-pacbti-m.s
-# as: -march=armv8.1-m.main+mve+pacbti
-# readelf: -u
+#name: Unwind Stack Frame information for Armv8.1-M.Mainline PAC extension
+#source: ehabi-pacbti-m.s
+#as: -march=armv8.1-m.main+mve+pacbti
+#readelf: -u
+#target: [is_elf_format]
 
 Unwind section '.ARM.exidx' at offset 0x5c contains 1 entry:
 
