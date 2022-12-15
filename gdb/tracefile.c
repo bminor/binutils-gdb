c91a13e4e6790324e2177fa4b98e4637e3b03f97
Andrew Burgess <aburgess@redhat.com>
2022-12-15 11:33:37 +0000

    gdb: int to bool conversion in tracefile.c
    Some obvious int to bool conversion in tracefile.c.
    
    Should be no user visible changes after this commit.

--- gdb/tracefile.c
+++ gdb/tracefile.c
@@ -420,7 +420,7 @@ tracefile_fetch_registers (struct regcache *regcache, int regno)
 bool
 tracefile_target::has_all_memory ()
 {
-  return 1;
+  return true;
 }
 
 /* This is the implementation of target_ops method to_has_memory.  */
@@ -428,7 +428,7 @@ tracefile_target::has_all_memory ()
 bool
 tracefile_target::has_memory ()
 {
-  return 1;
+  return true;
 }
 
 /* This is the implementation of target_ops method to_has_stack.
@@ -457,7 +457,7 @@ tracefile_target::has_registers ()
 bool
 tracefile_target::thread_alive (ptid_t ptid)
 {
-  return 1;
+  return true;
 }
 
 /* This is the implementation of target_ops method to_get_trace_status.
