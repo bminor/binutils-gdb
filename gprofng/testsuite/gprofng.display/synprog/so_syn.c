f8b6a781fb6b7e799c95c1c2be87accc39c0b7c0
Jan Beulich <jbeulich@suse.com>
2022-12-19 09:36:00 +0100

    gprofng/testsuite: eliminate bogus casts
    Casting pointers to unsigned int is generally problematic and hence
    compilers tend to warn about such. While here they're used only in
    fprintf(), it still seems better to omit such casts, even if only to
    avoid setting bad precedents.

--- gprofng/testsuite/gprofng.display/synprog/so_syn.c
+++ gprofng/testsuite/gprofng.display/synprog/so_syn.c
@@ -32,7 +32,7 @@ so_cputime ()
   /* put a memory leak in here */
   (void) malloc (13);
 
-  fprintf (stderr, "so_burncpu @ 0x%08x\n", (unsigned int) so_burncpu);
+  fprintf (stderr, "so_burncpu @ %p\n", so_burncpu);
   so_burncpu ();
 
   wlog ("end of so_cputime", NULL);
