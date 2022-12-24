3bef0f032c7744a097774ad32d927c018c06ef92
Mike Frysinger <vapier@gentoo.org>
2022-12-24 15:10:01 -0500

    sim: igen: drop move-if-changed usage
    Now that igen itself has this logic, drop these custom build rules
    to greatly simplify.

--- sim/v850/local.mk
+++ sim/v850/local.mk
@@ -60,36 +60,21 @@ $(%C%_BUILT_SRC_FROM_IGEN): %D%/stamp-igen
 		-i $(%C%_IGEN_INSN) \
 		-o $(%C%_IGEN_DC) \
 		-x \
-		-n icache.h    -hc %D%/tmp-icache.h \
-		-n icache.c    -c  %D%/tmp-icache.c \
-		-n semantics.h -hs %D%/tmp-semantics.h \
-		-n semantics.c -s  %D%/tmp-semantics.c \
-		-n idecode.h   -hd %D%/tmp-idecode.h \
-		-n idecode.c   -d  %D%/tmp-idecode.c \
-		-n model.h     -hm %D%/tmp-model.h \
-		-n model.c     -m  %D%/tmp-model.c \
-		-n support.h   -hf %D%/tmp-support.h \
-		-n support.c   -f  %D%/tmp-support.c \
-		-n itable.h    -ht %D%/tmp-itable.h \
-		-n itable.c    -t  %D%/tmp-itable.c \
-		-n engine.h    -he %D%/tmp-engine.h \
-		-n engine.c    -e  %D%/tmp-engine.c \
-		-n irun.c      -r  %D%/tmp-irun.c
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-icache.h %D%/icache.h
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-icache.c %D%/icache.c
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-idecode.h %D%/idecode.h
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-idecode.c %D%/idecode.c
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-semantics.h %D%/semantics.h
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-semantics.c %D%/semantics.c
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-model.h %D%/model.h
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-model.c %D%/model.c
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-support.h %D%/support.h
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-support.c %D%/support.c
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-itable.h %D%/itable.h
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-itable.c %D%/itable.c
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-engine.h %D%/engine.h
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-engine.c %D%/engine.c
-	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/tmp-irun.c %D%/irun.c
+		-n icache.h    -hc %D%/icache.h \
+		-n icache.c    -c  %D%/icache.c \
+		-n semantics.h -hs %D%/semantics.h \
+		-n semantics.c -s  %D%/semantics.c \
+		-n idecode.h   -hd %D%/idecode.h \
+		-n idecode.c   -d  %D%/idecode.c \
+		-n model.h     -hm %D%/model.h \
+		-n model.c     -m  %D%/model.c \
+		-n support.h   -hf %D%/support.h \
+		-n support.c   -f  %D%/support.c \
+		-n itable.h    -ht %D%/itable.h \
+		-n itable.c    -t  %D%/itable.c \
+		-n engine.h    -he %D%/engine.h \
+		-n engine.c    -e  %D%/engine.c \
+		-n irun.c      -r  %D%/irun.c
 	$(AM_V_at)touch $@
 
 MOSTLYCLEANFILES += $(%C%_BUILD_OUTPUTS)
