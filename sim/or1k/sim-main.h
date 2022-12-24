f5e82fac07c48dd72b44f7b11b866f59ec086815
Mike Frysinger <vapier@gentoo.org>
2022-12-23 20:51:39 -0500

    sim: or1k: move arch-specific settings to internal header
    There's no need for these settings to be in sim-main.h which is shared
    with common/ sim code, so move it all out to the existing or1k-sim.h.
    Unfortunately, we can't yet drop the or1k-sim.h include from sim-main.h
    as many of the generated CGEN files refer only to sim-main.h.  We'll
    have to improve the CGEN interface before we can make more progress,
    but this is at least a minor improvement.

--- sim/or1k/sim-main.h
+++ sim/or1k/sim-main.h
@@ -21,42 +21,14 @@
 
 #define WITH_SCACHE_PBB 1
 
-#include "ansidecl.h"
 #include "or1k-desc.h"
 #include "or1k-opc.h"
 #include "sim-basics.h"
 #include "arch.h"
 #include "sim-base.h"
-#include "sim-fpu.h"
-
 #include "cgen-sim.h"
+
+/* TODO: Move this to the CGEN generated files instead.  */
 #include "or1k-sim.h"
-
-#define OR1K_DEFAULT_MEM_SIZE 0x800000	/* 8M */
-
-struct or1k_sim_cpu
-{
-  OR1K_MISC_PROFILE or1k_misc_profile;
-#define CPU_OR1K_MISC_PROFILE(cpu) (& OR1K_SIM_CPU (cpu)->or1k_misc_profile)
-
-  /* CPU specific parts go here.
-     Note that in files that don't need to access these pieces WANT_CPU_FOO
-     won't be defined and thus these parts won't appear.  This is ok in the
-     sense that things work.  It is a source of bugs though.
-     One has to of course be careful to not take the size of this
-     struct and no structure members accessed in non-cpu specific files can
-     go after here.  Oh for a better language.  */
-  UWI spr[NUM_SPR];
-
-  /* Next instruction will be in delay slot.  */
-  BI next_delay_slot;
-  /* Currently in delay slot.  */
-  BI delay_slot;
-
-#ifdef WANT_CPU_OR1K32BF
-  OR1K32BF_CPU_DATA cpu_data;
-#endif
-};
-#define OR1K_SIM_CPU(cpu) ((struct or1k_sim_cpu *) CPU_ARCH_DATA (cpu))
 
 #endif /* SIM_MAIN_H */
