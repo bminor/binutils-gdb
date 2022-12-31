e3a5d5207584e31543d1812f76e288a08b8703ea
Nick Clifton <nickc@redhat.com>
2022-12-31 12:03:16 +0000

    sync libiberty sources with gcc mainline

--- include/demangle.h
+++ include/demangle.h
@@ -457,6 +457,17 @@ enum demangle_component_type
   DEMANGLE_COMPONENT_MODULE_PARTITION,
   DEMANGLE_COMPONENT_MODULE_ENTITY,
   DEMANGLE_COMPONENT_MODULE_INIT,
+
+  DEMANGLE_COMPONENT_TEMPLATE_HEAD,
+  DEMANGLE_COMPONENT_TEMPLATE_TYPE_PARM,
+  DEMANGLE_COMPONENT_TEMPLATE_NON_TYPE_PARM,
+  DEMANGLE_COMPONENT_TEMPLATE_TEMPLATE_PARM,
+  DEMANGLE_COMPONENT_TEMPLATE_PACK_PARM,
+
+  /* A builtin type with argument.  This holds the builtin type
+     information.  */
+  DEMANGLE_COMPONENT_EXTENDED_BUILTIN_TYPE
+
 };
 
 /* Types which are only used internally.  */
@@ -542,6 +553,15 @@ struct demangle_component
       /* Builtin type.  */
       const struct demangle_builtin_type_info *type;
     } s_builtin;
+
+    /* For DEMANGLE_COMPONENT_EXTENDED_BUILTIN_TYPE.  */
+    struct
+    {
+      /* Builtin type.  */
+      const struct demangle_builtin_type_info *type;
+      short arg;
+      char suffix;
+    } s_extended_builtin;
 
     /* For DEMANGLE_COMPONENT_SUB_STD.  */
     struct
