dfab9a23e9360f4aff68e1d93fee02b0b5cbfb83
Pedro Alves <pedro@palves.net>
2023-05-09 10:27:04 +0100

    Windows gdb+gdbserver: Move suspending thread to when returning event
    The current code suspends a thread just before calling
    GetThreadContext.  You can only call GetThreadContext if the thread is
    suspended.  But, after WaitForDebugEvent, all threads are implicitly
    suspended.  So I don't think we even needed to call SuspendThread
    explictly at all before our GetThreadContext calls.
    
    However, suspending threads when we're about to present a stop to gdb
    simplifies adding non-stop support later.  This way, the windows
    SuspendThread state corresponds to whether a thread is suspended or
    resumed from the core's perspective.  Curiously, I noticed that Wine's
    winedbg does something similar:
    https://github.com/wine-mirror/wine/blob/234943344f7495d1e072338f0e06fa2d5cbf0aa1/programs/winedbg/gdbproxy.c#L651
    
    This makes it much easier to reason about a thread's suspend state,
    and simplifies adding non-stop mode later on.
    
    Change-Id: Ifd6889a8afc041fad33cd1c4500e38941da6781b

--- gdb/windows-nat.c
+++ gdb/windows-nat.c
@@ -702,7 +702,6 @@ windows_nat_target::fetch_registers (struct regcache *regcache, int r)
      {
        if (context->ContextFlags == 0)
 	 {
-	   th->suspend ();
 	   context->ContextFlags = WindowsContext<decltype(context)>::all;
 	   CHECK (get_thread_context (th->h, context));
 	 }
@@ -1229,12 +1228,6 @@ windows_nat_target::windows_continue (DWORD continue_status, int id,
 
 	th->resume ();
       }
-    else
-      {
-	/* When single-stepping a specific thread, other threads must
-	   be suspended.  */
-	th->suspend ();
-      }
 
   std::optional<unsigned> err;
   do_synchronously ([&] ()
@@ -1713,6 +1706,11 @@ windows_nat_target::wait (ptid_t ptid, struct target_waitstatus *ourstatus,
 		  th->stopped_at_software_breakpoint = true;
 		  th->pc_adjusted = false;
 		}
+
+	      /* All-stop, suspend all threads until they are
+		 explicitly resumed.  */
+	      for (auto &thr : windows_process.thread_list)
+		thr->suspend ();
 	    }
 
 	  return result;
