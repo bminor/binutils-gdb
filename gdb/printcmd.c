e2c04ee000796100890bd176eeb2412f722b7e3f
Tom Tromey <tom@tromey.com>
2023-06-12 17:10:14 -0600

    Style history variable output
    When printing a value, I think the history reference -- the "$1" in
    the output -- should be styled using the "variable" style.  This patch
    implements this.

--- gdb/printcmd.c
+++ gdb/printcmd.c
@@ -1254,7 +1254,9 @@ print_value (value *val, const value_print_options &opts)
 
   annotate_value_history_begin (histindex, val->type ());
 
-  gdb_printf ("$%d = ", histindex);
+  std::string idx = string_printf ("$%d", histindex);
+  gdb_printf ("%ps = ", styled_string (variable_name_style.style (),
+				       idx.c_str ()));
 
   annotate_value_history_value ();
 
