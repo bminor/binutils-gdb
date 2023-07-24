3ccfc0b46caa7816a17d274a5df61b31d2541da8
Jose E. Marchesi <jose.marchesi@oracle.com>
2023-07-24 02:11:34 +0200

    bpf: gas,opcodes: fix pseudoc syntax for MOVS* and LDXS* insns
    This patch fixes the pseudoc syntax of the V4 instructions MOVS* and
    LDXS* in order to reflect https://reviews.llvm.org/D144829.
    
    opcodes/ChangeLog:
    
    2023-07-24  Jose E. Marchesi  <jose.marchesi@oracle.com>
    
    	* bpf-opc.c (bpf_opcodes): Fix pseudo-c syntax for MOVS* and LDXS*
    	instructions.
    
    gas/ChangeLog:
    
    2023-07-24  Jose E. Marchesi  <jose.marchesi@oracle.com>
    
    	* doc/c-bpf.texi (BPF Instructions): Fix pseudoc syntax for MOVS*
    	and LDXS* instructions.
    	* testsuite/gas/bpf/mem-pseudoc.d: Likewise.
    	* testsuite/gas/bpf/mem-be-pseudoc.d: Likewise.
    	* testsuite/gas/bpf/mem-pseudoc.s: Likewise.
    	* testsuite/gas/bpf/alu-pseudoc.s: Likewise.
    	* testsuite/gas/bpf/alu-pseudoc.d: Likewise.
    	* testsuite/gas/bpf/alu-be-pseudoc.d: Likewise.
    	* testsuite/gas/bpf/alu32-pseudoc.s: Likewise.
    	* testsuite/gas/bpf/alu32-pseudoc.d: Likewise.
    	* testsuite/gas/bpf/alu32-be-pseudoc.d: Likewise.

--- gas/testsuite/gas/bpf/alu-pseudoc.s
+++ gas/testsuite/gas/bpf/alu-pseudoc.s
@@ -55,6 +55,6 @@
 	r6 = be16 r6
 	r5 = be32 r5
 	r4 = be64 r4
-        r1 s= (i8) r2
-        r1 s= (i16) r2
-        r1 s= (i32) r2
+        r1 = (s8) r2
+        r1 = (s16) r2
+        r1 = (s32) r2
