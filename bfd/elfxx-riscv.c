5b576ed1434dfe9d80b7f0a4490c9faca94c0ed0
Tsukasa OI <research_trasio@irq.a4lg.com>
2023-08-03 01:01:23 +0000

    RISC-V: Remove support for non-existing 'Zve32d'
    Since this "extension" does not exist (on the other hand, 'Zve64d' exists)
    and it's not useful if we keep it (as other code portions just ignore
    "zve32d"), this commit just removes it.
    
    bfd/ChangeLog:
    
    	* elfxx-riscv.c (riscv_supported_std_z_ext): Remove 'Zve32d'
    	extension from the list.

--- bfd/elfxx-riscv.c
+++ bfd/elfxx-riscv.c
@@ -1284,7 +1284,6 @@ static struct riscv_supported_ext riscv_supported_std_z_ext[] =
   {"zkt",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zve32x",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zve32f",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
-  {"zve32d",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zve64x",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zve64f",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
   {"zve64d",		ISA_SPEC_CLASS_DRAFT,		1, 0,  0 },
