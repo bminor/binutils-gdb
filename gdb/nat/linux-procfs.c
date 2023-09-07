31141b552026de77f737c92c7a41789102bae8dc
Tom Tromey <tromey@adacore.com>
2023-09-07 09:03:43 -0600

    Minor cleanup in linux_proc_attach_tgid_threads
    linux_proc_attach_tgid_threads computes a file name, and then
    re-computes it for a warning.  It is better to reuse the
    already-computed name here.
    
    Approved-By: Simon Marchi <simon.marchi@efficios.com>

--- gdb/nat/linux-procfs.c
+++ gdb/nat/linux-procfs.c
@@ -287,7 +287,7 @@ linux_proc_attach_tgid_threads (pid_t pid,
   dir = opendir (pathname);
   if (dir == NULL)
     {
-      warning (_("Could not open /proc/%ld/task."), (long) pid);
+      warning (_("Could not open %s."), pathname);
       return;
     }
 
