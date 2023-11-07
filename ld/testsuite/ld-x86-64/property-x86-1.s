eab996435fe65a421541f59557c5f1fd427573a3
Jan Beulich <jbeulich@suse.com>
2023-11-07 13:58:32 +0100

    ld/x86: reduce testsuite dependency on system object files
    PR ld/30722
    Tests looking for certain .note-section recorded properties may not
    involve object files from the underlying platform (e.g. via using the C
    compiler for linking): Such object files may themselves have similar
    note sections, and hence they may influence the overall outcome.
    
    For now convert just the tests known to be affected by crt*.o coming
    with "ISA v3 needed" notes. Eventually other tests ought to be
    converted, too.

--- ld/testsuite/ld-x86-64/property-x86-1.s
+++ ld/testsuite/ld-x86-64/property-x86-1.s
@@ -1,8 +1,9 @@
-#ifdef __LP64__
-# define ALIGN 3
-#else
-# define ALIGN 2
-#endif
+	.ifdef __64_bit__
+	.equ ALIGN, 3
+	.else
+	.equ ALIGN, 2
+	.endif
+
 	.section ".note.gnu.property", "a"
 	.p2align ALIGN
 	.long 1f - 0f		/* name length.  */
@@ -24,14 +25,14 @@
 	.long 0xc0010002	/* pr_type.  */
 	.long 5f - 4f		/* pr_datasz.  */
 4:
-	.long 0xa
+	.long 0xa /* GNU_PROPERTY_X86_ISA_1_V2 | GNU_PROPERTY_X86_ISA_1_V4 */
 5:
 	.p2align ALIGN
 	/* GNU_PROPERTY_X86_ISA_1_NEEDED */
 	.long 0xc0008002	/* pr_type.  */
 	.long 5f - 4f		/* pr_datasz.  */
 4:
-	.long 0x3
+	.long 0x3 /* GNU_PROPERTY_X86_ISA_1_BASELINE | GNU_PROPERTY_X86_ISA_1_V2 */
 5:
 	.p2align ALIGN
 3:
