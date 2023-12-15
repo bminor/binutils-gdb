bminor/binutils-gdb

commit 0a129eb19a773d930d60b084209570f663db2053

sim: hoist cgen mloop rules up to common builds
These rules don't depend on the target compiler settings, so hoist
the build logic up to the common builds for better parallelization.

We have to extend the genmloop.sh logic a bit to allow outputting
to a subdir since it always assumed cwd was the right place.

We leave the cgen maintainer rules in the subdirs for now as they
aren't normally run, and they rely on cgen logic that has not yet
been generalized.

sim/m32r/local.mk
@@ -1,0 +1,66 @@
+# See sim/Makefile.am
+#
+# Copyright (C) 1996-2021 Free Software Foundation, Inc.
+# Contributed by Cygnus Support.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%C%_BUILD_OUTPUTS = \
+	%D%/eng.h \
+	%D%/mloop.c \
+	%D%/stamp-mloop \
+	%D%/engx.h \
+	%D%/mloopx.c \
+	%D%/stamp-mloop-x \
+	%D%/eng2.h \
+	%D%/mloop2.c \
+	%D%/stamp-mloop-2
+
+# This makes sure build tools are available before building the arch-subdirs.
+SIM_ALL_RECURSIVE_DEPS += $(%C%_BUILD_OUTPUTS)
+
+# FIXME: Use of `mono' is wip.
+%D%/mloop.c %D%/eng.h: %D%/stamp-mloop ; @true
+%D%/stamp-mloop: $(srccom)/genmloop.sh %D%/mloop.in
+	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
+		-mono -fast -pbb -switch sem-switch.c \
+		-cpu m32rbf \
+		-infile $(srcdir)/%D%/mloop.in -outfile-prefix %D%/
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/eng.hin %D%/eng.h
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/mloop.cin %D%/mloop.c
+	$(AM_V_at)touch $@
+
+# FIXME: Use of `mono' is wip.
+%D%/mloopx.c %D%/engx.h: %D%/stamp-mloop ; @true
+%D%/stamp-mloop-x: $(srccom)/genmloop.sh %D%/mloop.in
+	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
+		-mono -no-fast -pbb -parallel-write -switch semx-switch.c \
+		-cpu m32rxf \
+		-infile $(srcdir)/%D%/mloopx.in -outfile-prefix %D%/ -outfile-suffix x
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/engx.hin %D%/engx.h
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/mloopx.cin %D%/mloopx.c
+	$(AM_V_at)touch $@
+
+# FIXME: Use of `mono' is wip.
+%D%/mloop2.c %D%/eng2.h: %D%/stamp-mloop ; @true
+%D%/stamp-mloop-2: $(srccom)/genmloop.sh %D%/mloop.in
+	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
+		-mono -no-fast -pbb -parallel-write -switch sem2-switch.c \
+		-cpu m32r2f \
+		-infile $(srcdir)/%D%/mloop2.in -outfile-prefix %D%/ -outfile-suffix 2
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/eng2.hin %D%/eng2.h
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/mloop2.cin %D%/mloop2.c
+	$(AM_V_at)touch $@
+
+MOSTLYCLEANFILES += $(%C%_BUILD_OUTPUTS)

sim/bpf/Makefile.in
@@ -161,33 +161,7 @@ $(srcdir)/sem-be.c $(srcdir)/decode-be.c $(srcdir)/decode-be.h: \
               $(CGEN_MAINT) stamp-decode-be
 	@true
 
-# Note the following files are generated in objdir, not srcdir.
-
-stamp-mloop: stamp-mloop-le stamp-mloop-be
-
-stamp-mloop-le: $(srcdir)/../common/genmloop.sh mloop.in Makefile
-	$(ECHO_STAMP) mloop-le.c eng-le.h
-	$(SILENCE) $(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
-		-mono -scache -prefix bpfbf_ebpfle -cpu bpfbf \
-                -infile $(srcdir)/mloop.in -outfile-suffix -le
-	$(SILENCE) $(SHELL) $(srcroot)/move-if-change eng-le.hin eng-le.h
-	$(SILENCE) $(SHELL) $(srcroot)/move-if-change mloop-le.cin mloop-le.c
-	$(SILENCE) touch $@
-mloop-le.c eng-le.h: stamp-mloop-le
-	@true
-
-stamp-mloop-be: $(srcdir)/../common/genmloop.sh mloop.in Makefile
-	$(ECHO_STAMP) mloop-be.c eng-be.h
-	$(SILENCE) $(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
-		-mono -scache -prefix bpfbf_ebpfbe -cpu bpfbf \
-                -infile $(srcdir)/mloop.in -outfile-suffix -be
-	$(SILENCE) $(SHELL) $(srcroot)/move-if-change eng-be.hin eng-be.h
-	$(SILENCE) $(SHELL) $(srcroot)/move-if-change mloop-be.cin mloop-be.c
-	$(SILENCE) touch $@
-mloop-be.c eng-be.h: stamp-mloop-be
-	@true
-
 .PHONY = bpf-clean
 
 bpf-clean:
-	rm -f stamp-arch stamp-cpu stamp-decode stamp-defs stamp-mloop
+	rm -f stamp-arch stamp-cpu stamp-decode stamp-defs

sim/bpf/local.mk
@@ -1,0 +1,49 @@
+# See sim/Makefile.am
+#
+# Copyright (C) 2020-2021 Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%C%_BUILD_OUTPUTS = \
+	%D%/eng-le.h \
+	%D%/mloop-le.c \
+	%D%/stamp-mloop-le \
+	%D%/eng-be.h \
+	%D%/mloop-be.c \
+	%D%/stamp-mloop-be
+
+# This makes sure build tools are available before building the arch-subdirs.
+SIM_ALL_RECURSIVE_DEPS += $(%C%_BUILD_OUTPUTS)
+
+%D%/mloop-le.c %D%/eng-le.h: %D%/stamp-mloop-le ; @true
+%D%/stamp-mloop-le: $(srccom)/genmloop.sh %D%/mloop.in
+	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
+		-mono -scache -prefix bpfbf_ebpfle -cpu bpfbf \
+		-infile $(srcdir)/%D%/mloop.in \
+		-outfile-prefix %D%/ -outfile-suffix -le
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/eng-le.hin %D%/eng-le.h
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/mloop-le.cin %D%/mloop-le.c
+	$(AM_V_at)touch $@
+
+%D%/mloop-be.c %D%/eng-be.h: %D%/stamp-mloop-be ; @true
+%D%/stamp-mloop-be: $(srccom)/genmloop.sh %D%/mloop.in
+	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
+		-mono -scache -prefix bpfbf_ebpfbe -cpu bpfbf \
+		-infile $(srcdir)/%D%/mloop.in \
+		-outfile-prefix %D%/ -outfile-suffix -be
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/eng-be.hin %D%/eng-be.h
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/mloop-be.cin %D%/mloop-be.c
+	$(AM_V_at)touch $@
+
+MOSTLYCLEANFILES += $(%C%_BUILD_OUTPUTS)

sim/cris/Makefile.in
@@ -66,48 +66,18 @@ CRISV10F_INCLUDE_DEPS = \
 	$(CGEN_MAIN_CPU_DEPS) \
 	cpuv10.h decodev10.h engv10.h
 
-# FIXME: What is mono and what does "Use of `mono' is wip" mean (other
-# than the apparent; some "mono" feature is work in progress)?
-mloopv10f.c engv10.h: stamp-v10fmloop
-stamp-v10fmloop: $(srcdir)/../common/genmloop.sh mloop.in Makefile
-	$(ECHO_STAMP) mloopv10f.c engv10.h
-	$(SILENCE) $(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
-		-mono -no-fast -pbb -switch semcrisv10f-switch.c \
-		-cpu crisv10f -infile $(srcdir)/mloop.in
-	$(SILENCE) $(SHELL) $(srcroot)/move-if-change eng.hin engv10.h
-	$(SILENCE) $(SHELL) $(srcroot)/move-if-change mloop.cin mloopv10f.c
-	$(SILENCE) touch $@
-
 # CRISV32 objs
 
 CRISV32F_INCLUDE_DEPS = \
 	$(CGEN_MAIN_CPU_DEPS) \
 	cpuv32.h decodev32.h engv32.h
 
-# FIXME: What is mono and what does "Use of `mono' is wip" mean (other
-# than the apparent; some "mono" feature is work in progress)?
-mloopv32f.c engv32.h: stamp-v32fmloop
-# We depend on stamp-v10fmloop to get serialization to avoid
-# racing with it for the same temporary file-names when "make -j".
-stamp-v32fmloop: stamp-v10fmloop $(srcdir)/../common/genmloop.sh mloop.in Makefile
-	$(ECHO_STAMP) mloopv32f.c engv32.h
-	$(SILENCE) $(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
-		-mono -no-fast -pbb -switch semcrisv32f-switch.c \
-		-cpu crisv32f -infile $(srcdir)/mloop.in
-	$(SILENCE) $(SHELL) $(srcroot)/move-if-change eng.hin engv32.h
-	$(SILENCE) $(SHELL) $(srcroot)/move-if-change mloop.cin mloopv32f.c
-	$(SILENCE) touch $@
-
 cris-clean:
-	for v in 10 32; do \
-	  rm -f mloopv$${v}f.c engv$${v}.h stamp-v$${v}fmloop; \
-	  rm -f stamp-v$${v}fcpu; \
-	done
 	-rm -f stamp-arch
 	-rm -f tmp-*
 
 # Useful when making CGEN-generated files manually, without --enable-cgen-maint.
-stamps: stamp-v10fmloop stamp-v32fmloop stamp-arch stamp-v10fcpu stamp-v32fcpu
+stamps: stamp-arch stamp-v10fcpu stamp-v32fcpu
 
 stamp-arch: $(CGEN_READ_SCM) $(CGEN_ARCH_SCM) $(CPU_DIR)/cris.cpu Makefile
 	$(MAKE) cgen-arch $(CGEN_FLAGS_TO_PASS) mach=crisv10,crisv32 \

sim/cris/local.mk
@@ -1,0 +1,54 @@
+# See sim/Makefile.am
+#
+# Copyright (C) 2004-2021 Free Software Foundation, Inc.
+# Contributed by Axis Communications.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%C%_BUILD_OUTPUTS = \
+	%D%/engv10.h \
+	%D%/mloopv10f.c \
+	%D%/stamp-mloop-v10f \
+	%D%/engv32.h \
+	%D%/mloopv32f.c \
+	%D%/stamp-mloop-v32f
+
+# This makes sure build tools are available before building the arch-subdirs.
+SIM_ALL_RECURSIVE_DEPS += $(%C%_BUILD_OUTPUTS)
+
+# FIXME: What is mono and what does "Use of `mono' is wip" mean (other
+# than the apparent; some "mono" feature is work in progress)?
+%D%/mloopv10f.c %D%/engv10.h: %D%/stamp-mloop-v10f ; @true
+%D%/stamp-mloop-v10f: $(srccom)/genmloop.sh %D%/mloop.in
+	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
+		-mono -no-fast -pbb -switch semcrisv10f-switch.c \
+		-cpu crisv10f \
+		-infile $(srcdir)/%D%/mloop.in -outfile-prefix %D%/ -outfile-suffix -v10f
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/eng-v10f.hin %D%/engv10.h
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/mloop-v10f.cin %D%/mloopv10f.c
+	$(AM_V_at)touch $@
+
+# FIXME: What is mono and what does "Use of `mono' is wip" mean (other
+# than the apparent; some "mono" feature is work in progress)?
+%D%/mloopv32f.c %D%/engv32.h: %D%/stamp-mloop-v32f ; @true
+%D%/stamp-mloop-v32f: $(srccom)/genmloop.sh %D%/mloop.in
+	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
+		-mono -no-fast -pbb -switch semcrisv32f-switch.c \
+		-cpu crisv32f \
+		-infile $(srcdir)/%D%/mloop.in -outfile-prefix %D%/ -outfile-suffix -v32f
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/eng-v32f.hin %D%/engv32.h
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/mloop-v32f.cin %D%/mloopv32f.c
+	$(AM_V_at)touch $@
+
+MOSTLYCLEANFILES += $(%C%_BUILD_OUTPUTS)

sim/frv/Makefile.in
@@ -54,19 +54,7 @@ FRVBF_INCLUDE_DEPS = \
 	$(SIM_EXTRA_DEPS) \
 	cpu.h decode.h eng.h
 
-# FIXME: Use of `mono' is wip.
-mloop.c eng.h: stamp-mloop
-stamp-mloop: $(srcdir)/../common/genmloop.sh mloop.in Makefile
-	$(ECHO_STAMP) mloop.c eng.h
-	$(SILENCE) $(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
-		-mono -scache -parallel-generic-write -parallel-only \
-		-cpu frvbf -infile $(srcdir)/mloop.in
-	$(SILENCE) $(SHELL) $(srcroot)/move-if-change eng.hin eng.h
-	$(SILENCE) $(SHELL) $(srcroot)/move-if-change mloop.cin mloop.c
-	$(SILENCE) touch $@
-
 frv-clean:
-	rm -f mloop.c eng.h stamp-mloop
 	rm -f tmp-*
 	rm -f stamp-arch stamp-cpu
 

sim/frv/local.mk
@@ -1,0 +1,38 @@
+# See sim/Makefile.am
+#
+# Copyright (C) 1998-2021 Free Software Foundation, Inc.
+# Contributed by Red Hat.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%C%_BUILD_OUTPUTS = \
+	%D%/eng.h \
+	%D%/mloop.c \
+	%D%/stamp-mloop
+
+# This makes sure build tools are available before building the arch-subdirs.
+SIM_ALL_RECURSIVE_DEPS += $(%C%_BUILD_OUTPUTS)
+
+# FIXME: Use of `mono' is wip.
+%D%/mloop.c %D%/eng.h: %D%/stamp-mloop ; @true
+%D%/stamp-mloop: $(srccom)/genmloop.sh %D%/mloop.in
+	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
+		-mono -scache -parallel-generic-write -parallel-only \
+		-cpu frvbf \
+		-infile $(srcdir)/%D%/mloop.in -outfile-prefix %D%/
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/eng.hin %D%/eng.h
+	$(AM_V_at)$(SHELL) $(srcroot)/move-if-change %D%/mloop.cin %D%/mloop.c
+	$(AM_V_at)touch $@
+
+MOSTLYCLEANFILES += $(%C%_BUILD_OUTPUTS)

commit 10802d9ac0c6e39a672f86224ec89a9a54a37b9c

sim: m32r: fix mloop.in variant stamp deps
The migration to local.mk in commit 0a129eb19a773d930d60b084209570f663db2053
accidentally listed the deps for all mloop steps as mloop.in instead of the
various variants that m32r uses.

Reported-by: Simon Marchi <simon.marchi@polymtl.ca>

sim/Makefile.in
@@ -5218,7 +5218,7 @@ testsuite/common/bits64m63.c: testsuite/common/bits-gen$(EXEEXT) testsuite/commo
 @SIM_ENABLE_ARCH_m32r_TRUE@	$(AM_V_at)touch $@
 
 @SIM_ENABLE_ARCH_m32r_TRUE@m32r/mloopx.c m32r/engx.h: m32r/stamp-mloop-x ; @true
-@SIM_ENABLE_ARCH_m32r_TRUE@m32r/stamp-mloop-x: $(srccom)/genmloop.sh m32r/mloop.in
+@SIM_ENABLE_ARCH_m32r_TRUE@m32r/stamp-mloop-x: $(srccom)/genmloop.sh m32r/mloopx.in
 @SIM_ENABLE_ARCH_m32r_TRUE@	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
 @SIM_ENABLE_ARCH_m32r_TRUE@		-mono -no-fast -pbb -parallel-write -switch semx-switch.c \
 @SIM_ENABLE_ARCH_m32r_TRUE@		-cpu m32rxf \
@@ -5228,7 +5228,7 @@ testsuite/common/bits64m63.c: testsuite/common/bits-gen$(EXEEXT) testsuite/commo
 @SIM_ENABLE_ARCH_m32r_TRUE@	$(AM_V_at)touch $@
 
 @SIM_ENABLE_ARCH_m32r_TRUE@m32r/mloop2.c m32r/eng2.h: m32r/stamp-mloop-2 ; @true
-@SIM_ENABLE_ARCH_m32r_TRUE@m32r/stamp-mloop-2: $(srccom)/genmloop.sh m32r/mloop.in
+@SIM_ENABLE_ARCH_m32r_TRUE@m32r/stamp-mloop-2: $(srccom)/genmloop.sh m32r/mloop2.in
 @SIM_ENABLE_ARCH_m32r_TRUE@	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
 @SIM_ENABLE_ARCH_m32r_TRUE@		-mono -no-fast -pbb -parallel-write -switch sem2-switch.c \
 @SIM_ENABLE_ARCH_m32r_TRUE@		-cpu m32r2f \

sim/m32r/local.mk
@@ -105,7 +105,7 @@ BUILT_SOURCES += \
 
 ## FIXME: Use of `mono' is wip.
 %D%/mloopx.c %D%/engx.h: %D%/stamp-mloop-x ; @true
-%D%/stamp-mloop-x: $(srccom)/genmloop.sh %D%/mloop.in
+%D%/stamp-mloop-x: $(srccom)/genmloop.sh %D%/mloopx.in
 	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
 		-mono -no-fast -pbb -parallel-write -switch semx-switch.c \
 		-cpu m32rxf \
@@ -116,7 +116,7 @@ BUILT_SOURCES += \
 
 ## FIXME: Use of `mono' is wip.
 %D%/mloop2.c %D%/eng2.h: %D%/stamp-mloop-2 ; @true
-%D%/stamp-mloop-2: $(srccom)/genmloop.sh %D%/mloop.in
+%D%/stamp-mloop-2: $(srccom)/genmloop.sh %D%/mloop2.in
 	$(AM_V_GEN)$(SHELL) $(srccom)/genmloop.sh -shell $(SHELL) \
 		-mono -no-fast -pbb -parallel-write -switch sem2-switch.c \
 		-cpu m32r2f \