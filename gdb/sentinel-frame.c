6658f874cf7b521bf2ce431fcf31cc89aebc7009
Simon Marchi <simon.marchi@efficios.com>
2023-12-20 18:18:47 +0000

    gdb: don't set frame id after calling cooked_read_value
    I don't think that setting the next frame id is needed there, all code
    paths in cooked_read_value do set it properly, AFAIK.
    
    Change-Id: Idb9d9e6f89c2c95c5ebfeec2a63fde89ed84cf3d

--- gdb/sentinel-frame.c
+++ gdb/sentinel-frame.c
@@ -54,7 +54,6 @@ sentinel_frame_prev_register (frame_info_ptr this_frame,
   gdb_assert (is_sentinel_frame_id (this_frame_id));
 
   value = cache->regcache->cooked_read_value (regnum);
-  VALUE_NEXT_FRAME_ID (value) = this_frame_id;
 
   return value;
 }
