77b3c4f66647058625d92b866a7ef689ee75557e
Mike Frysinger <vapier@gentoo.org>
2023-12-21 01:35:14 -0500

    sim: or1k: fix -Wimplicit-fallthrough warnings
    Replace some fall through comments with the attribute.

--- sim/or1k/or1k.c
+++ sim/or1k/or1k.c
@@ -270,7 +270,7 @@ or1k32bf_nop (sim_cpu *current_cpu, USI uimm16)
 
     case NOP_EXIT:
       sim_io_printf (CPU_STATE (current_cpu), "exit(%d)\n", GET_H_GPR (3));
-      /* fall through */
+      ATTRIBUTE_FALLTHROUGH;
     case NOP_EXIT_SILENT:
       sim_engine_halt (sd, current_cpu, NULL, CPU_PC_GET (current_cpu),
 		       sim_exited, GET_H_GPR (3));
