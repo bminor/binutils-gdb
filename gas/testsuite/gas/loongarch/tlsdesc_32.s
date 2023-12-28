2ae49bc16fb00ce600da5b2bb503b6e9e3931fa4
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2023-12-28 23:58:01 +0900

    LoongArch: Use tab to indent assembly in TLSDESC test suite
    The usual convention is to use tabs. Not all test are following this,
    but at least when using tabs, let's use it consistently throughout the
    file.

--- gas/testsuite/gas/loongarch/tlsdesc_32.s
+++ gas/testsuite/gas/loongarch/tlsdesc_32.s
@@ -4,7 +4,7 @@
 	# R_LARCH_TLS_DESC_PC_LO12 var
 	addi.w  $a0,$a0,%desc_pc_lo12(var)
 	# R_LARCH_TLS_DESC_LD var
-        ld.w    $ra,$a0,%desc_ld(var)
+	ld.w    $ra,$a0,%desc_ld(var)
 	# R_LARCH_TLS_DESC_CALL var
 	jirl    $ra,$ra,%desc_call(var)
 
