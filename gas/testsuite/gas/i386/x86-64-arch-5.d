e78337d5780c9d837e186c22c11eb8f9ed5bac62
Jan Beulich <jbeulich@suse.com>
2024-01-05 12:08:23 +0100

    x86: relax AMD Zen5 testcase expectations
    One item was too strict for PE/COFF, and there's really no need to check
    for specific comment contents here.

--- gas/testsuite/gas/i386/x86-64-arch-5.d
+++ gas/testsuite/gas/i386/x86-64-arch-5.d
@@ -10,5 +10,5 @@ Disassembly of section .text:
 [ 	]*[a-f0-9]+:[ 	]*48 0f 38 f9 01[ 	]*movdiri %rax,\(%rcx\)
 [ 	]*[a-f0-9]+:[ 	]*66 0f 38 f8 01[ 	]*movdir64b \(%rcx\),%rax
 [ 	]*[a-f0-9]+:[ 	]*62 f2 6f 48 68 d9[ 	]*vp2intersectd %zmm1,%zmm2,%k3
-[ 	]*[a-f0-9]+:[ 	]*0f 18 3d 78 56 34 12[ 	]*prefetchit0 0x12345678\(%rip\)        # 0x[0-9a-f]+
+[ 	]*[a-f0-9]+:[ 	]*0f 18 3d 78 56 34 12[ 	]*prefetchit0 0x12345678\(%rip\)        # .*
 #pass
