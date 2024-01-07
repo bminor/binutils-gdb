b2ea48df925a6d41f63e88b26dbf3f17cde0f252
Mike Frysinger <vapier@gentoo.org>
2024-01-06 23:16:41 -0500

    sim: cgen: rework DI macros to avoid signed left shifts
    The cgen code uses DI as int64_t and UDI as uint64_t.  The DI macros
    are used to construct 64-bit values from 32-bit values (for the low
    and high parts).  The MAKEDI macro casts the high 32-bit value to a
    signed 32-bit value before shifting.  If this created a negative
    value, this would be undefined behavior according to the C standard.
    All we care about is shifting the 32-bits as they are to the high
    32-bits, not caring about sign extension (since there's nothing left
    to shift into), and the low 32-bits being empty.  This is what we
    get from shifting an unsigned value, so cast it to unsigned 32-bit
    to avoid undefined behavior.
    
    While we're here, change the SETLODI macro to truncate the lower
    value to 32-bits before we set it.  If it was passing in a 64-bit
    value, those high bits would get included too, and that's not what
    we want.
    
    Similarly, tweak the SETHIDI macro to cast the value to an unsigned
    64-bit instead of a signed 64-bit.  If the value was only 32-bits,
    the behavior would be the same.  If it happened to be signed 64-bit,
    it would trigger the undefined behavior too.

--- sim/common/cgen-types.h
+++ sim/common/cgen-types.h
@@ -72,9 +72,9 @@ typedef int64_t DI;
 typedef uint64_t UDI;
 #define GETLODI(di) ((SI) (di))
 #define GETHIDI(di) ((SI) ((UDI) (di) >> 32))
-#define SETLODI(di, val) ((di) = (((di) & 0xffffffff00000000LL) | (val)))
-#define SETHIDI(di, val) ((di) = (((di) & 0xffffffffLL) | (((DI) (val)) << 32)))
-#define MAKEDI(hi, lo) ((((DI) (SI) (hi)) << 32) | ((UDI) (USI) (lo)))
+#define SETLODI(di, val) ((di) = (((di) & 0xffffffff00000000LL) | (USI) (val)))
+#define SETHIDI(di, val) ((di) = (((di) & 0xffffffffLL) | (((UDI) (val)) << 32)))
+#define MAKEDI(hi, lo) ((DI) (((UDI) (hi) << 32) | (UDI) (USI) (lo)))
 
 /* These are used to record extracted raw data from an instruction, among other
    things.  It must be a host data type, and not a target one.  */
