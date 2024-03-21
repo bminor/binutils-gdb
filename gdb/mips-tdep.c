af25053d5f9e1867bd0df1d5f168c803e1d7d015
Tom Tromey <tromey@adacore.com>
2024-03-21 11:04:25 -0600

    Remove some unnecessary casts
    I found a few unnecessary casts when calling
    set_gdbarch_disassembler_options_implicit.
    
    Approved-By: John Baldwin <jhb@FreeBSD.org>

--- gdb/mips-tdep.c
+++ gdb/mips-tdep.c
@@ -8736,14 +8736,14 @@ mips_gdbarch_init (struct gdbarch_info info, struct gdbarch_list *arches)
 
   set_gdbarch_print_insn (gdbarch, gdb_print_insn_mips);
   if (mips_abi == MIPS_ABI_N64)
-    set_gdbarch_disassembler_options_implicit
-      (gdbarch, (const char *) mips_disassembler_options_n64);
+    set_gdbarch_disassembler_options_implicit (gdbarch,
+					       mips_disassembler_options_n64);
   else if (mips_abi == MIPS_ABI_N32)
-    set_gdbarch_disassembler_options_implicit
-      (gdbarch, (const char *) mips_disassembler_options_n32);
+    set_gdbarch_disassembler_options_implicit (gdbarch,
+					       mips_disassembler_options_n32);
   else
-    set_gdbarch_disassembler_options_implicit
-      (gdbarch, (const char *) mips_disassembler_options_o32);
+    set_gdbarch_disassembler_options_implicit (gdbarch,
+					       mips_disassembler_options_o32);
   set_gdbarch_disassembler_options (gdbarch, &mips_disassembler_options);
   set_gdbarch_valid_disassembler_options (gdbarch,
 					  disassembler_options_mips ());
