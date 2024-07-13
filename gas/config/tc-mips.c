61022df13c766266c17e44e121cda057ce59382e
Maciej W. Rozycki <macro@orcam.me.uk>
2024-07-13 06:00:43 +0100

    Revert "MIPS/GAS: Omit LI 0 for condition trap"
    This reverts commit bfa257b407270d1c808b31fbd97da779e0fd20d2.  It was
    applied unapproved.

--- gas/config/tc-mips.c
+++ gas/config/tc-mips.c
@@ -13815,14 +13815,9 @@ macro (struct mips_cl_insn *ip, char *str)
     case M_TNE_I:
       s = "tne";
     trap:
-      if (imm_expr.X_add_number != 0)
-	{
-	  used_at = 1;
-	  load_register (AT, &imm_expr, GPR_SIZE == 64);
-	  macro_build (NULL, s, "s,t", op[0], AT);
-	}
-      else
-	  macro_build (NULL, s, "s,t", op[0], ZERO);
+      used_at = 1;
+      load_register (AT, &imm_expr, GPR_SIZE == 64);
+      macro_build (NULL, s, "s,t", op[0], AT);
       break;
 
     case M_TRUNCWS:
--- gas/testsuite/gas/mips/cond-trap-imm-zero.s
+++
@@ -1,9 +0,0 @@
-        .set    noreorder
-        .set    nomacro
-f:
-        teq	$6,0
-        tge	$6,0
-        tgeu	$6,0
-	tlt	$6,0
-	tltu	$6,0
-        tne	$6,0
