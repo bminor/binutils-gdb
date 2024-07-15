ad633f2115b96cebcb601d5ac60241da16b343ca
Nick Alcock <nick.alcock@oracle.com>
2024-07-15 22:05:57 +0100

    libctf: link: fix error handling
    We were calling the wrong error function if opening failed, causing leaks.
    
    libctf/
    	* ctf-link.c (ctf_link_deduplicating_per_cu): Fix error handling.

--- libctf/ctf-link.c
+++ libctf/ctf-link.c
@@ -1221,7 +1221,7 @@ ctf_link_deduplicating_per_cu (ctf_dict_t *fp)
 							&parents)) == NULL)
 	{
 	  ctf_next_destroy (i);
-	  goto err_inputs;
+	  goto err_open_inputs;
 	}
 
       if ((out = ctf_create (&err)) == NULL)
