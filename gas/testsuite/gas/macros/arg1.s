429ac18107acdd759096b7924c111d0c7ea036dd
Alan Modra <amodra@gmail.com>
2024-08-13 12:23:03 +0930

    gas macro arg1 test
    A number of targets pad out the data section, and there are targets
    that have 2 or 4 octets per byte.  And some even that don't have '#'
    as a line comment char.  tic6x-elf fails the test with "Error: too
    many positional arguments".
    
    	* testsuite/gas/macros/arg1.s: Pad out data section.  Use C style
    	comments.
    	* testsuite/gas/macros/arg1.d: Adjust to suit.  Don't run on
    	multi-octet per byte targes.  xfail tic6x.

--- gas/testsuite/gas/macros/arg1.s
+++ gas/testsuite/gas/macros/arg1.s
@@ -1,5 +1,5 @@
-# Test expression argument with inner white spaces and a white space
-# before argument added by C preprocessor.
+/* Test expression argument with inner white spaces and a white space
+   before argument added by C preprocessor.  */
 
 	.macro test arg1, arg2, arg3
 	.byte \arg1
@@ -10,3 +10,4 @@
 	.data
  	test  0x10 + 0, 0x10 + 1, 0x10 + 2
  	test  0x10 + 3, 0x10 + 4, 0x15
+	.byte 0,0,0,0,0,0,0,0,0,0
