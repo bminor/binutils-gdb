b5e0214aad5de80f3d47406dc5182baeceffe1e0
Tom Tromey <tromey@adacore.com>
2024-08-14 06:52:19 -0600

    Remove unnecessary default argument from initialize_block_iterator
    I noticed that initialize_block_iterator has a default value for one
    of its arguments, but this is not needed as this function has a single
    caller that always passes all arguments.  This patch removes the
    default.  Tested by rebuilding.

--- gdb/block.c
+++ gdb/block.c
@@ -438,7 +438,7 @@ get_block_compunit_symtab (const struct block *block)
 static void
 initialize_block_iterator (const struct block *block,
 			   struct block_iterator *iter,
-			   const lookup_name_info *name = nullptr)
+			   const lookup_name_info *name)
 {
   enum block_enum which;
   struct compunit_symtab *cu;
