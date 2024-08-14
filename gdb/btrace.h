intel/gdb

btrace, python: Enable ptwrite filter registration.
By default GDB will be printing the hex payload of the ptwrite package as
auxiliary information.  To customize this, the user can register a ptwrite
filter function in python, that takes the payload and the PC as arguments and
returns a string which will be printed instead.  Registering the filter
function is done using a factory pattern to make per-thread filtering easier.

gdb/btrace.h
@@ -35,6 +35,7 @@
 #endif
 
 #include <vector>
+#include <string>
 
 struct thread_info;
 struct btrace_function;
@@ -351,6 +352,16 @@ struct btrace_thread_info
   /* Optional auxiliary information that is printed in all commands
      displaying or stepping through the execution history.  */
   std::vector<std::string> aux_data;
+
+  /* Function pointer to the ptwrite callback.  Returns the string returned
+     by the ptwrite filter function.  */
+  gdb::optional<std::string> (*ptw_callback_fun) (const uint64_t payload,
+						  const uint64_t ip,
+						  const void *ptw_context)
+						    = nullptr;
+
+  /* Context for the ptw_callback_fun.  */
+  void *ptw_context = nullptr;
 
   /* The function level offset.  When added to each function's LEVEL,
      this normalizes the function levels such that the smallest level

bminor/binutils-gdb

Approved-By: Markus Metzger <markus.t.metzger@intel.com>

gdb/btrace.h
@@ -35,6 +35,7 @@
 #endif
 
 #include <vector>
+#include <string>
 
 struct thread_info;
 struct btrace_function;
@@ -351,6 +352,16 @@ struct btrace_thread_info
   /* Optional auxiliary information that is printed in all commands
      displaying or stepping through the execution history.  */
   std::vector<std::string> aux_data;
+
+  /* Function pointer to the ptwrite callback.  Returns the string returned
+     by the ptwrite filter function.  */
+  std::optional<std::string> (*ptw_callback_fun) (const uint64_t payload,
+						  const uint64_t ip,
+						  const void *ptw_context)
+						    = nullptr;
+
+  /* Context for the ptw_callback_fun.  */
+  void *ptw_context = nullptr;
 
   /* The function level offset.  When added to each function's LEVEL,
      this normalizes the function levels such that the smallest level