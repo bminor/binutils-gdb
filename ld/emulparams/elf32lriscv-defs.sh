9cc911e5593c63a94067e0a1964b91c6e92f814d
Roland McGrath <mcgrathr@google.com>
2024-08-15 19:09:14 -0700

    ld: Don't disable -shared / -pie support for riscv*-elf
    Other *-elf targets support -shared and -pie modes.  There is no
    reason to presuppose that riscv64-elf is used for "embedded", nor
    for that matter that "embedded" is mutually exclusive with
    wanting -shared and -pie functionality available in the linker.

--- ld/emulparams/elf32lriscv-defs.sh
+++ ld/emulparams/elf32lriscv-defs.sh
@@ -16,15 +16,8 @@ if test `echo "$host" | sed -e s/64//` = `echo "$target" | sed -e s/64//`; then
   esac
 fi
 
-# Enable shared library support for everything except an embedded elf target.
-case "$target" in
-  riscv*-elf)
-    ;;
-  *)
-    GENERATE_SHLIB_SCRIPT=yes
-    GENERATE_PIE_SCRIPT=yes
-    ;;
-esac
+GENERATE_SHLIB_SCRIPT=yes
+GENERATE_PIE_SCRIPT=yes
 
 IREL_IN_PLT=
 TEXT_START_ADDR=0x10000
