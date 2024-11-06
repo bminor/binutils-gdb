56fe851ced5b3c856f04ff5611969ba47ffdd16d
Peter Bergner <bergner@linux.ibm.com>
2024-11-06 13:37:10 -0600

    PowerPC: Merge rfc2655 and rfc2656 test cases into one future test case
    gas/
    	* testsuite/gas/ppc/rfc02655.[ds]: Rename from this...
    	* testsuite/gas/ppc/future.[ds]: ... to this.
    	* testsuite/gas/ppc/rfc02656.[ds]: Delete.  Move tests to future.[ds].
    	* testsuite/gas/ppc/ppc.exp: Update for file name changes.

--- gas/testsuite/gas/ppc/future.s
+++ gas/testsuite/gas/ppc/future.s
@@ -1,5 +1,13 @@
 	.text
 _start:
+	subfus	10,0,12,11
+	subwus	10,11,12
+	subfus.	10,0,12,11
+	subwus.	10,11,12
+	subfus	20,1,22,21
+	subdus	20,21,22
+	subfus.	20,1,22,21
+	subdus.	20,21,22
 	lxvrl	 34,10,11
 	lxvrll	 35,10,12
 	lxvprl	 36,10,13
