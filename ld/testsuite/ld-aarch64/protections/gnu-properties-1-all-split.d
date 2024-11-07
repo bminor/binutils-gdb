9957996fa8b8b114ece97b74dc56e4ac8e3066b0
Matthieu Longo <matthieu.longo@arm.com>
2024-11-07 10:33:45 +0000

    aarch64: add tests to check the correct merge of the GCS feature with others.

--- ld/testsuite/ld-aarch64/protections/gnu-properties-1-all-split.d
+++ ld/testsuite/ld-aarch64/protections/gnu-properties-1-all-split.d
@@ -8,4 +8,4 @@
 Displaying notes found in: .note.gnu.property
 [ 	]+Owner[ 	]+Data size[ 	]+Description
   GNU                  0x00000010	NT_GNU_PROPERTY_TYPE_0
-      Properties: AArch64 feature: BTI, PAC
+      Properties: AArch64 feature: BTI, PAC, GCS
--- ld/testsuite/ld-aarch64/protections/gnu-properties-combine-2-all-bti-pac.d
+++ ld/testsuite/ld-aarch64/protections/gnu-properties-combine-2-all-bti-pac.d
@@ -1,7 +1,7 @@
-#name: GNU Property: readelf (combine 2 object files: all + all)
+#name: GNU Property: readelf (combine 2 object files: all + BTI + PAC)
 #source: gnu-properties-all.s
 #source: gnu-properties-selectable.s
-#as: -mabi=lp64 -defsym __property_bti__=1 -defsym __property_pac__=1 -defsym __property_gcs__=1
+#as: -mabi=lp64 -defsym __property_bti__=1 -defsym __property_pac__=1
 #ld: -e _start
 #readelf: -n
 #target: *linux*
@@ -9,4 +9,4 @@
 Displaying notes found in: .note.gnu.property
 [ 	]+Owner[ 	]+Data size[ 	]+Description
   GNU                  0x00000010	NT_GNU_PROPERTY_TYPE_0
-      Properties: AArch64 feature: BTI, PAC, GCS
+      Properties: AArch64 feature: BTI, PAC
