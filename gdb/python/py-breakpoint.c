8518ce5fc22fe33e1f6f698a26548edb1dcd2e74
Andrew Burgess <aburgess@redhat.com>
2024-11-10 19:58:25 +0000

    gdb/python: missing PyObject_IsTrue error check in bppy_init
    As with the previous two commits, this commit fixes a location where
    we called PyObject_IsTrue without including an error check, this time
    in bppy_init.
    
    The 'qualified' argument is supposed to be a bool, the docs say:
    
      The optional QUALIFIED argument is a boolean that allows
      interpreting the function passed in 'spec' as a fully-qualified
      name.  It is equivalent to 'break''s '-qualified' flag (*note
      Linespec Locations:: and *note Explicit Locations::).
    
    It's not totally clear that the only valid values are True or False,
    but I'm choosing to interpret the docs that way, and so I've added a
    PyBool_Type check during argument parsing.  Now, if a non-bool is
    passed the user will get a TypeError during argument parsing.  I've
    added a test to cover this case.
    
    This is a potentially breaking change to the Python API, but hopefully
    this will not impact too many people.  I've added a NEWS entry to
    highlight this change.
    
    Reviewed-By: Eli Zaretskii <eliz@gnu.org>
    Approved-By: Tom Tromey <tom@tromey.com>

--- gdb/python/py-breakpoint.c
+++ gdb/python/py-breakpoint.c
@@ -927,14 +927,14 @@ bppy_init (PyObject *self, PyObject *args, PyObject *kwargs)
   char *label = NULL;
   char *source = NULL;
   char *function = NULL;
-  PyObject * qualified = NULL;
+  PyObject *qualified = Py_False;
 
-  if (!gdb_PyArg_ParseTupleAndKeywords (args, kwargs, "|siiOOsssOO", keywords,
+  if (!gdb_PyArg_ParseTupleAndKeywords (args, kwargs, "|siiOOsssOO!", keywords,
 					&spec, &type, &access_type,
 					&internal,
 					&temporary, &source,
 					&function, &label, &lineobj,
-					&qualified))
+					&PyBool_Type, &qualified))
     return -1;
 
 
@@ -982,10 +982,11 @@ bppy_init (PyObject *self, PyObject *args, PyObject *kwargs)
 	case bp_hardware_breakpoint:
 	  {
 	    location_spec_up locspec;
+	    gdb_assert (PyBool_Check (qualified));
 	    symbol_name_match_type func_name_match_type
-	      = (qualified != NULL && PyObject_IsTrue (qualified)
-		  ? symbol_name_match_type::FULL
-		  : symbol_name_match_type::WILD);
+	      = (qualified == Py_True
+		 ? symbol_name_match_type::FULL
+		 : symbol_name_match_type::WILD);
 
 	    if (spec != NULL)
 	      {
