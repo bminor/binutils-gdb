8a00d45f0b1e910d8a09a7b6ac84d67c67224abd
Jens Remus <jremus@linux.ibm.com>
2024-12-03 14:33:32 +0100

    s390: Enable .bss/.struct data allocation directives tests
    This reduces the number of unsupported tests on s390 by two.
    
    gas/testsuite/
    	* gas/elf/bss.d: Enable for s390*-*-*.
    	* gas/elf/bad-bss.d: Likewise.
    
    Signed-off-by: Jens Remus <jremus@linux.ibm.com>

--- gas/testsuite/gas/elf/bad-bss.d
+++ gas/testsuite/gas/elf/bad-bss.d
@@ -2,4 +2,4 @@
 #source: bss.s
 #error_output: bad-bss.err
 #notarget: arm*-*-elf arm*-*-nto*
-#target: i?86-*-* x86_64-*-* ia64-*-* arm-*-* aarch64-*-* kvx-*-*
+#target: i?86-*-* x86_64-*-* ia64-*-* arm-*-* aarch64-*-* kvx-*-* s390*-*-*
