56ed62a9828b6a6932d00c945e8f7c3dd671a455
Alan Modra <amodra@gmail.com>
2024-12-19 08:33:44 +1030

    Adjust expected loongarch32 test results
    readelf and objdump differ in output for 32-bit vs 64-bit.
    
    	* testsuite/gas/loongarch/dwarf-regnum.d: Adjust to suit both
    	32-bit and 64-bit output.
    	* testsuite/gas/loongarch/localpic.d: Likewise.

--- gas/testsuite/gas/loongarch/dwarf-regnum.d
+++ gas/testsuite/gas/loongarch/dwarf-regnum.d
@@ -13,8 +13,8 @@ Contents of the .*:
   Return address column: .*
   Augmentation data:     .*
 #...
-[a-zA-Z0-9]+ [a-zA-Z0-9]+ [a-zA-Z0-9]+ FDE cie=00000000 pc=[a-zA-Z0-9]+\.\.[a-zA-Z0-9]+
-  DW_CFA_advance_loc: 4 to 0000000000000020
+[a-zA-Z0-9]+ [a-zA-Z0-9]+ [a-zA-Z0-9]+ FDE cie=0+ pc=[a-zA-Z0-9]+\.\.[a-zA-Z0-9]+
+  DW_CFA_advance_loc: 4 to 0+20
   DW_CFA_offset_extended_sf: r0 \(\$zero\) at cfa\+8
   DW_CFA_offset_extended_sf: r1 \(\$ra\) at cfa\+16
   DW_CFA_offset_extended_sf: r2 \(\$tp\) at cfa\+24
