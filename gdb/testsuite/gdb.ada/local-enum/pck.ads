63efd116baafd6210a65b29973766c3fa264098f
Tom Tromey <tromey@adacore.com>
2025-01-21 08:19:21 -0700

    Preserve local variables in another Ada test case
    I found another Ada test case where gnat-llvm optimizes away the local
    variables.  This patch applies the same fix to it as previous patches.

--- gdb/testsuite/gdb.ada/local-enum/pck.ads
+++ gdb/testsuite/gdb.ada/local-enum/pck.ads
@@ -1,4 +1,4 @@
---  Copyright 2021-2025 Free Software Foundation, Inc.
+--  Copyright 2014-2024 Free Software Foundation, Inc.
 --
 --  This program is free software; you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
@@ -13,19 +13,7 @@
 --  You should have received a copy of the GNU General Public License
 --  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-with Pck; use Pck;
-
-procedure Local is
-  type E1 is (one, two, three);
-  type E2 is (three, four, five);
-
-  type A1 is array (E1) of Integer;
-  type A2 is array (E2) of Integer;
-
-  V1 : A1 := (0, 1, 2);
-  V2 : A2 := (3, 4, 5);
-
-begin
-  Do_Nothing (V1'Address); -- STOP
-  Do_Nothing (V2'Address);
-end Local;
+with System;
+package Pck is
+   procedure Do_Nothing (A : System.Address);
+end Pck;
