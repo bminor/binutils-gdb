196fda592e66405ee423701943ca12dd1a996130
Jan Beulich <jbeulich@suse.com>
2025-01-22 09:51:00 +0100

    ld: replace another @progbits etc in an ELF testcase
    The canonical form (in the testsuite) is %progbits and alike.

--- ld/testsuite/ld-elf/merge4b.s
+++ ld/testsuite/ld-elf/merge4b.s
@@ -1,5 +1,5 @@
 /* Check that pointing into padding zeros of string sections works. */
-	.section	.rodata.str1.1,"aMS",@progbits,1
+	.section	.rodata.str1.1,"aMS",%progbits,1
 	.asciz ""
 .LC0:
 	.asciz	"foobar"
@@ -8,16 +8,16 @@
 	.asciz "whatever"
 	.section .data
 	.globl addr_of_str
-	.type addr_of_str, @object
+	.type addr_of_str, %object
 addr_of_str:
 	.dc.a str
 	.globl addr_of_str2
-	.type addr_of_str2, @object
+	.type addr_of_str2, %object
 addr_of_str2:
 	.dc.a 1b
 	.section	.rodata
-	.type	str, @object
+	.type	str, %object
 	.size	str, 7
 str:
 	.asciz	"foobar"
-	.section	.note.GNU-stack,"",@progbits
+	.section	.note.GNU-stack,"",%progbits
