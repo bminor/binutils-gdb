3b9fde6be7d1c07f02716ad8f4336cd436673252
Jan Beulich <jbeulich@suse.com>
2025-02-03 12:18:20 +0100

    PicoJava: use is_whitespace()
    Wherever blanks are permissible in input, tabs ought to be permissible,
    too. This is particularly relevant when -f is passed to gas (alongside
    appropriate input). Also convert ISSPACE(). At the same time use
    is_end_of_stmt() instead of an open-coded check in adjacent code.

--- gas/config/tc-pj.c
+++ gas/config/tc-pj.c
@@ -236,13 +236,13 @@ md_assemble (char *str)
   int nlen = 0;
 
   /* Drop leading whitespace.  */
-  while (*str == ' ')
+  while (is_whitespace (*str))
     str++;
 
   /* Find the op code end.  */
   op_start = str;
   for (op_end = str;
-       *op_end && !is_end_of_line[*op_end & 0xff] && *op_end != ' ';
+       !is_end_of_stmt (*op_end) && !is_whitespace (*op_end);
        op_end++)
     nlen++;
 
@@ -301,7 +301,7 @@ md_assemble (char *str)
 	  pending_reloc = 0;
 	}
 
-      while (ISSPACE (*op_end))
+      while (is_whitespace (*op_end))
 	op_end++;
 
       if (*op_end != 0)
