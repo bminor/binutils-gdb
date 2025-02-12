bminor/binutils-gdb

commit 8cfa1fc43e17c6c225afd7016b7e4e741e7e1ad7

Reorder gnatmake arguments in inline-section-gc.exp
inline-section-gc.exp ends up passing "-lm" to gnatmake as an "marg"
-- meaning gnatmake should process it itself.  However, the gnat-llvm
gnatmake does not know what to do with this, so the test fails.

This patch rearranges the arguments so that the (implicit) trailing
-lm ends up being passed through to the linker.

gdb/testsuite/gdb.ada/inline-section-gc.exp
@@ -23,10 +23,10 @@ set options {
     debug
     optimize=-O2
     additional_flags=-ffunction-sections
-    ldflags=-largs
-    ldflags=-Wl,--gc-sections
     ldflags=-margs
     additional_flags=-gnatn
+    ldflags=-largs
+    ldflags=-Wl,--gc-sections
 }
 if {[gdb_compile_ada "${srcfile}" "${binfile}" executable $options] != ""} {
     return -1

commit 03667c68a6e52e859895c236c9ac2a19b0dae915

Reorder gnatmake arguments in inline-section-gc.exp, again
Tom de Vries pointed out that commit 8cfa1fc4 ("Reorder gnatmake
arguments in inline-section-gc.exp") caused a regression with an older
version of dejagnu.

This patch works around that problem by further reordering the
arguments to gnatmake and also arranging to leave gnatmake in "-margs"
mode.

gdb/testsuite/gdb.ada/inline-section-gc.exp
@@ -19,14 +19,20 @@ require allow_ada_tests
 
 standard_ada_testfile caller
 
+# The ordering here works around a bug in older versions of dejagnu.
+# In particular we use "additional_flags" and not "ldflags" to ensure
+# the ordering (this is ok because the distinction doesn't really
+# matter for gnatmake anyway) and furthermore we take care to end with
+# "-margs" so that any flags appended by dejagnu are applied in the
+# correct mode.
 set options {
     debug
     optimize=-O2
     additional_flags=-ffunction-sections
-    ldflags=-margs
     additional_flags=-gnatn
-    ldflags=-largs
-    ldflags=-Wl,--gc-sections
+    additional_flags=-largs
+    additional_flags=-Wl,--gc-sections
+    additional_flags=-margs
 }
 if {[gdb_compile_ada "${srcfile}" "${binfile}" executable $options] != ""} {
     return -1