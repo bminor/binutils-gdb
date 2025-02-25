f1e96c08e7a45c210680829173bba3d2a88136c7
Tom Tromey <tom@tromey.com>
2025-02-24 21:06:27 -0700

    Remove struct print_one_inferior_data
    struct print_one_inferior_data is not used, so remove it.

--- gdb/mi/mi-main.c
+++ gdb/mi/mi-main.c
@@ -622,12 +622,6 @@ collect_cores (struct thread_info *ti, void *xdata)
   return 0;
 }
 
-struct print_one_inferior_data
-{
-  int recurse;
-  const std::set<int> *inferiors;
-};
-
 static void
 print_one_inferior (struct inferior *inferior, bool recurse,
 		    const std::set<int> &ids)
