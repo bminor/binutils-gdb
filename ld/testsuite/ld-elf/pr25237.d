3dab78a6866f18bc8023eb1dab88f1276cf9fa26
Alan Modra <amodra@gmail.com>
2025-03-07 09:15:29 +1030

    Re: ld: Add a test for PR ld/25237
    Delete the test.  It doesn't make sense to check a linker hack for
    a meaningless p_offset.

--- ld/testsuite/ld-elf/pr25237.d
+++
@@ -1,9 +0,0 @@
-#ld:
-#readelf: -l -W
-# Address bits of these targets don't support the .bss section alignment,
-# 1 << 16.
-#notarget: avr-*-* h8300-*-*
-
-#...
- +LOAD +0x0+ +0x[0-9a-f]+ +0x[0-9a-f]+ +0x0+ +0xb60000+ +RW +0x10000
-#pass
