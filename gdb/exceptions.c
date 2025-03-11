add96080cf9e460afe478cfad489d7f22ab10678
Simon Marchi <simon.marchi@efficios.com>
2025-03-11 15:04:21 -0400

    gdb: remove unused includes in exceptions.c
    These are reported as unused by clangd.
    
    Change-Id: I54b3fba4d7a73c955a9a26c0d340a384b2d37b32

--- gdb/exceptions.c
+++ gdb/exceptions.c
@@ -18,13 +18,11 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "exceptions.h"
-#include "breakpoint.h"
 #include "target.h"
 #include "inferior.h"
 #include "annotate.h"
 #include "ui-out.h"
 #include "serial.h"
-#include "gdbthread.h"
 #include "ui.h"
 #include <optional>
 
