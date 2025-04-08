bminor/binutils-gdb

commit 71a48752660b0771237ce1d3e9075db24800df60

gdb/dwarf: remove create_dwo_cu_reader
This function, as can be seen by its comment, is a remnant of past
design.  Inline its content into create_cus_hash_table.

Change-Id: Id900bae2cdce8f33bf01199fb1d366646effc76e
Approved-By: Tom Tromey <tom@tromey.com>

gdb/dwarf2/read.c
@@ -6514,36 +6514,6 @@ lookup_dwo_file (dwarf2_per_bfd *per_bfd, const char *dwo_name,
   return it != per_bfd->dwo_files.end () ? it->get() : nullptr;
 }
 
-/* die_reader_func for create_dwo_cu.  */
-
-static void
-create_dwo_cu_reader (dwarf2_cu *cu, die_info *comp_unit_die,
-		      dwo_file *dwo_file, dwo_unit *dwo_unit)
-{
-  sect_offset sect_off = cu->per_cu->sect_off;
-  struct dwarf2_section_info *section = cu->per_cu->section;
-
-  std::optional<ULONGEST> signature = lookup_dwo_id (cu, comp_unit_die);
-  if (!signature.has_value ())
-    {
-      complaint (_(DWARF_ERROR_PREFIX
-		   "debug entry at offset %s is missing its dwo_id"
-		   " [in module %s]"),
-		 sect_offset_str (sect_off), dwo_file->dwo_name.c_str ());
-      return;
-    }
-
-  dwo_unit->dwo_file = dwo_file;
-  dwo_unit->signature = *signature;
-  dwo_unit->section = section;
-  dwo_unit->sect_off = sect_off;
-  dwo_unit->length = cu->per_cu->length ();
-
-  dwarf_read_debug_printf ("  offset %s, dwo_id %s",
-			   sect_offset_str (sect_off),
-			   hex_string (dwo_unit->signature));
-}
-
 /* Create the dwo_units for the CUs in a DWO_FILE.
    Note: This function processes DWO files only, not DWP files.  */
 
@@ -6569,7 +6539,6 @@ create_cus_hash_table (dwarf2_cu *cu, dwo_file &dwo_file)
   end_ptr = info_ptr + section.size;
   while (info_ptr < end_ptr)
     {
-      struct dwo_unit read_unit {};
       sect_offset sect_off = (sect_offset) (info_ptr - section.buffer);
 
       /* The length of the CU gets set by the cutu_reader just below.  */
@@ -6578,17 +6547,34 @@ create_cus_hash_table (dwarf2_cu *cu, dwo_file &dwo_file)
       cutu_reader reader (&per_cu, per_objfile, language_minimal,
 			  cu, &dwo_file);
 
-      if (!reader.is_dummy ())
-	create_dwo_cu_reader (reader.cu (), reader.top_level_die (), &dwo_file,
-			      &read_unit);
       info_ptr += per_cu.length ();
 
-      /* If the unit could not be parsed, skip it.  */
-      if (read_unit.dwo_file == NULL)
+      if (reader.is_dummy())
 	continue;
 
+      std::optional<ULONGEST> signature
+	= lookup_dwo_id (cu, reader.top_level_die ());
+      if (!signature.has_value ())
+	{
+	  complaint (_(DWARF_ERROR_PREFIX
+		       "debug entry at offset %s is missing its dwo_id"
+		       " [in module %s]"),
+		     sect_offset_str (sect_off),
+		     dwo_file.dwo_name.c_str ());
+	  continue;
+	}
+
       dwo_unit *dwo_unit = OBSTACK_ZALLOC (&per_bfd->obstack, struct dwo_unit);
-      *dwo_unit = read_unit;
+
+      dwo_unit->dwo_file = &dwo_file;
+      dwo_unit->signature = *signature;
+      dwo_unit->section = &section;
+      dwo_unit->sect_off = sect_off;
+      dwo_unit->length = per_cu.length ();
+
+      dwarf_read_debug_printf ("  offset %s, dwo_id %s",
+			       sect_offset_str (sect_off),
+			       hex_string (dwo_unit->signature));
 
       auto [it, inserted] = dwo_file.cus.emplace (dwo_unit);
       if (!inserted)

commit 8ad92d35519a14ee31c6494be1c2eb8bdb52053d

gdb/dwarf2: pass correct dwarf2_cu to lookup_dwo_id in create_cus_hash_table
Commit 71a48752660b ("gdb/dwarf: remove create_dwo_cu_reader")
introduced a regression when handling files compiled with "-gsplit-dwarf
-fdebug-types-section" (at least with clang):

    $ cat test.cpp
    #include <vector>

    int main()
    {
      std::vector<int> v;
      return v.size ();
    }
    $ clang++ -O0 test.cpp -g -gdwarf-5 -gsplit-dwarf -fdebug-types-section -o test
    $ ./gdb -nx -q --data-directory=data-directory ./test -ex "maint expand-symtabs"
    Reading symbols from ./test...
    /home/smarchi/src/binutils-gdb/gdb/dwarf2/read.c:6159: internal-error: setup_type_unit_groups: Assertion `per_cu->is_debug_types' failed.

In the main file, we have a skeleton CU with a certain DWO ID:

    0x00000000: Compile Unit: ..., unit_type = DW_UT_skeleton, ..., DWO_id = 0x146eaa4daf5deef2, ...

In the .dwo file, the first unit is a type unit with a certain type
signature:

    0x00000000: Type Unit: ..., unit_type = DW_UT_split_type, ..., type_signature = 0xb499dcf29e2928c4, ...

and the split compile unit matching the DWO ID from the skeleton from
the main file comes later:

    0x0000117f: Compile Unit: ..., unit_type = DW_UT_split_compile, ..., DWO_id = 0x146eaa4daf5deef2, ...

The problem introduced by the aforementioned commit is that when
creating a dwo_unit structure representing the type unit, we use the
signature (DWO id) from the skeleton, instead of the signature from the
type unit's header.  As a result, all dwo_units get created with the
same signature (the DWO id) and only the first unit gets inserted in the
hash table.  When looking up the comp unit by DWO ID later on, we
wrongly find the type unit, and try to expand a type unit as a comp
unit, hitting the assert.

Before that commit, we passed `reader.cu ()` to lookup_dwo_id, which
yields a dwarf2_cu built from parsing the type unit's header.  This

gdb/dwarf2/read.c
@@ -6343,7 +6343,7 @@ create_cus_hash_table (dwarf2_cu *cu, dwo_file &dwo_file)
 	continue;
 
       std::optional<ULONGEST> signature
-	= lookup_dwo_id (cu, reader.top_level_die ());
+	= lookup_dwo_id (reader.cu (), reader.top_level_die ());
       if (!signature.has_value ())
 	{
 	  complaint (_(DWARF_ERROR_PREFIX

gdb/testsuite/gdb.dwarf2/fission-with-type-unit.c
@@ -1,0 +1,23 @@
+/* This testcase is part of GDB, the GNU debugger.
+
+   Copyright 2021-2024 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+int
+main (int argc, char **argv)
+{
+  return 0;
+}

gdb/testsuite/gdb.dwarf2/fission-with-type-unit.exp
@@ -1,0 +1,105 @@
+# Copyright 2021-2024 Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This test is meant to reproduce a bug where having a DWO file with a type unit
+# as the first unit would cause an internal error on CU expansion.
+
+load_lib dwarf.exp
+
+# This test can only be run on targets which support DWARF-2 and use gas.
+require dwarf2_support
+
+standard_testfile .c -dw.S -dwo.S
+
+set main_asm_file [standard_output_file $srcfile2]
+set dwo_asm_file [standard_output_file $srcfile3]
+
+# Debug info in the main file.
+Dwarf::assemble $main_asm_file {
+    cu {
+	version 5
+	dwo_id 0xF00D
+    } {
+	compile_unit {
+	    {DW_AT_dwo_name ${::gdb_test_file_name}.dwo DW_FORM_strp}
+	} {}
+    }
+}
+
+# Debug info in the DWO file.
+Dwarf::assemble $dwo_asm_file {
+    tu {
+	fission 1
+	version 5
+    } 0xCAFE "the_type" {
+	type_unit {} {
+	    the_type: base_type {
+		{DW_AT_byte_size 4 DW_FORM_sdata}
+		{DW_AT_encoding  @DW_ATE_signed}
+		{DW_AT_name      int}
+	    }
+	}
+    }
+
+    cu {
+	fission 1
+	version 5
+	dwo_id 0xF00D
+    } {
+	compile_unit {} {
+	    declare_labels int4_type
+
+	    int4_type: DW_TAG_base_type {
+		{DW_AT_byte_size 4 DW_FORM_sdata}
+		{DW_AT_encoding  @DW_ATE_signed}
+		{DW_AT_name      int}
+	    }
+
+	    DW_TAG_variable {
+		{DW_AT_name global_var}
+		{DW_AT_type :$int4_type}
+		{DW_AT_location {
+		    DW_OP_const1u 12
+		    DW_OP_stack_value
+		} SPECIAL_expr}
+	    }
+	}
+    }
+}
+
+# Build main file.
+if { [build_executable "${testfile}.exp" $binfile \
+	[list ${srcfile} ${main_asm_file}] {nodebug}] } {
+    return
+}
+
+# Build DWO file.
+set dwo_file [standard_output_file ${testfile}.dwo]
+if { [gdb_compile_shlib $dwo_asm_file $dwo_file nodebug] != "" } {
+    return
+}
+
+clean_restart ${testfile}
+
+# This would cause an internal error.
+gdb_test_no_output "maint expand-symtabs"
+
+# Sanity check that the CU was correctly loaded.
+gdb_test "print global_var" " = 12"
+
+# Test again just to see if the symbol can be found without having expanded the
+# symtab first.
+clean_restart ${testfile}
+gdb_test "print global_var" " = 12" "print global_var without symtab expansion"