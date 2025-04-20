f319a7fcafe2f1a7ccce29db88642cf1f1d715c9
Alice Carlotti <alice.carlotti@arm.com>
2025-04-20 17:38:59 +0100

    aarch64: Add missing widening fmops test
    Also remove the valid instructions from the test for invalid
    instructions - this meant that the instruction was previously being
    tested for assembly but not disassembly.

--- gas/testsuite/gas/aarch64/sme-f16f16-2-bad.s
+++ gas/testsuite/gas/aarch64/sme-f16f16-2-bad.s
@@ -1,5 +1,4 @@
 /* FMOPA.  */
-fmopa ZA0.s, p0/m, p0/m, z0.h, z0.h
 fmopa ZA1.h, p0, p0/m, z0.h, z0.h
 fmopa ZA0.h, p7/m, p0, z0.h, z0.h
 fmopa ZA0.h, p0/m, p7/m, z0.s, z0.s
@@ -8,7 +7,6 @@ fmopa ZA2.h, p0/m, p8/m, z0.s, z31.b
 fmopa ZA4.h, p15/m, p11/m, z0.s, z31.b
 
 /* FMOPS.  */
-fmops ZA0.s, p0/m, p0/m, z0.h, z0.h
 fmops ZA1.h, p0, p0/m, z0.h, z0.h
 fmops ZA0.h, p7/m, p0, z0.h, z0.h
 fmops ZA0.h, p0/m, p7/m, z0.s, z0.s
