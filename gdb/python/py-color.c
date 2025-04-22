bd7a2f22b11e37ce35c3a78f6fb54ef74270b903
Andrew Burgess <aburgess@redhat.com>
2025-04-22 18:01:07 +0100

    gdb/python: remove Py_TPFLAGS_BASETYPE from gdb.Color
    Remove the Py_TPFLAGS_BASETYPE flag from the gdb.Color type.  This
    effectively makes gdb.Color final; users can no longer create classes
    that inherit from gdb.Color.
    
    Right now I cannot think of any cases where inheritance would be
    needed over composition for a simple type like gdb.Color.  If I'm
    wrong, then it's easy to add Py_TPFLAGS_BASETYPE back in later, this
    would be an extension of the API.  But it's much harder to remove the
    flag later as that might break existing user code (note: there has
    been no release of GDB yet that includes the gdb.Color type).
    
    Introducing this restriction makes the next commit easier.
    
    Reviewed-By: Eli Zaretskii <eliz@gnu.org>
    Approved-By: Tom Tromey <tom@tromey.com>

--- gdb/python/py-color.c
+++ gdb/python/py-color.c
@@ -312,7 +312,7 @@ PyTypeObject colorpy_object_type =
   get_attr,			  /*tp_getattro*/
   0,				  /*tp_setattro*/
   0,				  /*tp_as_buffer*/
-  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
+  Py_TPFLAGS_DEFAULT,		  /*tp_flags*/
   "GDB color object",	  	  /* tp_doc */
   0,				  /* tp_traverse */
   0,				  /* tp_clear */
