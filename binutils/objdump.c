61d4ada564f8b7e10e731f28abda6c4192b63cd2
Nick Alcock <nick.alcock@oracle.com>
2025-04-25 21:45:45 +0100

    binutils: objdump, readelf: BTF dumping support
    objdump and readelf's --ctf option can now dump BTF as well (in CTF dumping
    format, which is quite high-level and C-like compared to bpftool btf dump:
    both have their uses).

--- binutils/objdump.c
+++ binutils/objdump.c
@@ -4875,12 +4875,22 @@ dump_ctf (bfd *abfd, const char *sect_name, const char *parent_name,
   ctf_next_t *i = NULL;
   const char *name;
   size_t member = 0;
+  int try_defaults = 0;
   int err;
 
   if (sect_name == NULL)
-    sect_name = ".ctf";
+    {
+      sect_name = ".ctf";
+      try_defaults = 1;
+    }
 
   sec = read_section (abfd, sect_name, &ctfdata);
+  if (sec == NULL && try_defaults)
+    {
+      sect_name = ".BTF";
+      sec = read_section (abfd, sect_name, &ctfdata);
+    }
+
   if (sec == NULL)
     {
       my_bfd_nonfatal (bfd_get_filename (abfd));
@@ -4936,7 +4946,7 @@ dump_ctf (bfd *abfd, const char *sect_name, const char *parent_name,
       return;
     }
 
-  printf (_("Contents of CTF section %s:\n"), sanitize_string (sect_name));
+  printf (_("Contents of type section %s:\n"), sanitize_string (sect_name));
 
   while ((fp = ctf_archive_next (ctfa, &i, &name, 0, &err)) != NULL)
     {
