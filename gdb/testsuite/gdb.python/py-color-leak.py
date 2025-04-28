1c32c7150dc955b62e810367d23535643dcb36d5
Tom de Vries <tdevries@suse.de>
2025-04-28 18:03:09 +0200

    [gdb/testsuite] Avoid generating gdb_leak_detector.cpython-<n>.pyc
    After running test-case gdb.python/py-color-leak.exp in a container where I
    don't have PYTHONDONTWRITEBYTECODE set, I get:
    ...
    $ ls src/gdb/testsuite/gdb.python/__pycache__/
    gdb_leak_detector.cpython-313.pyc
    ...
    
    Fix this by setting sys.dont_write_bytecode to True in the python scripts
    importing the module.
    
    Tested on x86_64-linux.

--- gdb/testsuite/gdb.python/py-color-leak.py
+++ gdb/testsuite/gdb.python/py-color-leak.py
@@ -13,6 +13,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+
+# Avoid generating
+# src/gdb/testsuite/gdb.python/__pycache__/gdb_leak_detector.cpython-<n>.pyc.
+sys.dont_write_bytecode = True
+
 import gdb_leak_detector
 
 
