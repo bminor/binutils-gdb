bminor/binutils-gdb

commit 344b1e0f5f7900da0087fee1704dd98fd30ea4c4

gas: range-check 3rd argument of .align et al
Negative values would have been silently converted to large positive
ones, which may not be the user's intention. Similarly overly large
values would have been silently truncated. Warn them instead, and zap
such values.

gas/read.c
@@ -1598,7 +1598,13 @@ s_align (signed int arg, int bytes_p)
       else
 	{
 	  ++input_line_pointer;
-	  max = get_absolute_expression ();
+	  offsetT val = get_absolute_expression ();
+	  max = val;
+	  if (val < 0 || max != val)
+	    {
+	      as_warn (_("ignoring out of range alignment maximum"));
+	      max = 0;
+	    }
 	}
     }
 

commit 12b4fc15e7261c2557175b3d5bda64da79718359

LoongArch: Warning about incorrect 3rd argument of .align
344b1e0f5f7 Introduced range-check 3rd argument of .align, incorrect
value are not converted silently. added warning message to fix regression.

gas/testsuite/gas/loongarch/relax_align.d
@@ -1,6 +1,7 @@
 #as:
 #objdump: -dr
 #skip: loongarch32-*-*
+#warning_output: relax_align.l
 
 .*:[    ]+file format .*
 
@@ -44,3 +45,8 @@ Disassembly of section .text:
 [ 	]+68:[ 	]+03400000[ 	]+nop
 [ 	]+6c:[ 	]+03400000[ 	]+nop
 [ 	]+70:[ 	]+4c000020[ 	]+ret
+[ 	]+74:[ 	]+03400000[ 	]+nop
+[ 	]+74: R_LARCH_ALIGN[ 	]+\*ABS\*\+0xc
+[ 	]+78:[ 	]+03400000[ 	]+nop
+[ 	]+7c:[ 	]+03400000[ 	]+nop
+[ 	]+80:[ 	]+4c000020[ 	]+ret

gas/testsuite/gas/loongarch/relax_align.l
@@ -1,0 +1,3 @@
+.*Assembler messages:
+.*: Warning: ignoring out of range alignment maximum
+.*: Warning: ignoring out of range alignment maximum

gas/testsuite/gas/loongarch/relax_align.s
@@ -1,5 +1,4 @@
-# If max < -0x80000000, max becomes a positive number because type conversion
-# (bfd_signed_vma -> unsigned int).
+# Range of max: 0<= max <= 0xffffffff
   .text
 .L1:
   ret
@@ -13,6 +12,9 @@
   ret
   .align 4, , 12
   ret
+  .align 4, , 0xffffffff
+  ret
+# ignore out of range alignment maxumum
   .align 4, , -1
   ret
   .align 4, , -0x80000000