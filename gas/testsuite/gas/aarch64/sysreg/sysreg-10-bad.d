ed62a5351cfbe8468c265d4ff732ddf99acdbbbb
Srinath Parvathaneni <srinath.parvathaneni@arm.com>
2025-06-20 17:21:14 +0100

    aarch64: Support 2024 Debug Architecture system registers.
    This patch adds support for following system registers and the spec
    can be found here[1].
    1. PMBSR_EL12, PMBSR_EL2, PMBSR_EL3, PMBMAR_EL1 depends on FEAT_SPE
       and Armv9.5-A architecture and these are enabled by passing
       -march=armv9.5-a+profile.
    2. TRBSR_EL12, TRBSR_EL2, and TRBSR_EL3 depends Armv9.5-A architecture
       and these are enabled by passing -march=armv9.5-a.
    3. HFGITR2_EL2 depends on Armv8.8-A architecture and enabled by passing
       -march=armv8.8-a.
    
    [1]: https://developer.arm.com/documentation/ddi0601/2025-03/AArch64-Registers?lang=en

---
+++ gas/testsuite/gas/aarch64/sysreg/sysreg-10-bad.d
@@ -0,0 +1,3 @@
+#source: sysreg-10.s
+#as: -march=armv8.7-a -I$srcdir/$subdir
+#error_output: sysreg-10-bad.l
---
+++ gas/testsuite/gas/aarch64/sysreg/sysreg-9-bad.d
@@ -0,0 +1,3 @@
+#source: sysreg-9-bad.s
+#as: -I$srcdir/$subdir
+#error_output: sysreg-9-bad.l
