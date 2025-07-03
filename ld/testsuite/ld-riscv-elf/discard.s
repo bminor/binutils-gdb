f6fc2b523161b7c459e7fc9ae80b288727071be9
Nelson Chu <nelson@rivosinc.com>
2025-07-03 11:16:25 +0800

    RISC-V: Don't allocate dynamic relocation if it's section was discarded

--- ld/testsuite/ld-riscv-elf/discard.s
+++ ld/testsuite/ld-riscv-elf/discard.s
@@ -13,8 +13,53 @@ sym_local:
 sym_global:
 	nop
 
+.global sym_hidden
+.hidden sym_hidden
+sym_hidden:
+	nop
+
+.global sym_global_abs
+.set sym_global_abs, 42
+
+.global sym_weak_undef
+.weak sym_weak_undef
+
+# Got
+
 .section .discard.got_local, "ax"
 	la	x1, sym_local
 
 .section .discard.got_global, "ax"
 	la	x1, sym_global
+
+# Data
+
+.section .discard.local,"a"
+.p2align 1
+discard_local:
+.quad sym_local
+
+.section .discard.global,"a"
+.p2align 1
+discard_global:
+.quad sym_global
+
+.section .discard.hidden,"a"
+.p2align 1
+discard_hidden:
+.quad sym_hidden
+
+.section .discard.global_abs,"a"
+.p2align 1
+discard_global_abs:
+.quad sym_global_abs
+
+.section .discard.weak_undef,"a"
+.p2align 1
+discard_weak_undef:
+.quad sym_weak_undef
+
+.section .discard._DYNAMIC,"a"
+.p2align 1
+discard_DYNAMIC:
+.quad _DYNAMIC
