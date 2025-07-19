bminor/binutils-gdb

commit 470a0288a8180e4e66e0d239dc63d58f013f3d16

gprofng: fix 32892 source line level information not available with "-g -O2"
gprofng ignored DW_AT_specification.
As a result, gprofng skiped Dwarf for all functions declared as:
  < 2>:<0x0000f725> DW_TAG_subprogram(46)
      DW_AT_linkage_name(110)     "func_name"
      DW_AT_declaration*(60)      0x1 (1)
  < 1>:<0x00015acc> DW_TAG_subprogram(46)
       DW_AT_specification(71)    0xf725 (63269)

Another problem was that gprofng ignored DW_AT_ranges.
As a result, many functions are mapped to the <Unknown> module.

gprofng/ChangeLog
2025-05-01  Vladimir Mezentsev  <vladimir.mezentsev@oracle.com>

	PR 32892
	* src/Dwarf.cc: Handle DW_AT_specification and DW_AT_ranges.
	* src/DwarfLib.cc: Likewise.
	* src/DwarfLib.h: Likewise.
	* src/Dwarf.h (get_ranges): New function.
	* src/Stabs.h (get_symbols): New function.
	* src/Stabs.cc: Move Symbol class to src/Symbol.cc.
	* src/Symbol.cc: New file.
	* src/Symbol.h: New file.
	* src/Makefile.am: Add Symbol.cc in build.
	* src/Makefile.in: Rebuild.
	* src/LoadObject.cc (dump_functions): Improve output for -dfunc option.

gprofng/src/Stabs.cc
@@ -35,6 +35,7 @@
 #include "StringBuilder.h"
 #include "DbeFile.h"
 #include "StringMap.h"
+#include "Symbol.h"
 
 #define DISASM_REL_NONE     0     /* symtab search only */
 #define DISASM_REL_ONLY     1     /* relocation search only */
@@ -61,77 +62,6 @@ private:
   int StrTabSize;
   int StabEntSize;
 };
-
-///////////////////////////////////////////////////////////////////////////////
-// class Symbol
-
-class Symbol
-{
-public:
-  Symbol (Vector<Symbol*> *vec = NULL);
-
-  ~Symbol ()
-  {
-    free (name);
-  }
-
-  inline Symbol *
-  cardinal ()
-  {
-    return alias ? alias : this;
-  }
-
-  static void dump (Vector<Symbol*> *vec, char*msg);
-
-  Function *func;
-  Sp_lang_code lang_code;
-  uint64_t value; // st_value used in sym_name()
-  uint64_t save;
-  int64_t size;
-  uint64_t img_offset; // image offset in the ELF file
-  char *name;
-  Symbol *alias;
-  int local_ind;
-  int flags;
-  bool defined;
-};
-
-Symbol::Symbol (Vector<Symbol*> *vec)
-{
-  func = NULL;
-  lang_code = Sp_lang_unknown;
-  value = 0;
-  save = 0;
-  size = 0;
-  img_offset = 0;
-  name = NULL;
-  alias = NULL;
-  local_ind = -1;
-  flags = 0;
-  defined = false;
-  if (vec)
-    vec->append (this);
-}
-
-void
-Symbol::dump (Vector<Symbol*> *vec, char*msg)
-{
-  if (!DUMP_ELF_SYM || vec == NULL || vec->size () == 0)
-    return;
-  printf (NTXT ("======= Symbol::dump: %s =========\n"
-		"         value |    img_offset     | flags|local_ind|\n"), msg);
-  for (int i = 0; i < vec->size (); i++)
-    {
-      Symbol *sp = vec->fetch (i);
-      printf (NTXT ("  %3d %8lld |0x%016llx |%5d |%8d |%s\n"),
-	      i, (long long) sp->value, (long long) sp->img_offset, sp->flags,
-	      sp->local_ind, sp->name ? sp->name : NTXT ("NULL"));
-    }
-  printf (NTXT ("\n===== END of Symbol::dump: %s =========\n\n"), msg);
-}
-
-// end of class Symbol
-///////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////
 // class Reloc
@@ -252,7 +182,6 @@ Stabs::removeDupSyms ()
   long ind, i, last;
   Symbol *symA, *symB;
   SymLst->sort (SymImgOffsetCmp);
-  dump ();
 
   last = 0;
   ind = SymLst->size ();
@@ -1761,10 +1690,12 @@ Stabs::readSymSec (Elf *elf, bool is_dynamic)
       if (asym == NULL)
 	break;
       const char *st_name = bfd_asymbol_name (asym);
+      if (st_name == NULL)
+	continue;
       switch (GELF_ST_TYPE (Sym.st_info))
 	{
 	case STT_FUNC:
-	  if (Sym.st_size == 0)
+	  if (Sym.st_size == 0 || ELF_ST_BIND (Sym.st_info) == STB_WEAK)
 	    break;
 	  if (Sym.st_shndx == 0)
 	    {
@@ -1839,8 +1770,7 @@ Stabs::readSymSec (Elf *elf, bool is_dynamic)
   fixSymtabAlias ();
   SymLst->sort (SymValueCmp);
   get_save_addr (elf->need_swap_endian);
-  dump ();
-}//check_Symtab
+}
 
 void
 Stabs::get_save_addr (bool need_swap_endian)
@@ -2339,6 +2269,7 @@ Stabs::openDwarf ()
     {
       dwarf = new Dwarf (this);
       check_Symtab ();
+      dump();
     }
   return dwarf;
 }
@@ -2363,8 +2294,8 @@ Stabs::dump ()
 	printf ("  %3d: %5d '%s'\n", i, LocalFileIdx->fetch (i),
 		LocalFile->fetch (i));
     }
-  Symbol::dump (SymLst, NTXT ("SymLst"));
-  Symbol::dump (LocalLst, NTXT ("LocalLst"));
+  SymLst->dump ("SymLst");
+  LocalLst->dump ("LocalLst");
   printf (NTXT ("\n===== END of Stabs::dump: %s =========\n\n"),
   path ? path : NTXT ("NULL"));
 }

gprofng/src/Dwarf.cc
@@ -29,6 +29,7 @@
 #include "LoadObject.h"
 #include "Module.h"
 #include "DefaultMap.h"
+#include "Symbol.h"
 
 static int
 datatypeCmp (const void *a, const void *b)
@@ -45,7 +46,6 @@ targetOffsetCmp (const void *a, const void *b)
   uint32_t o2 = ((target_info_t *) b)->offset;
   return o1 == o2 ? 0 : (o1 < o2 ? -1 : 1);
 }
-
 
 //////////////////////////////////////////////////////////
 //  class Dwr_type
@@ -441,7 +441,12 @@ DwrCU::get_linkage_name ()
   nm = Dwarf_string (DW_AT_SUN_link_name);
   if (nm != NULL)
     return nm;
-  return Dwarf_string (DW_AT_MIPS_linkage_name);
+  if (nm != NULL)
+    return nm;
+  nm = Dwarf_string (DW_AT_MIPS_linkage_name);
+  if (nm != NULL)
+    return nm;
+  return Dwarf_string (DW_AT_name);
 }
 
 void
@@ -490,8 +495,10 @@ DwrCU::parseChild (Dwarf_cnt *ctx)
 	    }
 	  break;
 	case DW_TAG_subprogram:
+	{
 	  if (dwrTag.get_attr (DW_AT_abstract_origin))
 	    break;
+	  Symbol *sym = NULL;
 	  if (dwrTag.get_attr (DW_AT_declaration))
 	    {
 	      // Only declaration
@@ -499,26 +506,71 @@ DwrCU::parseChild (Dwarf_cnt *ctx)
 		{
 		  char *link_name = Dwarf_string (DW_AT_name);
 		  if (link_name && streq (link_name, NTXT ("MAIN")))
-		    ctx->fortranMAIN = Stabs::find_func (NTXT ("MAIN"), ctx->module->functions, true, true);
+		    ctx->fortranMAIN = Stabs::find_func (NTXT ("MAIN"),
+					    ctx->module->functions, true, true);
+		  }
+		sym = Symbol::get_symbol (symbols_sorted_by_name,
+					 get_linkage_name ());
+		if (sym == NULL)
+		  break;
+		func = append_Function (sym, ctx->name);
+		break;
+	    }
+
+	  Dwr_Attr *dwrAttr = dwrTag.get_attr (DW_AT_specification);
+	  if (dwrAttr)
+	    {
+	      // Find previous declaration to inherit settings.
+	      sym = find_declaration (dwrAttr->u.offset);
+	      if (sym == NULL)
+		break;
+	      func = sym->func;
+	      if (func == NULL)
+		break;
+	      set_source (func);
+
+	      Vector <Range *> *ranges = get_ranges ();
+	      if (ranges)
+		{
+		  Vector<Symbol *> *syms = Symbol::find_symbols (symbols, ranges);
+		  Destroy (ranges);
+		  for (int i = 0, sz = VecSize (syms); i < sz; i++)
+		    {
+		      Symbol *sp = syms->get (i);
+		      if (sp->alias)
+			sp = sp->alias;
+		      Function *f = sp->func;
+		      if (f == NULL)
+			f = sp->createFunction (func->module);
+		      f->setLineFirst (func->line_first);
+		      f->setDefSrc (func->def_source);
+		    }
+		  delete (syms);
 		}
 	      break;
 	    }
-	  func = append_Function (ctx);
-	  if (func)
-	    {
-	      if (Stabs::is_fortran (ctx->module->lang_code) &&
-		  streq (func->get_match_name (), NTXT ("MAIN")))
-		ctx->fortranMAIN = func;
-	      old_name = ctx->name;
-	      Function *old_func = ctx->func;
-	      ctx->name = func->get_match_name ();
-	      ctx->func = func;
-	      parseChild (ctx);
-	      hasChild = 0;
-	      ctx->name = old_name;
-	      ctx->func = old_func;
-	    }
+
+	  sym = Symbol::get_symbol (symbols_sorted_by_name, get_linkage_name ());
+	  if (sym == NULL)
+	    sym = Symbol::get_symbol (symbols, get_low_pc ());
+	  if (sym == NULL)
+	    break;
+	  func = append_Function (sym, ctx->name);
+	  if (Stabs::is_fortran (ctx->module->lang_code) &&
+	      streq (func->get_match_name (), "MAIN"))
+	    ctx->fortranMAIN = func;
+	  set_source (func);
+
+	  old_name = ctx->name;
+	  Function *old_func = ctx->func;
+	  ctx->name = func->get_match_name ();
+	  ctx->func = func;
+	  parseChild (ctx);
+	  hasChild = 0;
+	  ctx->name = old_name;
+	  ctx->func = old_func;
 	  break;
+	}
 	case DW_TAG_module:
 	  old_name = ctx->name;
 	  ctx->name = Dwarf_string (DW_AT_SUN_link_name);
@@ -631,6 +683,21 @@ Dwarf::archive_Dwarf (LoadObject *lo)
 			STR (lo_name), STR (mod->get_name ()));
 	      dwrCU->dwrInlinedSubrs->dump (msg);
 	    }
+	  for (int i = 0, sz = VecSize (dwrCU->symbols); i < sz; i++)
+	    {
+	      Symbol *sp = dwrCU->symbols->get (i);
+	      Function *f = sp->func;
+	      if (f == NULL)
+		{
+		  f = sp->createFunction (mod);
+		  if (sp->alias && sp->alias->func)
+		    {
+		      Function *func = sp->alias->func;
+		      f->setLineFirst (func->line_first);
+		      f->setDefSrc (func->def_source);
+		    }
+		}
+	    }
 	}
     }
   return true;
@@ -645,6 +712,38 @@ Dwarf::srcline_Dwarf (Module *module)
   dwrCU->map_dwarf_lines (module);
 }
 
+static int
+rangeCmp (const void *a, const void *b)
+{
+  Range *item1 = *((Range **) a);
+  Range *item2 = *((Range **) b);
+  return item1->low < item2->low ? -1 : (item1->low == item2->low ? 0 : 1);
+}
+
+Vector<Range *> *
+Dwarf::get_ranges (uint64_t offset)
+{
+  if (debug_rangesSec == NULL)
+    return NULL;
+  if (offset >= debug_rangesSec->size)
+    {
+      Dprintf (DUMP_DWARFLIB, "ERROR: Dwarf::get_ranges(0x%llx). size=0x%llx\n",
+	       (long long) offset, (long long) debug_rangesSec->size);
+      return NULL;
+    }
+  Vector<Range*> *ranges = new Vector<Range*>();
+  debug_rangesSec->offset = offset;
+  for (;;)
+    {
+      uint64_t low_pc = debug_rangesSec->GetADDR ();
+      uint64_t high_pc = debug_rangesSec->GetADDR ();
+      if (low_pc == 0 || low_pc > high_pc)
+	break;
+      ranges->append (new Range (low_pc, high_pc));
+    }
+  ranges->sort (rangeCmp);
+  return ranges;
+}
 
 // parse hwcprof info for given module in loadobject
 
@@ -797,11 +896,17 @@ DwrCU::read_hwcprof_info (Dwarf_cnt *ctx)
 	case DW_TAG_subprogram:
 	  {
 	    Function *old_func = ctx->func;
-	    if (dwrTag.get_attr (DW_AT_abstract_origin)
-				 || dwrTag.get_attr (DW_AT_declaration))
-	      ctx->func = NULL;
-	    else
-	      ctx->func = append_Function (ctx);
+	    ctx->func = NULL;
+	    if (dwrTag.get_attr (DW_AT_abstract_origin) == NULL
+				 && dwrTag.get_attr (DW_AT_declaration) == NULL)
+	      {
+		Symbol *sym = Symbol::get_symbol (symbols_sorted_by_name,
+						 get_linkage_name ());
+		if (sym == NULL)
+		  sym = Symbol::get_symbol (symbols, get_low_pc ());
+		if (sym != NULL)
+		  ctx->func = sym->func;
+	      }
 	    read_hwcprof_info (ctx);
 	    ctx->func = old_func;
 	    break;
@@ -955,49 +1060,31 @@ DwrCU::read_hwcprof_info (Dwarf_cnt *ctx)
 
 // Append function to module
 Function *
-DwrCU::append_Function (Dwarf_cnt *ctx)
+DwrCU::append_Function (Symbol *sym, const char *outerName)
 {
-  char *outerName = ctx->name, *name, tmpname[2048];
-  Function *func;
+  if (sym->func != NULL)
+    return sym->func;
+  Function *func = sym->createFunction (module);
+
   char *fname = Dwarf_string (DW_AT_name);
-  if (fname && outerName && !strchr (fname, '.'))
+  if (fname)
     {
-      size_t outerlen = strlen (outerName);
-      if (outerlen > 0 && outerName[outerlen - 1] == '_')
+      if (outerName && !strchr (fname, '.'))
 	{
-	  outerlen--;
-	  snprintf (tmpname, sizeof (tmpname), NTXT ("%s"), outerName);
-	  snprintf (tmpname + outerlen, sizeof (tmpname) - outerlen, NTXT (".%s_"), fname);
+	  char *tmpname;
+	  int outerlen = (int) strlen (outerName);
+	  if (outerlen > 0 && outerName[outerlen - 1] == '_')
+	    tmpname = dbe_sprintf ("%.*s.%s_", outerlen - 1, outerName, fname);
+	  else
+	    tmpname = dbe_sprintf ("%s.%s", outerName, fname);
+	  func->set_match_name (tmpname);
+	  Dprintf (DUMP_DWARFLIB, "Generated innerfunc name %s\n", tmpname);
+	  free(tmpname);
 	}
       else
-	snprintf (tmpname, sizeof (tmpname), NTXT ("%s.%s"), outerName, fname);
-      name = tmpname;
-      Dprintf (DUMP_DWARFLIB, NTXT ("Generated innerfunc name %s\n"), name);
+	func->set_match_name (fname);
     }
-  else
-    name = fname;
-
-  char *link_name = get_linkage_name ();
-  if (link_name == NULL)
-    link_name = name;
-
-  uint64_t pc = get_low_pc ();
-  func = dwarf->stabs->append_Function (module, link_name, pc);
-  if (func != NULL)
-    {
-      int lineno = (int) Dwarf_data (DW_AT_decl_line);
-      func->set_match_name (name);
-      if (lineno > 0)
-	{
-	  func->setLineFirst (lineno);
-	  int fileno = ((int) Dwarf_data (DW_AT_decl_file));
-	  SourceFile *sf = ((fileno >= 0) && (fileno < VecSize (srcFiles))) ? srcFiles->get (fileno)
-		  : module->getMainSrc ();
-	  func->setDefSrc (sf);
-	  func->pushSrcFile (func->def_source, 0);
-	  func->popSrcFile ();
-	}
-    }
+  set_source (func);
   return func;
 }
 

gprofng/src/Dwarf.h
@@ -60,6 +60,7 @@ class LoadObject;
 class Module;
 class DwrCU;
 class DwrSec;
+class Range;
 
 class Dwarf
 {
@@ -69,6 +70,7 @@ public:
   bool archive_Dwarf (LoadObject *lo);
   void srcline_Dwarf (Module *module);
   void read_hwcprof_info (Module *module);
+  Vector<Range *> *get_ranges (uint64_t offset);
 
   Stabs::Stab_status status;
   Vector<DwrCU *> *dwrCUs;

gprofng/src/DwarfLib.cc
@@ -27,9 +27,9 @@
 #include "Elf.h"
 #include "Function.h"
 #include "Module.h"
-#include "StringBuilder.h"
 #include "DbeArray.h"
 #include "DbeSession.h"
+#include "Symbol.h"
 
 #define NO_STMT_LIST ((uint64_t) -1)
 #define CASE_S(x)   case x: s = (char *) #x; break
@@ -1795,6 +1795,8 @@ DwrLineRegs::getPath (int fn)
 DwrCU::DwrCU (Dwarf *_dwarf)
 {
   dwarf = _dwarf;
+  symbols = NULL;
+  symbols_sorted_by_name = NULL;
   cu_offset = dwarf->debug_infoSec->offset;
   debug_infoSec = new DwrSec (dwarf->debug_infoSec, cu_offset);
   next_cu_offset = debug_infoSec->ReadLength ();
@@ -1883,6 +1885,8 @@ DwrCU::~DwrCU ()
   Destroy (dwrInlinedSubrs);
   delete srcFiles;
   delete dwrLineReg;
+  delete symbols;
+  delete symbols_sorted_by_name;
   free (comp_dir);
 }
 
@@ -2187,7 +2191,78 @@ DwrCU::parse_cu_header (LoadObject *lo)
   else
     path = dbe_strdup (dwarf->stabs->path);
   module->set_name (path);
+
+  // create a list of functions in this CU
+  Vector <Range *> *ranges = get_ranges ();
+  if (ranges)
+    {
+      Vector <Symbol *> *syms = dwarf->stabs->get_symbols ();
+      symbols = Symbol::find_symbols (syms, ranges);
+      symbols_sorted_by_name = Symbol::sort_by_name (syms);
+      Destroy (ranges);
+    }
   return module;
+}
+
+Vector <Range *> *
+DwrCU::get_ranges ()
+{
+  Vector <Range *> *ranges = NULL;
+  Dwr_Attr *dwrAttr = dwrTag.get_attr (DW_AT_ranges);
+  if (dwrAttr)
+    {
+      Dprintf (DUMP_DWARFLIB, "DwrCU::get_ranges: 0x%llx\n",
+	       (long long) dwrAttr->u.offset);
+      ranges = dwarf->get_ranges (dwrAttr->u.offset);
+    }
+  else
+    {
+      uint64_t low_pc = Dwarf_addr (DW_AT_low_pc);
+      if (low_pc > 0)
+	{
+	  uint64_t high_pc = get_high_pc (low_pc);
+	  ranges = new Vector <Range *> (1);
+	  ranges->append (new Range (low_pc, high_pc));
+	  Dprintf (DUMP_DWARFLIB, "DwrCU::get_ranges: pc=0x%llx\n",
+		   (long long) low_pc);
+	}
+    }
+  if (ranges && DUMP_DWARFLIB)
+    ranges->dump (" ");
+  return ranges;
+}
+
+void
+DwrCU::set_source (Function *func)
+{
+  int lineno = (int) Dwarf_data (DW_AT_decl_line);
+  func->setLineFirst (lineno);
+
+  int fileno = (int) Dwarf_data (DW_AT_decl_file);
+  if (fileno > 0 && fileno < VecSize (srcFiles))
+    func->setDefSrc (srcFiles->get (fileno));
+}
+
+Symbol *
+DwrCU::find_declaration (int64_t offset)
+{
+  int64_t old_offset = dwrTag.offset;
+  Symbol *sym = NULL;
+  if (set_die (offset) == DW_DLV_OK)
+    {
+      sym = Symbol::get_symbol (symbols_sorted_by_name, get_linkage_name ());
+      if (sym && sym->func == NULL)
+	{
+	  Function *func = sym->createFunction (module);
+	  int lineno = (int) Dwarf_data (DW_AT_decl_line);
+	  func->setLineFirst (lineno);
+	  int fileno = (int) Dwarf_data (DW_AT_decl_file);
+	  if (fileno > 0 && fileno < VecSize (srcFiles))
+	    func->setDefSrc (srcFiles->get (fileno));
+	}
+    }
+  set_die (old_offset);
+  return sym;
 }
 
 Dwr_Attr *
@@ -2357,7 +2432,7 @@ DwrCU::map_dwarf_lines (Module *mod)
 	  InlinedSubr *p = func->inlinedSubr + func->inlinedSubrCnt;
 	  func->inlinedSubrCnt++;
 	  int fileno = inlinedSubr->file - 1;
-	  SourceFile *sf = ((fileno >= 0) && (fileno < VecSize (srcFiles))) ?
+	  SourceFile *sf = ((fileno > 0) && (fileno < VecSize (srcFiles))) ?
 		  srcFiles->get (fileno) : dbeSession->get_Unknown_Source ();
 	  p->dbeLine = sf->find_dbeline (inlinedSubr->line);
 	  p->high_pc = inlinedSubr->high_pc - low_pc;

gprofng/src/DwarfLib.h
@@ -25,6 +25,8 @@
 
 class ElfReloc;
 class Dwr_type;
+class Function;
+class Range;
 class SourceFile;
 
 template <class ITEM> class Vector;
@@ -72,12 +74,6 @@ public:
   {
     return (uint32_t) GetULEB128 ();
   }
-
-  bool
-  inRange (uint64_t left, uint64_t right)
-  {
-    return (offset >= left) && (offset < right);
-  };
 
   ElfReloc *reloc;
   uint64_t sizeSec;
@@ -280,6 +276,8 @@ public:
   uint64_t cu_header_offset;
   uint64_t cu_offset;
   uint64_t next_cu_offset;
+  Vector<Symbol*> *symbols;    // all symbols in this CU are sorted by pc
+  Vector<Symbol*> *symbols_sorted_by_name;
   Vector<DwrInlinedSubr*> *dwrInlinedSubrs;
   Vector<SourceFile *> *srcFiles;
   bool isMemop;
@@ -287,7 +285,10 @@ public:
 
 private:
   void build_abbrevTable (DwrSec *debug_abbrevSec, uint64_t stmt_list_offset);
-  Function *append_Function (Dwarf_cnt *ctx);
+  Function *append_Function (Symbol *sym, const char *outerName);
+  Symbol *find_declaration(int64_t offset);
+  Vector <Range *> *get_ranges();
+  void set_source (Function *func);
   void parse_inlined_subroutine (Dwarf_cnt *ctx);
   uint64_t get_low_pc ();
   uint64_t get_high_pc (uint64_t low_pc);

gprofng/src/LoadObject.cc
@@ -20,6 +20,7 @@
 
 #include "config.h"
 #include <errno.h>
+#include <libgen.h>
 
 #include "util.h"
 #include "StringBuilder.h"
@@ -303,13 +304,16 @@ LoadObject::dump_functions (FILE *out)
 	  {
 	    mname = fitem->module ? fitem->module->file_name : noname->file_name;
 	    sname = fitem->getDefSrcName ();
-	    fprintf (out,
-		     "id %6llu, @0x%llx - 0x%llx [save 0x%llx] o-%lld sz-%lld %s (module = %s)",
-		     (ull_t) fitem->id, (ull_t) fitem->img_offset,
-		     (ull_t) (fitem->img_offset + fitem->size),
-		     (ull_t) fitem->save_addr, (ull_t) fitem->img_offset,
-		     (ll_t) fitem->size, fitem->get_name (), mname);
-	    if (sname && !streq (sname, mname))
+	    fprintf (out, "id %6llu, @0x%llx-0x%llx sz-%lld", (ull_t) fitem->id,
+		    (ull_t) fitem->img_offset,
+		    (ull_t) (fitem->img_offset + fitem->size),
+		    (ll_t) fitem->size);
+	    if (fitem->save_addr != 0)
+	      fprintf (out, " [save 0x%llx]", (ull_t) fitem->save_addr);
+	    if (strcmp (fitem->get_mangled_name (), fitem->get_name ()) != 0)
+	      fprintf (out, " [%s]", fitem->get_mangled_name ());
+	    fprintf (out, " %s (module = %s)", fitem->get_name (), mname);
+	    if (sname && strcmp (basename (sname), basename (mname)) != 0)
 	      fprintf (out, " (Source = %s)", sname);
 	    fprintf (out, "\n");
 	  }

gprofng/src/Makefile.am
@@ -78,6 +78,7 @@ CCSOURCES = \
 	Stabs.cc \
 	Stats_data.cc \
 	StringBuilder.cc \
+	Symbol.cc \
 	Table.cc \
 	QLParser.tab.cc \
 	dbe_collctrl.cc \

gprofng/src/Makefile.in
@@ -174,7 +174,7 @@ am__objects_1 = Application.lo BaseMetric.lo BaseMetricTreeNode.lo \
 	MemorySpace.lo Metric.lo MetricList.lo Module.lo Ovw_data.lo \
 	PRBTree.lo PathTree.lo PreviewExp.lo Print.lo \
 	SAXParserFactory.lo Sample.lo Settings.lo SourceFile.lo \
-	Stabs.lo Stats_data.lo StringBuilder.lo Table.lo \
+	Stabs.lo Stats_data.lo StringBuilder.lo Symbol.lo Table.lo \
 	QLParser.tab.lo dbe_collctrl.lo i18n.lo parse.lo UserLabel.lo \
 	util.lo Dbe.lo
 am__objects_2 = dbe_hwcdrv.lo dbe_hwcfuncs.lo dbe_hwctable.lo \
@@ -505,6 +505,7 @@ CCSOURCES = \
 	Stabs.cc \
 	Stats_data.cc \
 	StringBuilder.cc \
+	Symbol.cc \
 	Table.cc \
 	QLParser.tab.cc \
 	dbe_collctrl.cc \
@@ -777,6 +778,7 @@ distclean-compile:
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/Stabs.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/Stats_data.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/StringBuilder.Plo@am__quote@
+@AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/Symbol.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/Table.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/UserLabel.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/checks.Po@am__quote@

commit 00df7c126b9a0e551b32aeba9c7cb71c0e2a8e7e

gprofng: do not skip weak symbols
PR gprofng/33151

gprofng ignores functions that are compiled as weak symbols. This
heavily affects C++ class methods that are always compiled by g++
and clang++ as weak symbols. In this case 'gprofng display text'
just displays <static>@ADDRESS(<FILENAME>) instead of proper method
name.

The bug has been introduced in the commit 470a0288a818.

gprofng/src/Stabs.cc
@@ -1546,7 +1546,7 @@ Stabs::readSymSec (Elf *elf, bool is_dynamic)
       switch (GELF_ST_TYPE (Sym.st_info))
 	{
 	case STT_FUNC:
-	  if (Sym.st_size == 0 || ELF_ST_BIND (Sym.st_info) == STB_WEAK)
+	  if (Sym.st_size == 0)
 	    break;
 	  if (Sym.st_shndx == 0)
 	    {