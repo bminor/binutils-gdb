1ff92d0903f6d660131e93f4274e0db9d42a7f25
Andrew Burgess <aburgess@redhat.com>
2025-08-01 12:26:41 +0100

    gdbserver: better handling for missing argument values
    By passing ':' within the optstring to getopt_long, the getopt_long
    call will now return ':' for missing value errors and '?' for unknown
    argument errors, rather than returning '?' for all error types.
    
    We can now print a different error message for missing argument
    values.  For example:
    
      $ gdbserver --debug-file :54321 /tmp/hello
      Missing argument value for: --debug-file
    
    Compared to:
    
      $ gdbserver --unknown :54321 ~/tmp/hello.x
      Unknown argument: --unknown
    
    Current HEAD gdbserver treats every error as the 'Unknown argument'
    error.
    
    While I was messing with the code that prints these error messages,
    I've wrapped then with _(...) to allow for internationalisation.
    
    Approved-By: Tom Tromey <tom@tromey.com>

--- gdbserver/server.cc
+++ gdbserver/server.cc
@@ -4202,7 +4202,7 @@ captured_main (int argc, char *argv[])
      If getopt_long is free to reorder ARGV then it will try to steal those
      arguments for itself.  */
   while ((longindex = -1,
-	  optc = getopt_long (argc, argv, "+", longopts, &longindex)) != -1)
+	  optc = getopt_long (argc, argv, "+:", longopts, &longindex)) != -1)
     {
       /* As a GNU extension, getopt_long supports '--arg value' form,
 	 without an '=' symbol between the 'arg' and the 'value'.  This
@@ -4254,7 +4254,7 @@ captured_main (int argc, char *argv[])
 	      /* For required arguments, if we don't have an argument, then
 		 this is an errror, set OPTC to reflect this.  */
 	      if (longopts[longindex].has_arg == required_argument)
-		optc = '?';
+		optc = ':';
 	    }
 	}
 
@@ -4427,6 +4427,7 @@ captured_main (int argc, char *argv[])
 	  escape_args = false;
 	  break;
 
+	case ':':
 	case '?':
 	  /* Figuring out which element of ARGV contained the invalid
 	     argument is not simple.  There are a couple of cases we need
@@ -4453,7 +4454,11 @@ captured_main (int argc, char *argv[])
 	  else
 	    bad_arg = argv[optind];
 
-	  fprintf (stderr, "Unknown argument: %s\n", bad_arg.c_str ());
+	  if (optc == '?')
+	    fprintf (stderr, _("Unknown argument: %s\n"), bad_arg.c_str ());
+	  else
+	    fprintf (stderr, _("Missing argument value for: %s\n"),
+		     bad_arg.c_str ());
 	  exit (1);
 	}
     }
