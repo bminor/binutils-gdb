2f6db3e2e77d7e10b407fb43b13a985e9b4211ed
Tom Tromey <tromey@adacore.com>
2025-08-06 07:14:56 -0600

    Revert "Call target_can_do_single_step from maybe_software_singlestep"
    This reverts commit 14de1447c9c52c1bfc52588f8652836f66ac6c47.
    
    An automated tester said that this patch caused a regression on
    aarch64:
    
    FAIL: gdb.arch/aarch64-atomic-inst.exp: Step through the ldxr/stxr sequence (timeout)
    
    I looked into it a bit yesterday but couldn't see an obvious problem;
    and it's somewhat of a pain to try to debug it at the moment.
    
    Tom de Vries also noticed this and filed it in bugzilla.  So, I'm
    backing the patch out until I can port the failing test to the AdaCore
    internal test suite in order to find out what went wrong.
    
    Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=28440
    Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=33255

--- gdb/breakpoint.c
+++ gdb/breakpoint.c
@@ -13998,26 +13998,11 @@ insert_single_step_breakpoint (struct gdbarch *gdbarch,
   update_global_location_list (UGLL_INSERT);
 }
 
-/* Try to setup for software single stepping.  Return true if
-   target_resume() should use hardware single step.
+/* Insert single step breakpoints according to the current state.  */
 
-   GDBARCH is the current gdbarch.  */
-
-bool
-maybe_software_singlestep (struct gdbarch *gdbarch)
+int
+insert_single_step_breakpoints (struct gdbarch *gdbarch)
 {
-  if (execution_direction != EXEC_FORWARD)
-    return true;
-
-  if (target_can_do_single_step () == 1)
-    {
-      /* The target definitely has hardware single step.  */
-      return true;
-    }
-
-  if (!gdbarch_software_single_step_p (gdbarch))
-    return true;
-
   regcache *regcache = get_thread_regcache (inferior_thread ());
   std::vector<CORE_ADDR> next_pcs;
 
@@ -14031,10 +14016,10 @@ maybe_software_singlestep (struct gdbarch *gdbarch)
       for (CORE_ADDR pc : next_pcs)
 	insert_single_step_breakpoint (gdbarch, aspace, pc);
 
-      return false;
+      return 1;
     }
   else
-    return true;
+    return 0;
 }
 
 /* See breakpoint.h.  */
