8acc4517846fda60360d8f14b2eddd35dbcff2c2
Alan Modra <amodra@gmail.com>
2025-08-20 08:24:23 +0930

    Re: gas: testsuite: fix regression in cfi-common-10.d
    Just match what the test is actually trying to verify, to fix more
    regressions.

--- gas/testsuite/gas/cfi/cfi-common-10.d
+++ gas/testsuite/gas/cfi/cfi-common-10.d
@@ -1,19 +1,8 @@
 #objdump: -Wf
 #name: CFI common 10
-#...
-Contents of the .eh_frame section:
 
-0+0000 0+00(10|14) 0+0000 CIE
-  Version:               1
-  Augmentation:          "zR"
-  Code alignment factor: .*
-  Data alignment factor: .*
-  Return address column: .*
-  Augmentation data:     [01][abc]
 #...
-
-0+00(14|18) 0+00(18|1c) 0+00(18|1c) FDE cie=0+0000 pc=.*
+Contents of the \.eh_frame section:
 #...
-  DW_CFA_advance_loc: 4 to 0+0004
   DW_CFA_def_cfa_offset: 1099511627808
-#...
+#pass
