tromey/gdb

Make dw_expand_symtabs_matching_file_matcher static
dw_expand_symtabs_matching_file_matcher is no longer needed outside of
read.c, so it can be made static.

gdb/dwarf2/read.c
@@ -867,6 +867,11 @@ static struct dwarf2_section_info *cu_debug_loc_section (struct dwarf2_cu *cu);
 static struct dwarf2_section_info *cu_debug_rnglists_section
   (struct dwarf2_cu *cu, dwarf_tag tag);
 
+static void dw_expand_symtabs_matching_file_matcher
+  (dwarf2_per_objfile *per_objfile,
+   auto_bool_vector &marked,
+   expand_symtabs_file_matcher file_matcher);
+
 static void get_scope_pc_bounds (struct die_info *,
 				 unrelocated_addr *, unrelocated_addr *,
 				 struct dwarf2_cu *);
@@ -2032,9 +2037,11 @@ dw2_expand_symtabs_matching_one
   return true;
 }
 
-/* See read.h.  */
+/* If FILE_MATCHER is non-NULL, set all the
+   dwarf2_per_cu_quick_data::MARK of the current DWARF2_PER_OBJFILE
+   that match FILE_MATCHER.  */
 
-void
+static void
 dw_expand_symtabs_matching_file_matcher
   (dwarf2_per_objfile *per_objfile,
    auto_bool_vector &marked,

bminor/binutils-gdb

Acked-By: Simon Marchi <simon.marchi@efficios.com>

gdb/dwarf2/read.c
@@ -851,6 +851,11 @@ static struct dwarf2_section_info *cu_debug_loc_section (struct dwarf2_cu *cu);
 static struct dwarf2_section_info *cu_debug_rnglists_section
   (struct dwarf2_cu *cu, dwarf_tag tag);
 
+static void dw_expand_symtabs_matching_file_matcher
+  (dwarf2_per_objfile *per_objfile,
+   auto_bool_vector &cus_to_skip,
+   expand_symtabs_file_matcher file_matcher);
+
 static void get_scope_pc_bounds (struct die_info *,
 				 unrelocated_addr *, unrelocated_addr *,
 				 struct dwarf2_cu *);
@@ -2030,9 +2035,10 @@ dw2_expand_symtabs_matching_one
   return true;
 }
 
-/* See read.h.  */
+/* If FILE_MATCHER is non-NULL, update CUS_TO_SKIP as appropriate
+   based on FILE_MATCHER.  */
 
-void
+static void
 dw_expand_symtabs_matching_file_matcher
   (dwarf2_per_objfile *per_objfile,
    auto_bool_vector &cus_to_skip,