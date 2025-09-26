65a5bb3c1fc4632ed71dfb8c211ff756619afd29
Andrew Hanson <andrew@andrewhanson.dev>
2025-09-26 12:22:17 +0200

    include/binary-io.h: guard O_BINARY usage with defined check
    include/
    
    	* binary-io.h: Fix -Wundef compiler warning when O_BINARY is not
    	defined. Wrap the check to ensure portability across platforms.
    
    Signed-off-by: Andrew Hanson <andrew@andrewhanson.dev>

--- include/binary-io.h
+++ include/binary-io.h
@@ -35,7 +35,7 @@
 # undef O_BINARY
 # undef O_TEXT
 #endif
-#if O_BINARY
+#if defined(O_BINARY) && O_BINARY
 # if defined __EMX__ || defined __DJGPP__ || defined __CYGWIN__
 #  include <io.h> /* declares setmode() */
 # else
