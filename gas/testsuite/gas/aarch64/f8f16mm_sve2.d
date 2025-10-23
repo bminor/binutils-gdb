facb933805565219a635beace8265d01c0318685
Alfie Richards <alfie.richards@arm.com>
2025-10-23 09:45:22 +0000

    aarch64: gas: Allow movprfx with fmmla and bfscale [PR gas/33562]
    These instructions were previously incorrectly marked as not accepting
    movprfx.  Fix this and add tests.
    
    	PR gas/33562
    
    opcodes:
    	* aarch64-tbl.h: Update widening fmmmla and bfscale instructions.
    gas:
    	* testsuite/gas/aarch64/f8f16mm_sve2-bad.l: Update test with movprfx.
    	* testsuite/gas/aarch64/f8f16mm_sve2.d: Ditto.
    	* testsuite/gas/aarch64/f8f16mm_sve2.s: Ditto.
    	* testsuite/gas/aarch64/f8f32mm_sve2-bad.l: Ditto.
    	* testsuite/gas/aarch64/f8f32mm_sve2.d: Ditto.
    	* testsuite/gas/aarch64/f8f32mm_sve2.s: Ditto.
    	* testsuite/gas/aarch64/sve-f16f32mm-bad.l: Ditto.
    	* testsuite/gas/aarch64/sve-f16f32mm.d: Ditto.
    	* testsuite/gas/aarch64/sve-f16f32mm.s: Ditto.
    	* testsuite/gas/aarch64/sve-bfscale-sve2.s: Ditto.
    	* testsuite/gas/aarch64/sve-bfscale-sve2.d: Ditto.
    
    Approved-By: Alice Carlotti <alice.carlotti@arm.com>

--- gas/testsuite/gas/aarch64/f8f16mm_sve2.d
+++ gas/testsuite/gas/aarch64/f8f16mm_sve2.d
@@ -16,3 +16,21 @@ Disassembly of section .*:
 .*:	647fe01f 	fmmla	z31.h, z0.b, z31.b
 .*:	6460e3ff 	fmmla	z31.h, z31.b, z0.b
 .*:	647fe3ff 	fmmla	z31.h, z31.b, z31.b
+
+.* <b>:
+.*:	0420bde0 	movprfx	z0, z15
+.*:	6461e020 	fmmla	z0.h, z1.b, z1.b
+.*:	0420bde0 	movprfx	z0, z15
+.*:	647fe020 	fmmla	z0.h, z1.b, z31.b
+.*:	0420bde0 	movprfx	z0, z15
+.*:	6461e3e0 	fmmla	z0.h, z31.b, z1.b
+.*:	0420bde0 	movprfx	z0, z15
+.*:	647fe3e0 	fmmla	z0.h, z31.b, z31.b
+.*:	0420bdff 	movprfx	z31, z15
+.*:	6460e01f 	fmmla	z31.h, z0.b, z0.b
+.*:	0420bdff 	movprfx	z31, z15
+.*:	647ee01f 	fmmla	z31.h, z0.b, z30.b
+.*:	0420bdff 	movprfx	z31, z15
+.*:	6460e3df 	fmmla	z31.h, z30.b, z0.b
+.*:	0420bdff 	movprfx	z31, z15
+.*:	647ee3df 	fmmla	z31.h, z30.b, z30.b
--- gas/testsuite/gas/aarch64/sve-bfscale-sve2.d
+++ gas/testsuite/gas/aarch64/sve-bfscale-sve2.d
@@ -16,3 +16,35 @@ Disassembly of section .*:
 .*:	650983ff 	bfscale	z31.h, p0/m, z31.h, z31.h
 .*:	65099c1f 	bfscale	z31.h, p7/m, z31.h, z0.h
 .*:	65099fff 	bfscale	z31.h, p7/m, z31.h, z31.h
+.*:	0420bde0 	movprfx	z0, z15
+.*:	65098020 	bfscale	z0.h, p0/m, z0.h, z1.h
+.*:	0420bde0 	movprfx	z0, z15
+.*:	650983e0 	bfscale	z0.h, p0/m, z0.h, z31.h
+.*:	0420bde0 	movprfx	z0, z15
+.*:	65099c20 	bfscale	z0.h, p7/m, z0.h, z1.h
+.*:	0420bde0 	movprfx	z0, z15
+.*:	65099fe0 	bfscale	z0.h, p7/m, z0.h, z31.h
+.*:	0420bdff 	movprfx	z31, z15
+.*:	6509801f 	bfscale	z31.h, p0/m, z31.h, z0.h
+.*:	0420bdff 	movprfx	z31, z15
+.*:	650983df 	bfscale	z31.h, p0/m, z31.h, z30.h
+.*:	0420bdff 	movprfx	z31, z15
+.*:	65099c1f 	bfscale	z31.h, p7/m, z31.h, z0.h
+.*:	0420bdff 	movprfx	z31, z15
+.*:	65099fdf 	bfscale	z31.h, p7/m, z31.h, z30.h
+.*:	045021e0 	movprfx	z0.h, p0/z, z15.h
+.*:	65098020 	bfscale	z0.h, p0/m, z0.h, z1.h
+.*:	045121e0 	movprfx	z0.h, p0/m, z15.h
+.*:	650983e0 	bfscale	z0.h, p0/m, z0.h, z31.h
+.*:	04503de0 	movprfx	z0.h, p7/z, z15.h
+.*:	65099c20 	bfscale	z0.h, p7/m, z0.h, z1.h
+.*:	04513de0 	movprfx	z0.h, p7/m, z15.h
+.*:	65099fe0 	bfscale	z0.h, p7/m, z0.h, z31.h
+.*:	045021ff 	movprfx	z31.h, p0/z, z15.h
+.*:	6509801f 	bfscale	z31.h, p0/m, z31.h, z0.h
+.*:	045121ff 	movprfx	z31.h, p0/m, z15.h
+.*:	650983df 	bfscale	z31.h, p0/m, z31.h, z30.h
+.*:	04503dff 	movprfx	z31.h, p7/z, z15.h
+.*:	65099c1f 	bfscale	z31.h, p7/m, z31.h, z0.h
+.*:	04513dff 	movprfx	z31.h, p7/m, z15.h
+.*:	65099fdf 	bfscale	z31.h, p7/m, z31.h, z30.h
