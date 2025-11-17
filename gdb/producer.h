c6092d24a69f85235b908b88486760458a6b1adb
Tom de Vries <tdevries@suse.de>
2025-11-17 17:33:03 +0100

    [gdb] Fix producer_is_gas comment
    The comment for producer_is_gas in gdb/producer.h:
    ...
    /* Returns nonzero if the given PRODUCER string is GAS and sets the MAJOR
       and MINOR versions when not NULL.  Returns zero if the given PRODUCER
       is NULL or it isn't GAS.  */
    bool producer_is_gas (const char *producer, int *major, int *minor);
    ...
    uses 'nonzero' and 'zero', while the return type is bool.
    
    Fix this by using 'true' and 'false' instead.
    
    Tested on x86_64-linux.
    
    Approved-By: Simon Marchi <simon.marchi@efficios.com>

--- gdb/producer.h
+++ gdb/producer.h
@@ -30,8 +30,8 @@ extern int producer_is_gcc_ge_4 (const char *producer);
    is NULL or it isn't GCC.  */
 extern bool producer_is_gcc (const char *producer, int *major, int *minor);
 
-/* Returns nonzero if the given PRODUCER string is GAS and sets the MAJOR
-   and MINOR versions when not NULL.  Returns zero if the given PRODUCER
+/* Return true if the given PRODUCER string is GAS and sets the MAJOR
+   and MINOR versions when not NULL.  Returns false if the given PRODUCER
    is NULL or it isn't GAS.  */
 bool producer_is_gas (const char *producer, int *major, int *minor);
 
