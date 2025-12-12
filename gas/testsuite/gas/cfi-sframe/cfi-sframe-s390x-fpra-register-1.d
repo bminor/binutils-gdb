25b5c556297106b71570685a68117a0f00823a2e
Jens Remus <jremus@linux.ibm.com>
2025-12-12 16:08:27 +0100

    s390: gas: sframe: add handling for .cfi_register for FP and RA
    Signed-off-by: Jens Remus <jremus@linux.ibm.com>

--- gas/testsuite/gas/cfi-sframe/cfi-sframe-s390x-fpra-register-1.d
+++ gas/testsuite/gas/cfi-sframe/cfi-sframe-s390x-fpra-register-1.d
@@ -1,4 +1,4 @@
-#name: SFrame generation on s390x - RA and then FP saved in registers
+#name: SFrame generation on s390x - RA and then FP saved in FPR registers
 #objdump: --sframe=.sframe
 #...
 Contents of the SFrame section .sframe:
@@ -15,8 +15,8 @@ Contents of the SFrame section .sframe:
     func idx \[0\]: pc = 0x0, size = 26 bytes
     STARTPC +CFA +FP +RA +
     0+0000 +sp\+160 +u +u +
-    0+0004 +sp\+160 +u +r16 +
-    0+0008 +sp\+160 +r17 +r16 +
-    0+0014 +sp\+160 +u +r16 +
+    0+0004 +sp\+160 +u +r16\+0 +
+    0+0008 +sp\+160 +r17\+0 +r16\+0 +
+    0+0014 +sp\+160 +u +r16\+0 +
     0+0018 +sp\+160 +u +u +
 #pass
