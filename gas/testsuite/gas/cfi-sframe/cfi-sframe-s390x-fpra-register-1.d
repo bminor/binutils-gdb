675d3a1fb99bec757b8e52fcde621aca9ecbb167
Jens Remus <jremus@linux.ibm.com>
2025-12-15 15:37:58 +0100

    TODO: fixup! s390: gas: sframe: Represent .cfi_register FP/RA using FLEX_TOPMOST_FRAME
    Why does FLEX_TOPMOST_FRAME always have RA tracking information?

--- gas/testsuite/gas/cfi-sframe/cfi-sframe-s390x-fpra-register-1.d
+++ gas/testsuite/gas/cfi-sframe/cfi-sframe-s390x-fpra-register-1.d
@@ -14,9 +14,9 @@ Contents of the SFrame section .sframe:
 
     func idx \[0\]: pc = 0x0, size = 26 bytes
     STARTPC +CFA +FP +RA +
-    0+0000 +sp\+160 +u +u +
+    0+0000 +sp\+160 +u +U +
     0+0004 +sp\+160 +u +r16\+0 +
     0+0008 +sp\+160 +r17\+0 +r16\+0 +
     0+0014 +sp\+160 +u +r16\+0 +
-    0+0018 +sp\+160 +u +u +
+    0+0018 +sp\+160 +u +U +
 #pass
