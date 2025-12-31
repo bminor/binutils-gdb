35554f7b14c0cd1c59be64010de67d895906fb21
Andre Vieira <andre.simoesdiasvieira@arm.com>
2025-12-31 16:14:29 +0000

    arm, as: Support case incensitive VLDR/VSTR SYSREG
    To be consistent with VMRS/VMSR this changes VLDR/VSTR SYSREG to support case
    insensitive system register operands.

--- gas/config/tc-arm.c
+++ gas/config/tc-arm.c
@@ -6412,19 +6412,15 @@ parse_sys_vldr_vstr (char **str)
     {"P0",		0x5, 0x1},
     {"FPCXTNS",		0x6, 0x1},
     {"FPCXT_NS",	0x6, 0x1},
-    {"fpcxtns",		0x6, 0x1},
-    {"fpcxt_ns",	0x6, 0x1},
     {"FPCXTS",		0x7, 0x1},
     {"FPCXT_S",		0x7, 0x1},
-    {"fpcxts",		0x7, 0x1},
-    {"fpcxt_s",		0x7, 0x1}
   };
   char *op_end = strchr (*str, ',');
   size_t op_strlen = op_end - *str;
 
   for (i = 0; i < sizeof (sysregs) / sizeof (sysregs[0]); i++)
     {
-      if (!strncmp (*str, sysregs[i].name, op_strlen))
+      if (!strncasecmp (*str, sysregs[i].name, op_strlen))
 	{
 	  val = sysregs[i].regl | (sysregs[i].regh << 3);
 	  *str = op_end;
